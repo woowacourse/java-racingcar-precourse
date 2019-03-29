7bac4a8f5075dae5fa30ef737727ceaa86723dd3
DoWon Lee <gch01410@ajou.ac.kr>
2019-03-29 22:25:44 +0900

    Car 클래스에 자동차를 이동시키는 메소드 및 자동차의 이름과 위치를 반환하는 메소드 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,6 +1,17 @@
+/*
+ * @Car.java    0.1 2019/03/29
+ */
+
 package domain;
 
+/**
+ * 자동차의 이름과 위치를 갖는 클래스
+ *
+ * @author 이도원
+ * @version 0.1 2019/03/29
+ */
 public class Car {
+    private static final int SPEED = 1;
     private final String name;
     private int position = 0;
 
@@ -8,5 +19,18 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    /* 자동차를 전진시키는 메소드 */
+    public void move() {
+
+        /* 자동차는 한번 움직일 때 SPEED 값 만큼 전진한다 */
+        position += SPEED;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
