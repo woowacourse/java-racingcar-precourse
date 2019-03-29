c6c99e33e7208d7525caf6c5bf723c2e6e732138
heebg2 <heebg2@naver.com>
2019-03-29 22:39:33 +0900

    메인 기능 2 : Car 객체 초기화 및 생성 주석 및 테스트 코드 삭제 - 주식 및 확인 테스트 코드 삭제

--- src/main/java/play/PlayRacingGame.java
+++ src/main/java/play/PlayRacingGame.java
@@ -8,13 +8,8 @@ public class PlayRacingGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 1. 사용자로부터 car 이름 받기
 		String carNames[] = receiveCarNamesFromUser();
-		// 2. car 객체 생성
 		Car racingCars[] = initCarObjs(carNames);
-		for(Car c : racingCars) {
-			System.out.println("car : " + c.getName());
-		}
 		// 3. 게임 진행
 
 	}
