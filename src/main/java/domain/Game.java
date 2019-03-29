eaf9d99fa8fdb8bf8701335bc8956295dc7d4b7e
tjdds1109 <tjdds1109@naver.com>
2019-03-29 19:43:20 +0900

    문자열을 ( , ) 를 기준으로 나누고, 차이름들을 가지고 Car 객체 생성.

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -4,17 +4,37 @@ import java.util.Scanner;
 
 public class Game {
 
-    String carsName;
+    String carNames;
+    String carName[];
+    Car car[];
+
+    int carNumber;
 
     public void run() {
         InputCarsName();
-        //System.out.println(carsName);
+        splitCarNamesWithComma();
 
     }
 
     public void InputCarsName(){
         Scanner scan = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        carsName = scan.nextLine();
+        carNames = scan.nextLine();
     }
+
+    public void splitCarNamesWithComma(){
+        carName = carNames.split(",");
+        carNumber = carName.length;
+        initCarObject();
+    }
+
+    public void initCarObject(){
+        car = new Car[carNumber];
+        for(int i = 0;i < carNumber;i++){
+            car[i] = new Car(carName[i]);
+        }
+    }
+
+
+
 }
