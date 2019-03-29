12dfbdbe46938d99f985f71e5d51a822e622b676
mrkwon <erased11@gmail.com>
2019-03-29 17:06:36 +0900

    CarGame 에 시도 횟수를 저장할 변수 runs 와 입력 받을 getRunNumbers() 메서드 구현

--- src/main/java/domain/CarGame.java
+++ src/main/java/domain/CarGame.java
@@ -5,16 +5,24 @@ import java.util.Scanner;
 
 public class CarGame {
     private ArrayList<Car> cars;
+    private int runs;
 
     public CarGame() {
-        String[] tmpCarNames = getCarNamesInput();
-        this.cars = makeCarListFromArray(tmpCarNames);
+        this.cars = getCarNames();
+        this.runs = getRunNumbers();
     }
 
-    private String[] getCarNamesInput() {
+    private int getRunNumbers() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextInt();
+    }
+
+    private ArrayList<Car> getCarNames() {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return sc.next().split(",");
+        String[] tmp = sc.next().split(",");
+        return makeCarListFromArray(tmp);
     }
 
     private ArrayList<Car> makeCarListFromArray(String[] carNames) {
