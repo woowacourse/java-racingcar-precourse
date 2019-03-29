5b70cf48e97a1580db397be2d1fb8d1ae05ef2d0
andole87 <andole87@gmail.com>
2019-03-30 06:56:26 +0900

    CarGroup 클래스 생성 생성자 생성

---
+++ src/main/java/domain/CarGroup.java
@@ -0,0 +1,20 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class CarGroup {
+    private final Class<Car> clazz_car = Car.class;
+    private ArrayList<Car> carInstances;
+
+    public CarGroup(String[] args) {
+        carInstances = new ArrayList<Car>();
+        try {
+            for (int i = 0; i < args.length; i++) {
+                Car instance = clazz_car.getConstructor(String.class).newInstance(args[i]);
+                carInstances.add(instance);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
