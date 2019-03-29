d7ec0e6b4b6df8d0e0832df10598d810f238dd51
mrkwon <erased11@gmail.com>
2019-03-29 16:57:57 +0900

    게임 진행을 위한 CarGame 클래스 구현, getCarNamesInput() 메서드와 makeCarListFromArray()를 Main에서 이동

---
+++ src/main/java/domain/CarGame.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CarGame {
+    private ArrayList<Car> cars;
+    private int runNumbers;
+
+    private String[] getCarNamesInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return sc.next().split(",");
+    }
+
+    private ArrayList<Car> makeCarListFromArray(String[] carNames) {
+        ArrayList<Car> carArrayList = new ArrayList<>();
+        for (String carName : carNames) {
+            Car tmpCar = new Car(carName);
+            carArrayList.add(tmpCar);
+        }
+
+        return carArrayList;
+    }
+}
