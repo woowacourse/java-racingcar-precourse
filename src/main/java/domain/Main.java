2ac8c0e32cfe641d82c2c5e01d7168ba8fb7e454
chowhr1231 <thddbswo14@gmail.com>
2019-03-29 18:37:16 +0900

    [ADD] input CarNames and cnt function in UserInterface class

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -2,6 +2,13 @@ package domain;
 
 public class Main {
     public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        String[] carNames;
+        int cnt;
+        boolean flag = true;
+
+        carNames = ui.inputCarNames();
+        cnt = ui.inputCnt();
 
     }
 }
--- src/main/java/domain/UserInterface.java
+++ src/main/java/domain/UserInterface.java
@@ -1,5 +1,17 @@
 package domain;
 
-public class UserInterface {
+import java.util.Scanner;
 
+public class UserInterface {
+    private Scanner sc = new Scanner(System.in);
+
+    public String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+        return sc.nextLine().split(",");
+    }
+
+    public int inputCnt(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextInt();
+    }
 }
