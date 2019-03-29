f0fb3dbc375e69e9f32438cebc7b0d5cd2072dc9
heebg2 <heebg2@naver.com>
2019-03-29 22:31:38 +0900

    메인 기능 2 : Car 객체 초기화 및 생성 - 입력받은 이름들로 Car 객체들 초기화

--- src/main/java/play/PlayRacingGame.java
+++ src/main/java/play/PlayRacingGame.java
@@ -2,6 +2,8 @@ package play;
 
 import java.util.Scanner;
 
+import domain.Car;
+
 public class PlayRacingGame {
 
 	public static void main(String[] args) {
@@ -9,6 +11,10 @@ public class PlayRacingGame {
 		// 1. 사용자로부터 car 이름 받기
 		String carNames[] = receiveCarNamesFromUser();
 		// 2. car 객체 생성
+		Car racingCars[] = initCarObjs(carNames);
+		for(Car c : racingCars) {
+			System.out.println("car : " + c.getName());
+		}
 		// 3. 게임 진행
 
 	}
@@ -19,6 +25,15 @@ public class PlayRacingGame {
 		sc.close();
 		
 		return inputNames.split(",");
+	}
+	
+	private static Car[] initCarObjs(String[] carNames) {
+		Car racingCars[] = new Car[carNames.length];
+		for(int i = 0 ; i < racingCars.length ; i++) {
+			racingCars[i] = new Car(carNames[i]);
+		}
+		
+		return racingCars;
 	}
 
 }
