2d5aaf86bb0e16501c8701508385664bf144b007
andole87 <andole87@gmail.com>
2019-03-30 07:31:09 +0900

    RacingGame 클래스 생성 getInputConsole 메서드 생성

---
+++ src/main/java/domain/RacingGame.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.Scanner;
+public class RacingGame {
+
+    private String getInputConsole() {
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextLine();
+    }
+}
