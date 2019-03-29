466002f8a75d35cc0eb80c95aea74465f5861c02
mrkwon <erased11@gmail.com>
2019-03-29 16:54:22 +0900

    Main class 에 사용자로부터 input을 받기 위한 getCarNamesInput() 메서드와 primitive array를 ArrayList로 바꾸는 makeCarListFromArray() 메서드 구현

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,7 +1,31 @@
 package domain;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    public static String[] getCarNamesInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return sc.next().split(",");
+    }
+
+    public static ArrayList<Car> makeCarListFromArray(String[] carNames) {
+        ArrayList<Car> carArrayList = new ArrayList<>();
+        for (String carName : carNames) {
+            Car tmpCar = new Car(carName);
+            carArrayList.add(tmpCar);
+        }
+
+        return carArrayList;
+    }
+    public static void main(String[] args) {
+        String[] carNmaes = getCarNamesInput();
+        ArrayList<Car> carArrayList = makeCarListFromArray(carNmaes);
+
+        // car name 확인용
+        for (Car car : carArrayList) {
+            System.out.println(car.getName());
+        }
     }
 }
