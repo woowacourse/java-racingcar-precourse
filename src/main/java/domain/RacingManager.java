5eb843f37d963e3dc336bd44010b5c6a1eba7bf8
Lee Nam Jun <dlskawns96@gmail.com>
2019-03-30 22:02:12 +0900

    기능 구현<3-2, 3-3>

--- src/main/java/domain/RacingManager.java
+++ src/main/java/domain/RacingManager.java
@@ -25,4 +25,19 @@ public class RacingManager {
     private static int getRndNum() {
         return random.nextInt(10);
     }
+
+    /**
+     * 사용자에게 자동차들의 이름을 입력하라는 메시지를 출력하는 메소드
+     */
+    private static void nameEnterMsg() {
+        System.out.print("경주할 자동차 이름을 입력하세요.");
+        System.out.println("(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    /**
+     * 사용자에게 시도할 회수를 입력하라는 멧기지를 출력하는 메소드
+     */
+    private static void timesEnterMsg() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
