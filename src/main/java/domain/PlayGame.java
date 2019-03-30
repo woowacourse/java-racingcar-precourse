8a6c9daf115af836f3faf1070604d5cfbb8d1599
blueLibertas <jinhok7890@gmail.com>
2019-03-30 14:37:25 +0900

    Car 객체를 저장한 list를 클래스 variable로 변경

--- src/main/java/domain/PlayGame.java
+++ src/main/java/domain/PlayGame.java
@@ -1,30 +1,35 @@
 package domain;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.List;
 
 public class PlayGame {
+    Car[] carList;
+
     public void startGame() {
-        Car[] carList;
-        getCarNames(carList);
+        getCarNames();
+/*        for (int i = 0; i < Array.getLength(carList); i++) {
+            System.out.println(carList[i].getName());
+        }*/
     }
 
-    private void getCarNames(Car[] carList) {
+    private void getCarNames() {
         Scanner myScanner = new Scanner(System.in);
         String carNameList = "";
         Boolean correctInput = false;
 
         while (!correctInput) {
-            System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             carNameList = myScanner.nextLine();
             List<String> carNames = Arrays.asList(carNameList.split(","));
-            correctInput = makeCars(carNames, carList);
+            correctInput = makeCars(carNames);
             if (!correctInput)
                 System.out.println("잘못된 값이 있습니다. 다시 입력해주세요.");
         }
     }
 
-    private Boolean makeCars(List<String> carNames, Car[] carList) {
+    private Boolean makeCars(List<String> carNames) {
         int carNum = carNames.size();
         carList = new Car[carNum];
 
