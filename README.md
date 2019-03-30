7ff8561f15ea45b23b931c7706b39447b0d14162
hyojaekim <hj00686@naver.com>
2019-03-30 10:13:40 +0900

    ADD : 메인클래스 & 게임시작메소드

---
+++ README.md
@@ -0,0 +1,3 @@
+# 레이싱 경주 게임
+* RacingCar(class)
+    * 게임 시작하기(gameStart)
---
+++ src/main/java/domain/RacingCar.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class RacingCar {
+    private void gameStart(){
+
+    }
+    public static void main(String[] args){
+        RacingCar rc = new RacingCar();
+        rc.gameStart();
+    }
+}
