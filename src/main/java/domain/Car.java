71f1f88019548649baa68992ea4fe20ac4fc618b
HeavyRyan92 <heavyryan92@gmail.com>
2019-03-30 19:06:10 +0900

    자동차 경주 게임
    추가 사항
     1. separateCarNameState(String carName) 메소드 추가
       - (,) 를 기준으로 자동차 이름을 분리 했을때 5자이내 의 조건을 만족하는지 확인하는 메소드 추가
    
    수정 사항
     1. README 수정
       -  3.자동차 이름 구분 -> 3.자동차 이름 분리 상태 가능 확인  4. 3번과정에서 5자 이내 일경우 (,) 기준으로 분리
           (이전 3번의 기능은 2가지 기능을 포함하는것으로 판단 하여 이를  자동차 이름 분리 가능 상태 와 상태에 따라서 분리시키도록 수정)

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -16,12 +16,32 @@ public class Car {
         this.name = name;
     }
 
-
     public static String getCarName(){
 
         Scanner inputCarName = new Scanner(System.in);
         Car carName = new Car(inputCarName.nextLine());
 
         return carName.name;
+    }
+
+    /*(,) 를 기준으로 단어를 나눴을때 5자 이내 조건을 만족하는지 확인 하는 메소드*/
+    public static boolean separateCarNameState(String carName){
+
+        String[] carNameArray = carName.split(",");         //매개변수를 (,)기준으로 구분후에 배열저장
+
+        boolean carNameState = true;
+
+        for(int i=0;i<carNameArray.length;i++){
+
+            /*분리된 자동차 이름 배열 모두 확인 하기 위한 반복문*/
+            if(carNameArray[i].length()>5){
+
+                /*자동차 이름이 5자 초과일경우 */
+                System.out.println("자동차 이름은 5자 이하 까지만 허용합니다");
+                carNameState = false;                               // 자동차 이름 생성 불가
+                break;
+            }
+        }
+        return carNameState;
     }
 }
