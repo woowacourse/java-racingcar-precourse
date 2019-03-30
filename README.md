57494f5ac01beab5f5a51f65e8387ad35cd39c9c
hyojaekim <hj00686@naver.com>
2019-03-30 11:19:42 +0900

    add -> tatalTryCounter()

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ### RacingCar
 1. 게임 시작하기(gameStart)
 2. 전체 차 이름 입력받기(totalCarsName) / String(반환)
+3. 전체 횟수 입력받기(totalTryCounter) / int(반환)
 
 ### RacingCarGame
-1. 이름 분리하는 생성자(RacingCarGame(String cars))
-2. 자동차 이름 등록하기(registerCar(String cars))
+1. 이름 분리하기(RacingCarGame(String cars)) / 생성자
+2. 자동차 이름 등록하기(registerCar(String cars)) / 생성자에서 움직이는 메소드
--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -3,15 +3,24 @@ package domain;
 import java.util.Scanner;
 
 public class RacingCar {
-    private String totalCarsName(Scanner sc){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+
+    private String totalCarsName(Scanner sc) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return sc.nextLine();
     }
-    private void gameStart(){
+
+    private int totalTryCounter(Scanner sc) {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextInt();
+    }
+
+    private void gameStart() {
         Scanner sc = new Scanner(System.in);
         RacingCarGame rcg = new RacingCarGame(totalCarsName(sc));
+        int maxCount = totalTryCounter(sc);
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         RacingCar rc = new RacingCar();
         rc.gameStart();
     }
