38b076dcf41d7c0b8b64f5beef00932ec703a0aa
DoorSupporter <jiwon3346@gmail.com>
2019-03-31 02:16:50 +0900

    Divide Name

---
+++ src/main/java/domain/Config.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Config {
+    public String[] splitName(String name){
+        String[] nameArray = name.split(",");
+        return nameArray;
+    };
+}
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,4 +1,5 @@
 import domain.Car;
+import domain.Config;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,16 +8,24 @@ import static org.junit.Assert.*;
 
 public class CarTest {
     private Car car;
+    private Config config;
 
     @Before
     public void setup(){
         car = new Car("Tommi");
+        config = new Config();
     }
 
     @Test
     public void getNameTest(){
         String result = car.getName();
         assertEquals(new String("Tommi"), result);
+    }
+
+    @Test
+    public void splitName(){
+        String[] result = config.splitName("Tommi,Pride,Cago");
+        assertEquals(new String[]{"Tommi","Pride","Cago"},result);
     }
 
     @After
