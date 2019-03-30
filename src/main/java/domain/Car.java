be71d6f71168537f597b9752250d61290fec2559
chowhr1231 <thddbswo14@gmail.com>
2019-03-30 14:48:48 +0900

    [ADD] Function for updating position

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -6,23 +6,33 @@ public class Car {
     private final String name;
     private int position = 0;
 
+    private static final int STANDARD = 3;
+
     public Car(String name) {
         this.name = name;
     }
 
-    public static Car[] createCars(String[] carNames){
+    public static Car[] createCars(String[] carNames) {
         int N = carNames.length;
         Car[] cars = new Car[N];
 
         int idx = 0;
-        for(String carName : carNames){
+        for (String carName : carNames) {
             cars[idx++] = new Car(carName);
         }
 
         return cars;
     }
 
-    private int genertaeRandom(){
+    public void updatePosition() {
+        int randomNum = generateRandom();
+
+        if (randomNum > 3) {
+            position++;
+        }
+    }
+
+    private int generateRandom() {
         return new Random().nextInt(9);
     }
 }
