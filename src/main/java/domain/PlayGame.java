ac2942d3f7bc408208354560a6ff372464f9e28e
blueLibertas <jinhok7890@gmail.com>
2019-03-30 12:51:04 +0900

    차 객체 array 만들기

--- src/main/java/domain/PlayGame.java
+++ src/main/java/domain/PlayGame.java
@@ -5,22 +5,35 @@ import java.util.List;
 
 public class PlayGame {
     public void startGame() {
-        getCarNames();
+        Car[] carList;
+        getCarNames(carList);
     }
 
-    private void getCarNames() {
+    private void getCarNames(Car[] carList) {
         Scanner myScanner = new Scanner(System.in);
-        String carList = "";
+        String carNameList = "";
         Boolean correctInput = false;
 
         while (!correctInput) {
             System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            carList = myScanner.nextLine();
-            List<String> carNames = Arrays.asList(carList.split(","));
-            correctInput = makeCars(carNames);
+            carNameList = myScanner.nextLine();
+            List<String> carNames = Arrays.asList(carNameList.split(","));
+            correctInput = makeCars(carNames, carList);
             if (!correctInput)
                 System.out.println("잘못된 값이 있습니다. 다시 입력해주세요.");
         }
+    }
+
+    private Boolean makeCars(List<String> carNames, Car[] carList) {
+        int carNum = carNames.size();
+        carList = new Car[carNum];
+
+        for (int i = 0; i < carNum; i++) {
+            carList[i] = new Car(carNames.get(i));
+            if (!checkCorrectName(carList[i]))
+                return false;
+        }
+        return true;
     }
 }
 
