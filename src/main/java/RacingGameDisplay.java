e8b0f63b87f320cf2f5b85c4a1f3915d65a1c960
HeavyRyan92 <heavyryan92@gmail.com>
2019-03-30 17:00:42 +0900

    자동차 경주 게임
    추가 사항
     1. RacingGameDisplay 클래스 추가
       - 자동차 경주 게임 관련된 모든 문구를 출력 하는 기능을 가진 클래스 추가
    
    2. getRacingCarName 메소드 추가
       - 자동차 이름 입력 받는 문구를 출력하는 메소드 추가

---
+++ src/main/java/RacingGameDisplay.java
@@ -0,0 +1,15 @@
+
+/**
+ *  RacingGameDisplay 클래스는 RacingGame 에 관련된 모든 문구를 출력하는 기능을 가진 클래스입니다.
+ *
+ * @version 2019년 3월 30일
+ * @author 이중혁
+ */
+
+public class RacingGameDisplay {
+
+    public static void getRacingCarName{
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+}
