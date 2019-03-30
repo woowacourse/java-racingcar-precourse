2e69aab46248e513f5cdf3313ef52b98144082c1
Lee Nam Jun <dlskawns96@gmail.com>
2019-03-30 20:05:59 +0900

    기능 구현<1-1>

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,5 +1,17 @@
+/**
+ * Car.class        1.0 2019/03/26
+ *
+ * <Copyright 2019. LeeNamJun. All rights reserved.>
+ */
+
 package domain;
 
+/**
+ * Racing에서 자동차의 역할을 하는 클래스
+ *
+ * @version         1.0
+ * @author          이남준
+ */
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +20,13 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    /**
+     * random 넘버가 4이상일 경우 차량을 전진시키는 메소드
+     * @param rndNum RacingManager에서 생성한 난수
+     */
+    public void moveCar(int rndNum) {
+        if(rndNum >= 4) {
+            position++;
+        }
+    }
 }
