a54cfb70b2f44ec2702b7247606ebdb7a23467ea
Jinho <wootechco.temp@gmail.com>
2019-03-30 11:37:51 +0900

    GameManager 클래스 moveCars 완성

--- src/main/java/com/game/GameManager.java
+++ src/main/java/com/game/GameManager.java
@@ -1,4 +1,20 @@
 package com.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameManager {
+    private List<Car> cars = new ArrayList<>();
+
+    public GameManager(String[] names) {
+        for (String i : names) {
+            cars.add(new Car(i));
+        }
+    }
+
+    public void moveCars() {
+        for (Car i : cars) {
+            i.move();
+        }
+    }
 }
