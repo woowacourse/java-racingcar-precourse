84768740699485198df85a2bd8cdcb3a9b33ab45
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-03-30 19:54:25 +0900

    패키지 재정의

---
+++ src/main/java/com/github/seokhyeonchoi/game/Main.java
@@ -0,0 +1,5 @@
+package com.github.seokhyeonchoi.game;
+
+public class Main {
+
+}
---
+++ src/main/java/com/github/seokhyeonchoi/game/racingcar/Car.java
@@ -0,0 +1,12 @@
+package com.github.seokhyeonchoi.game.racingcar;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // 추가 기능 구현
+}
