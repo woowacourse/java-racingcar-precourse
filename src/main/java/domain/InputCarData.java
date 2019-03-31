f46d5acf00ca77a98db6e7c2d37555675ed0fc41
YuSeonChoi <marine8743@naver.com>
2019-03-31 17:07:42 +0900

    1. 클래스 구분 및 소스코드 생성

---
+++ src/main/java/domain/InputCarData.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.Scanner;
+
+public class InputCarData {
+    void inputData(){
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분합니다.");
+
+    }
+}
