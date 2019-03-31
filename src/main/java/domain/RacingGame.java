1c90692ef7bc99b0e144b70d75e8f44a6cd6efad
Soojin Roh <rsj3112@gmail.com>
2019-03-31 17:55:00 +0900

    interface 메서드 작성
    기능 구현 목록에 따라 interface에 구현할 메서드 작성
    RacingGame.java 파일에서 RacingGameInterface파일 임플리먼트

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,9 +1,52 @@
 package domain;
 
-public class RacingGame {
+import java.util.ArrayList;
+
+public class RacingGame implements RacingGameInterface {
+
+    @Override
+    public String inputPlayerName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean checkPlayerNameLength() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ArrayList<Car> makePlayers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String inputMoveCount() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean checkNumberOrNot() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ArrayList<Car> judgeWinner() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void startRace() {
+        // TODO Auto-generated method stub
+        
+    }
     
     public static void main(String[] args) {
         
     }
-
 }
--- src/main/java/domain/RacingGameInterface.java
+++ src/main/java/domain/RacingGameInterface.java
@@ -1,5 +1,15 @@
 package domain;
 
+import java.util.ArrayList;
+
 public interface RacingGameInterface {
+    
+    abstract String inputPlayerName();
+    abstract boolean checkPlayerNameLength();
+    abstract ArrayList<Car> makePlayers();
+    abstract String inputMoveCount();
+    abstract boolean checkNumberOrNot();
+    abstract ArrayList<Car> judgeWinner();
+    abstract void startRace();
 
 }
