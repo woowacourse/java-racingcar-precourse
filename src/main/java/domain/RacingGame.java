9acdb0d8f7ff81e461c9d57f9dd68f4cf9dc65d4
Soojin Roh <rsj3112@gmail.com>
2019-03-31 18:08:27 +0900

    inputPlayerName() : 자동차 이름 입력 받는 기능 구현
    Scanner 클래스의 nextLine()을 이용해 자동차 이름 입력 받기
    입력받은 String의 길이를 검사해 입력값의 유무 확인

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,13 +1,25 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RacingGame implements RacingGameInterface {
-
-    @Override
+    private Scanner sc = new Scanner(System.in);
+    private String playerName = "";
+    
     public String inputPlayerName() {
-        // TODO Auto-generated method stub
-        return null;
+        boolean nameNotInput = true;
+        
+        while (nameNotInput) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분)");
+            playerName = sc.nextLine();
+            
+            if (playerName.length() != 0) {
+                nameNotInput = false;
+            }
+        }
+
+        return playerName;
     }
 
     @Override
