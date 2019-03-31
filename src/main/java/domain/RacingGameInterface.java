8a0883111c79e221df88ea2051f54e79a05b08a8
Soojin Roh <rsj3112@gmail.com>
2019-03-31 18:25:52 +0900

    RacingGameInterface.java 메서드 및 주석 추가

--- src/main/java/domain/RacingGameInterface.java
+++ src/main/java/domain/RacingGameInterface.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 
 public interface RacingGameInterface {
     
+    /** 자동차 이름 입력 받기  */ 
     abstract String inputPlayerName();
     abstract ArrayList<String> convertStringNameToArrayList(String name);
-    abstract boolean checkPlayerNameLength();
+    abstract boolean checkPlayerNameLength(ArrayList<String> nameList);
+    abstract ArrayList<String> setPlayerName();
+    
+    /** 자동차 객체 생성  */
     abstract ArrayList<Car> makePlayers();
+    
+    /** 실행 회수 입력 받기  */
     abstract String inputMoveCount();
     abstract boolean checkNumberOrNot();
+    
+    /** 승자 판정  */
     abstract ArrayList<Car> judgeWinner();
+    
+    /** 게임 시작 */
     abstract void startRace();
 
 }
