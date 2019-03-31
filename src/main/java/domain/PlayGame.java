8d07d84b22e0dd40dd4ea5a9a5511d50461f6bd8
ArtisanK <fancyartisan@gmail.com>
2019-03-31 18:51:22 +0900

    Add PlatGame.java Add getData() method

---
+++ src/main/java/domain/PlayGame.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.Scanner;
+
+public class PlayGame {
+    private String inputString;
+    private int inputNum;
+    private String[] arr;
+    private Car[] cars;
+
+    private void getData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분");
+        this.inputString = sc.nextLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        this.inputNum = sc.nextInt();
+    }
+
+}
