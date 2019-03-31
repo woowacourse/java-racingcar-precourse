608d64e03c11625e2d4f5edc9c48256341ff017e
isbee <zidane0428@gmail.com>
2019-03-31 19:41:08 +0900

    0~9 사이의 난수 생성 기능 구현, README 수정

---
+++ src/main/java/domain/RandomNumGenerator.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.Random;
+
+public class RandomNumGenerator {
+
+    private Random random;
+
+    public RandomNumGenerator() {
+        random = new Random();
+    }
+
+    public int generateRandomNum() {
+        return random.nextInt(RandomNumBound.UPPER.getBound());
+    }
+
+    enum RandomNumBound {
+        UPPER(10), LOWER(0);
+
+        final int bound;
+
+        RandomNumBound(int bound) {
+            this.bound = bound;
+        }
+
+        public int getBound() {
+            return bound;
+        }
+    }
+}
---
+++ src/test/java/domain/RandomNumGeneratorTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RandomNumGeneratorTest {
+
+    @Test
+    public void testFailToGenerateValidRandomNumIfRandomNumBelowLowerBound() throws AssertionError {
+        RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
+
+        int random = randomNumGenerator.generateRandomNum();
+        System.out.println(random);
+
+        assertFalse(random < RandomNumGenerator.RandomNumBound.LOWER.getBound());
+    }
+
+    @Test
+    public void testFailToGenerateValidRandomNumIfRandomNumAboveUpperBound() throws AssertionError {
+        RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
+
+        int random = randomNumGenerator.generateRandomNum();
+        System.out.println(random);
+
+        assertFalse(random > RandomNumGenerator.RandomNumBound.UPPER.getBound());
+    }
+}
