09eed7db407910ea74fe0604ccd76542b6d2a4b2
conatuseus <conatuseus@gmail.com>
2019-03-31 14:47:18 +0900

    Add input car's name
    자동차 이름 입력받는 메서드 추가.
    5글자가 넘어가거나, 알파벳 이외의 문자가 들어오면 예외처리.
    
    Squashed commit of the following:
    
    commit a84fa557101ffaea533d267dbcf987fb46d64886
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Sat Mar 30 18:23:57 2019 +0900
    
        Add method for input
    
        경주할 자동차 입력을 위한 예외처리 메소드 추가.
        isNameValid 메소드에서는 이름의 길이, 영문자 확인, 중복된 이름 확인해서
        반환
    
    commit b2ce0478fc9c2654b6b47c9c5744405d9f91a0e4
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Sat Mar 30 17:23:50 2019 +0900
    
        Fix inputNameOfCar method, README.md
    
         inputNameOfCar에 입력받는 부분 추가
         README 수정.

--- src/main/java/com/conatuseus/racingcar/appview/AppView.java
+++ src/main/java/com/conatuseus/racingcar/appview/AppView.java
@@ -1,6 +1,8 @@
 package com.conatuseus.racingcar.appview;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public final class AppView {
     private static final Scanner sc=new Scanner(System.in);
@@ -13,7 +15,39 @@ public final class AppView {
     }
 
     public static void inputNameOfCar(){
-        outputLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String[] scannedName;
+        while (true){
+            outputLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+            scannedName=sc.nextLine().split(",");
+
+            if(isNamesValid(scannedName)){
+                break;
+            }
+            outputLine("잘못 입력하였습니다.");
+        }
     }
+
+    public static boolean isNamesValid(String[] scannedName){
+        for(String name : scannedName){
+            if((name.length()>5) || (!name.matches("^[a-zA-Z]*$"))){
+                return false;
+            }
+        }
+        return !isContains(scannedName);
+    }
+
+    public static boolean isContains(String[] scannedName){
+        Set<String> hashSet=new HashSet<>();
+        for(String name : scannedName){
+            if(hashSet.contains(name)){
+                return true;
+            }
+            hashSet.add(name);
+        }
+        return false;
+    }
+
+
+
 
 }
--- src/test/java/TestAppView.java
+++ src/test/java/TestAppView.java
@@ -1,3 +1,4 @@
+import com.conatuseus.racingcar.appview.AppView;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -8,7 +9,25 @@ public class TestAppView {
 
     @Test
     public void 자동차_이름_입력_Test(){
+        String[][] names={{"a","b","c","d","e"},            // 정상 입력
+                {"abc","aaaaa","ccccc","qwert","zxcvb"},    //정상입력
+                {"a"," abc","cde","dfg","eabc"},            // 빈칸 들어가서 비정상
+                {"a","asw8","c","d","e"},                   // 숫자 들어갔으므로 비정상
+                {"a","b","aoskdoskeok","d","e"},            // length때문에 비정상
+                {"a","b","c","1234","e"},                   // 숫자있는 이름 = 비정상
+                {"a","b","c","d","1234567"},                // 위와 동일
+                {"abcde","abcde","abcde","abcde","abcde"}   // 중복된 이름 비정상
+        };
 
+        assertThat(AppView.isNamesValid(names[0]),is(true));
+        assertThat(AppView.isNamesValid(names[1]),is(true));
+        assertThat(AppView.isNamesValid(names[2]),is(false));
+        assertThat(AppView.isNamesValid(names[3]),is(false));
+        assertThat(AppView.isNamesValid(names[4]),is(false));
+        assertThat(AppView.isNamesValid(names[5]),is(false));
+        assertThat(AppView.isNamesValid(names[6]),is(false));
+        assertThat(AppView.isNamesValid(names[7]),is(false));
     }
+
 
 }
