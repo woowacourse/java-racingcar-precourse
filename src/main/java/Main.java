76035e863a73c17003881acbd0aff3ce148ef34f
Jo Jinsik <kunggom@gmail.com>
2019-03-31 18:13:42 +0900

    실제 게임 진행에 필요한 코드는 Manager 클래스에 몰아넣기로 결정.

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import domain.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // todo
+    }
+}
---
+++ src/main/java/domain/Manager.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Manager {
+    // 게임 진행 관리. 실제 게임 플레이 관련 내용은 여기에 다 있다.
+
+    public static void main(String[] args) {
+        // 메소드 테스트용 공간.
+        // todo
+    }
+}
