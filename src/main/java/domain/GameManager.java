30bde099110d6666a33fff6cca9591bf9a72c90b
moonsung <visitant5@nate.com>
2019-04-01 21:54:42 +0900

    add startRacing,  Racing 정보를 받아오는 Method
    add init(), startRacging(), getRacinginformation() method

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -1,11 +1,36 @@
 package domain;
 
-public class GameManager {
-    private void init() {
+import java.util.ArrayList;
 
+public class GameManager {
+    private Message message;
+    private InputGameInformation inputgameinformation;
+    private ArrayList<Car> car;
+    private ArrayList<String> maxPositionCar;
+    private int gameCount, carLength, maxPosition;
+    private StringBuilder winner;
+    private int maxPositionCarLength = 0;
+
+    private void init() {
+        message = new Message();
+        inputgameinformation = new InputGameInformation();
+        car = new ArrayList<>();
+        gameCount = 0;
+        carLength = 0;
+        maxPosition = 0;
+        maxPositionCarLength = 0;
+        maxPositionCar = new ArrayList<>();
     }
 
     public void startRacing() {
+        init();
+        getRacingInformation();
+    }
 
+    private void getRacingInformation() {
+        inputgameinformation.inputCar();
+        car = inputgameinformation.getCar();
+        carLength = car.size();
+        gameCount = inputgameinformation.getGameCount();
     }
 }
