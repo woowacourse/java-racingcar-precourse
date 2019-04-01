134e5e7871e0385fc924665e9b40ec0b27c54f22
bactoria <bactoria@gmail.com>
2019-04-01 15:28:06 +0900

    FEAT :: 경주할 자동차 이름 입력받는 기능 추가

---
+++ src/main/java/RacingCar.java
@@ -0,0 +1,5 @@
+public class RacingCar {
+    public static void main(String[] args) {
+        final String carNamesWithComma = UserInterface.getCarNamesWithComma();
+    }
+}
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public interface UserInterface {
+
+    Scanner scanner = new Scanner(System.in);
+
+    static String getCarNamesWithComma() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return scanner.nextLine();
+    }
+}
