4013bf2abb8420d61496a81bb114750f16e05b74
Jo Jinsik <kunggom@gmail.com>
2019-04-02 01:11:08 +0900

    자동차 이름을 받아와 객체 목록을 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
+import java.util.*;
 import domain.*;
 
 public class Main {
+    /** 경주에 참여할 자동차들을 생성 */
+    private static List<Car> makeCarList() {
+        List<Car> newList = new ArrayList<Car>();
+        String[] names = Manager.getCarNames();
+        
+        for (String name: names) {
+            newList.add(new Car(name));
+        }
+        
+        return newList;
+    }
+    
+    /** main 진입점 */
     public static void main(String[] args) {
-        // todo
-        System.out.println("test");
+        List<Car> carList = makeCarList();
     }
 }
