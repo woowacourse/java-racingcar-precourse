c7cc92bc7365d1a055d108102d8408832f993e14
yun <blpomspo@gmail.com>
2019-04-02 03:55:45 +0900

    move 구현  - 자동차 이동하는 method

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,8 +1,11 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
     private int position = 0;
+    static final int threshold = 4;
 
     public Car(String name) {
         this.name = name;
@@ -14,6 +17,13 @@ public class Car {
 
     public int getPosition() {
         return this.position;
+    }
+
+    public void move() {
+        Random rand = new Random();
+        int randomValue = rand.nextInt(10);
+        int ismoving = randomValue > threshold ? 1 : 0;
+        this.position += ismoving;
     }
 
 }
