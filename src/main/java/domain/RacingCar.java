a5ee732c5d48dbff1a2ac3d66bdc4a2d49612eb4
choihz <coiio@naver.com>
2019-04-01 21:50:00 +0900

    RacingCar 클래스의 setCarNames() 생성

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -13,6 +13,14 @@ public class RacingCar {
     }
 
     public String[] setCarNames() {
+        Scanner scan = new Scanner(System.in);
+        String[] carNames;
+        do {
+            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+            String userInput = scan.nextLine();
+            carNames = userInput.split(",");
+        } while (!areValidNames(carNames));
+        return carNames;
     }
 
     public boolean areValidNames(String[] carNames) {
