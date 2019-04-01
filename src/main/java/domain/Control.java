0c977d26191ae17372bd6109a7dd1aa37ac376ca
DGpress <bgbg9655@naver.com>
2019-04-01 13:49:28 +0900

    Update Control.java
    유저로부터 몇회의 게임을 실행할지 입력받는 함수 구현

--- src/main/java/domain/Control.java
+++ src/main/java/domain/Control.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Control {
     Scanner sc = new Scanner(System.in);
     ArrayList<Car> cars = new ArrayList<Car>();
+    int limitN = 0;
 
     public void userInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
@@ -22,7 +23,8 @@ public class Control {
     }
 
     public void userInputLimitTime() {
-
+        System.out.println("시도할 회수는 몇회인가요?");
+        this.limitN = sc.nextInt();
     }
 
     public void gameStart() {
