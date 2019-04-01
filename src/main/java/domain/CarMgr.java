6ca5b7b72e6eaa712065848a9109e6d641d28207
huisam <huisam@naver.com>
2019-04-01 23:00:46 +0900

    CarMgr Class : Car 관리 클래스 생성, 설명 주석 작성

---
+++ src/main/java/domain/CarMgr.java
@@ -0,0 +1,29 @@
+/*
+ * @CarMgr.java	1.00 2019/04/01
+ * 
+ * Copyright(c)2019	HwiJin Hong.
+ * All right reserved.
+ * 
+ * 자동차 경주 프로그램
+ * 자동차에 대한 리스트를 관리하기 위한 클래스이다.
+ * 관리하는 기능은 다음과 같다.
+ * 1.자동차에 대한 정보들 저장한다.
+ * 2.자동차에게 전진 또는 정지 명령을 내린다.
+ * 3.자동차들의 상태 정보를 콘솔창에 출력해준다.
+ */
+
+package domain;
+
+import java.util.ArrayList;
+
+/**
+ * 자동차의 전체 리스트를 관리해주는 클래스
+ * 
+ * @version 1.00 2019년 4월 1일
+ * @author huisam
+ */
+public class CarMgr {
+
+	private ArrayList<Car> carList;
+	
+}
