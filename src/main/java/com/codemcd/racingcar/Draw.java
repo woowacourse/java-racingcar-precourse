8668c4aea5ebd384b77cd37877ca231b84b5a6b0
SUNGBUMPARK <psbum1609@gmail.com>
2019-04-01 20:29:48 +0900

    Draw 클래스 구현
    * Draw.java: 이동마다 각 자동차가 움직인 결과와 게임의 최종 결과인
    우승자를 출력하는 메서드를 구현했다.
    
    * README.md: 구현을 진행하면서 변경된 사항을 갱신하였다.

--- src/main/java/com/codemcd/racingcar/Draw.java
+++ src/main/java/com/codemcd/racingcar/Draw.java
@@ -1,4 +1,25 @@
 package com.codemcd.racingcar;
 
 public class Draw {
+
+    public void drawMoveResult(Car[] cars, int carsArraySize) {
+
+        for (int i = 0; i < carsArraySize; ++i) {
+            System.out.print(cars[i].getName() + " : ");
+            for (int j = 0; j < cars[i].getPosition(); ++j)
+                System.out.print("-");
+            System.out.println();
+        }
+    }
+
+    public void drawWhoIsWinners(String[] winners) {
+        for (int i = 0; i < winners.length; ++i) {
+            System.out.print(winners[i]);
+            if (i != winners.length - 1)
+                System.out.print(", ");
+        }
+
+        System.out.println("가 최종 우승했습니다.");
+    }
+
 }
