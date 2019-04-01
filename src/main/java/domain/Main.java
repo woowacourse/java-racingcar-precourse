5ad01c0d3c5e9d5a415fde03a1fdaf361101e27a
andole87 <andole87@gmail.com>
2019-04-01 18:23:46 +0900

    Main 클래스, 메서드 생성

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        RacingGame rg = new RacingGame();
+        rg.startRace();
+    }
+}
