563061dec24046e80a2eed327e669c2104812a7d
Jo Jinsik <kunggom@gmail.com>
2019-04-02 01:19:50 +0900

    게임의 1개 라운드를 진행하는 메소드 작성.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,23 @@ public class Main {
         return newList;
     }
     
+    /** 게임의 1개 라운드를 진행 */
+    private static void playOneRound(List<Car> carList) {
+        for (Car car : carList) {
+            Manager.randomForward(car);
+            System.out.println(car.getStatus());
+        }
+    }
+    
     /** main 진입점 */
     public static void main(String[] args) {
         List<Car> carList = makeCarList();
+        int tryCount = Manager.getTryCount();
+        System.out.println("\n실행 결과 :");
+        
+        for (int i = 0; i < tryCount; i++) {
+            playOneRound(carList);
+            System.out.println(); // 구분용 빈 줄 출력
+        }
     }
 }
