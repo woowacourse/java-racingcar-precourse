baee41aa561f3f59400de9468aeea3a9bb3c95de
chowhr1231 <thddbswo14@gmail.com>
2019-04-01 22:32:24 +0900

    [FIX] Fix hard-coding of values using final static

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -12,6 +12,9 @@ public class Car {
     private final String name;
     private int position = 0;
 
+    private final static int STANDARD_GO = 3;
+    private final static int BOUNDARY = 0;
+
     public Car(String name) {
         this.name = name;
     }
@@ -19,13 +22,13 @@ public class Car {
     public void updatePosition() {
         int randomNum = generateRandom();
 
-        if (randomNum > 3) {
+        if (randomNum > STANDARD_GO) {
             position++;
         }
     }
 
     private int generateRandom() {
-        return new Random().nextInt(9);
+        return new Random().nextInt(BOUNDARY);
     }
 
     public String getName() {
