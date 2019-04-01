6f88477a1fad12be196fb3c8aa9ccd8a8a628073
DGpress <bgbg9655@naver.com>
2019-04-01 13:47:37 +0900

    Update Control.java
    유저로부터 자동차의 이름을 입력 받을 함수 구현 및 저장할 변수 셋팅

--- src/main/java/domain/Control.java
+++ src/main/java/domain/Control.java
@@ -1,8 +1,24 @@
 package domain;
 
-public class Control {
-    public void userInputCarNames() {
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class Control {
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Car> cars = new ArrayList<Car>();
+
+    public void userInputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String carNames = sc.nextLine();
+        setCar(carNames);
+    }
+
+    public void setCar(String str) {
+        String[] carArray = str.split(",");
+
+        for (int i = 0; i < carArray.length; i++) {
+            this.cars.add(new Car(carArray[i], 0));
+        }
     }
 
     public void userInputLimitTime() {
@@ -14,6 +30,6 @@ public class Control {
     }
 
     public void gameEnd() {
-        
+
     }
 }
