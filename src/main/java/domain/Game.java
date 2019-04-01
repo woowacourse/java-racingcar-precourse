0ce49391b7bf9ef1f1d8ab0b6707c73200ada254
unknown <ddu0422@naver.com>
2019-04-01 12:35:02 +0900

    각 자동차 이름 쉼표로 구분

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Game {
 
     String carName;
+    String[] carNames;
 
     public void run() {
         InputCarName();
+        SplitWithComma();
     }
 
     public void InputCarName() {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         carName = sc.nextLine();
+    }
+
+    public void SplitWithComma() {
+        carNames = carName.split(",");
     }
 }
