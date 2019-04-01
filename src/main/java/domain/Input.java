41749c27f40d3f28cd5039e4816377989f64c828
이인권 <hjun4512@gmail.com>
2019-04-01 21:12:31 +0900

    ADD : Input 클래스 생성 및 enrollCarName(), tryNumber() 함수 구현 	* enrollCarName()함수 : 사용자가 자동차 이름을 등록하는 함수 	* tryNumber()함수 : 사용자가 몇회 시도할지 정하는 함수 	* 클래스는 싱글톤 패턴으로 구현

---
+++ src/main/java/domain/Input.java
@@ -0,0 +1,38 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    private Input() {
+        scanner = new Scanner(System.in);
+    }
+
+    private static class InputHolder {
+        public static final Input INSTANCE = new Input();
+    }
+
+    public static Input getInstance() {
+        return InputHolder.INSTANCE;
+    }
+
+    public ArrayList<Car> enrollCarName() {
+        ArrayList<Car> carList = new ArrayList<>();
+        String[] inputCarName = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < inputCarName.length; i++) {
+            carList.add(new Car(inputCarName[i]));
+        }
+
+        return carList;
+    }
+
+    public int tryNumber(){
+        int tryNumber = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+
+        return tryNumber;
+    }
+}
--- src/main/java/domain/RaceingGameController.java
+++ src/main/java/domain/RaceingGameController.java
@@ -1,6 +1,24 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class RaceingGameController {
 
-    public void gameStart(){}
+    public void gameStart(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        ArrayList<Car> carNameList = Input.getInstance().enrollCarName();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryNumber = Input.getInstance().tryNumber();
+
+        System.out.print("자동차 등록 이름 : ");
+        for(Car cars : carNameList){
+            System.out.print(cars.getName());
+        }
+        System.out.println();
+        System.out.println("시도할 횟수 : " + tryNumber);
+
+    }
+
+
 }
