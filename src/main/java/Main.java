b7c8c642ddd942cb88909b65c5765abd6555c214
SunHo Van <vsh123@naver.com>
2019-04-02 00:07:47 +0900

    refactor : 게임 실행을 위한 Main.java 수정 1. RacingCarGame 패키지 import 2. RacingCarGame 객체 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
 /*
- * @(#)Main.java        0.1 2019/04/01
+ * @(#)Main.java        0.2 2019/04/02
  *
  *
  */
+
+import domain.RacingCarGame;
 
 /**
  * RacingCarGame 실행을 위한 Main클래스 입니다.
  *
  * @author 반선호
- * @version 0.1 2019년 4월 01일
+ * @version 0.2 2019년 4월 02일
  */
 public class Main {
     public static void main(String[] args) {
-
+        RacingCarGame game = new RacingCarGame();
+        game.gameStart();
     }
 }
