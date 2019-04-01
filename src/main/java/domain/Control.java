09101d268ff426a065bb4ddba87f312494e2d10c
DGpress <bgbg9655@naver.com>
2019-04-01 13:42:04 +0900

    Update Main.java
    게임 진행을 4 파트로 나눠 Control class에 생성할 함수 설계

--- src/main/java/domain/Control.java
+++ src/main/java/domain/Control.java
@@ -1,5 +1,19 @@
 package domain;
 
 public class Control {
-    
+    public void userInputCarNames() {
+
+    }
+
+    public void userInputLimitTime() {
+
+    }
+
+    public void gameStart() {
+
+    }
+
+    public void gameEnd() {
+        
+    }
 }
--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -2,6 +2,11 @@ package domain;
 
 public class Main {
     public static void main(String[] args) {
+        Control control = new Control();
 
+        control.userInputCarNames();
+        control.userInputLimitTime();
+        control.gameStart();
+        control.gameEnd();
     }
 }
