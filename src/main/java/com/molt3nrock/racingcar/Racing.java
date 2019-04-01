bec51e716df574c42257812f516ec86795a061ef
molt3nrock <molt3nrock@github.com>
2019-04-01 11:57:37 +0900

    Racing클래스: 필드 정의
    List<Car> cars: Car 목록
    int simulationTimes: 시뮬레이션 시도 횟수

---
+++ src/main/java/com/molt3nrock/racingcar/Racing.java
@@ -0,0 +1,10 @@
+package com.molt3nrock.racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racing {
+
+    private List<Car> cars = new ArrayList<>();
+    private int simulationTimes;
+}
