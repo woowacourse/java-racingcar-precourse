7ff7839465d204ef3ea1b5c53367ddd2d828f18d
wbluke <pwb0127@naver.com>
2019-04-01 18:03:24 +0900

    update the functions progressing game and showing results in RacingCar class

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -1,11 +1,14 @@
 package domain;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class RacingCar {
+    public static final int TIME_TO_BREAK = 1000;
     private ArrayList<Car> cars;
     private int tries;
     private int maxLenOfName = 0;
+    private int positionOfWinners = 0;
 
     public RacingCar(ArrayList<Car> cars, int tries) {
         this.cars = cars;
@@ -13,6 +16,13 @@ public class RacingCar {
 
         calculateMaxLenOfName();
 
+        System.out.println("실행 결과");
+        pause();
+        progressGame();
+
+        ArrayList<String> winners = getWinners();
+        showResults(winners);
+        pause();
     }
 
     public void calculateMaxLenOfName() {
@@ -24,11 +34,75 @@ public class RacingCar {
         }
     }
 
+    public void progressGame() {
+        while (tries-- > 0) {
+            for (Car car : cars) {
+                car.accelerate();
+                System.out.println(getAlignedName(car) + " : "
+                        + getDisplayOfPosition(car));
+
+                updatePositionOfWinners(car);
+            }
+            System.out.println();
+            sleep(TIME_TO_BREAK);
+        }
+    }
+
     public String getAlignedName(Car car) {
         String name = car.getName();
         while (name.length() < maxLenOfName) {
             name += " ";
         }
         return name;
+    }
+
+    public String getDisplayOfPosition(Car car) {
+        StringBuffer hyphens = new StringBuffer();
+        for (int i = 0; i < car.getPosition(); i++) {
+            hyphens.append("-");
+        }
+        return hyphens.toString();
+    }
+
+    public void updatePositionOfWinners(Car car) {
+        if (car.getPosition() > positionOfWinners) {
+            positionOfWinners = car.getPosition();
+        }
+    }
+
+    public ArrayList<String> getWinners() {
+        ArrayList<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == positionOfWinners) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
+    public void showResults(ArrayList<String> winners) {
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i));
+            if (i != winners.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("(이)가 최종 우승했습니다.");
+    }
+
+    public void sleep(int timeToBreak) {
+        try {
+            Thread.sleep(timeToBreak);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void pause() {
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
