3746214ad215107bd7f0e6d93c89b5c14de44622
taejinseok <stj930@gmail.com>
2019-04-01 18:43:18 +0900

    실행용 Main클래스 추가   요구사항기반 실행코드  추가

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,32 @@
+package domain;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String names[];
+		int round;
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			System.out.print("경주할 자동차 이름을 입력하세요.");
+			System.out.println("(이름은 쉼표(,)를 기준으로 구분)");
+			names = sc.next().split(",");
+			
+			//글자수 예외처리 조건 통과못하면 continue 시키는 코드 추가
+			//names로 Car 객체들을 생성코드 추가.
+			
+			break;
+		}
+		System.out.println("시도할 회수는 몇회인가요?");
+		round = sc.nextInt();
+		
+		//round만큼 게임 실행 코드 추가
+		//게임 종료후 결과 출력하는 코드 추가
+		
+		sc.close();
+	}
+
+}
