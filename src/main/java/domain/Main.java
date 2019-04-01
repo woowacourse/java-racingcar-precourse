4f130a9821e35afa37d7c1075a2ece2f742ef96d
DGpress <bgbg9655@naver.com>
2019-04-01 14:15:37 +0900

    Update Main.java
    Main calss의 main 함수 주석

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         control.userInputCarNames();
         control.userInputLimitTime();
+        // 유저로부터 car 이름과 총 시도 횟수를 입력받아 게임을 셋팅할 함수
+
         control.gameStart();
         control.gameEnd();
     }
