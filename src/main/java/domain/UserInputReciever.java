a9a77920e4e9876595c7eaeeab1a1402e84e8960
kangmin <kangmin789@naver.com>
2019-04-01 21:43:34 +0900

    Readme.md 수정, isValidCarName메소드 추가, RecieveCarNamesFromUser -> tryToRecieveCarNamesFromUser로 네이밍 변경
    ReceiveCarNamesFromUser에서 예외처리를 판단하기 위해서 tryToRecieveCarNamesFromUser로 네이밍을 변경하였고, isValidCarName을 추가하였습니다.
    isValidCarName 기능은 차의이름이 5가 넘는지를 판단해주는 기능을 합니다.

--- src/main/java/domain/UserInputReciever.java
+++ src/main/java/domain/UserInputReciever.java
@@ -8,11 +8,16 @@ import java.util.StringTokenizer;
 
 public class UserInputReciever  {
     Scanner scanner = new Scanner(System.in);
+    static final int MAX_CAR_NAME_LENGTH = 5;
 
-    private String[] recieveCarnamesFromUser(){
+    private String[] tryToRecieveCarnamesFromUser(){
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)기준으로 구분");
         String carNames[] = scanner.nextLine().split(",");
         return carNames;
     }
+    private boolean isValidCarname(String carname){
+        return carname.length() < MAX_CAR_NAME_LENGTH ? true:false;
+    }
+    
 
 }
