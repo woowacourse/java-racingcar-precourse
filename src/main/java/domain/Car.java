f68e52f50ae91a292a7d6200e8b0ab1d27b36c37
Jo Jinsik <kunggom@gmail.com>
2019-04-02 00:39:38 +0900

    Car 객체의 name과 position에 대한 getter 생성.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -9,6 +9,16 @@ public class Car {
     }
 
     // 추가 기능 구현
+    /** 현재 자동차의 이름을 반환 */
+    public String getName() {
+        return this.name;
+    }
+    
+    /** 현재 자동차의 위치값을 반환 */
+    public int getPosition() {
+        return this.position;
+    }
+    
     /** 자동차를 1칸 앞으로 전진 */
     public void moveForward() {
         this.position++;
