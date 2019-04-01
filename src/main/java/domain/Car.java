db7eee2c53daaece247159207ed4517d38a5c115
imkimheejoo <jujudamdadi@naver.com>
2019-04-01 20:37:56 +0900

    [Add] goOrStop() 랜덤값에 따라 전진/멈추어 position을 변경하는 함수

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -8,5 +8,15 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    /**
+     * 랜덤값에 따라 전진할지 멈출지 결정하는 함수
+     * 전진하면 position은 1증가한다.
+     */
+    public void goOrStop(){
+        int ran=(int)Math.random()*10;
+        if(ran>=4){
+            position++;
+        }
+    }
+
 }
