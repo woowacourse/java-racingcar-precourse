a4b3d5918632dc34087d3469a93342ee311b3fa1
SunHo Van <vsh123@naver.com>
2019-04-01 22:59:58 +0900

    feat : Car 이동 후 출력기능 구현 1. 0 ~ 9 사이의 Random 숫자 생성 메소드(createRandomDistance) 2. 만들어진 Random 숫자가 4 이상인지 검사하는 메소드(isLagerThanFour) 3. 전진 메소드(movePosition) 4. Car 인스턴스의 현재 위치출력 메소드(printCurrentPosition) 5. 1번 ~ 4번 메소드를 실행 할 public 메소드(runOneCycle)

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,58 @@
+/*
+ * @(#)Main.java        0.1 2019/04/01
+ *
+ *
+ */
+
 package domain;
 
+import java.util.Random;
+
+/**
+ * Car 객체 생성을 위한 클래스입니다.
+ *
+ * @author 반선호
+ * @version 0.1 2019년 4월 01일
+ */
+
 public class Car {
+    private static final int MAXIMUM_RANDOM_VALUE = 10;
+    private static final int MOVE_DECISION_VALUE = 4;
+
     private final String name;
     private int position = 0;
+    private int distance;
 
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void runOneCycle() {
+        createRandomDistance();
+        if (isLagerThanFour()) {
+            movePosition();
+        }
+        printCurrentPosition();
+    }
+
+    private void createRandomDistance() {
+        Random random = new Random();
+        distance = random.nextInt(MAXIMUM_RANDOM_VALUE);
+    }
+
+    private boolean isLagerThanFour() {
+        return (distance >= MOVE_DECISION_VALUE);
+    }
+
+    private void movePosition() {
+        position++;
+    }
+
+    private void printCurrentPosition() {
+        System.out.print(name + " : ");
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
