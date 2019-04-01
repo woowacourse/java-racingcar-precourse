38c7a0a009d7a9af74a21371369a4fb47ef5ce3a
moonsung <visitant5@nate.com>
2019-04-02 00:45:11 +0900

    update startRacing() code convention

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -29,7 +29,7 @@ public class GameManager {
         if (!getRacingInformation()) {
             runRacing();
             judgeRacing();
-        }else{
+        } else {
             startRacing();
         }
     }
