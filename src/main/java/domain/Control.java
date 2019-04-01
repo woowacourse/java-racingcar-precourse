4e27b9454675405aee790bf119e2caf3c6f10ca1
DGpress <bgbg9655@naver.com>
2019-04-01 13:54:27 +0900

    Update Control.java
    turnStart() 구현
    한번의 턴 실행 시 랜덤 숫자로 해당 차량이 전진해야 할지 정지 할지를 정하는 함수

--- src/main/java/domain/Control.java
+++ src/main/java/domain/Control.java
@@ -2,11 +2,13 @@ package domain;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Control {
     Scanner sc = new Scanner(System.in);
     ArrayList<Car> cars = new ArrayList<Car>();
     int limitN = 0;
+    Random random = new Random();
 
     public void userInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
@@ -39,11 +41,18 @@ public class Control {
     }
 
     public void turnStart() {
-
+        for (int i = 0; i < cars.size(); i++) {
+            int ranNum = random.nextInt(10);
+            Car preCar = cars.get(i);
+            Car temp = (ranNum >= 4) 
+                       ? new Car(preCar.getName(), preCar.getPosition() + 1) 
+                       : preCar;
+            cars.set(i, temp);
+        }
     }
 
     public void printTurn() {
-        
+
     }
 
     public void gameEnd() {
