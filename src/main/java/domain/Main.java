a12be4d4e44b23709dd643b282748e4a80bbf9b5
taejinseok <stj930@gmail.com>
2019-04-01 20:14:36 +0900

    Main.java - main 메소드 수정 (자동차 객체 배열 생성 추가)

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -17,11 +17,16 @@ public class Main {
 			System.out.println("(이름은 쉼표(,)를 기준으로 구분)");
 			names = sc.next().split(",");
 			
-			if(!isEffectiveFormat(names)) continue;
-			//names로 Car 객체들을 생성코드 추가.
-			
-			break;
+			if(isEffectiveFormat(names)) { 
+				break;
+			}
 		}
+		
+		racingCars = new Car[names.length];
+		for (int i = 0; i < racingCars.length; i++) {
+			racingCars[i] = new Car(names[i]); 
+		}
+		
 		System.out.println("시도할 회수는 몇회인가요?");
 		round = sc.nextInt();
 		
