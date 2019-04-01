d5b1629a4820a7d3c919ca25afc9c600dd1ad44e
Jo Jinsik <kunggom@gmail.com>
2019-04-01 17:52:00 +0900

    getNames() 메소드 작성.
    문자열을 입력받아, 쉼표를 기준으로 나누어 문자열 배열로 반환하는 함수. 입력을 요구하는 메시지를 인자로 받아 출력한다.
    
    쉼표 뒤의 띄어쓰기 여부에 관계없이 입력을 처리하기 위해 정규표현식을 약간 사용했다.

--- src/main/java/domain/Manager.java
+++ src/main/java/domain/Manager.java
@@ -1,19 +1,31 @@
 package domain;
 
 import java.util.*;
+import java.io.Console;
 
 public class Manager {
     // 게임 진행 관리. 실제 게임 플레이 관련 내용은 여기에 다 있다.
-    
+
     /** 0에서 9 사이의 임의의 정수 하나를 생성하여 반환 */
-    public static int getRandomInt() {
+    private static int getRandomInt() {
         Random random = new Random();
         return random.nextInt(10);
+    }
+
+    /** 문자열을 입력받아, 쉼표를 기준으로 나누어 문자열 배열로 반환 */
+    private static String[] getNames(String message) {
+        Console console = System.console();
+        String inputData = console.readLine(message + "\n"); // 메시지 출력
+        return inputData.split(",\\s*"); // 정규 표현식 적용 - 띄어쓰기 때문
     }
 
     public static void main(String[] args) {
         // 메소드 테스트용 공간.
         // todo
         // System.out.println(getRandomInt());
+        // String[] test = getNames("test");
+        // for (String a: test) {
+        //     System.out.println(a);
+        // }
     }
 }
