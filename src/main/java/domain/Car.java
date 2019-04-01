79f052f3962236d0a65ca421f6f4da9dcf33cd0f
imkimheejoo <jujudamdadi@naver.com>
2019-04-01 21:45:18 +0900

    [Update] code convention 확인

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,6 +1,6 @@
 package domain;
 
-public class Car implements Comparable<Car>{
+public class Car implements Comparable<Car> {
     private final String name;
     private int position = 0;
 
@@ -8,26 +8,28 @@ public class Car implements Comparable<Car>{
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public int getPosition(){
+
+    public int getPosition() {
         return position;
     }
+
     /**
      * 랜덤값에 따라 전진할지 멈출지 결정하는 함수
      * 전진하면 position은 1증가한다.
      */
-    public void goOrStop(){
-        int ran=(int)(Math.random()*10);
-        if(ran>=4){
+    public void goOrStop() {
+        int ran = (int) (Math.random() * 10);
+        if (ran >= 4) {
             position++;
         }
     }
 
-    public String printPosition(){
-        StringBuilder positionStatus = new StringBuilder(name+" : ");
-        for(int i=0; i<position; i++){
+    public String printPosition() {
+        StringBuilder positionStatus = new StringBuilder(name + " : ");
+        for (int i = 0; i < position; i++) {
             positionStatus.append("-");
         }
         return positionStatus.toString();
@@ -38,6 +40,6 @@ public class Car implements Comparable<Car>{
      */
     @Override
     public int compareTo(Car o) {
-        return o.position-this.position;
+        return o.position - this.position;
     }
 }
--- src/main/java/playRacing/Main.java
+++ src/main/java/playRacing/Main.java
@@ -7,37 +7,36 @@ import java.util.Scanner;
 
 public class Main {
     static Car[] cars;
-    static final String WINNER_MESSAGE="가 최종 우승했습니다.";
-    static final String EXCEPTION_MESSAGE="자동차의 이름은 5글자 이하로 입력해주세요.";
-    static final String INPUT_NAMES_MESSAGE="경주할 자동차의 이름을 입력해주세요.(이름은 쉼표(,)기준으로 구분";
-    static final String INPUT_TRIAL_MESSAGE="시도할 횟수는 몇 회인가요?";
+    static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
+    static final String EXCEPTION_MESSAGE = "자동차의 이름은 5글자 이하로 입력해주세요.";
+    static final String INPUT_NAMES_MESSAGE = "경주할 자동차의 이름을 입력해주세요.(이름은 쉼표(,)기준으로 구분";
+    static final String INPUT_TRIAL_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
 
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         String[] carNames;
 
-        while(true){
+        while (true) {
             System.out.println(INPUT_NAMES_MESSAGE);
-            carNames=sc.nextLine().split(",");
-            if(isNameValid(carNames)){
+            carNames = sc.nextLine().split(",");
+            if (isNameValid(carNames)) {
                 break;
             }
         }
 
         System.out.println(INPUT_TRIAL_MESSAGE);
-        int trial=Integer.parseInt(sc.nextLine());
+        int trial = Integer.parseInt(sc.nextLine());
 
         namesMappingToCars(carNames);
 
-        for(int i=0; i<trial; i++){
+        for (int i = 0; i < trial; i++) {
             startAction();
             printAllPosition();
         }
 
         System.out.println(judgeWinner());
-
-
 
     }
 
@@ -51,10 +50,10 @@ public class Main {
         return true;
     }
 
-    public static void namesMappingToCars(String[] names){
+    public static void namesMappingToCars(String[] names) {
         cars = new Car[names.length];
-        for(int i=0; i<names.length; i++){
-            cars[i]=new Car(names[i]);
+        for (int i = 0; i < names.length; i++) {
+            cars[i] = new Car(names[i]);
         }
     }
 
@@ -74,13 +73,14 @@ public class Main {
         }
         System.out.println(allPosition.toString());
     }
-    public static String judgeWinner(){
+
+    public static String judgeWinner() {
         Arrays.sort(cars);
         StringBuilder winner = new StringBuilder(cars[0].getName());
-        int maxPosition=cars[0].getPosition();
-        for(int i=1; i<cars.length; i++){
-            if(cars[i].getPosition()==maxPosition){
-                winner.append(","+cars[i].getName());
+        int maxPosition = cars[0].getPosition();
+        for (int i = 1; i < cars.length; i++) {
+            if (cars[i].getPosition() == maxPosition) {
+                winner.append("," + cars[i].getName());
             }
         }
         winner.append(WINNER_MESSAGE);
