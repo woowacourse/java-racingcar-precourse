06e33109e18ac68caec9ebc48299d71af3ca236d
imkimheejoo <jujudamdadi@naver.com>
2019-04-01 20:54:34 +0900

    [Add] startAction():모든자동차의 액션(멈춤/전진)을 실행시키는 함수

--- src/main/java/playRacing/Main.java
+++ src/main/java/playRacing/Main.java
@@ -1,6 +1,10 @@
 package playRacing;
 
+import domain.Car;
+
 public class Main {
+    static Car[] cars;
+
     public static void main(String[] args) {
 
     }
@@ -12,5 +16,14 @@ public class Main {
             }
         }
         return true;
+    }
+
+    /**
+     *  모든 자동차의 액션(멈춤/전진)을 실행시키는 함수
+     */
+    public static void startAction(){
+        for(int i=0; i< cars.length; i++){
+            cars[i].goOrStop();
+        }
     }
 }
