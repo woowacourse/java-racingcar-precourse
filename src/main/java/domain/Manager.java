876124b8ccf12e448ae02dddeace204d0d82d63d
Jo Jinsik <kunggom@gmail.com>
2019-04-01 17:28:19 +0900

    getRandomInt() 함수 작성.
    0에서 9 사이의 임의의 정수 하나를 생성하여 반환하는 메소드.

--- src/main/java/domain/Manager.java
+++ src/main/java/domain/Manager.java
@@ -1,10 +1,19 @@
 package domain;
 
+import java.util.*;
+
 public class Manager {
     // 게임 진행 관리. 실제 게임 플레이 관련 내용은 여기에 다 있다.
+    
+    /** 0에서 9 사이의 임의의 정수 하나를 생성하여 반환 */
+    public static int getRandomInt() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
 
     public static void main(String[] args) {
         // 메소드 테스트용 공간.
         // todo
+        // System.out.println(getRandomInt());
     }
 }
