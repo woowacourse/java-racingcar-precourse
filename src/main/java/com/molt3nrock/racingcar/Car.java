97a7faa51bc3938aef01f6dcfc67c051813f1fef
molt3nrock <molt3nrock@github.com>
2019-04-01 10:07:55 +0900

    Car 클래스 필드 정의
    POSITION_INCREMENT: Car position 증분 상수

--- src/main/java/com/molt3nrock/racingcar/Car.java
+++ src/main/java/com/molt3nrock/racingcar/Car.java
@@ -1,12 +1,12 @@
 package com.molt3nrock.racingcar;
 
 public class Car {
+
+    private final static int POSITION_INCREMENT = 1;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
     }
-
-    // 추가 기능 구현
 }
