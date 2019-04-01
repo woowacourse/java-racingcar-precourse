2dfb28f3ab8e8d9533aceecc727649a90de12efe
SUNGBUMPARK <psbum1609@gmail.com>
2019-04-01 20:15:19 +0900

    Input 클래스 구현
    * Input.java: 자동차의 이름과 이동 횟수를 입력받는 메서드를 구현했다.
    
    * README.md: 구현을 진행하면서 변경된 사항을 갱신하였다.

--- src/main/java/com/codemcd/racingcar/Input.java
+++ src/main/java/com/codemcd/racingcar/Input.java
@@ -1,4 +1,31 @@
 package com.codemcd.racingcar;
 
+import java.util.Scanner;
+
 public class Input {
+
+    public String[] inputCarName() {
+
+        Scanner scanner = new Scanner(System.in);
+        String carNames;
+        String[] splitedNames;
+
+        System.out.println("경주할 자동차의 이름을 입력하세오(이름은 쉼표(,) 기준으로 구분");
+        carNames = scanner.nextLine();
+
+        splitedNames = carNames.split(",");
+
+        return splitedNames;
+    }
+
+    public int inputHowManyMove() {
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOfMove;
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        numberOfMove = scanner.nextInt();
+
+        return numberOfMove;
+    }
 }
