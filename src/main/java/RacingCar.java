129aca030212c4e0f282d6f07971b79f1677d1e9
yun <blpomspo@gmail.com>
2019-04-01 18:18:51 +0900

    자동차 이름들 입력받기  - 5글자 넘어가는 이름 출력하고 다시 입력받음  - 모든 이름이 5글자 이하면 HashSet으로 중복 제거 후 반환

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,17 +1,34 @@
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class RacingCar {
 
-    public static String[] inputCarName() {
+    public static Set<String> inputCarName() {
         Scanner sc = new Scanner(System.in);
-        String carNames = sc.nextLine();
-        String[] splitCarName = carNames.split(",");
-        return splitCarName;
+        String carNames;
+        String[] splitCarNames;
+        Set<String> distinctCarName = new HashSet<>();
+        boolean isNamingRule;
+
+        while (true) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            carNames = sc.nextLine();
+            splitCarNames = carNames.split(",");
+            isNamingRule = namingRule(splitCarNames);
+            if (isNamingRule) {
+                Collections.addAll(distinctCarName, splitCarNames);
+                break;
+            }
+        }
+        return distinctCarName;
     }
 
     public static boolean namingRule(String[] carNames) {
         for (String name: carNames) {
             if (name.length() > 5) {
+                System.out.printf("자동차 이름의 길이는 5입니다(%s: %d).\n", name, name.length());
                 return false;
             }
         }
@@ -19,8 +36,8 @@ public class RacingCar {
     }
 
     public static void main(String[] args) {
-        String[] carNames = inputCarName();
-        System.out.println(namingRule(carNames));
+        Set<String> carNames = inputCarName();
+        System.out.println(carNames);
     }
 
 }
