6f1d6dce945014633222c72e2046a3d2d337224e
choihz <coiio@naver.com>
2019-04-01 21:35:00 +0900

    RacingCar 클래스 생성

---
+++ src/main/java/domain/RacingCar.java
@@ -0,0 +1,32 @@
+package domain;
+import java.util.Scanner;
+
+public class RacingCar {
+    public int maxPosition = 0;
+
+    public static void main(String[] args) {
+        RacingCar race = new RacingCar();
+        race.play();
+    }
+
+    public void play() {
+    }
+
+    public String[] setCarNames() {
+    }
+
+    public boolean areValidNames(String[] carNames) {
+    }
+
+    public Car[] makeCars(String[] carNames) {
+    }
+
+    public int setNumOfGames() {
+    }
+
+    public void doGame(Car[] cars) {
+    }
+
+    public void decideWinner(Car[] cars) {
+    }
+}
