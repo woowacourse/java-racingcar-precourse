9d643442a538882d688a0d508a9b7e9e2589d110
andole87 <andole87@gmail.com>
2019-04-01 13:23:40 +0900

    startRace 메서드 생성

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -53,4 +53,44 @@ public class RacingGame {
         return cg.getMaxCar();
     }
 
+    public void startRace() {
+        String inputName;
+        String gameCountString;
+        int gameCountNumber;
+
+        while (true) {
+            String guidInputCarNameMessage = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분";
+            printConsole(guidInputCarNameMessage);
+            inputName = getInputConsole();
+
+            if (isValidInput(inputName)) {
+                break;
+            }
+
+            invalidInputMessage();
+        }
+
+        while (true) {
+            printConsole("시도할 횟수는 몇 회인가요?");
+            gameCountString = getInputConsole();
+
+            if (isNumber(gameCountString)) {
+                break;
+            }
+
+            invalidInputMessage();
+        }
+
+        gameCountNumber = Integer.parseInt(gameCountString);
+        CarGroup cg = reacyCarGroup(inputName);
+
+        while (gameCountNumber > 0) {
+            cg.makeCarRace();
+            printConsole(getResult(cg));
+            gameCountNumber--;
+        }
+
+        printConsole(getWinner(cg));
+    }
+
 }
