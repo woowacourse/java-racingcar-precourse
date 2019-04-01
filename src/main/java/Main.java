9971025086414df42cc722f0a4ffed0c0b8232e5
Jo Jinsik <kunggom@gmail.com>
2019-04-02 01:38:06 +0900

    최종 우승자 판정까지 모두 구현 완료.
    어라? 다 된 것 같은데?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,22 +15,48 @@ public class Main {
     }
     
     /** 게임의 1개 라운드를 진행 */
-    private static void playOneRound(List<Car> carList) {
+    private static int playOneRound(List<Car> carList) {
+        int max = 0;
+        int position;
+        
         for (Car car : carList) {
             Manager.randomForward(car);
             System.out.println(car.getStatus());
+            position = car.getPosition();
+            if (position > max) {
+                max = position;
+            }
         }
+        
+        return max;
+    }
+    
+    /** 최종 우승자를 판정 */
+    private static String getWinners(List<Car> carList, int maxPosition) {
+        List<String> winnerList = new ArrayList<String>();
+        
+        for (Car car: carList) {
+            if (car.getPosition() == maxPosition) {
+                winnerList.add(car.getName());
+            }
+        }
+        
+        return String.join(", ", winnerList);
     }
     
     /** main 진입점 */
     public static void main(String[] args) {
+        int maxPosition = 0;
         List<Car> carList = makeCarList();
         int tryCount = Manager.getTryCount();
         System.out.println("\n실행 결과 :");
         
         for (int i = 0; i < tryCount; i++) {
-            playOneRound(carList);
+            maxPosition = playOneRound(carList);
             System.out.println(); // 구분용 빈 줄 출력
         }
+        
+        String winners = getWinners(carList, maxPosition);
+        System.out.println(winners + "가 최종 우승했습니다.");
     }
 }
