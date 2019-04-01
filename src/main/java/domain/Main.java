1d842ddc818f480716591ef4f002796f1ad8bae9
wschoi8640 <cws911112@gmail.com>
2019-04-01 18:48:49 +0900

    UPDATE Main.java(start Program)

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -3,8 +3,21 @@ package domain;
  * 게임 프로그램을 시작하는 클래스
  *
  * @author wschoi8640
- * @version 1.0
+ * @version 1.1
  */
 public class Main {
 
+	private static SettingForm settingForm;
+	private static GameForm gameForm;
+	private static String[] gameSetting = null;
+	
+	/**
+	 * 게임 전체를 시작하는 메소드
+	 * 게임 정보를 받아 온 후 이를 이용해 게임 진행 클래스 진행
+	 */
+	public static void main(String[] args) {
+		settingForm = new SettingForm();
+		gameSetting = settingForm.getGameSetting();
+		gameForm = new GameForm(gameSetting);
+	}
 }
