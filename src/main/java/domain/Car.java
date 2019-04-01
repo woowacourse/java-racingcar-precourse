6f2c48455ac53ae9f7a4c06484e64b3b42203c0f
youngbae10000 <youngbae10000@gmail.com>
2019-04-01 17:13:58 +0900

    Car class : method를 작성한다 /randomValueCreate/accelertorAndbreak/playRacing/getName/getPosition

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,52 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	private int randomValueCreate() {
+
+		int randomNumber = 0;
+
+		while (true) {
+			
+			randomNumber = (int) (Math.random() * 10);
+			
+			if (randomNumber == 0)
+				continue;
+			return randomNumber;
+		}
+	}
+
+	public void acceleratorAndbreak() {
+
+		if (randomValueCreate() >= 4)
+			this.position++;
+
+	}
+
+	public void playRacing() {
+
+		System.out.print(this.name + ":");
+		for (int i = 0; i < this.position; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	} 
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
 }
