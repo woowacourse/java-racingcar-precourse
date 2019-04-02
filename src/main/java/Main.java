fdf53aae027c85bdd46ac23a2206bb3f4f59953f
YuJieun <idpresent@naver.com>
2019-04-02 15:34:50 +0900

    arrange code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,18 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 import domain.Car;
 
 public class Main {
     static Car[] cars;
     static String[] car_names;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        while(true){
+        while (true) {
             car_names = sc.nextLine().split(",");
-            if(checkName(car_names))
+            if (checkName(car_names))
                 break;
         }
         System.out.println("시도할 회수는 몇회인가요?");
@@ -19,7 +21,7 @@ public class Main {
         NameMapping();
 
         System.out.println("실행 결과");
-        for(int i=0;i<num;i++){
+        for (int i = 0; i < num; i++) {
             gameStart();
             printCarsPosition();
         }
@@ -29,7 +31,7 @@ public class Main {
 
     public static boolean checkName(String[] names) {
         for (int i = 0; i < names.length; i++) {
-            if(names[i].length()>5){
+            if (names[i].length() > 5) {
                 System.out.println("자동차 이름은 5자 이하로 입력해주세요");
                 return false;
             }
@@ -37,36 +39,36 @@ public class Main {
         return true;
     }
 
-    public static void NameMapping(){
+    public static void NameMapping() {
         cars = new Car[car_names.length];
-        for(int i=0;i<cars.length;i++){
+        for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(car_names[i]);
         }
     }
 
-    public static void gameStart(){
-        for(int i=0; i<cars.length;i++)
+    public static void gameStart() {
+        for (int i = 0; i < cars.length; i++)
             cars[i].chooseGoOrStop();
     }
 
-    public static void printCarsPosition(){
-        for(int i=0;i<cars.length;i++){
-            System.out.print(cars[i].getName()+": ");
+    public static void printCarsPosition() {
+        for (int i = 0; i < cars.length; i++) {
+            System.out.print(cars[i].getName() + ": ");
             int position = cars[i].getPosition();
-            for(int j=0; j<position; j++){
+            for (int j = 0; j < position; j++) {
                 System.out.print("-");
             }
             System.out.println();
         }
     }
 
-    public static void whoIsWinner(){
+    public static void whoIsWinner() {
         Arrays.sort(cars);
         int max_position = cars[0].getPosition();
         System.out.print(cars[0].getName());
-        for(int i=1;i<cars.length;i++){
-            if(cars[i].getPosition() == max_position)
-                System.out.print(", "+cars[i].getName());
+        for (int i = 1; i < cars.length; i++) {
+            if (cars[i].getPosition() == max_position)
+                System.out.print(", " + cars[i].getName());
         }
         System.out.println("가 최종 우승했습니다.");
     }
--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,6 +1,6 @@
 package domain;
 
-public class Car implements Comparable<Car>{
+public class Car implements Comparable<Car> {
     private final String name;
     private int position = 0;
 
@@ -8,28 +8,28 @@ public class Car implements Comparable<Car>{
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getPosition(){
+    public int getPosition() {
         return position;
     }
 
     /*
      * 랜덤값에 따라 position을 1만큼 증가시키거나 변화를 주지 않는 함수
      */
-    public void chooseGoOrStop(){
+    public void chooseGoOrStop() {
         int random = (int) (Math.random() * 10);
-        if(random > 4)
+        if (random > 4)
             position++;
     }
 
     /*
-    * descending order
-    */
+     * descending order
+     */
     @Override
-    public int compareTo(Car car){
+    public int compareTo(Car car) {
         return car.position - this.position;
     }
 
