6c15afcfe94d19dc728780b6ac77ad0a64d75d9b
je941004 <je941004@naver.com>
2019-04-03 01:12:45 +0900

    #5 make inputNames method

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,11 +1,15 @@
 package domain;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
         static Car[] cars;
         static int n;
 
-        public static void main(String args[]){
+        public static void main(String args[]) throws IOException{
                 inputNames();
                 inputRandomNumber();
                 playRace();
@@ -13,11 +17,15 @@ public class Main {
                 outputWinner();
         }
 
-        public static void inputNames(){
-
+        public static void inputNames() throws IOException {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은쉼표(,)기준으로구분)");
+                BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+                String names = bf.readLine();
+                String[] name = names.split(",");
+                makeCars(name);
         }
 
-        public static void makeCars(){
+        public static void makeCars(String[] name){
 
         }
 
