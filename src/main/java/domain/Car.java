0a3310075ef43176e3dcc0183f43be4310359208
U-Yeon <leanner@naver.com>
2019-04-02 12:11:43 +0900

    5) bringRandomNumberMovePosition의 생성, 랜덤한 값 만큼 position이동. maxPosition값 생성

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,12 +2,26 @@ package domain;
 
 public class Car {
 	public static int carNum;
+	public static int maxPosition=0;
+	
     private final String name;
     private int position = 0;
  
 
     public Car(String name) {
         this.name = name;
+    }
+    
+    public void bringRandomNumberMovePosition() {
+    	int randomNumber = (int)(Math.random()*10);
+    	
+    	if (randomNumber >= 4) {
+    		this.position++;
+    		
+    		if(Car.maxPosition == this.position) {
+    			Car.maxPosition = position;
+    		}
+    	}
     }
 
    
