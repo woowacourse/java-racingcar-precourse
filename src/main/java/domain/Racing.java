a2c006ba5d2fabfd118d6cff07d3d8449cc3db18
김성훈 <seonghun127@gmail.com>
2019-04-03 00:06:22 +0900

    process input missmath exception when inputing a round

--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -1,7 +1,7 @@
 /*
  * Racing
  *
- * version 1.4
+ * version 1.5
  *
  * 2019/04/02
  */
@@ -15,11 +15,12 @@ import java.util.Scanner;
  * 자동차 경주를 진행할 수 있는 클래스
  *
  * @author 김성훈
- * @version 1.0 2019/04/02  사용자로부터 자동차 이름, 이동 횟수를 입력 받는 메소드 구현 / 입력받은 자동차 이름으로 객체 생성
+ * @version 1.0 2019/04/02  사용자로부터 자동차 이름, 이동 회수를 입력 받는 메소드 구현 / 입력받은 자동차 이름으로 객체 생성
  *          1.1 2019/04/02  입력받은 자동차 이름에 빈칸 제거 / 이름 길이에 오류가 난 경우 다시 입력 받게 처리
  *          1.2 2019/04/02  자동차들중에서 제일 큰 position 값을 구하는 메소드 구현 / 시도할 회수를 받는 메소드 재 구현
  *          1.3 2019/04/02  우승자를 계산하는 메소드 구현
  *          1.4 2019/04/02  경주중인 모든 자동차가 난수에 의해 이동하는 메소드 구현 / 우승자 출력 메소드 구현
+ *          1.5 2019/04/02  이동 회수에 숫자 외의 값이 입력됐을 때 다시 입력을 받는 메소드 구현
  */
 public class Racing {
     private static Scanner sc = new Scanner(System.in);
@@ -54,8 +55,23 @@ public class Racing {
     }
 
     public int inputRound() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        return sc.nextInt();
+        String round;
+        do {
+            System.out.println("시도할 회수는 몇회인가요?");
+            round = sc.nextLine();
+        } while (!isNumber(round));
+
+        return Integer.parseInt(round);
+    }
+
+    public boolean isNumber(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("숫자만 입력 가능합니다.");
+            return false;
+        }
     }
 
     public void moveAllCarsOnRace(List<Car> cars) {
@@ -77,19 +93,19 @@ public class Racing {
     }
 
     public String calcWinners(List<Car> cars) {
-        String winners = "";
+        StringBuilder winners = new StringBuilder();
         int max = calcMaximumPosition(cars);
 
         for (Car car : cars) {
             if (max == car.getPosition()) {
-                winners += car.getName() + ", ";
+                winners.append(car.getName() + ", ");
             }
         }
 
-        return winners.substring(0, winners.length() - 2);
+        return winners.toString().substring(0, winners.length() - 2);
     }
 
     public void printWinners(String winners) {
-        System.out.println(winners + "가 최종 우승했습니다.");
+        System.out.println(winners + "가(이) 최종 우승했습니다.");
     }
 }
