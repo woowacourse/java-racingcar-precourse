7824984299edf71f5a39fd3cee59d1c310f54afc
NotYet <wiseoldster@gmail.com>
2019-04-02 09:52:07 +0900

    기능 구현 : 단위시간동안 차를 움직이는 기능 구현 (MoveOneRound()), 입력 기능(inputCarsName, inputNumMove) 변수 추가 : int threshold(차의 움직임 효율 조정), getThreshold() 추가 불필요 instance 함수 static 으로 변경

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,6 +3,7 @@ package domain;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Car {
@@ -18,6 +19,8 @@ public class Car {
 	private static ArrayList<Car> cars = new ArrayList<Car>();
 	private static int numMove = 0;
 
+	private final int threshold = 4;
+
 	public String getName() {
 		return this.name;
 	}
@@ -26,14 +29,22 @@ public class Car {
 		return this.position;
 	}
 
-	public void playGame() {
+	public int getThreshold() {
+		return this.threshold;
+	}
+
+	public static void playGame() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		inputCarsName(br);
 		inputNumMove(br);
+		for (int i = 0; i < numMove; i++) {
+			moveOneRound();
+			// printState();
+		}
 
 	}
 
-	private void inputCarsName(BufferedReader br) {
+	private static void inputCarsName(BufferedReader br) {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		try {
 			StringTokenizer st = new StringTokenizer(br.readLine(), ",");
@@ -45,7 +56,7 @@ public class Car {
 		}
 	}
 
-	private String validName(String s) throws Exception {
+	private static String validName(String s) throws Exception {
 		if (s.length() > 5) {
 			System.out.println("입력 이름 중 길이가 5 초과인 값이 있습니다.");
 			throw new Exception();
@@ -53,14 +64,27 @@ public class Car {
 		return s;
 	}
 
-	private void inputNumMove(BufferedReader br) {
+	private static void inputNumMove(BufferedReader br) {
 		System.out.println("시도할 회수는 몇회인가요?");
 		try {
 			numMove = Integer.parseInt(br.readLine());
 		} catch (Exception e) {
 			System.out.println("잘못된 입력입니다.");
-			inputCarsName(br);
+			inputNumMove(br);
 		}
+	}
+
+	private static void moveOneRound() {
+		Iterator<Car> it = cars.iterator();
+		while (it.hasNext()) {
+			moveOrNot(it.next());
+		}
+	}
+
+	private static void moveOrNot(Car c) {
+		int tmp = (int) Math.random() * 10;
+		if (tmp > c.getThreshold())
+			c.position++;
 	}
 
 }
