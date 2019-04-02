2a178bf0a5db6ab68048cc7f086e359923eca986
huisam <huisam@naver.com>
2019-04-02 14:12:32 +0900

    CarMgr Class : 가만 생각해보니, 똑같은 차이름이 있으면 혼선을 야기하므로 중복을 제거하는 기능 추가 - addNames() 메서드 중복 제거 기능 추가

--- src/main/java/domain/CarMgr.java
+++ src/main/java/domain/CarMgr.java
@@ -1,5 +1,5 @@
 /*
- * @CarMgr.java	1.01 	2019/04/02
+ * @CarMgr.java	1.02 	2019/04/02
  * 
  * Copyright(c)2019	HwiJin Hong.
  * All right reserved.
@@ -7,7 +7,7 @@
  * 자동차 경주 프로그램
  * 자동차에 대한 리스트를 관리하기 위한 클래스이다.
  * 관리하는 기능은 다음과 같다.
- * 1.자동차에 대한 정보들 저장한다.
+ * 1.자동차에 대한 정보들 저장한다.(중복 이름은 제거한다!)
  * 2.자동차에게 전진 또는 정지 명령을 내린다.
  * 3.자동차들의 상태 정보를 콘솔창에 출력해준다.
  * 4.1등 자동차들의 이름을 가져온다.
@@ -16,11 +16,12 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * 자동차의 전체 리스트를 관리해주는 클래스
  * 
- * @version 1.01 2019년 4월 2일
+ * @version 1.02 2019년 4월 2일
  * @author huisam
  */
 public class CarMgr {
@@ -32,8 +33,12 @@ public class CarMgr {
 	}
 
 	public void addNames(String[] names) {
+		HashSet<String> nameSet = new HashSet<>();
 		for (String name : names) {
-			carList.add(new Car(name));
+			if (!nameSet.contains(name)) {
+				carList.add(new Car(name));
+				nameSet.add(name);
+			}
 		}
 	}
 
