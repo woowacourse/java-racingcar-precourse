537013819f1d73ec7e2392c6435363c800a481b4
이인권 <hjun4512@gmail.com>
2019-04-02 16:41:46 +0900

    ADD : Output클래스 생성 및 gameResultMessage(), getWinner(), gameOngoingMessage()함수 구현 	* gameOngoingMessage() : 회수마다 자동차가 얼마나 갔는지 '-'로 보여주는 함수 	* gameResult() : 게임에 결과를 console로 출력해주는 함수 	* getWinner() : 게임의 승자를 따로 console에 나타내 주기 위한 함수 	* Output클래스는 싱글톤으로 구현 	* 모든 출력 구문은 static final로 상수 생성

---
+++ src/main/java/domain/Output.java
@@ -0,0 +1,62 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Output {
+
+    static final String outputEnrollCarName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String outputTryNumber = "시도할 회수는 몇회인가요?";
+    static final String outputResult = "가 최종 우승했습니다.";
+
+    private Output() {
+    }
+
+    private static class OutputHolder {
+        public static final Output INSTANCE = new Output();
+    }
+
+    public static Output getInstance() {
+        return OutputHolder.INSTANCE;
+    }
+
+    public void gameOngoingMessage(Car car) {
+        System.out.printf("%s : ", car.getName());
+        for (int j = 0; j < car.getPosition(); j++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public StringBuilder gameRusultMessage(ArrayList<Car> carNameList) {
+
+        StringBuilder result = new StringBuilder();
+        ArrayList<String> winnerList = new ArrayList<>();
+        getWinner(winnerList, carNameList);
+
+        for (int i = 0; i < winnerList.size()-1; i++) {
+            result.append(winnerList.get(i)).append(",");
+        }
+        result.append(winnerList.get(winnerList.size()-1));
+
+        return result;
+    }
+
+    private void getWinner(ArrayList<String> winnerList, ArrayList<Car> carNameList){
+
+        int max = -1;
+
+        for (int i = 0; i < carNameList.size(); i++) {
+            if (carNameList.get(i).getPosition() > max) {
+                max = carNameList.get(i).getPosition();
+                winnerList.clear();
+                winnerList.add(carNameList.get(i).getName());
+                continue;
+            }
+
+            if (carNameList.get(i).getPosition() == max) {
+                winnerList.add(carNameList.get(i).getName());
+            }
+        }
+    }
+
+}
