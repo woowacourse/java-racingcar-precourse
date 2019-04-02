52c633574721a5fce3d0c525a2014a82677ca0d6
qkrjh0904 <qkrjh0904@gmail.com>
2019-04-02 22:04:01 +0900

    1.2 자동차 이름이 5자 이하인지 확인

--- src/main/java/domain/GameMain.java
+++ src/main/java/domain/GameMain.java
@@ -10,13 +10,27 @@ public class GameMain {
 	static void nameDistinguish(String names){
 		nameArray = names.split(",");	//쉼표로 자동차 이름 분리해 배열로 저장
 	}
+	
+	//이름이 5자 이하인지 확인하는 메소드
+	static boolean isNameLessThan5(){
+		for(int i=0; i<nameArray.length; ++i){
+			if(nameArray[i].length()>5){
+				System.out.println("자동차 이름은 5자 이하만 가능합니다.");
+				System.out.println("경주할 자동차 이름을 입력하세요. 이름은 쉼표(,) 기준으로 구분");
+				return false;
+			}
+		}
+		return true;
+	}
 		
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		
-		String names = in.next();	//자동차 이름 입력
-		nameDistinguish(names);		//쉼표 기준으로 이름 구분
+		do{
+			String names = in.next();	//자동차 이름 입력
+			nameDistinguish(names);		//쉼표 기준으로 이름 구분
+		}while( !isNameLessThan5() );	//이름이 5자 이하일 때 까지 반복
 
 	}
 
