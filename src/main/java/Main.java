e4011925c624a9878cea3f719c2b37e2f78ebfe1
kmdngyu <kmdngyu@gmail.com>
2019-04-02 18:23:12 +0900

    add : 유저 입력 클래스

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import util.Input;
+
+public class Main {
+
+    /**
+     * 게임 진행 메인 함수
+     * @param args
+     */
+    public static void main(String[] args) {
+        Input getUserInput = new Input();
+
+    }
+}
---
+++ src/main/java/util/Input.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class Input {
+    private Scanner scan = new Scanner(System.in);
+    public int gameTimes = 0;
+
+
+    public void gameTimesInput(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+    }
+    public void carNamesInput(){
+
+    }
+
+}
