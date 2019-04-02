2c91c55226a1aced10dedaf93b6e02b00e85947d
hyperpace <cnk9109@gmail.com>
2019-04-02 15:40:49 +0900

    add method askCarNames() to GameUI.java

--- src/main/java/domain/GameUI.java
+++ src/main/java/domain/GameUI.java
@@ -1,9 +1,21 @@
 package domain;
 
+import java.util.Scanner;
+
 public class GameUI {
 	private Game game;
 
 	public GameUI() {
 		this.game = new Game();
+	}
+
+	public String askCarNames(Scanner sc) {
+		String carNames;
+		String info = "경주할자동차이름을입력하세요.(이름은쉼표(,)기준으로구분)";
+
+		System.out.println(info);
+		carNames = sc.nextLine();
+
+		return carNames;
 	}
 }
