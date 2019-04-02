91d69a1797da00fb018141d8e002680d59e3ecfe
samsung <samsung@LAPTOP-859NPSQ7>
2019-04-02 14:31:54 +0900

    1. (2) 자동차 이름 입력에 대한 예외 처리

--- src/main/java/domain/RacingGameManager.java
+++ src/main/java/domain/RacingGameManager.java
@@ -12,8 +12,20 @@ public class RacingGameManager {
 	Scanner scanner = new Scanner(System.in);
 
 	public String getCarNamesFromUser() {
-		System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-		String carNames = scanner.nextLine();
+		String carNames;
+		do {
+			System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+			carNames = scanner.nextLine();
+		} while (!checkCarNames(carNames));
 		return carNames;
+	}
+
+	// 자동차 이름을 입력했는지 확인합니다.
+	public boolean checkCarNames(String carNames) {
+		if (carNames.length() == 0) {
+			System.out.println("자동차 이름이 입력되지 않았습니다.");
+			return false;
+		}
+		return true;
 	}
 }
