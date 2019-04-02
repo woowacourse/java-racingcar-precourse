5b4504d1a7e0ed47ffcbf81a33a3f1eba5f19e2d
parkjunghun <go9018@naver.com>
2019-04-02 13:35:09 +0900

    ErrorCodes class 및 README 수정

--- src/main/java/manager/ErrorCodes.java
+++ src/main/java/manager/ErrorCodes.java
@@ -1,6 +1,6 @@
 package manager;
 
-public class ErrorCodes {
+class ErrorCodes {
     static final String INPUT_FORMAT_ERROR = "시도할 횟수는 숫자만 입력해주세요!";
     static final String INPUT_LENGTH_ERROR = "자동차 이름은 5글자 이하로 입력해주세요!";
 }
