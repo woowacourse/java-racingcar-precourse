2e775c3251d48f06c21fea1331d5b88ff3457be7
usang <usang>
2019-04-02 17:59:17 +0900

    Car 클래스의 위치 갱신 기능 구현
    getRandomNumber() 를 통해 0~9사이의 난수를 생성하고, updatePosition()에서 난수를 받아 Car 객체의 position을 갱신합니다

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,40 @@
+/*
+ * 이 클래스는 자동차 경주 게임에서 사용되는 자동차에 대한 클래스입니다
+ *
+ *
+ * 클래스 이름    Car
+ *
+ * 버전 정보     1.0
+ *
+ * 날짜    2019/04/02
+ *
+ * 저작권   권유상
+ */
+
 package domain;
+
+import java.util.Random;
 
 public class Car {
     private final String name;
     private int position = 0;
+    private static final int GO = 4;
+    private static final int RANDOM_CONDITION = 9;
 
     public Car(String name) {
         this.name = name;
     }
 
+    public void updatePosition() {
+        int randomNumber = getRandomNumber();
+        if(randomNumber >= GO) {
+            position++;
+        }
+    }
+
+    private int getRandomNumber() {
+        Random ran = new Random();
+        return ran.nextInt(RANDOM_CONDITION);
+    }
 
 }
