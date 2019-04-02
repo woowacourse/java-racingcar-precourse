dcd9e9fe6cc1a344d92bfb045cc34f3e5b3981b2
김성훈 <seonghun127@gmail.com>
2019-04-02 22:59:53 +0900

    implement function 4-1

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,7 +1,7 @@
 /*
  * Car
  *
- * version 1.1
+ * version 1.2
  *
  * 2019/04/02
  *
@@ -15,6 +15,7 @@ package domain;
  * @author 김성훈
  * @version 1.0 2019/04/02  자동차의 이동 여부를 결정하는 난수 발생 메소드 구현 / 난수값을 확인 후 자동차 이동 처리
  *          1.1 2019/04/02  자동차의 이동한 거리를 출력하는 메소드 구현
+ *          1.2 2019/04/02  Car 클래스의 position을 외부에서 얻을 수 있는 get 메소드 구현
  */
 public class Car {
     private final String name;
@@ -22,6 +23,10 @@ public class Car {
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 
     public int makeRandomNumber() {
@@ -36,7 +41,7 @@ public class Car {
 
     public void printTotalDistance() {
         System.out.print(this.name + " : ");
-        for(int i=0; i<position; i++){
+        for (int i = 0; i < position; i++) {
             System.out.print("-");
         }
         System.out.println();
--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -18,6 +18,7 @@ import java.util.Scanner;
  * @author 김성훈
  * @version 1.0 2019/04/02  사용자로부터 자동차 이름, 이동 횟수를 입력 받는 메소드 구현 / 입력받은 자동차 이름으로 객체 생성
  *          1.1 2019/04/02  입력받은 자동차 이름에 빈칸 제거 / 이름 길이에 오류가 난 경우 다시 입력 받게 처리
+ *          1.2 2019/04/02  자동차들중에서 제일 큰 position 값을 구하는 메소드 구현 / 시도할 회수를 받는 메소드 재 구현
  */
 public class Racing {
     private static Scanner sc = new Scanner(System.in);
@@ -25,17 +26,17 @@ public class Racing {
     public String[] inputCarName() {
         String[] carNames;
 
-        do{
+        do {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            carNames= sc.nextLine().replaceAll(" ", "").split(",");
-        }while(hasCarNameException(carNames));
+            carNames = sc.nextLine().replaceAll(" ", "").split(",");
+        } while (hasCarNameException(carNames));
 
         return carNames;
     }
 
-    public boolean hasCarNameException (String[] carNames) {
-        for(String carName : carNames){
-            if(carName.length() == 0 || carName.length() > 5) {
+    public boolean hasCarNameException(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.length() == 0 || carName.length() > 5) {
                 System.out.println("자동차 이름이 잘못된 형식으로 입력됐습니다");
                 return true;
             }
@@ -45,13 +46,24 @@ public class Racing {
 
     public List<Car> createCars(String[] carNames) {
         List<Car> cars = new ArrayList<>();
-        for(String carName : carNames) {
+        for (String carName : carNames) {
             cars.add(new Car(carName));
         }
         return cars;
     }
 
-    public int inputCount() {
+    public int inputRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
         return sc.nextInt();
+    }
+
+    public int calcMaximunPostion(List<Car> cars) {
+        int max = 0;
+        for (Car car : cars) {
+            if (max < car.getPosition()) {
+                max = car.getPosition();
+            }
+        }
+        return max;
     }
 }
