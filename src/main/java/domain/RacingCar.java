2fe7f51ff29f17bdd4517c20bfb1142d296bfe89
Seongmo Jeon <oeeen3@gmail.com>
2019-04-02 21:20:38 +0900

    Car Class: getCarName 추가, RacingCar Class: receiveCarName 추가 getCarName: 생성된 객체의 name을 return 한다. receiveCarName: 안내문 출력 및 사용자로부터 자동차 이름을 받아 List에 넣어 return 한다.

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -1,6 +1,6 @@
 /**
  * RacingCar 클래스는 자동차 경주를 하기 위한 클래스이다.
- * 구성 Method는 goRacing, generateRandomNumber, 필요한 것들..?
+ * 구성 Method는 goRacing, receiveCarName, generateRandomNumber, 필요한 것들..?
  * Last modified: 2019.04.02
  * Author: SM Jeon
  */
@@ -21,12 +21,39 @@ public class RacingCar {
     private static void goRacing() {
         Scanner s = new Scanner(System.in);
         int gameCount = 0;
+        ArrayList<Car> carList = new ArrayList<Car>();
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        //receiveCarName();
+        carList = receiveCarName();
         System.out.println("시도할 회수는 몇회인가요?");
         gameCount = s.nextInt();
         //printCarList();                   // 각 시도 회수마다 결과 출력
         //printResult();                    // 결과 출력: 우승자1, 우승자2 가 최종 우승했습니다.
+    }
+
+    /*
+     * Method for receive Car Name
+     * 차의 이름을 사용자로부터 받는다. ","를 기준으로 이름을 나누어 ArrayList에 저장하고 최종 List를 return한다.
+     * 이름은 5자 이하여야 한다.
+     */
+    private static ArrayList<Car> receiveCarName() {
+        Scanner s = new Scanner(System.in);
+        ArrayList<Car> carList = new ArrayList<Car>();
+        String nameLine;
+        String[] nameList;
+        int nameCount = 0;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        nameLine = s.nextLine();
+
+        nameList = nameLine.split(",");
+        for (String name : nameList) {
+            if (name.length() > 5) {
+                System.out.println("자동차의 이름은 5자까지만 허용합니다. 다시 입력해주세요.");
+                return receiveCarName();
+            }
+            Car tempCar = new Car(name);
+            carList.add(tempCar);
+        }
+        return carList;
     }
 }
