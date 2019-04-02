041bc9e539c8ebb5d1718604e15c26ade0ef0673
je941004 <je941004@naver.com>
2019-04-03 00:42:17 +0900

    #1 make movePosition method

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,29 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+        private final String name;
+        private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+        public Car(String name) {
+                this.name = name;
+        }
 
-    // 추가 기능 구현
+        public void movePosition() {
+                if(drawRandomNumber()){
+                        position++;
+                }
+
+        }
+
+        public boolean drawRandomNumber(){ //뼈대 구축
+                return true;
+        }
+
+        public int getPosition(){
+                return 0;
+        }
+
+        // 추가 기능 구현
 }
