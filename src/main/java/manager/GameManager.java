7627c19100e800e7149183d677ebdab774174dd0
parkjunghun <go9018@naver.com>
2019-04-02 12:39:14 +0900

    사용자 입력 메소드 분리 및 자동차 이름 글자 체크 메소드  수정

--- src/main/java/manager/GameManager.java
+++ src/main/java/manager/GameManager.java
@@ -9,36 +9,63 @@ public class GameManager {
     private Car[] cars;
     private int carNumber;
     private StringBuilder sb = new StringBuilder();
+    private Scanner scanner = new Scanner(System.in);
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private int gameLoop;
 
-    public void startGame() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Scanner scanner = new Scanner(System.in);
-        String carNames = scanner.next();
-        makeCarObjects(carNames);
-
-        System.out.println("시도할 횟수는 몇회인가요?");
-        int gameRepeat = scanner.nextInt();
-        printGameState(gameRepeat);
-        System.out.println();
+    public void start() {
+        initRacing();
+        printGameState(gameLoop);
     }
 
-    private void makeCarObjects(String carNames) {
-        String[] carNameArray = carNames.split(",");
-        carNumber = carNameArray.length;
-        cars = new Car[carNameArray.length];
+    private void initRacing() {
 
-        while (!checkCarNameLength(carNameArray)) {
-            startGame();
+        boolean nameLengthChecker = false;
+        String[] carNames = null;
+
+        while (!nameLengthChecker) {
+            carNames = enterCarNames();
+            nameLengthChecker = checkCarNameLength(carNames);
+        }
+        
+        makeCarObjects(carNames);
+        enterLoopCount();
+    }
+
+    private String[] enterCarNames() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = scanner.next();
+
+        return carNames.split(",");
+
+    }
+
+    private void enterLoopCount() {
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        
+        gameLoop = scanner.nextInt();
+
+        System.out.println();
+
+    }
+
+    private void makeCarObjects(String[] carNames) {
+
+        carNumber = carNames.length;
+        cars = new Car[carNames.length];
+
+        for (int i = 0; i < carNames.length; i++) {
+            cars[i] = new Car(carNames[i]);
         }
 
-        for (int i = 0; i < carNameArray.length; i++) {
-            cars[i] = new Car(carNameArray[i]);
-        }
     }
 
     private boolean checkCarNameLength(String[] carNames) {
-        for (int i = 0; i < carNames.length; i++) {
-            if (carNames[i].length() > 5) {
+
+        for (String carName : carNames) {
+            if (carName.length() > MAX_CAR_NAME_LENGTH) {
                 System.out.println("자동차의 이름은 5글자 이하로 입력해주세요!");
                 return false;
             }
@@ -47,6 +74,7 @@ public class GameManager {
     }
 
     private void printGameState(int gameRepeat) {
+
         System.out.println("실행결과");
 
         for (int i = 0; i < gameRepeat; i++) {
@@ -55,16 +83,20 @@ public class GameManager {
             }
             System.out.println();
         }
+
         findWinner();
+
     }
 
     private void printWinner(ArrayList<String> winnerList) {
+
         int checkWinnerNum = 0;
 
         if (winnerList.size() == 1) {
             System.out.println(winnerList.get(0) + "가 최종 우승했습니다.");
             return;
         }
+
         for (int i = 0; i < winnerList.size(); i++) {
             sb.append(winnerList.get(i));
             checkWinnerNum++;
@@ -73,10 +105,13 @@ public class GameManager {
             }
         }
         sb.append("가 최종 우승했습니다.");
+
         System.out.println(sb.toString());
+
     }
 
     private void findWinner() {
+
         int winnerPosition = cars[0].getPosition();
         ArrayList<String> winnerList = new ArrayList<>();
 
@@ -85,13 +120,15 @@ public class GameManager {
                 winnerPosition = cars[i].getPosition();
             }
         }
+
         for (int i = 0; i < carNumber; i++) {
             if (cars[i].getPosition() == winnerPosition) {
                 winnerList.add(cars[i].getName());
             }
         }
-        printWinner(winnerList);
-    }
 
+        printWinner(winnerList);
+
+    }
 
 }
