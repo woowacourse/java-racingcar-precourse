189b9fd87127039deef61940742466ae9ea8a76c
kangmin <kangmin789@naver.com>
2019-04-02 22:39:57 +0900

    README.md 수정, MoveCar기능 추가, CarRacer 생성자추가
    MoveCar기능은 실제로 Car객체의 Go메소드를 호출하여 차를 움직이는 기능입니다.
    README에 MoveCar 기능과 설명을 추가하였습니다.

--- src/main/java/domain/CarRacer.java
+++ src/main/java/domain/CarRacer.java
@@ -1,8 +1,19 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class CarRacer {
     private static final int STANDARD_OF_GOING = 4;
     private int randomNumber;
+    private ArrayList<Car> carList;
+
+    CarRacer(ArrayList<Car> carList) {
+        this.carList = carList;
+    }
+
+    private void MoveCar(int carOrder) {
+        this.carList.get(carOrder).Go();
+    }
 
     private boolean isGo() {
         return this.randomNumber >= STANDARD_OF_GOING ? true : false;
