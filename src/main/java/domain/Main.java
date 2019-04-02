365d375d3921cf823ea221324a0a24f1e88d0e71
samsung <samsung@LAPTOP-859NPSQ7>
2019-04-02 14:44:47 +0900

    7. main 함수 구현

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,14 @@
+package domain;
+/* 
+ * 클래스 이름 : Main
+ * 작성자 : Moonyoung Chae
+ * 날짜 : 2019/04/02
+ * Copyright (c) 2019 Moonyoung Chae
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		RacingGameManager manager = new RacingGameManager();
+		manager.run();
+	}
+}
