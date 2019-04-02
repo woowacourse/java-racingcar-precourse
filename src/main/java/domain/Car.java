4cc2b8df18bc44ce778f9c8a25c642785a695aeb
deocksoo <dusol92@snu.ac.kr>
2019-04-03 02:06:08 +0900

    [ADD] Car 클래스에 randomMove 메소드를 추가합니다

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,26 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
+    private final int TRAVEL_DISTANCE_BY_ONE_MOVE = 1;
+    private final int RANDOM_BOUND = 10;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void moveForward() {
+        this.position += this.TRAVEL_DISTANCE_BY_ONE_MOVE;
+    }
+
+    public void randomMove() {
+        int randomPick = (new Random()).nextInt(this.RANDOM_BOUND);
+
+        if (randomPick >= 4) {
+            moveForward();
+        }
+    }
 }
