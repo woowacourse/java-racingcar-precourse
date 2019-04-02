d4d9d134e9d135c8303963423e40e36dfbcf16cd
deocksoo <dusol92@snu.ac.kr>
2019-04-02 16:11:52 +0900

    [ADD] RacingCarGame의 생성자를 추가합니다

--- src/main/java/RacingCarGame.java
+++ src/main/java/RacingCarGame.java
@@ -6,9 +6,17 @@ public class RacingCarGame {
     private RacingManager racingManager;
     private TrackingCenter trackingCenter;
     private DisplayBoard displayBoard;
-    private ArrayList<Car> cars = new ArrayList<Car>();
+    private ArrayList<Car> cars;
+
+    public RacingCarGame() {
+        this.racingManager = new RacingManager();
+        this.trackingCenter = new TrackingCenter();
+        this.displayBoard = new DisplayBoard();
+        this.cars = new ArrayList<Car>();
+    }
 
     public static void main(String args[]) {
+        RacingCarGame racingCarGame = new RacingCarGame();
 
     }
 }
