02547d36b9e02a4342e7b9341d2c4d836f9a8589
Yookibaek <psykibaek@gmail.com>
2019-04-02 21:41:10 +0900

    [add] [서브로직3-1] 개별자동차 전진여부 판단 로직 구현

--- src/main/java/com/woowa/racingcar/Car.java
+++ src/main/java/com/woowa/racingcar/Car.java
@@ -1,6 +1,10 @@
 package com.woowa.racingcar;
 
+import java.util.Random;
+
 public class Car {
+    private static final int RANDOM_MAX_NUMBER = 10;            /* 최대 랜덤 넘버 */
+    private static final int RANDOM_THRESHOLD_NUMBER = 4;       /* move하기위한 threshold 넘버 */
     private final String name;
     private int position = 0;
 
@@ -8,5 +12,10 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public boolean decideMoveOrNot() {
+        Random random = new Random();
+        int randomNum = random.nextInt(RANDOM_MAX_NUMBER);
+
+        return (randomNum >= RANDOM_THRESHOLD_NUMBER);
+    }
 }
