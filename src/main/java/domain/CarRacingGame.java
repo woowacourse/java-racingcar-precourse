d871d137f6a6d7ffbefadeb3341f2f5313ff9c3b
U-Yeon <leanner@naver.com>
2019-04-02 12:06:37 +0900

    4) isValidtext의 생성. 자동차 이름이 유효한지 확인

--- src/main/java/domain/CarRacingGame.java
+++ src/main/java/domain/CarRacingGame.java
@@ -9,6 +9,9 @@ public class CarRacingGame {
 		Car.carNum = carName.length;
 		Car cars[] = new Car[Car.carNum];
 		
+		for (int i=0; i<Car.carNum; ++i) {
+			cars[i] = new Car(carName[i]);
+		}
 	}
 	
 	public static String[] scanCarName() {
@@ -16,8 +19,21 @@ public class CarRacingGame {
 		String allCarNames = scan.nextLine();
 		String carName[] = allCarNames.split(",");
 		
+		while (!isValidtext(carName)) {
+			System.out.println("자동차의 이름은 5글자 이하여야합니다. 다시 입력하세요.");
+			allCarNames = scan.nextLine();
+			carName = allCarNames.split(",");
+		}
+		
 		return carName;
 	}
 
-	
+	public static boolean isValidtext(String[] carName) {
+		for (int i=0; i<carName.length; ++i) {
+			if(carName[i].length()>5) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
