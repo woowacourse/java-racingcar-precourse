113b33ebc1355c52d0f1eaa86d58b38b153c9abe
khx1712 <khx1712@gmail.com>
2019-04-02 23:38:50 +0900

    add class main getRacingTime

--- baeminHW2/src/baeminHW2/racingcar.java
+++ baeminHW2/src/baeminHW2/racingcar.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class racingcar {
 	public static void main(String[] args) {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기분으로 구분)");
-
+		String[] carName = getCarName();
+		int recingTime = getRacingTime();
 	}
 	
 	
@@ -14,6 +14,13 @@ public class racingcar {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기분으로 구분)");
 		String carName = scan.nextLine();
 		return carName.split(",");
+	}
+	
+	public static int getRacingTime() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("시도할 회수는 몇회인가요?");
+		int racingTime = scan.nextInt();
+		return racingTime;
 	}
 }
 
