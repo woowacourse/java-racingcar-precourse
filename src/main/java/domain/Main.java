f3f0ba4ba34c9dff84dc18bdcb5cd40558718f6e
je941004 <je941004@naver.com>
2019-04-03 01:07:30 +0900

    #4 make Main class

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,47 @@
+package domain;
+
+public class Main {
+
+        static Car[] cars;
+        static int n;
+
+        public static void main(String args[]){
+                inputNames();
+                inputRandomNumber();
+                playRace();
+                compare();
+                outputWinner();
+        }
+
+        public static void inputNames(){
+
+        }
+
+        public static void makeCars(){
+
+        }
+
+        public static void inputRandomNumber(){
+
+        }
+
+        public static void playRace(){
+
+        }
+
+        public static void gameOnce(){
+
+        }
+
+        public static void compare(){
+
+        }
+
+        public static void findMaximum(){
+
+        }
+
+        public static void outputWinner(){
+
+        }
+}
