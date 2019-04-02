1085eb74f2c3deec203c2e4ebe09dc0ef9a8b859
U-Yeon <leanner@naver.com>
2019-04-02 11:54:19 +0900

    2) scanCarName : 자동차 이름의 입력을 받고 콤마를 기준으로 구분한다.

--- src/main/java/domain/CarRacingGame.java
+++ src/main/java/domain/CarRacingGame.java
@@ -7,5 +7,13 @@ public class CarRacingGame {
 	public static void main(String[] args) {
 		
 	}
+	
+	public static String[] scanCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String allCarNames = scan.nextLine();
+		String carName[] = allCarNames.split(",");
+		
+		return carName;
+	}
 
 }
