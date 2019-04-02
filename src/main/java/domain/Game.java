86deded02431f505452b1defcfe06b502c287bdd
Sangwon <junothyme@gmail.com>
2019-04-02 22:18:06 +0900

    각각의 자동차를 한 턴 진행하는 기능

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -3,11 +3,24 @@ package domain;
 public class Game {
 
     private Car[] cars;
+    private Display display;
+
+    public Game() {
+        display = new Display();
+    }
 
     public Car[] makeCars(String[] names) {
         for (int i = 0; i < names.length; i++) {
             cars[i] = new Car(names[i]);
         }
         return cars;
+    }
+
+    public void goOneTurnForEachCar() {
+        for (int i = 0; i < cars.length; i++) {
+            int randomNum = cars[i].makeRandomNums();
+            cars[i].increasePosition(randomNum);
+            display.showResults(cars[i]);
+        }
     }
 }
