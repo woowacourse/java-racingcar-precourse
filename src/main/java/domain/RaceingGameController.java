2f7f989138238b6c7e36711b5e6b5a977be30c07
이인권 <hjun4512@gmail.com>
2019-04-02 16:44:01 +0900

    MOD : RaceingGameController클래스의 출력문들 모두 Output 클래스로 옮김(수정) 	-> Output에 있는구현된 함수를 사용

--- src/main/java/domain/RaceingGameController.java
+++ src/main/java/domain/RaceingGameController.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 public class RaceingGameController {
 
     public void gameStart(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        System.out.println(Output.outputEnrollCarName);
         ArrayList<Car> carNameList = Input.getInstance().enrollCarName();
 
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(Output.outputTryNumber);
         int tryNumber = Input.getInstance().tryNumber();
 
         gameResult(carNameList, tryNumber);
@@ -21,42 +22,14 @@ public class RaceingGameController {
             System.out.println();
         }
 
-        ArrayList<String> winnerList = new ArrayList<>();
-        int max = -1;
-
-        for(int i=0; i < carNameList.size() ; i++){
-            if(carNameList.get(i).getPosition() > max) {
-                max = carNameList.get(i).getPosition();
-                winnerList.clear();
-                winnerList.add(carNameList.get(i).getName());
-            } else if(carNameList.get(i).getPosition() == max) {
-                winnerList.add(carNameList.get(i).getName());
-            }
-        }
-
-        for(int i = 0 ; i < winnerList.size() ; i++){
-            if(i == (winnerList.size()-1)){
-                System.out.print(winnerList.get(i));
-                break;
-            }
-            System.out.print(winnerList.get(i)+",");
-        }
-
-        System.out.println("가 최종 우승했습니다.");
-
+        System.out.println(Output.getInstance().gameRusultMessage(carNameList).toString() + Output.outputResult);
     }
 
     private void racingStart(ArrayList<Car> carNameList){
         for(Car car : carNameList){
             car.movePosition();
-
-            System.out.printf("%s : ", car.getName());
-            for (int j = 0; j < car.getPosition(); j++) {
-                System.out.print("-");
-            }
-            System.out.println();
+            Output.getInstance().gameOngoingMessage(car);
         }
-
     }
 
 }
