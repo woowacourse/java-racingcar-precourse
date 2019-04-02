17dba9723412b2727629aac8d3a9e788bf29c6c2
YuJieun <idpresent@naver.com>
2019-04-02 14:33:44 +0900

    자동차 객체 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 import domain.Car;
 
+import javax.naming.Name;
+
 public class Main {
     static Car[] cars;
+    static String[] car_names;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String[] car_names;
         while(true){
             car_names = sc.nextLine().split(",");
             if(checkName(car_names))
                 break;
         }
-
         System.out.println("시도할 회수는 몇회인가요?");
         int num = sc.nextInt();
+
+        NameMapping();
+    
+
     }
 
     public static boolean checkName(String[] names) {
@@ -25,5 +30,12 @@ public class Main {
             }
         }
         return true;
+    }
+
+    public static void NameMapping(){
+        cars = new Car[car_names.length];
+        for(int i=0;i<cars.length;i++){
+            cars[i] = new Car(car_names[i]);
+        }
     }
 }
