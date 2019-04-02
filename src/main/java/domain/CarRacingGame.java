afd19e0ae62903983ae3f85ac4faf38cf3736b98
U-Yeon <leanner@naver.com>
2019-04-02 11:47:53 +0900

    1) Main함수를 가질 CarRacingGame 클래스 생성

---
+++ src/main/java/domain/CarRacingGame.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.Scanner;
+
+public class CarRacingGame {
+	public static Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+	}
+
+}
