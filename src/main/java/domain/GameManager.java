a1c689193915dd17ce796db36d324dbed4d6b284
JINSIL <kjin_048@gmail.com>
2019-04-02 22:49:28 +0900

    add 입력, 자동차 move, 상태 출력 기능

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -1,8 +1,41 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class GameManager {
+    private int carsize;
+    private int count;
+    private ArrayList<Car> cars;
+
+    GameManager() {
+        User user = new User();
+        cars = user.getCar();
+        carsize = cars.size();
+        count = user.getcount();
+    }
 
     public void start() {
-        
+        for (int i = 0; i < count; i++) {
+            front();
+            System.out.println();
+        }
+        gamefinish();
+    }
+
+    public void front() {
+        for (int j = 0; j < carsize; j++) {
+            Car c = cars.get(j);
+            c.move();
+            c.print();
+        }
+    }
+
+    /**
+     * gamefinish() : 게임 상태 확인
+     */
+    private void gamefinish() {
+        for (int i = 0; i < carsize; i++) {
+            Car c = cars.get(i);
+        }
     }
 }
