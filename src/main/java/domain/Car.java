8be92a99088586578dc40c737e56a4f774317911
nauco <hodong42@gmail.com>
2019-04-02 20:27:16 +0900

    implement input, split, make Objects and print position functions

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,7 +1,6 @@
 package domain;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Car {
     private final String name;
@@ -14,31 +13,45 @@ public class Car {
     }
 
     static public void enterCarName() {
-        String carName;
-        Scanner scan = new Scanner(System.io);
+        String carName[];
+        Scanner scan = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        carName = scan.nextLine();
+
+        /* 쉼표로 구분 된 이름들을 carList에 추가 */
+        carName = scan.nextLine().split(",");
+        carList = new ArrayList<>();
+        for (String token  :carName) {
+            carList.add(new Car(token));
+        }
     }
 
     static public void enterHowManyTime() {
-        Scanner scan = new Scanner(System.io);
+        Scanner scan = new Scanner(System.in);
         System.out.println("시도할 회수는 몇회인가요?");
         times = scan.nextInt();
+    }
+    public void printNameAndPosition() {
+        System.out.print(name+":");
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     static public void printRacingResult() {
         System.out.println("실행 결과");
         /* 실행 */
-    }
 
+        for (Car c  : carList) {
+            c.printNameAndPosition();
+        }
+    }
 
     static public void printWinner() {
         /* 우승자 판단 */
 
         System.out.println("가 최종 우승했습니다.");
     }
-
-
 
 
     public static void main(String[] args) {
