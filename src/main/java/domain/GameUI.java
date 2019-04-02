913c161984428a77602661282b04b1f4141871eb
hyperpace <cnk9109@gmail.com>
2019-04-02 15:45:51 +0900

    add method askLapNum() to GameUI.java

--- src/main/java/domain/GameUI.java
+++ src/main/java/domain/GameUI.java
@@ -11,11 +11,21 @@ public class GameUI {
 
 	public String askCarNames(Scanner sc) {
 		String carNames;
-		String info = "경주할자동차이름을입력하세요.(이름은쉼표(,)기준으로구분)";
+		String info = "경주할 자동차 이름을 입력하세요.(이름은쉼표(,)기준으로구분)";
 
 		System.out.println(info);
 		carNames = sc.nextLine();
 
 		return carNames;
+	}
+
+	public int askLapNum(Scanner sc) {
+		int lapNum;
+		String info = "시도할 회수는 몇회인가요?";
+
+		System.out.println(info);
+		lapNum = Integer.parseInt(sc.nextLine());
+
+		return lapNum;
 	}
 }
