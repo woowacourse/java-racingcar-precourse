3b9135200b110f5e18dec28c3a9df1024a0f7e1b
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-04-02 16:49:41 +0900

    메인 프로그램 자동차 이름 사용자 입력 기능 컴파일 성공, 테스트 실패

--- src/main/java/domain/MainApp.java
+++ src/main/java/domain/MainApp.java
@@ -1,6 +1,18 @@
 package domain;
 
+import java.util.Scanner;
+
 public class MainApp {
     public static void main(String[] args) {
+    }
+
+    /**
+     * 사용자로부터 자동차의 이름들을 입력받아 반환하는 메소드입니다.
+     * @return 자동차의 이름을 각 원소로 하는 문자열 배열
+     */
+    public static String[] getCarNames() {
+        String arrCarNames[] = new String[0];
+
+        return arrCarNames;
     }
 }
