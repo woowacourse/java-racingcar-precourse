56d2998cebbe39d536d3780364ba0c5aeb9a95d5
deocksoo <dusol92@snu.ac.kr>
2019-04-02 16:07:00 +0900

    [ADD] RacingCar Game 구현에 필요한 클래스들을 추가합니다
    RacingCarGame class, DisplayBoard class, RacingManager class, TrackingCenter class를
    추가하였습니다.

---
+++ src/main/java/RacingCarGame.java
@@ -0,0 +1,14 @@
+import domain.*;
+
+import java.util.ArrayList;
+
+public class RacingCarGame {
+    private RacingManager racingManager;
+    private TrackingCenter trackingCenter;
+    private DisplayBoard displayBoard;
+    private ArrayList<Car> cars = new ArrayList<Car>();
+
+    public static void main(String args[]) {
+
+    }
+}
