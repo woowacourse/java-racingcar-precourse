8aa480194f2dcf2af964e7ca5271f669e7d856fd
samsung <samsung@LAPTOP-859NPSQ7>
2019-04-02 14:38:49 +0900

    3. (1), (2) 각 자동차가 난수에 따라 전진하거나 멈춤

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,19 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
+	private Random random = new Random();
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public void race() {
+		if (random.nextInt(10) >= 4) {
+			position++;
+		}
+	}
 }
