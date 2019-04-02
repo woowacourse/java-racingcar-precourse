c7885037d5ed1aeaf5dafb2ef649e308c82123c4
deocksoo <dusol92@snu.ac.kr>
2019-04-03 07:50:32 +0900

    [ADD] TrackingCenter클래스의 showPositions 메소드를 구현합니다
    각 자동차별 현재 위치를 출력해주는 메소드입니다.

--- src/main/java/domain/TrackingCenter.java
+++ src/main/java/domain/TrackingCenter.java
@@ -2,6 +2,7 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TrackingCenter {
     private HashMap<String, Integer> realTimePositionsOfCars = new HashMap<String, Integer>();
@@ -12,8 +13,29 @@ public class TrackingCenter {
         }
     }
 
-    public void showPositions() {
+    public String repeat(String str, int repeatTime) {
+        StringBuffer repeated = new StringBuffer();
 
+        for (int i = 0; i < repeatTime; i++) {
+            repeated.append(str);
+        }
+        return repeated.toString();
+    }
+
+    /**
+     * 각 자동차의 이름과 위치를 출력해주는 메소드입니다.
+     * reaTimePositionsOfCars가 {"car1" : 2, "car2" : 3, "car3" : 1} 인 경우
+     * 출력 예시 :
+     * car1 : --
+     * car2 : ---
+     * car3 : -
+     */
+    public void showPositions() {
+        for (Map.Entry<String, Integer> position: this.realTimePositionsOfCars.entrySet()) {
+            System.out.print(position.getKey() + " : ");
+            System.out.println(repeat("-", position.getValue()));
+        }
+        System.out.print("\n");
     }
 
     public void showRanks() {
