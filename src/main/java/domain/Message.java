a338ecc4fcf39d10b3061f20ad5fd156b524f6ac
moonsung <visitant5@nate.com>
2019-04-02 14:14:08 +0900

    update Message 중복된 자동차 제외 메세지
    (“ERROR_NAMESTATE_OVERLAP", "자동차 이름이 중복되어 제외된 자동차가있습니다.");

--- src/main/java/domain/Message.java
+++ src/main/java/domain/Message.java
@@ -17,7 +17,8 @@ public class Message {
     };
     public static HashMap<String, String> errorMessage = new HashMap<String, String>() {
         {
-            put("ERROR_NAMELENGTH", " 자동차 이름이 5글자가 초과되어 제외합니다.");
+            put("ERROR_NAMESTATE_OVERLAP", "자동차 이름이 중복되어 제외된 자동차가있습니다.");
+            put("ERROR_NAMESTATE_LENGTH", "자동차 이름이 5글자가 초과되어 제외된 자동차가있습니다.");
             put("ERROR_NAMEEMPTY", " 자동차 이름을 1대 이상 입력하세요.");
             put("ERROR_GAMECOUNTLIMIT", " 시도할 횟수를 0이상으로 입력해주세요.");
             put("ERROR_GAMECOUNTTYPE", " 시도할 횟수를 숫자로 입력하세요.");
