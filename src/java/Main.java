e0eeb08941c67f71f66b1fcc87784b0529d9cd40
Yookibaek <psykibaek@gmail.com>
2019-04-02 22:32:54 +0900

    [update] 클래스 경로 변경
    클래스경로 까지의 거리가 길어 중간의 무의미한 파일을 제거했습니다.

---
+++ src/java/Main.java
@@ -0,0 +1,8 @@
+import com.woowa.racingcar.RacingGame;
+
+public class Main {
+    public static void main(String[] args){
+        RacingGame game = new RacingGame();
+        game.startGame();
+    }
+}
