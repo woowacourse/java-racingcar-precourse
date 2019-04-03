cd16f22561ed0f713c4ad97b06fd1ceb95ae3e96
men7627 <men1219@hanmail.net>
2019-04-04 01:01:52 +0900

    Car 클래스, Game 클래스 변수 및 상수 선언 및 초기화

---
+++ src/main/java/domain/Game.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Game {
+        private static final int MAXCARNAMENUMBER = 5;
+        private static final int MAXRANDOMNUMBER = 9;
+
+        Car[] cars;
+        int trialNumber;
+
+        Game(){}
+}
