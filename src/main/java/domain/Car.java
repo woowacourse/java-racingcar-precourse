d978d0bd91df8a16a70df765206fb8f1500462bf
men7627 <men1219@hanmail.net>
2019-04-04 01:42:11 +0900

    Car클래스 printPosition()메서드 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,18 +1,25 @@
 package domain;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+        private final String name;
+        private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+        public Car(String name) {
+                this.name = name;
+        }
 
-    private void printName(){
-        System.out.println(this.name + " : ");
-    }
+        private void printName() {
+                System.out.println(this.name + " : ");
+        }
 
-    private void moveCar(){
-        this.position++;
-    }
+        private void moveCar() {
+                this.position++;
+        }
+
+        private void printPosition() {
+                for (int i = 0; i<this.position;i++){
+                        System.out.print("-");
+                }
+                System.out.println();
+        }
 }
--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -9,7 +9,8 @@ public class Game {
         private Car[] cars;
         private int trialNumber;
 
-        Game() {}
+        Game() {
+        }
 
         private void printCarNamesInputMessage() {
                 System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
@@ -28,16 +29,16 @@ public class Game {
                 }
         }
 
-        private void printTrialNumberInputMessage(){
+        private void printTrialNumberInputMessage() {
                 System.out.println("시도할 회수는 몇회인가요?");
         }
 
-        private void inputTrialNumber(){
+        private void inputTrialNumber() {
                 Scanner scan = new Scanner(System.in);
                 this.trialNumber = scan.nextInt();
         }
 
-        private void printResultMessage(){
+        private void printResultMessage() {
                 System.out.println("\n실행 결과");
         }
 }
