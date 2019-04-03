68744aaf410686ca754f220ac734955c0405873b
hsycamp <hsycamp@gmail.com>
2019-04-03 17:49:24 +0900

    난수 생성 메소드 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,42 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	// 추가 기능 구현
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void oneRound() {
+		makeRandomNumber();
+		move();
+		printResult();
+	}
+
+	public int makeRandomNumber() {
+		Random random = new Random();
+		return random.nextInt(10);
+	}
+
+	private void move() {
+		// TODO
+
+	}
+
+	private void printResult() {
+		// TODO
+
+	}
 }
