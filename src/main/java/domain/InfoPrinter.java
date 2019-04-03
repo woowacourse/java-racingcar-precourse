1d92edee8b7ec0b764a14b8d623dc673ee03adbe
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-04-03 16:34:43 +0900

    [func : info printer] print current state & print winner

--- src/main/java/domain/InfoPrinter.java
+++ src/main/java/domain/InfoPrinter.java
@@ -1,8 +1,24 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class InfoPrinter {
+    public static void printCurrent(){
+        for(int i=0; i<CarRepository.carPark.size(); i++) {
+            System.out.print(CarRepository.carPark.get(i).getName()+" : ");
+            for(int j=0; j<CarRepository.carPark.get(i).getPosition(); j++)
+                System.out.print("-");
+            System.out.println();
+        }
+        System.out.println("\n");
+    }
+
     public static void printWinner(){
-        System.out.print("...");
+        ArrayList<Car> list = winnerPicker.getWinner();
+        String winners="";
+        for(int i=0; i<list.size(); i++)
+            winners += (i==0)? list.get(i).getName() : ", "+list.get(i).getName();
+        System.out.print(winners);
         System.out.println("가 최종우승했습니다.");
     }
 }
