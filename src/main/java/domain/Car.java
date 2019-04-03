c47f400dbde72f4179bb7ffa4cd6cc682ff60611
Soojin Roh <sooreal91@gmail.com>
2019-04-04 00:15:27 +0900

    하드코딩한 숫자를 상수 변수로 변경
    moveCar()에서 randomCount가 4 이상이면 position을 변경하는데 변경의 기준점이 되는 4를 직접 하드코딩하지 않고 변수를 만들어 사용하는 것으로 수정하였다.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,15 +1,23 @@
+/*
+ * @Car.java                 1.00 2019/03/31
+ * 
+ * Copyright(c) 2019         Soojin Roh
+ * All rights reserved.
+ */
+
 package domain;
 
 public class Car {
     private final String name;
+    private final int MOVE = 4;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
     }
-    
+
     public void moveCar(int randomCount) {
-        if ( randomCount >= 4) {
+        if (randomCount >= MOVE) {
             this.position += 1;
         }
     }
@@ -17,11 +25,11 @@ public class Car {
     public int getPosition() {
         return this.position;
     }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
     public void printRaceSituation() {
         System.out.print(name + " : ");
         for (int i = 0; i < position; i++) {
