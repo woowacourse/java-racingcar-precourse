f256bddc1024d15f3ecd324b3c6f1b12c277c466
Yujin Kim <99stella24@naver.com>
2019-04-03 14:24:36 +0900

    initCar 메서드 추가

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,7 +1,26 @@
 package domain;
 
-public class RacingGame {
-  public void run(){
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class RacingGame {
+
+  public void run() {
+    ArrayList<Car> carArrayList = initCar();
+    //initialize car names to each car object
+  }
+
+  public ArrayList<Car> initCar() {
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    Scanner scan = new Scanner(System.in);
+    String carNames = scan.next();
+    ArrayList<String> carNameList = new ArrayList<String>(Arrays.asList(carNames.split(",")));
+    ArrayList<Car> carArrayList = new ArrayList<Car>();
+    for(String car : carNameList){
+      Car realCar = new Car(car);
+      carArrayList.add(realCar);
+    }
+    return carArrayList;
   }
 }
