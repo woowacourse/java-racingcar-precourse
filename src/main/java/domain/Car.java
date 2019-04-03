2040c815fc73bf2f7912fde9880032135e0c9bc2
deocksoo <dusol92@snu.ac.kr>
2019-04-03 18:38:57 +0900

    [REFACTOR] Car 클래스에 상수 변수를 추가합니다
    randomMove 함수에서 하드코딩된 부분을 수정하기 위해 상수 변수를
    선언하였습니다.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -6,6 +6,7 @@ public class Car {
     private final String name;
     private final int TRAVEL_DISTANCE_BY_ONE_MOVE = 1;
     private final int RANDOM_BOUND = 10;
+    private final int THRESHOLD = 4;
     private int position = 0;
 
     public Car(String name) {
@@ -27,7 +28,7 @@ public class Car {
     public void randomMove() {
         int randomPick = (new Random()).nextInt(this.RANDOM_BOUND);
 
-        if (randomPick >= 4) {
+        if (randomPick >= this.THRESHOLD) {
             moveForward();
         }
     }
