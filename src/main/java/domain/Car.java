d18d45ccd3844aadeeef348563d399ba22be4d65
hyun <jhs695@naver.com>
2019-04-04 04:01:19 +0900

    Fix Car Class
    글자 제한 수 5글자 , position 변경 방법 변경

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -18,12 +18,14 @@ public class Car {
     }
 
     public void move(int result){
-        position += result;
+        if (result == 1){
+            position++;
+        }
     }
 
     private void checkNameSize(String name){
-        if (name.length() > 6){
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        if (name.length() > 5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다." + name);
         }
     }
 }
