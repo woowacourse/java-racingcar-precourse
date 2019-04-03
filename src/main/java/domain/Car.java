4a6da2ef470b9b0d75ea55238047a5949d2a69ee
EternalFete <comath@gmail.com>
2019-04-03 23:35:01 +0900

    Step 3. add `play` method in Car Class which get random number between 0 and 9 and move car or not depending on the number.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +11,12 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public void play() {
+        Random rand = new Random();
+        int number = rand.nextInt(10);
+
+        if (number >= 4) {
+            this.position++;
+        }
+    }
 }
