f99aa53d799ac93d48039689b8865df46bea49cd
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-04-03 16:33:48 +0900

    [func : car mover] decide car's movement & repeat cnt times

--- src/main/java/domain/CarMover.java
+++ src/main/java/domain/CarMover.java
@@ -1,7 +1,17 @@
 package domain;
 
+import java.util.Random;
+
 public class CarMover {
     public static void repeat(int cnt){
+        for(int i=0; i<cnt; i++)
+            moveOneStep();
+    }
 
+    private static void moveOneStep(){
+        for(int i=0; i<CarRepository.carPark.size(); i++)
+            if(RandomNumberCreater.createNum()>=4)
+                CarRepository.carPark.get(i).move();
+        InfoPrinter.printCurrent();
     }
 }
