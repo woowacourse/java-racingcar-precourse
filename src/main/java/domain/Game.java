16fa7539ebcf2c4e101c6d96b98b44c1585af2df
men7627 <men1219@hanmail.net>
2019-04-04 01:24:40 +0900

    printCarNamesInputMessage()메서드, inputCarNames()메서드 추가, Main 클래스, main()메서드 생성

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,11 +1,30 @@
 package domain;
 
+import java.util.Scanner;
+
 public class Game {
         private static final int MAXCARNAMENUMBER = 5;
         private static final int MAXRANDOMNUMBER = 9;
 
-        Car[] cars;
-        int trialNumber;
+        private Car[] cars;
+        private int trialNumber;
 
-        Game(){}
+        Game() {}
+
+        private void printCarNamesInputMessage() {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        }
+
+        private void inputCarNames() {
+                Scanner scan = new Scanner(System.in);
+                String inputCarNames = scan.nextLine();
+
+                String[] splitedCarNames = inputCarNames.split(",");
+
+                cars = new Car[splitedCarNames.length];
+
+                for (int i = 0; i < splitedCarNames.length; i++) {
+                        cars[i] = new Car(splitedCarNames[i]);
+                }
+        }
 }
