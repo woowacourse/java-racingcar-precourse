ecb9b9efab69888b67887bd41ae075f58b9f650a
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2019-04-03 13:59:57 +0900

    create receiveInputNames() method which recieve names of each cars

--- src/main/java/application/CarRacing.java
+++ src/main/java/application/CarRacing.java
@@ -1,8 +1,24 @@
 package application;
 
+import java.util.Scanner;
+
+import domain.Car;
+
 public class CarRacing {
 
+	Car[] cars;
+	
 	public void run() {
-		System.out.println("application start...");
+		initialize();
+	}
+
+	private void initialize() {
+		String names = receiveInputNames();
+	}
+
+	private String receiveInputNames() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
+		return scanner.nextLine();
 	}
 }
