f91ea7e203c1e1c500e57ba4de1268732cca1f55
changhoi0522 <0417kch@naver.com>
2019-04-04 07:59:36 +0900

    클래스를 나누어보았음

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,9 @@
 package domain;
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Car {
     private final String name;
     private int position = 0;
-    private static Random rand = new Random(System.currentTimeMillis());
 
     /**
      * @return the name
@@ -33,59 +30,25 @@ public class Car {
         }
         return false;
     }
-    
-    public static Car[] makeCar(String names) {
-        String[] name = names.split(",");
-        Car[] result = new Car[name.length];
-        for(int i = 0; i < name.length; i++) {
-            result[i] = new Car(name[i]);
-        }
-        return result;
-    }
 
-    public static int makeRandInt() {
-        return Car.rand.nextInt(10);
-    }
 
-    public static void racing(Car car) {
-        String s = "";
-        car.isGo(makeRandInt());
-        System.out.println(car.name + ": " + "-".repeat(car.getPosition()));
-    }
-
-    public static void winners(Car[] cars) {
-        int[] distances = new int[cars.length];
-        for(int i = 0; i < cars.length; i++) {
-            distances[i] = cars[i].getPosition();
-        }
-        Arrays.sort(distances);
-        String winnerIs = "";
-        for(Car car : cars) {
-            if(car.getPosition() == distances[distances.length - 1]){
-                winnerIs += car.getName() + " ";
-            }
-        }
-
-        winnerIs = winnerIs.trim().replace(" ", ", ");
-        System.out.println(winnerIs + "가 최종 우승했습니다.");
-    }
     public static void main(String[]args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         Scanner stdIn = new Scanner(System.in);
         String names = stdIn.nextLine();
         System.out.println("시도할 회수는 몇 회인가요?");
         int cases = stdIn.nextInt();
-        Car[] cars = makeCar(names);
+        stdIn.close();
+        Car[] cars = Racing.makeRacingCar(names);
 
-        System.out.println();
         while(cases-- > 0) {
             for(int i = 0; i < cars.length; i++) {
-                racing(cars[i]);
+                Racing.racing(cars[i]);
             }
             System.out.println();
         }
 
-        winners(cars);
+        Racing.winners(cars);
 
     }
 
---
+++ src/main/java/domain/Racing.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Racing {
+    private static Random rand = new Random(System.currentTimeMillis());
+	
+	public static int makeRandInt() {
+        return Racing.rand.nextInt(10);
+    }
+	
+	public static Car[] makeRacingCar(String names) {
+        String[] name = names.split(",");
+        Car[] result = new Car[name.length];
+        for(int i = 0; i < name.length; i++) {
+            result[i] = new Car(name[i]);
+        }
+        return result;
+    }
+	
+    public static void winners(Car[] cars) {
+        int[] distances = new int[cars.length];
+        for(int i = 0; i < cars.length; i++) {
+            distances[i] = cars[i].getPosition();
+        }
+        Arrays.sort(distances);
+        String winnerIs = "";
+        for(Car car : cars) {
+            if(car.getPosition() == distances[distances.length - 1]){
+                winnerIs += car.getName() + " ";
+            }
+        }
+
+        winnerIs = winnerIs.trim().replace(" ", ", ");
+        System.out.println(winnerIs + "가 최종 우승했습니다.");
+    }
+    
+    public static void racing(Car car) {
+        car.isGo(makeRandInt());
+        System.out.println(car.getName() + ": " + "-".repeat(car.getPosition()));
+    }
+}
