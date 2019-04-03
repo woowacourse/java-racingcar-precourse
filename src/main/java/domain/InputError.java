4ead80615cca7c20d4dbdb958d9a740596715aed
racoco083 <uytr083@naver.com>
2019-04-04 08:25:33 +0900

    update NUMBER_FORMAT_ERROR to recoznize this error

--- src/main/java/domain/InputError.java
+++ src/main/java/domain/InputError.java
@@ -2,4 +2,5 @@ package domain;
 
 public class InputError {
     static final String LENGTH_ERROR = "6자 이상의 이름을 가진 자동차가 있습니다. 다시 입력해주세요.";
+    static final String NUMBER_FORMAT_ERROR = "숫자가 아닌 값이 입력되었습니다. 다시 입려해주세요.";
 }
