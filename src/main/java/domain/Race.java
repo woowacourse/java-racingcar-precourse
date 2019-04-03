22480a76662a6cdc62840b53d8f68f52c494689b
EternalFete <comath@gmail.com>
2019-04-03 23:19:14 +0900

    Step 1-2 & 1-3. add `validateUserInput` and `parseInputString` methods. make ArrayList for car objects with name from user input.

--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -1,15 +1,25 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Race {
+    ArrayList<Car> cars = new ArrayList<>();
 
     public void setCarNames() {
         String userInputString;
+        ArrayList<String> carNames = new ArrayList<>();
 
-        userInputString = getInputCarNames();
+        while (carNames.isEmpty()) {
+            userInputString = getInputCarNames();
+            carNames = validateUserInput(userInputString);
+        }
 
-        System.out.println(userInputString);
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
     }
 
     private String getInputCarNames() {
@@ -17,5 +27,21 @@ public class Race {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return scan.next();
+    }
+
+    private ArrayList<String> validateUserInput(String inputString) {
+        ArrayList<String> parsedNames = parseInputString(inputString);
+
+        for (String names : parsedNames) {
+            if (names.length() > 5) {
+                return new ArrayList<>();
+            }
+        }
+
+        return parsedNames;
+    }
+
+    private ArrayList<String> parseInputString(String inputString) {
+        return new ArrayList<> (Arrays.asList(inputString.split(",")));
     }
 }
