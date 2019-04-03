e21022a3a03b40d9f83295e14f61c686f6c86d24
usang <usang>
2019-04-03 23:26:28 +0900

    printResult()에 대한 주석 추가

--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -24,9 +24,9 @@ public class Racing {
 
     private static final String MESSAGE_INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MESSAGE_INPUT_NUMBER = "시도할 횟수를 입력해주세요: ";
-    private static final String MESSAGE_ERROR_NAME= "5자 이하의 이름으로 입력해주세요";
+    private static final String MESSAGE_ERROR_NAME = "5자 이하의 이름으로 입력해주세요";
     private static final String MESSAGE_ERROR_NEGATIVE_NUMBER = "0보다 큰 숫자로 입력해주세요";
-    private static final String MESSAGE_ERROR_NOT_NUMBER= "숫자를 입력해주세요";
+    private static final String MESSAGE_ERROR_NOT_NUMBER = "숫자를 입력해주세요";
     private static final String MESSAGE_RESULT = "가 최종 우승했습니다.";
 
     public Racing() {
@@ -75,12 +75,11 @@ public class Racing {
     private boolean isCorrectMovingNumber(String num) {
         try {
             int integerNum = Integer.parseInt(num);
-            if(integerNum <= 0) {
+            if (integerNum <= 0) {
                 System.out.println(MESSAGE_ERROR_NEGATIVE_NUMBER);
                 return false;
             }
-        }
-        catch(NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.out.println(MESSAGE_ERROR_NOT_NUMBER);
             return false;
         }
@@ -88,7 +87,7 @@ public class Racing {
     }
 
     private boolean isCorrectCarName(String str) {
-        int charCount = 0;
+        int charCount = 0;      //글자수를 세기 위한 변수
         for (int i = 0; i < str.length(); i++) {
             charCount++;
             if (str.charAt(i) == ',') {
@@ -115,16 +114,18 @@ public class Racing {
     private void printResult() {
         int maxPosition = 0;
         String winner = "";
+        // carList에서 가장 큰 position을 찾는다
         for (int i = 0; i < carList.size(); i++) {
             maxPosition = Math.max(maxPosition, carList.get(i).getPosition());
         }
+        // carList에서 가장 큰 position과 같은 position을 가진 Car 객체의 name을 winner에 추가한다
         for (int i = 0; i < carList.size(); i++) {
             if (maxPosition == carList.get(i).getPosition()) {
                 winner += carList.get(i).getName() + ",";
             }
 
         }
-        winner = winner.substring(0, winner.length() - 1);
+        winner = winner.substring(0, winner.length() - 1);      //마지막 ","문자를 제거하기 위함
         System.out.println(winner + MESSAGE_RESULT);
     }
 }
