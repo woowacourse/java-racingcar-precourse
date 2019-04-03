d3c7a4df3150b35de1b89fa76c26834b895ff437
bactoria <bactoria@gmail.com>
2019-04-03 17:12:41 +0900

    CLN :: 유틸클래스를 외부에서 생성하지 못하도록 변경

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -1,6 +1,9 @@
 package utils;
 
 public class StringUtils {
+
+    private StringUtils(){}
+
     public static String hyphenParser(int n) {
         StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
