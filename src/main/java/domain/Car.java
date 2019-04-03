d48ff2e5063dca705c0247cc380a31a0e948dc8c
changhoi0522 <0417kch@naver.com>
2019-04-04 06:52:38 +0900

    랜덤으로 0-9 출력하는 함수 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,9 +1,11 @@
 package domain;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Car {
     private final String name;
     private int position = 0;
+    private static Random rand = new Random(System.currentTimeMillis());
 
     public Car(String name) {
         this.name = name;
@@ -22,11 +24,15 @@ public class Car {
         }
         return result;
     }
+
+    public static int makeRandInt() {
+        return Car.rand.nextInt(10);
+    }
     public static void main(String[]args) {
         Scanner stdIn = new Scanner(System.in);
         String names = stdIn.nextLine();
         Car[] cars = makeCar(names);
-        System.out.print(cars);
+
     }
 
     // 추가 기능 구현
