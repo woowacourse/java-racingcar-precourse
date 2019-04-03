666371c2728fcf695c1c4ff2b90342e5f8b1e4cd
woung717 <woung717@gmail.com>
2019-04-04 00:25:21 +0900

    Car 클래스 멤버 변수 position, name - Getter 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -21,4 +21,12 @@ public class Car {
             this.position++;
         }
     }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
