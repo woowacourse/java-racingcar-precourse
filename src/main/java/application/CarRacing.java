4f4d34803f63443780380afc334478b67c6d0775
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2019-04-03 14:26:18 +0900

    create receiveInputTimes() method and divide setCars() from initialize()

--- src/main/java/application/CarRacing.java
+++ src/main/java/application/CarRacing.java
@@ -6,25 +6,36 @@ import domain.Car;
 
 public class CarRacing {
 	private static final String NAME_SEPARATOR = ",";
-	Car[] cars;
-	
+	private int times;
+	private Car[] cars;
+	private Scanner scanner;
+
 	public void run() {
 		initialize();
 	}
 
 	private void initialize() {
+		Scanner scanner = new Scanner(System.in);
 		String[] names = receiveInputNames().split(NAME_SEPARATOR);
+		this.times = recieveInputTimes();
+		setCars(names);
+		scanner.close();
+	}
+
+	private String receiveInputNames() {
+		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(" + NAME_SEPARATOR + ") 기준으로 구분)");
+		return scanner.nextLine();
+	}
+
+	private int recieveInputTimes() {
+		System.out.println("시도할 횟수는 몇회인가요?");
+		return scanner.nextInt();
+	}
+
+	private void setCars(String[] names) {
 		cars = new Car[names.length];
 		for (int i = 0; i < names.length; i++) {
 			cars[i] = new Car(names[i].trim());
 		}
-	}
-
-	private String receiveInputNames() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표("+NAME_SEPARATOR+") 기준으로 구분)");
-		String inputNames = scanner.nextLine();
-		scanner.close();
-		return inputNames;
 	}
 }
