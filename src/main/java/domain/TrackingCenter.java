d1560324b3238fd1da4892b8ea4b59326c760e73
deocksoo <dusol92@snu.ac.kr>
2019-04-03 17:37:16 +0900

    [REFACTOR] TrackingCenter 클래스를 리펙토링합니다
    1. 불필요한 주석을 제거하였습니다.
    2. 자동차별 위치를 mapping하고 있는 hashmap의 이름을 변경하였습니다.
    3. for loop에서 불필요한 인자를 제거하여 코드를 다시 작성하였습니다.
    4. 모든 플레이어가 한번도 움직이지 않는 경우를 고려하여 maxPosition을 -1로
    초기화 하였습니다.

--- src/main/java/domain/TrackingCenter.java
+++ src/main/java/domain/TrackingCenter.java
@@ -2,14 +2,13 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class TrackingCenter {
-    private HashMap<String, Integer> realTimePositionsOfCars = new HashMap<String, Integer>();
+    private HashMap<String, Integer> carPositions = new HashMap<String, Integer>();
 
     public void detectPositions(ArrayList<Car> cars) {
         for (Car car: cars) {
-            realTimePositionsOfCars.put(car.getName(), car.getPosition());
+            this.carPositions.put(car.getName(), car.getPosition());
         }
     }
 
@@ -22,43 +21,31 @@ public class TrackingCenter {
         return repeated.toString();
     }
 
-    /**
-     * 각 자동차의 이름과 위치를 출력해주는 메소드입니다.
-     * reaTimePositionsOfCars가 {"car1" : 2, "car2" : 3, "car3" : 1} 인 경우
-     * 출력 예시 :
-     * car1 : --
-     * car2 : ---
-     * car3 : -
-     */
     public void showPositions() {
-        for (Map.Entry<String, Integer> position: this.realTimePositionsOfCars.entrySet()) {
-            System.out.print(position.getKey() + " : ");
-            System.out.println(repeat("-", position.getValue()));
+        for (String carName: this.carPositions.keySet()) {
+            System.out.print(carName + " : ");
+            System.out.println(repeat("-", this.carPositions.get(carName)));
         }
         System.out.print("\n");
     }
 
-    public void showRanks() {
-
-    }
-
-    public String pickWinners(HashMap<String, Integer> positionsOfCar) {
-        int maxPosition = 0;
+    public String pickWinners() {
+        int maxPosition = -1;
         StringBuffer winners = new StringBuffer();
 
-        for (Map.Entry<String, Integer> positionOfCar: positionsOfCar.entrySet()) {
-            if (positionOfCar.getValue() > maxPosition) {
-                winners = new StringBuffer().append(positionOfCar.getKey());
-                maxPosition = positionOfCar.getValue();
-            } else if (positionOfCar.getValue() == maxPosition) {
-                winners.append(", " + positionOfCar.getKey());
+        for (String carName: this.carPositions.keySet()) {
+            if (this.carPositions.get(carName) > maxPosition) {
+                winners = new StringBuffer().append(carName);
+                maxPosition = this.carPositions.get(carName);
+            } else if (this.carPositions.get(carName) == maxPosition) {
+                winners.append(", " + carName);
             }
         }
         return winners.toString();
     }
 
     public void showWinners() {
-        String winners = pickWinners(this.realTimePositionsOfCars);
+        String winners = pickWinners();
 
         System.out.println(winners + "(이)가 최종 우승했습니다.");
     }
