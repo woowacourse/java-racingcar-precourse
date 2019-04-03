6425dcaddd0e6dc0d45b6a2b56739aab8aa9cd11
EternalFete <comath@gmail.com>
2019-04-03 22:13:51 +0900

    Step 1-1. add Race Class and setCarNames method to set car names. setCarNames method call getInputCarNames which get user input of car names. add Main Class and main method to test setCarNames method.

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Main {
+    public static void main(String[] args) {
+        Race race = new Race();
+
+        race.setCarNames();
+    }
+}
---
+++ src/main/java/domain/Race.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.Scanner;
+
+public class Race {
+
+    public void setCarNames() {
+        String userInputString;
+
+        userInputString = getInputCarNames();
+
+        System.out.println(userInputString);
+    }
+
+    private String getInputCarNames() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return scan.next();
+    }
+}
