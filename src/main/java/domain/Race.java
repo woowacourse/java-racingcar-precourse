bb31c4b33cf3acfb84dfb53d9f8447316a844321
dasistHYOJIN <hgenie14@gmail.com>
2019-04-03 23:00:07 +0900

    실행결과를 출력하는 printResult() 반쯤 구현

--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -1,17 +1,19 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Race {
 
     private Scanner sc = new Scanner(System.in);
-    private String[] name;
+
+    private ArrayList<Car> car;
     private int count;
 
     // 레이스 실행
     public void doRace() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        name = sc.nextLine().split(",");
+        String[] name = sc.nextLine().split(",");
         System.out.println("시도할 회수는 몇회인가요?");
         count = sc.nextInt();
         sc.nextLine();
@@ -24,8 +26,19 @@ public class Race {
         return name.length() <= 5;
     }
 
-    // 우승자 리턴
+    // 실행결과 출력
+    private void printResult() {
+        System.out.println("실행 결과");
+        for (int i = 0; i < car.size(); i++) {
+            System.out.println(car.get(i).getName() + " : " + car.get(i).getPosition());
+        }
+
+        System.out.println();
+    }
+
+    // 우승자 출력
     private void printWinner() {
+        System.out.println("");
 
     }
 }
