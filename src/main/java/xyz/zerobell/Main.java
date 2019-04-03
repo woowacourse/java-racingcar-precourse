688a2237edc783bd398dbc58a1147ff7a744f78e
zerobell-lee <bnb3456@gmail.com>
2019-04-04 00:52:09 +0900

    added isStringsValid()

--- src/main/java/xyz/zerobell/Main.java
+++ src/main/java/xyz/zerobell/Main.java
@@ -18,4 +18,26 @@ public class Main {
         return str.split(",");
     }
 
+    static private boolean isStringsValid(String[] strings) {
+        for (String s : strings) {
+            if (!Car.isNameValid((s))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static private String[] getStrings() {
+        Scanner sc = getScanner();
+
+        while (true) {
+            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+            String[] strings = separateStrings(sc.next());
+
+            for (String s : strings) {
+
+            }
+        }
+    }
+
 }
