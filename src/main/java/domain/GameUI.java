e2d8d3a7699172e3250bcf11dc1c155699fbde44
hyperpace <cnk9109@gmail.com>
2019-04-03 17:07:29 +0900

    update method askLapNum() to GameUI.java

--- src/main/java/domain/GameUI.java
+++ src/main/java/domain/GameUI.java
@@ -21,6 +21,8 @@ public class GameUI {
 	private static final String DELIMITER = ",";
 	private static final String INFO_MESSAGE_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은쉼표(,)기준으로구분)";
 	private static final String ERR_MESSAGE_CAR_NAME_LENGTH = String.format("자동차 이름은 %d글자 이하입니다.", Car.NAME_LENGTH);
+
+	private static final String INFO_MESSAGE_LAP_NUM = "시도할 회수는 몇회인가요?";
 	private Game game;
 
 	public GameUI() {
@@ -64,9 +66,7 @@ public class GameUI {
 
 	public int askLapNum(Scanner sc) {
 		int lapNum;
-		String info = "시도할 회수는 몇회인가요?";
-
-		System.out.println(info);
+		System.out.println(INFO_MESSAGE_LAP_NUM);
 		lapNum = Integer.parseInt(sc.nextLine());
 
 		return lapNum;
