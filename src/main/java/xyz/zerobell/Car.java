4eae4a26a09b46315b904ae1c18a0ce701762fc8
zerobell-lee <bnb3456@gmail.com>
2019-04-04 00:44:42 +0900

    added getScanner()

--- src/main/java/xyz/zerobell/Car.java
+++ src/main/java/xyz/zerobell/Car.java
@@ -1,4 +1,4 @@
-package domain;
+package xyz.zerobell;
 
 public class Car {
     private final String name;
---
+++ src/main/java/xyz/zerobell/Main.java
@@ -0,0 +1,17 @@
+package xyz.zerobell;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static private Scanner sc;
+
+    static private Scanner getScanner() {
+        if (sc==null) {
+            sc = new Scanner(System.in);
+        }
+
+        return sc;
+    }
+
+}
