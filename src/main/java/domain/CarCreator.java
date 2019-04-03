42b25fc96fcfc7eb499f2f04336883d3ca230dff
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-04-03 16:29:23 +0900

    [func : car creator] implement car creator

--- src/main/java/domain/CarCreator.java
+++ src/main/java/domain/CarCreator.java
@@ -3,8 +3,11 @@ package domain;
 import java.util.StringTokenizer;
 
 public class CarCreator {
-    //TODO
     public static void createCar(String cars){
-//        StringTokenizer st = new StringTokenizer(cars,",");
+        StringTokenizer st = new StringTokenizer(cars,",");
+        while(st.hasMoreTokens()){
+            Car car = new Car(st.nextToken());
+            CarRepository.parking(car);
+        }
     }
 }
