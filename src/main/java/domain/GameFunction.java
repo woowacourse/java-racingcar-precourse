4a3fd9875a1da2384bf58ce03b63be80445fc4b8
hongduk kim <2012103911@khu.ac.kr>
2019-04-04 00:19:25 +0900

    게임진행에 필요한 함수를 구현하는 GameFunction 클래스 구현, 자동차이름을 입력받는 setCarname함수와 회수를 입력받는 setRounds함수 구현

---
+++ src/main/java/domain/GameFunction.java
@@ -0,0 +1,72 @@
+package domain;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GameFunction {
+
+    private ArrayList<Car> carlist;
+    private String inputCarname = null;
+    private String inputRound = null;
+    private int winnerPoint = 0;
+
+    public GameFunction() {
+        carlist = new ArrayList<>();
+    }
+
+    private static int initSetup() {
+
+        while (!inputCarname());
+
+        while (!inputRoundturn());
+
+        createCar();
+
+    }
+
+    private boolean setCarname() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+
+        this.inputCarname = scan.nextLine();
+        String[] carNameSplit = this.inputCarname.split(",");
+
+        for (int i = 0; i < carNameSplit.length; i++) {
+            if (carNameSplit[i].length() < 1 || carNameSplit[i].length() > 5)
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean setRounds() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+        this.inputRound = scan.nextLine();
+        int num = 0;
+
+        for (int i = 0; i < this.inputRound.length(); i++) {
+            try {
+                num = Integer.parseInt(inputRound);
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void createCar(String input, ArrayList<Car> carlist) {
+
+        String[] carNameSplite = input.split(",");
+
+        for (int i = 0; i < carNameSplite.length; i++) {
+            carlist.add(new Car(carNameSplite[i]));
+        }
+
+    }
+
+}
