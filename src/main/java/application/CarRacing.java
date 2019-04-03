b1da1692a8c2cd66b76bb34336d0d9721ff44c9e
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2019-04-03 13:52:20 +0900

    create Launcher Class(: it has main() method) and CarRacing Class

---
+++ src/main/java/application/CarRacing.java
@@ -0,0 +1,8 @@
+package application;
+
+public class CarRacing {
+
+	public void run() {
+		System.out.println("application start...");
+	}
+}
---
+++ src/main/java/application/Launcher.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		new CarRacing().run();
+	}
+}
