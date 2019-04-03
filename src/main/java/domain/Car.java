fc58efbd440650edf032c9965b65cdc929eb328c
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2019-04-03 13:49:32 +0900

    create race() method which calls canMove() and moveForward() method

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,16 +1,35 @@
 package domain;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final static int ONE_SPACE = 1;
+	private final static int MULTIPLES_FOR_DIGIT = 10;
+	private final static int STANDARD_NUMBER_OF_MOVING = 3;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
 	public int getPosition() {
 		return position;
 	}
 	
-	
+	public void race() {
+		if (canMove()) {
+			moveForward();
+		}
+	}
+
+	private boolean canMove() {
+		return (int) (Math.random() * MULTIPLES_FOR_DIGIT) > STANDARD_NUMBER_OF_MOVING;
+	}
+
+	private void moveForward() {
+		this.position += ONE_SPACE;
+	}
 }
