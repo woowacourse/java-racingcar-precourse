49444e192c1874de896276c69a24e6d644a85ca8
jinwook <jin942002@naver.com>
2019-04-03 12:10:00 +0900

    add private to final variables at InputHandler class

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class InputHandler {
-    static final String FIRST_WORD = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    static final String SEPARATOR = ",";
+    private static final String FIRST_WORD = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String SEPARATOR = ",";
 
     static String getInput() {
         Scanner s = new Scanner(System.in);
