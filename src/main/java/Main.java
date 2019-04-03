5570b99067f9a55a7f8d09799aeebf38ecc682a6
kmdngyu <kmdngyu@gmail.com>
2019-04-04 01:42:40 +0900

    fix : main 오타 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
      * 게임 진행 메인 함수
      * @param args
      */
-    public static void main(String[] args) {        System.out.println(Race.randomNum());
+    public static void main(String[] args) {
         MakeCar.makingCar();
     }
 }
