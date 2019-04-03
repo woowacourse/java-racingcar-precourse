5fd143ba80cfea8c95b961a8fbfc2e42af28634e
yejilee <yjll1019@naver.com>
2019-04-03 15:28:34 +0900

    getTryCout()메소드의 do-while문과 checkTryCount()메소드의 if문 조건 수정, static final 변수 선언.

--- src/main/java/domain/UserInput.java
+++ src/main/java/domain/UserInput.java
@@ -13,6 +13,11 @@ package domain;
 import java.util.*;
 
 public class UserInput {
+	static final String DEMAND_RACING_CAR_NAME = "경주할 자동차이름을 입력하세요.";
+	static final String DEMAND_CAR_GAME_TRY_COUNT = "시도할 회수는 몇 회인가요?";
+	static final String DEMAND_RULE_FOR_CAR_NAME = "자동차의 이름은 5자 이하만 가능합니다.";
+	static final String DEMAND_RULE_FOR_TRY_COUNT = "올바르지 않은 입력입니다.";
+	static final int MAX_CAR_NAME_LENGTH = 5;
 
 	public String[] getRacingCarName() {
 		Scanner scanner = new Scanner(System.in);
@@ -20,7 +25,7 @@ public class UserInput {
 		boolean result;
 
 		do {
-			System.out.println("경주할 자동차이름을 입력하세요.");
+			System.out.println(DEMAND_RACING_CAR_NAME);
 			carNameList = scanner.nextLine().split(",");
 			result = checkCarNameList(carNameList);
 		}
@@ -31,7 +36,8 @@ public class UserInput {
 
 	public boolean checkCarNameList(String[] carNameList) {
 		for (int i = 0; i < carNameList.length; ++i) {
-			if (carNameList[i].length() > 5) {
+			if (carNameList[i].length() > MAX_CAR_NAME_LENGTH) {
+				System.out.println(DEMAND_RULE_FOR_CAR_NAME);
 				return false;
 			}
 		}
@@ -45,17 +51,18 @@ public class UserInput {
 		boolean result;
 
 		do {
-			System.out.println("시도할 회수는 몇 회인가요?");
+			System.out.println(DEMAND_CAR_GAME_TRY_COUNT);
 			count = scanner.nextLine();
 			result = checkTryCount(count);
 		}
-		while (result != false);
+		while (result != true);
 
 		return Integer.parseInt(count);
 	}
 
 	public boolean checkTryCount(String count) {
-		if (count.matches("[1-9]||[1-9][0-9]+")) {
+		if (!count.matches("[1-9]||[1-9][0-9]+")) {
+			System.out.println(DEMAND_RULE_FOR_TRY_COUNT);
 			return false;
 		}
 		return true;
