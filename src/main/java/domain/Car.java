3a8746df5457631dc6fbcc428c32a96a1f8ffc0b
changhoi0522 <0417kch@naver.com>
2019-04-04 06:38:05 +0900

    자동차 이름 넣는 함수 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,4 +1,5 @@
 package domain;
+import java.util.Scanner;
 
 public class Car {
     private final String name;
@@ -11,6 +12,21 @@ public class Car {
     public boolean isGo(int val) {
         if(val >= 4) return true;
         return false;
+    }
+    
+    public static Car[] makeCar(String names) {
+        String[] name = names.split(",");
+        Car[] result = new Car[name.length];
+        for(int i = 0; i < name.length; i++) {
+            result[i] = new Car(name[i]);
+        }
+        return result;
+    }
+    public static void main(String[]args) {
+        Scanner stdIn = new Scanner(System.in);
+        String names = stdIn.nextLine();
+        Car[] cars = makeCar(names);
+        System.out.print(cars);
     }
 
     // 추가 기능 구현
