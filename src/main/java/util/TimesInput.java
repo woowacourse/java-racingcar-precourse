8783f274c414179986e561674e520ec4d8a823ba
kmdngyu <kmdngyu@gmail.com>
2019-04-04 02:18:49 +0900

    fix : 이동 횟수 입력 및 저장 / 0회 입력 예외 설정

--- src/main/java/util/TimesInput.java
+++ src/main/java/util/TimesInput.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class TimesInput {
     static Scanner scan = new Scanner(System.in);
     private static final String INPUT_TIMES_TEXT = "시도할 횟수는 몇회인가요";
+    private static final String WRONG_INPUT_NUMBER = "숫자를 입력해주세요!";
+    private static final String SMALL_INPUT_NUMBER = "0보다 큰 숫자를 입력해주세요";
 
     /**
      * 횟수를 입력받아 반환
@@ -14,7 +16,11 @@ public class TimesInput {
     public static int getTimes(){
         boolean isValidNumberInput = false;
         int times;
-        times = timesInput();
+        do{
+            times = timesInput();
+            isValidNumberInput = validateNumberInput(times);
+        }while(isValidNumberInput == false);
+
         return times;
     }
 
@@ -26,12 +32,25 @@ public class TimesInput {
         int inputInt = 0;
             try {
                 inputInt = scan.nextInt();
-            } catch (InputMismatchException ime) { //정수가 아닌 문자열이 들어 왔을 때 처리할 문장
-                System.out.println("숫자를 입력해주세요!");
+            } catch (InputMismatchException ime) {
+                //정수가 아닌 문자열이 들어 왔을 때 에러 캐치
+                System.out.println(WRONG_INPUT_NUMBER);
                 scan.next();
                 timesInput();
             }
         return inputInt;
+    }
+
+    /**
+     * 유효한 횟수 입력값인지 확인.
+     * @param times number
+     */
+    private static boolean validateNumberInput(int number){
+        if (number < Constants.MIN_TIMES){
+            System.out.println(SMALL_INPUT_NUMBER);
+            return false;
+        }
+        return true;
     }
 
 }
