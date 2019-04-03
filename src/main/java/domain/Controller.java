0c78aa0e86e1ceab17660969ce84b8501c0fccf6
Unknown <9105lgm@naver.com>
2019-04-03 22:11:28 +0900

    StartGame, getLaps 기능 구현 및 변수 name 변경, ArrayList 추가
    Car에 접근하기 편하기 위해서 ArrayList 추가

--- src/main/java/domain/Controller.java
+++ src/main/java/domain/Controller.java
@@ -1,17 +1,20 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Controller {
 
-    String[] racer;
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Car> cars = new ArrayList<Car>();
+    int laps;
+    String[] racers;
 
     public void askCarsName() {
         String names;
 
         do {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분");
-            Scanner scanner = new Scanner(System.in);
             names = scanner.nextLine();
 
         } while (!checkNamesCriterion(names));
@@ -25,9 +28,9 @@ public class Controller {
 
     /* 이름의 정당성 확인 및 split 기능 구현 */
     public boolean checkNamesCriterion(String name) {
-        racer = name.split(",");
+        racers = name.split(",");
 
-        for (String each : racer) {
+        for (String each : racers) {
 
             if (each.length() > 5) {
                 System.out.println("이름을 5자 이하로 적어주세요");
@@ -41,6 +44,11 @@ public class Controller {
 
     public void startGame() {
 
+        for(String racer : racers){
+            Car car = new Car(racer);
+            cars.add(car);
+        }
+
     }
 
     public void checkCarsPosition() {
@@ -49,6 +57,10 @@ public class Controller {
 
     public void checkWinner() {
 
+    }
+
+    public int getLaps(){
+        return laps;
     }
 
 }
