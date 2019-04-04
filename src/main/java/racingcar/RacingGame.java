0fbb979dea672081f0ebdb52c81fe7b08d56df8f
DaejunBae <dpudpu11@gmail.com>
2019-04-04 20:05:13 +0900

    Add 몇 회 이동 할지 입력

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,5 +7,13 @@ public class RacingGame {
     public void run(){
         RacingCars racingCars = new RacingCars(CarListGenerator.generate(InputView.inputCarNames()));
 
+        printResult(racingCars);
+    }
+
+    private void printResult(RacingCars racingCars) {
+        System.out.println("실행 결과");
+
+        int moveCount = InputView.inputGameCount();
+
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,4 +9,9 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분)");
         return sc.nextLine();
     }
+
+    public static int inputGameCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextInt();
+    }
 }
