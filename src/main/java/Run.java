97ffe42036c442b2b564985a367117df7bbb7d34
yun <blpomspo@gmail.com>
2019-04-04 17:02:59 +0900

    - 주석 작성   - 저작권   - Run class 설명 - Run.java 최초 완성

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,5 +1,17 @@
+/*
+ * @Car.java     0.1 2019-04-04
+ * Copyright(c) 2019 LeeYunSeop All rights reserved.
+ * */
+
 import domain.RacingCar;
 
+/**
+ * Run class
+ * @version 0.1
+ * @author yun
+ *
+ * RacingCar 실행
+ * */
 public class Run {
     public static void main(String[] args) {
         RacingCar racingCar = new RacingCar();
