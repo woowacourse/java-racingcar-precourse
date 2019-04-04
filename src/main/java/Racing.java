c51a50c5ab4c86b5b11bae503bf5d8320e74ce3a
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2019-04-04 21:40:13 +0900

    count_car_num

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -2,7 +2,17 @@ import domain.Car;
 import java.util.*;
 
 public class Racing {
-
+    public static int car_num, racing_num, winner_num;
+    public static Car[] cars;
+    public static String[] winners;
+    public static int count_car_num(String s){
+        int count=1;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==',')
+                count++;
+        }
+        return count;
+    }
     public static void main(String[] args){
         get_input();
         racing();
