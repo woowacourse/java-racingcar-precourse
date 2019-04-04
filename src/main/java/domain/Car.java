4950aade0e374d7263a52715ac834c45e39e0ddf
Leehaewon <leehaewon68@gmail.com>
2019-04-04 10:31:52 +0900

    modify Car Class, add moveOrStopCar function

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,9 +1,14 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
     public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final int MIN_CAR_MOVE_VALUE = 4;
+    public static final int CAR_MOVE_RANGE_SIZE = 10;
     private final String name;
     private int position = 0;
+    Random random;
 
     public Car(String name) {
         assertName(name);
@@ -21,6 +26,11 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public void moveOrStopCar() {
+        if(random.nextInt(CAR_MOVE_RANGE_SIZE) >= MIN_CAR_MOVE_VALUE)
+            position++;
     }
 
 }
