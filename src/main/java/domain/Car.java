d3f28ba0bd891fc66962b1414b183a671a93c85e
DiceMono <qrdfg@naver.com>
2019-04-04 12:27:02 +0900

    #3 Car 클래스 tryMove 메서드 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,36 @@
 package domain;
 
+import java.util.Random;
+import java.lang.StringBuilder;
+
 public class Car {
-    private final String name;
-    private int position = 0;
-
-    public Car(String name) {
-        this.name = name;
-    }
-
-    // 추가 기능 구현
+	private Random random = new Random();
+	private StringBuilder stringPosition = new StringBuilder();
+	private final String name;
+	private int position = 0;
+	
+	public Car(String name) {
+		this.name = name;
+	}
+	
+	public void tryMove() {
+		int randomInt = random.nextInt(10);
+		if(randomInt >= 4) {
+			position++;
+			stringPosition.append('-');
+		}
+		System.out.println(this.name + " : " + stringPosition);
+	}
+	
+	/* 테스트 코드 */
+	public static void main(String[] args) {
+		Car pobi = new Car("pobi");
+		Car crong = new Car("crong");
+		
+		for(int count = 0; count < 10; count++) {
+			pobi.tryMove();
+			crong.tryMove();
+			System.out.println();
+		}
+	}
 }
