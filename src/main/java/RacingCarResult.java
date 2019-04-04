9883a19156d4fb9b134bfd9c43ab29d9be30e682
Leehaewon <leehaewon68@gmail.com>
2019-04-04 10:42:12 +0900

    modify RacingCarResult Class, add check winner function

--- src/main/java/RacingCarResult.java
+++ src/main/java/RacingCarResult.java
@@ -1,5 +1,8 @@
 import domain.Car;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class RacingCarResult {
@@ -12,4 +15,31 @@ public class RacingCarResult {
     public List<Car> getCars() {
         return cars;
     }
+
+    public List<Car> getWinner() {
+        List<Car> winnerCars = new ArrayList<>();
+
+        Collections.sort(cars, carComparator);
+
+        int maxPosition = cars.get(0).getPosition();
+
+        for(int i = 0; i < cars.size(); i++) {
+            if(cars.get(i).getPosition() == maxPosition)
+                winnerCars.add(cars.get(i));
+        }
+
+        return winnerCars;
+    }
+
+    Comparator<Car> carComparator = new Comparator<Car>() {
+        @Override
+        public int compare(Car c1, Car c2) {
+            if(c1.getPosition() < c2.getPosition())
+                return 1;
+            else if(c1.getPosition() > c2.getPosition())
+                return -1;
+            else
+                return 0;
+        }
+    };
 } 
