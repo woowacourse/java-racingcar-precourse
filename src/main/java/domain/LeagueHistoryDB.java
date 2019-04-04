b44424066411bd39dfbaf6645199bcd7a5942909
deocksoo <dusol92@snu.ac.kr>
2019-04-04 19:42:54 +0900

    [ADD] LeagueHistoryDB의 writeHistory 메소드를 구현합니다
    write 하려는 record가 기존에 등록되지 않은 league일 경우에, 새로운 히스토리를
    만든 후에 저장하기 위해 createNewHistory 메소드를 추가하였습니다.

--- src/main/java/domain/LeagueHistoryDB.java
+++ src/main/java/domain/LeagueHistoryDB.java
@@ -3,11 +3,25 @@ package domain;
 import java.util.HashMap;
 
 public class LeagueHistoryDB {
-    public void writeHIstory(String leagueName, int trial, HashMap carPositions) {
+    private HashMap<String, HashMap> leagueHistories = new HashMap<String, HashMap>();
 
+    public void createNewHistroy(String leagueName) {
+        HashMap<Integer, HashMap> trialRecords = new HashMap<Integer, HashMap>();
+        leagueHistories.put(leagueName, trialRecords);
     }
 
-    public HashMap getHistory(String leagueName, int trial) {
+    public void writeHIstory(String leagueName, int trial, HashMap carPositions) {
+        if (!leagueHistories.containsKey(leagueName)) {
+            createNewHistroy(leagueName);
+        }
+        leagueHistories.get(leagueName).put(trial, carPositions);
+    }
+
+    public HashMap getHistoryOf(String leagueName) {
+        return new HashMap();
+    }
+
+    public HashMap getHistoryOf(String leagueName, int trial) {
         return new HashMap();
     }
 }
