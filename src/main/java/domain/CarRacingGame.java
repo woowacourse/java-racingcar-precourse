39f6e9a67c83b1fb003768a738f7606f60531bb4
chaehwan-oh <coghks56@gmail.com>
2019-04-04 15:55:56 +0900

    feature/move-count-input implemented

--- src/main/java/domain/CarRacingGame.java
+++ src/main/java/domain/CarRacingGame.java
@@ -1,9 +1,9 @@
 package domain;
 
 public class CarRacingGame {
-
     public static void main(String[] args) {
         Player user = new Player();
         user.selectCar();
+        user.setMoveCount();
     }
 }
--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Player {
     private final String CAR_SELECTION_GUIDE = "경주할 자동차 이름을 입력하세요."
         + "(이름은 쉼표(,) 기준으로 구분)";
+    private final String MOVE_COUNT_GUIDE = "시도할 회수는 몇회인가요?";
 
     public ArrayList<Car> selectCar() {
         String[] carNames = this.getCarNames();
@@ -39,6 +40,24 @@ public class Player {
             if (carNames[i].length() > 5) {
                 return false;
             }
+        }
+        return true;
+    }
+
+    public int setMoveCount() {
+        while (true) {
+            System.out.println(MOVE_COUNT_GUIDE);
+            Scanner prompt = new Scanner(System.in);
+            int moveCount = prompt.nextInt();
+            if (isValidMoveCount(moveCount)) {
+                return moveCount;
+            }
+        }
+    }
+
+    private boolean isValidMoveCount(int moveCount) {
+        if (moveCount <= 0) {
+            return false;
         }
         return true;
     }
