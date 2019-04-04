cce9ac0dc8127ce2e8143bb8fdaa058818fabba0
Junmo Han <wnsah052@naver.com>
2019-04-04 13:50:20 +0900

    시도할 횟수가 범위 내에 있는지 검사하는 기능 추가

--- src/main/java/domain/Validation.java
+++ src/main/java/domain/Validation.java
@@ -22,7 +22,7 @@ public class Validation {
 		
 		/** 사용자로부터 입력 받은 차 이름의 개수가 하나인지 확인하는 함수에서 사용되는 상수 */
 		private static final int ONLY_ONE_CAR = 1;
-	
+			
 		private static final String NAME_LENGTH_ERROR = "자동차 이름은 1자 이상 5자 이하만 가능합니다.";
 		private static final String NUM_OF_NAME_ERROR = "두 대 이상의 자동차 이름이 필요합니다.";
 		private static final String NUM_OF_TRY_ERROR = MIN_NUMBER_OF_TRY + " 이상 " 
@@ -73,6 +73,12 @@ public class Validation {
 		}
 
 		private static boolean isValidRangeOfTryNum(String strOfTry) {
+				int numOfTry = Integer.parseInt(strOfTry);
+				if (MIN_NUMBER_OF_TRY <= numOfTry && numOfTry <= MAX_NUMBER_OF_TRY) {
+						return true;
+				}
+				System.out.println(NUM_OF_TRY_ERROR);
+				return false;
 
 		}
 }
