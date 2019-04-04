7d564f83a819e50801333e4d6620540fc005687b
os94 <ss1511@naver.com>
2019-04-04 11:40:53 +0900

    add Main class, basic Racing class

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,16 @@
+/*
+ * @class       Main class
+ * @version     1.0.0
+ * @date        19.04.04
+ * @author      OHSANG SEO (tjdhtkd@gmail.com)
+ * @brief       run CarRacing as Main class.
+ */
+
+package domain;
+
+public class Main {
+    public static void main(String[] args) {
+        Racing racing = new Racing();
+        racing.run();
+    }
+}
---
+++ src/main/java/domain/Racing.java
@@ -0,0 +1,15 @@
+/*
+ * @class       Racing class
+ * @version     1.0.0
+ * @date        19.04.04
+ * @author      OHSANG SEO (tjdhtkd@gmail.com)
+ * @brief       include racing game info.
+ */
+
+package domain;
+
+public class Racing {
+    public void run() {
+
+    }
+}
