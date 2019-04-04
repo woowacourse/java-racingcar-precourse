ec7482d5bd869d16253530c1fb3f8032297e7992
hiwooj <hiwooj35@gmail.com>
2019-04-04 22:33:45 +0900

    자동차가 랜덤하게 전진하는 함수

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,8 +1,12 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
     private int position = 0;
+    private static final int LOWER_BOUNDARY = 4;
+    private static final int UPPER_BOUNDARY = 9;
 
 
     public Car(String name) {
@@ -15,6 +19,18 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    // 랜덤값에 따라 동작 여부 확인
+    public boolean goOrNot() {
+        Random rand = new Random();
+        int randomGo;
+
+        randomGo = rand.nextInt(UPPER_BOUNDARY + 1);
+        if (randomGo >= LOWER_BOUNDARY) {
+            return true;
+        }
+        return false;
     }
 
 
