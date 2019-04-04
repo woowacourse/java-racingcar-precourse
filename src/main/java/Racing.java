fe58a6ee1938f64ad6ef1290d214cd7e7d273d21
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2019-04-04 21:55:01 +0900

    get_input

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -13,7 +13,26 @@ public class Racing {
         }
         return count;
     }
-
+    public static void get_input(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String car_names;
+        car_names = scan.nextLine();
+        car_num = count_car_num(car_names);
+        cars = new Car[car_num];
+        winners = new String[car_num];
+        String temp = "";
+        int temp_num=0;
+        for(int i=0; i<car_names.length(); i++){
+            temp+=car_names.charAt(i);
+            if(car_names.charAt(i)!=',') continue;
+            cars[temp_num++] = new Car(temp.substring(0, temp.length()-1));
+            temp = "";
+        }
+        cars[temp_num] = new Car(temp);
+        System.out.println(("시도할 회수는 몇회인가요?"));
+        racing_num = scan.nextInt();
+    }
     public static void main(String[] args){
         get_input();
         racing();
