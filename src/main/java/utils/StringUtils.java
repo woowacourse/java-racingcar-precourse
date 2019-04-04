fe560c3b37c1cfb591485b7d296cf693c29cca96
bactoria <bactoria@gmail.com>
2019-04-04 14:34:07 +0900

    CLN :: 하드코딩된 문자를 상수로 수정

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -5,9 +5,10 @@ public class StringUtils {
     private StringUtils(){}
 
     public static String hyphenParser(int n) {
+        final char HYPHEN = '-';
         StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
-            sb.append("-");
+            sb.append(HYPHEN);
         }
         return sb.toString();
     }
