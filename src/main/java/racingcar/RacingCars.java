4c2a3a69c72ca328ff89dcd0f1890f740e1fe851
DaejunBae <dpudpu11@gmail.com>
2019-04-04 20:01:37 +0900

    Add Car 이름 유효성 검사 기능

--- src/main/java/racingcar/RacingCars.java
+++ src/main/java/racingcar/RacingCars.java
@@ -6,10 +6,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
+    private static final int MIN_CAR_SIZE = 2;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
     private final List<Car> cars;
 
     public RacingCars(List<Car> cars) {
+        validateSize(cars);
+        validateName(cars);
         this.cars = cars;
+    }
+
+    private void validateSize(List<Car> cars) {
+        if (cars.size() < MIN_CAR_SIZE)
+            throw new IllegalArgumentException("자동차 이름을 " + MIN_CAR_SIZE + "개 이상 입력해주세요.");
+    }
+
+    private void validateName(List<Car> cars) {
+        for (Car car : cars) {
+            if (isNamePossibleLength(car))
+                throw new IllegalArgumentException("자동차 이름은 " + MAX_CAR_NAME_LENGTH + "자 이하만 가능합니다.");
+
+            if (isBlank(car))
+                throw new IllegalArgumentException("자동차 이름을 공백으로 입력은 불가능합니다.");
+        }
+    }
+
+    private boolean isNamePossibleLength(Car car) {
+        return car.getName().length() > MAX_CAR_NAME_LENGTH;
+    }
+
+    private boolean isBlank(Car car) {
+        return car.getName().equals("");
     }
 
     public List<Car> getCars() {
