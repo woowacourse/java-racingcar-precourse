5cc5d6c323b6bbd611a8298ba3c17e83e7c9144c
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2019-04-04 21:54:19 +0900

    main

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -8,30 +8,6 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
-    public int random_value(){
-        Random generator = new Random();
-        return generator.nextInt(10);
-    }
-    public boolean gostop(){
-        int num = random_value();
-        if(num>=4) return true;
-        return false;
-    }
-    public void move(){
-        if(gostop())
-            this.position++;
-    }
-    public void print_status(){
-        System.out.print(this.name + " : ");
-        for(int i=1; i<=position; i++){
-            System.out.print("-");
-        }
-        System.out.println();
-    }
-    public int position(){
-        return this.position;
-    }
-    public String name() {
-        return this.name;
-    }
+
+    // 추가 기능 구현
 }
