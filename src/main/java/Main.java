370967fbd368730084bb6de96549f74551ebdf08
hyun <jhs695@naver.com>
2019-04-04 14:49:59 +0900

    Fix Main Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new Game().play();
+        Game game = new Game();
+        game.play();
     }
 }
