12bd362bcc04a67ccfe1752b71badf27e1748959
hyun <jhs695@naver.com>
2019-04-04 14:15:14 +0900

    ADD Package , interface

---
+++ src/main/java/interfaces/CarInterface.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface CarInterface {
+    String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 1자 이상 5자 이하만 가능합니다. ";
+}
---
+++ src/main/java/interfaces/ConsoleInterface.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public interface ConsoleInterface {
+    String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    String ASK_COUNT_ROUND = "시도할 회수는 몇회인가요?";
+    String ROUND_START_MESSAGE = "실행결과";
+    String WINNER_MESSAGE = "가 최종 우승했습니다";
+    String POSITION_MESSAGE = "-";
+    String DELIMITER = ",";
+}
