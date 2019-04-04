b465dc6730c8ff99ed815a7dafffce32767e36bd
os94 <ss1511@naver.com>
2019-04-04 12:38:30 +0900

    add enterCar(), enterMovingNumber()

--- src/main/java/domain/UserInput.java
+++ src/main/java/domain/UserInput.java
@@ -8,12 +8,43 @@
 
 package domain;
 
+import java.util.*;
+
 public class UserInput {
+    public static final String GUIDE_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String GUIDE_INPUT_MOVING_NUMBER = "시도할 회수는 몇회인가요?";
+    public static final String REGEX_CAR_NAME = "";
+    public static final String REGEX_MOVING_NUMBER = "";
+
     public String[] enterCar() {
-        return null;
+        Scanner sc = new Scanner(System.in);
+        String nameList;
+
+        do {
+            System.out.println(GUIDE_INPUT_CAR_NAME);
+            nameList = sc.nextLine().replace(" ", "");
+        } while (!(checkFormat(nameList, REGEX_CAR_NAME) && checkDuplication(nameList)));
+
+        return nameList.split(",");
     }
 
     public int enterMovingNumber() {
-        return 0;
+        Scanner sc = new Scanner(System.in);
+        String number;
+
+        do {
+            System.out.println(GUIDE_INPUT_MOVING_NUMBER);
+            number = sc.nextLine().replace(" ", "");
+        } while (!checkFormat(number, REGEX_MOVING_NUMBER));
+
+        return Integer.parseInt(number);
+    }
+
+    public boolean checkFormat(String nameList, String regex) {
+        return false;
+    }
+
+    public boolean checkDuplication(String nameList) {
+        return false;
     }
 }
