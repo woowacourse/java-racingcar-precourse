48b8f17e208b7a116104d42398abb65977802b0b
racoco083 <uytr083@naver.com>
2019-04-04 10:41:02 +0900

    add explanation some functions and fix some typo

--- src/main/java/domain/Manager.java
+++ src/main/java/domain/Manager.java
@@ -12,6 +12,10 @@ public class Manager {
         user = new User();
     }
 
+    /**
+     * 두 개의 질문에 대한 입력을 받고 시도횟수 만큼 각 자동차에 랜덤 값을 부여하여
+     * 조건에 맞으면 전진하고 아니면 멈춰있는걸 반복한다. 그 후 우승자를 출력한다.
+     */
     public void startGame() {
         int winnerPostion=0;
 
@@ -50,10 +54,14 @@ public class Manager {
         }
         return maxPosition;
     }
+
     public int max(int a, int b){
         return (a > b) ? a : b;
     }
 
+    /**
+     * 시도 횟수의 매 턴마다 자동차들의 경주 상태를 출력한다.
+     */
     public void printCurrentState(){
         for(int i=0; i<carList.size(); i++){
             System.out.print(carList.get(i).getName() + ":");
@@ -68,6 +76,10 @@ public class Manager {
         System.out.println();
     }
 
+    /**
+     * 랜덤 넘버를 이용하여 i번째 자동차를 앞으로 전진하게 하는 조건이면
+     * 앞으로 전진하게 한다.
+     */
     public void playIthTurn(){
         for(int i=0; i<carList.size(); i++){
             if(3 < getRandomNumber()){
@@ -84,12 +96,14 @@ public class Manager {
     public boolean secondQuery(){
         System.out.println("시도할 회수는 몇회인가요?");
         String ret = user.inputTryCount();
+
         try{
             Integer.parseInt(ret);
         }catch(NumberFormatException e){
             System.out.println(InputError.NUMBER_FORMAT_ERROR);
             return false;
         }
+
         tryCount = Integer.parseInt(ret);
         return true;
     }
@@ -100,6 +114,10 @@ public class Manager {
         return checkCarName();
     }
 
+    /**
+     * 모든 자동차의 이름의 길이가 5이하 인지 체크한다.
+     * @return boolean
+     */
     public boolean checkCarName(){
         for(int i=0; i<carList.size(); i++) {
             if(carList.get(i).getName().length() > 5) {
--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -22,6 +22,12 @@ public class User {
         return tokenize(input);
     }
 
+    /**
+     * 자동차의 이름들이 적혀 있는 문자열 input을 문자 "," 기준으로
+     * 문자열을 분리하여 carList에 저장한다.
+     * @param input
+     * @return vector<Car>
+     */
     public Vector<Car> tokenize(String input){
         Vector<Car> carList = new Vector<Car>();
         StringTokenizer token = new StringTokenizer(input, ",");
