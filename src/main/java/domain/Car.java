808a094904a2acc63a343db3457e70ac561fe3a4
Leehaewon <leehaewon68@gmail.com>
2019-04-04 10:28:51 +0900

    modify Car Class, add assertName() function(check name's length)

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,11 +1,18 @@
 package domain;
 
 public class Car {
+    public static final int MAX_CAR_NAME_LENGTH = 5;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        assertName(name);
         this.name = name;
+    }
+
+    private void assertName(String name) {
+        if(name.length() > MAX_CAR_NAME_LENGTH || name.length() <= 0)
+            throw new IllegalArgumentException("이름은 5자 이하로 입력해주세요.");
     }
 
     public String getName() {
