712ab725a7e930f765d7cfca588c6ededeb0303a
1000 <1000@DESKTOP-JT4V7I5>
2019-04-04 22:18:53 +0900

    시행횟수 입력받기

--- java-race/src/java/test.java
+++ java-race/src/java/test.java
@@ -1,12 +1,20 @@
 package java;
 
+import java.util.InputMismatchException;
+
 public class test {
 	public static void main(String[] args) {
         RacingGame racingGame = new RacingGame();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은쉼표(,)기준으로구분)");
         try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은쉼표(,)기준으로구분)");
             racingGame.inputCarName();
+            System.out.println("시도할회수는몇회인가요?");
+            racingGame.inputNumOfPlay();
+
+        } catch (InputMismatchException e) {
+            System.out.println("숫자가 아닙니다.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
