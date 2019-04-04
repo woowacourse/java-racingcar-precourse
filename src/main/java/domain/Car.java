ccdfb7c5ae02f23b5490dc8427ea84a8abbdaf40
os94 <ss1511@naver.com>
2019-04-04 13:08:01 +0900

    add Car class

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,3 +1,11 @@
+/*
+ * @class       Car class
+ * @version     1.0.0
+ * @date        19.04.04
+ * @author      OHSANG SEO (tjdhtkd@gmail.com)
+ * @brief       include car info.
+ */
+
 package domain;
 
 public class Car {
@@ -8,13 +16,26 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 
     public void move() {
-
+        Random random = new Random();
+        if (random.getRandomNumber() >= 4) {
+            position++;
+        }
     }
 
     public void printCurrentPosition() {
-
+        System.out.printf("%-5s : ", name);
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
