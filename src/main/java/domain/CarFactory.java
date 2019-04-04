5594e4a9bd12cf73c8e4205e7f35e58829e5935a
DiceMono <qrdfg@naver.com>
2019-04-04 21:02:35 +0900

    12. 가독성 위해 CarFactory의 생성자 제거하고 getCarNameArray메서드 추가.

--- src/main/java/domain/CarFactory.java
+++ src/main/java/domain/CarFactory.java
@@ -16,32 +16,35 @@ import java.util.Scanner;
 public class CarFactory {
 
 	private Scanner scan = new Scanner(System.in);
-	private String[] carNamesArray;
 
-	public CarFactory() {
+	public String[] getCarNameArray() {
+		String[] carNameArray;
+
 		do {
 			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-			String carNames = scan.nextLine();
-			carNamesArray = carNames.split(",");
-		} while (!isValidName(carNamesArray));
+			String carNameString = scan.nextLine();
+			carNameArray = carNameString.split(",");
+		} while (!isValidName(carNameArray));
+		return carNameArray;
 	}
 
-	public Car[] makeCarArray() {
-		Car[] carArray = new Car[carNamesArray.length];
-
-		for (int index = 0; index < carNamesArray.length; index++) {
-			carArray[index] = new Car(carNamesArray[index]);
-		}
-		return carArray;
-	}
-
-	private boolean isValidName(String[] carNamesArray) {
-		for (String carName : carNamesArray) {
+	private boolean isValidName(String[] carNameArray) {
+		for (String carName : carNameArray) {
 			if (carName.length() == 0 || carName.length() > 5) {
-				System.out.println("Error: 자동차 이름은 5자 이하여아 합니다.");
+				System.out.println("Error: 자동차 이름은 1자 이상, 5자이하여아 합니다.");
 				return false;
 			}
 		}
 		return true;
+	}
+
+	public Car[] makeCarArray() {
+		String[] carNameArray = getCarNameArray();
+		Car[] carArray = new Car[carNameArray.length];
+
+		for (int index = 0; index < carNameArray.length; index++) {
+			carArray[index] = new Car(carNameArray[index]);
+		}
+		return carArray;
 	}
 }
