2be5297ebc41a841c505e416530530d13e97035d
deocksoo <dusol92@snu.ac.kr>
2019-04-04 18:42:53 +0900

    [ADD] LeagueHistoryDB를 추가하였습니다
    LeagueHistoryDB 클래스는 경기 기록의 저장하고 관리하기 위한 객체입니다.

---
+++ src/main/java/domain/LeagueHistoryDB.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.util.HashMap;
+
+public class LeagueHistoryDB {
+    public void writeHIstory(String leagueName, int trial, HashMap carPositions) {
+
+    }
+
+    public HashMap getHistory(String leagueName, int trial) {
+        return new HashMap();
+    }
+}
