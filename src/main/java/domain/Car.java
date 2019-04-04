79b7db120827ea77cbe8edba8cd68179fcd9421e
1000 <1000@DESKTOP-JT4V7I5>
2019-04-04 22:05:22 +0900

    자동차의 전진, 정지 파악 후 위치 이동

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,15 +1,27 @@
 package domain;
 
-public class Car {
-    private final String name;
-    private int position = 0;
+import java.util.Random;
 
-    public Car(String name) {
-        this.name = name;
-    }
+public class Car {
+	
+    private final String name;
+    
+    private int position = 0;
     
     private void addPosition() {
         position++;
     }
 
+
+    public void goOrStop() {
+        Random random = new Random();
+        final int bound = 10;
+        final int condition = 3;
+
+        int randomNum = random.nextInt(bound);
+
+        if (randomNum > condition) {
+            addPosition();
+        }
+    }
 }
