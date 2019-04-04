28090909397b1789087157f239692b345da215b4
racoco083 <uytr083@naver.com>
2019-04-04 09:13:04 +0900

    implement forward function to go

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -7,6 +7,10 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
+
     public String getName() { return name; }
+
     public int getPosition() { return position; }
+
+    public void forward() { position++; }
 }
