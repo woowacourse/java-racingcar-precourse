938d6761745a6c674c6c3237ac21edd52d6a9c73
Leehaewon <leehaewon68@gmail.com>
2019-04-04 10:35:37 +0900

    add RacingCarResult Class

---
+++ src/main/java/domain/RacingCarResult.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class RacingCarResult {
+    private List<Car> cars;
+
+    public RacingCarResult(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+} 
