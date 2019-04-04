e7fd795fe1b3d1213016f505a73c5188ffb6702b
bactoria <bactoria@gmail.com>
2019-04-04 14:24:29 +0900

    CLN :: 하드코딩된 구분자를 상수로 수정

--- src/main/java/tokenizer/CommaTokenizer.java
+++ src/main/java/tokenizer/CommaTokenizer.java
@@ -3,10 +3,12 @@ package tokenizer;
 import java.util.Arrays;
 import java.util.List;
 
-public class CommaTokenizer implements Tokenizer{
+public class CommaTokenizer implements Tokenizer {
+
+    private static final String SEPARATOR = ",";
 
     @Override
     public List<String> tokenize(String s) {
-        return Arrays.asList(s.split(","));
+        return Arrays.asList(s.split(SEPARATOR));
     }
 }
