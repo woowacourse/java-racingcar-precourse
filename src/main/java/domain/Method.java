96e971176ef7bc41563e70a790edefc8f0610022
hayoung298 <dudgk298@gmail.com>
2019-12-05 01:03:36 +0900

    SplitCarName() 추가
    차 이름 ',' 로 분리

--- src/main/java/domain/Method.java
+++ src/main/java/domain/Method.java
@@ -9,9 +9,20 @@ public class Method {
 	Scanner scan=new Scanner(System.in);
 	
 	public void SetCarName() {
+		String car;
+		do {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기중으로 구분");
-		String cars=scan.next();
-		SplitCarName(cars);
+		car=scan.next();
+		}while(SplitCarName(car));
+	}
+
+	private boolean SplitCarName(String car) {
+		
+		for(String num : car.split(",")) {
+			System.out.println(num);
+		}
+		
+		return false;
 	}
 	
 	
