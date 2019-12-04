c7114fb776c196214d9a263d2f6ca6d8a58b1707
woonjang <woonjangahn@gmail.com>
2019-12-04 17:38:42 +0200

    FIX: InputController.java의 getNamesOfCars() 반환형 Array에서 List<Car>로 변경

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import domain.Car;
 
 public class InputController {
     private Scanner scanner;
@@ -9,12 +13,16 @@ public class InputController {
         this.scanner = new Scanner(System.in);
     }
 
-    public String[] getNameOfCars() {
+    public List<Car> getNameOfCars() {
+        List<Car> cars = new ArrayList<>();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = this.scanner.nextLine();
         //    TODO: 예외 처리
         String[] carNames = userInput.split(",");
-        return carNames;
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 
     public int getTimesToRun() {
