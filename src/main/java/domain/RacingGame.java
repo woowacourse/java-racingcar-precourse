e8fa10b747f9d2bfdeb62d426d285f1b12dc1db2
sssssseungmi <tmdal9516@naver.com>
2019-12-04 19:42:25 +0900

    Add inputRacingCount method in RacingGame.java

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class RacingGame {
 	private Scanner sc;
 	private List<Car> carList;
+	private int racingCount;
 
 	public RacingGame() {
 		sc = new Scanner(System.in);
@@ -17,7 +18,26 @@ public class RacingGame {
 
 	}
 
-	private void inputCarNames() {
+	private boolean inputCarName() {
+		boolean isAvailable = false;
+
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String carNames = sc.nextLine();
-	}	
+
+		return isAvailable;
+	}
+
+	private boolean inputRacingCount() {
+		boolean isAvailable = false;
+
+		System.out.println("시도할 횟수는 몇회인가요?");
+		try {
+			racingCount = Integer.parseInt(sc.nextLine());
+		} catch (Exception e) {
+			System.out.println("숫자를 입력해 주세요!!!!!!");
+		}
+
+		return isAvailable;
+	}
+
 }
