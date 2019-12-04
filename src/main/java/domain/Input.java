c34ae3917b18f6d390f63a77f70ffb8f1e9fc5f0
KimSeongGyu1 <KimSeongGyu1@github.com>
2019-12-04 18:08:05 +0900

    [입력] 반복문 대신 재귀 사용

--- src/main/java/domain/Input.java
+++ src/main/java/domain/Input.java
@@ -12,17 +12,21 @@ public class Input {
 		String carNames;
 		String[] dividedCarNames;
 
-		do {
-			carNames = scanner.nextLine();
-			dividedCarNames = carNames.split(",");
-		} while (!isValidInput(dividedCarNames));
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		carNames = scanner.nextLine();
+		dividedCarNames = carNames.split(",");
+		
+		if (!isValidInput(dividedCarNames)) {
+			System.out.println("5자 이하의 비어있지 않은 이름만 입력해주세요");
+			return enterCarNames();
+		}
 
 		return convertArrToList(dividedCarNames);
 	}
 
 	private static boolean isValidInput(String[] inputString) {
 		for (int i = 0; i < inputString.length; i++) {
-			if (inputString[i].length() > MAX_NAME_LEN) {
+			if ((inputString[i].length() > MAX_NAME_LEN) || (inputString[i].length() == 0)) {
 				return false;
 			}
 		}
