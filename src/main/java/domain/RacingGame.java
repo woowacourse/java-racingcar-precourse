a7b192dbb64ce6349ae392a51c65d2f0a16129d4
sssssseungmi <tmdal9516@naver.com>
2019-12-04 19:32:13 +0900

    Add inputCarNames method in RacingGame.java

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,5 +1,23 @@
 package domain;
 
-public class RacingGame {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class RacingGame {
+	private Scanner sc;
+	private List<Car> carList;
+
+	public RacingGame() {
+		sc = new Scanner(System.in);
+		carList = new ArrayList<>();
+	}
+
+	public void startGame() {
+
+	}
+
+	private void inputCarNames() {
+		String carNames = sc.nextLine();
+	}	
 }
