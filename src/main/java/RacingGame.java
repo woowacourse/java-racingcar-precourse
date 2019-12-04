81dc6c34333e6448916b748aedff66a21e576059
woonjang <woonjangahn@gmail.com>
2019-12-04 16:42:49 +0200

    FEAT: 게임 테스트를 위한 RacingGame.java 및 play() 메소드 작성

---
+++ src/main/java/RacingGame.java
@@ -0,0 +1,9 @@
+public class RacingGame {
+    public static void main(String[] args) {
+        play();
+    }
+
+    public static void play() {
+        System.out.println("게임을 시작합니다.");
+    }
+}
