38495eac3dc10c3f1569725f35d29bc02f9481e2
begaonnuri <begaonnuri@gmail.com>
2019-12-04 16:08:40 +0900

    [Etc] 구현할 클래스 생성, empty 파일 제거

---
+++ src/main/java/system/RacingCarSystem.java
@@ -0,0 +1,4 @@
+package system;
+
+public class RacingCarSystem {
+}
---
+++ src/test/java/RacingCarTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RacingCarTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testRacingCar() {
+    }
+}
