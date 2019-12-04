11980b1bff582d84d573f4f25737e41006cf63cf
woonjang <woonjangahn@gmail.com>
2019-12-04 17:06:38 +0200

    FEAT: RaceController.java 작성

---
+++ src/main/java/controller/RaceController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.util.List;
+
+import domain.Car;
+
+public class RaceController {
+    private List<Car> cars;
+
+    public RaceController(List<Car> cars) {
+        this.cars = cars;
+    }
+
+}
