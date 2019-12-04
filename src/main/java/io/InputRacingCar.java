2a2449b73b4544df4516b428030c754ee93a87b0
begaonnuri <begaonnuri@gmail.com>
2019-12-04 16:36:52 +0900

    [Feature] 자동차의 이름을 입력하는 기능, 몇 번 이동할지 입력하는 기능 구현

--- src/main/java/io/InputRacingCar.java
+++ src/main/java/io/InputRacingCar.java
@@ -1,4 +1,18 @@
 package io;
 
+import java.util.Scanner;
+
 public class InputRacingCar {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static String inputCarsName() {
+        // TODO: 2019-12-04 이름이 5글자 초과인 경우 예외처리 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return sc.nextLine();
+    }
+
+    public static int inputRepeatTime() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextInt();
+    }
 }
