11c3e8b532c8a8792a98978ce204d2c5e9424795
woonjang <woonjangahn@gmail.com>
2019-12-04 17:40:31 +0200

    FEAT: RaceController.java 에서 Car들 별로 랜덤하게 전진하는 메소드 구현

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,6 +1,8 @@
-import java.util.Arrays;
+import java.util.List;
 
 import controller.InputController;
+import controller.RaceController;
+import domain.Car;
 
 public class RacingGame {
     public static void main(String[] args) {
@@ -10,7 +12,9 @@ public class RacingGame {
     public static void play() {
         System.out.println("게임을 시작합니다.");
         InputController inputController = new InputController();
-        System.out.println(Arrays.toString(inputController.getNameOfCars()));
-        System.out.println(inputController.getTimesToRun());
+        List<Car> cars = inputController.getNameOfCars();
+        int timesToRun = inputController.getTimesToRun();
+        RaceController raceController = new RaceController(cars);
+        raceController.loopThroughCars();
     }
 }
--- src/main/java/controller/RaceController.java
+++ src/main/java/controller/RaceController.java
@@ -3,12 +3,33 @@ package controller;
 import java.util.List;
 
 import domain.Car;
+import util.RandomGenerator;
 
 public class RaceController {
     private List<Car> cars;
+    private RandomGenerator randomGenerator;
 
     public RaceController(List<Car> cars) {
         this.cars = cars;
+        this.randomGenerator = new RandomGenerator();
     }
 
+    public void loopThroughCars() {
+        for (Car car : this.cars) {
+            goOrStop(car);
+        }
+        printCars();
+    }
+
+    private void goOrStop(Car car) {
+        if (this.randomGenerator.isGoingForward()) {
+            car.goForward();
+        }
+    }
+
+    private void printCars() {
+        for (Car car : this.cars) {
+            System.out.println(car);
+        }
+    }
 }
