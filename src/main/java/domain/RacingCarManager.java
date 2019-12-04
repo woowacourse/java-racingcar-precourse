636ac03d98b35065a4f21af0c4f3e3229043ed93
DB <rkdrbgks22@ks.ac.kr>
2019-12-04 17:43:23 +0900

    [ADD] carName을 등록할 메서드 생성
    - Name으로 입력받은 파라미터가 오류가 없는지 판별하여 최종적으로 등록

--- src/main/java/domain/RacingCarManager.java
+++ src/main/java/domain/RacingCarManager.java
@@ -1,13 +1,33 @@
 package domain;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RacingCarManager {
-    public void start() {
+    List<Car> registeredCars = new LinkedList<>();
+
+    public boolean start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        //carName();
+        if (!carName(new Scanner(System.in).next())) {
+            return false;
+        }
 
         System.out.println("시도할 회수는 몇회인가요?");
         //tryTimes();
+
+        return true;
     }
+
+    public boolean carName(String name) {
+        Validator validator = new Validator();
+
+        //registeredCars = validator.splitName(name);
+        /*if (validator.isNotNull(registeredCars)
+                && validator.isNotExcess(registeredCars) && validator.isNotBelowZero(registeredCars)) {
+            return true;
+        }*/
+        return false;
+    }
+
 }
