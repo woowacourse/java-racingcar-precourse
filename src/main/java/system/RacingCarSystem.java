b10c723000b37f081d30ee6a73a879e2ac208aa9
begaonnuri <begaonnuri@gmail.com>
2019-12-04 23:23:17 +0900

    [Feature] Car 객체를 생성하는 기능 구현

--- src/main/java/system/RacingCarSystem.java
+++ src/main/java/system/RacingCarSystem.java
@@ -1,26 +1,36 @@
 package system;
 
+import domain.Car;
 import io.InputRacingCar;
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class RacingCarSystem {
-    private String[] setCarNameArr;
+    private String[] carNameArr;
+    private List<Car> cars = new LinkedList<>();
 
     public void setUpRacing() {
         String carsName = InputRacingCar.inputCarsName();
         setCarNameArr(carsName);
+        setCarList();
     }
 
     public void setCarNameArr(String carsName) {
         StringTokenizer st = new StringTokenizer(carsName, ",");
-        setCarNameArr = new String[st.countTokens()];
+        carNameArr = new String[st.countTokens()];
 
         int index = 0;
         while (st.hasMoreTokens()) {
-            setCarNameArr[index] = st.nextToken();
+            carNameArr[index] = st.nextToken();
             index++;
+        }
+    }
+
+    public void setCarList() {
+        for (int i = 0; i < carNameArr.length; i++) {
+            cars.add(new Car(carNameArr[i]));
         }
     }
 }
