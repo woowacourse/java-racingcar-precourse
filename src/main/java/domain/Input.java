0dcdc18b2345b1a279cfdc69db63c16ebf8cf586
KimSeongGyu1 <KimSeongGyu1@github.com>
2019-12-04 23:04:13 +0900

    [입력] 라운드 회수 최소 값 1 로 변경, 인풋에 띄어쓰기 무시 추가

--- src/main/java/domain/Input.java
+++ src/main/java/domain/Input.java
@@ -5,15 +5,18 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Input {
-	private static final int MAX_NAME_LEN = 5;
-	private static final int MIN_ROUNDS = 0;
+	private static final int MAX_LEN_OF_NAME = 5;
+	private static final int MIN_LEN_OF_NAME = 1;
+	private static final int MIN_ROUNDS = 1;
+	
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static List<String> enterCarNames() {
 		String[] carNames;
 
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		carNames = scanner.nextLine().split(",");
+		carNames = scanner.nextLine()
+				.replace(" ", "").split(",");
 		if (!isValidInput(carNames)) {
 			System.out.println("5자 이하의 비어있지 않은 이름만 입력해주세요");
 			return enterCarNames();
@@ -24,11 +27,12 @@ public class Input {
 
 	private static boolean isValidInput(String[] inputString) {
 		for (int i = 0; i < inputString.length; i++) {
-			if ((inputString[i].length() > MAX_NAME_LEN) || (inputString[i].length() == 0)) {
+			if ((inputString[i].length() > MAX_LEN_OF_NAME)
+					|| (inputString[i].length() < MIN_LEN_OF_NAME)) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 
@@ -38,25 +42,25 @@ public class Input {
 		for (int i = 0; i < inputString.length; i++) {
 			outputList.add(inputString[i]);
 		}
-		
+
 		return outputList;
 	}
-	
+
 	public static int enterRounds() {
 		int rounds = enterInt();
-		
+
 		if (rounds < MIN_ROUNDS) {
-			System.out.println("0 이상의 숫자를 입력해주세요");
+			System.out.println("1 이상의 숫자를 입력해주세요");
 			return enterRounds();
 		}
-		
+
 		return rounds;
 	}
-	
+
 	private static int enterInt() {
 		try {
 			System.out.println("시도할 회수는 몇회인가요?");
-			return Integer.parseInt(scanner.nextLine());
+			return Integer.parseInt(scanner.nextLine().trim());
 		} catch (IllegalArgumentException e) {
 			System.out.println("숫자를 입력해주세요");
 			return enterInt();
