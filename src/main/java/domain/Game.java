22a0e0c9202d11579506e6b0b07605d70bbcc13f
jiwon_lala <s26788761@naver.com>
2019-12-05 21:49:37 +0900

    feat : 입력받은 자동차 이름 체크함수 구현
    private boolean checkUserInput(String[] inputArray);
    //입력받은 inputArray이 문제가 있으면 false를 return하고, 문제가 없으면 새로운 Car 객체를 만들어 cars ArrayList에 add() 하고 true를 return하는 함수
    private boolean chekStringLength(String string, int min, int max);
    //입력받은 string의 길이가 min 이상 max 이하인지 체크하여 맞으면 true return, 아니면 false return 하는 함수

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,9 +1,13 @@
 package domain;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Game {
+    static final int MIN_CAR_NAME_LENGTH = 1;
+    static final int MAX_CAR_NAME_LENGTH = 5;
     Scanner scanner;
+    ArrayList<Car> cars;
 
     private String[] getUserInput() {
         String userInput;
@@ -14,6 +18,25 @@ public class Game {
         nameArray = userInput.split(",");
 
         return nameArray;
+    }
+
+    private boolean checkUserInput(String[] inputArray) {
+        for (int i=0; i<inputArray.length; i++) {
+            if (!checkStringLength(inputArray[i], MIN_CAR_NAME_LENGTH, MAX_CAR_NAME_LENGTH)) {
+                return false;
+            }
+            Car car = new Car(inputArray[i]);
+            cars.add(car);
+        }
+        return true;
+    }
+
+    private boolean checkStringLength(String string, int min, int max) {
+
+        if ((min <= string.length()) && (string.length() <= max)) {
+            return true;
+        }
+        return false;
     }
 
 }
