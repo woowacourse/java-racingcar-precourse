8f575c9026eeed8508659065c3d090839d53b696
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-05 17:37:33 +0900

    feat: 사용자가 입력한 자동차들의 정보를 반환하는 기능 추가

--- src/main/java/com/precourse/racingcar/User.java
+++ src/main/java/com/precourse/racingcar/User.java
@@ -1,5 +1,5 @@
 /*
- * User.java                       1.0.0   2019-12-05
+ * User.java                       1.1.0   2019-12-05
  *
  * Copyright (c) 2019 Hyungju An.
  * All rights reserved.
@@ -8,18 +8,29 @@
 
 package com.precourse.racingcar;
 
+import java.util.ArrayList;
+
 /**
  * User 클래스입니다.
  * 사용자에 대한 입력과 출력을 하는 기능들이 있습니다.
  * 입력이 올바르지 않은 경우 예외처리를 하는 기능이 있습니다.
  *
- * @version 1.0.0
+ * @version 1.1.0            자동차들의 정보를 반환하는 기능 추가
  * @date 2019-12-05
  * @author HyungjuAn
  */
 public class User {
 
 	public User() {
+	}
+
+	public ArrayList<Car> getCarsInformation() {
+		ArrayList<Car> carList = new ArrayList<>();
+
+		// 사용자에게 자동차들의 이름을 물어보는 기능
+		// 사용자가 자동차들의 이름을 입력하는 기능
+
+		return carList;
 	}
 
 	@Override
