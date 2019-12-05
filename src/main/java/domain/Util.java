d82f18bb48daca1fd7494f4f6cc5c22e54033b9b
barrett24 <barrett2440@gmail.com>
2019-12-05 20:54:07 +0900

    main함수 역할을 위한 Util 클래스의 접근제어자 수정

--- src/main/java/domain/Util.java
+++ src/main/java/domain/Util.java
@@ -1,19 +1,19 @@
 package domain;
 
 public class Util {
-    static String carNameArray[];
-    static Car carObjectArray[];
+    private String carNameArray[];
+    private Car carObjectArray[];
 
     static final int MINIMUM_NAME_LENGTH = 1;
     static final int LIMIT_NAME_LENGTH = 5;
 
     // 쉼표를 기준으로 자동차의 이름들을 분류
-   static void splitCarName(String rawCarName) {
+    void splitCarName(String rawCarName) {
         carNameArray = rawCarName.split(",");
     }
 
     // 공백이 아니고 길이가 5이하인 이름만으로 구성되어있는지 검사
-    static boolean isNameLengthValid() {
+    boolean isNameLengthValid() {
         for (int i = 0; i < carNameArray.length; i++) {
             if ((MINIMUM_NAME_LENGTH > carNameArray[i].length())
                     || (carNameArray[i].length() > LIMIT_NAME_LENGTH)) {
@@ -26,7 +26,7 @@ public class Util {
     }
 
     // 각 이름별로 객체 배열 생성
-    static void makeCarObjects() {
+    void makeCarObjects() {
         carObjectArray = new Car[carNameArray.length];
         for (int i = 0; i < carNameArray.length; i++) {
             carObjectArray[i] = new Car(carNameArray[i]);
