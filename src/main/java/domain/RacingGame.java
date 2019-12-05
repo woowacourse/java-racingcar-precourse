fde977acc449a38c26f87827a9b6d5e77e5d130e
jb-oh <jb.oh@icloud.com>
2019-12-05 18:15:43 +0900

    Added feature that gets user input for cars' name.

---
+++ src/main/java/domain/RacingGame.java
@@ -0,0 +1,58 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class RacingGame {
+
+    private static boolean isCarNameCorrect = false;
+
+    private static ArrayList<String> getCarNameInput() {
+        String carNames = "";
+        Scanner carNameInput = new Scanner(System.in);
+        ArrayList<String> userCarNameArray = new ArrayList<String>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        carNames = carNameInput.next();
+        Collections.addAll(userCarNameArray, carNames.split(","));
+
+        return userCarNameArray;
+    }
+
+    private static boolean checkCarNameInput(ArrayList<String> userCarNameArray) {
+        if (userCarNameArray.size() < 2) {
+            return false;
+        }
+
+        for (String s : userCarNameArray) {
+            if (s.length() > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static ArrayList<String> returnCarNameInput(ArrayList<String> userCarNameArray) {
+        ArrayList<String> emptyArray = new ArrayList<>();
+        emptyArray.add("Wrong");
+
+        if (isCarNameCorrect) {
+            return userCarNameArray;
+        }
+        return emptyArray;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> carNamesArray;
+
+        do {
+            carNamesArray = getCarNameInput();
+            isCarNameCorrect = checkCarNameInput(carNamesArray);
+            returnCarNameInput(carNamesArray);
+        } while (!isCarNameCorrect);
+
+        System.out.println(carNamesArray);
+    }
+}
