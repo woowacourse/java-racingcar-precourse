85cc75f05a92bda213f0f5cd3f4915c10ef93e31
jaeju.jang <jjj0611@gmail.com>
2019-12-05 21:03:21 +0900

    [ADD] 레이싱카 게임 전체 진행을 위한 RacingCarGame클래스를 추가했습니다.
    	- 게임 초기화 메서드를 추가했습니다.
    	- 게임 진행 메서드를 추가했습니다.

--- src/main/java/game/RacingCarGame.java
+++ src/main/java/game/RacingCarGame.java
@@ -1,4 +1,109 @@
 package game;
 
+import domain.Cars;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
 public class RacingCarGame {
+
+    private Cars cars;
+    private int numberOfTimes;
+    private Scanner gameScanner;
+    private Random random;
+
+    RacingCarGame() {
+        gameScanner = new Scanner(System.in);
+        random = new Random();
+    }
+
+    public static void main(String[] args) {
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.initGame();
+        racingCarGame.runGame();
+    }
+
+    public void initGame() {
+        initCars();
+        initNumberOfTimes();
+    }
+
+    public void runGame() {
+        for (int i = 0; i < numberOfTimes; i++) {
+            cars.runOneTime(random);
+            cars.showCarsStatus();
+        }
+    }
+
+    private void initNumberOfTimes() {
+        numberOfTimes = makeNumberOfTimes();
+    }
+
+    private int makeNumberOfTimes() {
+        while(true) {
+            try {
+                String numberOfTimesString = takeNumberOfTimes();
+                return Integer.parseInt(numberOfTimesString);
+            } catch (NumberFormatException ne) {
+                System.out.println("시도 횟수는 숫자만 입력할 수 있습니다.");
+            }
+        }
+    }
+
+    private void initCars() {
+        List<String> names = makeNamesList();
+        cars = new Cars(names);
+    }
+
+    private List<String> makeNamesList() {
+        while(true) {
+            try {
+                String namesString = takeNames();
+                List<String> names = Arrays.asList(namesString.split(","));
+                validates(names);
+                return names;
+            } catch (IllegalArgumentException ie) {
+                System.out.println(ie.getMessage());
+            }
+        }
+    }
+
+    private void validates(List<String> names) {
+        if (checkLengthOverBasis(names)) {
+            throw new IllegalArgumentException();
+        }
+        if (checkDuplication(names)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkLengthOverBasis(List<String> names) {
+        for (String name : names) {
+            if (name.length() > 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDuplication(List<String> names) {
+        return names.stream().distinct().count() != names.size();
+    }
+
+    private String takeNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        return gameScanner.nextLine();
+    }
+
+    private String takeNumberOfTimes() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return gameScanner.nextLine();
+    }
+
 }
