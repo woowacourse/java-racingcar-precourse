9ac84359db664f4a2b037dd51fc12f0206775f53
hayoung298 <dudgk298@gmail.com>
2019-12-05 09:24:02 +0900

    CheckStringLength() 추가
    이름 5자 초과시  조건문 함수

--- src/main/java/domain/Method.java
+++ src/main/java/domain/Method.java
@@ -4,29 +4,37 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Method {
-	
+
 	static Car[] cars;
-	Scanner scan=new Scanner(System.in);
-	
+	Scanner scan = new Scanner(System.in);
+
 	public void SetCarName() {
 		String car;
 		do {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기중으로 구분");
-		car=scan.next();
-		}while(SplitCarName(car));
+			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+			car = scan.next();
+		} while (SplitCarName(car));
+		//InputRunCount();
 	}
 
 	private boolean SplitCarName(String car) {
-		int i=0;
-		for(String num : car.split(",")) {
-			cars[i]=new Car(num);
+		int i = 0;
+		for (String num : car.split(",")) {
+			if (!CheckStringLength(num)) {
+				System.out.println("5자를 초과하셨습니다");
+				return true;
+			}
+			//cars[i] = new Car(num);
 			i++;
-		}	
-		if(CheckStringLength());
-			SetCarName();
-		InputRunCount();	
+		}
+		return false;
 	}
-	
-	
-	
+
+	private boolean CheckStringLength(String carName) {
+		if(carName.length()<6)
+			return true;
+
+		return false;
+	}
+
 }
