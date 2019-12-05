d84c064f04aaa6759e7f9e85d6ccb822e645bc3b
barrett24 <barrett2440@gmail.com>
2019-12-05 20:43:54 +0900

    Car 객체에 포지션 연산 및 반환기능 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -8,5 +8,15 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void addPosition() {
+        this.position++;
+    }
 }
