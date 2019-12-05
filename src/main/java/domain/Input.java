e8949a6b5f184a81865472f0480c3c70af8c6408
KimSeongGyu1 <KimSeongGyu1@github.com>
2019-12-05 15:27:01 +0900

    [입력]수정: 빈줄 처리 가독성 개선

--- src/main/java/domain/Input.java
+++ src/main/java/domain/Input.java
@@ -13,10 +13,9 @@ public class Input {
 	
 	public static List<String> enterCarNames() {
 		String[] carNames;
-		
 		try {
 			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-			carNames = SCANNER.nextLine().split(",");
+			carNames = SCANNER.nextLine().trim().split(",");
 			checkIfValidCarNames(carNames);
 			return convertArrToList(carNames);
 		} catch (IllegalArgumentException e) {
@@ -40,17 +39,14 @@ public class Input {
 	
 	private static List<String> convertArrToList(String[] inputStringArr) {
 		List<String> outputList = new ArrayList<String>();
-
 		for (int i = 0; i < inputStringArr.length; i++) {
 			outputList.add(inputStringArr[i]);
 		}
-
 		return outputList;
 	}
 
 	public static int enterRounds() {
 		int rounds;
-		
 		try {
 			System.out.println("시도할 회수는 몇회인가요?");
 			rounds = enterInt();
