e4c2fa2cd7aefea059f400a2364c39ee633e5747
choiyeonseok <smr603@snu.ac.kr>
2019-12-05 16:24:42 +0900

    기능 : 자동차 경주 게임 인스턴스 생성 및 게임 실행

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 자동차 경주 게임 인스턴스를 만들고 play로 게임실행
+		 */
+		RacingGame game = new RacingGame();
+		game.play();
+
+	}
+
+}
