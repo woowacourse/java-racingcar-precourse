f7df9046cb7f8246069693b0437de050c9d746df
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-05 17:12:21 +0900

    feat: 사용자의 입출력을 담당할 User class 추가

---
+++ src/main/java/com/precourse/racingcar/User.java
@@ -0,0 +1,33 @@
+/*
+ * User.java                       1.0.0   2019-12-05
+ *
+ * Copyright (c) 2019 Hyungju An.
+ * All rights reserved.
+ * Contact me for more information. a301dks@naver.com
+ */
+
+package com.precourse.racingcar;
+
+/**
+ * User 클래스입니다.
+ * 사용자에 대한 입력과 출력을 하는 기능들이 있습니다.
+ * 입력이 올바르지 않은 경우 예외처리를 하는 기능이 있습니다.
+ *
+ * @version 1.0.0
+ * @date 2019-12-05
+ * @author HyungjuAn
+ */
+public class User {
+
+	public User() {
+	}
+
+	@Override
+	protected final void finalize() throws Throwable {
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
