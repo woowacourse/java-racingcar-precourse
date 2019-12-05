71b67760b0b3170192b964b38181fdb6ccc62011
moonyoungCHAE <xpf_fl@naver.com>
2019-12-05 15:21:48 +0900

    사용자로부터 자동차 이름 입력받기

---
+++ src/main/java/domain/RaingCarManager.java
@@ -0,0 +1,23 @@
+package domain;
+/*
+ * 클래스 이름 : RacingGameManager
+ * 작성자 : Moonyoung Chae
+ * 날짜 : 2019/12/04
+ * Copyright (c) 2019 Moonyoung Chae
+ */
+
+import java.util.Scanner;
+
+public class RaingCarManager {
+    Scanner scanner = new Scanner(System.in);
+
+    void run(){
+        String carNamesFromUser = getCarNamesFromUser();
+    }
+
+    String getCarNamesFromUser() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String carNamesFromUser = scanner.nextLine();
+        return carNamesFromUser;
+    }
+}
