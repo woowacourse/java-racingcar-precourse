5d5afbdc2fa44b145b1f4c9e46faf078b062050f
Min-92 <0201.mj.lee@gmail.com>
2019-12-05 15:26:04 +0900

    feat : 자동차 이름 입력받는 기능 추가
    - getCarNames 메소드 생성

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -5,6 +5,9 @@ public class Game {
   IOController ioController = new IOController();
 
   public void play() {
+    String[] carNames = {};
+
     ioController.askCarsNames();
+    carNames = ioController.getCarsNames();
   }
 }
--- src/main/java/domain/IOController.java
+++ src/main/java/domain/IOController.java
@@ -1,7 +1,19 @@
 package domain;
 
+import java.util.Scanner;
+
 public class IOController {
   public void askCarsNames(){
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+  }
+
+  public String[] getCarsNames(){
+    String userInput;
+    String[] carNames = {};
+    Scanner scanner = new Scanner(System.in);
+
+    userInput = scanner.nextLine();
+
+    return carNames;
   }
 }
