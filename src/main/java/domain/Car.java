82fef1cc9802eb04ca5cf086e881037bed0217fa
choiyeonseok <smr603@snu.ac.kr>
2019-12-05 15:57:26 +0900

    기능 : 현재 포지션 만큼 결과 출력

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,17 +2,25 @@ package domain;
 
 public class Car {
 	private final static int RANGE = 10;
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    public void goForwardOrNot() {
+	public void goForwardOrNot() {
 		if ((int) (Math.random() * RANGE) >= 4) {
 			position++;
 		}
 	}
-    
+
+	public void showPosition() {
+		System.out.print(name + " : ");
+		for (int i = 0; i < position; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+
 }
