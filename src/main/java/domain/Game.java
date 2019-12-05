d01f5e511982c307cde4017db56cae5d9a7dbe5c
giantim <giantim132@gmail.com>
2019-12-05 09:11:43 +0900

    기본 클래스 추가

---
+++ src/main/java/domain/Game.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+	ArrayList<Car> myCar;
+	Scanner in;
+	
+	public Game() {
+		myCar = new ArrayList<Car>();
+		in = new Scanner(System.in);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
