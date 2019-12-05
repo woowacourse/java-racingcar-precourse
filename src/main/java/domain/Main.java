fa5010e0d3b8e0a6752372641f27bb4be5574afd
plzprayme <dbfpzk142@gmail.com>
2019-12-05 15:15:30 +0900

    기능구현 : N개의 자동차 이름 입력 받기 & 이동 횟수 입력 받기

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,28 @@
+/**
+ * Main 클래스
+ * <p>
+ * 자동차 이름, 이동 횟수 입력 받을 예정
+ * run() 함수도 호출할 예정
+ * 이동 횟수만큼 반복 예정
+ */
+package domain;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String carNames = scanner.next();
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int iterationNumber = scanner.nextInt();
+
+
+
+
+    }
+
+
+}
