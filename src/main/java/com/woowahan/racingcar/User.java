580fd47f6228f292fb3fa308d70340bd5446b3f6
Jinhong Kim <deepred.kim@gmail.com>
2019-12-05 16:58:12 +0900

    Create User.java

---
+++ src/main/java/com/woowahan/racingcar/User.java
@@ -0,0 +1,36 @@
+package com.woowahan.racingcar;
+
+import java.util.ArrayList;
+
+public class User extends AbstractUser {
+
+	@Override
+	public ArrayList<Car> getCars() {
+		return null;
+	}
+
+	@Override
+	public int getRound() {
+		return 0;
+	}
+
+	@Override
+	protected String getLine() {
+		return null;
+	}
+
+	@Override
+	protected void inputCars() {
+
+	}
+
+	@Override
+	protected void inputRound() {
+
+	}
+
+	@Override
+	public void input() {
+
+	}
+}
