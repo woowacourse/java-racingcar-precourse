59df57a1f72c960a857073258fcb069aaa098ff6
minwoo_yim <minu_yim@naver.com>
2019-12-05 23:25:15 +0900

    totalnum 입력 기능 추가

--- src/main/java/com/minuyim/race/utils/Constants.java
+++ src/main/java/com/minuyim/race/utils/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
     public static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String WRONG_TYPE_ERROR = "타입에 맞게 입력해주세요.";
     public static final String WRONG_LENGTH_ERROR = "각 차량의 이름을 5자 이하로 입력해주세요.";
+    public static final String TOTALNUM_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String WRONG_NUMBER_ERROR = "잘못된 값을 입력하셨습니다. 다시 입력해주세요.";
     public static final int STRING_LIMIT_NUMBER = 5;
 }
---
+++ src/main/java/com/minuyim/race/utils/NumberInputs.java
@@ -0,0 +1,41 @@
+package com.minuyim.race.utils;
+
+public class NumberInputs {
+    private int output;
+    
+    public NumberInputs() {
+        this.startInput();
+    }
+    
+    public int getOutput() {
+        return this.output;
+    }
+    
+    private void startInput() {
+        System.out.println(Constants.TOTALNUM_INPUT_MESSAGE);
+        inputRightValue();
+    }
+    
+    private void inputRightValue() {
+        while (true) {
+            try {
+                this.output = validate(UserInputs.input());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println(Constants.WRONG_TYPE_ERROR);
+            } catch (IllegalArgumentException e) {
+                System.out.println(Constants.WRONG_NUMBER_ERROR);
+            }
+        }
+    }
+    
+    private int validate(String input) {
+        int num = Integer.valueOf(input);
+        
+        if (num <= 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        return num;
+    }
+}
