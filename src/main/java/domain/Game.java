ab767344f097fe9c012d7fec3c558008fbfb09a0
cjw7242 <cjw7242@gmail.com>
2019-12-05 19:57:18 +0900

    기능 : 자동차 이름 분리 및 List저장 구현

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,11 +1,11 @@
 package domain;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Game {
-	private List<Car> carList;
+	private ArrayList<Car> carList;
 	private Scanner scan;
 	private int tryGame;
 
@@ -16,10 +16,20 @@ public class Game {
 		requestCarName();
 	}
 
-	public void requestCarName() {
+	private void requestCarName() {
 		String carName;
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
 		carName = scan.nextLine();
 
+		divideCarName(carName);
+	}
+
+	private void divideCarName(String cars) {
+		StringTokenizer carsToken = new StringTokenizer(cars);
+
+		for (int i = 1; carsToken.hasMoreTokens(); i++) {
+			carList.add(new Car(carsToken.nextToken(",")));
+			System.out.println(carList.get(carList.size() - 1).getName());
+		}
 	}
 }
--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -3,6 +3,8 @@ package domain;
 public class Main {
 
 	public static void main(String[] args) {
+		Game game;
+		game = new Game();
 
 	}
 
