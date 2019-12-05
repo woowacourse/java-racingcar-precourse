114c559e83024623e34ae0e3ebde2cd3c743bd69
Min-92 <0201.mj.lee@gmail.com>
2019-12-05 15:32:07 +0900

    feat : 이름을 쉼표 기준으로 구분하는 기능 생성
    - parseName 메소드 생성

--- src/main/java/domain/IOController.java
+++ src/main/java/domain/IOController.java
@@ -3,16 +3,27 @@ package domain;
 import java.util.Scanner;
 
 public class IOController {
-  public void askCarsNames(){
+
+  public void askCarsNames() {
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
   }
 
-  public String[] getCarsNames(){
+  public String[] getCarsNames() {
     String userInput;
     String[] carNames = {};
     Scanner scanner = new Scanner(System.in);
 
     userInput = scanner.nextLine();
+
+    carNames = parseNames(userInput);
+
+    return carNames;
+  }
+
+  private String[] parseNames(String input) {
+    String[] carNames = {};
+
+    carNames = input.split(",");
 
     return carNames;
   }
