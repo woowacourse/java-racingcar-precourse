0afa6cd750a65b9bfafd00af2785e04775888228
barrett24 <barrett2440@gmail.com>
2019-12-05 20:10:20 +0900

    공백이 아니고 길이가 5이하인 이름만으로 구성되어있는지 검사

--- src/main/java/domain/Util.java
+++ src/main/java/domain/Util.java
@@ -3,8 +3,24 @@ package domain;
 public class Util {
     static String carNameArray[];
 
-    //쉼표를 기쥰
-    static void splitCarName(String rawCarName) {
+    static final int MINIMUM_NAME_LENGTH = 1;
+    static final int LIMIT_NAME_LENGTH = 5;
+
+    // 쉼표를 기준으로 자동차의 이름들을 분류
+   static void splitCarName(String rawCarName) {
         carNameArray = rawCarName.split(",");
+    }
+
+    // 공백이 아니고 길이가 5이하인 이름만으로 구성되어있는지 검사
+    static boolean isNameLengthValid() {
+        for (int i = 0; i < carNameArray.length; i++) {
+            if ((MINIMUM_NAME_LENGTH > carNameArray[i].length())
+                    || (carNameArray[i].length() > LIMIT_NAME_LENGTH)) {
+                System.out.println("유효하지 않은 자동차 이름이 있습니다.");
+                System.out.println("공백이 아니고 길이가 5이하인 이름을 쉼표로 구분하여 입력해주세요.");
+                return false;
+            }
+        }
+        return true;
     }
 }
