10244253aa0f0fe2e0b97c6adb02abe606511a6e
barrett24 <barrett2440@gmail.com>
2019-12-05 22:07:24 +0900

    main 함수의 구동 부분

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -1,7 +1,23 @@
 package domain;
 
+import java.util.Scanner;
+
 public class GameManager {
     public static void main(String args[]) {
-        
+        Scanner sc = new Scanner(System.in);
+        Util util = new Util();
+
+        do {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String tempIn = sc.nextLine();
+            util.splitCarName(tempIn);
+        } while(!util.isNameLengthValid());
+        util.makeCarObjects();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int repeat = sc.nextInt();
+
+        System.out.println("실행결과");
+        util.repeatGameAndPrint(repeat);
+        util.printFinalWinners();
     }
 }
