ab7a8add4b17ca27023dd14470d12b7c37e00672
DB <rkdrbgks22@ks.ac.kr>
2019-12-05 11:17:58 +0900

    [UPDATE] Test 분리
    - Validator 관련된 테스트 코드를 분리

--- src/main/java/domain/Validator.java
+++ src/main/java/domain/Validator.java
@@ -1,7 +1,6 @@
 package domain;
 
 import java.util.Collections;
-import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.List;
 
--- src/test/java/domain/RacingCarManagerTest.java
+++ src/test/java/domain/RacingCarManagerTest.java
@@ -22,64 +22,6 @@ class RacingCarManagerTest {
     }
 
     @Test
-    public void carNameNullTest() {
-        Validator validator = new Validator();
-        List<String> carNames = new LinkedList<>();
-
-        carNames.add("John");
-        carNames.add("Kim");
-        carNames.add("James");
-
-        assertThat(validator.isNotNull(carNames)).isEqualTo(true);
-    }
-
-    @Test
-    public void carNameExcessTest() {
-        Validator validator = new Validator();
-        List<String> carNames = new LinkedList<>();
-        List<String> carNamesWrong = new LinkedList<>();
-
-        carNames.add("John");
-        carNames.add("Kim");
-        carNames.add("James");
-
-        carNamesWrong.add("Jonathan");
-        carNamesWrong.add("Kim");
-        carNamesWrong.add("James");
-
-        assertThat(validator.isNotExcess(carNames)).isEqualTo(true);
-        assertThat(validator.isNotExcess(carNamesWrong)).isEqualTo(false);
-    }
-
-    @Test
-    public void carNameBelowZeroTest() {
-        Validator validator = new Validator();
-        List<String> carNames = new LinkedList<>();
-        List<String> carNamesWrong = new LinkedList<>();
-
-        carNames.add("John");
-        carNames.add("Kim");
-        carNames.add("James");
-
-        carNamesWrong.add("");
-        carNamesWrong.add("Kim");
-        carNamesWrong.add("James");
-
-        assertThat(validator.isNotBelowZero(carNames)).isEqualTo(true);
-        assertThat(validator.isNotBelowZero(carNamesWrong)).isEqualTo(false);
-    }
-
-    @Test
-    public void tryTimeBelowZeroTest() {
-        Validator validator = new Validator();
-
-        assertThat(validator.isNotBelowZero(1)).isEqualTo(true);
-        assertThat(validator.isNotBelowZero(12321)).isEqualTo(true);
-        assertThat(validator.isNotBelowZero(0)).isEqualTo(false);
-        assertThat(validator.isNotBelowZero(-3)).isEqualTo(false);
-    }
-
-    @Test
     public void carRegisterSizeTest() {
         RacingCarManager racingCarManager = new RacingCarManager();
         List<String> carName = new LinkedList<>();
