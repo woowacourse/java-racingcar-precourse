9fe69a5417c0c8ca0b052ac6d8f28a235401b9a9
KimSeongGyu1 <KimSeongGyu1@github.com>
2019-12-05 17:23:15 +0900

    [입력] 구현: 중복된 이름 입력시 예외처리

--- src/main/java/domain/Input.java
+++ src/main/java/domain/Input.java
@@ -11,46 +11,55 @@ public class Input {
 	
 	private static Scanner SCANNER = new Scanner(System.in);
 	
+	/* public 함수 */
 	public static List<String> enterCarNames() {
-		String[] carNames;
+		List<String> carNames;
 		try {
 			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-			carNames = SCANNER.nextLine().trim().split(",");
-			checkIfValidCarNames(carNames);
-			return convertArrToList(carNames);
+			carNames = convertArrToList(SCANNER.nextLine().trim().split(","));
+			checkValidCarNames(carNames);
+			return carNames;
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 			return enterCarNames();
 		}
 	}
 	
-	private static void checkIfValidCarNames(String[] carNames) {
-		for (int i = 0; i < carNames.length; i++) {
-			checkIfValidCarName(carNames[i]);
+	private static List<String> convertArrToList(String[] inputStringArr) {
+		List<String> outputList = new ArrayList<String>();
+		for (int i = 0; i < inputStringArr.length; i++) {
+			checkOverlapping(outputList,inputStringArr[i]);
+			outputList.add(inputStringArr[i]);
+		}
+		return outputList;
+	}
+	
+	private static void checkOverlapping(List<String> carNames, String carName) {
+		if (carNames.contains(carName)) {
+			throw new IllegalArgumentException("중복되지 않은 이름들을 입력해주세요");
 		}
 	}
 	
-	private static void checkIfValidCarName(String carName) {
+	private static void checkValidCarNames(List<String> carNames) {
+		for (int i = 0; i < carNames.size(); i++) {
+			checkValidLenOfCarName(carNames.get(i));
+		}
+	}
+	
+	private static void checkValidLenOfCarName(String carName) {
 		if ((carName.length() > MAX_LEN_OF_NAME)
 				|| (carName.length() < MIN_LEN_OF_NAME)) {
 			throw new IllegalArgumentException("5자 이하의 비어있지 않은 이름만 입력해주세요");
 		}
 	}
-	
-	private static List<String> convertArrToList(String[] inputStringArr) {
-		List<String> outputList = new ArrayList<String>();
-		for (int i = 0; i < inputStringArr.length; i++) {
-			outputList.add(inputStringArr[i]);
-		}
-		return outputList;
-	}
 
+	/* public 함수 */
 	public static int enterRounds() {
 		int rounds;
 		try {
 			System.out.println("시도할 회수는 몇회인가요?");
 			rounds = enterInt();
-			checkIfValidRounds(rounds);
+			checkValidRounds(rounds);
 			return rounds;
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
@@ -67,7 +76,7 @@ public class Input {
 		}
 	}
 	
-	private static void checkIfValidRounds(int rounds) {
+	private static void checkValidRounds(int rounds) {
 		if(rounds < MIN_ROUNDS) {
 			throw new IllegalArgumentException("1 이상의 숫자를 입력해주세요");
 		}
