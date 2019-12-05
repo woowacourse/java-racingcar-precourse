23f77cba64d330f8419e4428791ba4d9be146b6c
junho <hwanghe159@naver.com>
2019-12-05 21:38:40 +0900

    입력받은 문자열로부터 Car이름들 분리

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,12 +1,25 @@
+/*
+ * RacingGame
+ * v1.0
+ * 2019.12.05
+ */
+
 package domain;
+
+import java.util.Scanner;
+import java.util.Random;
 
 public class RacingGame {
 	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String input;
+		String[] carNames;
 		
-		
-		
-		
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		input = scan.nextLine();
+		carNames = input.split(",");
 		
 		
 	}
+	
 }
