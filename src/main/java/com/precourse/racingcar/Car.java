ec096b238d7ce9712fa719981aff6dc9d570f9c2
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-12-05 11:38:58 +0900

    feat: 자동차가 전진할지 말지 정하는 기능 추가

--- src/main/java/com/precourse/racingcar/Car.java
+++ src/main/java/com/precourse/racingcar/Car.java
@@ -1,5 +1,5 @@
 /*
- * Car.java                        1.0.0   2019-12-04
+ * Car.java                        1.1.0   2019-12-05
  *
  * Copyright (c) 2019 Hyungju An.
  * All rights reserved.
@@ -11,18 +11,30 @@ package com.precourse.racingcar;
 /**
  * 자동차 클래스입니다.
  *
- * @version 1.0.0
- * @date 2019-12-04
+ * @version 1.1.0            차가 전진할지 말지 정하는 기능 추가
+ * @date 2019-12-05
  * @author HyungjuAn
  */
 public class Car {
 	private final String name;
 	private final int ZERO = 0;
+	private final int TEN = 10;
+	private final int MOVE_THRESHOLD = 4;
 
-	private int position;
+	private int position = ZERO;
 
 	public Car(String name) {
 		this.name = name;
-		position = ZERO;
+	}
+
+	private boolean isMove() {
+		boolean move = false;
+		int randomNumber = MOVE_THRESHOLD; // randomNumber를 구하는 기능 이 들어갈 위치
+
+		if (randomNumber >= MOVE_THRESHOLD) {
+			move = true;
+		}
+
+		return move;
 	}
 }
