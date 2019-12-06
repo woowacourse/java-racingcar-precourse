6a16382709d15a9d7d91c058ed5a9f7d1c90681d
chohanjoo <johanjoo@naver.com>
2019-12-07 04:09:48 +0900

    git Edit Main.java

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,4 +1,20 @@
 package domain;
 
 public class Main {
+    public static void main(String[] args) {
+        start_game();
+    }
+
+    public static void start_game(){
+        Manager manager = new Manager();
+
+        System.out.println("실행 결과");
+        for(int i=0;i<manager.getNumber();++i){
+            manager.cars_go_or_stop();
+            manager.print_cars_position();
+            System.out.println("");
+        }
+        manager.print_winner();
+
+    }
 }
