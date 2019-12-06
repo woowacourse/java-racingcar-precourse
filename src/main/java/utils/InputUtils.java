280045126a4ae1aa9efef0dd9919333d78a1c90e
MOON JINJU <mjj920930@gmail.com>
2019-12-06 14:24:54 +0900

    feat: 경주 게임의 라운드회수를 입력받는 기능 추가

--- src/main/java/utils/InputUtils.java
+++ src/main/java/utils/InputUtils.java
@@ -13,4 +13,13 @@ public class InputUtils {
             return inputCarNames();
         }
     }
+
+    public static int inputRacingRounds() {
+        try {
+            System.out.println("시도할 회수는 몇회인가요?");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return inputRacingRounds();
+        }
+    }
 }
