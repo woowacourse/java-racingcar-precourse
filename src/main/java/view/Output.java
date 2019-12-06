c737eea0b891ca69fd0033fbc00eddb36bdb042c
DB2 <10100aaaaa@gmail.com>
2019-12-06 11:21:50 +0900

    우승자(들) 출력하는 기능 추가

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Output {
     public static void showNamesInputForm() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -9,4 +12,7 @@ public class Output {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
+    public static void showWinners(List<String> winners) {
+        System.out.println(String.join(",", winners) + "가 최종 우승했습니다.");
+    }
 }
