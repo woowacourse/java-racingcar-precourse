dd2ce3de8ed936e38e87a5fa8d6e2c2429d32cdd
DB2 <10100aaaaa@gmail.com>
2019-12-06 11:17:24 +0900

    라운드 횟수 사용자입력 ui 보여주는 기능 추가

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -5,4 +5,8 @@ public class Output {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
+    public static void showRoundsInputForm() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
 }
