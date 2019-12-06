7cb68f790434a654e8d66bb65ff2a03d8b5e583b
HWooKim97 <rgd5454@naver.com>
2019-12-06 13:45:28 +0900

    Add player function
    Add function using scanner
    inputCarName, inputTime, scanner closer

--- src/main/java/com/woowa/racingcar/Player.java
+++ src/main/java/com/woowa/racingcar/Player.java
@@ -1,6 +1,21 @@
 package com.woowa.racingcar;
 
-public class Player {
+import java.util.Scanner;
 
-    
+public class Player {
+    private Scanner s = new Scanner(System.in);
+
+    Player() {}
+
+    public String inputCarName() {
+        return s.nextLine();
+    }
+
+    public int inputTime() {
+        return s.nextInt();
+    }
+
+    public void close() {
+        s.close();
+    }
 }
