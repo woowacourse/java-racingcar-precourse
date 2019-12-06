89454ae6bbb2d2d8452e02c3d36ad33f836f3284
junk-sound <wom2277@naver.com>
2019-12-06 20:18:46 +0900

    기능 2: 자동차가 전진할 지, 멈춰있을 지 정하는 기능. 즉, 난수를 발생시키고 그 수가 특정 값을 넘는 지 확인하는 기능.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Random;
+
 public class Car implements RacingCar {
     private final String name;
     private int position = 0;
@@ -12,6 +14,17 @@ public class Car implements RacingCar {
     public void race() {
 
     }
+    private boolean canGo() {
+        int randomNumber = generateRandomNumber();
+        return RacingGameConfig.STANDARD_TO_GO < randomNumber;
+    }
+    private int generateRandomNumber() {
+        Random randomGenerator = new Random();
+        //todo: check java convention
+        return randomGenerator.ints(RacingGameConfig.MIN_RANDOM_NUMBER, RacingGameConfig.MAX_RANDOM_NUMBER + 1)
+                .limit(1).findFirst().orElse(RacingGameConfig.MIN_RANDOM_NUMBER - 1);
+    }
+
     private void go() {
         position += RacingGameConfig.STEP;
     }
--- src/main/java/domain/RacingGameConfig.java
+++ src/main/java/domain/RacingGameConfig.java
@@ -2,4 +2,7 @@ package domain;
 
 class RacingGameConfig {
     static final int STEP = 1;
+    static final int MIN_RANDOM_NUMBER = 0;
+    static final int MAX_RANDOM_NUMBER = 9;
+    static final int STANDARD_TO_GO = 3;
 }
