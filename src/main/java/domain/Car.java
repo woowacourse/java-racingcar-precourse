3df685e6df9ddf02c2c9250e10356bf851cac07a
In-Yeong <in0_2015@naver.com>
2019-12-06 23:53:28 +0900

    make random number and update position method

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -7,14 +7,33 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
-
-    public void Move(int move_or_not) {
-    	if (move_or_not == 1) {
-    		position += 1;
-    	}
-    }
     
     public int Position() {
     	return position;
+    }
+    
+    public String Name() {
+    	return name;
+    }
+    
+    public void MoveOrNot() {
+    	int num = Random_Number();
+    	if (num >= 4) {
+    		position += 1;
+    	}
+    	System.out.println(num);
+    }
+    
+    public int Random_Number() {
+    	int random_number = (int) (Math.random() * 10);
+    	return random_number;
+    }
+    
+    public void Visual() {
+    	System.out.print(name+" : ");
+    	for (int i=0; i<position; i++) {
+    		System.out.print("-");
+    	}
+    	System.out.println();
     }
 }
