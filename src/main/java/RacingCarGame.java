34d55d426003558d5e4415afab5f3a38fbcaa5fa
MOON JINJU <mjj920930@gmail.com>
2019-12-06 17:46:18 +0900

    docs: README.md에 구현할 기능 목록 수정

--- src/main/java/RacingCarGame.java
+++ src/main/java/RacingCarGame.java
@@ -1,13 +1,12 @@
 import domain.Cars;
 import utils.InputUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class RacingCarGame {
     private Cars cars;
+    private int racingRounds;
 
     public void run() {
         cars = Cars.makeCars(InputUtils.inputCarNames());
+        racingRounds = InputUtils.inputRacingRounds();
     }
 }
