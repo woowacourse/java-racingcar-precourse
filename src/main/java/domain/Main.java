923e6d33d73e4adae83b63a1bf6a5af039ef5efe
Devhyein <jhn7121@naver.com>
2019-12-06 12:35:13 +0900

    5글자 이상 입력되면 다시 입력받기.

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -3,23 +3,35 @@ package domain;
  * Created by gram on 2019-12-05.
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static String[] carName;
     public static void main(String[] args) {
-        //car 이름 입력받아 생성하는 메소드 호출
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표 기준으로 구분)");
         createCar();
+        
     }
 
     private static void createCar() {
-        String[] carName;
         String totalInputName;
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표 기준으로 구분) : ");
         totalInputName = sc.next();
         carName = totalInputName.split(",");
-
+        if( !checkFormAboutName() ) {
+            System.out.println("5글자 내로 입력해주세요.");
+            createCar();
+        }
     }
 
+    private static boolean checkFormAboutName() {
+        for(int i = 0; i < carName.length; i++) {
+            if(carName[i].length() > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
