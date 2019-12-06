7c5005308fb8161ff9a44bd0d6a58b2cf11dd832
SMJin <sstaw@naver.com>
2019-12-06 13:50:45 +0900

    [CarComparator Class] 클래스 생성

---
+++ src/main/java/domain/CarComparator.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.Comparator;
+
+public class CarComparator implements Comparator<Car> {
+
+    @Override
+    public int compare(Car o1, Car o2) {
+        return 0;
+    }
+
+}
