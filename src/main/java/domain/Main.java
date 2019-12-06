cf138f63c5ad35a7285d591e074c263b2ef18aed
Devhyein <jhn7121@naver.com>
2019-12-06 13:09:47 +0900

    이동 횟수 입력받기.

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -8,21 +8,23 @@ import java.util.Scanner;
 
 public class Main {
     static String[] carName;
+    static int movingNum;
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        createCar();
-        
+        inputCarName();
+        inputMovingNum();
     }
 
-    private static void createCar() {
+    private static void inputCarName() {
         String totalInputName;
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표 기준으로 구분) : ");
         totalInputName = sc.next();
         carName = totalInputName.split(",");
         if( !checkFormAboutName() ) {
             System.out.println("5글자 내로 입력해주세요.");
-            createCar();
+            inputCarName();
         }
     }
 
@@ -34,4 +36,10 @@ public class Main {
         }
         return true;
     }
+
+    private static void inputMovingNum() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        movingNum = sc.nextInt();
+    }
+
 }
