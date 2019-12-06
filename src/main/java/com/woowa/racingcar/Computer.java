f0500a28caef4d8132b89c6a3c833d3ccfcee33a
HWooKim97 <rgd5454@naver.com>
2019-12-06 14:02:47 +0900

    Fix function splitCarName
    - change func name to "setCarList"
    - add private member variable carList
    - function set carList

--- src/main/java/com/woowa/racingcar/Computer.java
+++ src/main/java/com/woowa/racingcar/Computer.java
@@ -1,9 +1,16 @@
 package com.woowa.racingcar;
 
 public class Computer {
+    private Car[] carList;
+
     Computer() {}
 
-    public String[] splitCarName(String s) {
-        return s.split(",");
+    public void setCarList(String carName) {
+        String[] carNameList = carName.split(",");
+        carList = new Car[carNameList.length];
+        
+        for(int i = 0; i < carNameList.length; i++) {
+            carList[i] = new Car(carNameList[i]);
+        }
     }
 }
