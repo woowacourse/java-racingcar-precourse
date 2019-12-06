b28cc7bbc6f71160d25cbb74eb24f70b93494679
SMJin <sstaw@naver.com>
2019-12-06 20:14:36 +0900

    [CarRacint Class] getCarName 메소드 생성

--- src/main/java/domain/CarRacing.java
+++ src/main/java/domain/CarRacing.java
@@ -1,4 +1,19 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class CarRacing {
+
+    private static ArrayList<Car> getCarName(String carName) {
+        List<String> name = Arrays.asList(carName.split(","));
+        ArrayList<Car> carList = new ArrayList<Car>();
+        for (int i = 0; i < name.size(); i ++) {
+            carList.add(new Car(name.get(i)));
+        }
+        return carList;
+    }
+
 }
