faef36bfd196721d8a7ecf52775ea95250db2f87
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-06 14:16:36 +0900

    [feat] Car 클래스에 기능 추가
    - getPosition(): 현재까지 차량이 이동한 거리 반환
    - getName(): 차량의 이름 반환
    - getRandomNumber(): 1~9까지 랜덤 숫자 생성
    - move(): 랜덤 숫자가 4이상일 경우 한칸 전진

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,36 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	// 추가 기능 구현
+	int getPosition() {
+		return this.position;
+	}
+
+	String getName() {
+		return this.name;
+	}
+
+	private int getRandomNumber() {
+		int randomNumber;
+		Random random = new Random();
+		randomNumber = random.nextInt(10) + 1;
+		return randomNumber;
+	}
+
+	void move() {
+		int randomNumber = getRandomNumber();
+		if (randomNumber >= 4) {
+			this.position++;
+		}
+	}
+
 }
