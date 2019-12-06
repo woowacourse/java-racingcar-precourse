0086ad9e310f49b8d3fff28bfb3a95d673d21d50
KongsonPark <sonypark0204+wootech@gmail.com>
2019-12-06 14:22:57 +0900

    [feat] 경주할 자동차 입력 기능 구현
    - runRacingCar(): 자동차 경주 게임을 실행하는 함수
    - getCarNamesFromUser(): 사용자로부터 자동차의 이름을 입력받는 함수
    - isNull(): 입력값이 null인지 체크하는 함수
    - isValidLengthOfName(): 자동차 이름이 1~5인지 체크하는 함수

---
+++ src/main/java/domain/Stadium.java
@@ -0,0 +1,52 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Stadium {
+
+	private Scanner scanner = new Scanner(System.in);
+	private ArrayList<Car> carList;
+
+	public void runRacingCar() {
+		List<String> carNames = getCarNamesFromUser();
+	}
+
+	private List<String> getCarNamesFromUser() {
+		List<String> cars;
+
+		do {
+			System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
+			String input = scanner.nextLine();
+			cars = Arrays.asList(input.replaceAll("\\s", "").split(","));
+
+			System.out.println(cars);
+			System.out.println(cars.size());
+
+		} while (isNull(cars) || !isValidLengthOfName(cars));
+
+		return cars;
+	}
+
+	private boolean isNull(List<String> cars) {
+		if (cars.size() == 0) {
+			System.out.println("입력값 형식이 잘못되었습니다.");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isValidLengthOfName(List<String> cars) {
+		for (String car : cars) {
+			System.out.println("차량 이름:" + car);
+			if (car.length() > 5 || car.isEmpty()) {
+				System.out.println("차량 이름은 1이상 5이하로 입력 해주세요.");
+				return false;
+			}
+		}
+		return true;
+	}
+}
