52e411f0532478bd1d6bfc52aba459da87ffda5e
Sujin <sujinemilycho@gmail.com>
2019-12-06 22:23:13 +0900

    feat: 입력된 자동차 이름의 validation 기능 추가

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -16,11 +16,29 @@ public class RacingGame {
 		try {
 			scanner = new Scanner(System.in);
 			nameList = scanner.nextLine();
+			while (!validate(nameList)) {
+				nameList = scanner.nextLine();
+			}
 		} finally {
 			if (scanner != null) {
 				scanner.close();
 			}
 		}
 		return nameList;
+	}
+
+	public static boolean validate(String nameList) {
+		String[] names = nameList.split(",", -1);
+		int length = names.length;
+
+		for (int i = 0; i < length; i++) {
+			String name = names[i].trim();
+			if ((name.equals("")) || (name.length() > 5)) {
+				System.out.println("이름 입력형식이 잘못되었습니다. 다시 입력해주세요.");
+				System.out.println("예시) pobi, crong, honux");
+				return false;
+			}
+		}
+		return true;
 	}
 }
