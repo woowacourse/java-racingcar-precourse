6801798771deb97699e694e087daf93e402e4516
HWooKim97 <rgd5454@naver.com>
2019-12-06 16:45:15 +0900

    Add main
    - play the game

--- src/main/java/com/woowa/racingcar/RacingCar.java
+++ src/main/java/com/woowa/racingcar/RacingCar.java
@@ -2,6 +2,16 @@ package com.woowa.racingcar;
 
 public class RacingCar {
     public static void main(String[] args) {
-        
+        Player user = new Player();
+        Computer com = new Computer();
+
+        com.setCarList(user.inputCarName());
+        int playTime = user.inputTime();
+
+        System.out.println("\n실행결과");
+        for (int i = 0; i < playTime; i++) {
+            com.checkFoward();
+        }
+        com.showResult();
     }
 }
