61f8a27e436944d4add819eea8a884feecd8fc9d
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-06 16:44:04 +0900

    [Feature] 시도할 횟수 입력 기능 구현.

---
+++ src/main/java/domain/utils/inputUtils.java
@@ -0,0 +1,32 @@
+package domain.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class inputUtils {
+    private static final String PROMPTNAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PROMPTTRIES = "시도할 회수는 몇회인가요?";
+    private static final String ERRORMESSAGE = "유효하지 않은 입력입니다.";
+    private static final String SEPARATOR = ",";
+    static Scanner scanner = new Scanner(System.in);
+
+    public static Integer inputNumber() {
+        Integer inputValue;
+        while (true) {
+            System.out.println(PROMPTTRIES);
+            if (scanner.hasNextInt()) {
+                inputValue = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            }
+            System.out.println(ERRORMESSAGE);
+            scanner.nextLine();
+        }
+        return inputValue;
+    }
+
+    public static void inputClose() {
+        scanner.close();
+    }
+}
