49d06bacb2e9b8282d67d7e53478e2d716d1de4d
jaeju.jang <jjj0611@gmail.com>
2019-12-06 14:47:55 +0900

    [ADD] Car 리스트 중 1등 차를 뽑는 메서드 추가
    	- Car리스트 중에서 가장 앞선 position의 값 반환하는 메서드 추가
    	- position의 값이 주어지면 해당 position과 같은 위치의 자동차 리스트 반환하는 메서드 추가

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class Cars {
 
     private static final int RANDOM_NUMBER_BOUND = 10;
+    private static final int DEFAULT_POSITION = 0;
 
     private List<Car> cars;
 
@@ -19,9 +20,9 @@ public class Cars {
     }
 
     public void runOneTime(Random random) {
-       for (Car car : cars) {
-           car.proceedOrStop(generateRandomNumber(random));
-       }
+        for (Car car : cars) {
+            car.proceedOrStop(generateRandomNumber(random));
+        }
     }
 
     private int generateRandomNumber(Random random) {
@@ -29,9 +30,29 @@ public class Cars {
     }
 
     public void showCarsStatus() {
-        for(Car car: cars) {
+        for (Car car : cars) {
             System.out.println(car.currentStatus());
         }
+    }
+
+    public int measureFirstPosition() {
+        int first = DEFAULT_POSITION;
+        for (Car car : cars) {
+            if (car.getPosition() > first) {
+                first = car.getPosition();
+            }
+        }
+        return first;
+    }
+
+    public List<Car> takeCarsInFirstPosition(int position) {
+        List<Car> carsInFirstPosition = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.isSamePosition(position)) {
+                carsInFirstPosition.add(car);
+            }
+        }
+        return carsInFirstPosition;
     }
 
 }
