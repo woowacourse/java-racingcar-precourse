e1a473d5c08dace76146c3bc79b6e4a9083a6322
SMJin <sstaw@naver.com>
2019-12-06 19:58:20 +0900

    [CarComparator Class] compare 메소드 구현

--- src/main/java/domain/CarComparator.java
+++ src/main/java/domain/CarComparator.java
@@ -5,8 +5,11 @@ import java.util.Comparator;
 public class CarComparator implements Comparator<Car> {
 
     @Override
-    public int compare(Car o1, Car o2) {
-        return 0;
+    public int compare(Car car1, Car car2) {
+        if (car1.getPosition() < car2.getPosition()) {
+            return 1;
+        }
+        return -1;
     }
 
 }
