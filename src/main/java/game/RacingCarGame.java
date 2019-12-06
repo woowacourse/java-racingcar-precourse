340a699bf3a9af0d05775e68ab1567a47cfce872
jaeju.jang <jjj0611@gmail.com>
2019-12-06 16:09:05 +0900

    [EDIT] RacingCarGame메서드의 문자열을 변수화했습니다.

--- src/main/java/game/RacingCarGame.java
+++ src/main/java/game/RacingCarGame.java
@@ -3,6 +3,7 @@ package game;
 import com.sun.deploy.util.StringUtils;
 import domain.Car;
 import domain.Cars;
+import exception.InvalidInputException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +12,14 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class RacingCarGame {
+    private static final int CAR_NAME_LENGTH_LIMIT = 5;
+    private static final String NOT_NUMBER_EXCEPTION_MESSAGE = "시도 횟수는 숫자만 입력할 수 있습니다.";
+    private static final String NAME_OVER_LENGTH_EXCEPTION_MESSAGE = "자동차의 이름은 " + CAR_NAME_LENGTH_LIMIT + "을 넘을 수 없습니다.";
+    private static final String NAME_DUPLICATION_EXCEPTION_MESSAGE = "자동차의 이름은 중복될 수 없습니다.";
+    private static final String CAR_NAME_ASK_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIAL_TIMES_ASK_MESSAGE = "시도할 횟수는 몇회인가요?";
+    private static final String AWARDS_TAIL_MESSAGE = "가 우승했습니다.";
+    private static final String CAR_NAME_DELIMITER = ",";
 
     private Cars cars;
     private int numberOfTimes;
@@ -42,14 +51,14 @@ public class RacingCarGame {
     }
 
     public void holdAwardsCeremony() {
-        System.out.println(makeCarsInFirstPositionString() + "가 우승했습니다.");
+        System.out.println(makeCarsInFirstPositionString() + AWARDS_TAIL_MESSAGE);
     }
 
     private String makeCarsInFirstPositionString() {
         int firsPosition = cars.measureFirstPosition();
         List<Car> carsInFirstPosition = cars.takeCarsInFirstPosition(firsPosition);
         List<String> carNamesInFirstPosition = carsInFirstPosition.stream().map(Car::getName).collect(Collectors.toList());
-        return StringUtils.join(carNamesInFirstPosition, ", ");
+        return StringUtils.join(carNamesInFirstPosition, CAR_NAME_DELIMITER);
     }
 
     private void initNumberOfTimes() {
@@ -57,12 +66,12 @@ public class RacingCarGame {
     }
 
     private int makeNumberOfTimes() {
-        while(true) {
+        while (true) {
             try {
                 String numberOfTimesString = takeNumberOfTimes();
                 return Integer.parseInt(numberOfTimesString);
             } catch (NumberFormatException ne) {
-                System.out.println("시도 횟수는 숫자만 입력할 수 있습니다.");
+                System.out.println(NOT_NUMBER_EXCEPTION_MESSAGE);
             }
         }
     }
@@ -73,13 +82,13 @@ public class RacingCarGame {
     }
 
     private List<String> makeNamesList() {
-        while(true) {
+        while (true) {
             try {
                 String namesString = takeNames();
-                List<String> names = Arrays.asList(namesString.split(","));
+                List<String> names = Arrays.asList(namesString.split(CAR_NAME_DELIMITER));
                 validates(names);
                 return names;
-            } catch (IllegalArgumentException ie) {
+            } catch (InvalidInputException ie) {
                 System.out.println(ie.getMessage());
             }
         }
@@ -87,16 +96,16 @@ public class RacingCarGame {
 
     private void validates(List<String> names) {
         if (checkLengthOverBasis(names)) {
-            throw new IllegalArgumentException();
+            throw new InvalidInputException(NAME_OVER_LENGTH_EXCEPTION_MESSAGE);
         }
         if (checkDuplication(names)) {
-            throw new IllegalArgumentException();
+            throw new InvalidInputException(NAME_DUPLICATION_EXCEPTION_MESSAGE);
         }
     }
 
     private boolean checkLengthOverBasis(List<String> names) {
         for (String name : names) {
-            if (name.length() > 5) {
+            if (name.length() > CAR_NAME_LENGTH_LIMIT) {
                 return true;
             }
         }
@@ -108,12 +117,12 @@ public class RacingCarGame {
     }
 
     private String takeNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println(CAR_NAME_ASK_MESSAGE);
         return gameScanner.nextLine();
     }
 
     private String takeNumberOfTimes() {
-        System.out.println("시도할 횟수는 몇회인가요?");
+        System.out.println(TRIAL_TIMES_ASK_MESSAGE);
         return gameScanner.nextLine();
     }
 
