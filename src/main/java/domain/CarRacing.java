b336322e7c416f5580b3ef65a60851dd81c4667b
SMJin <sstaw@naver.com>
2019-12-06 20:43:31 +0900

    [CarRacint Class] canDrive 메소드 생성

--- src/main/java/domain/CarRacing.java
+++ src/main/java/domain/CarRacing.java
@@ -2,18 +2,26 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class CarRacing {
+
+    private static final int RANDOM_MAX_NUMBER = 10;                 // 차가 전진할 수 있을지 결정될 때, 랜덤으로 나올 수 있는 수들의 최대 크기.
+    private static final int RANDOM_PARTITION_NUMBER = 4;           // 4보다 큰 수가 나오면 차는 전진할 수 있다. 그래서 전진할 지 말지 나누는 기준이 되는 수.
 
     private static ArrayList<Car> getCarName(String carName) {
         List<String> name = Arrays.asList(carName.split(","));
         ArrayList<Car> carList = new ArrayList<Car>();
-        for (int i = 0; i < name.size(); i ++) {
+        for (int i = 0; i < name.size(); i++) {
             carList.add(new Car(name.get(i)));
         }
         return carList;
+    }
+
+    private static boolean carDrive() {
+        Random random = new Random();
+        return (random.nextInt(RANDOM_MAX_NUMBER) >= RANDOM_PARTITION_NUMBER);
     }
 
 }
