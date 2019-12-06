557fda1361f9306d8132ef266fb18db41747bbc9
HWooKim97 <rgd5454@naver.com>
2019-12-06 15:52:19 +0900

    Fix function input~
    - add syso

--- src/main/java/com/woowa/racingcar/Player.java
+++ src/main/java/com/woowa/racingcar/Player.java
@@ -8,10 +8,12 @@ public class Player {
     Player() {}
 
     public String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return s.nextLine();
     }
 
     public int inputTime() {
+        System.out.println("시도할 횟수는 몇회인가요?");
         return s.nextInt();
     }
 
