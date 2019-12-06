008614c5d7d2f04e5527c4d5e33eb3b280627a87
kimjunseok <jnunseok@gmail.com>
2019-12-06 17:00:03 +0900

    [update] 자동차 및 시도회수 함수 수정

--- src/main/java/woowacourse/racingcar/RacingCarGame.java
+++ src/main/java/woowacourse/racingcar/RacingCarGame.java
@@ -6,20 +6,17 @@ public class RacingCarGame {
     private Scanner sc = new Scanner(System.in);
 
     public void runGame() {
-        String carNames = getCarNames();
+        String [] carNames = getCarNames();
         int tryNumber = getTryNumber();
-
     }
 
-    private String getCarNames() {
+    private String [] getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String getNames = sc.nextLine();
-        return getNames;
+        return sc.nextLine().split(",");
     }
 
     private int getTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int getNumber = sc.nextInt();
-        return getNumber;
+        return sc.nextInt();
     }
 }
