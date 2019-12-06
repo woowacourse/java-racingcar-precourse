cfe19a959cd38c11aa2cf97ebffed15d12c0c0b3
koreaaerospaceU <zx811@naver.com>
2019-12-06 15:54:12 +0900

    create Car class(with make random value, move the car, print position)

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -7,6 +7,23 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
+    public int makeRandom(){
+        double random_Float = Math.random() * 9; // 난수 생성
+        int random_Int = (int)random_Float;
+        return random_Int;
+    }
+    public void moveToCar(){
+        int random = makeRandom();
+        if(random >= 4){
+            position ++;
+        }else{
+        }
+    }
+    public void progressOfCar(){
+        System.out.print(this.name + ':');
+        for(int progress = 0; progress < position; progress++){
+            System.out.print('-');
+        }
+    }
 
-    // 추가 기능 구현
 }
