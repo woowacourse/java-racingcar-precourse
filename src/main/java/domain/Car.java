797ead21a7900547518fe5e61f29f5e269451ff9
kouz <lastpuzly@gmail.com>
2019-12-07 22:39:28 +0900

    refactor: 멤버 변수로 isWinner 추가, get & set isWinner 메소드 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,6 +3,7 @@ package domain;
 public class Car {
 	private final String name;
 	private int position = 0;
+	private boolean isWinner = false;
 
 	public Car(String name) {
 		this.name = name;
@@ -14,6 +15,14 @@ public class Car {
 
 	public int getPosition() {
 		return position;
+	}
+
+	public boolean getIsWinner() {
+		return isWinner;
+	}
+
+	public void setIsWinner() {
+		isWinner = true;
 	}
 
 	public void tryToGo() {
