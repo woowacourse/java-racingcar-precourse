60cc21cd35de0f95344277b866a8ab30b2508352
jenny <jennylee4517@gmail.com>
2019-12-07 15:00:21 +0900

    Racing 클래스 생성

---
+++ src/main/java/com/jenny/racing/Racing.java
@@ -0,0 +1,63 @@
+package com.jenny.racing;
+
+public class Racing implements Racable{
+    @Override
+    public void printNameAskMsg() {
+
+    }
+
+    @Override
+    public boolean areValidNames(String names) {
+        return false;
+    }
+
+    @Override
+    public void getNames() {
+
+    }
+
+    @Override
+    public void createCars(String[] names) {
+
+    }
+
+    @Override
+    public void printTrialAskMsg() {
+
+    }
+
+    @Override
+    public boolean isValidTrial(String trial) {
+        return false;
+    }
+
+    @Override
+    public void getTrial() {
+
+    }
+
+    @Override
+    public boolean doesGoOrStop() {
+        return false;
+    }
+
+    @Override
+    public void printCarPosition(int num) {
+
+    }
+
+    @Override
+    public void printProcess() {
+
+    }
+
+    @Override
+    public void printWinner() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
