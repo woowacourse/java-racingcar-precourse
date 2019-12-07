55f6a031ae308e2dbd002d45daefacd85a12927f
chws <rebeccachoosir@gmail.com>
2019-12-07 18:35:37 +0900

    [FEATURE] 게임 시작하기(기능 1, 2 아웃라인)
    - GameManager 클래스 작성
    - startGame 메소드로 시작하게 작성

---
+++ src/main/java/domain/GameManager.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class GameManager {
+
+    public static void main(String[] args) {
+        GameManager game = new GameManager();
+        game.startGame();
+    }
+
+    void startGame(){
+        setCarName();
+        getNumOfRounds();
+    }
+
+    Car[] setCarName(){
+        getNumOfCars();
+        Car[] cars = new Car[10];
+        return cars;
+    }
+
+    void getNumOfCars(){
+
+    }
+
+    void getNumOfRounds(){
+
+    }
+
+
+}
