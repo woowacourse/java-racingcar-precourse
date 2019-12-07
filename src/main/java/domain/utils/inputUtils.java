569fa096c50f0e0fec2cd0f60b24870da2b5b2f6
Min Kyu, Song <ohh93mk@gmail.com>
2019-12-07 17:31:39 +0900

    [Feature] 자동차 이름 입력 기능 추가

--- src/main/java/domain/utils/inputUtils.java
+++ src/main/java/domain/utils/inputUtils.java
@@ -1,35 +1,53 @@
 package domain.utils;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class inputUtils {
-    private static final String PROMPTNAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String PROMPTTRIES = "시도할 회수는 몇회인가요?";
-    private static final String ERRORMESSAGE = "유효하지 않은 입력입니다.";
-    private static final String SEPARATOR = ",";
-    static Scanner scanner = new Scanner(System.in);
+	private static final String[] SEPARATOR = {",", "쉼표"};
+	private static final String PROMPT_NAMES = String.format(
+		"경주할 자동차 이름을 입력하세요.(이름은 %s(%s) 기준으로 구분)", SEPARATOR[1], SEPARATOR[0]);
+	private static final int NAMES_MAX_LEN = 5;
+	private static final String PROMPT_TRIES = "시도할 회수는 몇회인가요?";
+	private static final String ERROR_MESSAGE = "유효하지 않은 입력입니다.";
 
-    public static Integer inputTries() {
-        Integer inputValue;
-        while (true) {
-            System.out.println(PROMPTTRIES);
-            if (scanner.hasNextInt() && (inputValue = scanner.nextInt()) > 0) {
-                break;
-            }
-            System.out.println(ERRORMESSAGE);
-            inputFlush();
-        }
-        inputFlush();
-        return inputValue;
-    }
+	static Scanner scanner = new Scanner(System.in);
 
-    private static void inputFlush() {
-        scanner.nextLine();
-    }
+	public static Integer inputTries() {
+		Integer inputValue;
+		while (true) {
+			System.out.println(PROMPT_TRIES);
+			if (scanner.hasNextInt() && (inputValue = scanner.nextInt()) > 0) {
+				break;
+			}
+			System.out.println(ERROR_MESSAGE);
+			inputFlush();
+		}
+		inputFlush();
+		return inputValue;
+	}
 
-    public static void inputClose() {
-        scanner.close();
-    }
+	public static List<String> inputNames() {
+		String inputValue;
+		List<String> namesList;
+		while (true) {
+			System.out.println(PROMPT_NAMES);
+			inputValue = scanner.nextLine();
+			namesList = Arrays.asList(inputValue.split(SEPARATOR[0]));
+			if (isValidName(namesList)) {
+				break;
+			}
+			System.out.println(ERROR_MESSAGE);
+		}
+		return namesList;
+	}
+
+	private static void inputFlush() {
+		scanner.nextLine();
+	}
+
+	public static void inputClose() {
+		scanner.close();
+	}
 }
