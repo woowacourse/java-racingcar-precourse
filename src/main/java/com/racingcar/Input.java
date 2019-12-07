712ed08eaa55cef8798e596fe1329605ebabd76d
김효찬 <kom1230@hanyang.ac.kr>
2019-12-07 14:21:00 +0900

    [ADD] 입력을 담당하는 클래스를 추가

---
+++ src/main/java/com/racingcar/Input.java
@@ -0,0 +1,18 @@
+package com.racingcar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Input
+ *
+ * @author hyochan
+ * @version 0.0.1
+ * @since 2019-12-06
+ */
+
+public class Input {
+    Scanner scan = new Scanner(System.in);
+}
