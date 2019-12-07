4da271ccddaa349524fc82263d43431ea0a4fdea
lalize <printf@kakao.com>
2019-12-07 15:10:43 +0900

    fix: 사용자로부터 입력받은 자동차 이름 유효성 검사 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,17 +3,29 @@ package view;
 import java.util.List;
 import java.util.Scanner;
 
+import validation.CarNameValidator;
+import validation.CarNamesValidator;
+import validation.Validator;
+
 public class InputView {
 	private static final String CAR_NAME_DELIMITER = ",";
 	private static final Scanner SCANNER = new Scanner(System.in);
+	private static final Validator carNamesValidator = new CarNamesValidator(new CarNameValidator());
 
 	private static void printGetCarNamesMessage() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 	}
 
 	public static List<String> getCarNames() {
-		printGetCarNamesMessage();
-		return List.of(SCANNER.nextLine().split(CAR_NAME_DELIMITER));
+		try {
+			printGetCarNamesMessage();
+			List<String> names = List.of(SCANNER.nextLine().split(CAR_NAME_DELIMITER));
+			carNamesValidator.validate(names);
+			return names;
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return getCarNames();
+		}
 	}
 
 	private static void printGetTrialCountMessage() {
