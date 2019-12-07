c50bba9d0f5d7dcfae0a7b1f64334473a84b5ccc
jenny <jennylee4517@gmail.com>
2019-12-07 15:28:11 +0900

    Test 클래스 구현

---
+++ src/test/java/com/jenny/test/Test.java
@@ -0,0 +1,11 @@
+package com.jenny.test;
+
+import com.jenny.racing.Racable;
+import com.jenny.racing.Racing;
+
+public class Test {
+    public static void main(String[] args) {
+        Racable race = new Racing();
+        race.run();
+    }
+}
