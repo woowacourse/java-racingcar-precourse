0dc836d68ea09ea229b86c7cc64fb50dc7f1fc93
SMJin <sstaw@naver.com>
2019-12-07 18:17:44 +0900

    [CarRacing Class] nameLengthCheck 메소드 구현

--- src/main/java/domain/CarRacing.java
+++ src/main/java/domain/CarRacing.java
@@ -7,9 +7,12 @@ public class CarRacing {
     private static final int RANDOM_MAX_NUMBER = 10;        // 차가 전진할 수 있을지 결정될 때, 랜덤으로 나올 수 있는 수들의 최대 크기.
     private static final int RANDOM_PARTITION_NUMBER = 4;   // 4보다 큰 수가 나오면 차는 전진할 수 있다. 그래서 전진할 지 말지 나누는 기준이 되는 수.
     private static final int FIRST_WINNER_NUMBER = 0;       // 승자는 무조건 한 명이상이다. 그 때 첫 승자의 인덱스 값.
+    private static final int CAR_NAME_MAX_LENGTH = 5;       // 차 객체가 가질 수 있는 최대 이름 길이.
+
+    public static Scanner scanner;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = scanner.nextLine();
         ArrayList<Car> carList = getCarName(carNames);
@@ -28,9 +31,18 @@ public class CarRacing {
         List<String> carNames = Arrays.asList(carName.split(","));
         ArrayList<Car> carList = new ArrayList<Car>();
         for (int i = 0; i < carNames.size(); i++) {
-            carList.add(new Car(carNames.get(i)));
+            String currentName = nameLengthCheck(carNames.get(i));
+            carList.add(new Car(currentName));
         }
         return carList;
+    }
+
+    private static String nameLengthCheck(String name) {
+        while (name.length() > CAR_NAME_MAX_LENGTH) {
+            println("차 객체의 이름 '"+name+"'은 5글자가 넘습니다. 다시 입력해주세요.");
+            name = scanner.nextLine();
+        }
+        return name;
     }
 
     public static boolean canDrive() {
@@ -65,7 +77,11 @@ public class CarRacing {
     }
 
     private static String printLastWinners(ArrayList<Car> carList) {
-        Collections.sort(carList, new CarComparator());
+        CarComparator carComparator = new CarComparator();
+        Collections.sort(carList, carComparator);
+
+        for(int i=0; i<carList.size(); i++)
+            System.out.print(carList.get(i).getName()+",");
 
         String winnersNameList = carList.get(FIRST_WINNER_NUMBER).getName();
         for (int i = 1; i < howManyWin(carList); i ++) {
