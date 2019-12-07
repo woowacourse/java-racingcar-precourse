5618d8117d0e043890f1d4ca596949c01e148d1a
lalize <printf@kakao.com>
2019-12-07 16:57:12 +0900

    feat: 자동차 경주 시뮬레이션 로직 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,15 @@
+import domain.RacingCarGame;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
 	public static void main(String[] args) {
-
+		RacingCarGame game = new RacingCarGame(InputView.getCarNames(), InputView.getTrialCount());
+		OutputView.printRacingStart();
+		while (game.hasNextRace()) {
+			game.race();
+			OutputView.printRaceResult(game.getCars());
+		}
+		OutputView.printWinners(game.getWinners());
 	}
 }
