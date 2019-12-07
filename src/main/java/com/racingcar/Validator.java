2d8d48a3e3bfe80b61e80758a5435e75520bfdf8
김효찬 <kom1230@hanyang.ac.kr>
2019-12-07 14:24:09 +0900

    [ADD] 입력 유효성 검사에 필요한 메소드 생성

--- src/main/java/com/racingcar/Validator.java
+++ src/main/java/com/racingcar/Validator.java
@@ -1,5 +1,8 @@
 package com.racingcar;
 
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Validator
  *
@@ -9,4 +12,45 @@ package com.racingcar;
  */
 
 public class Validator {
+    public static final int CAR_NAME_MAX_SIZE = 5;
+
+    public boolean isValidName(List<String> input) {
+        if (this.isNull(input)) {
+            return false;
+        }
+        Iterator itr = input.iterator();
+        while (itr.hasNext()) {
+            String carName = (String) itr.next();
+            if (hasBlank(carName) || isOverSize(carName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isValidCount(int input) {
+        if (this.isNull(input)) {
+            return false;
+        }
+        if (!this.isPositiveNumber(input)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean hasBlank(String name) {
+        return name.contains(" ");
+    }
+
+    public boolean isNull(Object input) {
+        return input == null;
+    }
+
+    public boolean isOverSize(String name) {
+        return name.length() > CAR_NAME_MAX_SIZE;
+    }
+
+    public boolean isPositiveNumber(int num) {
+        return num > 0;
+    }
 }
