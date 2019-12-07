59f87136fb256cc7d629445363cac05c8a34eb5d
lalize <printf@kakao.com>
2019-12-07 14:37:28 +0900

    feat: 사용자로부터 자동차 이름을 입력받는 기능 추가
    이름은 쉼표(,)를 기준으로 분리한다

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+	private static final String CAR_NAME_DELIMITER = ",";
+	private static final Scanner SCANNER = new Scanner(System.in);
+
+	private static void printGetCarNamesMessage() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+	}
+
+	public static List<String> getCarNames() {
+		printGetCarNamesMessage();
+		return List.of(SCANNER.nextLine().split(CAR_NAME_DELIMITER));
+	}
+}
