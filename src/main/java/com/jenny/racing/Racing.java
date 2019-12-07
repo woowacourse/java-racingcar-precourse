b9d40055eaefae9c38db8c689345489b83eb5ad2
jenny <jennylee4517@gmail.com>
2019-12-07 15:02:44 +0900

    printNameAskMsg 메서드 구현

--- src/main/java/com/jenny/racing/Racing.java
+++ src/main/java/com/jenny/racing/Racing.java
@@ -1,9 +1,15 @@
 package com.jenny.racing;
 
+import domain.Car;
+
 public class Racing implements Racable{
+
+    private Car[] cars;
+    private int trial;
+
     @Override
     public void printNameAskMsg() {
-
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     @Override
