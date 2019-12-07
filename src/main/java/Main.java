4a68a60c2420e93c3c26209bcdad5dd5df7d2009
lxxjn0 <ljy3510@gmail.com>
2019-12-07 14:40:43 +0900

    feat : Main 클래스 구현
    게임 진행을 위한 Main 클래스, main 메소드 구현.

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+/*
+ * @(#)Main.java     0.1 2019.12.07
+ *
+ * Copyright (c) 2019 lxxjn0.
+ */
+import domain.RacingGame;
+
+/**
+ * 게임 시작을 위한 Main 클래스.
+ *
+ * @author JUNYOUNG LEE (lxxjn0)
+ * @version 0.1 2019.12.07
+ */
+public class Main {
+    /**
+     * RacingGame 클래스의 객체를 생성하여 게임을 시작하는 Main 메소드.
+     *
+     * @param args 입력되는 매개변수 존재하지 않음.
+     */
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.run();
+    }
+}
