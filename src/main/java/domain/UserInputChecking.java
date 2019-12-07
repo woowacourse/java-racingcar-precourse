72897de01c708a679f0cdc9e7874e1e7a842cd50
YerinCho <forjustice9839@gmail.com>
2019-12-07 19:28:48 +0900

    UserInputChecking 클래스 추가

---
+++ src/main/java/domain/UserInputChecking.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class UserInputChecking {
+    private String name;
+    private String count;
+    
+    public UserInputChecking(String n, String c){
+	this.name=n;
+	this.count=c;
+    }
+    
+    public void checkCarName() {
+    }
+    
+    public void checkCount() {}
+}
