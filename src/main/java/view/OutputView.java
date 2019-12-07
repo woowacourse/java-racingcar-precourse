2c27f205fcd5470a1d0220a74e599de32eb3ae9b
lalize <printf@kakao.com>
2019-12-07 16:56:30 +0900

    feat: 우승자 이름 목록을 출력하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,6 +5,8 @@ import java.util.List;
 import domain.Car;
 
 public class OutputView {
+	private static final String WINNER_DELIMITER = ", ";
+
 	public static void printRacingStart() {
 		System.out.println("\n실행 결과");
 	}
@@ -16,5 +18,10 @@ public class OutputView {
 	public static void printRaceResult(List<Car> cars) {
 		cars.forEach(OutputView::printCarState);
 		System.out.println();
+	}
+
+	public static void printWinners(List<String> winners) {
+		String result = String.join(WINNER_DELIMITER, winners);
+		System.out.println(result + "가 최종 우승했습니다.");
 	}
 }
