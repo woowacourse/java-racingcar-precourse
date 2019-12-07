c4f11143d2328b2dec410a48f7010c444d9e31f3
lalize <printf@kakao.com>
2019-12-07 15:37:15 +0900

    feat: 자동차를 관리할 CarManager 클래스 추가

---
+++ src/main/java/domain/CarManager.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarManager {
+	private final List<Car> cars;
+
+	public CarManager(List<String> names) {
+		cars = names.stream().map(Car::new).collect(Collectors.toList());
+	}
+
+	public void update() {
+		cars.forEach(Car::update);
+	}
+}
