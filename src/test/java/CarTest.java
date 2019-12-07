38a39831c4c7dd30400bd2cf6ea115c311b61983
SangBoLee <sangbo0229@gmail.com>
2019-12-07 17:03:35 +0900

    이름 입력받고 그 개수만큼 인스턴스 만들기

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		
+		String names;
+		String[] cars;
+		int nameCnt;
+		int carCnt = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,)기준으로 구분)");
+		names = scanner.nextLine();
+		
+		cars = names.split(",");
+		nameCnt = cars.length;
+		System.out.println(nameCnt);
+		Car[] carNames = new Car[nameCnt];
+		
+		for( String name : cars) {
+			if(name.length() <= 5) {
+				carNames[carCnt++] = new Car(name);
+				System.out.println(name);
+			}
+		}
+		
+	}
+
+}
