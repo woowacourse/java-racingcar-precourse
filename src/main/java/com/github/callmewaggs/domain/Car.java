3c3914950326e57d1ae84a66875dc22b9eee1844
callmewaggs <zxv74014@gmail.com>
2019-12-08 19:45:42 +0900

    1. convention 을 적용하여 패키징 재분리 2. Car 객체에 대한 기능 정의 3. 이를 반영하여 README.md 수정
    추후에 생성할 Game 객체에서 유저에게 입력을 받아 validation 검증을 진행하려고 생각했으나, 그랬을 경우 Game 객체가 다른 객체로 바뀌었을 때 제한사항이 적용되지 않을 수 있다고 생각하여 애초에 Car 객체에서 이 제약조건을 반영하도록 설계.

---
+++ src/main/java/com/github/callmewaggs/domain/Car.java
@@ -0,0 +1,13 @@
+package com.github.callmewaggs.domain;
+
+public class Car {
+
+  private final String name;
+  private int position = 0;
+
+  public Car(String name) {
+    this.name = name;
+  }
+
+  // 추가 기능 구현
+}
