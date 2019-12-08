e9461b9df817e61c7a6de92fe24031217d13c2a4
YerinCho <forjustice9839@gmail.com>
2019-12-09 07:11:31 +0900

    임의의 수가 4이상이면 position+1 하는 기능 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,17 @@
 package domain;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
+	
+	public void isGoStop(int n) {
+	    if(n>=4) {
+		this.position+=1;
+	    }
+	}
 
-    // 추가 기능 구현
 }
