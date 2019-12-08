d0e0366fac2bc0868afddd4874df6301672eee63
hyesun.choi <gptjs409@naver.com>
2019-12-09 00:04:20 +0900

    [기능구현] Count.java checkCount()

--- src/main/java/domain/Count.java
+++ src/main/java/domain/Count.java
@@ -10,4 +10,20 @@ public class Count {
         count = Integer.parseInt(countString);
         return count;
     }
+
+    static boolean checkCount(String count) {
+        boolean again = true;
+        for (int i = 0; i < count.length(); i++) {
+            if (count.charAt(i) < '0' || count.charAt(i) > '9') {
+                System.out.println("숫자만 입력해주세요.");
+                return again;
+            }
+        }
+        if (Integer.parseInt(count) == 0) {
+            System.out.println("1 이상의 수를 입력하세요.");
+            return again;
+        }
+        again = true;
+        return again;
+    }
 }
