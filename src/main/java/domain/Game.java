f5af3fa81201aed4d1ac14ea85d22e89ea1a4408
Devhyein <jhn7121@naver.com>
2019-12-08 17:21:41 +0900

    main과 game 분리

---
+++ src/main/java/domain/Game.java
@@ -0,0 +1,124 @@
+package domain;
+
+/**
+ * Created by gram on 2019-12-08.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Game {
+    static String[] carName;
+    static Car[] car;
+    static int movingNum;
+    static Scanner sc = new Scanner(System.in);
+
+    public void run() {
+        inputCarName();
+        car = new Car[carName.length];
+        inputMovingNum();
+        createCar();
+        playGame();
+    }
+
+    private void inputCarName() {
+        String totalInputName;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표 기준으로 구분) : ");
+        totalInputName = sc.next();
+        carName = totalInputName.split(",");
+        if( !checkFormAboutName() ) {
+            System.out.println("5글자 내로 입력해주세요.");
+            inputCarName();
+        }
+    }
+
+    private boolean checkFormAboutName() {
+        for(int i = 0; i < carName.length; i++) {
+            if(carName[i].length() > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void inputMovingNum() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        movingNum = sc.nextInt();
+        System.out.println();
+    }
+
+    private void createCar() {
+        for(int i = 0; i < carName.length; i++) {
+            car[i] = new Car(carName[i]);
+        }
+    }
+
+    private void playGame() {
+        int[] randomNum = new int[carName.length];
+        for(int i = 0; i < movingNum; i++) {
+            //차 별로 난수 생성
+            makeRandomNumber(randomNum);
+            //난수만큼 게임 진행
+            advanceAccordingToARandomNum(randomNum);
+            //중간 레이싱 과정 출력
+            if(i == 0) {
+                System.out.println("실행 결과");
+            }
+            showCurrScore();
+        }
+        findWinner();
+    }
+
+    private void makeRandomNumber(int[] randomNum) {
+        for(int i = 0; i < carName.length; i++) {
+            randomNum[i] =(int)(Math.random() * 10);
+        }
+    }
+
+    private void advanceAccordingToARandomNum(int[] randomNum) {
+        for(int i = 0; i < carName.length; i++) {
+            if(randomNum[i] >= 4){
+                car[i].addPosition();
+            }
+        }
+    }
+
+    private void showCurrScore() {
+        for(int i = 0; i < carName.length; i++) {
+            System.out.print(car[i].getName() + " : ");
+            for(int j = 0; j < car[i].currPosition(); j++){
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private void findWinner() {
+        int max = car[0].currPosition();
+        ArrayList winner = new ArrayList();
+
+        for(int i=0; i<carName.length; i++){
+            if(max < car[i].currPosition()) {
+                max = car[i].currPosition();
+                winner.clear();
+                winner.add(car[i].getName());
+            }else if(max == car[i].currPosition()) {
+                winner.add(car[i].getName());
+            }else if(max > car[i].currPosition()) {
+                continue;
+            }
+        }
+        printWinner(winner);
+    }
+
+    private void printWinner(ArrayList winner) {
+        for(int i = 0; i < winner.size() - 1; i++) {
+            System.out.print(winner.get(i)+", ");
+        }
+        System.out.print(winner.get(winner.size() - 1));
+        System.out.print("가 최종 우승했습니다.");
+    }
+}
