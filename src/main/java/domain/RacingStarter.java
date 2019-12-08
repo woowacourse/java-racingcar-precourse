cf20db7379a92a78dc8956162966cb622631476d
toneyparky <qkrtmddhks95@gmail.com>
2019-12-08 22:16:11 +0900

    Update: javadocs for RacingStater.java and RoundNumberGetter.java

--- src/main/java/domain/RacingStarter.java
+++ src/main/java/domain/RacingStarter.java
@@ -23,6 +23,11 @@ public class RacingStarter {
     private static int roundNumber = 1;
     private static String positionByDash;
 
+    /**
+     * Race를 시작하는 로직을 수행하는 메서드(생성자).
+     * 입력값으로 Car 인스턴스가 저장된 ArrayList와 라운드 횟수를 받는다.
+     * 리턴값으로 race후 차량의 position이 저장된 ArrayList를 돌려준다.
+     */
     public static ArrayList<Car> RacingStarter(ArrayList<Car> inputCarArrayList, int inputRoundNumber) {
         carArrayList = inputCarArrayList;
         roundNumber = inputRoundNumber;
@@ -30,6 +35,9 @@ public class RacingStarter {
         return carArrayList;
     }
 
+    /**
+     * Round별로 로직을 컨트롤하기 위한 메서드.
+     */
     public static void controlRacingTotalRound() {
         System.out.println("실행 결과");
         for (int i = 0; i < roundNumber; i++) {
@@ -38,6 +46,9 @@ public class RacingStarter {
         }
     }
 
+    /**
+     * 각 round에 해당하는 차량의 전진여부를 출력하는 메서드.
+     */
     public static void controlRacingByRound() {
         for (Car car : carArrayList) {
             executeRacing(car);
@@ -47,10 +58,16 @@ public class RacingStarter {
         }
     }
 
+    /**
+     * 차량의 전진여부를 결정하는 메서드.
+     */
     public static void executeRacing(Car car) {
         car.moveForward(car.decideGoOrNot(car.getRandomInt()));
     }
 
+    /**
+     * 차량의 position 멤버변수의 정수를 dash로 바꾸는 메서드. 예) 3 -> ---
+     */
     public static void exchangePositionToDash(Car car) {
         positionByDash = new String(new char[car.getPosition()]).replace("\0", "-");
     }
--- src/main/java/domain/RoundNumberGetter.java
+++ src/main/java/domain/RoundNumberGetter.java
@@ -23,6 +23,9 @@ public class RoundNumberGetter {
 
     private static int roundNumber = 1;
 
+    /**
+     * 사용자에게 round 횟수를 입력받는 로직을 수행하는 메서드.
+     */
     public static int controlRoundNumberGetting() {
         while (true) {
             if (askRoundNumber()) {
@@ -33,6 +36,10 @@ public class RoundNumberGetter {
         return roundNumber;
     }
 
+    /**
+     * 사용자로부터 정수를 입력받는 문장 출력과 에러 컨트롤하는 메서드.
+     * (Exception handling과 관련하여 메서드를 더 나눠보고 싶은데 어찌 해야할지 모르겠습니다. 추후 레퍼런스를 찾아보겠습니다.)
+     */
     public static boolean askRoundNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         try {
@@ -45,7 +52,7 @@ public class RoundNumberGetter {
     }
 
     /**
-     * 사용자로부터 받은 입력을 정수로 저장하는 메서드
+     * 사용자로부터 받은 입력을 정수로 저장하는 메서드. 오류시 exception을 throw.
      */
     public static void getInputNumber() {
         try {
