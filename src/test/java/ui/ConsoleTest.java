eca8c3a4505253439e5162335a8a5d7c95eeeb22
junk-sound <wom2277@naver.com>
2019-12-08 13:07:29 +0900

    비기능: Console 객체에 대한 단위 테스트 개선 - mocking 적용.

--- src/test/java/ui/ConsoleTest.java
+++ src/test/java/ui/ConsoleTest.java
@@ -2,7 +2,6 @@ package ui;
 
 import domain.RacingGameConfig;
 import domain.Validator;
-import domain.ValidatorImpl;
 import domain.errors.InvalidInputException;
 
 import org.apache.commons.lang3.StringUtils;
@@ -10,19 +9,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import util.RacingPrinter;
-import util.RacingPrinterImpl;
 
 import java.io.ByteArrayInputStream;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//todo: mocking
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class ConsoleTest {
 
     private Console console;
     private Scanner scanner;
-
+    private Validator mockValidator = mock(Validator.class);
+    private RacingPrinter mockRacingPrinter = mock(RacingPrinter.class);
 
     @AfterEach
     void clear() {
@@ -40,17 +43,15 @@ class ConsoleTest {
         String input = "pobi,crong,honux";
         console = createConsoleForTest(input);
         String[] result = {"pobi", "crong", "honux"};
-
+        when(mockValidator.validateNames(any())).thenReturn(true);
         //when & then
         assertArrayEquals(result, console.extractNames());
 
         //given
         input = " pobi,crong,honux ";
         console = createConsoleForTest(input);
-
         //when & then
         assertArrayEquals(result, console.extractNames());
-
     }
     @Test
     void extractNamesWithTooLongNames() {
@@ -58,32 +59,30 @@ class ConsoleTest {
         String tooLongName = StringUtils.repeat("a", RacingGameConfig.MAX_NAME_LENGTH + 1);
         String input = String.format("%s,crong,honux", tooLongName);
         console = createConsoleForTest(input);
-
+        when(mockValidator.validateNames(any())).thenReturn(false);
         //when& then
         assertThrows(InvalidInputException.class, () -> console.extractNames());
     }
     @Test
-    void extractRacingCarsWithNotAllowdCharacter() {
+    void extractNamesWithNotAllowdCharacter() {
         //given
         String input = "pobi,crong,hon*ux";
         console = createConsoleForTest(input);
-
+        when(mockValidator.validateNames(any())).thenReturn(false);
         //when & then
         assertThrows(InvalidInputException.class, () -> console.extractNames());
 
         //given
         input = "pobi, crong,honux";
         console = createConsoleForTest(input);
-
         //when & then
         assertThrows(InvalidInputException.class, () -> console.extractNames());
     }
     @Test
-    void extractRacingCarsWithSeparatorAtLast() {
+    void extractNamesWithSeparatorAtLast() {
         //given
         String input = ",pobi,crong,honux,";
         console = createConsoleForTest(input);
-
         //when & then
         assertThrows(InvalidInputException.class, () -> console.extractNames());
     }
@@ -91,31 +90,39 @@ class ConsoleTest {
 
     @Test
     void getCyclesWitchValidInput() {
+        //given
         String input = "1";
         console = createConsoleForTest(input);
-
+        when(mockValidator.validateCycles(anyInt())).thenReturn(true);
+        //when & then
         assertEquals(Integer.parseInt(input), console.getCycles());
     }
     @Test
     void getCyclesWitchInvalidInputs() {
+        //given
         String input = String.valueOf(RacingGameConfig.MIN_CYCLES - 1) ;
         console = createConsoleForTest(input);
+        when(mockValidator.validateCycles(anyInt())).thenReturn(false);
+        //when & then
         assertThrows(InvalidInputException.class, () -> console.getCycles());
 
+        //given
         input = "a";
         console = createConsoleForTest(input);
+        //when & then
         assertThrows(InputMismatchException.class, () -> console.getCycles());
 
+        //given
         input = "1.1";
         console = createConsoleForTest(input);
+        //when & then
         assertThrows(InputMismatchException.class, () -> console.getCycles());
     }
 
     private Console createConsoleForTest(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         scanner = new Scanner(System.in);
-        Validator validator = new ValidatorImpl();
-        RacingPrinter racingPrinter = new RacingPrinterImpl();
-        return new Console(scanner, validator, racingPrinter);
+
+        return new Console(scanner, mockValidator, mockRacingPrinter);
     }
 }
