51aec812c37202f82ad238c1d3754e895187884e
aegis1920 <aegis1920@gmail.com>
2019-12-08 20:46:22 +0900

    feat : add getCarNameArr function

--- src/main/java/com/woowacourse/two/CarRacingGame.java
+++ src/main/java/com/woowacourse/two/CarRacingGame.java
@@ -1,8 +1,23 @@
 package com.woowacourse.two;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CarRacingGame {
 
+    Scanner sc = new Scanner(System.in);
+
     public void run(){
+        String[] carNameArr = getCarNameArr(sc);
+        System.out.println(Arrays.toString(carNameArr));
+    }
+
+    public String[] getCarNameArr(Scanner sc){
+
+        String carNameStr = sc.nextLine();
+        String[] carNameArr = carNameStr.split(",");
+
+        return carNameArr;
 
     }
 }
