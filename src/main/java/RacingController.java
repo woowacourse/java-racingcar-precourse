9ae94b2960177a68f8686120903a404b6c88791f
chanin <pci2676@gmail.com>
2019-12-08 17:33:06 +0900

    feat - create RacingController
     - 게임 컨트롤러 작성

---
+++ src/main/java/RacingController.java
@@ -0,0 +1,9 @@
+import view.RacingGame;
+
+public class RacingController {
+
+    public static void main(String[] args) {
+        RacingGame racingGame = RacingGame.newGame();
+        racingGame.run();
+    }
+}
