88f81b86272eb9e8480553295dd8fa45667815f2
unknown <woncoh1@gmail.com>
2019-12-08 21:26:12 +0900

    [추가] Car 클래스 추가 및 그 안에 기본 get 메서드 추가

---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+}
