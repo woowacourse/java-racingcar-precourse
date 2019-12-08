7c89a61719322cd3720a2126cbcdeb77f458d038
hyesun.choi <gptjs409@naver.com>
2019-12-09 00:34:06 +0900

    [기능구현] CarSetting.java carSet()

---
+++ src/main/java/domain/CarSetting.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.List;
+
+public class CarSetting {
+    private Car[] cars;
+    private int count;
+
+    void carSet() {
+        String[] names = Naming.inputNames();
+        cars = new Car[names.length];
+        for(int i = 0; i < names.length; i++) {
+            cars[i] = new Car(names[i]);
+        }
+        count = Count.inputCount();
+    }
+
+}
