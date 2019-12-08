af441409f2674778388361705bdbe9195007ecb9
hyesun.choi <gptjs409@naver.com>
2019-12-09 01:24:50 +0900

    [코드컨벤션] 마무리

--- src/main/java/domain/CarSetting.java
+++ src/main/java/domain/CarSetting.java
@@ -1,7 +1,5 @@
 package domain;
 
-import java.util.List;
-
 public class CarSetting {
     private Car[] cars;
     private int count;
@@ -9,7 +7,7 @@ public class CarSetting {
     void carSet() {
         String[] names = Naming.inputNames();
         cars = new Car[names.length];
-        for(int i = 0; i < names.length; i++) {
+        for (int i = 0; i < names.length; i++) {
             cars[i] = new Car(names[i]);
         }
         count = Count.inputCount();
