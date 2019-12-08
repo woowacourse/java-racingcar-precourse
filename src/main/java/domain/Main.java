2fa71a6391188081d4f375cfdc089c7b24c81c42
SMJin <sstaw@naver.com>
2019-12-08 17:55:46 +0900

    [CarList Class] setGamePlayNumber 메소드 구현

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -6,14 +6,13 @@ public class Main {
 
     private static final int INIT_NUMBER = 0;       // 5번의 게임세트가 있다면, 그 세트의 첫 번째 세트를 가리킨다.
     private static OutputPrint outputPrint = new OutputPrint();
-    private static InputScanner inputScanner = new InputScanner();
     private static CarList carList;
 
     public static void main(String[] args) {
         outputPrint.insertName();
         carList = new CarList();
         outputPrint.howManyTry();
-        int gameSets = inputScanner.howManyPlay();
+        carList.setGamePlayNumber();
         outputPrint.executeResult();
         carList.play(INIT_NUMBER);
 
