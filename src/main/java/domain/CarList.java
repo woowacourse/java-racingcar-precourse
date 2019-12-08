aa6024a1f507440ab06271ff3a3a7c60ca7827c2
SMJin <sstaw@naver.com>
2019-12-08 17:29:26 +0900

    [CarList Class] canDrive 메소드 구현

--- src/main/java/domain/CarList.java
+++ src/main/java/domain/CarList.java
@@ -2,14 +2,22 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class CarList {
 
+    private static final int RANDOM_MAX_NUMBER = 10;        // 차가 전진할 수 있을지 결정될 때, 랜덤으로 나올 수 있는 수들의 최대 크기.
+    private static final int RANDOM_PARTITION_NUMBER = 4;   // 4보다 큰 수가 나오면 차는 전진할 수 있다. 그래서 전진할 지 말지 나누는 기준이 되는 수.
     private static final int FIRST_WINNER_NUMBER = 0;       // 승자는 무조건 한 명이상이다. 그 때 첫 승자의 인덱스 값.
     private static ArrayList<Car> carList;
 
     public CarList() {
         this.carList = new CarNameCheck().splitCarName();
+    }
+
+    public static boolean canDrive() {
+        Random random = new Random();
+        return (random.nextInt(RANDOM_MAX_NUMBER) >= RANDOM_PARTITION_NUMBER);
     }
 
     public static String lastWinners() {
--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -4,9 +4,6 @@ import java.util.*;
 
 public class Main {
 
-    private static final int RANDOM_MAX_NUMBER = 10;        // 차가 전진할 수 있을지 결정될 때, 랜덤으로 나올 수 있는 수들의 최대 크기.
-    private static final int RANDOM_PARTITION_NUMBER = 4;   // 4보다 큰 수가 나오면 차는 전진할 수 있다. 그래서 전진할 지 말지 나누는 기준이 되는 수.
-
     private static OutputPrint outputPrint = new OutputPrint();
     private static InputScanner inputScanner = new InputScanner();
     private static CarList carList;
@@ -34,11 +31,6 @@ public class Main {
             outputPrint.println("");
         }
         outputPrint.println("");
-    }
-
-    public static boolean canDrive() {
-        Random random = new Random();
-        return (random.nextInt(RANDOM_MAX_NUMBER) >= RANDOM_PARTITION_NUMBER);
     }
 
 }
