6a3488cf93b69afd41d2a3ee5f8f3c38ffccc235
callmewaggs <zxv74014@gmail.com>
2019-12-08 21:02:35 +0900

    1. 사용자에게 입력 받는 부분에 있어 테스트가 넘어가지 않고 콘솔 입력이 불가능 일단 넘어감

--- src/main/java/com/github/callmewaggs/racinggame/CarRacingGame.java
+++ src/main/java/com/github/callmewaggs/racinggame/CarRacingGame.java
@@ -1,7 +1,25 @@
 package com.github.callmewaggs.racinggame;
 
+import java.util.Scanner;
+
 public class CarRacingGame {
 
+  private String inputCarNames;
+  private String inputTrial;
+
   public void gameStart() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("경주할 자동차의 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    this.inputCarNames = scan.nextLine();
+    System.out.println("시도할 회수는 몇회인가요?");
+    this.inputTrial = scan.nextLine();
+  }
+
+  public String getInputCarNames() {
+    return inputCarNames;
+  }
+
+  public String getInputTrial() {
+    return inputTrial;
   }
 }
--- src/test/java/com/github/callmewaggs/racinggame/CarRacingGameTest.java
+++ src/test/java/com/github/callmewaggs/racinggame/CarRacingGameTest.java
@@ -1,13 +1,14 @@
 package com.github.callmewaggs.racinggame;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Matchers.any;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 class CarRacingGameTest {
 
+  // TODO : 테스트가 되지 않음.. 일단 넘어감
   @DisplayName("게임을 시작하면 사용자에게 자동차 이름과 시행에 대한 입력을 받는다.")
   @Test
   public void get_user_input_when_game_start() {
