28b19794a2f5cf40ced58bbc9b9f1f3a3e97d1db
aegis1920 <aegis1920@gmail.com>
2019-12-08 20:07:43 +0900

    feat : add CarRacingGame class, add Game class

---
+++ src/main/java/com/woowacourse/two/CarRacingGame.java
@@ -0,0 +1,8 @@
+package com.woowacourse.two;
+
+public class CarRacingGame {
+
+    public void run(){
+
+    }
+}
---
+++ src/main/java/com/woowacourse/two/Game.java
@@ -0,0 +1,10 @@
+package com.woowacourse.two;
+
+public class Game {
+
+    public static void main(String[] args){
+        CarRacingGame carRacingGame = new CarRacingGame();
+        carRacingGame.run();
+    }
+
+}
