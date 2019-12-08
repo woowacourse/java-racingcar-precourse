3885334aa53e1957fd87a59df6e8c1df8a36aab8
hyesun.choi <gptjs409@naver.com>
2019-12-08 23:23:56 +0900

    [기능구현] Car.java goingForward()

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,11 +1,21 @@
 package domain;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    void goingForward() {
+        Random random = new Random();
+        int criterionValue = random.nextInt(9);
+        if (criterionValue >= 4) {
+            position++;
+        }
     }
 
     int getPosition() {
