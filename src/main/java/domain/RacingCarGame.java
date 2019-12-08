dc69af555de96a6aa10b41ea9df7560db466d228
HeavyRyan92 <heavyryan92@gmail.com>
2019-12-08 17:21:33 +0900

    (기능구현) 자동차 이름을 입력받는 문구를 출력하는 기능 구현

--- src/main/java/domain/RacingCarGame.java
+++ src/main/java/domain/RacingCarGame.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class RacingCarGame {
+    public static void main(String[] args) {
+        startRacingCarGame();
+    }
+
+    private static void startRacingCarGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
 }
