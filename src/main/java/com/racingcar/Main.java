1ee74101f0466cac8d12c855cd3528f901906b8d
김효찬 <kom1230@hanyang.ac.kr>
2019-12-08 15:04:25 +0900

    [Add] Main Class 작성

---
+++ src/main/java/com/racingcar/Main.java
@@ -0,0 +1,16 @@
+package com.racingcar;
+
+/**
+ * Main
+ *
+ * @author hyochan
+ * @version 0.0.1
+ * @since 2019-12-06
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.racingCarGame();
+    }
+}
