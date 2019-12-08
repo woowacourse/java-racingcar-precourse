590d11784d416770f8f5b4502339b690c3053554
ParkDooWon <pass345@naver.com>
2019-12-08 15:52:56 +0900

    update : 초기 문서화 주석 작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,12 @@
+/*
+ * 클래스명 : Main
+ * 
+ * version : 0.1
+ * 
+ * 날짜 : 2019.12.08
+ * 
+ * 작성자 : ParkDooWon
+ */
 import domain.RacingCarGame;
 
 public class Main {
--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,3 +1,12 @@
+/*
+ * 클래스명 : Car
+ * 
+ * version : 0.1
+ * 
+ * 날짜 : 2019.12.08
+ * 
+ * 작성자 : ParkDooWon
+ */
 package domain;
 
 import java.util.Random;
@@ -9,37 +18,36 @@ public class Car {
 	private static final int CANT_MOVE = 3;
 	private static final int STAY = 0;
 	private static final String IS = " : ";
-    private final String name;
-    private int position = 0;
-    private int randNum;
- 
+	private final String name;
+	private int position = 0;
+	private int randNum;
 
-    public Car(String name) {
-        this.name = name;
-    }
-    
-    public void makeRandomNum() {
-    	this.randNum = random.nextInt(MAX_RANDOM_NUM);
-    }
-    
-    public void move() {
-    	 if (randNum >= CAN_MOVE) {
-    		 this.position++;
-    	 } else if (randNum <= CANT_MOVE) {
-    		 this.position += STAY;
-    	 }
-    }
-    
-    public void printCarPosition(char shape) {
-    	System.out.println(this.name + IS + printShapes(shape));
-    }
-    
-    private String printShapes(char shape) {
-    	String shapes = "";
-    	for (int i = 0; i < this.position; i++) {
-    		shapes += shape;
-    	}
-    	return shapes;
-    }
-    
+	public Car(String name) {
+		this.name = name;
+	}
+
+	public void makeRandomNum() {
+		this.randNum = random.nextInt(MAX_RANDOM_NUM);
+	}
+
+	public void move() {
+		if (randNum >= CAN_MOVE) {
+			this.position++;
+		} else if (randNum <= CANT_MOVE) {
+			this.position += STAY;
+		}
+	}
+
+	public void printCarPosition(char shape) {
+		System.out.println(this.name + IS + printShapes(shape));
+	}
+
+	private String printShapes(char shape) {
+		String shapes = "";
+		for (int i = 0; i < this.position; i++) {
+			shapes += shape;
+		}
+		return shapes;
+	}
+
 }
