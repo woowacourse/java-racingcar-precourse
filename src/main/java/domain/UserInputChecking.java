71c8fc23dac16a3e59b0d24223c2871430594107
YerinCho <forjustice9839@gmail.com>
2019-12-08 20:04:47 +0900

    사용자의 입력이 올바른지 확인하는 기능 추가

--- src/main/java/domain/UserInputChecking.java
+++ src/main/java/domain/UserInputChecking.java
@@ -1,16 +1,36 @@
 package domain;
 
 public class UserInputChecking {
-    private String name;
-    private String count;
     
-    public UserInputChecking(String n, String c){
-	this.name=n;
-	this.count=c;
+    public boolean checkCarName(String n) {
+	String[] tmpname = n.split(",");
+	for(int i=0;i<tmpname.length;i++) {
+	    if(tmpname[i].length()==0 || tmpname[i].length()>5) {
+		System.out.println("자동차 이름이 잘못되었습니다.");
+		return false;
+	    }
+	}
+	return true;
     }
     
-    public void checkCarName() {
+    public boolean checkCount(String n) {
+	if(isStringInt(n)==false) {
+	    System.out.println("횟수가 잘못 입력되었습니다. 정수만 입력해주세요.");
+	    return false;
+	}
+	if( Integer.parseInt(n)<=0 ) {
+	    System.out.println("횟수가 잘못 입력되었습니다. 양의 정수만 입력해주세요.");
+	    return false;
+	}
+	return true;
     }
     
-    public void checkCount() {}
+    public boolean isStringInt(String n) {
+	 try {
+	        Integer.parseInt(n);
+	        return true;
+	    } catch (NumberFormatException e) {
+	        return false;
+	    }
+    }
 }
