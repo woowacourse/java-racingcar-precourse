dda0cfd72ab33bd77610cf6d9610cb278747a8fd
junk-sound <wom2277@naver.com>
2019-12-08 12:53:53 +0900

    비기능: RacingFactory 객체에 대한 단위 테스트.

--- src/main/java/domain/RacingCarFactory.java
+++ src/main/java/domain/RacingCarFactory.java
@@ -16,21 +16,15 @@ public class RacingCarFactory implements RaceableFactory {
 
     public List<Raceable> create(String[] names) {
         if (!validator.validateNames(names)) {
-            throw new InvalidParameterException("자동차 이름이 옳바르지 않습니다. ");
+            throw new InvalidParameterException("자동차 이름이 옳바르지 않습니다.");
         }
 
         List<Raceable> raceables = new ArrayList<>();
         for (String name : names) {
-            if (!isValidName(name)) {
-                throw new InvalidInputException(String.format("입력값이 옳바르지 않습니다. 이름이 %d 글자를 초과할 수 없습니다.",
-                        RacingGameConfig.MAX_NAME_LENGTH));
-            }
             raceables.add(new RacingCar(name));
         }
 
         return raceables;
     }
-    private boolean isValidName(String name) {
-        return name.length() <= RacingGameConfig.MAX_NAME_LENGTH;
-    }
+
 }
---
+++ src/test/java/domain/RacingCarFactoryTest.java
@@ -0,0 +1,59 @@
+package domain;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class RacingCarFactoryTest {
+
+    private Validator mockValidator = mock(Validator.class);
+    private RacingCarFactory racingCarFactory;
+
+    @BeforeEach
+    void init() {
+        racingCarFactory = new RacingCarFactory(mockValidator);
+    }
+
+    @Test
+    void createWithValidNames() {
+        //given
+        String[] names = {"pobi", "crong", "honux"};
+        List<Raceable> result = Arrays.asList(
+                new RacingCar("pobi"), new RacingCar("crong"), new RacingCar("honux")
+        );
+        when(mockValidator.validateNames(names)).thenReturn(true);
+
+        //when & then
+        assertEquals(result, racingCarFactory.create(names));
+    }
+    @Test
+    void createWithInvalidNames() {
+        //given
+        String[] namesWithSpace = {"pobi ", "crong", "honux"};
+        when(mockValidator.validateNames(namesWithSpace)).thenReturn(false);
+        //when & then
+        assertThrows(InvalidParameterException.class, () -> racingCarFactory.create(namesWithSpace));
+
+        //given
+        String nameWithSpecialSymbol = "po*bi";
+        String[] namesWithSpecialSymbol = {nameWithSpecialSymbol, "crong", "honux"};
+        when(mockValidator.validateNames(namesWithSpecialSymbol)).thenReturn(false);
+        //when & then
+        assertThrows(InvalidParameterException.class, () -> racingCarFactory.create(namesWithSpecialSymbol));
+
+        //given
+        String tooLongName = StringUtils.repeat("a", RacingGameConfig.MAX_NAME_LENGTH + 1);
+        String[] namesWithTooLongName = {tooLongName, "crong", "honux"};
+        when(mockValidator.validateNames(namesWithTooLongName)).thenReturn(false);
+        //when & then
+        assertThrows(InvalidParameterException.class, () -> racingCarFactory.create(namesWithTooLongName));
+    }
+}
