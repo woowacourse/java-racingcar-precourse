a18d301dcf96a58525983b806402aecbec10cbd4
hamliet <hamliet@hanyang.ac.kr>
2019-12-08 19:45:12 +0900

    [추가] 경주할 자동차의 초깃값을 설정한다.

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,7 +1,37 @@
 package controller;
 
+import java.util.Scanner;
+import domain.Car;
+import view.PrintMessage;
+
 public class Main {
-	public static void main (String args[]) {
-		
+	private static final Scanner scanner = new Scanner(System.in);
+	private static final PrintMessage printMessage = new PrintMessage();
+	private static Car car[];
+
+	public static void main(String args[]) {
+		init();
+	}
+
+	private static void init() {
+		String inputString;
+		do {
+			printMessage.inputName();
+			inputString = scanner.next();
+		} while (validateInput(inputString) != true);
+		car = new Car[inputString.split(",").length];
+		for (int i = 0; i < car.length; i++) {
+			car[i] = new Car(inputString.split(",")[i]);
+		}
+	}
+
+	private static boolean validateInput(String inputString) {
+		String inputArray[] = inputString.split(",");
+		for (int i = 0; i < inputArray.length; i++) {
+			if (inputArray[i].length() > 5) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
---
+++ src/main/java/view/PrintMessage.java
@@ -0,0 +1,11 @@
+package view;
+
+public class PrintMessage {
+	public void inputName() {
+		System.out.println("경주할 자동차 이름을 입력하세	요.(이름은 쉼표(,) 기준으로 구분)");
+	}
+
+	public void inputRound() {
+		System.out.println("시도할 회수는 몇회인가요?");
+	}
+}
