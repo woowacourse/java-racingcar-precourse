e3d2f5082471891ef0cf309417fbabbb3e8c4c6a
Devhyein <jhn7121@naver.com>
2019-12-08 17:59:44 +0900

    필요없는 import 삭제

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,11 +1,4 @@
 package domain;
-/**
- * Created by gram on 2019-12-05.
- */
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
