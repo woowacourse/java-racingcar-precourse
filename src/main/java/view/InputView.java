ac97097534fc8fcbf1e7298ca22d0aefde93cd00
JunSeong <ebseud6135@naver.com>
2019-12-09 19:09:11 +0900

    입력 받는 기능들 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -17,13 +17,17 @@ public class InputView {
     private static final String CARS_NAME = "경주할 자동차 이름을 입력해주세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REPETITION_NUMBER = "시도할 회수는 몇회인가요?";
 
-    public String inputCarsName() {
+    public String inputCarNames() {
         System.out.println(CARS_NAME);
         return scanner.nextLine();
     }
 
-    public String inputRepetitionNumber() {
-        System.out.println(REPETITION_NUMBER);
-        return scanner.nextLine();
+    public int inputRepetitionNumber() {
+        try {
+            System.out.println(REPETITION_NUMBER);
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
     }
 }
