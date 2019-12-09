66777ea978147aef6e4b0bd3f9e31b81a9f30685
sezikim <kdghj1002@gmail.com>
2019-12-09 23:47:35 +0900

    fix String constants

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -5,16 +5,20 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class RacingGame {
+	public static final String COMMA = ",";
+	private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_MOVE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
 	public static void main(String[] args) {
 		User user = new User();
 		runRacingGame(user);
 	}
 
 	private static void runRacingGame(User user) {
-		printToInputCarNamesMessage();
+		System.out.println(INPUT_CAR_NAMES_MESSAGE);
 		Entry entry = makeRacingEntry(getCarNameList(user.getCarNames()));
 
-		printToInputMoveCountMessage();
+		System.out.println(INPUT_MOVE_COUNT_MESSAGE);
 		int carMoveCount = user.getCarMoveCount();
 
 		for (int i = 0; i < carMoveCount; ++i) {
@@ -37,20 +41,12 @@ public class RacingGame {
 
 	private static List<String> getCarNameList(String carNames) {
 		List<String> carNameList = new ArrayList<>();
-		StringTokenizer tokenizer = new StringTokenizer(carNames, ",");
+		StringTokenizer tokenizer = new StringTokenizer(carNames, COMMA);
 
 		while (tokenizer.hasMoreTokens()) {
 			carNameList.add(tokenizer.nextToken());
 		}
 
 		return carNameList;
-	}
-
-	private static void printToInputCarNamesMessage() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-	}
-
-	private static void printToInputMoveCountMessage() {
-		System.out.println("시도할 회수는 몇회인가요?");
 	}
 }
