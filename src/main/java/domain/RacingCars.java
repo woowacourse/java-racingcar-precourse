42b5f5a179330bd723579a5b85fa7cf590fcfcba
Tak Hyungmin <milkho11@naver.com>
2019-12-09 23:01:44 +0900

    기능 : RacingCars를 순회하며 Car의 이동
    move 함수는 racingCar 배열을 순회하며 Car의 move함수는 실행한다.

---
+++ src/main/java/domain/RacingCars.java
@@ -0,0 +1,20 @@
+package domain;
+
+import generater.RandomGenerater;
+
+import java.util.List;
+
+public class RacingCars {
+    private final List<Car> racingCars;
+    private RandomGenerater randomGenerater = new RandomGenerater();
+
+    public RacingCars(List<Car> racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    public void move() {
+        this.racingCars.stream().forEach(car -> {
+            car.move(randomGenerater.generateRandom());
+        });
+    }
+}
