c461dd1e7077ad09cb79b2df9107a26a68bc6f81
pandahun <gnsgnsek@gmail.com>
2019-12-09 10:45:08 +0900

    feature: Modify Car
    Name 객체의 name 설정
    전진 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,42 @@
+/*
+ *  @Car.java
+ *
+ *  @Version: 0.1
+ *
+ *  @Date: 2019.12.06
+ *
+ *  @Author: pandahun
+ */
+
 package domain;
 
+import domain.Name;
+import domain.RandomValue;
+
 public class Car {
-    private final String name;
+
+    private static final int BOUNDARY = 4;
+
+    private final Name name;
     private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
+    public Car( String name ) {
+        this.name = new Name(name);
     }
 
-    // 추가 기능 구현
+    public void forward() {
+        int randomValue = new RandomValue().getRandomValue();
+        if(isRunnable(randomValue))
+            this.position++;
+    }
+
+    private boolean isRunnable(int randomValue) {
+        return randomValue >= BOUNDARY;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() { return position};
 }
