73caff54b480aefbdef215c1729e4ec103095319
ikswary <ikswary@naver.com>
2019-12-09 14:21:11 +0900

    게임 수행 회수 제어 메소드에 대한 README 업데이트

--- src/main/java/UtilityTest.java
+++ src/main/java/UtilityTest.java
@@ -6,16 +6,15 @@ public class UtilityTest {
 		//슬라이싱 동작 테스트
 		Scanner sc = new Scanner(System.in);
 
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
 		String testString = sc.nextLine();
 		Car[] testCars = UtilityMethods.sliceString(testString);
 		for (int i = 0; i < testCars.length; i++) {
-			//testCars[i].goForward();
 			System.out.println(testCars[i].getName() + " : " + testCars[i].getPosition());
 		}
 
 		UtilityMethods.goForwardRandomly(testCars);
 		for (int i = 0; i < testCars.length; i++) {
-			//testCars[i].goForward();
 			System.out.println(testCars[i].getName() + " : " + testCars[i].getPosition());
 		}
 	}
