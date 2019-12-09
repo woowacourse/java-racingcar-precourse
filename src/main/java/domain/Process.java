3cd9b5983e9d028e06e4baa9497cd1025cdd00db
tiroring09 <tiroring09@gmail.com>
2019-12-09 19:56:32 +0900

    차량 인스턴스를 입력받아 전진유무를 판단하는 메서드 생성.

--- src/main/java/domain/Process.java
+++ src/main/java/domain/Process.java
@@ -2,6 +2,13 @@ package domain;
 
 public class Process {
 	
+	public static void isCarProceed(Car currentCar) {
+		int condition = (int) Math.floor(Math.random() * 10);
+		if (condition > 3) {
+			currentCar.addPosition();
+		}
+	}
+	
 	public static void main(String[] args) {
 	
 	}
