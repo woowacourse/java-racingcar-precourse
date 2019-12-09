2961a8e2b30fe35c3893af1a32c57cfb38ef878b
Tak Hyungmin <milkho11@naver.com>
2019-12-09 21:51:42 +0900

    버그 픽스: RandomGenerator 랜덤값 범위 수정
    RandomGenerator의 랜덤값 범위를 설정하는 UPPER_BOUND 설정

--- src/main/java/generater/RandomGenerater.java
+++ src/main/java/generater/RandomGenerater.java
@@ -6,6 +6,6 @@ public class RandomGenerater {
     private static final int UPPER_BOUND = 10;
 
     public int generateRandom() {
-        return new Random().nextInt();
+        return new Random().nextInt(UPPER_BOUND);
     }
 }
