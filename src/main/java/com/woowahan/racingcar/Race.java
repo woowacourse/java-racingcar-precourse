7349a771764011e784fddf848890fb1845a80f05
Jinhong Kim <deepred.kim@gmail.com>
2019-12-09 09:40:04 +0900

    Create Race.java

---
+++ src/main/java/com/woowahan/racingcar/Race.java
@@ -0,0 +1,46 @@
+package com.woowahan.racingcar;
+
+import java.util.ArrayList;
+
+public class Race extends AbstractRace {
+
+	@Override
+	protected void getRandomInteger(int min, int max) {
+
+	}
+
+	@Override
+	protected void processRound() {
+
+	}
+
+	@Override
+	protected void tryDrive(Car car) {
+
+	}
+
+	@Override
+	protected void printStatus() {
+
+	}
+
+	@Override
+	protected boolean isFinalRound() {
+		return false;
+	}
+
+	@Override
+	protected ArrayList<Car> getWinners() {
+		return null;
+	}
+
+	@Override
+	protected void printResult() {
+
+	}
+
+	@Override
+	public void start() {
+
+	}
+}
