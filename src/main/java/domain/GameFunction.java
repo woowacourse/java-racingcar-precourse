78bf990ec73ccf7ede293d144b2237ea5f4db78a
JH <surfing2003@naver.com>
2019-12-09 16:57:23 +0900

    최종 우승자 출력 부분 완성

--- src/main/java/domain/GameFunction.java
+++ src/main/java/domain/GameFunction.java
@@ -1,12 +1,20 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class GameFunction {
     public static int randomNumber;
+    public static int maxCarPosition;
+    public static ArrayList winnerList = new ArrayList();
+
     public static void startRacingGame(Car[] cars, int raceCount) {
         for (int i = 0; i < raceCount; i++) {
             moveCars(cars);
             printRacingResult(cars);
         }
+        findMaxCarPosition(cars);
+        makeWinnerList(cars);
+        printWinner(cars);
     }
 
     public static void moveCars(Car[] cars) {
@@ -18,15 +26,36 @@ public class GameFunction {
     }
 
     public static boolean checkRandomNumber() {
-        randomNumber = (int) ((Math.random() * 9) + 1);
+        randomNumber = (int) (Math.random() * 10);
+        System.out.println(randomNumber);
         return randomNumber >= 4;
     }
 
     public static void printRacingResult(Car[] cars) {
         System.out.println("실행 결과");
         for (Car car : cars) {
-            System.out.printf("%s : %s \n",car.getCarName(),car.printCarPosition());
+            System.out.printf("%s : %s \n", car.getCarName(), car.printCarPosition());
         }
     }
 
+    public static void findMaxCarPosition(Car[] cars) {
+        maxCarPosition = 0;
+        for (Car car : cars) {
+            if (car.getCarPosition() > maxCarPosition) {
+                maxCarPosition = car.getCarPosition();
+            }
+        }
+    }
+
+    public static void makeWinnerList(Car[] cars) {
+        for (Car car : cars) {
+            if (car.getCarPosition() == maxCarPosition) {
+                winnerList.add(car.getCarName());
+            }
+        }
+    }
+
+    public static void printWinner(Car[] cars) {
+        System.out.printf("%s가 최종 우승했습니다.",String.join(", ", winnerList));
+    }
 }
