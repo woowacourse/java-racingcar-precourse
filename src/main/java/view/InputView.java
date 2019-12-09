0cb8b9025c61663806794771b4db580a9b98c94c
MOON JINJU <mjj920930@gmail.com>
2019-12-09 17:42:46 +0900

    fix: 유틸성 클래스의 인스턴스 생성을 막기 위해 private 생성자 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    private InputView() {}
+
     public static String inputCarNames() {
         try {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,6 +3,7 @@ package view;
 import domain.Cars;
 
 public class OutputView {
+    private OutputView() {}
 
     public static void printCarPositions(Cars cars) {
         cars.printPositions();
