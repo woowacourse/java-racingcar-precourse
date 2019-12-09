296d9db85378f6b2587a3ea48e8cc82e738822de
pandahun <gnsgnsek@gmail.com>
2019-12-09 11:19:06 +0900

    feature: Create RacingGame
    RacingGame 객체 생성

---
+++ src/main/java/domain/RacingGame.java
@@ -0,0 +1,54 @@
+/*
+ *  @RacingGame.java
+ *
+ *  @Version: 0.1
+ *
+ *  @Date: 2019.12.09
+ *
+ *  @Author: pandahun
+ */
+
+package domain;
+
+import io.InputView;
+import io.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGame {
+
+    public static final int ZERO = 0;
+
+    private final RacingCars racingCars;
+    private final int tryCount;
+
+
+    private RacingGame(RacingCars racingCars, int tryCount) {
+        this.racingCars = racingCars;
+        this.tryCount = tryCount;
+    }
+
+    public static RacingGame init() {
+        RacingCars racingCars = assembleCars();
+        int tryCount = InputView.inputTryCount();
+        return new RacingGame(racingCars, tryCount);
+    }
+
+    private static RacingCars assembleCars() {
+        List<String> names = InputView.inputNames();
+        List<Car> cars = names.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+        return new RacingCars(cars);
+    }
+
+    public void run() {
+        for(int i=0; i<tryCount; i++){
+            racingCars.move();
+            OutputView.showNow(racingCars);
+        }
+        OutputView.showWinners(racingCars.getWinners());
+    }
+
+}
