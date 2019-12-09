f7579fc124d3d68ffe748a6c32a41865fc31f41c
YerinCho <forjustice9839@gmail.com>
2019-12-09 20:12:47 +0900

    전진/멈춤 판단 후 Car 객체의 position 설정기능 추가

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -7,8 +7,13 @@ public class RacingGame {
     private Car[] car;
     private int count;
     
+    Computer computer = new Computer();
+    
     public void run() {
 	enterCarInfo();
+	for(int i=0;i<this.count;i++) {
+	    createAndSetPosition();
+	}
     }
     
     public void enterCarInfo() {
@@ -26,7 +31,14 @@ public class RacingGame {
 	while(tmpcount!=0) {
 	    tmpcount=userinputcheck.checkCount(user.inputCount());
 	}
+	
 	this.count=tmpcount;
+    }
+    
+    public void createAndSetPosition() {
+	for(int i=0;i<car.length;i++) {
+	    car[i].isGoStop(computer.createRandNum());
+	}
     }
 }
 
