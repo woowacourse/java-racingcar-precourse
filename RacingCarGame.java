ffb48b3b3b0e879309a99945ed9487fbcb9b25e2
unknown <woncoh1@gmail.com>
2019-12-09 22:23:17 +0900

    [수정] Coding convention: Line separator from CRLF to LF

--- RacingCarGame.java
+++ RacingCarGame.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class RacingCar {
+public class RacingCarGame {
     private static final String PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String PROMPT_TRIAL_LENGTH = "시도할 회수는 몇 회인가요?";
     private static final String TITLE_RACE_RESULT = "%n실행결과%n";
