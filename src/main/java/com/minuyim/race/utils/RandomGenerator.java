28f322a8b096add2c4a83c78e3e0bb52075abcb5
minwoo_yim <minu_yim@naver.com>
2019-12-09 12:57:58 +0900

    randomGenerator int형 출력으로 수정 및 README.md 내용에 맞게 수정

--- src/main/java/com/minuyim/race/utils/RandomGenerator.java
+++ src/main/java/com/minuyim/race/utils/RandomGenerator.java
@@ -5,10 +5,9 @@ import java.util.Random;
 public class RandomGenerator {
     private static Random random = new Random();
     
-    public static double generateRandom() {
-        double randomNumber = random.nextDouble() 
-                * (Constants.RANDOM_MAX_NUMBER - Constants.RANDOM_MIN_NUMBER)
-                + Constants.RANDOM_MIN_NUMBER;
+    public static int generateRandom() {
+        int randomNumber = random.nextInt(Constants.RANDOM_MAX_NUMBER
+                - Constants.RANDOM_MIN_NUMBER + 1) + Constants.RANDOM_MIN_NUMBER;
         
         return randomNumber;
     }
