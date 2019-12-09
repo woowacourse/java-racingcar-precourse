a5c6596f4c9a73c1b371c3abf7b93831f08fbc7a
Minwoo Kim <rlaalsdn456456@naver.com>
2019-12-09 23:11:29 +0900

    기능 : added addCar, runCar

--- src/main/java/CarRace.java
+++ src/main/java/CarRace.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
+import domain.*;
+import InOut.*;
+
 /**
  * CarRace
  */
 public class CarRace {
-
+    private ArrayList<Car> cars = new ArrayList<Car>();
 
     public void start(){
+        //scan
+        
+        //create cars and control
+        addCar();
+        
+        //execute
+        runCar();
+
+        //printresult
+        
+    }
+    public void addCar(){
+        cars.add(new Car("test"));
+    }
+    public void runCar(){
+        for (Car each : cars){
+            each.run();
+        }
     }
 }
