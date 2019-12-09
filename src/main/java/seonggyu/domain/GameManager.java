5dfc1a6f5b664343409ac1a820fd3151ee1bb724
KimSeongGyu1 <KimSeongGyu1@github.com>
2019-12-09 10:58:12 +0900

    [게임관리]수정: 자동차와 회수정보를 게임관리자가 좀더 컨트롤 하기 좋게 구조 변경

--- src/main/java/seonggyu/domain/GameManager.java
+++ src/main/java/seonggyu/domain/GameManager.java
@@ -1,30 +1,36 @@
 package seonggyu.domain;
 
 import seonggyu.cars.Cars;
-import seonggyu.cars.Winners;
 import seonggyu.rounds.Rounds;
+import seonggyu.cars.Winners;
 import seonggyu.input.Input;
 
 public class GameManager {
+	private Cars cars;
+	private Rounds rounds;
+	
 	public void startGame() {
-		Cars cars = new Cars(Input.enterCarNames());
-		Rounds rounds = new Rounds(Input.enterRounds());
-		
-		doRacing(cars, rounds);
-		findWinners(cars);
+		enterInputs();
+		doRacing();
+		findWinners();
 	}
 	
-	private void doRacing(Cars cars, Rounds rounds) {
+	private void enterInputs() {
+		this.cars = new Cars(Input.enterCarNames());
+		this.rounds = new Rounds(Input.enterRounds());
+	}
+
+	private void doRacing() {
 		System.out.println("\n실행결과");
-		for(int i = 0; i < rounds.getRounds(); i++) {
-			cars.goOrStay();
-			cars.printNamesAndPositions();
+		for(int i = 0; i < this.rounds.getRounds(); i++) {
+			this.cars.goOrStay();
+			this.cars.printNamesAndPositions();
 		}
 	}
 	
-	private void findWinners(Cars cars) {
+	private void findWinners() {
 		Winners winners;
-		winners = cars.getWinners();
+		winners = this.cars.getWinners();
 		winners.printWinners();
 	}
 }
--- src/main/java/seonggyu/rounds/Rounds.java
+++ src/main/java/seonggyu/rounds/Rounds.java
@@ -1,10 +1,10 @@
 package seonggyu.rounds;
 
 public class Rounds {
-	private final int rounds;
+	private int rounds;
 	
-	public Rounds (int rounds) {
-		this.rounds = rounds;
+	public Rounds(int input) {
+		this.rounds = input;
 	}
 	
 	public int getRounds() {
