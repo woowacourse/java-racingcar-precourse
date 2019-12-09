2ad38f6118d877c7d820aa71b4c4f052cf20d833
aegis1920 <aegis1920@gmail.com>
2019-12-09 23:09:58 +0900

    feat : add max position을 찾는 메소드, add 마지막 콤마를 지우는 메소드, docs : add 추가한 메소드 변경 내용

--- src/main/java/com/woowacourse/two/CarRacingGame.java
+++ src/main/java/com/woowacourse/two/CarRacingGame.java
@@ -2,20 +2,36 @@ package com.woowacourse.two;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CarRacingGame {
 
-    Scanner sc = new Scanner(System.in);
+    private Scanner sc;
+    private int maxPosition;
+    private int countComma;
+    private static final int MIN_LETTER_SIZE = 0;
+    private static final int MAX_LETTER_SIZE = 5;
+    private static final int MAX_RANDOM = 10;
+    private static final int OVER_NUMBER_MOVE = 4;
+
+
 
     public void run(){
+
+        sc = new Scanner(System.in);
+        maxPosition = Integer.MIN_VALUE;
+        countComma = 0;
+
         String[] carNameArr = setCarNameArr(sc);
         Car[] carArr = setCar(carNameArr);
         int playCount = setPlayCount(sc);
         playCarMove(carArr, playCount);
+        printWinner(carArr);
     }
 
     public String[] setCarNameArr(Scanner sc){
 
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carNameStr = sc.nextLine();
 
         while(true){
@@ -45,7 +61,7 @@ public class CarRacingGame {
     }
 
     public boolean isRightCarName(String carName){
-        if(carName.length() == 0 || carName.length() > 5){
+        if(carName.length() == MIN_LETTER_SIZE || carName.length() > MAX_LETTER_SIZE){
             return false;
         }
         return true;
@@ -64,37 +80,69 @@ public class CarRacingGame {
     }
 
     public int setPlayCount(Scanner sc){
+        System.out.println("시도할 횟수는 몇 회인가요?");
         int playCount = sc.nextInt();
         return playCount;
     }
 
     public void playCarMove(Car[] carArr, int playCount){
-
+        System.out.println("실행결과");
         for(int i = 0 ; i < playCount ; i++){
             for(int j = 0 ; j < carArr.length ; j++){
                 moveCarOrNot(carArr, j);
             }
-
-            printCarMove(carArr);
+            printCarMove(carArr, i, playCount);
         }
-
 
     }
 
     public void moveCarOrNot(Car[] carArr, int carIndex){
-        int randomNumber = (int) (Math.random() * 10);
+        int randomNumber = (int) (Math.random() * MAX_RANDOM);
 
-        if(randomNumber >= 4){
+        if(randomNumber >= OVER_NUMBER_MOVE){
             carArr[carIndex].movePosition();
         }
 
     }
 
-    public void printCarMove(Car[] carArr){
+    public void printCarMove(Car[] carArr, int playIndex, int playCount){
         for(Car car : carArr){
             System.out.println(car.getName() + " : " + car.getCarDistance());
+
+            if(playIndex == playCount - 1){
+                findMaxPosition(car);
+            }
+        }
+        System.out.println();
+    }
+
+    public void findMaxPosition(Car car){
+        if(car.getPosition() > maxPosition){
+            maxPosition = car.getPosition();
+            countComma++;
         }
     }
 
+    public void printWinner(Car[] carArr){
+
+        String str = "";
+
+        for(Car car : carArr){
+            if(car.getPosition() == maxPosition){
+                str += car.getName() + ", ";
+            }
+        }
+
+        str = deleteComma(str);
+
+        System.out.println(str + "가 최종 우승했습니다.");
+    }
+
+    public String deleteComma(String str){
+        if(str.length() > 0 && str.charAt(str.length() - 2) == ','){
+            str = str.substring(0, str.length() - 2);
+        }
+        return str;
+    }
 
 }
