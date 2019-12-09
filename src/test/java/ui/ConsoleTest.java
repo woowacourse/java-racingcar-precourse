e0dc02f06cd8a4cb8616f2d7a52b107da89d1ed2
junk-sound <wom2277@naver.com>
2019-12-09 23:36:47 +0900

    비기능: '사용자가 입력을 잘못한 상황을 처리하는 기능'에 대한 단위 테스트.

--- src/test/java/ui/ConsoleTest.java
+++ src/test/java/ui/ConsoleTest.java
@@ -2,7 +2,6 @@ package ui;
 
 import domain.RacingGameConfig;
 import domain.Validator;
-import domain.errors.InvalidInputException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterEach;
@@ -17,20 +16,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ConsoleTest {
 
     private Console console;
     private Scanner scanner;
+    private InputController inputController;
     private Validator mockValidator = mock(Validator.class);
     private RacingPrinter mockRacingPrinter = mock(RacingPrinter.class);
 
     @AfterEach
     void clear() {
         try {
-            scanner.close();
+            if (scanner != null) {
+                scanner.close();
+            }
         } catch (RuntimeException e) {
             System.out.println(String.format("테스트 중 다음과 같은 오류로 시스템을 종료합니다 : %s", e.getMessage()));
             System.exit(-1);
@@ -56,37 +57,71 @@ class ConsoleTest {
     @Test
     void extractNamesWithTooLongNames() {
         //given
+        console = createConsoleWithMockInputControllerForTest();
         String tooLongName = StringUtils.repeat("a", RacingGameConfig.MAX_NAME_LENGTH + 1);
-        String input = String.format("%s,crong,honux", tooLongName);
-        console = createConsoleForTest(input);
-        when(mockValidator.validateNames(any())).thenReturn(false);
-        //when& then
-        assertThrows(InvalidInputException.class, () -> console.extractNames());
+        String normalNames = "pobi,crong,honux";
+        String[] result = {"pobi", "crong", "honux"};
+        when(inputController.nextLine())
+                .thenReturn(tooLongName)
+                .thenReturn(normalNames);
+        when(mockValidator.validateNames(any()))
+                .thenReturn(false)
+                .thenReturn(true);
+        //when & then
+        assertArrayEquals(result, console.extractNames());
+        verify(mockRacingPrinter).printErrorWhenExtractingNames(any());
+
     }
     @Test
     void extractNamesWithNotAllowdCharacter() {
         //given
-        String input = "pobi,crong,hon*ux";
-        console = createConsoleForTest(input);
-        when(mockValidator.validateNames(any())).thenReturn(false);
+        console = createConsoleWithMockInputControllerForTest();
+        String namesWithNotAllowedCharacter = "pobi,crong,hon*ux";
+        String normalNames = "pobi,crong,honux";
+        String[] result = {"pobi", "crong", "honux"};
+        when(inputController.nextLine())
+                .thenReturn(namesWithNotAllowedCharacter)
+                .thenReturn(normalNames);
+        when(mockValidator.validateNames(any()))
+                .thenReturn(false)
+                .thenReturn(true);
         //when & then
-        assertThrows(InvalidInputException.class, () -> console.extractNames());
-
-        //given
-        input = "pobi, crong,honux";
-        console = createConsoleForTest(input);
-        //when & then
-        assertThrows(InvalidInputException.class, () -> console.extractNames());
+        assertArrayEquals(result, console.extractNames());
+        verify(mockRacingPrinter).printErrorWhenExtractingNames(any());
     }
     @Test
-    void extractNamesWithSeparatorAtLast() {
+    void extractNamesWithSpace() {
         //given
-        String input = ",pobi,crong,honux,";
-        console = createConsoleForTest(input);
+        console = createConsoleWithMockInputControllerForTest();
+        String namesWithSpace = "pobi, crong,honux";
+        String normalNames = "pobi,crong,honux";
+        String[] result = {"pobi", "crong", "honux"};
+        when(inputController.nextLine())
+                .thenReturn(namesWithSpace)
+                .thenReturn(normalNames);
+        when(mockValidator.validateNames(any()))
+                .thenReturn(false)
+                .thenReturn(true);
         //when & then
-        assertThrows(InvalidInputException.class, () -> console.extractNames());
+        assertArrayEquals(result, console.extractNames());
+        verify(mockRacingPrinter).printErrorWhenExtractingNames(any());
     }
-
+    @Test
+    void extractNamesWithSeparatorsAtFirstAndLast() {
+        //given
+        console = createConsoleWithMockInputControllerForTest();
+        String namesWithSperatorsAtFirstAndLast = ",pobi,crong,honux,";
+        String normalNames = "pobi,crong,honux";
+        String[] result = {"pobi", "crong", "honux"};
+        when(inputController.nextLine())
+                .thenReturn(namesWithSperatorsAtFirstAndLast)
+                .thenReturn(normalNames);
+        when(mockValidator.validateNames(any()))
+                .thenReturn(true);
+        //when & then
+        assertArrayEquals(result, console.extractNames());
+        verify(mockRacingPrinter).printErrorWhenExtractingNames(any());
+    }
 
     @Test
     void getCyclesWitchValidInput() {
@@ -98,31 +133,46 @@ class ConsoleTest {
         assertEquals(Integer.parseInt(input), console.getCycles());
     }
     @Test
-    void getCyclesWitchInvalidInputs() {
+    void getCyclesWithTooSmallInput() {
         //given
-        String input = String.valueOf(RacingGameConfig.MIN_CYCLES - 1) ;
-        console = createConsoleForTest(input);
-        when(mockValidator.validateCycles(anyInt())).thenReturn(false);
+        console = createConsoleWithMockInputControllerForTest();
+        String tooSmallInput = String.valueOf(RacingGameConfig.MIN_CYCLES - 1);
+        String normalInput = "1";
+        when(inputController.nextLine())
+                .thenReturn(tooSmallInput)
+                .thenReturn(normalInput);
+        when(mockValidator.validateCycles(anyInt()))
+                .thenReturn(false)
+                .thenReturn(true);
         //when & then
-        assertThrows(InvalidInputException.class, () -> console.getCycles());
+        assertEquals(Integer.parseInt(normalInput), console.getCycles());
+        verify(mockRacingPrinter).printErrorWithTooSmallCycle(anyInt());
+    }
+    @Test
+    void getCyclesWithNotNaturalInput() {
+        //given
+        console = createConsoleWithMockInputControllerForTest();
+        String alphabetInput = "a";
+        String normalInput = "1";
+        when(inputController.nextLine())
+                .thenReturn(alphabetInput)
+                .thenReturn(normalInput);
+        when(mockValidator.validateCycles(anyInt()))
+                .thenReturn(true);
 
-        //given
-        input = "a";
-        console = createConsoleForTest(input);
         //when & then
-        assertThrows(InputMismatchException.class, () -> console.getCycles());
-
-        //given
-        input = "1.1";
-        console = createConsoleForTest(input);
-        //when & then
-        assertThrows(InputMismatchException.class, () -> console.getCycles());
+        assertEquals(Integer.parseInt(normalInput), console.getCycles());
+        verify(mockRacingPrinter).printErrorWithNotNaturalNumber();
     }
 
     private Console createConsoleForTest(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         scanner = new Scanner(System.in);
-
-        return new Console(scanner, mockValidator, mockRacingPrinter);
+        inputController = new InputControllerImpl(scanner);
+        return new Console(inputController, mockValidator, mockRacingPrinter);
+    }
+    private Console createConsoleWithMockInputControllerForTest() {
+        inputController = mock(InputController.class);
+        return new Console(inputController, mockValidator, mockRacingPrinter);
     }
 }
