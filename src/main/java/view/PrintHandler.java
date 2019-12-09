803ce5f5d1534760cf027c10dfc58f01ac9cd947
KIMSIYOUNG <tldud2404@gmail.com>
2019-12-09 19:08:07 +0900

    refactor, docs: 상수 네이밍 수정 및 리팩토링 할 일 추가(readme.md)

--- src/main/java/view/PrintHandler.java
+++ src/main/java/view/PrintHandler.java
@@ -13,33 +13,33 @@ import domain.Car;
 public class PrintHandler {
     private static final String NEXT_LINE = "\n";
     private static final String COMMA = ", ";
-    private static final String notificationForCarName = "경주 할 자동차의 이름을 입력해주세요 ','를 기준으 구분합니다.";
-    private static final String notificationForHowMany = "시도 할 횟수는 몇번인가요?";
-    private static final String carNameInputError = "각 자동차의 이름은 1글자 이상 5글자 이하로만 가능합니다. ','를 기준으로 입력해주세요.";
-    private static final String formatError = "올바르지 않은 입력입니다. 원하는 횟수를 1보다 큰 자연수로 입력해주세요.";
-    private static final String gameResult = "실행 결과";
+    private static final String NOTIFICATION_FOR_CAR_NAME = "경주 할 자동차의 이름을 입력해주세요 ','를 기준으 구분합니다.";
+    private static final String NOTIFICATION_FOR_HOW_MANY = "시도 할 횟수는 몇번인가요?";
+    private static final String CAR_NAME_INPUT_ERROR = "각 자동차의 이름은 1글자 이상 5글자 이하로만 가능합니다. ','를 기준으로 입력해주세요.";
+    private static final String FORMAT_ERROR = "올바르지 않은 입력입니다. 원하는 횟수를 1보다 큰 자연수로 입력해주세요.";
+    private static final String GAME_RESULT = "실행 결과";
     private static final String WINNER = "최종우승자는 : ";
     private static final String HERE = " 입니다.";
 
 
     public static void notifyCarNameRule() {
-        System.out.println(notificationForCarName);
+        System.out.println(NOTIFICATION_FOR_CAR_NAME);
     }
 
     public static void notifyHowManyTimes() {
-        System.out.println(notificationForHowMany);
+        System.out.println(NOTIFICATION_FOR_HOW_MANY);
     }
 
     public static void notifyCarNameError() {
-        System.out.println(carNameInputError);
+        System.out.println(CAR_NAME_INPUT_ERROR);
     }
 
     public static void notifyFormatError() {
-        System.out.println(formatError);
+        System.out.println(FORMAT_ERROR);
     }
 
     public static void gameResult() {
-        System.out.println(gameResult);
+        System.out.println(GAME_RESULT);
     }
 
     public static void nextLine() {
