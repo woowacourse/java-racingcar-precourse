c554d010fba85ec4d11bd9e29038d553b06994f1
sezikim <kdghj1002@gmail.com>
2019-12-09 15:46:16 +0900

    add printToInputMessages

---
+++ src/main/java/domain/RacingGame.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class RacingGame {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static void printToInputCarNamesMessage() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+	}
+
+	private static void printToInputMoveCountMessage() {
+		System.out.println("시도할 회수는 몇회인가요?");
+	}
+}
