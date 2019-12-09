99ed051b8b42d44a9a86b6281513393d0bd5720b
Minwoo Kim <rlaalsdn456456@naver.com>
2019-12-09 22:26:47 +0900

    기능 : add getters for Car

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,7 +2,7 @@ package domain;
 import  java.util.Random;
 
 public class Car {
-    private final static int criteria = 4;
+    private final static int CRITERIA = 4;
     private final String name;
     private int position = 0;
     private Random random = new Random();
@@ -13,13 +13,21 @@ public class Car {
 
     
     // 추가 기능 구현
+
+    public String getName(){
+        return name;
+    }
+    public int getPosition(){
+        return position;
+    }
+
     public void run(){
         if(canRun())
             position=position+1;
     }
 
     public boolean canRun(){
-        return random.nextInt(9)>=criteria;
+        return random.nextInt(9)>=CRITERIA;
     }
 
 
