03116b7b633249723128d5c18f0d696dfa4e44ed
sezikim <kdghj1002@gmail.com>
2019-12-09 23:17:41 +0900

    add runRacingGame, main

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -5,11 +5,25 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class RacingGame {
-
 	public static void main(String[] args) {
-
+		User user = new User();
+		runRacingGame(user);
 	}
 
+	private static void runRacingGame(User user) {
+		printToInputCarNamesMessage();
+		Entry entry = makeRacingEntry(getCarNameList(user.getCarNames()));
+
+		printToInputMoveCountMessage();
+		int carMoveCount = user.getCarMoveCount();
+
+		for (int i = 0; i < carMoveCount; ++i) {
+			entry.moveEachCar();
+			entry.printEachCarPosition();
+		}
+
+		entry.printWhoWin();
+	}
 
 	private static Entry makeRacingEntry(List<String> carNameList) {
 		List<Car> carList = new ArrayList<>();
