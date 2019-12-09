02b4e202e121719db712ce875175af9f69430e70
junk-sound <wom2277@naver.com>
2019-12-10 02:11:02 +0900

    비기능: '자동차가 경주하는 기능'에 대한 단위 테스트.

--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -1,5 +1,44 @@
+package domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class RacingCarTest {
 
+    private Random mockRandomGenerator = mock(Random.class);
+    private RacingCar racingCar;
+
+    @BeforeEach
+    void init() {
+        racingCar = new RacingCar("testRacingCar", mockRandomGenerator);
+    }
+
+    @Test
+    void raceWhenCanGo() {
+        //given
+        int defaultPosition = racingCar.getPosition();
+        when(mockRandomGenerator.nextInt(anyInt())).thenReturn(RacingGameConfig.STANDARD_TO_GO);
+        //when
+        racingCar.race();
+        //then
+        assertEquals(defaultPosition + RacingGameConfig.STEP, racingCar.getPosition());
+    }
+
+    @Test
+    void raceWhenCanNotGo() {
+        //given
+        int defaultPosition = racingCar.getPosition();
+        when(mockRandomGenerator.nextInt(anyInt())).thenReturn(RacingGameConfig.STANDARD_TO_GO - 1);
+        //when
+        racingCar.race();
+        //then
+        assertEquals(defaultPosition, racingCar.getPosition());
+    }
 }
