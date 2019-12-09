926c8a7fc3b5965248ebe67d438c8b8a74c151d9
unknown <woncoh1@gmail.com>
2019-12-10 00:56:06 +0900

    [수정] 변수 및 파일이름 조정

--- CarRacing.java
+++ CarRacing.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class RacingCarGame {
+public class CarRacing {
     private static final String PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String PROMPT_TRIAL_LENGTH = "시도할 회수는 몇 회인가요?";
     private static final String MESSAGE_RACE_START = "실행결과";
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        RacingCarGame game = new RacingCarGame();
+        CarRacing game = new CarRacing();
         game.play();
     }
 }
