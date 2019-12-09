06896ddae8aff6a291d48c7a933848871ec5184b
tiroring09 <tiroring09@gmail.com>
2019-12-09 19:55:14 +0900

    사용자로부터 반복할 횟수를 입력받고, 유효성을 검사하는 메서드 생성.

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -45,11 +45,46 @@ public class User {
 		return User.carNames;
 	}
 	
+	private static void setTrialNum() throws Exception {
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("경주할 횟수를 입력하세요.");
+		trialNum = input2.nextInt();
+		// 정수아닌 자료 입력시 ImputMismatchException예외 출력
+		// 자연수 아닌 자료 입력시 아래 if문에서 Exception예외 출력
+		if (trialNum <= 0) {
+			Exception ex1 = new Exception();
+			throw ex1;
+		}
+	}
+	
+	public static void setValidTrialNum() {
+		while (true) {
+			try {
+				User.setTrialNum();
+				break;				
+			} catch (InputMismatchException ime) {
+				System.out.println("잘못된 입력: 정수를 입력하세요.");
+			} catch (Exception ex1) {
+				System.out.println("잘못된 입력: 양수를 입력하세요.");
+			}
+		}
+		System.out.println("사용자 입력 : "+ trialNum 
+				+ "이(가) 정상적으로 입력되었습니다." );
+	}
+	
+	public static int getTrialNum() {
+		return User.trialNum;
+	}
+	
 	public static void main(String[] args) throws Exception{
 		//User test
 //		getValidCarName();
 //		for (int i = 0; i < carNames.length; i++) {
 //			System.out.println(carNames[i]);
 //		}
+		
+		//trialNum() test
+		setValidTrialNum();
+		System.out.println(trialNum);
 	}
 }
