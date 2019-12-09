29febd602f4da82bcff1b58176fc314e84e6fc26
Tak Hyungmin <milkho11@naver.com>
2019-12-09 21:07:11 +0900

    기능:초기 출력
    OutputUtil에는 입력을 받기 위한 출력 함수를 담고 있음.

---
+++ src/main/java/utils/OutPutUtil.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class OutPutUtil {
+    public void printRacingCarsNameDemand() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void askAttempTime() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    // TODO : 실행 결과 출력 함수
+}
