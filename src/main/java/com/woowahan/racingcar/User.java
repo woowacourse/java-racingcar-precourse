c7a714a8e9a1e43e2894fcc3792c1076601098dc
Jinhong Kim <deepred.kim@gmail.com>
2019-12-09 09:39:00 +0900

    Implement input() in User.java

--- src/main/java/com/woowahan/racingcar/User.java
+++ src/main/java/com/woowahan/racingcar/User.java
@@ -48,6 +48,10 @@ public class User extends AbstractUser {
 
 	@Override
 	public void input() {
+		System.out.println("경주할 자동차 이름을 입력하세.(이름은 쉼표(,) 기준으로 구분)");
+		inputCars();
 
+		System.out.println("시도할 회수는 몇회인가요?");
+		inputRound();
 	}
 }
