e979b01a31f66b1febffd9b1e13aad0d67932cfa
Tak Hyungmin <milkho11@naver.com>
2019-12-09 21:49:36 +0900

    기능 : 랜덤 값 생성
    RandomGenerator 함수는 랜덤 값을 생성하는 기능을 가진다.

---
+++ src/main/java/generater/RandomGenerater.java
@@ -0,0 +1,11 @@
+package generater;
+
+import java.util.Random;
+
+public class RandomGenerater {
+    private static final int UPPER_BOUND = 10;
+
+    public int generateRandom() {
+        return new Random().nextInt();
+    }
+}
