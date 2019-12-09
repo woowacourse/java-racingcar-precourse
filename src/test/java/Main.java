cb6d9073dc0a4135f72cfc04e6c07c9afbd84118
Jinhong Kim <deepred.kim@gmail.com>
2019-12-09 10:36:24 +0900

    Implement Main()

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,12 @@
+import com.woowahan.racingcar.Race;
+import com.woowahan.racingcar.User;
+
 public class Main {
 	public static void main(String[] args) {
+		User user = new User();
+		user.input();
 
+		Race race = new Race(user);
+		race.start();
 	}
 }
