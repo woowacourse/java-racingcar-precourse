9e5927e8ce287a830a5e41c2df79a310c709ba2d
tiroring09 <tiroring09@gmail.com>
2019-12-09 19:48:52 +0900

    개별 자동차의 이름과 위치정보를 저장, 출력, 제어하기 위한 클래스 `Car` 생성.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,6 +1,7 @@
 package domain;
 
 public class Car {
+	//띄어쓰기 컨벤션 체크하자. Space인지, tab인지
     private final String name;
     private int position = 0;
 
@@ -8,5 +9,32 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+	public String getName() {
+		return name;
+	}
+	
+    public int getPosition() {
+    	return this.position;
+    }
+    
+    public void addPosition() {
+    	this.position += 1;
+    }
+    
+    //Car class test
+    public static void main(String[] args) {
+//		Car test1 = new Car("asdf");
+//		System.out.println(test1.getName());
+//		System.out.println(test1.getPosition());
+//		test1.addPosition();
+//		System.out.println(test1.getPosition());
+//		
+		Car asdf = new Car("asdf");
+		Car qwer = new Car("qwer");
+		Car[] carArray = {asdf, qwer};
+		for (int i = 0; i < carArray.length; i++) {
+			carArray[i].addPosition();
+			System.out.println(carArray[i].getName() + carArray[i].getPosition());
+		}
+	}
 }
