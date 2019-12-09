49d98106f2ea37b4ce4ef42a703716ec655a55bb
sezikim <kdghj1002@gmail.com>
2019-12-09 17:08:39 +0900

    add constant

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,8 +1,12 @@
 package domain;
 
 public class Car {
+	public static final int START_POSITION = 0;
+	public static final int MAX_STOP_NUMBER = 3;
+	public static final int MAX_RANDOM_NUMBER = 9;
+
 	private final String name;
-	private int position = 0;
+	private int position = START_POSITION;
 
 	public Car(String name) {
 		this.name = name;
@@ -10,14 +14,14 @@ public class Car {
 
 	public void moveCar() {
 
-		if (getRandomNumber() > 3) {
+		if (getRandomNumber() > MAX_STOP_NUMBER) {
 			position++;
 		}
 
 	}
 
 	private int getRandomNumber() {
-		return (int)(Math.random() * 10);
+		return (int)(Math.random() * (MAX_RANDOM_NUMBER + 1);
 	}
 
 	public String getName() {
