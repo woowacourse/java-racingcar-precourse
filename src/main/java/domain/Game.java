7110d99c46f2061f6bfdf58a95aec592d9e188b6
Andy H. Jung <commaniakr@gmail.com>
2019-12-10 19:29:44 +0900

    refactor: README, 주석

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,6 +1,6 @@
 /**
  * Game.java
- * 아직 리팩토링 중...
+ * 자동차 경주 게임
  * 우아한테크코스 프리코스 2주차.
  * Original code https://github.com/hotheadfactory/java-racingcar-precourse
  * Version: v0.0.1, 2019.12.05 (c) 정회형
