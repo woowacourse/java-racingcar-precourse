9d83deef79d05c8079954477ac02b6e7ec5c9beb
JeongSeHwan-93 <rgn2002@naver.com>
2019-12-10 17:47:01 +0900

    Game 기능을 추가

--- src/main/java/domain/Racings.java
+++ src/main/java/domain/Racings.java
@@ -2,4 +2,22 @@ package domain;
 import java.util.Scanner;
 
 public class Racings {
+
+    Scanner scanner = new Scanner(System.in);
+    String[] Names;
+    Car[] cars;
+    int i,num,j,k;
+
+    public void game(){
+        String Car;
+        System.out.println("Input Racing Cars Name");
+        Car = scanner.nextLine();
+        Names = Car.split(",");
+        if(CKBoundary()) return;
+        setCar();
+        System.out.println("How many times game implements?");
+        num = scanner.nextInt();
+        Gamestart();
+        EndGame();
+    }
 }
