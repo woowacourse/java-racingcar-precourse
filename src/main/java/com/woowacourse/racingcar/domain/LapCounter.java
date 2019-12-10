c58e84e8aedd7b034f4271523f0c6d22b7054adf
KSKIM <kwangsoonkim@outlook.com>
2019-12-10 20:30:53 +0900

    refactor: Change variable name for consistency

--- src/main/java/com/woowacourse/racingcar/domain/LapCounter.java
+++ src/main/java/com/woowacourse/racingcar/domain/LapCounter.java
@@ -6,8 +6,8 @@ package com.woowacourse.racingcar.domain;
  * @since 2019-12-09
  */
 public class LapCounter {
-    private static final String EXCEPTION_ALREADY_END_LAP = "모든 랩을 돌아 더 이상 돌 수 없습니다.";
-    private static final String EXCEPTION_INVALID_TOTAL_LAP = "한 바퀴 이상 돌아야 합니다.";
+    private static final String ALREADY_END_LAP_EXCEPTION = "모든 랩을 돌아 더 이상 돌 수 없습니다.";
+    private static final String INVALID_TOTAL_LAP_EXCEPTION = "한 바퀴 이상 돌아야 합니다.";
     private final static int START_LAP_COUNT = 0;
 
     private final int totalLap;
@@ -20,13 +20,13 @@ public class LapCounter {
 
     private void validateTotalLap(int totalLap) {
         if (totalLap <= START_LAP_COUNT) {
-            throw new IllegalArgumentException(EXCEPTION_INVALID_TOTAL_LAP);
+            throw new IllegalArgumentException(INVALID_TOTAL_LAP_EXCEPTION);
         }
     }
 
     public void nextLap() {
         if (isEndOfLap()) {
-            throw new IndexOutOfBoundsException(EXCEPTION_ALREADY_END_LAP);
+            throw new IndexOutOfBoundsException(ALREADY_END_LAP_EXCEPTION);
         }
         ++currentLap;
     }
