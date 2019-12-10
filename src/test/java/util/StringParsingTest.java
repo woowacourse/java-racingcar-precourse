cb2be90190cd89e48b5c5664f184d01172539bac
mjung1798 <mor2222@naver.com>
2019-12-10 19:44:53 +0900

    [ feat 7 ] : String Parsing 로직 - 입력받을 때

--- src/test/java/util/StringParsingTest.java
+++ src/test/java/util/StringParsingTest.java
@@ -1,6 +1,8 @@
 package util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +10,8 @@ class StringParsingTest {
 
 	@Test
 	void makeNumbersFromString() {
+		String stringArray = "a, b, c";
+		List<String> strings = StringParsing.makeNumbersFromString(stringArray);
+		assertThat(strings).containsExactly("a", "b", "c");
 	}
 }
