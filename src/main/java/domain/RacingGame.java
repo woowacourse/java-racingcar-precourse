4d56031758d3a22f0946160d0920ec8607731401
jb-oh <jb.oh@icloud.com>
2019-12-10 17:11:53 +0900

    Added feature that uses user's input for number of games to set as for loop's number of iterations.

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,12 +1,19 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RacingGame {
 
     static String returnCarName(String name) {
         Car newCar = new Car(name);
         return newCar.getName();
+    }
+
+    static void playGame(int gameNumber) {
+        for (int i = 0; i < gameNumber; i++) {
+            // moveCar();
+        }
     }
 
     public static void main(String[] args) {
@@ -30,5 +37,7 @@ public class RacingGame {
             carArray.add(new Car(name));
         }
         // System.out.println(carArray.get(0).getName());
+
+        playGame(gameNumber);
     }
 }
