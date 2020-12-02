d5113746b6a47cef5c79bc3e56d6efba051c4a28
RinSabbit <celena1004@naver.com>
2020-12-02 20:03:59 +0900

    feat(Car): getPosition, run 함수 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,9 +8,15 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
-
     public String getName(){
         return this.name;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public void run(){
+        position++;
     }
 }
