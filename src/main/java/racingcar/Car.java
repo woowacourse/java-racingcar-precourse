8c54e0f6b0f1880d24e57a4ee910910dfbe9295e
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-02 19:47:08 +0900

    feat 자동차의 위치값을 증가시켜주는 메서드 추가 및 증가 시켜주는 기준 4이상인 경우만 RandomUtils.nextInt 사용

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,12 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
+    private static final int DEFAULT_MAX_RANDOM_NUM = 9;
+    private static final int DEFAULT_MIN_RANDOM_NUM = 0;
+    private static final int DEFAULT_MOVE_NUM = 3;
+
     private final String name;
     private int position = 0;
 
@@ -9,4 +15,13 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public void increasePosition() {
+        if(RandomUtils.nextInt(DEFAULT_MIN_RANDOM_NUM,DEFAULT_MAX_RANDOM_NUM) > DEFAULT_MOVE_NUM) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
