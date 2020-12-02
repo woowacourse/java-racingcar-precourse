c2cb7e254c4c8d9bb7575816e16f0a9e51bac086
platanus <platanus@canxan.com>
2020-12-02 22:41:35 +0900

    feat(stringutils): Make parser by character format

--- src/main/java/racingcar/StringUtils.java
+++ src/main/java/racingcar/StringUtils.java
@@ -1,5 +1,33 @@
 package racingcar;
 
-public class StringUtils {
+import com.sun.tools.sjavac.pubapi.TypeVarTypeDesc;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class StringUtils {
+    public static final char runnerFormat = ',';
+
+    public String inputRunnerString;
+    public int inputRoundCount;
+
+    public StringUtils(Scanner promptInput){
+        inputRunnerString = promptInput.next();
+    }
+    public String[] splitRunner(){
+        String[] runners = inputRunnerString.split(String.valueOf(runnerFormat), 0);
+
+        return runners;
+    }
+
+
+    public static void main(String[] args) {
+        String[] cast;
+        final Scanner scanner = new Scanner(System.in);
+        // Test
+
+        StringUtils input = new StringUtils(scanner);
+        cast = input.splitRunner();
+
+
+    }
 }
