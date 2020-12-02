2789131a911ecb304d23eab1ea51acce5f71db40
doorisopen <audauddl2@gmail.com>
2020-12-02 22:06:53 +0900

    feat: 자동차 이름의 길이가 5자 이하인지 검증한다

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -7,12 +7,23 @@ import java.util.Scanner;
 public class InputController {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String DELIMITER_COMMA = ",";
+    private static final int NAME_MAX_LENGTH = 5;
 
     public List<String> inputCarName(Scanner scanner) {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         List<String> participants = parseStringToStringList(scanner.nextLine());
         //validate
+        validateCarNameLengthIs5OrLess(participants);
         return participants;
+    }
+
+    private void validateCarNameLengthIs5OrLess(List<String> participants) {
+        boolean isCarNameLength5OrLess = participants.stream()
+                .filter(name -> name.length() <= NAME_MAX_LENGTH)
+                .count() == participants.size();
+        if (!isCarNameLength5OrLess) {
+            throw new IllegalArgumentException("이름의 길이는 5이하여야 합니다.");
+        }
     }
 
     private List<String> parseStringToStringList(String input) {
