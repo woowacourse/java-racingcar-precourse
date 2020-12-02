f2f78f7f4af8f5d03932fd2c243cebaaba0c87d0
kihwankim <clack2933@gmail.com>
2020-12-03 00:02:48 +0900

    test: 입력받은 flag 번호 리스트를 실제 enum flag 리스트 반환 검즘

--- src/test/java/utils/CarMovingFlagUtilsTest.java
+++ src/test/java/utils/CarMovingFlagUtilsTest.java
@@ -3,6 +3,9 @@ package utils;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.CarMovingFlag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -47,5 +50,60 @@ class CarMovingFlagUtilsTest {
         // then
         assertThrows(IllegalArgumentException.class, () -> CarMovingFlagUtils.selectMovingFlag(errorFlagLessNumberMinusOne));
         assertThrows(IllegalArgumentException.class, () -> CarMovingFlagUtils.selectMovingFlag(errorFlagExceedNumberTen));
+    }
+
+    @Test
+    void 모두_STOP_플래그_리스트_검증() {
+        // given
+        final int stopFlagNumber = 1;
+        final int lenOfList = 3;
+        List<Integer> onlyStopFlagNumberList = new ArrayList<>(lenOfList);
+        List<CarMovingFlag> expectedFlagList = new ArrayList<>(lenOfList);
+
+        // when
+        for (int index = 0; index < lenOfList; index++) {
+            onlyStopFlagNumberList.add(stopFlagNumber);
+            expectedFlagList.add(CarMovingFlag.STOP);
+        }
+
+        // then
+        assertThat(CarMovingFlagUtils.makeCarMovieFlagList(onlyStopFlagNumberList)).isEqualTo(expectedFlagList);
+    }
+
+    @Test
+    void flag_1_STOP_1_FORWARD_플래그_리스트_검증() {
+        // given
+        final int stopFlagNumber = 1;
+        final int forwardFlagNumber = 5;
+        final int lenOfList = 2;
+        List<Integer> oneStopOneForwardFlagNumberList = new ArrayList<>(lenOfList);
+        List<CarMovingFlag> expectedFlagList = new ArrayList<>(lenOfList);
+
+        // when
+        oneStopOneForwardFlagNumberList.add(stopFlagNumber);
+        expectedFlagList.add(CarMovingFlag.STOP);
+        oneStopOneForwardFlagNumberList.add(forwardFlagNumber);
+        expectedFlagList.add(CarMovingFlag.FORWARD);
+
+        // then
+        assertThat(CarMovingFlagUtils.makeCarMovieFlagList(oneStopOneForwardFlagNumberList)).isEqualTo(expectedFlagList);
+    }
+
+    @Test
+    void 모두_FORWARD_플래그_리스트_검증() {
+        // given
+        final int forwardFlagNumber = 5;
+        final int lenOfList = 3;
+        List<Integer> onlyForwardFlagNumberList = new ArrayList<>(lenOfList);
+        List<CarMovingFlag> expectedFlagList = new ArrayList<>(lenOfList);
+
+        // when
+        for (int index = 0; index < lenOfList; index++) {
+            onlyForwardFlagNumberList.add(forwardFlagNumber);
+            expectedFlagList.add(CarMovingFlag.FORWARD);
+        }
+
+        // then
+        assertThat(CarMovingFlagUtils.makeCarMovieFlagList(onlyForwardFlagNumberList)).isEqualTo(expectedFlagList);
     }
 }
