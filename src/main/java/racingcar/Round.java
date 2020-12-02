8169262cf2fd68093fdb7ef95978327fdcbae124
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-03 01:53:55 +0900

    feat(round) : 진행 된 횟수를 뜻하는 객체 구현

---
+++ src/main/java/racingcar/Round.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class Round {
+    public int round;
+
+    public Round() {
+        this.round = round;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public void decreaseRound(int round) {
+        round--;
+    }
+}
