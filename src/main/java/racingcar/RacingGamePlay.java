56e3e68ac9c999141050c4b5b7f3902bac512415
Jun-Space <tjrwns126@gmail.com>
2020-12-02 20:45:15 +0900

    feat(RacingGamePlay): 게임 진행 중 Car의 이름과 현재 위치를 저장하도록 객체 구현 - Hashmap 구조를 사용하여 각 key에 car의 이름이, value에 현재 위치가 저장되도록 함

--- src/main/java/racingcar/RacingGamePlay.java
+++ src/main/java/racingcar/RacingGamePlay.java
@@ -1,9 +1,16 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RacingGamePlay {
     int gameCount;
+    Map<String, Integer> racingPlayBoard;
 
     public RacingGamePlay() {
       this.gameCount = 0;
+      this.racingPlayBoard = new HashMap<String, Integer>();
     }
+
+    
 }
