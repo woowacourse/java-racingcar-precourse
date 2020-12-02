86c7496089e76dd724ac8c8d280fdcfc82457f11
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-03 01:48:35 +0900

    feat(Sentences) : 게임 진행 중 사용 될 문장들을 모은 객체 구현

---
+++ src/main/java/utils/Sentences.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Sentences {
+    public static final String ASK_CAR_NAME =
+            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    public static final String ASK_TRACK_LENGTH =
+            "시도할 회수는 몇회인가요?";
+    public static final String SHOW_RESULT = "실행 결과";
+    public static final String SHOW_WINNER = "최종 우승자: ";
+    public static final String PROGRESS = "-";
+    public static final String MAXIMUM_CARNAME_LENGTH_ERROR =
+            "이름은 5자 이하만 가능하다";
+
+
+
+}
