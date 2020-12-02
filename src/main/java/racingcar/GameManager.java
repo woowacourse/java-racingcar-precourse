55fadf4063fb6d954469815f3f45b295955a359a
sounmind <sounmind777@gmail.com>
2020-12-03 06:48:39 +0900

    feat(GameManger): 문자열 초과 입력 시 예외 처리 구현

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class GameManager {
+    private static final int LENGTH_LIMIT_OF_INDIVIDUAL_INPUT = 5;
     private static final String DELIMITER = ",";
+    private static final String MESSAGE_LIMIT_EXCESS = "[ERROR] 문자열 입력 한계를 초과했습니다.";
     private static int participantsTotal; // 참가자 총원
     private static String[] stringArrayParticipants;
     private static HashMap<String, Car> participants = new HashMap<String, Car>(); // 참가자 이름: 참가자 Car
@@ -15,12 +17,25 @@ public class GameManager {
 
     public void gameStart(Scanner scanner) {
         inputParticipants(scanner);
+        registerCar(stringArrayParticipants);
     }
 
     private void inputParticipants(Scanner scanner) {
         stringArrayParticipants = scanner.nextLine().split(DELIMITER);
         participantsTotal = stringArrayParticipants.length;
-        registerCar(stringArrayParticipants);
+        validateInput(stringArrayParticipants);
+    }
+
+    private void validateInput(String[] stringArrayParticipants) {
+        for (String participants : stringArrayParticipants) {
+            validateIndividualInputLength(participants);
+        }
+    }
+
+    private void validateIndividualInputLength(String participants) {
+        if (participants.length() > LENGTH_LIMIT_OF_INDIVIDUAL_INPUT) {
+            throw new IllegalArgumentException(MESSAGE_LIMIT_EXCESS);
+        }
     }
 
     private void registerCar(String[] stringArrayParticipants) {
