2be95e432b45f3d801ef48fb567901c43774cd08
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-02 22:15:23 +0900

    docs(README.md): added comments about the case when input for car names is formed with consecutive (,)
    e.g. input case like (,,,,) will be ignored and considered as no names are provided

--- src/main/java/racingcar/GameSetting.java
+++ src/main/java/racingcar/GameSetting.java
@@ -5,26 +5,31 @@ import java.util.stream.Collectors;
 
 public class GameSetting {
     // CAR NAME INPUT
+    private static final int MINIMUM_NUMBER_OF_CARS_REQUIRED = 1;
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String DUPLICATE_CAR_NAME_INPUT_ERROR_MESSAGE = "[ERROR] 차 이름이 중복됩니다.";
     private static final String CAR_NAME_LENGTH_OVER_FIVE_INPUT_ERROR_MESSAGE = "[ERROR] 차 이름은 5자 이하여야 합니다.";
-    private static final String NUMBER_OF_CARS_INPUT_ERROR_MESSAGE = "[ERROR] 차 이름이 2개 이상 필요합니다.";
-    private static final int MINIMUM_NUMBER_OF_CARS_REQUIRED = 2;
+    private static final String CAR_NAME_LENGTH_UNDER_ONE_INPUT_ERROR_MESSAGE = "[ERROR] 차 이름은 1자 이상이어야 합니다.";
+    private static final String NUMBER_OF_CARS_INPUT_ERROR_MESSAGE = "[ERROR] 차 이름이 적어도 " + MINIMUM_NUMBER_OF_CARS_REQUIRED + "개 필요합니다.";
 
     // NUMBER OF ROUNDS INPUT
     private static final String NUMBER_OF_ROUNDS_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final String NON_INTEGER_NUMBER_OF_ROUNDS_INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 정수여야 한다.";
-    private static final String NEGATIVE_NUMBER_OF_ROUNDS_INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 정수여야 한다.";
+    private static final String NON_INTEGER_NUMBER_OF_ROUNDS_INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 정수여야 합니다.";
+    private static final String NEGATIVE_NUMBER_OF_ROUNDS_INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 양수여야 합니다.";
 
     private static void checkCarNameInputValidity(String[] carNames) throws IllegalArgumentException{
-        // at least 2 names required + names must be unique + name length less than 5
+        // at least 1 name required + names must be unique + name length less than 5
         Set<String> carNameSet = new HashSet<>();
         if (carNames.length < MINIMUM_NUMBER_OF_CARS_REQUIRED) {
             throw new IllegalArgumentException(NUMBER_OF_CARS_INPUT_ERROR_MESSAGE);
         }
         for (String carName : carNames) {
+            System.out.println(carName);
             if (carNameSet.contains(carName)) {
                 throw new IllegalArgumentException(DUPLICATE_CAR_NAME_INPUT_ERROR_MESSAGE);
+            }
+            if (carName.length() < 1) {
+                throw new IllegalArgumentException(CAR_NAME_LENGTH_UNDER_ONE_INPUT_ERROR_MESSAGE);
             }
             if (5 < carName.length()) {
                 throw new IllegalArgumentException(CAR_NAME_LENGTH_OVER_FIVE_INPUT_ERROR_MESSAGE);
