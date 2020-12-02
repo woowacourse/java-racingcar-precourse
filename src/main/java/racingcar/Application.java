5f2f22aa19df905d6c71f900e932ed27cfb80856
SiHyung92 <fjzjqhdl@gmail.com>
2020-12-02 19:08:23 +0900

    feat : 유저 입력에 대한 유효성 검사 구현 및 검사 항목 추가
    - Car.java의 이름 유효성 검사 - 이름 최대길이, 공백 입력, 최대 인원 검사
    - 중복이름 검사 문서 기능목록에 추가 및 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,7 @@ package racingcar;
 
 import view.ConsoleView;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
     private static final String REQUEST_INPUT_RACING_CAR_MSG = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
@@ -21,12 +19,43 @@ public class Application {
         ConsoleView view = new ConsoleView(scanner);
         view.println(REQUEST_INPUT_RACING_CAR_MSG);
         String userInput = view.inputNextLine();
+
+        cars = makeCarList(userInput);
+    }
+
+    private List<Car> makeCarList(String userInput) {
         String[] carNames = userInput.split(COMMA);
 
-        cars = new ArrayList<>();
+        validateCarNamesSize(carNames);
+        validateDuplicateCarNames(carNames);
 
+        List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
+            validateCarNameFormat(carName);
             cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+    private void validateCarNameFormat(String carName) {
+        if(carName.isEmpty()){
+            throw new IllegalArgumentException("[ERROR] 공백 이름은 사용할 수 없습니다.");
+        }
+        if(carName.length() > 5){
+            throw new IllegalArgumentException("[ERROR] 사용자의 아이디는 5글자 이하여야 합니다.");
+        }
+    }
+
+    private void validateDuplicateCarNames(String[] carNames) {
+        Set<String> duplicateCheckSet = new HashSet<>(Arrays.asList(carNames));
+        if (duplicateCheckSet.size() != carNames.length) {
+            throw new IllegalArgumentException("[ERROR] 중복된 사용자 이름입니다.");
+        }
+    }
+
+    private void validateCarNamesSize(String[] carNames) {
+        if (carNames.length > 5) {
+            throw new IllegalArgumentException("[ERROR] 인원숫자를 확인해주십시오. ( 5명 이하)");
         }
     }
 }
