c487d1b72a063b1833a3ab2f5c31f9d2b99608d8
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-12-02 22:07:57 +0900

    feat: 자동차 객체 구현 및 이동조건 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,9 @@
 package racingcar;
 
-public class Car {
+public class Car implements Comparable<Car>{
+    private static final String MOVE_REPRESENT="-";
+    private static final Integer MOVE_LIMIT=4;
+
     private final String name;
     private int position = 0;
 
@@ -9,4 +12,32 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public void move(){
+        position++;
+    }
+
+    boolean isSatisfiedByNumber(int number){
+        return number>=MOVE_LIMIT;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        return Integer.compare(car.position,this.position);
+    }
+
+    public void printCurrentPosition(){
+        System.out.print(String.format("%s : ",name));
+        for(int i=0;i<position;i++){
+            System.out.print(MOVE_REPRESENT);
+        }
+        System.out.println();
+    }
 }
