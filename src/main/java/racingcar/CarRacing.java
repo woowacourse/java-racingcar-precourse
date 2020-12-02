f68416311395fac979ee1bb016f78918f17f62c7
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-02 17:30:03 +0900

    feat CarRacing 클래스 생성 및 게임 실행 메서드 추가

---
+++ src/main/java/racingcar/CarRacing.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class CarRacing {
+    private final Scanner scanner;
+
+    public CarRacing(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        
+    }
+}
