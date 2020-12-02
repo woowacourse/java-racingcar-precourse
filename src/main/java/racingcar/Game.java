387d0844115244a52d6da11b36bdb71ce4fb3c2c
Wannte <T.wannte@gmail.com>
2020-12-02 19:58:51 +0900

    feat(Game): enter, 전체 play 로직 구현.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,9 +2,29 @@ package racingcar;
 
 import utils.RandomUtils;
 
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 public class Game {
     private final int MIN_RANDOM_NUMBER = 0;
     private final int MAX_RANDOM_NUMBER = 9;
+    private ArrayList<Car> cars;
+
+    public Game() {
+        cars = new ArrayList<>();
+    }
+
+    public void enter(Car car) {
+        cars.add(car);
+    }
+
+    public void play(int round) {
+        IntStream.range(0,round).forEach(i -> playSingleRound(cars));
+    }
+
+    private void playSingleRound(ArrayList<Car> cars) {
+        cars.forEach(this::playSingleRound);
+    }
 
     private void playSingleRound(Car car) {
         if (randomGameNumber() <= 4) {
