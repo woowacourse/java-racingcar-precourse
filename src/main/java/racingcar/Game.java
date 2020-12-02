b00190957c1da65af256696f3fba49a7a99ed256
Eugine Park <pkeugine@gmail.com>
2020-12-02 17:20:02 +0900

    feat(Game): validate for at least one car name
    - add OutputView class for error printing
    - when error is found, ask for user input again

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,10 +1,13 @@
 package racingcar;
 
 import view.InputView;
+import view.OutputView;
 
 public class Game {
 
     public static final String NAME_SEPARATOR = ",";
+    public static final int MINIMUM_NAME_LENGTH = 1;
+    public static final int MAXIMUM_NAME_LENGTH = 5;
 
     private final InputView inputView;
 
@@ -22,5 +25,28 @@ public class Game {
 
     private void createPlayingCars() {
         String rawCarNames = inputView.getCarNames();
+        try {
+            validate(rawCarNames);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e);
+            createPlayingCars();
+        }
+    }
+
+    private void validate(String rawCarNames) {
+        if (hasNoName(rawCarNames)) {
+            throw new IllegalArgumentException("[ERROR] 최소 하나의 자동차 이름을 입력해야한다.");
+        }
+        if (hasNameExceedingLength(rawCarNames)) {
+            throw new IllegalArgumentException("[ERROR] 이름은 5글자 내로 만들어야한다.");
+        }
+    }
+
+    private boolean hasNoName(String rawCarNames) {
+        return rawCarNames.length() == 0;
+    }
+
+    private boolean hasNameExceedingLength(String rawCarNames) {
+        return false;
     }
 }
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,8 @@
+package view;
+
+public class OutputView {
+
+    public static void printError(Exception e) {
+        System.out.println(e.getMessage());
+    }
+}
