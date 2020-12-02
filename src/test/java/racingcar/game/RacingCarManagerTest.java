53751fca4734bd802aac4b66338778876d7000a6
kihwankim <clack2933@gmail.com>
2020-12-02 23:54:13 +0900

    test: 최종 승리자 검색 테스트 코드 추가

--- src/test/java/racingcar/game/RacingCarManagerTest.java
+++ src/test/java/racingcar/game/RacingCarManagerTest.java
@@ -1,8 +1,11 @@
 package racingcar.game;
 
 import org.junit.jupiter.api.Test;
+import utils.CarMovingFlagUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,5 +40,74 @@ class RacingCarManagerTest {
 
         // then
         assertThat(racingCarManager.numberOfRacingCar() == 2).isTrue();
+    }
+
+    @Test
+    void 첫_번째_위치_최종_승리자_거리_검증() {
+        // given
+        final int stopFlagNumber = 1;
+        final int forwardFlagNumber = 5;
+
+        List<Integer> flagNumberList = new ArrayList<>();
+        flagNumberList.add(forwardFlagNumber); //foward
+        flagNumberList.add(stopFlagNumber); // stop
+
+        List<String> nameList = new ArrayList<>(2);
+        nameList.add("kkh");
+        nameList.add("pobi");
+
+        // when
+        RacingCarManager racingCarManager = new RacingCarManager(nameList);
+        racingCarManager.updateRacingCarPosition(CarMovingFlagUtils.makeCarMovieFlagList(flagNumberList));
+
+        // then
+        assertThat(racingCarManager.getNowWinnerPlayersName()).isEqualTo(Collections.singletonList("kkh"));
+    }
+
+    @Test
+    void 두_번째_위치_최종_승리자_거리_검증() {
+        // given
+        final int stopFlagNumber = 1;
+        final int forwardFlagNumber = 5;
+
+        List<Integer> flagNumberList = new ArrayList<>();
+        flagNumberList.add(stopFlagNumber); // stop
+        flagNumberList.add(forwardFlagNumber); //foward
+
+        List<String> nameList = new ArrayList<>(2);
+        nameList.add("kkh");
+        nameList.add("pobi");
+
+        // when
+        RacingCarManager racingCarManager = new RacingCarManager(nameList);
+        racingCarManager.updateRacingCarPosition(CarMovingFlagUtils.makeCarMovieFlagList(flagNumberList));
+
+        // then
+        assertThat(racingCarManager.getNowWinnerPlayersName()).isEqualTo(Collections.singletonList("pobi"));
+    }
+
+    @Test
+    void 두_명의_최종_승리자_거리_검증() {
+        // given
+        final int forwardFlagNumber = 5;
+
+        List<Integer> flagNumberList = new ArrayList<>();
+        flagNumberList.add(forwardFlagNumber); // forward
+        flagNumberList.add(forwardFlagNumber); //foward
+
+        List<String> nameList = new ArrayList<>(2);
+        nameList.add("kkh");
+        nameList.add("pobi");
+
+        // when
+        RacingCarManager racingCarManager = new RacingCarManager(nameList);
+        racingCarManager.updateRacingCarPosition(CarMovingFlagUtils.makeCarMovieFlagList(flagNumberList));
+
+        // then
+        List<String> resultWinnerList = racingCarManager.getNowWinnerPlayersName();
+        Collections.sort(resultWinnerList);
+        List<String> expectedWinnerList = Arrays.asList("kkh", "pobi");
+        Collections.sort(expectedWinnerList);
+        assertThat(resultWinnerList).isEqualTo(expectedWinnerList);
     }
 }
