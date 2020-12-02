71d5ae7c320c87ee3e8a98bdd627bd74ab21313d
김주원 <kjw11077@naver.com>
2020-12-02 16:46:10 +0900

    feat(class): add class CarRacingGame

---
+++ src/main/java/racingcar/CarRacingGame.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class CarRacingGame {
+
+    public void play() {
+        // todo 경주 할 자동차 이름 입력(이름은 쉼표(,) 기준으로 구분)
+        // todo 시도할 회수 입력
+        // todo 각 차수별 실행 결과
+
+        // todo 우승자 안내 문구
+        // 단독
+        // 최종 우승자: pobi
+        // 공동
+        // 최종 우승자: pobi, jun
+    }
+
+}
