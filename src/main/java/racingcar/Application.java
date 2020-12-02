4b7a5945fe2c69dcfa74ccb894dd25a9482345a1
Eugine Park <pkeugine@gmail.com>
2020-12-02 16:30:25 +0900

    feat(Game): begin the game when program starts

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,11 @@ package racingcar;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        Game racingCarGame = new Game();
+
+        racingCarGame.play();
     }
 }
---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class Game {
+
+    public Game() {
+
+    }
+
+    public void play() {
+        initialize();
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+    }
+
+    private void initialize() {
+
+    }
+}
