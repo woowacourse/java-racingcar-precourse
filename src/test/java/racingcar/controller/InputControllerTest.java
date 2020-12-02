e935520c2d58a36a622594e643535fbcf16650c6
doorisopen <audauddl2@gmail.com>
2020-12-02 21:53:52 +0900

    test: 자동차 이름의 길이가 5자 이하인지 검증한다

---
+++ src/test/java/racingcar/controller/InputControllerTest.java
@@ -0,0 +1,24 @@
+package racingcar.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InputControllerTest {
+
+    @Test
+    public void 자동차_이름_길이가_5_이하이다() throws Exception {
+        //given
+        List<String> participants = Arrays.asList("java","c++","react");
+        int nameMaxLength = 5;
+        //when
+        boolean isCarNameLengthLT5 = participants.stream()
+                .filter(name -> name.length() <= nameMaxLength)
+                .count() == participants.size();
+        //then
+        assertThat(isCarNameLengthLT5).isTrue();
+    }
+}
