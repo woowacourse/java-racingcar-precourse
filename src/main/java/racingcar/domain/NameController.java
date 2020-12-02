c12c3344442405dd0ce9f6e85a29cec0d404cade
Minjeong <mathmjseo@naver.com>
2020-12-03 00:05:09 +0900

    feat: 자동차의 이름이 5자 이하인지 확인하는 기능 구현

--- src/main/java/racingcar/domain/NameController.java
+++ src/main/java/racingcar/domain/NameController.java
@@ -1,12 +1,21 @@
 package racingcar.domain;
 
 public class NameController {
+    private final static int MAX_LEN_OF_NAME = 5;
+
     private NameController() {
 
     }
 
-    public String[] splitNamebyComma(String name){
+    public String[] splitNamebyComma(String name) {
         String[] names = name.split(",");
         return names;
+    }
+
+    public boolean checkLengthOfName(String name) {
+        if (name.length() <= MAX_LEN_OF_NAME) {
+            return true;
+        }
+        return false;
     }
 }
