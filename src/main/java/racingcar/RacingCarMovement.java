b3b498e3e013ec05c6c40050502f4fe035aa5c09
Jun-Space <tjrwns126@gmail.com>
2020-12-02 20:52:05 +0900

    feat(RacingCarMovement): 차가 움직이는 기능 구현

---
+++ src/main/java/racingcar/RacingCarMovement.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import utils.RandomUtils;
+
+public class RacingCarMovement {
+
+    public static boolean moveCar() {
+        if(RandomUtils.nextInt(0,9) <= 3) {
+          return false;
+        }
+        return true;
+    }
+}
