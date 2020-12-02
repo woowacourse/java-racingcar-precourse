a2c6e72268eb4e0f470789970e047626ca2c6952
Eugine Park <pkeugine@gmail.com>
2020-12-02 18:09:34 +0900

    feat(PlayingCars): add collection PlayingCars
    keep track of cars in the game

---
+++ src/main/java/racingcar/PlayingCars.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.List;
+
+public class PlayingCars {
+
+    private final List<Car> cars;
+
+    public PlayingCars(List<Car> cars) {
+        this.cars = cars;
+    }
+}
