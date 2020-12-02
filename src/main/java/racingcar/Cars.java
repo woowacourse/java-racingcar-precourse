5d052c556a7b2cc787ceaacea2b315779313da50
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-12-02 22:10:32 +0900

    feat: 자동차들의 조건에따른 이동기능 구현

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,43 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private static final String WINNER_TEXT="최종 우승자: ";
+    private static final String NON_ANSWER="[ERROR] 해당 값이 존재하지 않습니다.";
+    private List<Car> cars;
+
+    public Cars(){
+        cars=new ArrayList<>();
+    }
+
+    public void add(Car car){
+        cars.add(car);
+    }
+
+    public void go(){
+        cars.stream()
+                .filter(car->car.isSatisfiedByNumber(RandomNumberGenerator.generate()))
+                .forEach(Car::move);
+    }
+
+    public void printWinner(){
+        System.out.print(WINNER_TEXT);
+        StringBuilder sb=new StringBuilder();
+        List<Car> sortedCars=cars.stream().sorted().collect(Collectors.toList());
+        Car farthestCar=sortedCars.get(0);
+        for(int i=0;i<sortedCars.size();i++){
+            if(sortedCars.get(i).getPosition()!=farthestCar.getPosition())break;
+            sb.append(sortedCars.get(i).getName()).append(", ");
+        }
+        System.out.println(sb.toString().substring(0,sb.length()-2));
+    }
+
+    public void printAllCarPosition(){
+        cars.forEach(Car::printCurrentPosition);
+        System.out.println();
+    }
+}
