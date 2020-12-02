b451aa85c76fe35b2e15942a56a5a6c9fee53452
Taehwa Lee <xoghk0321@gmail.com>
2020-12-03 00:50:48 +0900

    feat: 메인 실행 로직을 구현한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,39 @@
 package racingcar;
 
 import java.util.Scanner;
+import racingcar.domain.race.Race;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        InputView inputView = InputView.of(scanner);
+        OutputView outputView = OutputView.basic();
+
+        start(inputView, outputView);
+    }
+
+    private static void start(InputView inputView, OutputView outputView) {
+        outputView.printInputCarNamesMessage();
+        String carNamesStr = inputView.readCarNames();
+
+        outputView.printInputMovingCountMessage();
+        int movingCount = inputView.readMovingCount();
+
+        outputView.printResultMessage();
+        Race race = Race.of(carNamesStr, movingCount);
+        outputView.printRaceTrack(race);
+        race(race, outputView);
+        outputView.printWinnerCars(race);
+    }
+
+    private static void race(Race race, OutputView outputView) {
+        while (!race.isComplete()) {
+            race.move();
+            outputView.printRaceTrack(race);
+            outputView.println();
+        }
     }
 }
