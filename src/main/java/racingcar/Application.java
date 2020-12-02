25e6a2c364fab072cc34949a047cea9413e16fbd
SiHyung92 <fjzjqhdl@gmail.com>
2020-12-02 18:42:21 +0900

    feat : 유저가 입력한 자동차 이름들을 쉼표를 구분자로 분할하는 기능 추가
    - Application.java에 Car.java의 컬렉션인 cars를 List로 선언
    - 사용자가 입력한 이름들을 인자로 Car 객체 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,15 @@
 package racingcar;
 
 import view.ConsoleView;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     private static final String REQUEST_INPUT_RACING_CAR_MSG = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String COMMA = ",";
+    private List<Car> cars;
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
@@ -15,8 +20,13 @@ public class Application {
     private void start(Scanner scanner) {
         ConsoleView view = new ConsoleView(scanner);
         view.println(REQUEST_INPUT_RACING_CAR_MSG);
-        String cars = view.inputNextLine();
+        String userInput = view.inputNextLine();
+        String[] carNames = userInput.split(COMMA);
+
+        cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
     }
-
-
 }
