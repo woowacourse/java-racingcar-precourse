d99f5d8ecd0f46cdfc4ef24a83a4d9c0c3a197ac
JinseongHwang <eddy5360@naver.com>
2020-12-02 19:25:19 +0900

    test: use split() in String class

--- src/test/java/Split.java
+++ src/test/java/Split.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Split {
+public class Split {
+
+    public static void main(String[] args) {
+        String names = "pobi,woni,jun";
+        String[] splitName = names.split(",");
+
+        for (String name : splitName) {
+            System.out.println(name);
+        }
+    }
 }
