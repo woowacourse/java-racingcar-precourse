61d959397e5194061433102ec1b881eeb29b68ce
Minjeong <mathmjseo@naver.com>
2020-12-03 21:37:13 +0900

    docs: README 기능 목록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,5 +10,6 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         Game.run(scanner);
+        scanner.close();
     }
 }
