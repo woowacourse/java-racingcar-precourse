3a2aabf89c00216a12a93dc591b44bed16952df4
김주원 <kjw11077@naver.com>
2020-12-03 15:38:04 +0900

    feat(Race): update startRace

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -4,6 +4,15 @@ public class Race {
 
     public static GameResult startRace() {
         // todo 각 차수별 실행 결과 구하기
-        return new GameResult();
+        List<Car> winners = new ArrayList<>();
+
+        System.out.println("%n실행 결과");
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : cars) {
+
+            }
+        }
+
+        return new Winners(winners);
     }
 }
