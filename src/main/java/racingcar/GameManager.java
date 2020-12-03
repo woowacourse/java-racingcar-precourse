4deefe63f82a57b7ac92721a1b4910cf2e97f2f9
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 18:55:16 +0900

    feat: 앞으로 갈지 여부를 랜덤으로 구해서 판단하는 기능 추가

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,16 +1,25 @@
 package racingcar;
 
+import utils.RandomUtils;
 import view.InputView;
 import view.OutputView;
 
 import java.util.Scanner;
 
 public class GameManager {
+    private static final int RANDOM_START_VALUE = 0;
+    private static final int RANDOM_END_VALUE = 10;
+    private static final int GO_FORWARD_VALUE = 4;
+
     private Cars cars;
 
     public void createCar(Scanner scanner) {
         OutputView.printInputCarName();
 
         cars = new Cars(InputView.inputPlayerName(scanner));
+    }
+
+    private boolean isGoForward() {
+        return RandomUtils.nextInt(RANDOM_START_VALUE, RANDOM_END_VALUE) >= GO_FORWARD_VALUE;
     }
 }
