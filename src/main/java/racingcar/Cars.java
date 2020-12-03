5f50e10d97c1456892392bb7bedb7434d691ff3a
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 19:34:40 +0900

    feat: 게임 승리자의 이름을 출력하는 기능 추가
    - 최대 이동거리 구하는 기능 추가
    - 최대로 이동한 이름을 구하는 기능 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -4,6 +4,7 @@ import utils.RandomUtils;
 import view.OutputView;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class Cars {
@@ -49,5 +50,25 @@ public class Cars {
         for (Car car : cars) {
             OutputView.printRaceResultEachCar(car.getName(), car.getMoveSign());
         }
+    }
+    
+    public void showRaceResult() {
+        OutputView.printRaceResult();
+        OutputView.printWinner(getWinner());
+    }
+
+    public List<String> getWinner() {
+        return getWinnerName(getMaxPosition());
+    }
+
+    private int getMaxPosition() {
+        return cars.stream().mapToInt(Car::getPosition).max().orElseThrow(NoSuchElementException::new);
+    }
+
+    private List<String> getWinnerName(int maxPosition) {
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 }
