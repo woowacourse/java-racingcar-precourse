0dd47ffcae132ae62e92c88b5f8b3c454127f165
김주원 <kjw11077@naver.com>
2020-12-03 14:00:37 +0900

    feat(GameResult): add method printWinners
    우승자 출력

--- src/main/java/racingcar/GameResult.java
+++ src/main/java/racingcar/GameResult.java
@@ -1,5 +1,23 @@
 package racingcar;
 
+import java.util.List;
+
 public class GameResult {
+
+    List<Car> winners;
+
+    public void printWinners() {
+        // todo 우승자 안내 문구
+        // 단독
+        // 최종 우승자: pobi
+        // 공동
+        // 최종 우승자: pobi, jun
+        StringBuilder result = new StringBuilder();
+
+
+
+        System.out.println(result);
+
+    }
 
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -21,30 +21,22 @@ public class RacingGame {
         tryCount = getTryCount();
 
         GameResult gameResult = Race.startRace();
-
-        // todo 우승자 안내 문구
-        // 단독
-        // 최종 우승자: pobi
-        // 공동
-        // 최종 우승자: pobi, jun
     }
 
     private int getTryCount() {
-        // todo 시도할 회수 입력
         int tryCount = 0;
 
         while (tryCount <= 0) {
             try {
                 tryCount = Integer.parseInt(getInput());
             } catch (Exception e) {
-                System.out.println("다시입력");
+                System.out.println("[ERROR] 다시 입력");
             }
         }
         return tryCount;
     }
 
     private List<Car> getCars(String input) {
-        // todo 경주 할 자동차 이름 입력(이름은 쉼표(,) 기준으로 구분)
         return Arrays.stream(input.split(","))
                 .map(Car::new)
                 .collect(Collectors.toList());
