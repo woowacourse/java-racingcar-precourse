c37be03a5f9dfbfe95f98fa1ffbd8cc1ade6012e
PaperCrafter <leejonghee342@gmail.com>
2020-12-03 12:00:52 +0900

    feat(Car): add toString method for displaying distance
    added toString
    - CarToStringUtil: utilty class containing constant used for displaying single car distnace
    - toString: return string thant mathes with specific format

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,27 @@
-package racingcar;
+package racingcar.domain;
+
+import java.util.Collections;
 
 public class Car {
     private final String name;
     private int position = 0;
+    private Engine engine = new Engine();
 
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append(CarToStringUtil.colonWithSpace);
+        sb.append(Collections.nCopies(position, CarToStringUtil.distanceIndicator));
+        return sb.toString();
+    }
+
+    private class CarToStringUtil{
+        public static final String colonWithSpace = " : ";
+        public static final String distanceIndicator = "-";
+    }
 }
