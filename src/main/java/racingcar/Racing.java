7a02df53d3b8218da06466f947950947e637cb85
483759 <483759@naver.com>
2020-12-03 12:12:31 +0900

    feat(Racing): implement inputCarsNameList

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class Racing {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        String[] nameList=inputCarsNameList(scanner);
         // TODO 구현 진행
     }
 
+    /* 자동차들의 입력받아 쉼표로 구분하여(,) 반환하는 메소드 */
+    public static String[] inputCarsNameList(Scanner scanner){
+        String namesInput;
+        String[] nameList;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        namesInput=scanner.nextLine();
+        nameList=namesInput.split(",");
+        return nameList;
+    }
 }
