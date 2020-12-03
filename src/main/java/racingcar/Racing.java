d6469f7d9e8265adb20321b0d98c89df24fbb751
483759 <483759@naver.com>
2020-12-03 12:31:23 +0900

    feat(Racing): implement isValidNames

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -12,11 +12,29 @@ public class Racing {
     /* 자동차들의 입력받아 쉼표로 구분하여(,) 반환하는 메소드 */
     public static String[] inputCarsNameList(Scanner scanner){
         String namesInput;
-        String[] nameList;
+        String[] nameList=null;
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        namesInput=scanner.nextLine();
-        nameList=namesInput.split(",");
+        try{
+            namesInput=scanner.nextLine();
+            nameList=namesInput.split(",");
+            if(isValidNames(nameList)==false){
+                throw new IllegalArgumentException();
+            }
+        }catch(IllegalArgumentException e){
+            System.out.println("[ERROR] 이름은 5자리 이하 문자를 입력해주세요.");
+            return inputCarsNameList(scanner);
+        }
         return nameList;
+    }
+
+    /* 5자리를 초과하거나 빈 문자가 있는지 검사하는 메소드 */
+    public static boolean isValidNames(String[] nameList){
+        for(int i=0;i<nameList.length;i++){
+            if((nameList[i].length()>5)||(nameList[i].indexOf(" ")>-1)||nameList[i].length()==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
