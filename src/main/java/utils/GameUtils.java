b79e0cd2730bd7b09f030e7d11424f3f0b801318
이동건 <tnpdlzi@naver.com>
2020-12-03 15:58:28 +0900

    feat(GameUtils): 게임 유틸 틀 작성

---
+++ src/main/java/utils/GameUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class GameUtils {
+
+    public GameUtils() {
+    }
+
+    public Boolean isCarNameRight() {
+        return true;
+    }
+
+    public Boolean isNumOfRunsRight() {
+        return true;
+    }
+}
