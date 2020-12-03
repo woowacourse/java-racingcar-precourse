516bed02f1dbdfda2dcd49d41afcc5f13b538b49
Livenow14 <xowjd41@naver.com>
2020-12-03 14:26:16 +0900

    feat: 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,15 @@
 package racingcar;
 
+import utils.InputUtils;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        InputUtils inputUtils = InputUtils.of(scanner);
+        String carName = inputUtils.inputAndReturnCarName();
+        System.out.println("carName = " + carName);
     }
 }
