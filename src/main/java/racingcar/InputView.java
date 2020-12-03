2a1198202a5629bb95b91292f098ea08e63deaed
Wannte <T.wannte@gmail.com>
2020-12-03 13:54:40 +0900

    feat(InputView): Validate 로직 추가, 에러 메세지 따로 분리

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,12 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class InputView {
+    private static final String ERROR_NOT_INTEGER = "[ERROR] 시도 횟수는 숫자여야 한다.";
+    private static final String ERROR_NOT_POSITIVE_INTEGER = "[ERROR] 시도 횟수는 양수여야 한다.";
+    private static final String ERROR_BLANK_INPUT = "[ERROR] 입력값이 공백이면 안됩니다.";
+    private static final String ERROR_ZERO_PLAYER = "[ERROR] 참가자 수는 한명 이상이여야 합니다.";
+    private static final String ERROR_BLANK_NAME = "[ERROR] 이름은 공백이면 안된다.";
+    private static final String ERROR_DUPLICATE_NAME = "[ERROR] 이름이 같은 참가자가 있습니다.";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -11,33 +17,54 @@ public class InputView {
     }
 
     public List<String> getCarNames(){
-        List<String> names = getCarNamesInput();
-        validateNoneEmptyElements(names);
+        List<String> names = getCarNames(scanner.nextLine());
+        validateZeroPlayer(names);
+        validateBlankElements(names);
         validateDuplicate(names);
         return names;
     }
 
-    public int getRoundNumber() {
-        try {
-            return scanner.nextInt();
-        } catch (InputMismatchException e) {
-            throw new IllegalArgumentException("[Error] 시도 횟수는 숫자여야 한다.");
-        }
-    }
-
-    private List<String> getCarNamesInput() {
-        return Arrays.stream(scanner.nextLine().split(","))
+    private List<String> getCarNames(String inputString) {
+        validateBlankInput(inputString);
+        return Arrays.stream(inputString.split(","))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
 
-    private void validateNoneEmptyElements(List<String> names) {
-        names.forEach(this::validateNoneEmpty);
+    public int getRoundNumber() {
+        int number= getRoundNumber(scanner.nextLine());
+        validatePositiveInt(number);
+        return number;
     }
 
-    private void validateNoneEmpty(String name) {
+    private int getRoundNumber(String inputString) {
+        try {
+            return Integer.parseInt(inputString.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_NOT_INTEGER);
+        }
+    }
+
+    private void validateBlankInput(String inputString) {
+        if (inputString.isBlank()) {
+            throw new IllegalArgumentException(ERROR_BLANK_INPUT);
+        }
+    }
+
+    private void validateZeroPlayer(List<String> names) {
+        if (names.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_ZERO_PLAYER);
+        }
+    }
+
+    private void validateBlankElements(List<String> names) {
+        System.out.println(names);
+        names.forEach(this::validateBlankName);
+    }
+
+    private void validateBlankName(String name) {
         if (name.isEmpty()){
-            throw new IllegalArgumentException("[Error] 이름은 공백이면 안된다.");
+            throw new IllegalArgumentException(ERROR_BLANK_NAME);
         }
     }
 
@@ -45,9 +72,15 @@ public class InputView {
         Set<String> set = new HashSet<>();
         for (String name : names) {
             if (set.contains(name)) {
-                throw new IllegalArgumentException("[Error] 이름이 같은 참가자가 있습니다.");
+                throw new IllegalArgumentException(ERROR_DUPLICATE_NAME);
             }
             set.add(name);
+        }
+    }
+
+    private void validatePositiveInt(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException(ERROR_NOT_POSITIVE_INTEGER);
         }
     }
 }
