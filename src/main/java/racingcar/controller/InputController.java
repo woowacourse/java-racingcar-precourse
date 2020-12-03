94a84d95fc1dd56cbb349396f4f322beec738f5f
doorisopen <audauddl2@gmail.com>
2020-12-03 17:22:28 +0900

    feat: 중복된 참여자 이름을 검증한다
    - 중복된 참여자 이름이 있으면 이름 뒤에 번호를 부여한다(ex, "java","java2")

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -1,18 +1,22 @@
 package racingcar.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputController {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String DELIMITER_COMMA = ",";
     private static final int NAME_MAX_LENGTH = 5;
+    private static HashMap<String, Integer> register = new HashMap<>();
 
     public List<String> inputCarName(Scanner scanner) {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         List<String> participants = parseStringToStringList(scanner.nextLine());
+        participants = validateCarNameIsNotDuplicate(participants);
         validateCarNameLengthIs5OrLess(participants);
         validateCarNameIsNotContainBlank(participants);
         return participants;
@@ -41,6 +45,36 @@ public class InputController {
         if (!isCarNameNotContainBlank) {
             throw new IllegalArgumentException("[ERROR] 이름에 공백이 포함되어 있습니다.");
         }
+    }
+
+    private List<String> validateCarNameIsNotDuplicate(List<String> participants) {
+        boolean isCarNameNotDuplicate = participants.stream()
+                .distinct()
+                .count() == participants.size();
+        if (!isCarNameNotDuplicate) {
+            return checkNameDuplicate(participants);
+        }
+        return participants;
+    }
+
+    private List<String> checkNameDuplicate(List<String> participants) {
+        return participants.stream()
+                .map(this::nameGenerator)
+                .collect(Collectors.toList());
+    }
+
+    private String nameGenerator(String name) {
+        int order = 0;
+        if(register.containsKey(name)) {
+            order = register.get(name);
+            register.put(name, order+1);
+            register.put(name+order, 1);
+            name += order;
+        }
+        if(!register.containsKey(name)) {
+            register.put(name, 1);
+        }
+        return name;
     }
 
     private void validateTryCountIsDigit(String input) {
