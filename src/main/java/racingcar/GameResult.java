3c76940aef65108d75a5499f11aef4914a0dbdf7
김주원 <kjw11077@naver.com>
2020-12-03 14:14:49 +0900

    feat(GameResult): update method printWinners

--- src/main/java/racingcar/GameResult.java
+++ src/main/java/racingcar/GameResult.java
@@ -1,23 +1,22 @@
 package racingcar;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameResult {
+
+    public GameResult(List<Car> winners) {
+        this.winners = winners;
+    }
 
     List<Car> winners;
 
     public void printWinners() {
-        // todo 우승자 안내 문구
-        // 단독
-        // 최종 우승자: pobi
-        // 공동
-        // 최종 우승자: pobi, jun
-        StringBuilder result = new StringBuilder();
+        String results = winners.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
 
-
-
-        System.out.println(result);
-
+        System.out.println(results);
     }
 
 }
