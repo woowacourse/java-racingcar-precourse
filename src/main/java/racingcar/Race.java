7b41c79b4d5eff1543f13db858bedff45997ddeb
bepoz <kangsy763@naver.com>
2020-12-03 23:34:52 +0900

    feat(Race): raceStart 메서드 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
@@ -7,8 +8,17 @@ public class Race {
     private final List<Car> cars;
 
     public Race(List<Car> cars, int attemptNum) {
-        this.cars = cars;
+        this.cars = new ArrayList<>(cars);
         this.attemptNum = attemptNum;
+    }
+
+    public void raceStart() {
+        for (int i = 0; i < attemptNum; i++) {
+            Car car = cars.get(i);
+            carExcel(car);
+            System.out.println();
+        }
+        winnerPrize();
     }
 
 
