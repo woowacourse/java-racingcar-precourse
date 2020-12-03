af3c76313478806d6a01ffaf265726c97b67dfd9
NewWisdom <wlgp2500@gmail.com>
2020-12-03 21:53:21 +0900

    feat(validation-utils): add the skeleton code

---
+++ src/main/java/utils/ValidationUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class ValidationUtils {
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final int MIN_TRY_NUMBER = 1;
+
+    private ValidationUtils() {
+    }
+
+    public static void validateCarName() {
+    }
+
+    public static void validateTryNumber() {
+    }
+}
