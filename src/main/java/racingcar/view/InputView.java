03012d46083e1368230cbea29494be5b6e7ae987
Jack2ee-dev <jack2ee.dev@gmail.com>
2020-12-03 21:37:16 +0900

    refactor(InputView): capitalize class name

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,12 +3,12 @@ package racingcar.view;
 import java.util.List;
 import java.util.Scanner;
 
-public class inputView {
+public class InputView {
     private static final String INPUT_CAR_NAMES_PHRASE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
 
     private Scanner scanner;
 
-    public inputView(Scanner scanner) {
+    public InputView(Scanner scanner) {
         this.scanner = scanner;
     }
 
