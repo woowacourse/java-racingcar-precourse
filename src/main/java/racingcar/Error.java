bbc3cbb1ccea14a2f40db959f52a1fa58cac7fc3
eoc940 <hgk0000@naver.com>
2020-12-04 02:37:02 +0900

    feat(error): 시도 횟수가 숫자가 아닐 때의 에러 메시지를 출력하는 trialNotIntegerError 메서드 구현

--- src/main/java/racingcar/Error.java
+++ src/main/java/racingcar/Error.java
@@ -9,6 +9,7 @@ public class Error {
     static final String SAME_CAR_NAME_ERROR = "자동차 이름이 중복됩니다";
     static final String INVALID_CAR_NAME_LENGTH_ERROR = "자동차 이름은 1자 이상 5자 이하이어야 한다";
     static final String INVALID_TRIAL_NUMBER_ERROR = "1이상의 숫자를 입력하세요";
+    static final String INVALID_TRIAL_TYPE_ERROR = "시도 횟수는 숫자여야 한다";
     static final int MAX_CAR_NAME_LENGTH = 5;
     static final int MIN_CAR_NAME_LENGTH = 1;
 
@@ -39,6 +40,10 @@ public class Error {
             System.out.println(ERROR_PREFIX + INVALID_TRIAL_NUMBER_ERROR);
             throw new IllegalArgumentException();
         }
+    }
+
+    public void trialNotIntegerError() {
+        System.out.println(ERROR_PREFIX + INVALID_TRIAL_TYPE_ERROR);
     }
 
 
