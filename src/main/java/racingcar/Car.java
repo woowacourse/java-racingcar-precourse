c0b35b5cc69471d0a56b7587a591711e4b450fe5
park seok chae <tjrco56@naver.com>
2020-12-03 21:09:09 +0900

    feat(Car): Car 객체의 이름과 위치정보를 문자열로 리턴하는 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,15 @@
 package racingcar;
 
+/**
+ * 자동차 객체를 정의하는 클래스
+ * 기능 1. Car 객체의 멤버 position을 1만큼 증가시키는 기능
+ * 기능 2. 출력 요구사항에 맞춰 Car 객체의 정보를 문자열로 리턴하는 기능
+ */
 public class Car {
+	private static final String SEPERATOR_COLON = " : ";
+	private static final String MOVING_HYPHEN = "-";
+	private static final Integer ZERO = 0;
+
 	private final String name;
 	private int position = 0;
 
@@ -10,5 +19,18 @@ public class Car {
 
 	public void moveForwardByOne() {
 		position++;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(name).append(SEPERATOR_COLON);
+
+		for (int idx = ZERO; idx < position; idx++) {
+			sb.append(MOVING_HYPHEN);
+		}
+
+		return sb.toString();
 	}
 }
