1d20444795154cc4ee67bdec78ce8b2ac2a88903
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-03 15:44:33 +0900

    feat: 자동차 이름 글자수 예외 검사와 에러 메시지 출력 및 재입력 기능 구현
    자동차 이름 글자수가 유효한 범위(1~5)를 벗어난 경우, 사용자 정의 예외를
    호출하고 에러메시지를 출력하고 재입력을 받는다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,16 @@
 package racingcar.view;
 
+import racingcar.domain.exception.CarNameLengthException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
+    private static final String INPUT_CAR_NAMES_NOTICE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COMMA_DELIMITER = ",";
+    private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
 
     private final Scanner scanner;
 
@@ -15,9 +19,36 @@ public class InputView {
     }
 
     public List<String> inputCarNames() {
-        String[] carNames = this.scanner.nextLine()
+        System.out.println(INPUT_CAR_NAMES_NOTICE_MESSAGE);
+        String[] inputCarNames = this.scanner.nextLine()
                 .split(COMMA_DELIMITER);
-        return Arrays.stream(carNames)
+        while (!isValidInputCarNames(inputCarNames)) {
+            inputCarNames = this.scanner.nextLine()
+                    .split(COMMA_DELIMITER);
+        }
+        return Arrays.stream(inputCarNames)
                 .collect(Collectors.toList());
+    }
+
+    private boolean isValidInputCarNames(String[] inputCarNames) {
+        try {
+            validateInputCarNameLength(inputCarNames);
+            return true;
+        } catch (RuntimeException runtimeException) {
+            System.out.println(runtimeException.getMessage());
+            return false;
+        }
+    }
+
+    private void validateInputCarNameLength(String[] inputCarNames) {
+        boolean isAllValidLength = Arrays.stream(inputCarNames)
+                .allMatch(this::isValidInputCarNameLength);
+        if (!isAllValidLength) {
+            throw new CarNameLengthException();
+        }
+    }
+
+    private boolean isValidInputCarNameLength(String inputCarName) {
+        return !inputCarName.isEmpty() && inputCarName.length() <= MAXIMUM_CAR_NAME_LENGTH;
     }
 }
