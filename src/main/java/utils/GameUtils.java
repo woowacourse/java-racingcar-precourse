cd5880b5a524a5d3d92945d4da905893ac4e6f6f
이동건 <tnpdlzi@naver.com>
2020-12-03 16:40:49 +0900

    feat(GameUtils): 자동차 이름이 5글자 이하인가 판별

--- src/main/java/utils/GameUtils.java
+++ src/main/java/utils/GameUtils.java
@@ -1,12 +1,15 @@
 package utils;
 
 public class GameUtils {
+    private static final String NAME_OVER_MESSAGE = "[ERROR] 차 이름은 5글자 이하여야 한다.";
 
-    public GameUtils() {
+    private GameUtils() {
     }
 
-    public Boolean isCarNameRight() {
-        return true;
+    public static void isCarNameRight(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException(NAME_OVER_MESSAGE);
+        }
     }
 
     public Boolean isNumOfRunsRight() {
