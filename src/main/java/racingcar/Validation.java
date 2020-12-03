c1bb7a419e1b948e730f2d2f4ef9231e8e425dbf
KJunseo <max9106@naver.com>
2020-12-04 00:03:54 +0900

    feat: [예외처리 추가] 중복된 차 이름이 존재할 수 없다

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -1,8 +1,12 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Validation {
     public static boolean carNameValidation(String[] cars) {
-        return isNotEmptyName(cars) && isRightLength(cars);
+        return isNotEmptyName(cars) && isRightLength(cars)
+                    && isNonDuplicate(cars);
     }
 
     private static boolean isNotEmptyName(String[] cars) {
@@ -18,6 +22,17 @@ public class Validation {
         for (String carName : cars) {
             if (carName.length() < Constant.MIN_CAR_NAME_LENGTH
                         || carName.length() > Constant.MAX_CAR_NAME_LENGTH) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNonDuplicate(String[] cars) {
+        Set<String> carList = new HashSet<>();
+
+        for (String carName : cars) {
+            if (!carList.add(carName)) {
                 return false;
             }
         }
