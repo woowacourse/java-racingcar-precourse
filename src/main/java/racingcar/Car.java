f44c616f13c37b157f7e959959f7c1eed2317a80
park seok chae <tjrco56@naver.com>
2020-12-03 14:11:31 +0900

    feat(Car): Car 객체가 앞으로 한 칸 이동하는 메소드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,14 @@
 package racingcar;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public void moveForwardByOne() {
+		position++;
+	}
 }
