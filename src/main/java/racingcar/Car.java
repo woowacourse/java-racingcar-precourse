4f627e82195f4a6e6b2f6d26efa098612107e73d
김주원 <kjw11077@naver.com>
2020-12-03 16:37:51 +0900

    feat(Car): add method printStatus, validateNameRange

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,20 @@
 package racingcar;
 
 public class Car {
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateNameRange(name);
         this.name = name;
+    }
+
+    private void validateNameRange(String name) {
+        // 5자 이하만 가능
+        if (5 < name.length()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
@@ -16,4 +25,15 @@ public class Car {
     public void moveForward() {
         position += 1;
     }
+
+    public void printStatus() {
+        StringBuilder progress = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            progress.append("-");
+        }
+
+        System.out.println(name + " : " + progress);
+    }
+
+
 }
