32832ec7bdf768bac4318222c755c7da7e5c8f6f
doorisopen <audauddl2@gmail.com>
2020-12-03 19:26:27 +0900

    feat: 0~9 사이의 random 값을 생성한다

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -2,11 +2,14 @@ package racingcar;
 
 import racingcar.controller.InputController;
 import racingcar.domain.Cars;
+import utils.RandomUtils;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class RacingCarGame {
+    private static final int RANGE_OF_NUMBER_ZERO = 0;
+    private static final int RANGE_OF_NUMBER_NINE = 9;
     private final InputController input;
 
     public RacingCarGame() {
@@ -22,5 +25,9 @@ public class RacingCarGame {
 
     private void playGame(Cars cars, int tryCount) {
 
+    }
+
+    private int getRandomNumber() {
+        return RandomUtils.nextInt(RANGE_OF_NUMBER_ZERO, RANGE_OF_NUMBER_NINE);
     }
 }
