5414557dfb61c6e9dc88e5a01cbe96159a3b8d11
teo.kkim <taehongkim496@gmail.com>
2020-12-03 10:55:52 +0900

    feat : add SysMessages
    - 프로그램에서 사용할 각종 메세지들을 상수값으로 저장하는 클래스

---
+++ src/main/java/utils/SysMessages.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class SysMessages {
+    public static final String carNameErrorMessage = "[ERROR] 자동차 이름은 5자 이하만 가능하다.";
+    public static final String roundNumberErrorMessage = "[ERROR] 시도 횟수는 숫자여야 한다.";
+    public static final String carNameInputMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String roundNumberInputMessage = "시도할 횟수는 몇회인가요?";
+    public static final String winnerPrizeMessage = "최종 우승자: ";
+    public static final String executeMessage = "실행 결과";
+}
