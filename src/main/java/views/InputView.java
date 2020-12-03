0632cdd995f1f275606fc461f57d15ccdb234e78
tomas <cmg14111@gmail.com>
2020-12-03 16:09:10 +0900

    refactor(InputView): 쉼표 상수처리

--- src/main/java/views/InputView.java
+++ src/main/java/views/InputView.java
@@ -8,15 +8,16 @@ public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String EMPTY_INPUT = "";
     private static final String EXCEPTION_MESSAGE = "[ERROR] 공백을 입력할 수 없습니다.";
+    private static final String SEPARATOR = ",";
 
     public Stream<String> inputCarNames(Scanner scanner) {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         String carNames = scanner.nextLine();
         isValidCarNames(carNames);
-        return Arrays.stream(carNames.split(","));
+        return Arrays.stream(carNames.split(SEPARATOR));
     }
 
-    public void isValidCarNames(String carNames) {
+    void isValidCarNames(String carNames) {
         if (carNames.equals(EMPTY_INPUT)) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
