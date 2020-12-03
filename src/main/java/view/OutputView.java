b56aeb4bb8fdfd052dcd20ab033fa8b3f5f2a35a
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 14:49:25 +0900

    feat: 시도할 회수 문구 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void printInputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public static void printInputRaceTime() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
