20c82f3c294581fc46ac240d8cdbb62b6168d35c
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 19:00:47 +0900

    refactor: 앞으로 갈지를 판단하는 기능을 GameManager -> Cars 로 이동

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,12 +1,22 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
+    private static final int RANDOM_START_VALUE = 0;
+    private static final int RANDOM_END_VALUE = 10;
+    private static final int GO_FORWARD_VALUE = 4;
+
     List<Car> cars;
 
     public Cars(List<String> names) {
         cars = names.stream().map(Car::new).collect(Collectors.toList());
+    }
+
+    private boolean isGoForward() {
+        return RandomUtils.nextInt(RANDOM_START_VALUE, RANDOM_END_VALUE) >= GO_FORWARD_VALUE;
     }
 }
--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,16 +1,11 @@
 package racingcar;
 
-import utils.RandomUtils;
 import view.InputView;
 import view.OutputView;
 
 import java.util.Scanner;
 
 public class GameManager {
-    private static final int RANDOM_START_VALUE = 0;
-    private static final int RANDOM_END_VALUE = 10;
-    private static final int GO_FORWARD_VALUE = 4;
-
     private Cars cars;
     private int tryTimes;
 
@@ -22,9 +17,5 @@ public class GameManager {
         OutputView.printInputRaceTime();
 
         tryTimes = InputView.inputTryTimes(scanner);
-    }
-
-    private boolean isGoForward() {
-        return RandomUtils.nextInt(RANDOM_START_VALUE, RANDOM_END_VALUE) >= GO_FORWARD_VALUE;
     }
 }
