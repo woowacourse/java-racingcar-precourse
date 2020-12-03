b025f1f8c569d7e8752d0437c92a184c6cc2167b
tomas <cmg14111@gmail.com>
2020-12-03 15:27:56 +0900

    feat(InputView): 입력받은 문자열을 쉼표로 구분하여 스트림으로 만들어서 반환
    before:
    inputCarName()의 반환형이 String
    
    after:
    Stream<String> 반환

--- src/main/java/views/InputView.java
+++ src/main/java/views/InputView.java
@@ -1,17 +1,19 @@
 package views;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String EMPTY_INPUT = "";
     private static final String EXCEPTION_MESSAGE = "[ERROR] 공백을 입력할 수 없습니다.";
 
-    public String inputCarNames(Scanner scanner) {
+    public Stream<String> inputCarNames(Scanner scanner) {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         String carNames = scanner.nextLine();
         isValidCarNames(carNames);
-        return carNames;
+        return Arrays.stream(carNames.split(","));
     }
 
     public void isValidCarNames(String carNames) {
