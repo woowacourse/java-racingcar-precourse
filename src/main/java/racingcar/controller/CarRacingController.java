a35c4a12e73df91f352888aed143df07f7cf6d31
daeun <leede0418@likelion.org>
2020-12-03 22:13:53 +0900

    feat(car-racing-controller): scan times
    - [사용자] 몇 번의 이동을 할 것인지, 시도할 횟수를 입력하는 기능 구현

--- src/main/java/racingcar/controller/CarRacingController.java
+++ src/main/java/racingcar/controller/CarRacingController.java
@@ -1,7 +1,6 @@
 package racingcar.controller;
 
 import racingcar.Car;
-import racingcar.generator.NumberGenerator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,18 +11,25 @@ import java.util.Scanner;
 public class CarRacingController {
 
     private static final String CAR_NAME_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String CAR_RACING_TIME_TEXT = "시도할 회수는 몇회인가요?";
     private static final String COMMA = ",";
     private static final int MINIMUM_CAR_NAME_LENGTH = 1;
     private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
     private static final String INVALID_CAR_NAME_LENGTH = "[ERROR] 자동차 이름의 길이는 1자 이상 5자 이하여야 합니다.";
     private static final String BLANK = " ";
     private static final String INVALID_CAR_NAME_FORMAT = "[ERROR] 자동차 이름은 공백 없이 입력해주세요.";
+    private static final String INVALID_SCANNER_TYPE = "[ERROR] 시도 횟수는 숫자만 입력 가능합니다.";
+    private static final int ZERO = 0;
+    private static final String INVALID_SCANNER_TIMES = "[ERROR] 시도 횟수는 1 이상이어야 합니다.";
 
     private static ArrayList<Car> cars = new ArrayList<>();
+    private static int times;
 
     public static void startCarRacing(Scanner scanner) {
         System.out.println(CAR_NAME_TEXT);
         scanCars(scanner);
+        System.out.println(CAR_RACING_TIME_TEXT);
+        scanTimes(scanner);
     }
 
     public static void scanCars(Scanner scanner) {
@@ -34,17 +40,36 @@ public class CarRacingController {
         }
     }
 
+    public static void scanTimes(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println(INVALID_SCANNER_TYPE);
+        }
+
+        int scannerTimes = scanner.nextInt();
+        times = validateTimes(scannerTimes);
+    }
+
     public static Car validateCar(String carName) {
         // 자동차 이름의 길이가 1자 미만 또는 5자 초과인 경우
         if (carName.length() < MINIMUM_CAR_NAME_LENGTH || carName.length() > MAXIMUM_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
         }
 
-        // 자동차 이름에 공백이 있는 경우 (ex. ph o)
+        // 자동차 이름에 공백이 있는 경우
         if (carName.contains(BLANK)) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_FORMAT);
         }
 
         return new Car(carName);
+    }
+
+    public static int validateTimes(int scannerTimes) {
+        // 시도할 횟수에 0이 입력되는 경우
+        if (scannerTimes == ZERO) {
+            throw new IllegalArgumentException(INVALID_SCANNER_TIMES);
+        }
+
+        return scannerTimes;
     }
 }
