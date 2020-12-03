c4d27eb18694ce69a6760c0817846c1f1eeccffb
eoc940 <hgk0000@naver.com>
2020-12-03 17:52:23 +0900

    feat(input): makeCarNameArray 메서드 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,4 +1,11 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Input {
+
+    public String[] makeCarNameArray(String inputCars) {
+        String[] carNames = inputCars.split(",");
+        return carNames;
+    }
 }
