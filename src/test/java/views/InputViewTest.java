6c4a0cdf11b7b1685565b4cff86f6ab1b9ebcd1a
tomas <cmg14111@gmail.com>
2020-12-03 18:21:57 +0900

    test(InputViewTest): 라운드 수 입력에 대한 예외 테스트

--- src/test/java/views/InputViewTest.java
+++ src/test/java/views/InputViewTest.java
@@ -5,14 +5,35 @@ import org.junit.jupiter.api.Test;
 
 class InputViewTest {
     private static final String EMPTY_INPUT = "";
-    private static final String EXPECTED_EXCEPTION_MESSAGE = "[ERROR] 공백을 입력할 수 없습니다.";
+    private static final String NOT_NUMBER = "12a";
+    private static final String ZERO = "0";
+    private static final String EMPTY_EXCEPTION = "[ERROR] 공백을 입력할 수 없습니다.";
+    private static final String NOT_NUMBER_EXCEPTION = "[ERROR] 숫자를 입력해야 합니다.";
+    private static final String IMPOSSIBLE_NUMBER_ZERO_EXCEPTION = "[ERROR] 0은 임력할 수 없습니다.";
+
+    InputView inputView = new InputView();
 
     @Test
-    void inputTest() {
-        InputView inputView = new InputView();
+    void isEmptyTest() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            inputView.isValidCarNames(EMPTY_INPUT);
+            inputView.isEmptyValue(EMPTY_INPUT);
         });
-        Assertions.assertEquals(EXPECTED_EXCEPTION_MESSAGE, exception.getMessage());
+        Assertions.assertEquals(EMPTY_EXCEPTION, exception.getMessage());
+    }
+
+    @Test
+    void stringToIntTest() {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.isNumber(NOT_NUMBER);
+        });
+        Assertions.assertEquals(NOT_NUMBER_EXCEPTION, exception.getMessage());
+    }
+
+    @Test
+    void isZeroTest() {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.isZero(ZERO);
+        });
+        Assertions.assertEquals(IMPOSSIBLE_NUMBER_ZERO_EXCEPTION, exception.getMessage());
     }
 }
