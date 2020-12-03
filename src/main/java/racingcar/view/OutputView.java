0f751d9d0ad6c4e2993b03e5c5688dbd967c82cf
PaperCrafter <leejonghee342@gmail.com>
2020-12-03 19:57:01 +0900

    refactor(OutputView): seperate concaternating function from getWinnerLog
    extract concatWinners from getWinnerLog to simplify logic

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,9 +6,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private static final String ERROR_LOG_PREFIX = "[ERROR]";
+    private static final String RACE_START_LOG = "실행 결과";
     private static final String WINNER_LOG_BEGIN = "최종 우승자: ";
     private static final String AND_LOG = ", ";
+
+    public static void printRaceStartLog(){
+        System.out.println(RACE_START_LOG);
+    }
 
     public static void printRaceProgressLog(List<Car> carList){
         for(Car car : carList){
@@ -32,7 +36,7 @@ public class OutputView {
     private static int getMaxPosition(List<Car> carList){
         int maxPosition = 0;
         for(Car car : carList){
-            maxPosition = car.getPosition();
+            maxPosition = Math.max(car.getPosition(), maxPosition);
         }
         return maxPosition;
     }
@@ -40,12 +44,16 @@ public class OutputView {
     private static String getWinnerLog(List<Car> winners){
         StringBuilder sb = new StringBuilder();
         sb.append(WINNER_LOG_BEGIN);
+        concatWinners(winners, sb);
+        return sb.toString();
+    }
+
+    private static void concatWinners(List<Car> winners, StringBuilder sb){
         for(int i = 0; i < winners.size(); i++){
             sb.append(winners.get(i).getName());
             if(i != winners.size() -1){
                 sb.append(AND_LOG);
             }
         }
-        return sb.toString();
     }
 }
