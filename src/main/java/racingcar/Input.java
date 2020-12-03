6dcc3983e71652f4eaaa22617d840fc8e6524a5a
eoc940 <hgk0000@naver.com>
2020-12-03 18:02:49 +0900

    feat(input): 자동차 이름 입력값을 리스트로 반환하는 makeCarNameList 메서드 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Input {
@@ -7,5 +9,14 @@ public class Input {
     public String[] makeCarNameArray(String inputCars) {
         String[] carNames = inputCars.split(",");
         return carNames;
+    }
+
+    public List<String> makeCarNameList(String inputCars) {
+        String[] carNameArray = makeCarNameArray(inputCars);
+        List<String> carNameList = new ArrayList<>(carNameArray.length);
+        for (String carName : carNameArray) {
+            carNameList.add(carName);
+        }
+        return carNameList;
     }
 }
