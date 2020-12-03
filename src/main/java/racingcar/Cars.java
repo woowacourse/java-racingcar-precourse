c99b39c725ffe6a39e443103fe6e04ef680a649d
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 19:07:53 +0900

    feat: 레이스 시도 회수 만큼 레이스를 진행하는 기능 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -19,4 +19,23 @@ public class Cars {
     private boolean isGoForward() {
         return RandomUtils.nextInt(RANDOM_START_VALUE, RANDOM_END_VALUE) >= GO_FORWARD_VALUE;
     }
+
+    // 재귀를 통해 게임 시도 회수 까지 레이스 실행
+    public int doRace(int tryTimes) {
+        if (tryTimes <= 0) {
+            return tryTimes;
+        }
+
+        for (Car car : cars) {
+            applyNewCarPosition(car);
+        }
+
+        return doRace(--tryTimes);
+    }
+
+    public void applyNewCarPosition(Car car) {
+        if (isGoForward()) {
+            car.goForward();
+        }
+    }
 }
