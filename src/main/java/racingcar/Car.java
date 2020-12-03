aa4f42bb0e238840f98bf142adcea41da375e684
Sehwan_Jang <whilter08@gmail.com>
2020-12-03 19:47:33 +0900

    feat(Car): move 기능 구현
    * RandomUtils의 nextInt에 따라 정지 혹은 전진.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +10,10 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public int move() {
+        if (RandomUtils.nextInt(0, 9) > 3) {
+            position++;
+        }
+        return position;
+    }
 }
