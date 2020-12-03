b25c401320045d49e6136f21511490a384bb1ad3
NewWisdom <wlgp2500@gmail.com>
2020-12-04 01:26:11 +0900

    feat(player): add input-try-number and getter method

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -7,25 +7,31 @@ import java.util.Scanner;
 public class Player {
     private final Scanner scanner;
     private String[] carNameArray;
-    private String tryNumber;
+    private int tryNumber;
 
     public Player(Scanner scanner) {
         this.scanner = scanner;
-        this.tryNumber = "";
     }
 
     public void inputCarNameArray() {
         this.carNameArray = splitCarNamesWithComma(scanner.nextLine());
     }
 
-    public String getTryNumber() {
-        this.tryNumber = scanner.nextLine();
-        return tryNumber;
+    public void inputTryNumber() {
+        this.tryNumber = ValidationUtils.validateTryNumber(scanner);
     }
 
     public String[] splitCarNamesWithComma(String carNames) {
         String[] splitCarNameResultArray = carNames.split(",");
         ValidationUtils.validateCarNameArray(splitCarNameResultArray);
         return splitCarNameResultArray;
+    }
+
+    public String[] getCarNameArray() {
+        return carNameArray;
+    }
+
+    public int getTryNumber() {
+        return tryNumber;
     }
 }
