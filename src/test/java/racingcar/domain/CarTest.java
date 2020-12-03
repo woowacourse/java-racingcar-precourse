68940331642eca839914fc90f7abdf46433aafca
PaperCrafter <leejonghee342@gmail.com>
2020-12-04 02:17:47 +0900

    test(CarTest): add some test methods
    add CarTest class move method test
    - carMoveTestOne: test case when engine returns 1;
    - carMoveTestZero: test case when engine returns 0;

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,47 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.reflect.Field;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+class CarTest {
+    @Mock
+    private Engine mockEngine;
+
+    private Car car;
+
+    @BeforeEach
+    public void setup() throws NoSuchFieldException, IllegalAccessException {
+        MockitoAnnotations.openMocks(this);
+        car = new Car("bumble_bee");
+        Field engine = car.getClass().getDeclaredField("engine");
+        engine.setAccessible(true);
+        engine.set(car, mockEngine);
+    }
+
+    @Test
+    public void carMoveTestOne() {
+        //given
+        //when
+        when(mockEngine.run()).thenReturn(1);
+        car.move();
+        //then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void carMoveTestZero() {
+        //given
+        //when
+        when(mockEngine.run()).thenReturn(0);
+        car.move();
+        //then
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
