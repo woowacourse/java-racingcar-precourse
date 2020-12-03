c53b3cbe71b9eb972cf03d88a7957f7532d786b8
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 20:15:34 +0900

    feat: 입력받은 문자열이 숫자인 지 판별하는 기능 추가

--- src/main/java/utils/NumberValidator.java
+++ src/main/java/utils/NumberValidator.java
@@ -2,7 +2,17 @@ package utils;
 
 public class NumberValidator {
     private static final int NON_RACE_VALUE = 0;
+    private static final String RACE_VALUE_NOT_NUMBER_MESSAGE = "[ERROR] 레이스 시도할 회수는 숫자 여야 합니다.";
     private static final String VALUE_OVER_ZERO_MESSAGE = "[ERROR] 게임 시도 회수는 0 보다 커야 합니다.";
+
+    public static void checkNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException(RACE_VALUE_NOT_NUMBER_MESSAGE);
+        }
+    }
 
     public static void checkNumberOverZero(int input) {
         if (input <= NON_RACE_VALUE) {
