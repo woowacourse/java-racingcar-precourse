45607e0cc2748e9d657403f9361916048c9781f7
이동건 <tnpdlzi@naver.com>
2020-12-03 20:41:52 +0900

    feat(RacingGame): 시도 횟수만큼 랜덤 숫자 받아 자동차 전진 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,12 +3,14 @@ package racingcar;
 import utils.GameUtils;
 import utils.RandomUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class RacingGame {
     private Scanner scanner;
-    private List<Car> racingCars;
+    private List<Car> racingCars = new ArrayList<Car>();
     private int numberOfRuns;
     private final int RANDOM_NUM_START = 0;
     private final int RANDOM_NUM_END = 9;
@@ -18,14 +20,21 @@ public class RacingGame {
     }
 
     public void play() {
+        registerCars();
+        getNumberOfRunsFromUser();
+        for (int i = 0; i < numberOfRuns; i++) {
+            moveCars();
+            printCars();
+            System.out.println();
+        }
     }
 
     public void registerCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNames = scanner.next().split(",");
-        for (int i = 0; i < carNames.length; i++) {
-            GameUtils.isCarNameRight(carNames[i]);
-            racingCars.add(new Car(carNames[i]));
+        String[] carNames = scanner.next().split("\\,");
+        for (String carName : carNames) {
+            GameUtils.isCarNameRight(carName);
+            racingCars.add(new Car(carName));
         }
     }
 
@@ -42,12 +51,15 @@ public class RacingGame {
         }
     }
 
+    public void printCars() {
+        for (Car car : racingCars) {
+            System.out.println(car.getName() + " : " + (String.join("", Collections.nCopies(car.getPosition(), "-"))));
+        }
+    }
+
     public void findWinner() {
     }
 
     public void printWinner() {
-    }
-
-    public void printCars() {
     }
 }
