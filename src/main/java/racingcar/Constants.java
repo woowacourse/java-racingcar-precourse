515d264430d01512fe2ff920929de105cc718fe8
platanus <platanus@canxan.com>
2020-12-04 02:04:33 +0900

    refactor(constants): Fix to context

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -5,21 +5,25 @@ public class Constants {
     public static final String RACER_NAME_ANNOUNCEMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String LAP_QUESTION = "시도할 회수는 몇회인가요?";
     public static final String LAP_IS_INTEGER_ANNOUNCEMENT = "0 이상의 수를 입력해 주세요.";
-    public static final char RACER_LIST_FORMAT = ',';
-    public static final int RACER_NAME_LENGTH = 5;
     public static final String STANDING_START_ANNOUNCEMENT = "실행 결과";
+    public static final String EVENT_WINNER_ANNOUNCEMENT = "최종 우승자: ";
+
     public static final String LAP_POSITION_CHARACTER = "-";
     public static final String RACER_POSITION_DIVIDE_CHARACTER = " : ";
-    public static final String EVENT_WINNER_ANNOUNCEMENT = "최종 우승자: ";
     public static final String EVENT_WINNER_FORMAT_CHARACTER = ", ";
+
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String FORMATTER_ERROR_ANNOUNCEMENT = "맨 앞의 이름이 없습니다.";
+    public static final String DUPLICATION_ERROR_ANNOUNCEMENT = "이름이 중복됩니다.";
+    public static final String NAME_OVER_LENGTH_ERROR_ANNOUNCEMENT = "이름이 5자가 넘습니다.";
+
+    public static final char RACER_LIST_FORMAT = ',';
+    public static final int RACER_NAME_LENGTH = 5;
     public static final int GO_FORWARD_MIN_NUMBER = 4;
     public static final int KEEP_POSITION_MAX_NUMBER = 3;
     public static final int MOVING_POSITION_POINT = 1;
     public static final int MIN_RANDOM_RANGE = 0;
     public static final int MAX_RANDOM_RANGE = 9;
-    public static final String ERROR_HEADER = "[ERROR] ";
-    public static final String FORMATTER_ERROR_ANNOUNCEMENT = "맨 앞의 이름이 없습니다.";
-    public static final String DUPLICATION_ERROR_ANNOUNCEMENT = "이름이 중복됩니다. ";
-    public static final String NAME_OVER_LENGTH_ERROR_ANNOUNCEMENT = "이름이 5자가 넘습니다.";
+
 
 }
--- src/main/java/racingcar/MessageUtils.java
+++ src/main/java/racingcar/MessageUtils.java
@@ -3,8 +3,8 @@ package racingcar;
 public class MessageUtils {
 
     public static void errorExceptionThrow(String messageConstant) {
-        System.out.println(Constants.ERROR_HEADER + messageConstant);
-        throw new IllegalArgumentException(Constants.ERROR_HEADER + messageConstant);
+        System.out.println(Constants.ERROR_PREFIX + messageConstant);
+        throw new IllegalArgumentException(Constants.ERROR_PREFIX + messageConstant);
     }
 
     public static void printMessageWithConstant(String[] messages) {
