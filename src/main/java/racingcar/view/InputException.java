d61fbf8638d5b30276cf247db12ea2c19818164f
Jack2ee-dev <jack2ee.dev@gmail.com>
2020-12-03 23:47:29 +0900

    feat(InputView, InputException): print trial input(function 3) & get trial input and handle exception(function 4)

--- src/main/java/racingcar/view/InputException.java
+++ src/main/java/racingcar/view/InputException.java
@@ -2,17 +2,20 @@ package racingcar.view;
 
 public class InputException {
     private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String NO_INPUT_ERROR_MESSAGE = "자동차 이름을 입력해주세요.\n";
-    private static final String LONGER_THAN_FIVE_CHARACTER_ERROR_MESSAGE = "각 자동차 이름은 5자를 초과할 수 없습니다.\n";
-    private static final String INCLUDE_BLANK_BACK_AND_FORTH_ERROR_MESSAGE = "자동차 이름 앞 뒤에 공백이 포함될 수 없습니다.\n";
-    private static final String BLANK_NEXT_TO_REST_ERROR_MESSAGE = "쉼표 뒤에는 공백이 아니어야 합니다.\n";
+    private static final String CAR_NAMES_NO_INPUT_ERROR_MESSAGE = "자동차 이름을 입력해주세요.\n";
+    private static final String CAR_NAMES_LONGER_THAN_FIVE_CHARACTER_ERROR_MESSAGE = "각 자동차 이름은 5자를 초과할 수 없습니다.\n";
+    private static final String CAR_NAMES_INCLUDE_BLANK_BACK_AND_FORTH_ERROR_MESSAGE = "자동차 이름 앞 뒤에 공백이 포함될 수 없습니다.\n";
+    private static final String CAR_NAMES_BLANK_NEXT_TO_REST_ERROR_MESSAGE = "쉼표 뒤에는 공백이 아니어야 합니다.\n";
+    private static final String TRIAL_NUMBER_NO_INPUT_ERROR_MESSAGE = "시도 횟수는 공백이 아니어야 합니다.\n";
+    private static final String TRIAL_NUMBER_NOT_INTEGER_ERROR_MESSAGE = "시도 횟수는 숫자 형식이어야 합니다.\n";
+
     public static final String SPLIT_REGEX = ",";
 
     public InputException() {}
 
-    public boolean invoke(String input) {
+    public boolean invokeRegardingToCarNames(String input) {
         String[] split = input.split(SPLIT_REGEX);
-        if (noInputException(input)
+        if (noInputException(input, CAR_NAMES_NO_INPUT_ERROR_MESSAGE)
                 || longerThanFiveCharacterException(split)
                 || includeBlankBackAndForthException(split)
                 || blankNextToRestException(input)
@@ -22,13 +25,21 @@ public class InputException {
         return false;
     }
 
+    public boolean invokeRegardingToTrialNumber(String input) {
+        if (noInputException(input, TRIAL_NUMBER_NO_INPUT_ERROR_MESSAGE)
+                || parseToIntException(input)) {
+            return true;
+        }
+        return false;
+    }
+
     private void fullExceptionMessage(String exception) {
         System.out.print(ERROR_PREFIX + exception);
     }
 
-    private boolean noInputException(String input) {
+    private boolean noInputException(String input, String errorMessage) {
         if (input.length() == 0) {
-            fullExceptionMessage(NO_INPUT_ERROR_MESSAGE);
+            fullExceptionMessage(errorMessage);
             return true;
         }
         return false;
@@ -37,7 +48,7 @@ public class InputException {
     private boolean longerThanFiveCharacterException(String[] split) {
         for (String s : split) {
             if (s.length() > 5) {
-                fullExceptionMessage(LONGER_THAN_FIVE_CHARACTER_ERROR_MESSAGE);
+                fullExceptionMessage(CAR_NAMES_LONGER_THAN_FIVE_CHARACTER_ERROR_MESSAGE);
                 return true;
             }
         }
@@ -47,7 +58,7 @@ public class InputException {
     private boolean includeBlankBackAndForthException(String[] split) {
         for (String s : split) {
             if (s.substring(0, 1).equals(" ") || s.substring(s.length()-1).equals(" ")) {
-                fullExceptionMessage(INCLUDE_BLANK_BACK_AND_FORTH_ERROR_MESSAGE);
+                fullExceptionMessage(CAR_NAMES_INCLUDE_BLANK_BACK_AND_FORTH_ERROR_MESSAGE);
                 return true;
             }
         }
@@ -56,9 +67,19 @@ public class InputException {
 
     private boolean blankNextToRestException(String input) {
         if (input.substring(input.length()-1).equals(SPLIT_REGEX)) {
-            fullExceptionMessage(BLANK_NEXT_TO_REST_ERROR_MESSAGE);
+            fullExceptionMessage(CAR_NAMES_BLANK_NEXT_TO_REST_ERROR_MESSAGE);
             return true;
         }
         return false;
+    }
+
+    private boolean parseToIntException(String input) {
+        try {
+            Integer.parseInt(input);
+            return false;
+        } catch (Exception e) {
+            fullExceptionMessage(TRIAL_NUMBER_NOT_INTEGER_ERROR_MESSAGE);
+            return true;
+        }
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,19 +8,27 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_PHRASE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    private static final String INPUT_TRIAL_NUMBER_PHRASE = "시도할 횟수는 몇 회인가요\n";
 
     private Scanner scanner;
+    private InputException inputException;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
+        this.inputException = new InputException();
     }
 
     public List<Car> getCarNames() {
         System.out.print(INPUT_CAR_NAMES_PHRASE);
-        return parse(validated());
+        return parseCarNames(validatedCarNames());
     }
 
-    private List<Car> parse(String validatedInput) {
+    public int getTrialNumber() {
+        System.out.print(INPUT_TRIAL_NUMBER_PHRASE);
+        return validatedTrialNumber();
+    }
+
+    private List<Car> parseCarNames(String validatedInput) {
         List<Car> parsedCars = new ArrayList<>();
         for (String s : validatedInput.split(InputException.SPLIT_REGEX)) {
             parsedCars.add(new Car(s));
@@ -28,12 +36,11 @@ public class InputView {
         return parsedCars;
     }
 
-    private String validated() {
-        InputException inputException = new InputException();
+    private String validatedCarNames() {
         String validatedCarNames;
         while (true) {
             validatedCarNames = getInput();
-            if (!inputException.invoke(validatedCarNames)) {
+            if (!inputException.invokeRegardingToCarNames(validatedCarNames)) {
                 break;
             }
         }
@@ -41,8 +48,19 @@ public class InputView {
     }
 
     private String getInput() {
-        String carNames = scanner.nextLine();
-        return carNames;
+        String carNamesInput = scanner.nextLine();
+        return carNamesInput;
+    }
+
+    private int validatedTrialNumber() {
+        String validatedTrialNumber;
+        while (true) {
+            validatedTrialNumber = getInput();
+            if (!inputException.invokeRegardingToTrialNumber(validatedTrialNumber)) {
+                break;
+            }
+        }
+        return Integer.parseInt(validatedTrialNumber);
     }
 
 }
