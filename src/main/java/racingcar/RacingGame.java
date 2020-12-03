e2a0bb4992527c1ae537f78e08fa6f0d8c6f5d87
park seok chae <tjrco56@naver.com>
2020-12-03 17:32:45 +0900

    feat(RacingGame): Car 리스트의 모든 객체들을 랜덤한 숫자를 기준으로 전진시키거나 멈춰두는 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,13 +2,20 @@ package racingcar;
 
 import java.util.ArrayList;
 
+import utils.RandomUtils;
+
 /**
- * 자동차 경주 게임 객체 생성을 위한 클래스
- * 기능 1. 자동차 경주 게임 싱글톤 객체 반환
- * 기능 2. Car 리스트에 이름으로 새로운 Car 객체 추가
- * 기능 3. Car 리스트 초기화
+ * 자동차 경주 게임 객체 생성을 위한 클래스 기능
+ * 1. 자동차 경주 게임 싱글톤 객체 반환 기능
+ * 2. Car 리스트에 이름으로 새로운 Car 객체 추가 기능
+ * 3. Car 리스트 초기화 기능
+ * 4. Car 리스트의 Car 객체들이 모두 랜덤한 숫자를 기준으로 전진하거나 멈춰있는 기능
  */
 public class RacingGame {
+	private static final Integer MAX_RANDOM_NUM = 9;
+	private static final Integer MIN_RANDOM_NUM = 0;
+	private static final Integer MOVE_CRITERIA_NUM = 4;
+
 	private static RacingGame racingGame = new RacingGame();
 
 	private ArrayList<Car> cars = new ArrayList<Car>();
@@ -26,5 +33,14 @@ public class RacingGame {
 
 	public void clearCars() {
 		cars = new ArrayList<Car>();
+	}
+
+	public void playOnePeriod() {
+		for (Car car : cars) {
+			int randNum = RandomUtils.nextInt(MAX_RANDOM_NUM, MIN_RANDOM_NUM);
+			if (randNum >= MOVE_CRITERIA_NUM) {
+				car.moveForwardByOne();
+			}
+		}
 	}
 }
