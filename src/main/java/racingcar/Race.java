78454ab90566bafa960da553af7e1c5c1283134f
bepoz <kangsy763@naver.com>
2020-12-03 18:39:37 +0900

    feat(Race): start 메서드 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -3,12 +3,18 @@ package racingcar;
 import java.util.Scanner;
 
 public class Race {
+    private final Scanner scanner;
 
     public Race(Scanner scanner) {
-
+        this.scanner = scanner;
     }
 
     public void start() {
-
+        nameInput();
+        attemptNumberInput();
+        carsMoveProcess();
+        printWinner();
     }
+
+
 }
