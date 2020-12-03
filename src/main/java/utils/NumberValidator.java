016f0c95a4da5d8eabdde89a673094beadaa4b6e
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 20:12:20 +0900

    refactor: VALUE_OVER_ZERO_MESSAGE 오류 메세지 수정

--- src/main/java/utils/NumberValidator.java
+++ src/main/java/utils/NumberValidator.java
@@ -2,7 +2,7 @@ package utils;
 
 public class NumberValidator {
     private static final int NON_RACE_VALUE = 0;
-    private static final String VALUE_OVER_ZERO_MESSAGE = "[ERROR] 게임 참가자 이름의 길이는 0 보다 커야 합니다.";
+    private static final String VALUE_OVER_ZERO_MESSAGE = "[ERROR] 게임 시도 회수는 0 보다 커야 합니다.";
 
     public static void checkNumberOverZero(int input) {
         if (input <= NON_RACE_VALUE) {
