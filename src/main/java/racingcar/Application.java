4f7d3057528312ab231165d18867ba45e322f9dd
SiHyung92 <fjzjqhdl@gmail.com>
2020-12-03 11:26:35 +0900

    refactor : 에러 출력 문자열 상수화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,12 @@ import java.util.*;
 public class Application {
     private static final String REQUEST_INPUT_RACING_CAR_MSG = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_INPUT_ROUND_MSG = "시도할 횟수는 몇 회인가요?";
+    private static final String ERROR_EMPTY_NAME_MSG = "[ERROR] 공백 이름은 사용할 수 없습니다.";
+    private static final String ERROR_ID_MAX_LENGTH_MSG = "[ERROR] 사용자의 아이디는 5글자 이하여야 합니다.";
+    private static final String ERROR_ID_DUPLICATE_MSG = "[ERROR] 중복된 사용자 이름입니다.";
+    private static final String ERROR_MAX_SIZE_OF_PEOPLE_MSG = "[ERROR] 인원숫자를 확인해주십시오. ( 5명 이하)";
     private static final String COMMA = ",";
+
     private List<Car> cars;
 
     public static void main(String[] args) {
@@ -43,23 +48,23 @@ public class Application {
 
     private void validateCarNameFormat(String carName) {
         if(carName.isEmpty()){
-            throw new IllegalArgumentException("[ERROR] 공백 이름은 사용할 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_EMPTY_NAME_MSG);
         }
         if(carName.length() > 5){
-            throw new IllegalArgumentException("[ERROR] 사용자의 아이디는 5글자 이하여야 합니다.");
+            throw new IllegalArgumentException(ERROR_ID_MAX_LENGTH_MSG);
         }
     }
 
     private void validateDuplicateCarNames(String[] carNames) {
         Set<String> duplicateCheckSet = new HashSet<>(Arrays.asList(carNames));
         if (duplicateCheckSet.size() != carNames.length) {
-            throw new IllegalArgumentException("[ERROR] 중복된 사용자 이름입니다.");
+            throw new IllegalArgumentException(ERROR_ID_DUPLICATE_MSG);
         }
     }
 
     private void validateCarNamesSize(String[] carNames) {
         if (carNames.length > 5) {
-            throw new IllegalArgumentException("[ERROR] 인원숫자를 확인해주십시오. ( 5명 이하)");
+            throw new IllegalArgumentException(ERROR_MAX_SIZE_OF_PEOPLE_MSG);
         }
     }
 }
