81097a5b248250e864f7cd653cf4451045783398
Wannte <T.wannte@gmail.com>
2020-12-03 12:38:40 +0900

    feat(InputView): Validate 로직 추가. Stream -> List
    만들어진 Stream을 여러번 사용해야 하는데, Stream의 경우 한 번 사용하기 위해 만들어 놓은 것. List를 이용하여 구현.

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,8 +1,7 @@
 package racingcar;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class InputView {
     private final Scanner scanner;
@@ -11,11 +10,44 @@ public class InputView {
         this.scanner = scanner;
     }
 
-    public Stream<String> getCarNames(){
-        return Arrays.stream(scanner.nextLine().split(","));
+    public List<String> getCarNames(){
+        List<String> names = getCarNamesInput();
+        validateNoneEmptyElements(names);
+        validateDuplicate(names);
+        return names;
     }
 
     public int getRoundNumber() {
-        return scanner.nextInt();
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("[Error] 시도 횟수는 숫자여야 한다.");
+        }
+    }
+
+    private List<String> getCarNamesInput() {
+        return Arrays.stream(scanner.nextLine().split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    private void validateNoneEmptyElements(List<String> names) {
+        names.forEach(this::validateNoneEmpty);
+    }
+
+    private void validateNoneEmpty(String name) {
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("[Error] 이름은 공백이면 안된다.");
+        }
+    }
+
+    private void validateDuplicate(List<String> names) {
+        Set<String> set = new HashSet<>();
+        for (String name : names) {
+            if (set.contains(name)) {
+                throw new IllegalArgumentException("[Error] 이름이 같은 참가자가 있습니다.");
+            }
+            set.add(name);
+        }
     }
 }
