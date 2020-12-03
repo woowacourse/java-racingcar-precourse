330ef0adc14fa9e6f1b995ef70e08074ab22da6e
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-04 00:13:36 +0900

    modified: 시도 횟수 묻는 메소드 이름 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,17 +3,17 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-    private Scanner input = new Scanner(System.in);
+    private static Scanner input = new Scanner(System.in);
 
     private static String ASK_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static String ASK_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
 
-    public String askCarName() {
+    public static String askCarName() {
         System.out.println(ASK_CAR_NAME_MESSAGE);
         return input.nextLine();
     }
 
-    public String getAskNumberOfAttempts() {
+    public static String askNumberOfAttempts() {
         System.out.println(ASK_NUMBER_OF_ATTEMPTS);
         return input.nextLine();
     }
