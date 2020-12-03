8f7e72e16d4fad9001fe3dcd69363206898cbf32
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 14:57:03 +0900

    feat: 각 차수별 실행 결과 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,11 +1,34 @@
 package view;
 
+import java.util.List;
+
 public class OutputView {
+    private static final String NEW_LINE = "\n";
+    private static final String MOVE_SIGN = "-";
+
     public static void printInputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public static void printInputRaceTime() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printRaceResult(List<String> names) {
+        System.out.println(NEW_LINE + "실행 결과");
+    }
+
+    public static void printRaceResultEachPlayer(String name, int position) {
+        System.out.print(name + " : " );
+
+        for (int i = 0; i < position; i++) {
+            System.out.print(MOVE_SIGN);
+        }
+
+        System.out.println();
+    }
+
+    public static void printNewLine() {
+        System.out.println();
     }
 }
