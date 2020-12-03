220f62a899b74bddb93732601a5347bd0ab512d7
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 20:00:38 +0900

    feat: 게임 참가자 이름의 길이가 0 일 경우의 예외 처리

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -2,11 +2,19 @@ package utils;
 
 public class Validator {
     private static final int NAME_LENGTH_MAX_VALUE = 5;
+    private static final String NULL = "";
     private static final String OVER_LENGTH_MESSAGE = "[ERROR] 게임 참가자 이름의 길이는 5 이하 여야 합니다.";
+    private static final String LENGTH_ZERO_MESSAGE = "[ERROR] 게임 참가자 이름의 길이는 0 보다 커야 합니다.";
 
     public static void checkInputLengthLessThanValue(String input) {
         if (input.length() > NAME_LENGTH_MAX_VALUE) {
             throw new IllegalArgumentException(OVER_LENGTH_MESSAGE);
+        }
+    }
+
+    public static void checkInputLengthNotZero(String input) {
+        if (input.equals(NULL)) {
+            throw new IllegalArgumentException(LENGTH_ZERO_MESSAGE);
         }
     }
 }
