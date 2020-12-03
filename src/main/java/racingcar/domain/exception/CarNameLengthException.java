0daf7f01f67d9261acf5ec0636238b6ff730049a
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-03 15:30:21 +0900

    feat: 자동차 이름 글자수에 대한 사용자 정의 예외 구현
    해당 커스텀 예외는 자동차 이름 글자수가 유효하지 않을 때 사용한다.
    (1~5자 외의 범위)

---
+++ src/main/java/racingcar/domain/exception/CarNameLengthException.java
@@ -0,0 +1,9 @@
+package racingcar.domain.exception;
+
+public class CarNameLengthException extends RuntimeException {
+    private static final String ERROR_MESSAGE = "[ERROR] 자동차 이름의 길이는 1자 이상 5자 이하입니다.";
+
+    public CarNameLengthException() {
+        super(ERROR_MESSAGE);
+    }
+}
