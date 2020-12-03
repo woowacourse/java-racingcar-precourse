dcf683164b5691f0d7211dcc00b5283a370dc6f7
naru <hsik0225@gmail.com>
2020-12-04 04:53:30 +0900

    feat(validator): 자동차의 이름의 길이가 빈 문자열이거나 6 이상일 경우 예외 발생 기능 구현

--- src/main/java/racingcar/domain/validator/NameValidator.java
+++ src/main/java/racingcar/domain/validator/NameValidator.java
@@ -1,5 +1,27 @@
 package racingcar.domain.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import racingcar.domain.CustomIllegalArgumentException;
+
 public class NameValidator extends Validator {
 
+    // 자동차 이름은 콤마(Comma)를 제외한 모든 문자를 1글자 이상 5글자 이하로 입력할 수 있다
+    public static final Pattern NAMES_LENGTH_PATTERN = Pattern.compile("[^,]{1,5}(,[^,]{1,5})*");
+
+    public static final String OUT_OF_BOUND_MESSAGE = "자동차 이름은 1글자 이상 5글자 이하이어야 합니다.";
+
+    @Override
+    public void validate(String input) {
+        super.validate(input);
+        checkNameLength(input);
+    }
+
+    public void checkNameLength(String input) {
+        Matcher matcher = NAMES_LENGTH_PATTERN.matcher(input);
+        if (!matcher.matches()) {
+            throw new CustomIllegalArgumentException(OUT_OF_BOUND_MESSAGE);
+        }
+    }
 }
--- src/test/java/racingcar/domain/NameValidatorTest.java
+++ src/test/java/racingcar/domain/NameValidatorTest.java
@@ -1,10 +1,13 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import racingcar.domain.validator.NameValidator;
 import racingcar.domain.validator.Validator;
@@ -18,11 +21,36 @@ public class NameValidatorTest {
     }
 
     @ParameterizedTest
+    @ValueSource(strings = {"pobi", "pobi, woni", "pobi,워니,준1"})
+    @DisplayName("유효한 값 입력 시 예외 미발생")
+    public void validate_ValidValue_NoExceptionThrown(String carNames) {
+        assertThatCode(() -> validator.validate(carNames)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("null 혹은 빈 문자열 입력 시 예외 발생")
-    public void checkNullOrEmpty_NullAndEmpty_ExceptionThrown(String input) {
-        assertThatThrownBy(() -> validator.validate(input))
+    public void checkNullOrEmpty_NullAndEmpty_ExceptionThrown(String carNames) {
+        assertThatThrownBy(() -> validator.validate(carNames))
                 .isExactlyInstanceOf(CustomIllegalArgumentException.class)
                 .hasMessage(CustomIllegalArgumentException.ERROR_MESSAGE + Validator.INPUT_EMPTY_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("자동차의 이름이 빈 문자열 경우 예외 발생")
+    public void checkNameLength_Zero_ExceptionThrown() {
+        String carNames = "pobi,";
+        assertThatThrownBy(() -> validator.validate(carNames))
+                .isExactlyInstanceOf(CustomIllegalArgumentException.class)
+                .hasMessage(CustomIllegalArgumentException.ERROR_MESSAGE + NameValidator.OUT_OF_BOUND_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("자동차의 이름의 길이가 5 초과일 경우 예외 발생")
+    public void checkNameLength_Six_ExceptionThrown() {
+        String carNames = "woowatechcourse";
+        assertThatThrownBy(() -> validator.validate(carNames))
+                .isExactlyInstanceOf(CustomIllegalArgumentException.class)
+                .hasMessage(CustomIllegalArgumentException.ERROR_MESSAGE + NameValidator.OUT_OF_BOUND_MESSAGE);
     }
 }
