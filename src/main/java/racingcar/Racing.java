e05a5522c10cc0a576e51513fddd2aededc8e342
platanus <platanus@canxan.com>
2020-12-04 00:25:51 +0900

    feat(racing): Make showing race winner

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -8,7 +8,9 @@ public class Racing {
     public static final String STANDING_START_ANNOUNCEMENT = "실행 결과";
     public static final String LAP_POSITION_CHARACTER = "-";
     public static final String RACER_POSITION_DIVIDE_CHARACTER = " : ";
-    
+    public static final String EVENT_WINNER_ANNOUNCEMENT = "최종 우승자: ";
+    public static final String EVENT_WINNER_FORMAT_CHARACTER = ", ";
+
     public StringUtils racingEvent;
 
     ArrayList<Car> racerList = new ArrayList<Car>();
@@ -46,4 +48,23 @@ public class Racing {
         }
     }
 
+    public void showAwards() {
+        String eventWinnerList = "";
+        int maxPositionInRacingEvent = -1;
+        for (Car carInRacingEvent : racerList) {
+            int thisRacerPositionInRacingEvent = carInRacingEvent.getPosition();
+            if (maxPositionInRacingEvent < thisRacerPositionInRacingEvent) {
+                maxPositionInRacingEvent = thisRacerPositionInRacingEvent;
+            }
+        }
+        for (Car carInRacingEvent : racerList) {
+            int thisRacerPositionInRacingEvent = carInRacingEvent.getPosition();
+            if (maxPositionInRacingEvent == thisRacerPositionInRacingEvent) {
+                eventWinnerList += carInRacingEvent.getRacerName();
+                eventWinnerList += EVENT_WINNER_FORMAT_CHARACTER;
+            }
+        }
+        String result = eventWinnerList.substring(0, eventWinnerList.length() - 2);
+        System.out.println(EVENT_WINNER_ANNOUNCEMENT + result);
+    }
 }
