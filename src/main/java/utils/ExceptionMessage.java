f8932c6b6978ae234e5853dd36bc0e39ddb2247b
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-03 22:54:49 +0900

    feat: 유효성 검사를 위한 기능 구현
    입력값에 대한 유효성 검사 구현
    
    Validator : 유효성 검사
    ExceptionMessage: 유효하지 않은 입력에 대한 에러메시지

---
+++ src/main/java/utils/ExceptionMessage.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class ExceptionMessage {
+    public static final String isNotValidCarName = "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
+    public static final String isNotInteger = "[ERROR] 입력한 시도 횟수가 숫자가 아닙니다.";
+}
---
+++ src/main/java/utils/Validator.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.util.List;
+
+public class Validator {
+
+    public void isValidCarName(List<String> carList) {
+        for (String carName : carList) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException(ExceptionMessage.isNotValidCarName);
+            }
+        }
+    }
+
+    public void isPlayRoundInteger (String playRound) {
+        try {
+            Integer.parseInt(playRound);
+        } catch (NumberFormatException ne) {
+            throw new NumberFormatException(ExceptionMessage.isNotInteger);
+        }
+
+    }
+
+}
