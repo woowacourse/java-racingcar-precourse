51b3663b8af3648d67d2f8a26b67757757a870e9
bepoz <kangsy763@naver.com>
2020-12-03 18:43:34 +0900

    feat(MessageBox): MessageBox 클래스 생성

---
+++ src/main/java/racingcar/MessageBox.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class MessageBox {
+    public static final String NAME_ASK_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ATTEMPT_NUMBER_ASK_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String PROCESS_RESULT_MESSAGE = "실행 결과";
+    public static final String FINAL_WINNER_MESSAGE = "최종 우승자: ";
+}
