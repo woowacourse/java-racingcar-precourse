b24b31cd6c1bd488930da16a9b821ac2329fbcce
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-03 17:16:22 +0900

    feat: 입력받은 시도 횟수만큼 반복하는 기능 추가

--- src/main/java/racingcar/domain/Rule.java
+++ src/main/java/racingcar/domain/Rule.java
@@ -4,7 +4,7 @@ public class Rule {
 
     private static final int MIN_ATTEMPT_NUMBER = 1;
     private static final String ATTEMPT_NUMBER_RANGE_ERROR = "[ERROR] 시도 횟수는 0보다 커야 합니다.";
-    public int attemptNumber;
+    private int attemptNumber;
 
     public Rule(int attemptNumber) {
         validateRangeAttemptNumber(attemptNumber);
@@ -14,6 +14,12 @@ public class Rule {
     private void validateRangeAttemptNumber(int attemptNumber) {
         if (attemptNumber < MIN_ATTEMPT_NUMBER) {
             throw new IllegalArgumentException(ATTEMPT_NUMBER_RANGE_ERROR);
+        }
+    }
+
+    public void attemptMoveByAttemptNumber(Cars cars) {
+        for(int i = 0; i < attemptNumber; i++) {
+            cars.goOrStopCars();
         }
     }
 }
