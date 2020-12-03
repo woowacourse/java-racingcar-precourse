619ce4353743555bfe83f6c5b9a01f7fd3184d9f
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-04 01:27:16 +0900

    feat: 사용자가 입력한 자동차 이름을 자동차 리스트로 만드는 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,23 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Car {
     private final String name;
     private int position = 0;
+    private List<Car> carList;
 
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public List<Car> makeCarList(String userInput) {
+        List<String> userList = CheckValidation.stringToList(userInput);
+        for (String car : userList) {
+            carList.add(new Car(car));
+        }
+        return carList;
+    }
+
 }
