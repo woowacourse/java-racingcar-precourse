24a12b6cbc112f49d100a549971e8ac0767c8246
platanus <platanus@canxan.com>
2020-12-03 22:56:09 +0900

    feat(racing): Make setter to create instances array

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,14 +1,25 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Racing {
 
     public StringUtils racingEvent;
 
+    ArrayList<Car> racerList = new ArrayList<Car>();
+
     public Racing(Scanner promptInput) {
         // start game prompt
         racingEvent = new StringUtils(promptInput);
+        setGrid(racingEvent);
+    }
+
+    private void setGrid(StringUtils racingEvent) {
+        for (String name : racingEvent.racerNameList) {
+            Car racer = new Car(name);
+            racerList.add(racer);
+        }
     }
 
 }
