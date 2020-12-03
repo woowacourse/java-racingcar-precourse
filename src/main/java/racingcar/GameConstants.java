c447fe0aa1f4a18d2f0fc426df53a7b093cbd6b4
Injun <ison@sfu.ca>
2020-12-03 16:34:19 +0900

    feat: GameConstants 클래스 구현

---
+++ src/main/java/racingcar/GameConstants.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class GameConstants {
+    public static final String INPUT_ROUND_MESSAGE = "시도할 횟수를 입력해주세요: ";
+    public static final String INVALID_ROUND_INPUT_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
+    public static final String INPUT_CAR_NAMES_MESSAGE = "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)하여 입력해주세요: ";
+    public static final String INVALID_CAR_NAMES_MESSAGE = "[ERROR] 자동차 이름은 쉼표(,)로 구분됩니다";
+}
