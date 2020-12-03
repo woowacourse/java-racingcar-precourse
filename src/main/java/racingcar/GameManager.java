5859e3599b4a44cbf65279bdf21b41f129df5006
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 19:23:35 +0900

    refactor: 시도할 회수를 입력 받는 기능 분리

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -13,7 +13,10 @@ public class GameManager {
         OutputView.printInputCarName();
 
         cars = new Cars(InputView.inputPlayerName(scanner));
+        makeTryTimes(scanner);
+    }
 
+    private void makeTryTimes(Scanner scanner) {
         OutputView.printInputRaceTime();
 
         tryTimes = InputView.inputTryTimes(scanner);
