db13cc9013cffc08261dee4cdb13f253ffe8e960
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-03 20:35:06 +0900

    feat : race 전반을 담당하는 GameManager 최종 구현 및 Application을 통해 실행 가능하도록 설정
    - progress 출력 시 Arraylist로 인해 괄호 출력되는 것 수정 필요.

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -10,15 +10,47 @@ import java.util.Scanner;
 public class GameManager {
     public static final int MAXIMUM_CARNAME_LENGTH = 5;
 
-    public void startGame(Scanner scanner) {
+    public GameManager(Scanner scanner) {
         InputManager inputManager = new InputManager();
         ArrayList<Car> cars = new ArrayList<>(inputManager.scanCarNames(scanner));
         Round round = new Round(inputManager.scanRound(scanner));
-        System.out.println(Sentences.SHOW_RESULT);
+        System.out.println("최종 우승자 : " + Sentences.SHOW_RESULT);
+
+        while(true) {
+            startRace(cars);
+            showRace(cars);
+            if(judge(cars, round).size() != 0) {
+                System.out.println(judge(cars, round));
+                break;
+            }
+        }
     }
 
     public void startRace(ArrayList<Car> cars) {
-
-
+        for(Car car : cars) {
+            car.moveForward();
+        }
     }
+
+    public void showRace(ArrayList<Car> cars) {
+        for(Car car : cars) {
+            ArrayList<String> progress = new ArrayList<>();
+            for(int i = 0; i < car.getPosition(); i++) {
+                progress.add(Sentences.PROGRESS);
+            }
+            System.out.println(car.getName() + " : " + progress);
+            System.out.println("\n");
+        }
+    }
+
+    public ArrayList<String> judge(ArrayList<Car> cars, Round round) {
+        ArrayList<String> winners = new ArrayList<>();
+        for(Car car : cars) {
+            if(car.getPosition() == round.getRound()) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
 }
