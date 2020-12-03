3a17fcc6badb60af36d3f124c29edd574a2d40e0
PaperCrafter <leejonghee342@gmail.com>
2020-12-03 12:11:15 +0900

    feat(Engine): add run method in engine
    added move method
    - if returned random value is less than certain threshold then return 0
    - if returned random value is more than certain threshold then return 1

--- src/main/java/racingcar/domain/Engine.java
+++ src/main/java/racingcar/domain/Engine.java
@@ -1,4 +1,17 @@
 package racingcar.domain;
 
+import racingcar.generator.RandomNumberGenerator;
+import utils.RandomUtils;
+
+import java.util.Random;
+
 public class Engine {
+    private static final int MOVABLE_THRESHOLD = 4;
+    public int run(){
+        int randomValue = RandomNumberGenerator.generateRandomNumber();
+        if(randomValue < MOVABLE_THRESHOLD){
+            return 0;
+        }
+        return 1;
+    }
 }
