8956e938e240afa5a59efe9a3e7f4cb8560dbb04
bepoz <kangsy763@naver.com>
2020-12-04 00:38:28 +0900

    feat(Car): Car compareTo 메서드 오버라이딩

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-public class Car {
+public class Car implements Comparable<Car>{
     private final String name;
     private int position = 0;
 
@@ -19,5 +19,10 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    @Override
+    public int compareTo(Car c) {
+        return c.position - this.position;
     }
 }
