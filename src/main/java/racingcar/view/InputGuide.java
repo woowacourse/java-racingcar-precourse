e2b402cc81ce0babae264781dcb527b15827d29b
RinSabbit <celena1004@naver.com>
2020-12-03 20:12:06 +0900

    refactor: InputGuide 클래스에서 입력값을 검증하는 기능 분리

---
+++ src/main/java/racingcar/view/InputGuide.java
@@ -0,0 +1,48 @@
+package racingcar.view;
+
+import java.util.Scanner;
+import racingcar.model.Car;
+import racingcar.controller.ErrorException;
+import utils.ValidUtils;
+
+/**
+ * 입력값을 받는 클래스
+ */
+public class InputGuide {
+
+    private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String MOVES_INPUT_MESSAGE = "시도할 횟수는 몇회인가요? : ";
+    private static final String REST = ",";
+    private final ValidUtils validUtils;
+    private final Scanner scanner;
+
+    public InputGuide(Scanner scanner) {
+        this.scanner = scanner;
+        validUtils = new ValidUtils(scanner);
+    }
+
+    public Car[] inputCars() {
+        System.out.println(CAR_INPUT_MESSAGE);
+        String[] cars = splitCarsName(scanner.nextLine());
+        try{
+            return validUtils.isValid(cars);
+        } catch(ErrorException exception){
+            System.out.println(exception.getMessage());
+            return inputCars();
+        }
+    }
+
+    private String[] splitCarsName(String input) {
+        return input.split(REST);
+    }
+
+    public int countMove() {
+        System.out.println(MOVES_INPUT_MESSAGE);
+        try {
+            return validUtils.moreThanZero();
+        } catch (ErrorException exception) {
+            System.out.println(exception.getMessage());
+            return countMove();
+        }
+    }
+}
--- src/main/java/utils/ValidUtils.java
+++ src/main/java/utils/ValidUtils.java
@@ -1,52 +1,33 @@
-package racingcar;
+package utils;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+import racingcar.model.Car;
+import racingcar.controller.ErrorException;
 
 /**
- * 입력값을 처리하는 클래스
+ * 입력값이 조건에 맞는지 검증하는 클래스
  */
-public class InputGuide {
+public class ValidUtils {
 
-    private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
-    private static final String MOVES_INPUT_MESSAGE = "시도할 횟수는 몇회인가요? : ";
     private static final String CAR_NAME_INPUT_EXCEPTION_MESSAGE = "자동차 이름은 0자 이상 5자 이내여야 합니다.";
     private static final String CAR_NUMBERS_INPUT_EXCEPTION_MESSAGE = "자동차 갯수는 2개 이상이어야 합니다.";
     private static final String MOVES_INPUT_EXCEPTION_MESSAGE = "자연수를 입력해야 합니다.";
-    private static final String REST = ",";
     private static final int FIVE = 5;
     private static final int TWO = 2;
-    public static final int ZERO = 0;
+    private static final int ZERO = 0;
     private final Scanner scanner;
-    private Car[] cars;
 
-    InputGuide(Scanner scanner) {
+    public ValidUtils(Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public Car[] inputCars() {
-        System.out.println(CAR_INPUT_MESSAGE);
-        String[] cars = splitCarsName(scanner.nextLine());
-        try{
-            return isValid(cars);
-        } catch(ErrorException exception){
-            System.out.println(exception.getMessage());
-            return inputCars();
-        }
-    }
 
-    private Car[] isValid(String[] cars) {
+    public Car[] isValid(String[] cars) {
         if (isMoreThanOne(cars)) {
             return makeCar(cars);
         }
         throw new ErrorException(CAR_NUMBERS_INPUT_EXCEPTION_MESSAGE);
-    }
-
-    private String[] splitCarsName(String input) {
-        return input.split(REST);
     }
 
     private boolean isMoreThanOne(String[] input) {
@@ -68,24 +49,14 @@ public class InputGuide {
     }
 
     private Car[] makeCar(String[] names) {
-        cars = new Car[names.length];
+        Car[] cars = new Car[names.length];
         for (int i = 0, length = names.length; i < length; i++) {
             cars[i] = new Car(names[i]);
         }
         return cars;
     }
 
-    public int countMove() {
-        System.out.println(MOVES_INPUT_MESSAGE);
-        try {
-            return moreThanZero();
-        } catch (ErrorException exception) {
-            System.out.println(exception.getMessage());
-            return countMove();
-        }
-    }
-
-    private int moreThanZero(){
+    public int moreThanZero(){
         try{
             int input = Integer.parseInt(scanner.nextLine());
             return isNaturalNumber(input);
