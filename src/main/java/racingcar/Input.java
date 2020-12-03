2134fd57698c26c758a80a0a5a33fe0d128b7524
eoc940 <hgk0000@naver.com>
2020-12-04 04:07:23 +0900

    refactor(input): 출력 메시지 추가

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -6,7 +6,12 @@ import java.util.Scanner;
 
 public class Input {
 
+    static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String INPUT_TRIAL_MESSAGE = "시도할 회수는 몇회인가요?";
+
+
     public String[] getCarNameArray(Scanner scanner) {
+        System.out.println(INPUT_NAME_MESSAGE);
         String inputCars = scanner.next();
         String[] carNames = inputCars.split(",");
         return carNames;
@@ -21,6 +26,7 @@ public class Input {
     }
 
     public int getTrial(Scanner scanner) {
+        System.out.println(INPUT_TRIAL_MESSAGE);
         int trial = scanner.nextInt();
         return trial;
     }
