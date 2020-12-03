3c505192eceaadcb30620e8a34c6daa7e65c05e6
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-04 02:08:48 +0900

    modified: Car 생성자에 position 초기화

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,17 +8,18 @@ public class Car {
     private int position = 0;
     private static List<Car> carList = new ArrayList<>();
 
-    public Car(String name) {
+    public Car(String name, int position) {
         this.name = name;
+        this.position = position;
     }
 
     public static List<Car> makeCarList(String userInput) {
         List<String> userList = CheckValidation.stringToList(userInput);
         for (String car : userList) {
-            carList.add(new Car(car));
+            carList.add(new Car(car, 0));
         }
         return carList;
     }
 
-    
+
 }
