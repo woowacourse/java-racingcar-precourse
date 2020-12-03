0ed6cbb9d3f4097771214f9f9e5aefe8bef5c543
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-03 17:26:08 +0900

    feat: Car class에 field, method 추가하여 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,9 +4,16 @@ public class Car {
     private final String name;
     private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+    public Car(String name) { this.name = name; }
 
     // 추가 기능 구현
+    public void moveForward() {
+        this.position++;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getPosition() {
+        return this.position;
+    }
 }
