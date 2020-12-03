fbc200123d44e23fb0251533943c164e918cf606
PaperCrafter <leejonghee342@gmail.com>
2020-12-03 14:16:40 +0900

    feat(RacingGame): add print racing functions
    added functions related with racing and pring racing
    - printRace: logic for print racing
    - moveCars: move cars in each iteration
    - printCars: print cars for each iteration
    - printWinners: print winners

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.domain.Car;
+import racingcar.view.OutputView;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -13,25 +14,36 @@ public class RacingGame {
 
     private Scanner scanner;
     private List<Car> carList;
+    private int numberOfRaces = 0;
 
     public RacingGame(Scanner scanner){
         this.scanner = scanner;
     }
 
     public void runGame(){
-
+        setup();
+        startRace();
     }
 
-    public void setup(){
+    private void setup(){
         try {
             String carNames = scanner.nextLine();
             setupCars(carNames);
         } catch (IllegalArgumentException ie){
-
+            System.out.println();
         }
     }
 
-    public void setupCars(String inputCars){
+    private void startRace() {
+        try {
+            numberOfRaces = Integer.parseInt(scanner.nextLine());
+            printRace();
+        } catch (IllegalArgumentException ie){
+            System.out.println();
+        }
+    }
+
+    private void setupCars(String inputCars){
         String[] cars = inputCars.split(",");
         for(String car : cars){
             checkCarNameLength(car);
@@ -39,14 +51,37 @@ public class RacingGame {
         initializeCars(cars);
     }
 
-    public void initializeCars(String[] cars){
+    private void initializeCars(String[] cars){
         carList = new LinkedList<>(Arrays.stream(cars)
                 .map(name -> new Car(name))
                 .collect(Collectors.toList()));
     }
 
-    public void checkCarNameLength(String car){
+    private void checkCarNameLength(String car){
         if(car.length() >= MAX_CAR_NAME_LENGTH)
             throw new IllegalArgumentException();
+    }
+
+    private void printRace(){
+        for (int i = 0; i < numberOfRaces; i++) {
+            moveCars();
+            printCars();
+        }
+        printWinners();
+    }
+
+    private void moveCars(){
+        for (Car car : carList) {
+            car.move();
+        }
+    }
+
+    private void printCars(){
+        OutputView.printRaceProgressLog(carList);
+        System.out.println();
+    }
+
+    private void printWinners(){
+        OutputView.printWinnerLog(carList);
     }
 }
