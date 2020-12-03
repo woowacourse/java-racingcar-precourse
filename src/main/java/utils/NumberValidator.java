b3c695494506f8471b4065cf9e642e452e4445f3
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 20:05:22 +0900

    feat: 입력된 경기 시도할 회수 값을 판단하는 기능 추가

---
+++ src/main/java/utils/NumberValidator.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class NumberValidator {
+    private static final int NON_RACE_VALUE = 0;
+    private static final String VALUE_OVER_ZERO_MESSAGE = "[ERROR] 게임 참가자 이름의 길이는 0 보다 커야 합니다.";
+
+    public static void checkNumberOverZero(int input) {
+        if (input <= NON_RACE_VALUE) {
+            throw new IllegalArgumentException(VALUE_OVER_ZERO_MESSAGE);
+        }
+    }
+}
