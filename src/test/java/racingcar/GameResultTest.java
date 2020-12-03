8a65c03991b0db7c54135b3258f67ef7865c4dcc
김주원 <kjw11077@naver.com>
2020-12-03 15:11:06 +0900

    test(GameResult): add unittest

--- src/test/java/racingcar/GameResultTest.java
+++ src/test/java/racingcar/GameResultTest.java
@@ -3,22 +3,34 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GameResultTest {
 
+    Car joy;
+    Car happy;
+
     @Before
     public void setUp() throws Exception {
+        joy = new Car("joy");
+        happy = new Car("happy");
     }
 
     @Test
-    public void getWinnerNames() {
+    public void getWinnerNames_multiWinners() {
         GameResult gameResult = new GameResult(
-                Arrays.asList(
-                        new Car("joy"),
-                        new Car("happy")));
+                Arrays.asList(joy, happy));
 
         assertThat(gameResult.getWinnerNames()).isEqualTo("joy, happy");
+    }
+
+    @Test
+    public void getWinnerNames_singleWinner() {
+        GameResult gameResult = new GameResult(
+                Collections.singletonList(joy));
+
+        assertThat(gameResult.getWinnerNames()).isEqualTo("joy");
     }
 }
