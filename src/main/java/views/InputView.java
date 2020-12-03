a2d199bf88009ee53426057046d502107b2d566f
tomas <cmg14111@gmail.com>
2020-12-03 18:21:19 +0900

    feat(InputView): 라운드 수를 입력받는 기능 구현
    이름의 입력과 라운드 수의 입력이 공백인 예외처리를 isEmptyValue()에서 공통으로 처리하도록 변경
    
    라운드 수의 입력이 문자인 경우, 0인 경우의 예외처리

--- src/main/java/views/InputView.java
+++ src/main/java/views/InputView.java
@@ -6,20 +6,47 @@ import java.util.stream.Stream;
 
 public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String EMPTY_INPUT = "";
-    private static final String EXCEPTION_MESSAGE = "[ERROR] 공백을 입력할 수 없습니다.";
     private static final String SEPARATOR = ",";
+    private static final String INPUT_ROUND_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String EMPTY_EXCEPTION = "[ERROR] 공백을 입력할 수 없습니다.";
+    private static final String NOT_NUMBER_EXCEPTION = "[ERROR] 숫자를 입력해야 합니다.";
+    private static final String IMPOSSIBLE_NUMBER_ZERO_EXCEPTION = "[ERROR] 0은 임력할 수 없습니다.";
+    private static final String EMPTY_INPUT = "";
+    private static final String ZERO = "0";
 
     public Stream<String> inputCarNames(Scanner scanner) {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         String carNames = scanner.nextLine();
-        isValidCarNames(carNames);
+        isEmptyValue(carNames);
         return Arrays.stream(carNames.split(SEPARATOR));
     }
 
-    void isValidCarNames(String carNames) {
-        if (carNames.equals(EMPTY_INPUT)) {
-            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+    public int inputRoundNumber(Scanner scanner) {
+        System.out.println(INPUT_ROUND_NUMBER_MESSAGE);
+        String roundNumber = scanner.nextLine();
+        isEmptyValue(roundNumber);
+        isNumber(roundNumber);
+        isZero(roundNumber);
+        return Integer.parseInt(roundNumber);
+    }
+
+    void isEmptyValue(String input) {
+        if (input.equals(EMPTY_INPUT)) {
+            throw new IllegalArgumentException(EMPTY_EXCEPTION);
+        }
+    }
+
+    void isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(NOT_NUMBER_EXCEPTION);
+        }
+    }
+
+    void isZero(String input) {
+        if (input.equals(ZERO)) {
+            throw new IllegalArgumentException(IMPOSSIBLE_NUMBER_ZERO_EXCEPTION);
         }
     }
 }
