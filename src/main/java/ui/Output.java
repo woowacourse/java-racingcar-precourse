9fd0f6f9fa05ed02eb712a7da3d2fb36dab043c9
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-03 21:38:51 +0900

    refactor(Output) : 하드코딩된 문자열을 상수로 치환

--- src/main/java/ui/Output.java
+++ src/main/java/ui/Output.java
@@ -3,23 +3,28 @@ package ui;
 import java.util.List;
 
 public class Output {
+    private static final String REQUEST_RACING_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_ATTEMPTS_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String RESULT = "실행 결과";
+    private static final String WINNER = "최종 우승자";
+    private static final String ONE_STEP = "-";
 
     public static void printRequestRacingCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(REQUEST_RACING_CAR_NAMES);
     }
 
     public static void printRequestAttemptsCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(REQUEST_ATTEMPTS_COUNT);
     }
 
     public static void willPrintResult() {
-        System.out.println("실행 결과");
+        System.out.println(RESULT);
     }
 
     public static void printResult(String name, int position) {
         String movingResult = "";
         for (int i = 0; i < position; i++) {
-            movingResult += "-";
+            movingResult += ONE_STEP;
         }
         System.out.println(name + " : " + movingResult);
     }
@@ -29,7 +34,7 @@ public class Output {
     }
 
     public static void printWinners(List<String> winners) {
-        System.out.print("최종 우승자: ");
+        System.out.print(WINNER + ": ");
         System.out.println(String.join(", ", winners));
     }
 }
