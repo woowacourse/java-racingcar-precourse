5d766f0f19fee04749e1eaf83f10a706b686a76a
Noh Hyeon Jin <hjforj@naver.com>
2020-12-03 17:49:57 +0900

    feat: 시도 횟수 입력 받는 기능 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class Input {
 	private static final String SPLIT_DELIMITER = ",";
+	
     private static final String GET_CAR_NAMES_MESSAGE = "경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String GET_NUMBER_OF_TRY_MESSAGE = "시도할 회수는 몇회인가요?";
+    
     private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
+    private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
     
     private final Scanner scanner;
     
     private String carNames;
+    private int numberOfTry;
     
     public Input(Scanner scanner) {
         this.scanner = scanner;
@@ -21,9 +26,21 @@ public class Input {
         String[] carName = carNames.split(SPLIT_DELIMITER);
         return carName;
     }
+    public int getNumberOfTry() {
+        System.out.println(GET_NUMBER_OF_TRY_MESSAGE);
+        numberOfTry = scanner.nextInt();
+        checkIsDigit(numberOfTry);
+        return numberOfTry;
+    }
+    
     public void checkCarNameLength(String carName) {
         if (carName.length() > 5) {
         	throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+    public void checkIsDigit(int numberOfTry) {
+        if (!Character.isDigit(numberOfTry)) {
+        	throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
         }
     }
 }
