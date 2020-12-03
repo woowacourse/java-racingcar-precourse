643dfdd53beae56136e51c302a5714b7153dcb40
platanus <platanus@canxan.com>
2020-12-04 01:14:14 +0900

    feat(messageutils): Make exception generator

--- src/main/java/racingcar/MessageUtils.java
+++ src/main/java/racingcar/MessageUtils.java
@@ -2,4 +2,9 @@ package racingcar;
 
 public class MessageUtils {
 
+    public static void errorExceptionThrow(String messageConstant) {
+        System.out.println(Constants.ERROR_HEADER + messageConstant);
+        throw new IllegalArgumentException(Constants.ERROR_HEADER + messageConstant);
+    }
+
 }
