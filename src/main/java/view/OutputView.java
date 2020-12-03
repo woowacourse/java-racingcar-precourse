3b6a1441fd8a662b7c6fcf1d417d0d4042187993
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 20:20:34 +0900

    style: 사용 되지 않는 변수 제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class OutputView {
     private static final String NEW_LINE = "\n";
-    private static final String MOVE_SIGN = "-";
 
     public static void printInputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
