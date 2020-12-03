50c65f2b6ba962a4131a03fd978befa74bf88434
eoc940 <hgk0000@naver.com>
2020-12-03 18:33:21 +0900

    feat(error): 자동차의 이름이 중복될때 에러 메시지를 출력하는 checkSameCarName 메서드 출력

--- src/main/java/racingcar/Error.java
+++ src/main/java/racingcar/Error.java
@@ -1,4 +1,21 @@
 package racingcar;
 
+import java.util.List;
+
 public class Error {
+
+    static final String ERROR_PREFIX = "[ERROR] ";
+    static final String SAME_CAR_NAME_ERROR = "자동차 이름이 중복됩니다";
+
+    Input input = new Input();
+
+    public void checkSameCarName(String inputCars) throws IllegalArgumentException{
+        List<String> carNameList = input.getCarNameList(inputCars);
+        for (String carName : carNameList) {
+            if(carNameList.contains(carName)) {
+                System.out.print(ERROR_PREFIX + SAME_CAR_NAME_ERROR);
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
