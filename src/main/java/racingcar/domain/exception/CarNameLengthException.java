c25fbd73c2e12c4fe4cf1c232d50929efe66d321
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-03 21:46:15 +0900

    style: 에러메시지 내용 중 어미(접사) 변경

--- src/main/java/racingcar/domain/exception/CarNameLengthException.java
+++ src/main/java/racingcar/domain/exception/CarNameLengthException.java
@@ -1,7 +1,7 @@
 package racingcar.domain.exception;
 
 public class CarNameLengthException extends RuntimeException {
-    private static final String ERROR_MESSAGE = "[ERROR] 자동차 이름의 길이는 1자 이상 5자 이하입니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 자동차 이름의 길이는 1자 이상 5자 이하여야 합니다.";
 
     public CarNameLengthException() {
         super(ERROR_MESSAGE);
