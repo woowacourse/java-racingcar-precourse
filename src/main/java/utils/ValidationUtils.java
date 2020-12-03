081a655a16f3cebbfbb20ddf88955c09aaf1e81c
NewWisdom <wlgp2500@gmail.com>
2020-12-04 01:25:16 +0900

    feat(validation-utils): add validate-try-number method

--- src/main/java/utils/ValidationUtils.java
+++ src/main/java/utils/ValidationUtils.java
@@ -2,13 +2,16 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class ValidationUtils {
     private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
-    private static final String EXCEED_LENGTH_ERROR = "이름은 5자 이하여야 한다.";
-    private static final String NOT_ALPHA_NAME_ERROR = "이름은 영문자여야 한다.";
-    private static final String DUPLICATE_NAME_ERROR = "같은 이름을 가지면 안된다.";
+    private static final String EXCEED_LENGTH_ERROR_MESSAGE = "이름은 5자 이하여야 한다.";
+    private static final String NOT_ALPHA_ERROR_MESSAGE = "이름은 영문자여야 한다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "같은 이름을 가지면 안된다.";
+    private static final String NOT_DIGIT_ERROR_MESSAGE = "시도 횟수는 숫자여야 한다.";
+    private static final String LESS_THAN_MIN_ERROR_MESSAGE = "시도 횟수는 최소 1번 이상이여야 한다.";
     private static final String REGULAR_EXPRESSION_ALPHA = "^[a-zA-Z]*$";
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final int MIN_TRY_NUMBER = 1;
@@ -19,18 +22,26 @@ public class ValidationUtils {
     public static void validateCarNameArray(String[] carNameArray) {
         for (int i = 0; i < carNameArray.length; i++) {
             if (isInvalidLength(carNameArray[i])) {
-                throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + EXCEED_LENGTH_ERROR);
+                throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + EXCEED_LENGTH_ERROR_MESSAGE);
             }
             if (isNotAlpha(carNameArray[i])) {
-                throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + NOT_ALPHA_NAME_ERROR);
+                throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + NOT_ALPHA_ERROR_MESSAGE);
             }
         }
         if (hasDuplicateValue(carNameArray)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + DUPLICATE_NAME_ERROR);
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + DUPLICATE_ERROR_MESSAGE);
         }
     }
 
-    public static void validateTryNumber() {
+    public static int validateTryNumber(Scanner scanner) {
+        if (scanner.hasNext()) {
+            int tryNumber = scanner.nextInt();
+            if (tryNumber > MIN_TRY_NUMBER) {
+                return tryNumber;
+            }
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + LESS_THAN_MIN_ERROR_MESSAGE);
+        }
+        throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + NOT_DIGIT_ERROR_MESSAGE);
     }
 
     private static boolean isInvalidLength(String carName) {
