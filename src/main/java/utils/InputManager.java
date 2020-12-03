1c1104b31c5b8afed204a812432991888b4c4dd2
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-03 19:34:07 +0900

    feat(docs) : 입력값이 올바른지 체크하는 함수 구현

--- src/main/java/utils/InputManager.java
+++ src/main/java/utils/InputManager.java
@@ -14,8 +14,15 @@ public class InputManager {
         String[] carNames;
         List<Car> cars = new ArrayList<>();
         System.out.println(Sentences.ASK_CAR_NAME);
-        carNames = scanner.nextLine().trim().split(",");
-        checkCarNames(carNames);
+        while(true) {
+            try{
+                carNames = scanner.nextLine().trim().split(",");
+                checkCarNames(carNames);
+                break;
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
         for (String carName : carNames) {
             cars.add(new Car(carName));
         }
@@ -26,17 +33,34 @@ public class InputManager {
         String input;
         int round;
         System.out.println(Sentences.ASK_TRACK_LENGTH);
-        input = scanner.nextLine().trim();
+        while(true) {
+            try{
+                input = scanner.nextLine().trim();
+                checkRound(input);
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
         round = Integer.parseInt(input);
         return round;
     }
 
-    private static IllegalArgumentException checkCarNames(String[] carNames) {
+    private static void checkCarNames(String[] carNames) {
         for(String carName : carNames) {
             if(carName.length() > GameManager.MAXIMUM_CARNAME_LENGTH) {
-                return new IllegalArgumentException(Sentences.MAXIMUM_CARNAME_LENGTH_ERROR);
+                throw new IllegalArgumentException(Sentences.MAXIMUM_CARNAME_LENGTH_ERROR);
             }
         }
-        return null;
+    }
+
+    private static void checkRound(String round) {
+        char temp;
+        for(int i = 0; i < round.length(); i++) {
+            temp = round.charAt(i);
+            if(!Character.isDigit(temp)) {
+                throw new IllegalArgumentException(Sentences.ROUND_INPUT_ERROR);
+            }
+        }
     }
 }
