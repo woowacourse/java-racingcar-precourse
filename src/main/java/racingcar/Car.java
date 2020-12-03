8effe7b50e2e139034e64e1ee533b1c1a357a4af
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-03 20:30:46 +0900

    feat(Car) : RandomUtils 함수를 이용하여 앞으로 전진하는 함수 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,28 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
     private int position = 0;
+    private final int RANDOM_UPPER_BOUND = 9;
+    private final int RANDOM_LOWER_BOUND = 0;
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    public int getPosition() {
+        return position;
+    }
+    public String getName() { return name; }
+    public void moveForward() {
+        if(RandomUtils.nextInt(RANDOM_LOWER_BOUND, RANDOM_UPPER_BOUND) >= 4) {
+            position++;
+            return;
+        }
+        return;
+    }
+
 }
