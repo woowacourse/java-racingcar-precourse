de5ddcd43347de4257490a692322c2ed10200eeb
sounmind <sounmind777@gmail.com>
2020-12-03 19:35:51 +0900

    style(Application, GameManager): 메서드 이름 동사 중심으로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,6 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         GameManager gameManager = new GameManager();
-        gameManager.gameStart(scanner);
+        gameManager.startGame(scanner);
     }
 }
