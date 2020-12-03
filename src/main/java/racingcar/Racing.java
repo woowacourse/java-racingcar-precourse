b630298ff7a89ef7cf18b8902ef425e8c605df80
eoc940 <hgk0000@naver.com>
2020-12-04 02:59:31 +0900

    feat(racing): 입력값으로 car객체들을 생성하여 리스트로 반환하는 makeCars 구현

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,4 +1,17 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Racing {
+
+    public List<Car> makeCars(List<String> carNames) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            carList.add(car);
+        }
+        return carList;
+    }
+
 }
