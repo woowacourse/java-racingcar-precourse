490d23929b28025a6496c9894b5cf32b7ca5c0ab
이동건 <tnpdlzi@naver.com>
2020-12-03 16:55:48 +0900

    feat(GameUtils): 시도 횟수가 숫자인지 판별

--- src/main/java/utils/GameUtils.java
+++ src/main/java/utils/GameUtils.java
@@ -2,6 +2,7 @@ package utils;
 
 public class GameUtils {
     private static final String NAME_OVER_MESSAGE = "[ERROR] 차 이름은 5글자 이하여야 한다.";
+    private static final String NUM_OF_RUNS_NUMBER_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
 
     private GameUtils() {
     }
@@ -12,7 +13,14 @@ public class GameUtils {
         }
     }
 
-    public Boolean isNumOfRunsRight() {
-        return true;
+    public static int isNumOfRunsRight(String numberOfRuns) {
+        int runs;
+        try {
+            runs = Integer.parseInt(numberOfRuns);
+        } catch (Exception e){
+            throw new IllegalArgumentException(NUM_OF_RUNS_NUMBER_MESSAGE);
+        }
+
+        return runs;
     }
 }
