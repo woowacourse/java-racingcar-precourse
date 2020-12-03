106e56442c4d2e0246064555c128b00794e8754e
Younghoon Kwon <younghoon.kwon91@gmail.com>
2020-12-03 15:12:53 +0900

    feat: 자동차 경주 승자 판결 메서드 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,12 +1,13 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Cars {
     List<Car> cars = new ArrayList<>();
 
-    public void makeCars(String userInput){
+    public void makeCars(String userInput) {
         String[] users = userInput.split(",");
 
         for (String user : users) {
@@ -15,12 +16,48 @@ public class Cars {
         }
     }
 
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
     public List<Car> getCars() {
         return cars;
     }
 
-    public void addCars(Car car){
+    public void addCars(Car car) {
         this.cars.add(car);
     }
 
+    public void runCars() {
+        for (Car car : cars) {
+            car.moveCar();
+        }
+    }
+
+    public ArrayList<Integer> getCarsPosition() {
+        ArrayList<Integer> carsPosition = new ArrayList<>();
+        for (Car car : cars) {
+            carsPosition.add(car.getPosition());
+        }
+        return carsPosition;
+    }
+
+    public ArrayList<String> getCarsName() {
+        ArrayList<String> carsName = new ArrayList<>();
+        for (Car car : cars) {
+            carsName.add(car.getName());
+        }
+        return carsName;
+    }
+
+    public void selectWinners() {
+        int winnerPosition = Collections.max(getCars()).getPosition();
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == winnerPosition) {
+                winners.add(car);
+            }
+        }
+        this.cars = winners;
+    }
 }
