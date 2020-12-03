568cd580e36a4851b41db61d9e8664260c063953
Jack2ee-dev <jack2ee.dev@gmail.com>
2020-12-03 21:35:41 +0900

    feat(inputView): implement function 1

--- src/main/java/racingcar/view/inputView.java
+++ src/main/java/racingcar/view/inputView.java
@@ -1,4 +1,19 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class inputView {
+    private static final String INPUT_CAR_NAMES_PHRASE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+
+    private Scanner scanner;
+
+    public inputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public List<String> getCarNames() {
+        System.out.print(INPUT_CAR_NAMES_PHRASE);
+        return null;
+    }
 }
