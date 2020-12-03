4e23a2b8b98357100b0b6d42eca9ad84ddc0a99e
이동건 <tnpdlzi@naver.com>
2020-12-03 15:42:47 +0900

    feat(RacingGame): 레이싱 게임 틀 작성

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class RacingGame {
+    Scanner scanner;
+    List<Car> racingCars;
+
+    public RacingGame(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void play() {
+    }
+
+    public void registerCars() {
+    }
+
+    public void getNumberOfRunsFromUser() {
+    }
+
+    public void findWinner() {
+    }
+
+    public void printWinner() {
+    }
+
+    public void printCars() {
+    }
+}
