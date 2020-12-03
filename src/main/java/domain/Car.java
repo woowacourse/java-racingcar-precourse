0c7559c9d54219f941b376ea33acb357b7461eb9
tomas <cmg14111@gmail.com>
2020-12-04 00:23:39 +0900

    feat(Car): 자동차가 움직이는 기능 구현, Car클래스 위치 이동
    before:
    src/java/racingcar
    
    after:
    src/java/domain

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,8 +1,10 @@
-package racingcar;
+package domain;
 
 public class Car {
     private static final int NAME_MAX_LENGTH = 5;
+    private static final int FOUR_TO_NINE_GO = 4;
     private static final String LENGTH_OVER_EXCEPTION_MESSAGE = "[ERROR] 이름은 5자 이하여야 합니다.";
+
     private final String name;
     private int position = 0;
 
@@ -12,8 +14,15 @@ public class Car {
     }
 
     private void isValidName(String name) {
-        if(name.length() > NAME_MAX_LENGTH) {
+        if (name.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException(LENGTH_OVER_EXCEPTION_MESSAGE);
+        }
+    }
+
+    // 0~9의 랜덤한 숫자를 매개변수로 입력
+    public void moveCar(int randomNumber) {
+        if (randomNumber >= FOUR_TO_NINE_GO) {
+            this.position += 1;
         }
     }
 }
