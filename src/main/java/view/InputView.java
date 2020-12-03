1148c4a61508f0d61b1cdb9463da279ce9354b21
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-03 18:13:53 +0900

    feat: 참가할 플레이어 입력 받는 기능 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String inputPlayerName(Scanner scanner) {
+        String next = scanner.next();
+
+        return next;
+    }
+}
