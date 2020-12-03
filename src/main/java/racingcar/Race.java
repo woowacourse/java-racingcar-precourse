f9435c8453a0d6733c89b6b627ebf9cc09d0e3cd
김주원 <kjw11077@naver.com>
2020-12-03 16:03:10 +0900

    feat(Race): add method getRandomInt

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -2,8 +2,12 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import utils.RandomUtils;
 
 public class Race {
+
+    public static final int RANDOM_START = 0;
+    public static final int RANDOM_END = 9;
 
     public static GameResult start(List<Car> cars, int tryCount) {
         // todo 각 차수별 실행 결과 구하기
@@ -12,10 +16,16 @@ public class Race {
         System.out.println("%n실행 결과");
         for (int i = 0; i < tryCount; i++) {
             for (Car car : cars) {
+                int ranNum = getRandomInt();
+
 
             }
         }
 
         return new GameResult(new Winners(winners));
+    }
+
+    private static int getRandomInt() {
+        return RandomUtils.nextInt(RANDOM_START, RANDOM_END);
     }
 }
