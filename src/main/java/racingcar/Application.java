b32b5b1c8e4ac8b5e658440ef5b197b36803111e
mungto <daum7766@naver.com>
2020-12-05 00:14:16 +0900

    feat(racingcar): update function createCar
    A function that receives the car name and creates multiple car objects

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -28,7 +28,11 @@ public class Application {
     }
 
     private static Car[] createCars(String[] carNames) {
-        return new Car[0];
+        Car[] cars = new Car[carNames.length];
+        for (int i = 0; i < carNames.length; i++) {
+            cars[i] = new Car(carNames[i]);
+        }
+        return cars;
     }
 
     private static int findMaxPosition(Car[] cars) {
