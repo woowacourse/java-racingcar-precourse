798473d3439ee28d7dc57bba1212b2641e043a01
Livenow14 <xowjd41@naver.com>
2020-12-04 13:28:23 +0900

    feat: 보장된 이름이 아닐 시 에러 출력하기
    숫자, 특수문자, 스페이스, 한글, 5자 초과일 시 에러 출력 테스트 완료

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,10 +6,30 @@ public class Car {
 
     public Car(String name) {
         this.name = name;
+        validateCarName();
     }
 
     // 추가 기능 구현
     public String getCarName() {
         return name;
+    }
+
+    private void validateCarName() {
+        if (this.name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 이름은 5자 이하만 입력 해주세요.");
+        }
+        for (int i = 0; i < this.name.length(); i++) {
+            char targetChar = this.name.charAt(i);
+            validateSingleChar(targetChar);
+        }
+    }
+
+    private void validateSingleChar(char targetChar) {
+        if (targetChar == 32) {
+            throw new IllegalArgumentException("[ERROR] 이름에 스페이스바는 허용되지 않습니다.");
+        }
+        if (targetChar < 65 || (targetChar > 90 && targetChar < 97) || targetChar > 122) {
+            throw new IllegalArgumentException("[ERROR] 이름에는 대,소문 영문자만 입력 해주세요.");
+        }
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -43,4 +43,100 @@ class CarTest {
         assertThat(cars.get(1).getCarName()).isEqualTo("now");
         assertThat(cars.get(2).getCarName()).isEqualTo("here");
     }
+
+    @Test
+    @DisplayName("Car 객체 생성 시 보장된 이름이 들어간다")
+    void testCarInitialize() {
+        //given
+        String nameHello = "Hello";
+        String nameWorld = "World";
+
+        //when
+        Car car = new Car(nameHello);
+        Car car2 = new Car(nameWorld);
+
+        //then
+        assertThat(car.getCarName()).isEqualTo(nameHello);
+        assertThat(car2.getCarName()).isEqualTo(nameWorld);
+    }
+
+    @Test
+    @DisplayName("Car 객체 생성 시 이름이 5글자 이상이면 에러가 발생한다.")
+    void testCarInitializeLengthError() {
+        //given
+        String nameHello = "Hello!";
+        String nameWorld = "World!";
+
+        //then
+        assertThatThrownBy(() -> new Car(nameHello))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름은 5자 이하만 입력 해주세요.");
+        assertThatThrownBy(() -> new Car(nameWorld))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름은 5자 이하만 입력 해주세요.");
+    }
+
+    @Test
+    @DisplayName("Car 객체 생성 시 이름에 숫자가 들어가면 에러가 발생한다.")
+    void testCarInitializeNumberError() {
+        //given
+        String nameHi = "Hi12";
+        String nameBye = "Bye34";
+
+        //then
+        assertThatThrownBy(() -> new Car(nameHi))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에는 대,소문 영문자만 입력 해주세요.");
+        assertThatThrownBy(() -> new Car(nameBye))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에는 대,소문 영문자만 입력 해주세요.");
+    }
+
+    @Test
+    @DisplayName("Car 객체 생성 시 이름에 특수문자가 들어가면 에러가 발생한다.")
+    void testCarInitializeSpecialCharacterError() {
+        //given
+        String nameHi = "Hi!@#";
+        String nameBye = "Bye$%";
+
+        //then
+        assertThatThrownBy(() -> new Car(nameHi))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에는 대,소문 영문자만 입력 해주세요.");
+        assertThatThrownBy(() -> new Car(nameBye))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에는 대,소문 영문자만 입력 해주세요.");
+    }
+
+    @Test
+    @DisplayName("Car 객체 생성 시 이름에 스페이스가 들어가면 에러가 발생한다.")
+    void testCarInitializeSpaceError() {
+        //given
+        String nameHi = "H i";
+        String nameBye = "B ye";
+
+        //then
+        assertThatThrownBy(() -> new Car(nameHi))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에 스페이스바는 허용되지 않습니다.");
+        assertThatThrownBy(() -> new Car(nameBye))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에 스페이스바는 허용되지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("Car 객체 생성 시 이름에 한글이 들어가면 에러가 발생한다.")
+    void testCarInitializeKoreanError() {
+        //given
+        String nameHi = "ㅎe";
+        String nameBye = "바이";
+
+        //then
+        assertThatThrownBy(() -> new Car(nameHi))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에는 대,소문 영문자만 입력 해주세요.");
+        assertThatThrownBy(() -> new Car(nameBye))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름에는 대,소문 영문자만 입력 해주세요.");
+    }
 }
