56c317822f874e86d7860a3af2e1732d14e872c8
김경록 <goodboy3421@gmail.com>
2020-12-05 04:18:12 +0900

    feat: 우승자를 출력하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,12 +3,23 @@ package racingcar.view;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Cars;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class OutputView {
     private static final String DELIMITER_COLON = " : ";
     private static final String EACH_POSITION_SYMBOL = "-";
+    private static final String DELIMITER_COMMA = ", ";
+    private static final String WINNER_INFO_MESSAGE = "최종 우승자: ";
+
+    public static void printWinners(List<Car> winners) {
+        System.out.print(WINNER_INFO_MESSAGE);
+        String printWinner = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(DELIMITER_COMMA));
+        System.out.println(printWinner);
+    }
 
     public static void printCars(Cars cars) {
         cars.getValue().stream()
