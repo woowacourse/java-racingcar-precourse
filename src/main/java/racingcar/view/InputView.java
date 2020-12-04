c3e0d9ad9d9244ef866e369a30a8a53f8676f390
김경록 <goodboy3421@gmail.com>
2020-12-04 21:33:57 +0900

    refactor: 문자를 입력받을 경우 예외를 던지는 기능 stream 코드로 변경
    chars() 함수를 이용하여 String 타입을 IntStream으로 변경하는 방식으로 코드 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String DELIMITER_COMMA = ",";
-    private static final String ONLY_INPUT_NUMBER_MESSAGE = "숫자만 입력할 수 있습니다.";
+    private static final String ONLY_INPUT_NUMBER_MESSAGE = "[ERROR] 숫자만 입력할 수 있습니다.";
     private static final String INPUT_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static List<Car> inputCarNames(Scanner scanner) {
@@ -42,12 +42,10 @@ public class InputView {
     }
 
     private static void validateTryNumberType(String tryNumber) {
-        char[] eachTryNumbers = tryNumber.toCharArray();
-        for (char eachTryNumber : eachTryNumbers) {
-            if (Character.isDigit(eachTryNumber)) {
-                continue;
-            }
+        boolean numberTypeOk = tryNumber.chars()
+                .allMatch(Character::isDigit);
 
+        if (!numberTypeOk) {
             throw new IllegalArgumentException(ONLY_INPUT_NUMBER_MESSAGE);
         }
     }
