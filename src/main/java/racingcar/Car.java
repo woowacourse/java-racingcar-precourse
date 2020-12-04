d08df21236ede67ecea68649ae3e7956acae4d4c
DH Lim <dlarkfl2@gamil.com>
2020-12-04 13:43:18 +0900

    feat(Car): 랜덤숫자 생성, 전진 후 위치변경 기능 추가
    Car 클래스 주요 수정 내용
    - generateRandomNumber() 메소드를 통해 랜덤숫자 생성
    - moveForwardOrStop() 메소드를 통해 전진조건에 따라 position 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,35 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
-    private int position = 0;
+    private int position;
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    static final int RANDOM_NUMBER_INCLUSIVE_START = 0;
+    static final int RANDOM_NUMBER_INCLUSIVE_END = 9;
+    static final int MOVE_FORWARD_NUMBER_INCLUSIVE_START = 4;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveForwardOrStop() {
+        if (generateRandomNumber() >= MOVE_FORWARD_NUMBER_INCLUSIVE_START) {
+            position++;
+        }
+    }
+
+    private int generateRandomNumber() {
+        return RandomUtils.nextInt(RANDOM_NUMBER_INCLUSIVE_START, RANDOM_NUMBER_INCLUSIVE_END);
+    }
 }
