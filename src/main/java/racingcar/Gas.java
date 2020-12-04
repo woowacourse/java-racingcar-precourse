e986952ff358bfa6096271c0081cbccb365fcb6a
Hyeon9mak <jinha3507@gmail.com>
2020-12-04 13:33:30 +0900

    feat(Gas): add Gas object

--- src/main/java/racingcar/Gas.java
+++ src/main/java/racingcar/Gas.java
@@ -1,2 +1,13 @@
-package racingcar;public class Gas {
+package racingcar;
+
+import utils.RandomUtils;
+
+public class Gas {
+
+    private static final int MIN_GAS = 0;
+    private static final int MAX_GAS = 9;
+
+    public int getChargeGas() {
+        return RandomUtils.nextInt(MIN_GAS, MAX_GAS);
+    }
 }
