e4245b980bdf3704e2da65af3b5ebe28f42b4b8c
Gayoung <gyyoon4u@naver.com>
2020-12-04 15:28:43 +0900

    feat: 자동차이름에 빈칸 포함되면 예외처리

--- src/main/java/racingcar/InputCarNameCheck.java
+++ src/main/java/racingcar/InputCarNameCheck.java
@@ -14,15 +14,21 @@ public class InputCarNameCheck {
             return false;
         }
         if (!checkCarNameDuplicate(input)) {
+            System.out.println(Arrays.toString(input));
             return false;
         }
+        if (!checkAllCarNameHasBlank(input)) {
+            System.out.println(Arrays.toString(input));
+            return false;
+        }
+
         return true;
     }
 
     public static boolean checkAllCarNameLength(String[] carNames) {
         for (String name : carNames) {
             if (!checkEachCarNameLength(name)) {
-                System.out.println("[ERROR] 자동차 이름은 5글자 이하로 입력해야합니다.");
+                System.out.println("[ERROR] 자동차 이름은 1글자 이상 5글자 이하로 입력해야합니다.");
                 return false;
             }
         }
@@ -35,11 +41,28 @@ public class InputCarNameCheck {
 
     public static boolean checkCarNameDuplicate(String[] carNames) {
         Arrays.sort(carNames);
-        for (int i=0; i < carNames.length-1; i++) {
-            if (carNames[i].equals(carNames[i+1])) {
+        for (int i = 0; i < carNames.length - 1; i++) {
+            if (carNames[i].equals(carNames[i + 1])) {
                 System.out.println("[ERROR] 자동차 이름은 중복 작성할 수 없습니다.");
                 return false;
             }
+        }
+        return true;
+    }
+
+    public static boolean checkAllCarNameHasBlank(String[] carNames) {
+        for (String name : carNames) {
+            if (!checkEachCarNameHasBlank(name)) {
+                System.out.println("[ERROR] 자동차 이름에는 빈칸을 입력할 수 없습니다.");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkEachCarNameHasBlank (String name){
+        if (name.contains(" ")) {
+            return false;
         }
         return true;
     }
