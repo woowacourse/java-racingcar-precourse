9f5ce87b066837f77603a07b54c91ce25ce80d1e
tomas <cmg14111@gmail.com>
2020-12-04 20:48:51 +0900

    feat(Output): 승자를 출력하는 기능 구현

--- src/main/java/domain/RaceWinner.java
+++ src/main/java/domain/RaceWinner.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 public class RaceWinner {
     List<Car> raceWinners = new ArrayList<>();
-    int maxPosition = 0;
 
     public void addRaceWinner(Car car) {
         raceWinners.add(car);
+    }
+
+    public List<Car> getRaceWinners() {
+        return raceWinners;
     }
 }
--- src/main/java/views/OutputView.java
+++ src/main/java/views/OutputView.java
@@ -1,11 +1,16 @@
 package views;
 
 import domain.Car;
+import domain.RaceWinner;
 import domain.RacingCars;
 
 public class OutputView {
     private final static String COLON = " : ";
     private final static String HYPHEN = "-";
+    private final static String FINAL_WINNER = "최종 우승자: ";
+    private final static String NAME_SEPARATOR = ", ";
+    private final static int START_INDEX = 0;
+    private final static int END_INDEX = 1;
 
     public static void printOneRound(RacingCars racingCars) {
         for (Car car : racingCars.getCarList()) {
@@ -21,5 +26,19 @@ public class OutputView {
         for (int i = 1; i <= position; i++) {
             System.out.print(HYPHEN);
         }
+    }
+
+    public static void printWinner(RaceWinner raceWinner) {
+        System.out.print(FINAL_WINNER);
+        System.out.println(makeWinnerString(raceWinner));
+    }
+
+    private static String makeWinnerString(RaceWinner raceWinner) {
+        String winnerString = new String();
+        for (Car car : raceWinner.getRaceWinners()) {
+            winnerString += car.getName();
+            winnerString += NAME_SEPARATOR;
+        }
+        return winnerString.substring(START_INDEX, winnerString.length()-END_INDEX);
     }
 }
