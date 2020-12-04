e1d183a81c8e11034c21a1ec475817ecf788133d
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-04 13:45:25 +0900

    style: Car 객체 생성/실패 테스트 메서드 명 구체화
    명확하게 어떤 테스트인지 두괄식으로 서술하며,
    메서드는 동사로 마무리한다.

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -10,20 +10,20 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 class CarTest {
 
-    @DisplayName("이름 문자열의 길이가 1~5인 경우")
+    @DisplayName("Car 생성 성공 : 이름 문자열의 길이가 1~5인 경우")
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "crong", "a", "b1"})
-    public void Car_유효한_이름_문자열_객체_생성_성공(String name) {
+    public void Car_유효한_이름_문자열_객체_생성_성공한다(String name) {
         assertThatCode(() -> {
             new Car(name);
         }).doesNotThrowAnyException();
     }
 
-    @DisplayName("이름 문자열이 공백이거나, 빈 문자열이거나, null인 경우")
+    @DisplayName("Car 생성 예외 : 이름 문자열이 공백이거나, 빈 문자열이거나, null인 경우")
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"  \t", " "})
-    public void Car_유효하지_않은_이름_문자열_예외_발생(String name) {
+    public void Car_유효하지_않은_이름_문자열_예외_발생한다(String name) {
         assertThatCode(() -> {
             new Car(name);
         }).isInstanceOf(CarNameLengthException.class);
