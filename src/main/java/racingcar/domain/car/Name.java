49eb86352e4ffbd64a8dbc7b26db5cac6f31ae9d
김경록 <goodboy3421@gmail.com>
2020-12-05 08:16:54 +0900

    refactor: Name 객체의 상수명 변경
    Car에서 Name으로 책임을 이동하면서 따라온 상수또한 리팩토링

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -1,11 +1,11 @@
 package racingcar.domain.car;
 
 public class Name {
-    private static final int CAR_NAME_MIN_LENGTH_LIMIT = 1;
-    private static final int CAR_NAME_MAX_LENGTH_LIMIT = 5;
-    private static final String CAR_NAME_LENGTH_OVER_MESSAGE = "[ERROR] 자동차의 이름은 최소 1글자 최대 5글자만 가능합니다.";
+    private static final int NAME_MIN_LENGTH_LIMIT = 1;
+    private static final int NAME_MAX_LENGTH_LIMIT = 5;
+    private static final String NAME_LENGTH_OVER_MESSAGE = "[ERROR] 자동차의 이름은 최소 1글자 최대 5글자만 가능합니다.";
     private static final String BLANK = " ";
-    private static final String CAR_NAME_NOT_CONTAIN_BLANK_MESSAGE = "[ERROR] 자동차 이름은 공백을 포함할 수 없습니다.";
+    private static final String NAME_NOT_CONTAIN_BLANK_MESSAGE = "[ERROR] 자동차 이름은 공백을 포함할 수 없습니다.";
 
     private final String name;
 
@@ -20,14 +20,14 @@ public class Name {
     }
 
     private void validateNameLength(String name) {
-        if (name.length() > CAR_NAME_MAX_LENGTH_LIMIT || name.length() < CAR_NAME_MIN_LENGTH_LIMIT) {
-            throw new IllegalArgumentException(CAR_NAME_LENGTH_OVER_MESSAGE);
+        if (name.length() > NAME_MAX_LENGTH_LIMIT || name.length() < NAME_MIN_LENGTH_LIMIT) {
+            throw new IllegalArgumentException(NAME_LENGTH_OVER_MESSAGE);
         }
     }
 
     private void validateNameContainBlank(String name) {
         if (name.contains(BLANK)) {
-            throw new IllegalArgumentException(CAR_NAME_NOT_CONTAIN_BLANK_MESSAGE);
+            throw new IllegalArgumentException(NAME_NOT_CONTAIN_BLANK_MESSAGE);
         }
     }
 
