8aee625e89349c6829881641264cd20fe72d7e73
김경록 <goodboy3421@gmail.com>
2020-12-05 05:03:06 +0900

    refactor: 자동차의 이름을 입력받는 기능의 리턴 타입을 Cars 객체로 변경
    Cars 객체를 생성하는 시점에 유효성을 검사하기 위해서 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,10 +1,10 @@
 package racingcar.view;
 
 import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
 import racingcar.domain.trynumber.TryNumber;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -12,15 +12,19 @@ public class InputView {
     private static final String DELIMITER_COMMA = ",";
     private static final String ONLY_INPUT_NUMBER_MESSAGE = "[ERROR] 숫자만 입력할 수 있습니다.";
     private static final String INPUT_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_RACING_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public static List<Car> inputCarNames(Scanner scanner) {
+    public static Cars inputCarNames(Scanner scanner) {
+        System.out.println(INPUT_RACING_CAR_NAMES_MESSAGE);
         String carNames = scanner.nextLine();
 
         try {
-            return Arrays.stream(carNames.split(DELIMITER_COMMA))
-                    .map(String::trim)
-                    .map(Car::new)
-                    .collect(Collectors.toList());
+            return new Cars(
+                    Arrays.stream(carNames.split(DELIMITER_COMMA))
+                            .map(String::trim)
+                            .map(Car::new)
+                            .collect(Collectors.toList())
+            );
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputCarNames(scanner);
@@ -30,6 +34,7 @@ public class InputView {
     public static TryNumber inputTryNumber(Scanner scanner) {
         System.out.println(INPUT_TRY_NUMBER_MESSAGE);
         String tryNumber = scanner.nextLine();
+        System.out.println();
 
         try {
             validateTryNumberType(tryNumber);
