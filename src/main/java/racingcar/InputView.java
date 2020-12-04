c8f9a47ae326dcff27ca7ea7ba321ca06788b84f
Wannte <T.wannte@gmail.com>
2020-12-04 22:59:47 +0900

    style(InputView): 불필요한 static 삭제, 이름 길이 변수를 따로 분리

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,13 +4,14 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class InputView {
-    private static final String ERROR_NOT_INTEGER = "[ERROR] 시도 횟수는 숫자여야 한다.";
-    private static final String ERROR_NOT_POSITIVE_INTEGER = "[ERROR] 시도 횟수는 양수여야 한다.";
-    private static final String ERROR_BLANK_INPUT = "[ERROR] 입력값이 공백이면 안된다.";
-    private static final String ERROR_ZERO_PLAYER = "[ERROR] 참가자 수는 한명 이상이여야 한다.";
-    private static final String ERROR_BLANK_NAME = "[ERROR] 이름은 공백이면 안된다.";
-    private static final String ERROR_LENGTH_OVER_FIVE_NAME = "[ERROR] 이름은 5글자 이하여야 한다.";
-    private static final String ERROR_DUPLICATE_NAME = "[ERROR] 이름이 같은 참가자가 있으면 안된다.";
+    private final  int NAME_LENGTH_STANDARD = 5;
+    private final String ERROR_NOT_INTEGER = "[ERROR] 시도 횟수는 숫자여야 한다.";
+    private final String ERROR_NOT_POSITIVE_INTEGER = "[ERROR] 시도 횟수는 양수여야 한다.";
+    private final String ERROR_BLANK_INPUT = "[ERROR] 입력값이 공백이면 안된다.";
+    private final String ERROR_ZERO_PLAYER = "[ERROR] 참가자 수는 한명 이상이여야 한다.";
+    private final String ERROR_BLANK_NAME = "[ERROR] 이름은 공백이면 안된다.";
+    private final String ERROR_NAME_LENGTH = "[ERROR] 이름은 5글자 이하여야 한다.";
+    private final String ERROR_DUPLICATE_NAME = "[ERROR] 이름이 같은 참가자가 있으면 안된다.";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -20,9 +21,8 @@ public class InputView {
     public List<String> getCarNames(){
         List<String> names = getCarNames(scanner.nextLine());
         validateZeroPlayer(names);
-        //validateNameLength
         validateBlankName(names);
-        validateLengthOverFiveName(names);
+        validateNameLength(names);
         validateDuplicate(names);
         return names;
     }
@@ -70,13 +70,13 @@ public class InputView {
         }
     }
 
-    private void validateLengthOverFiveName(List<String> names) {
-        names.forEach(this::validateLengthOverFiveName);
+    private void validateNameLength(List<String> names) {
+        names.forEach(this::validateNameLength);
     }
 
-    private void validateLengthOverFiveName(String name) {
-        if (name.length() > 5){
-            throw new IllegalArgumentException(ERROR_LENGTH_OVER_FIVE_NAME);
+    private void validateNameLength(String name) {
+        if (name.length() > NAME_LENGTH_STANDARD){
+            throw new IllegalArgumentException(ERROR_NAME_LENGTH);
         }
     }
 
