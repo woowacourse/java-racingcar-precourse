dbcf4b95b6a223aed2765c01c6cd0e9a15b1d5ce
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-04 21:17:32 +0900

    [구현] 사용자가 입력한 차 이름 문자열이 유효한지 검증하는 함수

--- src/main/java/InputView/InputView.java
+++ src/main/java/InputView/InputView.java
@@ -1,10 +1,23 @@
 package InputView;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    final Scanner scanner;
+    int VALID_MINIMUM_INPUT_LENGTH = 2;
+    private final Scanner scanner;
 
-    InputView(final Scanner scanner){
+    InputView(Scanner scanner) {
         this.scanner = scanner;
     }
+
+
+
+    private boolean isValidInput(String inputCarNames){
+        if(inputCarNames.length() < VALID_MINIMUM_INPUT_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
 }
