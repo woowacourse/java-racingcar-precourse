4409301b1ca39434621c6a2ac18ac62650bdc8c7
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-04 13:24:36 +0900

    test: 이름 문자열에 따른 Car 객체 생성 성공 및 실패 예외 테스트 추가

---
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -0,0 +1,31 @@
+package racingcar.domain.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.exception.CarNameLengthException;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class CarTest {
+
+    @DisplayName("이름 문자열의 길이가 1~5인 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "crong", "a", "b1"})
+    public void Car_유효한_이름_문자열_객체_생성_성공(String name) {
+        assertThatCode(() -> {
+            new Car(name);
+        }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("이름 문자열이 공백이거나, 빈 문자열이거나, null인 경우")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  \t", " "})
+    public void Car_유효하지_않은_이름_문자열_예외_발생(String name) {
+        assertThatCode(() -> {
+            new Car(name);
+        }).isInstanceOf(CarNameLengthException.class);
+    }
+}
