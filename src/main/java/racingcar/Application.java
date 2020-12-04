a07a04939aeff406d61b7df0acb36253211110f2
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-04 18:10:08 +0900

    feat(Application): 게임 실행 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,5 +6,10 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        RacingGame racingGame = new RacingGame();
+
+        racingGame.readyGame();
+        racingGame.playGame();
+        racingGame.displayWinner();
     }
 }
