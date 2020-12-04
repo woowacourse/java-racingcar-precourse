05dea88c861f12ff871ec1fe035a2001a2ec2b27
최문경 <munkyung0324@gmail.com>
2020-12-05 01:51:30 +0900

    feat(Application): split cars by ','
    Application:
    - split car input list by ','
    README:
    - fix typo in README.md

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,14 @@
 package racingcar;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        String carsInput = scanner.next();
+        String[] cars = carsInput.split(",");
+        System.out.println(Arrays.toString(cars));
     }
 }
