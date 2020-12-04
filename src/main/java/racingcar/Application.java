a38b3399cb37a33e660de2afacdd31c7b1aa899e
mungto <daum7766@naver.com>
2020-12-04 23:00:54 +0900

    feat(racingcar): update function inputCarName
    A function that takes user input and returns the car name

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class Application {
 
-    private static String[] inputCarNames() {
-        return new String[0];
+    private static String[] inputCarNames(Scanner scanner) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = scanner.next();
+        return names.split(",");
     }
 
     private static int inputNumberOfAttempts() {
@@ -28,7 +30,7 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        String[] carNames = inputCarNames();
+        String[] carNames = inputCarNames(scanner);
         int attemptNumber = inputNumberOfAttempts();
         Car[] cars = createCars(carNames);
         boolean isNotFinish = true;
