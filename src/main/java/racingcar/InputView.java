629c12860372fbdee868b45e5501f254995d2e67
awesomeo184 <awesomeo184@gmail.com>
2020-12-05 02:53:24 +0900

    feat(InputView): 차 이름 입력 및 유효성 검사 구현
    - 이름이 5자를 초과하거나 빈 문자를 입력하는 경우 에러 메시지 출력

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,61 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String SEPERATOR = ",";
+    private static final String INVALID_CAR_NAME_ERROR_MESSAGE = "[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.";
+    private static final String INVALID_PLAYER_NUMBER_ERROR_MESSAGE = "[ERROR] 최소 한 개 이상의 이름을 입력해주세요.";
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MIN_PLAYER_NUMBER = 1;
+
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String[] getCarList() {
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        String[] carNames = getNames();
+        if (!isValidCar(carNames)) {
+            carNames = getCarList();
+        }
+        return carNames;
+    }
+
+    private String getInput() {
+        return scanner.nextLine();
+    }
+
+    private String[] getNames() {
+        return getInput().split(SEPERATOR);
+    }
+
+    private boolean isValidCar(String[] carNames) {
+        return isValidNameLength(carNames) && isValidPlayerNumber(carNames);
+    }
+
+    private boolean isValidNameLength(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.length() < MIN_NAME_LENGTH || carName.length() > MAX_NAME_LENGTH) {
+                System.out.println(INVALID_CAR_NAME_ERROR_MESSAGE);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isValidPlayerNumber(String[] carNames) {
+        if (carNames.length < MIN_PLAYER_NUMBER) {
+            System.out.println(INVALID_PLAYER_NUMBER_ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+}
