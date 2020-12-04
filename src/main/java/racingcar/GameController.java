fffe6c9cc77ab2aaf36b0bc66812a18c5387a573
Injun <ison@sfu.ca>
2020-12-04 14:27:06 +0900

    feat: GameController 클래스에 generateCars 구현

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -6,8 +6,16 @@ public class GameController {
     public static void playGame(Scanner scanner) {
         String[] carNames = GameInput.getCarNames(scanner);
         int gameRound = GameInput.getGameRound(scanner);
-//        Cars gameCars = Cars.generateCars(carNames);
+        Car[] gameCars = generateCars(carNames);
 //        Simulator.simulateRace(gameCars, gameRound);
 //        Referee.judge(gameCars);
+    }
+
+    public static Car[] generateCars(String[] carNames) {
+        Car[] gameCars = new Car[carNames.length];
+        for (int i=0; i < carNames.length; i++){
+            gameCars[i] = new Car(carNames[i]);
+        }
+        return gameCars;
     }
 }
