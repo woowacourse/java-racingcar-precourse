df9935e16cc37b232789405928c01ab3ec26f712
김경록 <goodboy3421@gmail.com>
2020-12-05 05:16:54 +0900

    feat: main(Controller) 함수에 자동차 경주 게임기능 조합

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,22 @@
 package racingcar;
 
+import racingcar.domain.car.Cars;
+import racingcar.domain.car.DefaultCarMovement;
+import racingcar.domain.racinggame.RacingGame;
+import racingcar.domain.trynumber.TryNumber;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        Cars cars = InputView.inputCarNames(scanner);
+        TryNumber tryNumber = InputView.inputTryNumber(scanner);
+
+        RacingGame racingGame = new RacingGame(cars, tryNumber);
+        OutputView.printProcess(racingGame.play(new DefaultCarMovement()));
+        OutputView.printWinners(racingGame.findWinners());
     }
 }
