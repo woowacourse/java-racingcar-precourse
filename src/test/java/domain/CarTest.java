2c5fdc2348f9423c0265a34fc42caf727cf3f429
tomas <cmg14111@gmail.com>
2020-12-04 18:06:26 +0900

    test(CarTest): 비정상적 입력으로 자동차 이름 입력시 예외 발생 테스트 작성
    이름이 5자 이상일 때, 이름이 비었을 때의 예외처리 테스트
    이름이 비었을 때의 예외처리가 없음

---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    private final static String LONG_NAME = "asdfas";
+    private final static String EMPTY_NAME = "";
+    private final static String TOO_LONG_NAME = "[ERROR] 이름은 5자 이하여야 합니다.";
+    private final static String EMPTY_NAME_EXCEPTION = "[ERROR] 빈 이름이 있습니다.";
+
+    @Test
+    public void makeLongNameCarTest() {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Car(LONG_NAME);
+        });
+        Assertions.assertEquals(TOO_LONG_NAME, exception.getMessage());
+    }
+
+    @Test
+    public void makeEmptyNameCarTest() {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Car(EMPTY_NAME);
+        });
+        Assertions.assertEquals(EMPTY_NAME_EXCEPTION, exception.getMessage());
+    }
+}
