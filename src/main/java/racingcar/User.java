3198d3d4a877607bd770146d374c318f4dc12b7c
yhh1056 <yhh1056@naver.com>
2020-12-04 13:35:30 +0900

    Feat: 이름에 공백이 존재할 경우 예외 처리

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -11,15 +11,20 @@ public class User {
     public static final String DELIMITER = ",";
     private static final String WINNER = "최종우승자: ";
     private static final String SPACE = " ";
+    private static final CharSequence TAB = "\t";
 
     public List<String> createNames(String names) {
+        String[] splitNames = names.split(DELIMITER);
         validateName(names);
-        return Arrays.asList(names.split(DELIMITER));
+        return Arrays.asList(splitNames);
     }
 
     private void validateName(String names) {
         if (names.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 이름이 존재하지 않습니다.");
+        }
+        if (names.contains(SPACE) || names.contains(TAB)) {
+            throw new IllegalArgumentException("[ERROR] 이름에 공백이 존재합니다.");
         }
     }
 
