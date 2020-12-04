6c6f556975350a0f5949f5578f627c362a50c9dc
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-04 18:06:25 +0900

    feat(RacingGame): 우승자 구하고 출력하는 함수 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import racingcar.InputView;
@@ -9,6 +10,7 @@ public class RacingGame {
     private String attemptsInput;
     private List<Car> cars;
     private int attemptsNum;
+    private static String WINNER_MESSAGE = "최종 우승자: ";
 
     public void readyGame() {
         userInput = InputView.askCarName();
@@ -27,7 +29,6 @@ public class RacingGame {
             setCarPosition(cars);
             Car.displayPosition(cars);
         }
-
     }
 
     public static void setCarPosition(List<Car> cars) {
@@ -36,4 +37,11 @@ public class RacingGame {
         }
     }
 
+    public void displayWinner() {
+        List<String> winnerList = Car.getWinner(cars);
+        String[] winnerArr = winnerList.toArray(new String[winnerList.size()]);
+
+        String winner = String.join(", ", winnerArr);
+        System.out.print(WINNER_MESSAGE + winner);
+    }
 }
