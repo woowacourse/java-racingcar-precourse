f3d1b7511610aeef84854c46f5cd09d8c3b588a2
Hyeon9mak <jinha3507@gmail.com>
2020-12-04 11:52:18 +0900

    feat(Track): add Track object

--- src/main/java/racingcar/Track.java
+++ src/main/java/racingcar/Track.java
@@ -1,2 +1,17 @@
-package racingcar;public class Track {
+package racingcar;
+
+import java.util.ArrayList;
+
+public class Track {
+
+    private ArrayList<Car> cars = new ArrayList<Car>();
+    private int loopTime;
+
+    public Track(int loopTime) {
+        this.loopTime = loopTime;
+    }
+
+    public void enterTrack(Car car){
+        cars.add(car);
+    }
 }
