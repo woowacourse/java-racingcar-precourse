06fe226ee16da781cde06112abd2ecec077676f9
NewWisdom <wlgp2500@gmail.com>
2020-12-04 14:36:38 +0900

    feat(printer): add print-output-final-winner method

--- src/main/java/racingcar/Printer.java
+++ src/main/java/racingcar/Printer.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import java.util.List;
+
 public class Printer {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_NUMBER_MESSAGE = "시도할 횟수는 몇회인가요?";
     private static final String MOVING_RESULT_HEADER_MESSAGE = "실행 결과";
     private static final String OUTPUT_EACH_MOVING_RESULT_COLON = " : ";
     private static final String OUTPUT_EACH_MOVING_RESULT_BAR = "-";
+    private static final String OUTPUT_FINAL_WINNER_MESSAGE = "최종 우승자: ";
 
     public void printInputCarNameMessage() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
@@ -23,6 +26,11 @@ public class Printer {
         System.out.println(carName + OUTPUT_EACH_MOVING_RESULT_COLON + OUTPUT_EACH_MOVING_RESULT_BAR.repeat(carPosition));
     }
 
-    public void printOutputFinalWinner() {
+    public void printOutputFinalWinner(List<String> winnerNameList) {
+        System.out.print(OUTPUT_FINAL_WINNER_MESSAGE);
+        for (String winnerName : winnerNameList) {
+            System.out.print(winnerName + " ");
+        }
     }
 }
+
