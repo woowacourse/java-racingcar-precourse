a2a278f4d5bba0b8a730fbe24e53b5669d234786
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-04 23:57:48 +0900

    feat: Car 객체를 통한 CarDto 객체 생성 정적 팩토리 메서드 구현

--- src/test/java/racingcar/domain/dto/CarDtoTest.java
+++ src/test/java/racingcar/domain/dto/CarDtoTest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import racingcar.domain.car.Car;
 import racingcar.domain.strategy.RandomMovingStrategy;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarDtoTest {
 
     @Test
     void CarDto_객체_정상_생성된다() {
         Car car = new Car("dto", new RandomMovingStrategy());
 
-        CarDto carDto = CarDto.of(car);
+        CarDto carDto = CarDto.from(car);
 
         assertThat(carDto.getName()).isEqualTo("dto");
         assertThat(carDto.getPosition()).isZero();
