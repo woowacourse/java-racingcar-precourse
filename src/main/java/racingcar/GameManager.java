e7beb4bb3d8c206af8fae27b0946d07ccc599f6d
박우림 <woorimprog@gmail.com>
2020-12-04 23:47:39 +0900

    feat(racing-car) : add the tried-count input function

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,14 +1,34 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GameManager {
+	private static int triedCnt;
+	private static List<Car> carList = new ArrayList<>();
 	
-	GameManager() {}
+	private static Player player = new Player();
 	
-	public void start(Scanner scanner) {
-		Player player = new Player();
-		
+	public void start(Scanner scanner) {		
 		player.inputRacingCar(scanner);
+		
+		startRace();
+	}
+	
+	public static void setCar(List<String> carNames) {
+		for (String car : carNames) {
+			carList.add(new Car(car));
+		}
+	}
+	
+	public static void setTriedCount(int cnt) {
+		triedCnt = cnt;
+	}
+	
+	public void startRace() {
+		for (Car car : carList) {
+			
+		}
 	}
 }
--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -6,13 +6,19 @@ import java.util.Scanner;
 
 public class Player {
 	private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+	private static final String COUNT_QUESTION = "시도할 회수는 몇회인가요?";
 	
 	Player() {}
 	
 	public void inputRacingCar(Scanner scanner) {
 		System.out.println(START_MESSAGE);
+		String car = scanner.nextLine();
+		List<String> carNames = Arrays.asList(car.replaceAll(" ", "").split(","));
 		
-		String car = scanner.next();
-		List<String> carList = Arrays.asList(car.split(","));
+		System.out.println(COUNT_QUESTION);
+		int triedCnt = scanner.nextInt();
+		
+		GameManager.setCar(carNames);
+		GameManager.setTriedCount(triedCnt);
 	}
 }
