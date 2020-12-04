b8d739073ec3c22336cf7f5e47e5388721baf81c
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-12-05 00:33:59 +0900

    feat(RaceGame) : 클래스 생성 및 내부 함수 명명

---
+++ src/main/java/racingcar/RaceGame.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceGame {
+
+    List<Car> carList;
+
+    public RaceGame() {
+
+    }
+
+    public void inputCarsName(){
+
+    }
+
+    public void checkForwardMovement(){
+        
+    }
+}
