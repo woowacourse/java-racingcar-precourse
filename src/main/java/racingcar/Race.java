95d20037c13da18acc3e475a4060cf015d465cb9
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-04 16:25:41 +0900

    feat:  레이스에 관련된 기능 추가 및 수정

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -8,12 +8,30 @@ public class Race {
     public static final int MINIMUM_RANGE = 0;
     public static final int MAXIMUM_RANGE = 9;
 
-    public void runCarRace(CarList carList) {
+    private CarList carList;
+    private int raceRound;
+
+    public Race(CarList carList, int raceRound) {
+        this.carList = carList;
+        this.raceRound = raceRound;
+    }
+
+    public void runCarRace() {
+        int roundIndex = 0;
+        System.out.println("\n실행결과");
+        while (roundIndex < raceRound) {
+            moveCarRandomValue();
+            roundIndex++;
+            Printer.carRacingResultPrint(carList);
+        }
+    }
+
+    private void moveCarRandomValue () {
         for (Car car : carList.getCarList()) {
             int moveStatus = RandomUtils.nextInt(MINIMUM_RANGE, MAXIMUM_RANGE);
-            if (moveStatus >= MOVE)
+            if (moveStatus >= MOVE) {
                 car.setPosition(car.getPosition() + 1);
+            }
         }
-        Printer.carRacingResult(carList);
     }
 }
