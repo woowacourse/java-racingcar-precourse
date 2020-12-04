c9f67c7c713e63cd09fabb13f84b4e54a108f558
Hyeon9mak <jinha3507@gmail.com>
2020-12-04 13:53:39 +0900

    refactor(GasTank): fix method return type

--- src/main/java/racingcar/GasTank.java
+++ src/main/java/racingcar/GasTank.java
@@ -6,8 +6,12 @@ public class GasTank {
 
     private static final int MIN_GAS = 0;
     private static final int MAX_GAS = 9;
+    private static final int ENOUGH_GAS = 4;
 
-    public int getChargeGas() {
-        return RandomUtils.nextInt(MIN_GAS, MAX_GAS);
+    public boolean getChargeGas() {
+        if(RandomUtils.nextInt(MIN_GAS, MAX_GAS) < ENOUGH_GAS){
+            return false;
+        }
+        return true;
     }
 }
