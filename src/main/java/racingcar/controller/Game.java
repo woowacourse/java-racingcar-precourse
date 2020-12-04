1d7c23944706a65bbbfa346624f92552a6370097
RinSabbit <celena1004@naver.com>
2020-12-04 17:47:09 +0900

    style: 코드 포맷팅

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -73,5 +73,4 @@ public class Game {
             .max()
             .orElseThrow(() -> new RacingCarErrorException(NO_WINNERS_EXCEPTION_MESSAGE));
     }
-
 }
--- src/main/java/racingcar/exception/RacingCarErrorException.java
+++ src/main/java/racingcar/exception/RacingCarErrorException.java
@@ -2,7 +2,7 @@ package racingcar.exception;
 
 public class RacingCarErrorException extends IllegalArgumentException {
 
-    private static final String EXCEPTION_PREFIX = "[ERROR]";
+    private static final String EXCEPTION_PREFIX = "[ERROR] ";
 
     public RacingCarErrorException(String message) {
         super(EXCEPTION_PREFIX + message);
