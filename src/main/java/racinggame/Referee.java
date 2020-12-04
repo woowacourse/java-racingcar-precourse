f93bc98ead74154cc9c0a536f1e01f52c596acd7
tomas <cmg14111@gmail.com>
2020-12-04 20:32:01 +0900

    feat(Referee): 경주의 결과에 따라 승자를 정하는 기능 구현
    Referee는 심판으로서 1등 자동차의 거리를 구하고, 그 거리와 같은 거리를 간 자동차를 모두 승자로 정의

---
+++ src/main/java/racinggame/Referee.java
@@ -0,0 +1,31 @@
+package racinggame;
+
+import domain.Car;
+import domain.RaceWinner;
+import domain.RacingCars;
+
+public class Referee {
+    private int maxDistance;
+
+    public void calculateMaxDistance(RacingCars racingCars) {
+        for (Car car : racingCars.getCarList()) {
+            checkMaxDistance(car);
+        }
+    }
+
+    private void checkMaxDistance(Car car) {
+        int carPosition = car.getPosition();
+        if (carPosition > maxDistance) {
+            maxDistance = carPosition;
+        }
+    }
+
+    public void selectWinner(RacingCars racingCars) {
+        RaceWinner raceWinner = new RaceWinner();
+        for (Car car : racingCars.getCarList()) {
+            if (car.getPosition() == maxDistance) {
+                raceWinner.addRaceWinner(car);
+            }
+        }
+    }
+}
