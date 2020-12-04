e071a1614a40e97c6686bfafee7d5683ff56bb3a
bluewood <bluewood.truth@gmail.com>
2020-12-04 14:46:10 +0900

    refactor: 게임에 사용되는 데이터를 중첩클래스로 정의하고 CarList 인스턴스 변수 제거

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -4,26 +4,45 @@ import java.util.Scanner;
 
 public class GameManager {
     private Scanner scanner;
-    private CarList carList;
 
     public GameManager(Scanner scanner) {
         this.scanner = scanner;
     }
 
     public void run() {
-        InputHandler inputHandler = new InputHandler(scanner);
-        String[] carNames = inputHandler.askCarNames();
-        carList = new CarList(carNames);
-        int moveCount = inputHandler.askMoveCount();
+        GameData gameData = setupGameData();
         
         OutputHandler.printRunMessage();
-        move(moveCount);
+        move(gameData);
+    }
+    
+    private GameData setupGameData() {
+        InputHandler inputHandler = new InputHandler(scanner);
+        
+        CarList carList = new CarList(inputHandler.askCarNames());
+        int moveCount = inputHandler.askMoveCount();
+
+        return new GameData(carList, moveCount);
     }
 
-    private void move(int moveCount) {
-        for (int i = 0; i < moveCount; i++) {
-            carList.moveAllCars();
-            OutputHandler.printCarList(carList);
+    private void move(GameData gameData) {
+        for (int i = 0; i < gameData.moveCount; i++) {
+            moveAllCars(gameData.carList);
+            OutputHandler.printCarList(gameData.carList);
+        }
+    }
+
+    private void moveAllCars(CarList carList) {
+        carList.moveAllCars();
+    }
+
+    private class GameData {
+        public CarList carList;
+        public int moveCount;
+
+        public GameData(CarList carList, int moveCount) {
+            this.carList = carList;
+            this.moveCount = moveCount;
         }
     }
 }
