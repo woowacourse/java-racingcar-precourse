adec4505d45ff807df76f1d747ed53b62eebf8fe
김주원 <kjw11077@naver.com>
2020-12-04 22:51:52 +0900

    test(Winners): move tests

--- src/test/java/racingcar/GameResultTest.java
+++ src/test/java/racingcar/GameResultTest.java
@@ -9,28 +9,4 @@ import org.junit.Test;
 
 public class GameResultTest {
 
-    Car joy;
-    Car happy;
-
-    @Before
-    public void setUp() throws Exception {
-        joy = new Car("joy");
-        happy = new Car("happy");
-    }
-
-    @Test
-    public void getWinnerNames_multiWinners() {
-        Winners gameResult = new Winners(
-                Arrays.asList(joy, happy));
-
-        assertThat(gameResult.getWinnerNames()).isEqualTo("joy, happy");
-    }
-
-    @Test
-    public void getWinnerNames_singleWinner() {
-        Winners gameResult = new Winners(
-                Collections.singletonList(joy));
-
-        assertThat(gameResult.getWinnerNames()).isEqualTo("joy");
-    }
 }
--- src/test/java/racingcar/WinnersTest.java
+++ src/test/java/racingcar/WinnersTest.java
@@ -4,13 +4,34 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.Collections;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WinnersTest {
 
+    Car joy;
+    Car happy;
+
+    @Before
+    public void setUp() throws Exception {
+        joy = new Car("joy");
+        happy = new Car("happy");
+    }
+
     @Test
-    public void getWinnerNames() {
-        Winners winners = new Winners(Arrays.asList(new Car("aa"), new Car("bb")));
-        assertThat(winners.getWinnerNames()).isEqualTo("aa, bb");
+    public void getWinnerNames_multiWinners() {
+        Winners gameResult = new Winners(
+                Arrays.asList(joy, happy));
+
+        assertThat(gameResult.getWinnerNames()).isEqualTo("joy, happy");
+    }
+
+    @Test
+    public void getWinnerNames_singleWinner() {
+        Winners gameResult = new Winners(
+                Collections.singletonList(joy));
+
+        assertThat(gameResult.getWinnerNames()).isEqualTo("joy");
     }
 }
