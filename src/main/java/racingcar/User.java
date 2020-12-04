e89a5bb00a0d4b350406d092199a75e412ee35c1
yhh1056 <yhh1056@naver.com>
2020-12-04 14:04:18 +0900

    Refactor: 메소드 분리
    한 메소드에서 두 가지 이상의 예외를 처리하고 있어서 분리하였습니다.

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -18,18 +18,38 @@ public class User {
     private static final int NAMES_SIZE = 30;
 
     public List<String> createNames(String names) {
-        String[] splitNames = names.split(DELIMITER);
-        validateName(names);
-        validateNameLength(splitNames);
-        return Arrays.asList(splitNames);
+        return validateName(names);
     }
 
-    private void validateName(String names) {
+    private List<String> validateName(String names) {
+        validateEmpty(names);
+        validateWhitespace(names);
+        return validateNames(names.split(DELIMITER));
+    }
+
+    private void validateWhitespace(String names) {
+        if (names.contains(SPACE) || names.contains(TAB)) {
+            throw new IllegalArgumentException("[ERROR] 이름에 공백이 존재합니다.");
+        }
+    }
+
+    private void validateEmpty(String names) {
         if (names.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 이름이 존재하지 않습니다.");
         }
-        if (names.contains(SPACE) || names.contains(TAB)) {
-            throw new IllegalArgumentException("[ERROR] 이름에 공백이 존재합니다.");
+    }
+
+    private List<String> validateNames(String[] splitNames) {
+        validateNameLength(splitNames);
+        validateDuplicate(splitNames);
+        validateNamesSize(splitNames);
+        return Arrays.asList(splitNames);
+    }
+
+    private void validateDuplicate(String[] splitNames) {
+        Set<String> nonDuplicateName = new HashSet<>(Arrays.asList(splitNames));
+        if (splitNames.length != nonDuplicateName.size()) {
+            throw new IllegalArgumentException("[ERROR] 이름이 중복됩니다.");
         }
     }
 
@@ -39,12 +59,9 @@ public class User {
                 throw new IllegalArgumentException("[ERROR] 이름이 5자가 넘습니다.");
             }
         }
+    }
 
-        Set<String> nonDuplicateName = new HashSet<>(Arrays.asList(splitNames));
-        if (splitNames.length != nonDuplicateName.size()) {
-            throw new IllegalArgumentException("[ERROR] 이름이 중복됩니다.");
-        }
-
+    private void validateNamesSize(String[] splitNames) {
         if (splitNames.length > NAMES_SIZE) {
             throw new IllegalArgumentException("[ERROR] 참가자 수가 30명이 넘습니다.");
         }
