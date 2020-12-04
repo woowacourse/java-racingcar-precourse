e2137452094aef50caefc3edaca7a4859e475a94
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-04 13:10:51 +0900

    add: Car 진행 상황 구하기, 위치 값을 기호로 변환, 위치 출력 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Car {
     private final String name;
     private int position = 0;
+    private String positionSign = "";
     private static List<Car> carList = new ArrayList<>();
 
     public Car(String name, int position) {
@@ -20,6 +23,36 @@ public class Car {
         }
         return carList;
     }
+
+    public static void setPosition(Car car) {
+        moveOrStop(car);
+    }
+
+    public static void moveOrStop(Car car) {
+        if (RandomUtils.nextInt(0,9) >= 4) {
+            car.move();
+        }
+    }
+
+    public void move() {
+        position++;
+    }
+
+    public static void displayPosition(List<Car> cars) {
+        for (Car car : cars) {
+            car.changePositionsign(car);
+            System.out.println(car.name + " : " + car.positionSign);
+        }
+        System.out.println();
+    }
+
+    public String changePositionsign(Car car) {
+        for (int i = 0; i < car.position; i++) {
+            positionSign += "-";
+        }
+        return positionSign;
+    }
+
 
 
 }
