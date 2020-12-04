a73661f85bac96f960f3aaf633e6c04c502b6a5e
김경록 <goodboy3421@gmail.com>
2020-12-05 06:51:02 +0900

    style: 불필요한 import문 제거

--- src/test/java/racingcar/domain/car/NameTest.java
+++ src/test/java/racingcar/domain/car/NameTest.java
@@ -1,7 +1,6 @@
 package racingcar.domain.car;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
