85117b750076829c26c164ed8fd98e71fd992d1d
김경록 <goodboy3421@gmail.com>
2020-12-05 01:07:14 +0900

    feat: 각 자동차의 이름과 이동한 위치를 출력하는 기능
    Cars를 출력하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,6 +9,12 @@ import java.util.stream.Stream;
 public class OutputView {
     private static final String DELIMITER_COLON = " : ";
     private static final String EACH_POSITION_SYMBOL = "-";
+
+    public static void printCars(Cars cars) {
+        cars.getValue().stream()
+                .forEach(car -> printCarPosition(car));
+        System.out.println();
+    }
 
     public static void printCarPosition(Car car) {
         System.out.println(car.getName() + DELIMITER_COLON + convertToPositionSymbols(car.getPosition()));
