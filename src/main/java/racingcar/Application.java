3cca6d5d16f8f6eaa5ca1e6aa50bfe787e0042fa
taelee <taelee@student.42seoul.kr>
2020-12-04 15:29:09 +0900

    refactor(Application): scanner argument is moved into startGame method from Game class constructor

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,8 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        Game newGame = new Game(scanner);
+        Game newGame = new Game();
+        newGame.startGame(scanner);
+        scanner.close();
     }
 }
