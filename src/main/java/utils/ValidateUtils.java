97aaf462beba358bb64c547f23015fcb7549c4ad
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-04 14:31:17 +0900

    fix 하드코딩 수정

--- src/main/java/utils/ValidateUtils.java
+++ src/main/java/utils/ValidateUtils.java
@@ -3,10 +3,13 @@ package utils;
 import java.util.List;
 
 public class ValidateUtils {
+    private static final int DEFAULT_CAR_NAME_MAX = 5;
 
     public static void carNameLengthCheck(List<String> carNamesSplitList) {
+
+
         for (int i = 0; i < carNamesSplitList.size(); i++) {
-            if (carNamesSplitList.get(i).length() > 5) {
+            if (carNamesSplitList.get(i).length() > DEFAULT_CAR_NAME_MAX) {
                 throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 아래로 입력 바랍니다.");
             }
         }
