c167ac82a4d198eea97352f3d1784d6a36f72c22
seunghyun <ggyool@naver.com>
2020-12-05 03:05:31 +0900

    feat: 자동차 이름을 입력받는 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final String COMMA = ",";
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public List<String> inputCarNames() {
+        String carNames = scanner.nextLine();
+
+        return Arrays.stream(carNames.split(COMMA))
+                .map(s -> s.trim())
+                .collect(Collectors.toList());
+    }
+}
+
