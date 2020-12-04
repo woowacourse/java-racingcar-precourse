fa28bf927abdbfc51dd9e17e839331dd9e32daec
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-04 16:24:01 +0900

    feat: 최종 우승자 결과 출력 추가

--- src/main/java/utils/Printer.java
+++ src/main/java/utils/Printer.java
@@ -4,20 +4,22 @@ import racingcar.Car;
 import racingcar.CarList;
 
 public class Printer {
-    public static final String CAR_NAME_PRINT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String PLAY_ROUND_PRINT = "시도할 회수는 몇회인가요?";
+    public static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String RACE_ROUND_INPUT = "시도할 회수는 몇회인가요?";
+    private static final String FINAL_WINNER = "최종 우승자: ";
     private static final String NEW_LINE = "\n";
 
-    public static void setCarNamePrinter() {
-        System.out.println(CAR_NAME_PRINT);
+    public static void setCarPrint() {
+        System.out.println(CAR_NAME_INPUT);
     }
 
-    public static void setPlayRoundPrinter() {
-        System.out.println(PLAY_ROUND_PRINT);
+    public static void setPlayRoundPrint() {
+        System.out.println(RACE_ROUND_INPUT);
     }
 
-    public static void carRacingResult(CarList carList) {
+    public static void carRacingResultPrint(CarList carList) {
         StringBuilder result = new StringBuilder();
+
         for (Car car : carList.getCarList()) {
             result.append(car.getName() + " : ");
             for (int i = 0; i < car.getPosition(); i++) {
@@ -28,4 +30,7 @@ public class Printer {
         System.out.println(result.toString());
     }
 
+    public static void carRaceWinnerPrint(String winners) {
+        System.out.println(FINAL_WINNER + winners);
+    }
 }
