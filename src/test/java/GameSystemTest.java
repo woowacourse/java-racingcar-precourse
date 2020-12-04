28a25b976b9db928a987cf6549f056c5587a6ab9
JuGyeong Lim <zoov0716@gmail.com>
2020-12-04 13:31:05 +0900

    test: 최종 테스트 완료

--- src/test/java/GameSystemTest.java
+++ src/test/java/GameSystemTest.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-
-import racingcar.Car;
 import racingcar.GameSystem;
 
 public class GameSystemTest {
@@ -8,19 +6,6 @@ public class GameSystemTest {
         final Scanner scanner = new Scanner(System.in);
         GameSystem game = new GameSystem();
 
-        game.makeCars(scanner);
-        Car[] cars = game.getCars();
-
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i].getName());
-        }
-
-        game.setTrialCount(scanner);
-        System.out.println(game.getTrialCount());
-
-        game.doAllTrial();
-        game.setMaxPosition();
-        game.setWinners();
-        game.printWinners();
+        game.playGame(scanner);
     }
 }
