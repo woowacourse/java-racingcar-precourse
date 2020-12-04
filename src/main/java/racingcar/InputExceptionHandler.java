29ae0ad3da78c67f31d2259941e28d10cb917768
bluewood <bluewood.truth@gmail.com>
2020-12-04 12:17:59 +0900

    feat: 이동 횟수 입력에 대한 예외 처리 추가

--- src/main/java/racingcar/InputExceptionHandler.java
+++ src/main/java/racingcar/InputExceptionHandler.java
@@ -7,10 +7,14 @@ public class InputExceptionHandler {
     private static final String NO_DELIMITER_ERROR = "자동차 이름을 \"%s\"로 구분하여 입력해주세요.";
     private static final String INVALID_LENGTH_ERROR = "자동차 이름은 1~5자여야 합니다.";
     private static final String SPACE_NAME_ERROR = "자동차 이름은 공백일 수 없습니다.";
+    private static final String NOT_NUMERIC_ERROR = "이동 횟수는 양의 정수여야 합니다.";
+    private static final String OUT_OF_RANGE_ERROR = "입력값이 정수 범위를 벗어났습니다.";
+    private static final String ZERO_OR_LESS_ERROR = "이동 횟수는 0보다 커야 합니다.";
 
     private static final int CAR_NAME_LENGTH_MINIMUM = 1;
     private static final int CAR_NAME_LENGTH_MAXIMUM = 5;
     private static final String SPACE_REGEX = "^[ 　ㅤ​]+$";
+    private static final String NUMERIC_REGEX = "^[0-9]+$";
 
     private InputExceptionHandler() {}
 
@@ -52,7 +56,30 @@ public class InputExceptionHandler {
     }
 
     public static void validateMoveCount(String userInput) {
-        // TODO 예외처리
+        checkIsNumeric(userInput);
+        checkIsRangeOfInteger(userInput);
+        checkIsGreaterThanZero(userInput);
+    }
+
+    private static void checkIsNumeric(String userInput) {
+        boolean isNumeric = Pattern.matches(NUMERIC_REGEX, userInput);
+        if (!isNumeric) {
+            printError(NOT_NUMERIC_ERROR);
+        }
+    }
+
+    private static void checkIsRangeOfInteger(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException exception) {
+            printError(OUT_OF_RANGE_ERROR);
+        }
+    }
+
+    private static void checkIsGreaterThanZero(String userInput) {
+        if (Integer.parseInt(userInput) <= 0) {
+            printError(ZERO_OR_LESS_ERROR);
+        }
     }
 
     private static void printError(String error) {
