bdea90d4ca4c533c787e7019ba58a3455151c1fa
binghe819 <binghe819@naver.com>
2020-12-05 00:37:30 +0900

    feat: 가장 빠른 자동차들 판별 기능 구현
    여러 레이싱 차(RacingCars)중 가장 빠른 자동차들 판변 기능.

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -2,11 +2,13 @@ package racingcar.domain;
 
 import racingcar.domain.engine.Engine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
 
     private List<Car> cars;
+    private int fastestPosition = 0;
 
     public RacingCars(List<Car> cars) {
         this.cars = cars;
@@ -16,6 +18,7 @@ public class RacingCars {
         StringBuilder moveResult = new StringBuilder();
         for (Car car : cars) {
             car.move(engine);
+            calculateFastestPosition(car.getPosition());
             moveResult
                     .append(car.toString())
                     .append("\n");
@@ -23,4 +26,19 @@ public class RacingCars {
         return moveResult.toString();
     }
 
+    private void calculateFastestPosition(int carPosition) {
+        if (carPosition > fastestPosition) {
+            fastestPosition = carPosition;
+        }
+    }
+
+    public List<Car> getFastestCars() {
+        List<Car> fastestCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == fastestPosition) {
+                fastestCars.add(car);
+            }
+        }
+        return fastestCars;
+    }
 }
