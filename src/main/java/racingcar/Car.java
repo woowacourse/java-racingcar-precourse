9572730a62a284aa9a810c41523911303744e6cd
youngsil <kys4548@gmail.com>
2020-12-04 21:09:03 +0900

    inputCarInfo 기능구현 완료

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,4 +9,22 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public void carNameValidate() {
+        checkCarNameNotEmpty(this.name);
+        checkCarNameGreaterThenFive(this.name);
+    }
+
+    private void checkCarNameNotEmpty(String name) {
+        if (name.isBlank()) {
+            System.out.println("[ERROR] 자동차 이름이 정해지지 않았습니다.");
+            throw new RuntimeException();
+        }
+    }
+
+    private void checkCarNameGreaterThenFive(String name) {
+        if (name.length() > 5) {
+            System.out.println("[ERROR] 자동차 이름은 5글자 이하만 가능합니다.");
+            throw new RuntimeException();
+        }
+    }
 }
