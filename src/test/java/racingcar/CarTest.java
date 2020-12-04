b60dc3ab58396cb643a93d83a00ac623c79e5276
Hyeon9mak <jinha3507@gmail.com>
2020-12-04 10:35:51 +0900

    test(Car): test Car methods

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,15 +1,24 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
     private static String name = "pobi";
+    private static String secondname = "json";
 
     @Test
     public void testCreate() {
         Car car = new Car(name);
+        car.addPosition();
+        assertThat(car.getPosition()).isEqualTo(1);
 
-        car.moveOneBlock();
+        Car secondCar = new Car(secondname);
+        secondCar.addPosition();
+        secondCar.addPosition();
+        secondCar.addPosition();
+        assertThat(secondCar.getPosition()).isEqualTo(3);
     }
 }
