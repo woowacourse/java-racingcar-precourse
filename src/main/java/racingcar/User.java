c4c893bbdbac46b37e4557ce4923f4e1d064b26c
yhh1056 <yhh1056@naver.com>
2020-12-04 21:02:38 +0900

    Feat: 시도 횟수가 30회가 넘을 경우 예외 처리

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -27,6 +27,14 @@ public class User {
         } catch (NumberFormatException e) {
             throw new NumberFormatException("[ERROR] 허용하는 숫자가 아닙니다.");
         }
+
+        if (racingCount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 1회 이상이어야 합니다.");
+        }
+
+        if (racingCount > 30) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 30회 이하여야 합니다.");
+        }
         return racingCount;
     }
 
