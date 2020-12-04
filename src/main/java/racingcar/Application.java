1ca68c44136b6a9c5c8f9afee565d864f41527f9
최문경 <munkyung0324@gmail.com>
2020-12-05 02:08:16 +0900

    feat(Application): validate car name size, create CarValidator class
    Application:
    - validate car name size
    README:
    - fix typo in README.md

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,32 @@
 package racingcar;
 
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
+    static String[] cars;
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        String carsInput = scanner.next();
-        String[] cars = carsInput.split(",");
-        System.out.println(Arrays.toString(cars));
+        do {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carsInput = scanner.next();
+            cars = carsInput.split(",");
+            System.out.println(Arrays.toString(cars));
+        } while (!validCarNameList());
+
     }
+
+    private static boolean validCarNameList() {
+        CarValidator carValidator = new CarValidator();
+        for (String carName : cars) {
+            if(!carValidator.isValid(carName)) {
+                System.out.println("자동차 이름은 5자 이하로 입력하세요.");
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
---
+++ src/main/java/racingcar/CarValidator.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class CarValidator {
+    public boolean isValid(String carName) {
+        if(carName.length() > 5)
+            return false;
+        return true;
+    }
+}
