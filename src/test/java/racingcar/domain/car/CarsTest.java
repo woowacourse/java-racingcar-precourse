0357989baef20894f4fb8aa776cd54f602e1abba
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-04 15:49:15 +0900

    test: 이름들에 중복이 있는 경우 Cars 객체 생성 실패 테스트 추가

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -2,6 +2,8 @@ package racingcar.domain.car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.exception.CarNameDuplicationException;
+import racingcar.domain.strategy.RandomMovingStrategy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,13 +12,23 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 class CarsTest {
 
-    @DisplayName("Cars 객체 생성 성공 : 이름에 중복이 없는 경우")
+    @DisplayName("Cars 객체 생성 성공 : 이름들에 중복이 없는 경우")
     @Test
     public void createCars_중복없는_이름들_객체_생성_성공() {
         List<String> carNames = Arrays.asList("pobi", "crong", "jiko", "ajax");
 
         assertThatCode(() -> {
-            Cars.createCars(carNames);
+            Cars.createCars(carNames, new RandomMovingStrategy());
         }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("Cars 객체 생성 실패 : 이름들에 중복이 존재하는 경우")
+    @Test
+    public void createCars_중복있는_이름들_예외_발생() {
+        List<String> carNames = Arrays.asList("pobi", "pobi", "jiko", "ajax");
+
+        assertThatCode(() -> {
+            Cars.createCars(carNames, new RandomMovingStrategy());
+        }).isInstanceOf(CarNameDuplicationException.class);
     }
 }
