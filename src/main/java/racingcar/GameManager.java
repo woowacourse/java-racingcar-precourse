61ef11c7e35f8d42461fd47593a0cd21962bb824
sounmind <sounmind777@gmail.com>
2020-12-04 11:49:54 +0900

    feat(GameManager): 자동차 이름 입력 예외 처리 기능 구현

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -9,8 +9,11 @@ public class GameManager {
     public static int numberTryToMove; // 이동 횟수 = 라운드 수
     private static HashMap<String, Car> participants = new HashMap<String, Car>(); // 참가자 이름: 참가자 Car
     private static final int LENGTH_LIMIT_OF_INDIVIDUAL_INPUT = 5;
+    private static final int LENGTH_EMPTY = 0;
     private static final String DELIMITER = ",";
-    private static final String MESSAGE_ERROR_LIMIT_EXCESS = "[ERROR] 문자열 입력 한계를 초과했습니다.";
+    private static final String MESSAGE_ERROR_NOT_VALID_CAR_NAME = "[ERROR] 길이 "+LENGTH_LIMIT_OF_INDIVIDUAL_INPUT+"이하의 자동차 이름을 입력해주세요";
+    private static final String MESSAGE_ERROR_EMPTY_CAR_NAME = "[ERROR] 자동차 이름이 없습니다.";
+    private static final String MESSAGE_ERROR_FOUND_EMPTY_CAR_NAME = "[ERROR] 자동차 이름이 없는 것이 있습니다.";
     private static final String MESSAGE_INPUT_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MESSAGE_INPUT_NUMBER_TRY_TO_MOVE = "시도할 횟수는 몇 회인가요?";
     private static final String MESSAGE_ERROR_NOT_VALID_TRY_NUMBER = "[ERROR] 시도 횟수는 숫자여야 합니다.";
@@ -23,7 +26,7 @@ public class GameManager {
     }
 
     public void startGame(Scanner scanner) {
-        inputParticipants(scanner);
+        inputParticipantsWithoutValidation(scanner);
         inputNumberTryToMove(scanner);
         registerCar(stringArrayParticipants);
         startAllRounds();
@@ -52,22 +55,40 @@ public class GameManager {
         }
     }
 
-    private void inputParticipants(Scanner scanner) {
+    private void inputParticipantsWithoutValidation(Scanner scanner) {
         System.out.println(MESSAGE_INPUT_CARS);
         stringArrayParticipants = scanner.nextLine().split(DELIMITER);
         participantsTotal = stringArrayParticipants.length;
+        reInputTrimmedParticipants(stringArrayParticipants);
         validateCarInput(stringArrayParticipants);
     }
 
+    private void reInputTrimmedParticipants(String[] stringArrayParticipants) {
+        for (int i = 0; i < participantsTotal; i++) {
+            stringArrayParticipants[i] = stringArrayParticipants[i].trim();
+        }
+    }
+
     private void validateCarInput(String[] stringArrayParticipants) {
+        checkAllEmptyName();
         for (String participant : stringArrayParticipants) {
             validateIndividualCarInputLength(participant);
         }
     }
 
-    private void validateIndividualCarInputLength(String participants) {
-        if (participants.length() > LENGTH_LIMIT_OF_INDIVIDUAL_INPUT) {
-            throw new IllegalArgumentException(MESSAGE_ERROR_LIMIT_EXCESS);
+    private void checkAllEmptyName() {
+        if (participantsTotal == LENGTH_EMPTY) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_EMPTY_CAR_NAME);
+        }
+    }
+
+    private void validateIndividualCarInputLength(String participant) {
+        int oneCarNameLength = participant.length();
+        if (oneCarNameLength == LENGTH_EMPTY) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_FOUND_EMPTY_CAR_NAME);
+        }
+        if (oneCarNameLength > LENGTH_LIMIT_OF_INDIVIDUAL_INPUT) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_NOT_VALID_CAR_NAME);
         }
     }
 
@@ -77,4 +98,6 @@ public class GameManager {
             participants.put(carOwner, new Car(carOwner));
         }
     }
+
+
 }
