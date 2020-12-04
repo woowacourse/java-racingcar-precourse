d158b403b1f6ca28abd59bb768e8ad2bae33bcf3
NewWisdom <wlgp2500@gmail.com>
2020-12-04 14:31:06 +0900

    feat(game): add show-moving-result feature

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 import java.util.Scanner;
 
 public class Game {
@@ -7,6 +9,7 @@ public class Game {
     private final Printer printer;
     private static final int START_INCLUSIVE = 0;
     private static final int END_INCLUSIVE = 9;
+    private static final int MOVEMENT_BASE_VALUE = 4;
     private Car[] cars;
 
     public Game(Scanner scanner) {
@@ -26,18 +29,31 @@ public class Game {
     /* 각 차수별로 각 자동차를 전진 또는 멈춤하고 실행 결과 출력 */
     public void showMovingResult() {
         printer.printMovingResultHeader();
+        for (int i = 0; i < player.getTryNumber(); i++) {
+            produceResult();
+        }
+    }
 
+    public void produceResult() {
+        for (int i = 0; i < cars.length; i++) {
+            judgeMovement(cars[i]);
+            printer.printOutputEachResultPosition(cars[i].getName(),cars[i].getPosition());
+        }
+        System.out.println();
     }
 
     /* 최종 우승자 뽑고 출력 */
     public void finish() {
     }
 
-    public void createRandomNumber() {
-
+    public int generateRandomNumber() {
+        return RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE);
     }
 
-    public void judgeMovement() {
+    public void judgeMovement(Car car) {
+        if (generateRandomNumber() >= MOVEMENT_BASE_VALUE) {
+            car.move();
+        }
     }
 
     public void selectWinner() {
