447ad3cedfe6e7a523927734f7aaba283282ea78
opdshe <opdshe@naver.com>
2020-12-05 01:10:13 +0900

    feat: 자동차들의 현재 진행 상태 출력 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -33,4 +33,8 @@ public class Car {
     public int getPosition() {
         return position;
     }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/racingcar/views/OutputView.java
+++ src/main/java/racingcar/views/OutputView.java
@@ -1,7 +1,38 @@
 package racingcar.views;
 
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+
 public class OutputView {
+    private static final String RESULT_GUIDING_MESSAGE = "실행 결과";
+    private static final String DISTANCE = "-";
+    private static final String COLON = " : ";
+    private static final String BLANK = "";
+
     private OutputView() {
+    }
+
+    public static void printCurrentStatus(RacingGame racingGame) {
+        racingGame.getCars().forEach(OutputView::printCarStatus);
+        printMessageAndNewLine(BLANK);
+    }
+
+    private static void printCarStatus(Car car) {
+        String statusMessage = car.getName() + COLON + getDistanceBar(car);
+        printMessageAndNewLine(statusMessage);
+    }
+
+    private static String getDistanceBar(Car car) {
+        StringBuilder distanceBar = new StringBuilder();
+        int distance = car.getPosition();
+        for (int i = 0; i < distance; i++) {
+            distanceBar.append(DISTANCE);
+        }
+        return distanceBar.toString();
+    }
+
+    public static void printResultGuidingMessage() {
+        printMessageAndNewLine(RESULT_GUIDING_MESSAGE);
     }
 
     public static void printMessageAndNewLine(String message) {
