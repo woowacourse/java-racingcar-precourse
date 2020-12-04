f576f9c9cc7eb9b564af97472d85f55881b66919
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-05 05:47:43 +0900

    자동차 경주 로직 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,6 +4,7 @@ public class RacingGame {
 
     private Player player;
     private Computer computer;
+    private Car[] cars;
 
     public RacingGame(Player player, Computer computer) {
         this.player = player;
@@ -21,13 +22,40 @@ public class RacingGame {
 
         String stringTryCount = getTryCount();
 
-        if(!computer.isDigit(stringTryCount)){
+        if (!computer.isDigit(stringTryCount)) {
             System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.");
             return;
         }
 
+        cars = createCarInstance(carList);
+
         int tryCount = Integer.parseInt(stringTryCount);
 
+        playRace(tryCount);
+    }
+
+    private Car[] createCarInstance(String[] carList) {
+        Car[] cars = new Car[carList.length];
+
+        for (int i = 0; i < carList.length; i++) {
+            cars[i] = new Car(carList[i]);
+        }
+
+        return cars;
+    }
+
+    private void playRace(int tryCount) {
+        for (int i = 0; i < tryCount; i++) {
+            moveCars();
+        }
+    }
+
+    private void moveCars() {
+        for (int i = 0; i < cars.length; i++) {
+            if(computer.moveOrStop()){
+                cars[i].upPosition();
+            }
+        }
     }
 
     private String getTryCount() {
