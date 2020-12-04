f8d9eaf89982efca6895a075fb75fd60c707ea4d
yhh1056 <yhh1056@naver.com>
2020-12-04 23:07:10 +0900

    Refactor: 하드코딩 수정
    범위를 나타내는 하드코딩된 값을 상수로 변경하였습니다.

--- src/main/java/racingcar/Count.java
+++ src/main/java/racingcar/Count.java
@@ -5,6 +5,9 @@ package racingcar;
  * @since 2020/12/04
  */
 public class Count {
+    private static final int MAXIMUM_COUNT = 30;
+    private static final int MINIMUM_COUNT = 1;
+
     private int racingCount = 0;
 
     public int validate(String input) {
@@ -27,13 +30,13 @@ public class Count {
     }
 
     private void validateMinimal() {
-        if (racingCount <= 0) {
+        if (racingCount < MINIMUM_COUNT ) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 1회 이상이어야 합니다.");
         }
     }
 
     private void validateMaximal() {
-        if (racingCount > 30) {
+        if (racingCount > MAXIMUM_COUNT) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 30회 이하여야 합니다.");
         }
     }
