3db81763a753b49264ceaf2d06de675049cba735
pjy1368 <basejin0810@naver.com>
2020-12-04 17:37:48 +0900

    feat(constants): 시스템 메시지 추가.

--- src/main/java/constant/SystemMessage.java
+++ src/main/java/constant/SystemMessage.java
@@ -4,6 +4,8 @@ public class SystemMessage {
     public static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_TURN_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String RUN_RESULT = "실행 결과";
+    public static final String SEPARATOR = ":";
+    public static final String DISTANCE = "-";
     public static final String CHAMPION = "최종 우승자:";
     public static final String ERROR_MESSAGE = "[ERROR]";
 }
