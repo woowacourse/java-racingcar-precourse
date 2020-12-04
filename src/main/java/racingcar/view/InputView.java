ec6e3c88748945ae0e0bd137b097cc898051f38e
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-04 13:39:48 +0900

    refactor: InputView 상수명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_NOTICE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_RACING_TRY_COUNTS_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_RACING_TRY_COUNTS_NOTICE_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String COMMA_DELIMITER = ",";
     private static final int SPLIT_LIMIT_THRESHOLD_PARAMETER = -1;
     private static final int MINIMUM_CAR_NAME_LENGTH = 1;
@@ -72,7 +72,7 @@ public class InputView {
     }
 
     public int inputRacingTryCounts() {
-        System.out.println(INPUT_RACING_TRY_COUNTS_MESSAGE);
+        System.out.println(INPUT_RACING_TRY_COUNTS_NOTICE_MESSAGE);
         String racingTryCounts = this.scanner.nextLine();
         while (!isValidRacingTryCounts(racingTryCounts)) {
             racingTryCounts = this.scanner.nextLine();
