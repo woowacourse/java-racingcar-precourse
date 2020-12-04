9f23572010121994c3aef2c30f92d06275223f47
Ji-Ha <xodhkd36@gmail.com>
2020-12-04 15:17:16 +0900

    test(GameRoutineTest.java): Car객체 저장이 잘 되었는지 확인하는 test

---
+++ src/main/java/methodtest/GameRoutineTest.java
@@ -0,0 +1,16 @@
+package methodtest;
+
+import inpututils.InputName;
+import inpututils.InputRepetitions;
+import racingcar.GameCycle;
+
+import java.util.Scanner;
+
+public class GameRoutineTest {
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		GameCycle gameTest = new GameCycle(new InputName(scanner).inputAnswer(), new InputRepetitions(scanner).inputAnswer());
+
+
+	}
+}
