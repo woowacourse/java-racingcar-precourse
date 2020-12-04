1e0a332f1275dc3454b49bf57f0a119957e65e5a
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-04 13:32:16 +0900

    feat: Car 객체 생성 시 이름 문자열에 대한 유효성 검사 기능 구현
    이름은 1~5자리 문자열이어야 하며, null이면 안 된다.
    또한 이름이 모두 공백인 경우 구분이 불가능하기 때문에,
    trim을 통해 이름이 모두 공백인 이름도 예외를 처리해준다.

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,12 +1,26 @@
 package racingcar.domain.car;
 
+import racingcar.domain.exception.CarNameLengthException;
+
 public class Car {
+    private static final int MINIMUM_CAR_NAME_LENGTH = 1;
+    private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateCarName(name);
         this.name = name;
     }
 
-    // 추가 기능 구현
+    private void validateCarName(String name) {
+        if (name == null) {
+            throw new CarNameLengthException();
+        }
+        int trimNameLength = name.trim().length();
+        if (trimNameLength < MINIMUM_CAR_NAME_LENGTH || trimNameLength > MAXIMUM_CAR_NAME_LENGTH) {
+            throw new CarNameLengthException();
+        }
+    }
 }
