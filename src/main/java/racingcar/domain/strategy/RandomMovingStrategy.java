8f98c4e266c7deac71a5c1631d7c8a1c4827e45f
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-04 14:04:03 +0900

    feat: 랜덤값에 의존해 이동하는 RandomMovingStrategy 기능 구현
    랜덤 값이 유효한 범위(0~9)가 아니면 예외를 발생시킨다.
    랜덤 값이 4 이상일 때만 이동(true)한다.

---
+++ src/main/java/racingcar/domain/strategy/RandomMovingStrategy.java
@@ -0,0 +1,22 @@
+package racingcar.domain.strategy;
+
+import utils.RandomUtils;
+
+public class RandomMovingStrategy implements MovingStrategy {
+    private static final int MINIMUM_RANDOM_NUMBER = 0;
+    private static final int MAXIMUM_RANDOM_NUMBER = 9;
+    private static final int MINIMUM_MOVABLE_NUMBER = 4;
+
+    @Override
+    public boolean isMovable() {
+        int randomNumber = RandomUtils.nextInt(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
+        validateRandomNumber(randomNumber);
+        return randomNumber >= MINIMUM_MOVABLE_NUMBER;
+    }
+
+    private void validateRandomNumber(int randomNumber) {
+        if (randomNumber < MINIMUM_RANDOM_NUMBER || randomNumber > MAXIMUM_RANDOM_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
