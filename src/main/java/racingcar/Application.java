6699ab441fe6b09eb38827531d32f2cd7c36851e
Beenie93 <seongbeen93@naver.com>
2020-12-06 01:21:12 +0900

    refactor(racingcar-Application): 자동차 게임 실행 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,49 @@
 package racingcar;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        InputView inputView = new InputView(scanner);
+        OutputView outputView = new OutputView();
+        GameController gameController = new GameController(inputView, outputView);
+
+        run(gameController);
+    }
+
+    public static void run(GameController gameController) {
+        setPlayers(gameController);
+        setRounds(gameController);
+        playgame(gameController);
+    }
+
+    public static void setPlayers(GameController gameController) {
+
+        try {
+            gameController.registerCars();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setPlayers(gameController);
+        }
+
+    }
+
+    public static void setRounds(GameController gameController) {
+
+        try {
+            gameController.setGameRounds();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setRounds(gameController);
+        }
+
+    }
+
+    public static void playgame(GameController gameController) {
+        gameController.startRace();
     }
 }
