f0f652319eab52fddfbf04ff1f1c0dfbc86e85e1
awesomeo184 <awesomeo184@gmail.com>
2020-12-05 12:24:06 +0900

    feat(Car): 난수를 생성하여 이동하는 move, 현재 위치를 출력하는 result 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,33 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
+
     private final String name;
     private int position = 0;
+    private static final int UPPER_BOUND = 9;
+    private static final int LOWER_BOUND = 0;
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    public void move() {
+        int randomNumber = RandomUtils.nextInt(LOWER_BOUND, UPPER_BOUND);
+        if (randomNumber > 3) {
+            position++;
+        }
+        result();
+    }
+
+
+    public void result() {
+        System.out.print(name + ":");
+        for (int i = 0; i < position; i++) {
+            System.out.print('-');
+        }
+        System.out.println();
+    }
 }
