ee251582c5ad5f061bb2a8220930d8b214a241a4
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-06 01:48:28 +0900

    feat: 이름 입력 안내 메시지 출력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    private void printNameInputMessage() {
+        System.out.println(NAME_INPUT_MESSAGE);
+    }
+}
