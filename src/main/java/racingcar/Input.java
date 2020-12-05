78ac87d6025b2ecb717b8921113e07d9934330b7
yejin <jinee0717@daum.net>
2020-12-06 02:07:04 +0900

    fix : Input 클래스에서 범위무관하게 숫자를 입력할 수 있도록 수정

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -13,7 +13,18 @@ public class Input {
     public String[] getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = scanner.nextLine();
+        String[] names = makeValidNameList(inputNames);
+        return names;
+    }
 
+    public int getMoveCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputNum = scanner.nextLine();
+        int moveCount = makeValidMoveCount(inputNum);
+        return moveCount;
+    }
+
+    private String[] makeValidNameList(String inputNames){
         String[] names = inputNames.split(",");
         for (int i = 0; i < names.length; i++) {
             names[i] = names[i].trim();
@@ -24,16 +35,16 @@ public class Input {
         return names;
     }
 
-    public int getMoveCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        String inputNum = scanner.nextLine();
-        if (!inputNum.matches(pattern)) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수에는 숫자만 입력 가능합니다.");
-        }
-        return Integer.parseInt(inputNum);
-    }
-
     private boolean checkNameLen(String input) {
         return (input.length() <= Constant.MAX_NAME_LEN) && (input.length() >= Constant.MIN_NAME_LEN);
+    }
+
+    private int makeValidMoveCount(String inputNum){
+        for(int i=Constant.ZERO; i<inputNum.length(); i++){
+            if(!String.valueOf(inputNum.charAt(i)).matches(pattern)) {
+                throw new IllegalArgumentException("[ERROR] 시도 횟수에는 숫자만 입력 가능합니다.");
+            }
+        }
+        return Integer.parseInt(inputNum);
     }
 }
