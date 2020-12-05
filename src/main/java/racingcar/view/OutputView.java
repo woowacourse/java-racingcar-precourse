5d6cba4de3e25c2286bbe91351875bbf61813aa6
naru <hsik0225@gmail.com>
2020-12-05 21:34:49 +0900

    fix(output-view): 실행 결과 메세지가 여러번 출력되는 에러 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,21 +5,33 @@ import racingcar.domain.RacingCars;
 
 public class OutputView {
 
-    public static final String GAME_RESULT = "실행 결과";
+    public static final String GAME_RESULT = "\n실행 결과";
 
     public static final String MOVE_SYMBOL = "-";
 
     public static final String NULL_CHARACTER = "\0";
 
-    public void printRoundResult(RacingCars racingCars) {
-        System.out.println(GAME_RESULT);
+    public static final String FINAL_WINNERS = "최종 우승자: ";
 
+    public static final int OPEN_BRACKET_INDEX = 1;
+
+    public void printGameResult() {
+        System.out.println(GAME_RESULT);
+    }
+
+    public void printRoundResult(RacingCars racingCars) {
         for (Car racingCar : racingCars.getRacingCars()) {
-            System.out.printf("%s : %s",
+            System.out.printf("%s : %s\n",
                     racingCar.getName(), repeatSymbol(racingCar.getPosition()));
         }
 
         System.out.println();
+    }
+
+    public void printWinners(RacingCars racingCars) {
+        String winners = racingCars.findWinners().toString();
+        String removedBracketWinners = winners.substring(OPEN_BRACKET_INDEX, winners.length() - 1);
+        System.out.println(FINAL_WINNERS + removedBracketWinners);
     }
 
     private String repeatSymbol(int times) {
