4ef2560a91c0d38da8e1c43219a953c619dd4ae9
Gayoung <gyyoon4u@naver.com>
2020-12-05 12:27:45 +0900

    feat: position 이동기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,18 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 import java.util.*;
 
 public class Application {
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        String[] result = scanner.nextLine().split(",");
-        System.out.println(InputCarNameCheck.checkInputCarName(result));
+//        System.out.println(Position.);
+//        String[] result = scanner.nextLine().split(",");
+//        System.out.println(InputCarNameCheck.checkInputCarName(result));
 //        System.out.println(InputCarNameCheck.checkEachCarNameIsEmpty(scanner.nextLine()));
+//        System.out.println(Car.goOrStop(RandomUtils.nextInt(1, 9)));
     }
 
 }
