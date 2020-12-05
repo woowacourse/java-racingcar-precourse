4bbbd07a26822862f8239a581564ba0b45615e0e
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-05 22:50:41 +0900

    [구현] 경기 (Race) 클래스를 생성함

--- src/main/java/InputView/InputView.java
+++ src/main/java/InputView/InputView.java
@@ -1,20 +1,38 @@
 package InputView;
 
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    int VALID_MINIMUM_INPUT_LENGTH = 2;
+    private final int VALID_MINIMUM_CAR_NAMES_LENGTH = 2;
+    private final String ERROR_TAG = "[Error]";
+    private final String INVAlID_INPUT_LENGTH = "You should input at least one car name";
+
     private final Scanner scanner;
 
     InputView(Scanner scanner) {
         this.scanner = scanner;
     }
 
+    public List<String> inputCarNames() {
+        String carNames = this.scanner.nextLine();
 
+        if(!isValidInput(carNames)) {
+            Exception e = new Exception(ERROR_TAG + INVAlID_INPUT_LENGTH);
+            System.out.println(e.toString());
+        }
 
-    private boolean isValidInput(String inputCarNames){
-        if(inputCarNames.length() < VALID_MINIMUM_INPUT_LENGTH) {
+        return this.ConvertToList(carNames);
+    }
+
+    private List<String> ConvertToList(String target) {
+        return Arrays.asList(target.split(","));
+    }
+
+    private boolean isValidInput(String inputCarNames) {
+        if(inputCarNames.length() < VALID_MINIMUM_CAR_NAMES_LENGTH) {
             return false;
         }
         return true;
