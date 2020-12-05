ee1c140fdeebf68bd4da27a5879e8dfcd9d3082e
yunjung <cairofaith@gmail.com>
2020-12-06 01:54:18 +0900

    refactor(Car): 하드코딩 값 제거
    - 자동차 전진 기준 숫자를 static final 변수로 대체

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,6 +5,7 @@ import utils.RandomUtils;
 public class Car {
     private final String name;
     private int position = 0;
+    static final int MOVE_FORWARD = 4;
 
     public Car(String name) {
         this.name = name;
@@ -23,6 +24,6 @@ public class Car {
     }
 
     private boolean judgeIfRun(int roundNumber) {
-        return roundNumber >= 4;
+        return roundNumber >= MOVE_FORWARD;
     }
 }
