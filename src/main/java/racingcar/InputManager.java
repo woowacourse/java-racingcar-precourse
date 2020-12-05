c497b410c9c7f9d6ec1bb5125506599bb8471db1
JUNGHOJONG <hojong1351@naver.com>
2020-12-05 12:19:36 +0900

    feat: 자동차 경주 횟수 입력 시 발생할 수 있는 예외상황 처리 구현

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class InputManager {
     private static final String CAR_ERROR_STATEMENT = "[ERROR] 이름이 6자 이상 또는 중복 이름일 경우는 허용하지 않습니다";
+    private static final String COUNT_ERROR_STATEMENT = "[ERROR] 숫자가 아닌 경우는 허용하지 않습니다";
     private static final int NAME_MAX_LENGTH = 5;
     private static final String SEPARATE = ",";
     private static final IllegalArgumentException CAR_INPUT_ERROR = new IllegalArgumentException(CAR_ERROR_STATEMENT);
+    private static final IllegalArgumentException COUNT_INPUT_ERROR = new IllegalArgumentException(COUNT_ERROR_STATEMENT);
 
     private Scanner scanner;
 
@@ -30,8 +32,13 @@ public class InputManager {
         return new ArrayList<>(filteredCars);
     }
 
-    public int inputTrialCount() {
+    public int inputTrialCount() throws IllegalArgumentException{
         String trialCount = scanner.next();
+        for (int i=0; i<trialCount.length(); i++) {
+            if (trialCount.charAt(i) - '0' > 9 || trialCount.charAt(i) - '0' < 0) {
+                throw COUNT_INPUT_ERROR;
+            }
+        }
         return Integer.parseInt(trialCount);
     }
 }
