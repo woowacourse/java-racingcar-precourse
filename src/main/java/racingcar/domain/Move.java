ab369a3cea8980313ad7c2b05b1062bd7bc8f8b8
operasystem6 <operasystem6@naver.com>
2020-12-05 14:35:00 +0900

    refactor: 랜덤 값을 통한 자동차 전진 여부 클래스 구현

---
+++ src/main/java/racingcar/domain/Move.java
@@ -0,0 +1,19 @@
+/*
+ * Move.java
+ *
+ * version 1.1
+ *
+ * 2020/12/05
+ *
+ * Copyright (c) 2020 Junhyoung Park
+ */
+
+package racingcar.domain;
+
+public class Move {
+
+    static boolean isMove() {
+        return RandomNumber.generatedRandom();
+    }
+
+}
