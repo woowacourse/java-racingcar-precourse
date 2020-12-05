2640c0955ef6ba9653997fb5651445da46ec8abf
hybeom0720 <hybeom@gmail.com>
2020-12-05 15:48:35 +0900

    feat(Cars) : add the class representing the Entire Cars
    Add the Cars Class to the System on :
    - 전체적인 차들을 모두 관리 할 수 있도록 해당 객체를 추가하였습니다.
    - 생성시에 조건을 걸어 놓아서, 잘못된 생성이 안되도록 하였습니다.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class Cars {
+    private List<Car> carList;
+
+    public Cars(String[] carNames) {
+        checkOverlap(carNames);
+        for (String carName : carNames) {
+            Car tempCar = new Car(carName);
+            this.carList.add(tempCar);
+        }
+    }
+
+    private void checkOverlap(String[] carNames) {
+        HashSet<String> checkOverlapSet = new HashSet<>(Arrays.asList(carNames));
+        if (checkOverlapSet.size() != carNames.length) {
+            return;
+        }
+    }
+}
