373047e3c8a23c4ae7ffaad90564e20c286c28ac
Hyeon9mak <jinha3507@gmail.com>
2020-12-05 15:59:41 +0900

    test: add Car object constructor test

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -11,14 +11,35 @@ public class CarTest {
 
     @Test
     public void testCreate() {
-        Car car = new Car(name);
+        Car car = new Car(name, new GasTank());
         car.tryForward();
         assertThat(car.getPosition()).isEqualTo(1);
 
-        Car secondCar = new Car(secondname);
+        Car secondCar = new Car(secondname, new GasTank());
         secondCar.tryForward();
         secondCar.tryForward();
         secondCar.tryForward();
         assertThat(secondCar.getPosition()).isEqualTo(3);
+    }
+
+    @Test
+    public void testGasTankInstance() {
+        Car car1 = new Car("porori1", new GasTank());
+        Car car2 = new Car("porori2", new GasTank());
+        Car car3 = new Car("porori3", new GasTank());
+
+        for (int i = 0; i < 3; i++) {
+            car1.tryForward();
+            System.out.println(car1.getPosition());
+            car2.tryForward();
+            System.out.println(car2.getPosition());
+            car3.tryForward();
+            System.out.println(car3.getPosition());
+            System.out.println("---------------------");
+        }
+
+        System.out.println(car1.gasTank);
+        System.out.println(car2.gasTank);
+        System.out.println(car3.gasTank);
     }
 }
