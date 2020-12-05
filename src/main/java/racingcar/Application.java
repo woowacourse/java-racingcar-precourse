0d28f740d310c91bdd381c3694126bb6eeb31479
Seonho Kim <zxzx235@naver.com>
2020-12-05 15:51:18 +0900

    feat: 자동차 객체 리스트 생성하는 기능 추가
    자동차 이름 배열을 사용해 객체 리스트를 생성하고 리턴.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,13 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-
+        ArrayList<Car> carList = CarSetting.carList(scanner);
 
     }
 
--- src/main/java/racingcar/CarSetting.java
+++ src/main/java/racingcar/CarSetting.java
@@ -3,17 +3,20 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class CarName {
+public class CarSetting {
     final static String inputCarNamePrint = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
     public static ArrayList<Car> carList(Scanner scanner) {
         ArrayList<Car> result = new ArrayList<Car>();
         String inputString = inputAndOutput.inputString(scanner, inputCarNamePrint);
-        carNameList(inputString);
+        String[] carNameList = carNameList(inputString);
+        for (int i = 0; i < carNameList.length; i++) {
+            result.add(new Car(carNameList[i]));
+        }
         return result;
     }
 
-    static String[] carNameList(String inputString){
+    static String[] carNameList(String inputString) {
         return inputString.split(",");
     }
 }
