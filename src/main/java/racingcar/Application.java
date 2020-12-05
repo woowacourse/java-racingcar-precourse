1d33b80332f37628fdfa122a272526e6d4f5fc22
Seonho Kim <zxzx235@naver.com>
2020-12-05 19:54:31 +0900

    feat: 예외처리 기능 추가
    자동차 이름이 5자 초과인 경우, 시도 횟수가 0인 경우, 시도 횟수에 문자를 입력한 경우.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-        ArrayList<Car> carList = GameSetting.carList(scanner);
-        int trackLength = GameSetting.trackLength(scanner);
+        ArrayList<Car> carList = GameSetting.carListCatchException(scanner);
+        int trackLength = GameSetting.trackLengthException(scanner);
+
         GamePlay.play(trackLength, carList);
     }
 }
--- src/main/java/racingcar/GameSetting.java
+++ src/main/java/racingcar/GameSetting.java
@@ -6,12 +6,41 @@ import java.util.Scanner;
 public class GameSetting {
     final static String inputCarNamePrint = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     final static String inputTrackLengthPrint = "시도할 회수는 몇회인가요?";
+    final static String exceptionCarNamePrint = "[ERROR] 이름은 5자 이하만 입력 가능";
+    final static String exceptionTrackLengthPrint = "[ERROR] 0이 아닌 숫자만 입력 가능";
 
-    public static ArrayList<Car> carList(Scanner scanner) {
+    public static ArrayList<Car> carListCatchException(Scanner scanner) {
+        ArrayList<Car> result;
+        while (true) {
+            try {
+                result = carList(scanner);
+                break;
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return result;
+    }
+
+    static int trackLengthException(Scanner scanner) {
+        int result;
+        while (true) {
+            try {
+                result = trackLength(scanner);
+                break;
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return result;
+    }
+
+    static ArrayList<Car> carList(Scanner scanner) {
         ArrayList<Car> result = new ArrayList<Car>();
         String inputString = inputAndOutput.inputString(scanner, inputCarNamePrint);
         String[] carNameList = carNameList(inputString);
         for (int i = 0; i < carNameList.length; i++) {
+            isNameOverLength(carNameList[i]);
             result.add(new Car(carNameList[i]));
         }
         return result;
@@ -22,6 +51,25 @@ public class GameSetting {
     }
 
     static int trackLength(Scanner scanner) {
-        return inputAndOutput.inputInt(scanner, inputTrackLengthPrint);
+        String resultString = inputAndOutput.inputString(scanner, inputTrackLengthPrint);
+        int result;
+
+        try {
+            result = Integer.parseInt(resultString);
+        }catch (Exception e){
+            throw new IllegalArgumentException(exceptionTrackLengthPrint);
+        }
+
+        if (result == 0) {
+            throw new IllegalArgumentException(exceptionTrackLengthPrint);
+        }
+
+        return result;
+    }
+
+    static void isNameOverLength(String carName) {
+        if ((carName.length() > 5) || (carName.length() == 0)) {
+            throw new IllegalArgumentException(exceptionCarNamePrint);
+        }
     }
 }
