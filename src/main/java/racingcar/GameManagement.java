74948cf01684d4b04a42a62ec3d24708ca6f945d
Ji-Ha <xodhkd36@gmail.com>
2020-12-05 15:24:23 +0900

    refactor(GameWinners.java): GameManagement의 우승자 결정 메소드들을 GameWinners,java 클래스로 구성하여 refactor

---
+++ src/main/java/racingcar/GameManagement.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import outpututils.Print;
+
+import java.util.ArrayList;
+
+public class GameManagement {
+	private final int repetitionTime;
+	static final private ArrayList<Car> participatedCars = new ArrayList<Car>();
+
+	public GameManagement(String[] carInformation, int repetitionTime) {
+		this.repetitionTime = repetitionTime;
+		SetGamePlayer(carInformation);
+		StartGame();
+		Print.PrintWinner(new GameWinners(participatedCars, repetitionTime).GetGameWinners());
+	}
+
+	private void SetGamePlayer(String[] carInformation) {
+		for (int i = 0; i < carInformation.length; i++) {
+			participatedCars.add(new Car(carInformation[i]));
+		}
+	}
+
+	private void StartGame() {
+		System.out.println("\n실행결과");
+		for (int turn = 0; turn < repetitionTime; turn++) {
+				TurnManagement();
+				Print.PrintResult(participatedCars);
+		}
+	}
+
+	private void TurnManagement() {
+		for (Car player : participatedCars) {
+			player.StopOrGo();
+		}
+	}
+
+
+}
---
+++ src/main/java/racingcar/GameWinners.java
@@ -0,0 +1,41 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class GameWinners {
+	final private ArrayList<String> winners = new ArrayList<String>();
+	private int victoryScore = 0;
+
+	public ArrayList<String> GetGameWinners(){
+		return winners;
+	}
+
+	public GameWinners(ArrayList<Car> participatedCars, int repetitionTime){
+		FindVictoryScore(participatedCars);
+		FindWinner(participatedCars);
+	}
+
+	private void FindWinner(ArrayList<Car> participatedCars){
+		for(Car player : participatedCars){
+			CheckWinner(player);
+		}
+	}
+
+	private void CheckWinner(Car player){
+		if(player.GetPosition() == victoryScore){
+			winners.add((player.GetName()));
+		}
+	}
+
+	private void FindVictoryScore(ArrayList<Car> participatedCars){
+		for(Car player : participatedCars){
+			CheckVictoryScore(player.GetPosition());
+		}
+	}
+
+	private void CheckVictoryScore(int playerScore) {
+		if (playerScore > victoryScore) {
+			victoryScore = playerScore;
+		}
+	}
+}
