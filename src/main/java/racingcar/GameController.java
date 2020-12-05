e7f503b621b938fe159c3d8303a60d7767e49016
Beenie93 <seongbeen93@naver.com>
2020-12-06 01:20:17 +0900

    feat(racingcar-GameController): 자동차 경주 게임 컨트롤러 구현

---
+++ src/main/java/racingcar/GameController.java
@@ -0,0 +1,66 @@
+package racingcar;
+
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.model.GameRounds;
+import racingcar.service.CarNameTokenizer;
+import racingcar.service.RandomNumberGenerator;
+import racingcar.service.WinnersFinder;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.stream.Collectors;
+
+import static racingcar.constant.Constants.MIN_INDEX_VALUE;
+import static racingcar.constant.Constants.STANDARD_VALUE_FOR_MOVE_FORWARD;
+
+public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private Cars cars;
+    private GameRounds gameRound;
+
+    GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void registerCars() {
+        this.cars = new Cars(CarNameTokenizer.split(inputView.inputCarName())
+                .stream()
+                .map(carName -> new Car(carName))
+                .collect(Collectors.toList()));
+    }
+
+    public void setGameRounds() {
+        this.gameRound = new GameRounds(inputView.inputNumberOfAttempts());
+    }
+
+    public void startRace() {
+        outputView.showGameResult();
+
+        for (int index = MIN_INDEX_VALUE; index < this.gameRound.getRounds(); index++) {
+            startEachRound();
+            outputView.showEachRoundResult(this.cars);
+        }
+
+        outputView.showWinners(WinnersFinder.find(this.cars.getCarList()));
+    }
+
+    public void startEachRound() {
+
+        for (Car car : this.cars.getCarList()) {
+            int randomNumber = RandomNumberGenerator.generateRandomNumber();
+            updateEachRoundResult(randomNumber, car);
+        }
+
+    }
+
+    public void updateEachRoundResult(int randomNumber, Car car) {
+
+        if (randomNumber >= STANDARD_VALUE_FOR_MOVE_FORWARD) {
+            car.moveFoward();
+        }
+
+    }
+}
