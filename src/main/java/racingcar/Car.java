173c1452215a6366f2e3815b0f18488c640d0c4b
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-05 16:52:14 +0900

    feat(Car): add variables and methods
    전진 성공 여부를 판별할 때 사용할 상수 추가
    메소드 추가:
    - Car.isAbleToAccelerate
    - Car.accelerate
    - Car.getName
    - Car.getPosition
    상기한 변화에 맞게 README.md 수정
    
    경주의 구현 중 차와 관련된 기능들을 되도록 Car에 담을 수 있도록 디자인 하였습니다.
    차의 전진 여부를 판별하는 것은 Car 클래스에서만 그 성공 여부를 알면 충분하다고 생각하여, utils.RandomUtils 내의 메소드를 사용하는 행위를 Car.isAbleToAccelerate 메소드 내에서만 진행하도록 디자인했습니다.
    차의 위치를 변경하는 건 Car.isAbleToAccelerate 메소드를 Car.accelerate 메소드 내에서 호출하여 true를 받았을 경우에만 그 위치를 변경하도록 메소드를 디자인 하였습니다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,13 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
+
+    public static final int MIN_RANDOM_VALUE = 0;
+    public static final int MAX_RANDOM_VALUE = 9;
+    public static final int MIN_VALUE_FOR_ACCELERATE = 4;
+
     private final String name;
     private int position = 0;
 
@@ -8,5 +15,23 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    // nextInt 함수를 통해 불러온 인자가 MAX_VALUE_FOR_ACCELERATE 수치를 넘었는지 확인
+    private boolean isAbleToAccelerate() {
+        return RandomUtils.nextInt(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE) >= MIN_VALUE_FOR_ACCELERATE;
+    }
+
+    // 가속 시도를 한 후 성공하면 포지션을 한 칸 이동시킴
+    public void accelerate() {
+        if (isAbleToAccelerate()) {
+            this.position++;
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
 }
