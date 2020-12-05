1c0339a227e521208960649de82a1b4041d29d7d
KJunseo <max9106@naver.com>
2020-12-05 16:08:02 +0900

    style: 사용되지 않는 return값 받지 않기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,6 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
-        Race race = new Race(scanner);
+        new Race(scanner);
     }
 }
