3206d293f0ffe10d1ae06bcffc8115bf94728b03
park seok chae <tjrco56@naver.com>
2020-12-06 06:23:24 +0900

    feat(Application): 자동차 이름 입력해 추가하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,30 @@ package racingcar;
 
 import java.util.Scanner;
 
+import utils.CarNameInputValidator;
+
 public class Application {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-    }
+	private static final String DELIMITER = ",";
+	private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		inputCarNames(scanner);
+	}
+
+	private static void inputCarNames(Scanner scanner) {
+		RacingGame racingGame = RacingGame.getInstance();
+		racingGame.clearCars();
+		
+		System.out.println(INPUT_NAME_MESSAGE);
+		String[] names = scanner.nextLine().split(DELIMITER);
+
+		try {
+			CarNameInputValidator.inputVaildCheck(names);
+			racingGame.addCarByNames(names);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			inputCarNames(scanner);
+		}
+	}
 }
