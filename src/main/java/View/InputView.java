ed31971909595a73f00a9952c7437c6b4dfaac68
soongjamm <kimstz0303@gmail.com>
2020-12-05 10:25:51 +0900

    refactor: rename a variable in InputView

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_NUMBER_OF_PROGRESS = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_NUMBER_OF_PROGRESS_MESSAGE = "시도할 회수는 몇회인가요?";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -20,7 +20,7 @@ public class InputView {
     }
 
     public int inputNumberOfProgress() {
-        System.out.println(INPUT_NUMBER_OF_PROGRESS);
+        System.out.println(INPUT_NUMBER_OF_PROGRESS_MESSAGE);
         String numberOfProgress = scanner.nextLine();
         Validator.validateNumberOfProgress(numberOfProgress);
 
