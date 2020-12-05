65b3dc391e4121e92e7cc2953363affb4989af75
DWL <DWL>
2020-12-06 01:24:53 +0900

    feat: Text Resource 추가

---
+++ src/main/java/utils/TextResource.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class TextResource {
+    public static final String STATEMENT_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String STATEMENT_INPUT_ROUND = "시도할 회수는 몇회인가요?";
+
+    public static final String STATEMENT_ERROR_DEFAULT = "에러가 발생했습니다.";
+    public static final String STATEMENT_ERROR_INPUT_NOT_VALID = "입력이 올바르지 않습니다. 만약 시도 횟수의 경우라면 숫자여야 합니다.";
+    public static final String STATEMENT_ERROR_CAR_NAME_SIZE_OVER = "자동차 이름은 5자 이하만 가능합니다.";
+
+    public static final String WORD_RESULT = "실행 결과";
+    public static final String WORD_WINNER = "최종 우승자";
+}
