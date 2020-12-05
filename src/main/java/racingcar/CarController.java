7b72545e2edf52511118cf372845d9703d03384a
yejin <jinee0717@daum.net>
2020-12-06 01:33:12 +0900

    feat : CarController에 자동차 전진 로직 구현
    GameController에는 게임 전체 진행에 필요한 명령을 포함하고,
    CarController에는 자동차 객체를 관리하는 명령을 포함하도록 구현

--- src/main/java/racingcar/CarController.java
+++ src/main/java/racingcar/CarController.java
@@ -1,4 +1,29 @@
 package racingcar;
 
 public class CarController {
+    private int carAmount;
+    private Car[] cars;
+
+    public CarController(String[] names) {
+        this.carAmount = names.length;
+        cars = new Car[carAmount];
+        makeCarInstance(names);
+    }
+
+    private void makeCarInstance(String[] names) {
+        for (int i = 0; i < carAmount; i++) {
+            cars[i] = new Car(names[i]);
+        }
+    }
+
+    public void makeMove() {
+        System.out.println("실행 결과");
+        for (int i = 0; i < carAmount; i++) {
+            cars[i].move();
+            cars[i].print();
+        }
+        System.out.println();
+    }
+
+
 }
--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 
 public class GameController {
     private Scanner scanner;
-    private Car[] cars;
     private String[] names;
     private int move;
-    private int carAmount;
 
     public GameController(Scanner scanner) {
         this.scanner = scanner;
@@ -15,41 +13,16 @@ public class GameController {
 
     public void controlGame() {
         getInput();
-        makeCarInstance(names);
-        makeMove();
-        getWinner();
+        CarController carController = new CarController(names);
+        while (move-- > 0) {
+            carController.makeMove();
+        }
     }
 
     private void getInput() {
         Input input = new Input(scanner);
         names = input.getCarNames();
         move = input.getMoveCount();
-        carAmount = names.length;
-        cars = new Car[carAmount];
     }
 
-    private void makeCarInstance(String[] names) {
-        for (int i = 0; i < carAmount; i++) {
-            cars[i] = new Car(names[i]);
-        }
-    }
-
-    private void makeMove(){
-        System.out.println("실행 결과");
-        while (move-- > 0) {
-            for(int i=0; i<carAmount; i++){
-                cars[i].move();
-                cars[i].print();
-            }
-            System.out.println();
-        }
-    }
-
-    private void getWinner(){
-        int maxMove = 0;
-        for(int i=0; i<carAmount; i++){
-            maxMove = Math.max(maxMove, cars[i].getPosition());
-        }
-
-    }
 }
