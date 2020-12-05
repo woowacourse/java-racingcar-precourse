1a035c17f2b6e65cf40d79ea657c30296e1ef1ca
soongjamm <kimstz0303@gmail.com>
2020-12-05 23:37:54 +0900

    add exception for empty car name and update README.md

--- src/main/java/exceptions/Validator.java
+++ src/main/java/exceptions/Validator.java
@@ -10,7 +10,7 @@ public class Validator {
     private static final int MINIMUM_CARS_SIZE = 1;
     private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
     private static final String EMPTY_CAR_NAME_ERROR_MESSAGE = "자동차 이름이 입력되어야 한다.";
-    private static final String EXCEED_MAXIMUM_CAR_NAME_ERROR_MESSAGE = "자동차의 이름은 " + MAXIMUM_CAR_NAME_LENGTH + "자 이하여야 한다.";
+    private static final String EXCEED_MAXIMUM_CAR_NAME_ERROR_MESSAGE = "자동차의 이름은 공백이 아니고 " + MAXIMUM_CAR_NAME_LENGTH + "자 이하여야 한다.";
     private static final String MUST_BE_INTEGER_ERROR_MESSAGE = "시도 횟수는 숫자여야 한다.";
     private static final String EXCEED_INTEGER_ERROR_MESSAGE = "시도 횟수는 integer 범위에 포함되어야 한다.";
     private static final String MUST_BE_POSITIVE_NUMBER_ERROR_MESSAGE = "시도 횟수는 양수여야 한다.";
@@ -23,7 +23,7 @@ public class Validator {
 
     public static void validateCarNameLength(List<Car> cars) {
         for (Car car : cars) {
-            if (car.getName().length() > MAXIMUM_CAR_NAME_LENGTH) {
+            if (car.getName().trim().equals("") || car.getName().length() > MAXIMUM_CAR_NAME_LENGTH) {
                 throw new NotValidInputException(EXCEED_MAXIMUM_CAR_NAME_ERROR_MESSAGE);
             }
         }
