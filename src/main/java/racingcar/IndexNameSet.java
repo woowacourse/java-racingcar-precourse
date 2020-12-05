7fc11eb96f58f9838478a9e0353aca8a498bd3eb
JinseongHwang <eddy5360@naver.com>
2020-12-05 16:53:28 +0900

    refactor: separate class from AskQuestion.java

---
+++ src/main/java/racingcar/IndexNameSet.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class IndexNameSet {
+    private int index;
+    private String name;
+
+    public IndexNameSet(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
