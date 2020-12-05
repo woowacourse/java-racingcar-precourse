154b7fa71f6079d5316cc8d065a9a6212a44f77b
zeroGone <dudrhs571@gmail.com>
2020-12-05 18:06:23 +0900

    refactor(Car): Car 수정
    - 필요없는 필드 삭제

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,30 +6,27 @@ public class Car {
 	private static final int MOVE_RANDOM_MIN_VALUE = 0;
 	private static final int MOVE_RANDOM_MAX_VALUE = 9;
 	private static final int MOVE_CRITERIA_VALUE = 4;
-
-	private final StringBuilder movementBuilder;
+	
 	private final String name;
 	private int position = 0;
 
 	public Car(String name) {
-		this.movementBuilder = new StringBuilder();
-		movementBuilder.append(name).append(" : ");
-
 		this.name = name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+	
 	public void movdOrStop() {
 		int moveRandomValue = RandomUtils.nextInt(MOVE_RANDOM_MIN_VALUE, MOVE_RANDOM_MAX_VALUE);
 
 		if (moveRandomValue >= MOVE_CRITERIA_VALUE) {
 			position += 1;
-
-			movementBuilder.append("-");
 		}
-	}
-
-	@Override
-	public String toString() {
-		return movementBuilder.toString();
 	}
 }
