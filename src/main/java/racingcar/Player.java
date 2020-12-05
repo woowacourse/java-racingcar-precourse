ace4faea2f52dd50fc7e28cde28f967eb299e4b4
박우림 <woorimprog@gmail.com>
2020-12-06 00:56:38 +0900

    fix(exception) : handle exceptions

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -1,24 +1,59 @@
 package racingcar;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+
+import utils.Validator;
 
 public class Player {
 	private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
 	private static final String COUNT_QUESTION = "시도할 회수는 몇회인가요?";
 	
-	Player() {}
+	private String car;
+	private List<String> carNames;
+	
+	private int triedCnt;
 	
 	public void inputRacingCar(Scanner scanner) {
-		System.out.println(START_MESSAGE);
-		String car = scanner.nextLine();
-		List<String> carNames = Arrays.asList(car.replaceAll(" ", "").split(","));
-		
-		System.out.println(COUNT_QUESTION);
-		int triedCnt = scanner.nextInt();
+		inputCar(scanner);
+		inputTriedCnt(scanner);
 		
 		GameManager.setCar(carNames);
 		GameManager.setTriedCount(triedCnt);
+	}
+	
+	private void inputCar(Scanner scanner) {
+		while (true) {
+			System.out.println(START_MESSAGE);
+			car = scanner.nextLine();
+			carNames = Arrays.asList(car.replaceAll(" ", "").split(","));
+			
+			if (Validator.isUseableCar(carNames)) {
+				return;
+			}
+			System.out.println("[ERROR] 자동차 이름은 1자 이상, 5자 이하여야 한다.\n");
+		}
+	}
+	
+	private void inputTriedCnt(Scanner scanner) {
+		while (true) {
+			System.out.println(COUNT_QUESTION);
+			
+			try {
+				triedCnt = scanner.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.\n");
+				scanner.nextLine();
+				continue;
+			}
+			
+			if (Validator.isUseableTriedCnt(triedCnt)) {
+				return;
+			}
+			System.out.println("[ERROR] 시도 횟수는 0보다 커야 한다.\n");
+		}
+		
 	}
 }
--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.List;
+
 public class Validator {
 	private static final int START_INCLUSIVE = 0;
 	private static final int END_INCLUSIVE = 9;
@@ -12,5 +14,25 @@ public class Validator {
 		}
 		
 		return false;
+	}
+	
+	public static boolean isUseableCar(List<String> carNames) {
+		if (carNames.size() <= 0) {
+			return false;
+		}
+		
+		for (String car : carNames) {
+			if (car.length() < 1 || car.length() > 5) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isUseableTriedCnt(int triedCnt) {
+		if (triedCnt <= 0) {
+			return false;
+		}
+		return true;
 	}
 }
