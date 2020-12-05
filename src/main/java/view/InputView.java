1670c2b2734dcb83d17d4f9f3855b4c82e75f6b3
operasystem6 <operasystem6@naver.com>
2020-12-05 12:57:58 +0900

    feat: 데이터 입력 VIEW 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,3 +1,13 @@
+/*
+ * InputView.java
+ *
+ * version 1.0
+ *
+ * 2020/12/04
+ *
+ * Copyright (c) 2020 Junhyoung Park
+ */
+
 package view;
 
 import java.util.Scanner;
@@ -6,17 +16,17 @@ public class InputView {
     private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 횟수는 몇회인가요?";
 
-    public String[] inputCarName(Scanner scanner) {
+    public static String[] inputCarName(Scanner scanner) {
         System.out.println(START_MESSAGE);
         return dataSplit(scanner.nextLine());
     }
 
-    public int inputNumberOfAttempts(Scanner scanner) {
+    public static int inputNumberOfAttempts(Scanner scanner) {
         System.out.println(NUMBER_OF_ATTEMPTS_MESSAGE);
         return scanner.nextInt();
     }
 
-    private String[] dataSplit(String data) {
+    private static String[] dataSplit(String data) {
         return data.split(",");
     }
 }
