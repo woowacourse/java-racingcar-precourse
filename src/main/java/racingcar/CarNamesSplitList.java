16eddf913d3747f8b47d1e821c9a0263c542ba64
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-05 11:14:55 +0900

    fix carNamesSplitList 리턴값 코드 수정 및 하드코딩 수정

--- src/main/java/racingcar/CarNamesSplitList.java
+++ src/main/java/racingcar/CarNamesSplitList.java
@@ -5,10 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarNamesSplitList {
+    private final static String CAR_NAME_SPLITER = ",";
 
     public static List<String> carNamesSplitList(String carNames) {
-        List<String> carNamesSplitList = new ArrayList<>(Arrays.asList(carNames.split(",")));
-
-        return carNamesSplitList;
+        return new ArrayList<>(Arrays.asList(carNames.split(CAR_NAME_SPLITER)));
     }
 }
