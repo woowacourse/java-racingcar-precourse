ca27d1c5c5a767cb2da05c5a4b657e493a119279
DWL <DWL>
2020-12-06 01:37:53 +0900

    feat: 자동차 이름이 5글자 이하 여부 체크하는 기능 추가

--- src/main/java/racingcar/ReceptionDesk.java
+++ src/main/java/racingcar/ReceptionDesk.java
@@ -10,11 +10,13 @@ import java.util.Scanner;
 
 public class ReceptionDesk {
     public static final String SEPARATOR = ",";
+    public static final int MAX_PARTICIPANTS_NAME_LENGTH = 5;
 
     public Cars applyCars(Scanner scanner) throws ParticipantNameSizeOverException {
         System.out.println(TextResource.STATEMENT_INPUT_CAR_NAME);
         String input = scanner.nextLine();
         List<String> participants = parseInput(input);
+        checkParticipantNameValid(participants);
         return readyCars(participants);
     }
 
@@ -28,5 +30,13 @@ public class ReceptionDesk {
 
     private List<String> parseInput(String input) {
         return Arrays.asList(input.split(SEPARATOR));
+    }
+
+    private void checkParticipantNameValid(List<String> participants) throws ParticipantNameSizeOverException {
+        for (String name : participants) {
+            if (name.length() > MAX_PARTICIPANTS_NAME_LENGTH) {
+                throw new ParticipantNameSizeOverException();
+            }
+        }
     }
 }
