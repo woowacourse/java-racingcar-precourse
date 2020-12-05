e926e5474c9e59bbeadbf68cd8b13139f46794e0
operasystem6 <operasystem6@naver.com>
2020-12-05 14:34:31 +0900

    refactor: HashMap->ArrayList 변경 및 실행 결과를 위한 record 메소드 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,25 +1,30 @@
 /*
  * OutputView.java
  *
- * version 1.0
+ * version 1.1
  *
- * 2020/12/04
+ * 2020/12/05
  *
  * Copyright (c) 2020 Junhyoung Park
  */
 
 package view;
 
-import java.util.HashMap;
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
 
 public class OutputView {
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자:";
 
-    public static void executionResult(HashMap<String, Integer> playerRecord) {
+    public static void executionMessage() {
         System.out.println(EXECUTION_RESULT_MESSAGE);
-        for (String key : playerRecord.keySet()) {
-            System.out.println(key + " : " + playerRecord.get(key));
+    }
+
+    public static void executionResult(ArrayList<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + racingRecord(car.getPosition()));
         }
     }
 
@@ -34,5 +39,13 @@ public class OutputView {
             result += " " + text + ",";
         }
         return  result.substring(0, result.length() - 1);
+    }
+
+    private static String racingRecord(int count) {
+        String record = "";
+        for (int i = 0; i < count; i++) {
+            record += "-";
+        }
+        return record;
     }
 }
