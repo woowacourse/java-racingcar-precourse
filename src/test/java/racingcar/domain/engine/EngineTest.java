61e67b917be5dfd05ca1890b686786fec5ae5a2b
yangfriendship <yangfriendship@naver.com>
2020-12-05 18:23:09 +0900

    test (Engine) : DefaultEngine 테스트

---
+++ src/test/java/racingcar/domain/engine/EngineTest.java
@@ -0,0 +1,71 @@
+package racingcar.domain.engine;
+
+import jdk.jfr.Description;
+import jdk.jfr.internal.test.WhiteBox;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Spy;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EngineTest {
+
+    @RepeatedTest(100)
+    @Description("0~9까지 난수 생성")
+    public void getRandomNumberTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        //given
+        Engine engine = new DefaultEngine();
+
+        //when
+        Method method = DefaultEngine.class.getDeclaredMethod("getRandomNumber");
+        method.setAccessible(true);
+        int result = (int) method.invoke(engine);
+
+        //then
+        assertTrue(result >= 0 && result < 10);
+
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    @Description("숫자가 4이상이면 true, 4미만이면 false를 반환")
+    public void canMoveTest(int number) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        //given
+        Engine engine = new DefaultEngine();
+
+        //when
+        Method canMove = DefaultEngine.class.getDeclaredMethod("canMove", int.class);
+        canMove.setAccessible(true);
+
+        boolean result = (boolean) canMove.invoke(engine, number);
+
+        //then
+        Assertions.assertEquals(number >= 4, result);
+
+    }
+
+    @RepeatedTest(100)
+    @Description("1 혹은 0을 반환")
+    public void engineTest() {
+
+        //given
+        Engine engine = new DefaultEngine();
+
+        //when
+        int result = engine.getPower();
+
+        //then
+        assertTrue(result == 1 || result == 0);
+
+    }
+
+}
