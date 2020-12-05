0f5e87c98842db78fe2a2bf49c4330a5ff787567
bbakjae <qkrwogus0724@naver.com>
2020-12-05 18:38:37 +0900

    docs(Application): 기본 구현 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,5 +7,27 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         
+        String Players;
+        String[] player_name;
+        Car[] car = null;
+        int try_num;
+        Racing_game game = new Racing_game();
+        
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Players = scanner.next();
+        player_name = Players.split(",");
+        
+        for (int i = 0; i < player_name.length; i++) {
+        	System.out.println(player_name[i]);
+			car[i] = new Car(player_name[i]);
+		}
+        
+        System.out.println("시도할 회수는 몇회인가요?");
+        try_num = scanner.nextInt();
+        
+        System.out.println("실행 결과");
+        for (int i = 0; i < try_num; i++) {
+			game.play_game(car);
+		}
     }
 }
