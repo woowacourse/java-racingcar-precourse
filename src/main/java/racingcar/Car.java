4186b7f472a9c9267642625618f86dceec606f8f
Gayoung <gyyoon4u@naver.com>
2020-12-05 14:46:26 +0900

    feat: 차 한대마다 진행과정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,4 +8,27 @@ public class Car {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void goOrStop(int randomNumber) {
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getCurrentPosition() {
+        StringBuilder carPosition = new StringBuilder(name);
+        carPosition.append(" : ");
+        for (int i = 0; i < position; i++) {
+            carPosition.append("-");
+        }
+        return carPosition.toString();
+    }
+
 }
