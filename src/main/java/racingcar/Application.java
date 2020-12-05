6d764c8b5117650eddbafb346257b043277e9fac
김주원 <kjw11077@naver.com>
2020-12-06 01:04:04 +0900

    refactor(Application): update main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,9 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
-        new RacingGame(scanner).play();
+        RacingGame racingGame = new RacingGame(scanner);
+        racingGame.play();
+
         scanner.close();
     }
 }
