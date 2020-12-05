31473c0e1303aa4fdef7a4755933507daca862e0
hybeom0720 <hybeom@gmail.com>
2020-12-05 18:00:06 +0900

    feat(Game) : add the controller of game
    Add the controller as  and modfiy the method of :
    - 전체 게임을 진행하는 Game 객체를 추가하고, Application에 해당 사항을 반영하였습니다.

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import racingcar.controller.RacingGame;
+
+public class Game {
+    public void play() {
+        RacingGame racingGame = new RacingGame();
+        racingGame.play();
+    }
+}
