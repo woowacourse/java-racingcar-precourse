240be30f20dff9bab522e4f4d30ee31d812670b6
naru <hsik0225@gmail.com>
2020-12-05 21:58:59 +0900

    refactor(random-fuel-generator): 랜덤 범위 숫자 상수화

--- src/main/java/racingcar/domain/generator/RandomFuelGenerator.java
+++ src/main/java/racingcar/domain/generator/RandomFuelGenerator.java
@@ -4,8 +4,12 @@ import utils.RandomUtils;
 
 public class RandomFuelGenerator implements FuelGenerator {
 
+    public static final int LOWER_BOUND = 0;
+
+    public static final int UPPER_BOUND = 10;
+
     @Override
     public int generate() {
-        return RandomUtils.nextInt(1, 10);
+        return RandomUtils.nextInt(LOWER_BOUND, UPPER_BOUND);
     }
 }
