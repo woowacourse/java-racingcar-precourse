5ed56336d6a5556340045028b494953d84863a38
DWL <DWL>
2020-12-06 01:36:53 +0900

    feat: 입력 받은 자동차 이름을 파싱하여 Car 객체를 생성하도록 구현

--- src/main/java/racingcar/ReceptionDesk.java
+++ src/main/java/racingcar/ReceptionDesk.java
@@ -4,14 +4,29 @@ import racingcar.exception.ParticipantNameSizeOverException;
 import utils.TextResource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReceptionDesk {
+    public static final String SEPARATOR = ",";
 
     public Cars applyCars(Scanner scanner) throws ParticipantNameSizeOverException {
         System.out.println(TextResource.STATEMENT_INPUT_CAR_NAME);
         String input = scanner.nextLine();
-        return new Cars(new ArrayList<>());
+        List<String> participants = parseInput(input);
+        return readyCars(participants);
     }
 
+    private Cars readyCars(List<String> participants) {
+        ArrayList cars = new ArrayList();
+        for (String name : participants) {
+            cars.add(new Car(name));
+        }
+        return new Cars(cars);
+    }
+
+    private List<String> parseInput(String input) {
+        return Arrays.asList(input.split(SEPARATOR));
+    }
 }
