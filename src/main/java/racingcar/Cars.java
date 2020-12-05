694c0c25544ac0b4ceeafcc92575668dd408c3f0
seunghyun <ggyool@naver.com>
2020-12-06 02:07:12 +0900

    feat: 자동차 여러 대를 가지는 Cars 클래스 생성

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import racingcar.validator.CarNameValidator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<String> carNames) {
+        validate(carNames);
+        cars = carNames.stream()
+                .map(CarNameValidator::validate)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private void validate(List<String> carNames) {
+        validateDuplicatedName(carNames);
+        validateEmpty(carNames);
+    }
+
+    private void validateDuplicatedName(List<String> carNames) {
+        Set<String> nonDuplicatedNames = new HashSet<>(carNames);
+        if (nonDuplicatedNames.size() != carNames.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 이름이 있습니다.");
+        }
+    }
+
+    private void validateEmpty(List<String> carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 입력된 이름이 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/racingcar/CarsTests.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CarsTests {
+    private static Stream<Arguments> correctArguments() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("pobi", "woni", "jun")),
+                Arguments.of(Arrays.asList("sp ce", "a", "b"))
+        );
+    }
+
+    private static Stream<Arguments> wrongArguments() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("longname", "ok", "name")),
+                Arguments.of(Arrays.asList("", "ok", "name")),
+                Arguments.of(Arrays.asList("dupli", "dupli", "name")),
+                Arguments.of(Arrays.asList())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("correctArguments")
+    public void 객체생성_성공(List<String> names) {
+        assertThatCode(() -> new Cars(names))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("wrongArguments")
+    public void 객체생성_실패_예외발생(List<String> names) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Cars(names));
+    }
+}
+
