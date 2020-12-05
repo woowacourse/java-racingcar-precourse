ded3f6609aa757349e516774257186bfa9ab46f2
Beenie93 <seongbeen93@naver.com>
2020-12-06 02:17:57 +0900

    refactor(model-Cars): 중복된 차 이름이 있을 경우, 생성된 Car 인스턴스가 1개일 경우에 대한 예외 추가
    Before:
    
    - 중복된 차 이름이 있을 경우, 게임이 진행되었다.
    - "pobi," 를 입력하였을 경우, "pobi"라는 이름을 가진 Car 인스턴스 하나만 생성되고 게임이 진행되었다.
    
    After:
    
    - 중복된 차 이름이 있을 경우, 에러문구 출력 후 자동차 이름 재입력 받게한다.
    - Car 인스턴스 하나만 생성됐을 경우, 에러문구 출력 후 자동차 이름 재입력 받게한다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -2,14 +2,34 @@ package racingcar.model;
 
 import java.util.List;
 
+import static racingcar.constant.Constants.*;
+
 public class Cars {
     private List<Car> cars;
 
     public Cars(List<Car> cars) {
+        validateCarAmount(cars);
+        validateSameName(cars);
         this.cars = cars;
     }
 
     public List<Car> getCarList() {
         return cars;
+    }
+
+    private void validateCarAmount(List<Car> cars) {
+
+        if (cars.size() < MIN_TOTAL_CARS_AMOUNT) {
+            throw new IllegalArgumentException(ERROR_ONLY_ONE_CAR);
+        }
+
+    }
+
+    private void validateSameName(List<Car> cars) {
+
+        if (cars.size() != cars.stream().map(Car::getName).distinct().count()) {
+            throw new IllegalArgumentException(ERROR_SAME_NAME_CAR);
+        }
+
     }
 }
