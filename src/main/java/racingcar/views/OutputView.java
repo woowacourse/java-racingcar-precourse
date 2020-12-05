0710798dc55732dbd14c09b08dc6418933620963
opdshe <opdshe@naver.com>
2020-12-05 15:45:53 +0900

    feat: 최종 우승자 출력 기능 구현

--- src/main/java/racingcar/views/OutputView.java
+++ src/main/java/racingcar/views/OutputView.java
@@ -2,11 +2,14 @@ package racingcar.views;
 
 import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
+import racingcar.domain.Winners;
 
 public class OutputView {
     private static final String RESULT_GUIDING_MESSAGE = "실행 결과";
+    private static final String FINAL_WINNERS = "최종 우승자";
     private static final String DISTANCE = "-";
     private static final String COLON = " : ";
+    private static final String COMMA = ", ";
     private static final String BLANK = "";
 
     private OutputView() {
@@ -29,6 +32,11 @@ public class OutputView {
             distanceBar.append(DISTANCE);
         }
         return distanceBar.toString();
+    }
+
+    public static void printWinners(Winners winners) {
+        String finalWinners = FINAL_WINNERS + COLON + String.join(COMMA, winners.getWinners());
+        printMessageAndNewLine(finalWinners);
     }
 
     public static void printResultGuidingMessage() {
