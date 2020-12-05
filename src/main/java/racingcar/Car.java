8d3a047ef8b54fd077e2664059ee2514b89d476c
kyle456 <kyle2123@naver.com>
2020-12-06 00:44:37 +0900

    feat: 각 자동차마다 0에서 9사이의 난수 구하는 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -7,5 +9,9 @@ public class Car {
     public Car(String name) {
         CarValidator.validateCarName(name);
         this.name = name;
+    }
+
+    public int makeRandomNumber() {
+        return RandomUtils.nextInt(0, 9);
     }
 }
