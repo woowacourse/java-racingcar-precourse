71df15d982a41517f5c921502443325d4380d6c9
bbakjae <qkrwogus0724@naver.com>
2020-12-05 17:28:53 +0900

    docs(Car) Car객체의 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,17 +1,31 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
-    public void increase_position() {
-    	this.position++;
-    }
-    
-    
+	// 추가 기능 구현
+	public void increase_position() {
+		this.position++;
+	}
+
+	public void move_or_stop() {
+		if (RandomUtils.nextInt(0, 9) >= 4) {
+			increase_position();
+		}
+	}
+
+	public void print_position() {
+		System.out.println(name+" : ");
+		for (int i = 0; i < position; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
 }
