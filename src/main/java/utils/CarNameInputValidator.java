7a907dde42985de4bdbe9e954eafa1ce60a15b8e
park seok chae <tjrco56@naver.com>
2020-12-06 06:10:49 +0900

    feat(CarNameInputValidator): 자동차 이름 체크해 문자열을 인자로 예외 발생시키는 기능 추가

--- src/main/java/utils/CarNameInputValidator.java
+++ src/main/java/utils/CarNameInputValidator.java
@@ -5,6 +5,28 @@ import java.util.HashSet;
 
 public class CarNameInputValidator {
 	private static final Integer MAX_CARNAME_LENGTH = 5;
+	private static final String ERROR_PREFIX = "[ERROR] ";
+	private static final String NULL_ERROR_MESSAGE = "자동차 이름은 1자 이상이여야 합니다.";
+	private static final String LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하이여야 합니다.";
+	private static final String UNIQUE_ERROR_MESSAGE = "자동차 이름은 중복되면 안 됩니다.";
+
+	public static void inputVaildCheck(String[] names) {
+		for (String name : names) {
+			if (isNull(name)) {
+				throw new IllegalArgumentException(ERROR_PREFIX + NULL_ERROR_MESSAGE);
+			}
+		}
+
+		for (String name : names) {
+			if (!isRightLength(name)) {
+				throw new IllegalArgumentException(ERROR_PREFIX + LENGTH_ERROR_MESSAGE);
+			}
+		}
+
+		if (!isAllUniqueName(names)) {
+			throw new IllegalArgumentException(ERROR_PREFIX + UNIQUE_ERROR_MESSAGE);
+		}
+	}
 
 	public static boolean isNull(String name) {
 		if (name == null || name.isEmpty()) {
