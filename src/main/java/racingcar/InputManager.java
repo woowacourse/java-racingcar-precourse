9fa37172dbdd12fa536945d1c615b9f7898f39f1
yunjung <cairofaith@gmail.com>
2020-12-06 01:11:09 +0900

    feat(InputManager): 시도 횟수 입력을 관리하는 메소드 추가
    - 입력된 시도 횟수가 숫자인지 검증
    - 입력된 시도 횟수가 양의 정수인지 검증

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -13,4 +13,25 @@ public class InputManager {
         carList = (Arrays.asList(input.split(",")));
         return carList;
     }
+
+    public int readTrialNum(String trialsNum) throws IllegalArgumentException {
+        if (!checkIfNumber(trialsNum)) {
+            throw new IllegalArgumentException("[Error] 시도 횟수는 양의 정수로 입력합니다.");
+        }
+
+        if (Integer.parseInt(trialsNum) < 0) {
+            throw new IllegalArgumentException("[Error] 시도 횟수는 양의 정수로 입력합니다.");
+        }
+
+        return Integer.parseInt(trialsNum);
+    }
+
+    private boolean checkIfNumber(String trialsNum) {
+        for(int i = 0; i < trialsNum.length(); i++) {
+            if(!Character.isDigit(trialsNum.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
