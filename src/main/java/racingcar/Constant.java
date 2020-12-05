3a1809dd126d76ccafd6d04e39aa4af37bdb0709
yejin <jinee0717@daum.net>
2020-12-05 22:21:47 +0900

    feat : Constant 클래스 추가
    기본적으로 필요한 상수 정의

---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class Constant {
+    public static final int MIN_RANGE_NUM = 0;
+    public static final int MAX_RANGE_NUM = 9;
+    public static final int MAX_NAME_LEN = 5;
+    public static final int ADVANCE_NUM = 4;
+    public static final int STOP_NUM = 3;
+}
