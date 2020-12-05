6a2a7f496b7e73cce145ed15226c59dca74ad6c8
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-06 00:19:15 +0900

    refactor: InputView 클래스 내 콤마 상수 처리 및 자동차 이름 입력 받는 메소드 로직 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,19 @@
 package racingcar.view;
 
+import racingcar.domain.AttemptNumber;
 import racingcar.domain.Car;
-
 import racingcar.domain.Cars;
-import racingcar.domain.Rule;
 import utils.InputValidator;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String COMMA = ",";
 
     private InputView() {
     }
@@ -21,11 +22,9 @@ public class InputView {
         try {
             System.out.println(INPUT_CAR_NAMES_MESSAGE);
             String inputCarNames = InputValidator.validateInputCarNames(scanner.nextLine());
-            String[] carNames = inputCarNames.split(",");
-            List<Car> cars = new ArrayList<>();
-            for (String carName : carNames) {
-                cars.add(new Car(carName));
-            }
+            List<Car> cars = Arrays.stream(inputCarNames.split(COMMA))
+                    .map(carName -> new Car(carName))
+                    .collect(Collectors.toList());
             return new Cars(cars);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -33,10 +32,10 @@ public class InputView {
         }
     }
 
-    public static Rule inputAttemptNumber(Scanner scanner) {
+    public static AttemptNumber inputAttemptNumber(Scanner scanner) {
         try {
             System.out.println(INPUT_ATTEMPT_NUMBER_MESSAGE);
-            return new Rule(InputValidator.validateInteger(scanner.nextLine()));
+            return new AttemptNumber(InputValidator.validateInteger(scanner.nextLine()));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputAttemptNumber(scanner);
