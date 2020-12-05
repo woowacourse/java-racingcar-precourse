e8c1ce88e57e061dddcf4c994066cf63e68171f3
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-05 11:11:04 +0900

    refactor run 메서드 내부 로직 분리

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class CarRacing {
     private final static String WINNERS_SPLITER = ", ";
     private final static int TRYCOUNT_INDEX = 0;
+
     private final Scanner scanner;
 
     public CarRacing(Scanner scanner) {
@@ -15,19 +16,29 @@ public class CarRacing {
     }
 
     public void run() {
+        List<Car> carList = initCar();
+        printCarRacingRepeat(carList);
+        printCarRacingResult(carList);
+    }
+
+    public List<Car> initCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameInput = scanner.nextLine();
         List<String> carNamesSplitList = CarNamesSplitList.carNamesSplitList(carNameInput);
         ValidateUtils.carNameLengthCheck(carNamesSplitList);
-        List<Car> carList = CarGenerate.carGenerate(carNamesSplitList);
+        return CarGenerate.carGenerate(carNamesSplitList);
+    }
 
+    public void printCarRacingRepeat(List<Car> carList) {
         System.out.println("시도할 회수는 몇회인가요?");
         int tryCount = ValidateUtils.checkTryCountParseInt(scanner.nextLine());
         System.out.println("실행 결과");
         for (int i = TRYCOUNT_INDEX; i < tryCount; i++) {
             CarRacingRepeat.repeatCarRacing(carList);
         }
+    }
 
+    public void printCarRacingResult(List<Car> carList) {
         List<String> winnerList = CarRacingPrint.printWinnerCar(carList);
         System.out.println(String.format("최종 우승자: %s ", String.join(WINNERS_SPLITER, winnerList)));
     }
