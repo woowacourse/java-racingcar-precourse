3f6aacf57ee8725e746e0b37f4b73da7c6682f6f
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-12-06 01:19:19 +0900

    feat : NameValidator, RepetitionCountValidator 클래스 생성 및 내부 함수 명명

---
+++ src/main/java/utils/NameValidator.java
@@ -0,0 +1,25 @@
+package utils;
+
+public class NameValidator {
+
+    public static void checkNameList(){
+
+    }
+
+    private void checkInvalidCharacters(){
+
+    }
+
+    private void checkNamesLength(){
+
+    }
+
+    private void checkNameDuplicate(){
+
+    }
+
+    private void checkNull(){
+
+    }
+
+}
---
+++ src/main/java/utils/RepetitionCountValidator.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class RepetitionCountValidator {
+
+    public static void checkValidRepetitionCount(){
+
+    }
+
+    private void checkInteger(){
+
+    }
+
+    private void checkPositiveInteger(){
+
+    }
+
+}
