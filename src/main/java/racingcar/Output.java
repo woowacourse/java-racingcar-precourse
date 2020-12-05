ed5df4be15f52c54daa2309a8bcb11014ff62623
Noh Hyeon Jin <hjforj@naver.com>
2020-12-05 19:13:11 +0900

    feat: 최종 우승자 출력 기능 구현

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Output {
     private static final String GO_MARK = "-";
+    private static final String FINAL_WINNER_INFO_MESSAGE = "최종 우승자: ";
+    private static final String SPLIT_DELIMITER = ", ";
     
     private Car[] car;
     
@@ -19,5 +23,31 @@ public class Output {
             }
             System.out.println();
         }
+    }
+    public void printFinalWinner() {
+        ArrayList<Car> finalWinner = getFinalWinner();
+        System.out.print(FINAL_WINNER_INFO_MESSAGE);
+        int index = 0;
+        for (Car car : finalWinner) {
+            System.out.print(car.getName());
+            index++;
+            if (index != finalWinner.size() - 1) {
+                System.out.print(SPLIT_DELIMITER);
+            }
+        }
+    }
+    
+    public ArrayList<Car> getFinalWinner() {
+        ArrayList<Car> finalWinner = new ArrayList<Car>();
+        int maxPosition = 0;
+        for (int i = 0; i < car.length; i++) {
+            maxPosition = Math.max(maxPosition, car[i].getPosition());
+        }
+        for (int i = 0; i < car.length; i++) {
+            if (car[i].getPosition() == maxPosition) {
+                finalWinner.add(car[i]);
+            }
+        }
+        return finalWinner;
     }
 }
