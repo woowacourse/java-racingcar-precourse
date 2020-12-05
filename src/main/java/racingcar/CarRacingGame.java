60a72b61e5a31b478aaffeaaca4c5762005f1940
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-05 22:28:08 +0900

    refactor: 입력에 대한 처리 기능 다른 클래스로 분리
    입력값 처리를 InputManager.java에서 관리

--- src/main/java/racingcar/CarRacingGame.java
+++ src/main/java/racingcar/CarRacingGame.java
@@ -3,72 +3,45 @@ package racingcar;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import utils.InputPrinter;
-import utils.OutPutPrinter;
-import utils.Validator;
+import utils.InputManager;
+import utils.OutPutManager;
 
 public class CarRacingGame {
     private static final int INITIAL_VALUE = 0;
-    private static final String COMMA = ",";
 
-    private CarList carList;
+    private final RaceCars raceCars;
     private int raceRound;
-    private Validator validator;
 
     public CarRacingGame() {
-        carList = new CarList();
+        raceCars = new RaceCars();
         raceRound = INITIAL_VALUE;
-        validator = new Validator();
     }
 
-    public void inputInfoForPlayGame(Scanner scanner)
-    {
-        carList.setCarList(inputRaceCar(scanner));
-        raceRound = inputRaceRound(scanner);
-    }
+    public void inputInfoForPlayGame(Scanner scanner) {
+        InputManager inputManager = new InputManager(scanner);
 
-    private List<String> inputRaceCar(Scanner scanner) {
-        try {
-            InputPrinter.inputCarPrint();
-            List<String> carList = new ArrayList<>(Arrays.asList(scanner.nextLine().split(COMMA)));
-            validator.isValidCars(carList);
-            return carList;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return inputRaceCar(scanner);
-        }
-    }
-
-    private int inputRaceRound(Scanner scanner) {
-        try {
-            InputPrinter.inputRaceRoundPrint();
-            String raceRound = scanner.nextLine();
-            validator.isValidRaceRound(raceRound);
-            return Integer.parseInt(raceRound);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return inputRaceRound(scanner);
-        }
+        raceCars.setRaceCars(inputManager.inputRaceCar());
+        raceRound = inputManager.inputRaceRound();
     }
 
     public void startCarRacing() {
-        Race race = new Race(carList, raceRound);
+        Race race = new Race(raceCars, raceRound);
         race.runCarRace();
     }
 
     public void carRaceWinner() {
-        List<Car> raceDoneCarList = carList.getCarList();
-        List<Car> sortedCarList = raceDoneCarList.stream()
+        List<Car> sortedCarList = raceCars.getRaceCars().stream()
                 .sorted((a, b) -> b.getPosition() - a.getPosition())
                 .collect(Collectors.toList());
 
         List<String> winners = new ArrayList<>();
         int maxPosition = sortedCarList.get(0).getPosition();
+
         for (Car car : sortedCarList) {
             if (maxPosition == car.getPosition()) {
                 winners.add(car.getName());
             }
         }
-        OutPutPrinter.carRaceWinnerPrint(String.join(COMMA, winners));
+        OutPutManager.carRaceWinnerPrint(winners);
     }
 }
