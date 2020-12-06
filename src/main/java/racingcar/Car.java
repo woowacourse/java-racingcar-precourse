cbf7975340008799527c6013cbb9fc31e64028fa
baekchan1024 <dlqhs1024@naver.com>
2020-12-06 23:03:18 +0900

    feat (Car) : randomNumber에 의한 position 증가 및 출력 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,10 +3,13 @@ package racingcar;
 public class Car {
     private final String name;
     private int position = 0;
+    private final String FOWARD_CAR = "-";
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    
+    
 }
