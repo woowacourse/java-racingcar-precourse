a166f9abcaa2c07dcc880d23b3a0a89d0eba20b3
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-06 20:05:30 +0900

    feat: 전진 여부 결정 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,6 +5,7 @@ import utils.RandomUtils;
 public class Car {
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
+    private static final int MARCH_ORDER_STANDARD = 4;
     private final String name;
     private int position = 0;
 
@@ -12,9 +13,14 @@ public class Car {
         this.name = name;
     }
 
+    private boolean CheckMarchOrder(int randomNumber) {
+        if (randomNumber >= MARCH_ORDER_STANDARD) {
+            return true;
+        }
+        return false;
+    }
+
     private int generateRandomNumber() {
         return RandomUtils.nextInt(MIN_NUMBER, MAX_NUMBER);
     }
-
-    // 추가 기능 구현
 }
