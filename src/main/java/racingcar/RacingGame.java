9677a32c4412f172bc4dd419350081297a4d6a09
zzudy <wngus1100@gmail.com>
2020-12-06 19:52:55 +0900

    feat(RacingGame) : 입력을 받아 자동차ArrayList 생성하는 기능 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,23 +1,46 @@
 package racingcar;
 
+import utils.GameUtils;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class RacingGame {
     Scanner scanner;
     ArrayList<Car> Cars;
+    GameUtils gameUtils;
 
     public RacingGame(Scanner scanner){
         this.scanner = scanner;
+        this.gameUtils = new GameUtils();
+        this.Cars = new ArrayList<Car>();
     }
-    public void playGame(){
+    public void playGame() {
+        try {
+            inputCars();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-    }
     public void inputCars(){
-
+        final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        final String ERROR_MESSAGE = "[ERROR] 자동차의 이름은 5자 이하여야 한다";
+        System.out.println(INPUT_MESSAGE);
+        String userCarsName = scanner.nextLine();
+        String[] nameList = userCarsName.split(",");
+        for(int i=0; i<nameList.length; i++){
+            if(gameUtils.isRightCarName(nameList[i])) {
+                registerCars(nameList[i]);
+                continue;
+            }
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
     }
-    public void makeCarList(){
 
+    public void registerCars(String carName){
+        Car car = new Car(carName);
+        this.Cars.add(car);
     }
 
     public void inputNumberOfGames(){
