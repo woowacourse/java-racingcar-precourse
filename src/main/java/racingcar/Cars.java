1053894ece31e6d64904f43b8d72f3e4572c629b
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-06 19:27:06 +0900

    refactor(Cars): validateCarAmount method deleted as it is duplicated with combination of other validate methods now

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -13,8 +13,6 @@ public class Cars {
 	private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 차 이름은 " + CAR_NAME_LENGTH_LOWER_BOUND + "자 이상 "
 			+ CAR_NAME_LENGTH_UPPER_BOUND + "자 이하여야 합니다.";
 	private static final String SPACES_IN_CAR_NAME_ERROR_MESSAGE = "[ERROR] 차 이름에 공백이 포함될 수 없습니다.";
-	private static final String CAR_VOID_NAME_ERROR_MESSAGE = "[ERROR] 차 이름은 공백일 수 없습니다";
-	private static final String NUMBER_OF_CARS_ERROR_MESSAGE = "[ERROR] 차 이름이 적어도 " + MINIMUM_NUMBER_OF_CARS_REQUIRED + "개 필요합니다.";
 	private static final int START_INCLUSIVE_NUMBER = 0;
 	private static final int END_EXCLUSIVE_NUMBER = 10;
 	private static final String WINNER_DELIMITER = ", ";
@@ -22,16 +20,17 @@ public class Cars {
 	private final List<Car> cars;
 
 	public Cars(List<Car> cars) throws IllegalArgumentException {
-		validateCarAmount(cars);
-		validateDuplicateCarName(cars);
 		validateCarNameLength(cars);
+		validateDuplicateCarName(cars);
 		validateSpacesInCarName(cars);
 		this.cars = cars;
 	}
 
-	private void validateCarAmount(List<Car> cars) throws IllegalArgumentException {
-		if (cars.size() == 0) {
-			throw new IllegalArgumentException(NUMBER_OF_CARS_ERROR_MESSAGE);
+	private void validateCarNameLength(List<Car> cars) throws IllegalArgumentException {
+		for (Car car : cars) {
+			if (car.getNameLength() < CAR_NAME_LENGTH_LOWER_BOUND || CAR_NAME_LENGTH_UPPER_BOUND < car.getNameLength()) {
+				throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
+			}
 		}
 	}
 
@@ -45,14 +44,6 @@ public class Cars {
 	private void validateDuplicateCarName(List<Car> cars) throws IllegalArgumentException {
 		if (countUniqueCarName(cars) != cars.size()) {
 			throw new IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR_MESSAGE);
-		}
-	}
-
-	private void validateCarNameLength(List<Car> cars) throws IllegalArgumentException {
-		for (Car car : cars) {
-			if (car.getNameLength() < CAR_NAME_LENGTH_LOWER_BOUND || CAR_NAME_LENGTH_UPPER_BOUND < car.getNameLength()) {
-				throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
-			}
 		}
 	}
 
