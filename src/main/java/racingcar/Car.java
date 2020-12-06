e59329efb2244d4891548dc023b7ea0b56bf3820
DH Lim <dlarkfl2@gamil.com>
2020-12-06 21:09:13 +0900

    refactor(Car): 생성자에 예외 필터 추가
    기존 InputView에서 실시하던 Car.name 유효성 검사를 Car 생성자에 포함시킴

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,13 +7,18 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        validateLength(name);
         this.name = name;
     }
 
     // 추가 기능 구현
-    static final int RANDOM_NUMBER_INCLUSIVE_START = 0;
-    static final int RANDOM_NUMBER_INCLUSIVE_END = 9;
-    static final int MOVE_FORWARD_NUMBER_INCLUSIVE_START = 4;
+    private static final int RANDOM_NUMBER_INCLUSIVE_START = 0;
+    private static final int RANDOM_NUMBER_INCLUSIVE_END = 9;
+    private static final int MOVE_FORWARD_NUMBER_INCLUSIVE_START = 4;
+    private static final int CAR_NAME_LENGTH_INCLUSIVE_START = 1;
+    private static final int CAR_NAME_LENGTH_INCLUSIVE_END = 5;
+    private static final String ERROR_MESSAGE_START = "[ERROR] ";
+    private static final String ERROR_MESSGAE_END_NAME_ISINBOUND = "경주할 자동차 이름은 1글자 이상, 5글자 이하여야 한다.";
 
     public String getName() {
         return name;
@@ -23,13 +28,20 @@ public class Car {
         return position;
     }
 
+    private void validateLength(String name) {
+        int nameLength = name.length();
+        if (name == null || nameLength > CAR_NAME_LENGTH_INCLUSIVE_END || nameLength < CAR_NAME_LENGTH_INCLUSIVE_START) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_START + ERROR_MESSGAE_END_NAME_ISINBOUND);
+        }
+    }
+
     public void moveForwardOrStop() {
-        if (generateRandomNumber() >= MOVE_FORWARD_NUMBER_INCLUSIVE_START) {
+        if (createRandomNumber() >= MOVE_FORWARD_NUMBER_INCLUSIVE_START) {
             position++;
         }
     }
 
-    private int generateRandomNumber() {
+    private int createRandomNumber() {
         return RandomUtils.nextInt(RANDOM_NUMBER_INCLUSIVE_START, RANDOM_NUMBER_INCLUSIVE_END);
     }
 }
