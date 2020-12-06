86133024dc39f844fb6cccfd5f802ef48ad045be
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-06 21:02:57 +0900

    상수 추가

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final int MIN_NUMBER = 0;
     public static final int MAX_NUMBER = 9;
     public static final int STANDARD_NUMBER = 4;
-
+    public static final int MAX_NAME_COUNT = 5;
 }
