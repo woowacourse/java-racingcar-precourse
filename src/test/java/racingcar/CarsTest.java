6124261916cd49ef45490b519dd319d25660b487
bangjom <kbj7227@gmail.com>
2020-12-06 18:47:17 +0900

    refactor: intellij GoogleStyle 적용

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -8,16 +8,16 @@ import org.junit.jupiter.api.Test;
 
 class CarsTest {
     @Test
-    public void findMaxPositionCarsTest() throws Exception{
+    public void findMaxPositionCarsTest(){
         //given
-        List<String> carsList = Arrays.asList("BJ","YJ","AJ");
+        List<String> carsList = Arrays.asList("BJ", "YJ", "AJ");
         Cars cars = new Cars(carsList);
         //when
-        do{
+        do {
             cars.moveByRandomNumber();
-        } while(cars.findMaxPositionCars().size() != 2);
-        List<Car> result= cars.findMaxPositionCars();
+        } while (cars.findMaxPositionCars().size() != 2);
+        List<Car> result = cars.findMaxPositionCars();
         //then
-        assertEquals(0,result.get(0).compareTo(result.get(1)));
+        assertEquals(0, result.get(0).compareTo(result.get(1)));
     }
 }
