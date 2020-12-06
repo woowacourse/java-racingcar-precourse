1ac26070f40dcf840da24467644d53ed96421b81
Naeun <knae11@naver.com>
2020-12-06 11:29:58 +0900

    feat(ValidatorUtils): 유효검사에 조건 추가, 메세지 상수처리
    조건추가
    1. 자동차 이름이 빈 문자열인 경우
    2. 시도횟수가 양의정수가 아닌 경우
    메세지 상수처리함

--- src/main/java/utils/ValidatorUtils.java
+++ src/main/java/utils/ValidatorUtils.java
@@ -1,10 +1,14 @@
 package utils;
 
-import racingcar.Car;
-
 public class ValidatorUtils {
 
     private static final int MAX_CAR_NAME = 5;
+    private static final int MIN_VALUE_ONE = 1;
+    private static final String ERR0R_PREFIX = "[ERROR] ";
+    private static final String OVER_FIVE_LETTERS = ERR0R_PREFIX + "자동차 이름은 5자 이하이어야 한다.";
+    private static final String EMPTY_LETTER = ERR0R_PREFIX + "모든 자동차는 이름이 있어야 한다.";
+    private static final String MUST_BE_NUMBER = ERR0R_PREFIX + "시도 횟수는 숫자여야 한다.";
+    private static final String MUST_BE_POSITIVE_INTEGER = ERR0R_PREFIX + "시도 횟수는 양수여야 한다.";
 
     private ValidatorUtils() {
 
@@ -13,16 +17,22 @@ public class ValidatorUtils {
     public static void checkUnderFiveLetters(String[] carNames) {
         for (String carName : carNames) {
             if (carName.length() > MAX_CAR_NAME) {
-                throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하이어야 한다.");
+                throw new IllegalArgumentException(OVER_FIVE_LETTERS);
+            }
+            if (carName.length() < MIN_VALUE_ONE) {
+                throw new IllegalArgumentException(EMPTY_LETTER);
             }
         }
     }
 
     public static void checkIsNumber(String num) {
         try {
-            Integer.parseInt(num);
+            int number = Integer.parseInt(num);
+            if (number < MIN_VALUE_ONE) {
+                throw new IllegalArgumentException(MUST_BE_POSITIVE_INTEGER);
+            }
         } catch (NumberFormatException n) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 한다.");
+            throw new IllegalArgumentException(MUST_BE_NUMBER);
         }
     }
 }
