ec7dfb0da84ca8f13e9903375f17dbe84a115034
DWL <DWL>
2020-12-06 17:37:46 +0900

    feat: 우승자 판별하는 기능 추가

---
+++ src/main/java/racingcar/WinnerCondition.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinnerCondition {
+
+    public Cars getWinners(Cars participants) {
+
+        return new Cars(checkWinner(participants.getCars()));
+    }
+
+    private List<Car> checkWinner(List<Car> cars) {
+        int max = getMaxPosition(cars);
+        ArrayList<Car> winners = new ArrayList();
+        for (Car car : cars) {
+            if (car.getPosition() == max) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    private int getMaxPosition(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+}
