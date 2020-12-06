5f56a51ababd0d1f2955468d8be4522f5d61dfd1
baekchan1024 <dlqhs1024@naver.com>
2020-12-06 19:07:34 +0900

    feat (Message) : 게임 진행 메시지 문자열을 담은 클래스

---
+++ src/main/java/racingcar/Message.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Message {
+	public static String CHOOSE_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static String CHOOSE_RACING_COUNT = "시도할 회수는 몇회인가요?";
+	public static String EXECUTION_RESULT = "실행 결과";
+	public static String FINAL_WINNER = "최종 우승자";
+}
