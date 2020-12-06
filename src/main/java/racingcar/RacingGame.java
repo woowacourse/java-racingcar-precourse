85b43f2e608f0a47b4c8db00673694a4445b96e2
zzudy <wngus1100@gmail.com>
2020-12-06 18:52:19 +0900

    feat : 레이싱게임 클래스 틀 작성

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class RacingGame {
+    Scanner scanner;
+    ArrayList<Car> Cars;
+
+    public RacingGame(Scanner scanner){
+        this.scanner = scanner;
+    }
+    public void playGame(){
+
+    }
+    public void inputCars(){
+
+    }
+    public void makeCarList(){
+
+    }
+
+    public void inputNumberOfGames(){
+
+    }
+
+    public void getCarinfo(){
+
+    }
+    public void printResult(){
+
+    }
+    public void printWinner(){
+
+    }
+
+}
