96f8442aea30978aef0a09c6860031a6229c9eeb
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-07 00:37:08 +0900

    refactor(racing-car): 자동차 입력 값 검증 로직 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,18 +1,21 @@
 package racingcar;
 
+import racingcar.validator.CarValidator;
+
+import javax.xml.validation.Validator;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
     private List<Car> cars;
-    private final String DELIMITER = ",";
 
     public Cars() {
     }
 
-    public void createCars(String inputRacingCars) {
-        cars = Arrays.stream(inputRacingCars.split(DELIMITER))
+    public void createCars(String racingCarNames) {
+        CarValidator.validate(racingCarNames);
+        cars = Arrays.stream(racingCarNames.split(CarValidator.DELIMITER))
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
