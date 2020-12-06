6f523329b06db5596ab966726aca9c4151168da2
Injun <ison@sfu.ca>
2020-12-06 15:36:59 +0900

    refactor: 자동차 이름 구분자 상수화

--- src/main/java/racingcar/GameConstants.java
+++ src/main/java/racingcar/GameConstants.java
@@ -7,6 +7,9 @@ public class GameConstants {
     public static final String INVALID_CAR_NAMES_MESSAGE = "[ERROR] 자동차 이름은 쉼표(,)로 구분됩니다";
     public static final String SHOW_RACING_NAME_MARGIN = " : ";
     public static final String RACING_ROAD = "-";
+    public static final String CAR_NAME_DELIMETER = ",";
+    public static final String CAR_NAME_SPLITTER = ", ";
+
     public static final int CAR_NAME_MAX_LENGTH = 5;
     public static final int MIN_RANDOM_NUM = 0;
     public static final int MAX_RANDOM_NUM = 9;
