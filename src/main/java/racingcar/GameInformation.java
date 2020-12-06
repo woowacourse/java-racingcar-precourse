4471ae244c926f0065fb965cc9275dcc93687c11
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2020-12-06 16:20:40 +0900

    refactor(racing-car): 상수화 작업

--- src/main/java/racingcar/GameInformation.java
+++ src/main/java/racingcar/GameInformation.java
@@ -9,6 +9,8 @@ public class GameInformation {
     private static int numberOfTrial;
     private static ArrayList<Car> carsInGame = new ArrayList<Car>();
 
+    private static final int MIN_CAR_NAME_LENGTH = 0;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ENTER_NUMBER_OF_TRIAL = "시도할 회수는 몇회인가요?";
     private static final String ERROR_CAR_NAME = "[ERROR] 경주할 자동차 이름이 올바르게 입력되지 않았습니다.";
@@ -37,7 +39,7 @@ public class GameInformation {
 
     private static void checkEachCarName(String car) {
         try {
-            if ((car.length() == 0) || (car.length() > 5)) {
+            if ((car.length() == MIN_CAR_NAME_LENGTH) || (car.length() > MAX_CAR_NAME_LENGTH)) {
                 throw new UserInputException();
             }
         } catch (UserInputException e) {
