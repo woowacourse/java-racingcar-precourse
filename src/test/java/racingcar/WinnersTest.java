1d2c72fb7283d17cd08219ebd25ad362fef6c8c1
김주원 <kjw11077@naver.com>
2020-12-06 22:58:43 +0900

    test(Winners): add unittest getWinners

--- src/test/java/racingcar/WinnersTest.java
+++ src/test/java/racingcar/WinnersTest.java
@@ -5,33 +5,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
 public class WinnersTest {
 
-    Car joy;
-    Car happy;
+    Car a, b, c;
 
     @Before
     public void setUp() throws Exception {
-        joy = new Car("joy");
-        happy = new Car("happy");
+        a = new Car("a");
+        b = new Car("b");
+        c = new Car("c");
     }
 
     @Test
     public void getWinnerNames_multiWinners() {
         Winners gameResult = new Winners(
-                Arrays.asList(joy, happy));
+                Arrays.asList(a, b));
 
-        assertThat(gameResult.getWinnerNames()).isEqualTo("joy, happy");
+        assertThat(gameResult.getWinnerNames()).isEqualTo("a, b");
     }
 
     @Test
     public void getWinnerNames_singleWinner() {
         Winners gameResult = new Winners(
-                Collections.singletonList(joy));
+                Collections.singletonList(a));
 
-        assertThat(gameResult.getWinnerNames()).isEqualTo("joy");
+        assertThat(gameResult.getWinnerNames()).isEqualTo("a");
+    }
+
+    @Test
+    public void getWinners() {
+        List<Car> cars = Arrays.asList(a, b, c);
+        a.moveForward();
+
+        Car car = new Car("a");
+        car.moveForward();
+        Winners winners = new Winners(Collections.singletonList(car));
+        assertThat(Winners.getWinners(cars)).isEqualTo(winners);
     }
 }
