cb5ae16ba913645243e41285f5ebb94d140182c9
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-06 18:24:02 +0900

    [구현] 차가 전진 또는 멈추는 함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,5 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,19 +5,28 @@ import utils.RandomUtils;
 public class Car {
     private static final int MINIMUM_DISTANCE = 0;
     private static final int MAXIMUM_DISTANCE = 9;
+    private static final int MOVING_POSSIBLE_MIN_LENGTH = 4;
 
     private final String name;
-    private final int position = 0;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
-
     }
 
-    public int randomDistance() {
+    private int randomDistance() {
         return RandomUtils.nextInt(MINIMUM_DISTANCE, MAXIMUM_DISTANCE);
     }
 
+    private boolean isReallyMoving(int distance) {
+        return distance >= MOVING_POSSIBLE_MIN_LENGTH;
+    }
 
+    public void tryingMove() {
+        int distance = randomDistance();
+        if(isReallyMoving(distance)) {
+            this.position += distance;
+        }
+    }
     // 추가 기능 구현
 }
