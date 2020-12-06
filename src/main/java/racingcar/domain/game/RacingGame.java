4f0c080a987f41f05ba108a903f27e4dff47c8b1
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-06 12:29:31 +0900

    refactor: RacingGame 인스턴스 변수 int에서 Iterator<Integer>로 변경
    int 값인 경우, 게임의 종료 유무를 확인하기 위해서는 객체의 상태가
    계속 변경되는 가변 객체가 된다. 불변 객체로 사용하기 위해, Iterator를
    응용한다.

--- src/main/java/racingcar/domain/game/RacingGame.java
+++ src/main/java/racingcar/domain/game/RacingGame.java
@@ -3,16 +3,22 @@ package racingcar.domain.game;
 import racingcar.domain.car.Cars;
 import racingcar.domain.exception.RacingTryCountsNumberFormatException;
 
+import java.util.Iterator;
+import java.util.stream.Stream;
+
 public class RacingGame {
     private static final int MINIMUM_RACING_TRY_COUNTS = 1;
+    private static final int EMPTY_VALUE = 0;
 
     private final Cars cars;
-    private final int racingTryCounts;
+    private final Iterator<Integer> racingTryCounts;
 
     public RacingGame(Cars cars, int racingTryCounts) {
         validateRacingTryCounts(racingTryCounts);
         this.cars = cars;
-        this.racingTryCounts = racingTryCounts;
+        this.racingTryCounts = Stream.generate(() -> EMPTY_VALUE)
+                .limit(racingTryCounts)
+                .iterator();
     }
 
     private void validateRacingTryCounts(int racingTryCounts) {
