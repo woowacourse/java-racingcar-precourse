0298748b797ef07757ab2b1d9794a5c3bde5f0d9
zzudy <wngus1100@gmail.com>
2020-12-06 20:01:33 +0900

    feat(GameUtils) : 시도 횟수가 숫자인지 확인하는 함수 구현

--- src/main/java/utils/GameUtils.java
+++ src/main/java/utils/GameUtils.java
@@ -3,9 +3,18 @@ package utils;
 public class GameUtils {
     public GameUtils() {
     }
-    public boolean isRightCarName(String userCarsName){
-        if(userCarsName.length() <=5) return true;
+
+    public boolean isRightCarName(String userCarsName) {
+        if (userCarsName.length() <= 5) return true;
         return false;
     }
 
+    public boolean isNumber(String userNumber) {
+        for (int i = 0; i < userNumber.length(); i++) {
+            if (!Character.isDigit(userNumber.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
