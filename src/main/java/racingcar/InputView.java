6d426d320323ddadee8f01e696d0cdcf00280a7b
baekchan1024 <dlqhs1024@naver.com>
2020-12-07 00:48:16 +0900

    feat (InputView) : 사용자 입력 횟수 리턴 함수 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class InputView {
 	Scanner scanner = new Scanner(System.in);
-	String carNames = scanner.nextLine();
-	String[] carNamesArray = carNames.split(carNames, ',');
+
+	public String[] inputCarNames() {
+		Message.chooseCarName();
+		String carNames = scanner.nextLine();
+		String[] carNamesArray = carNames.split(",");
+		
+		return carNamesArray;
+	}
+	
+	public int inputChooseRacingCount() {
+		Message.chooseRacingCount();
+		int racingCount = scanner.nextInt();
+		
+		return racingCount;
+	}
 }
