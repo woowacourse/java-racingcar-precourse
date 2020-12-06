b9770de091cf3b8d497c7e4fde1b9d848d1ce786
Gomding <backchida@gmail.com>
2020-12-07 04:23:24 +0900

    feat : 자동차 이름에 공백이 있는경우를 검사하는 메서드 추가
    havaBlankCarName() : 자동차 이름에 빈칸이 있는경우
    "[ERROR] 자동차 이름에 공백은 사용이 불가능합니다." 를 출력합니다

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,15 +9,15 @@ public class RacingGame {
     private static final int START = 0;
     private static final int END = 9;
 
+    private String[] carNames;
+
     public void playGame(Scanner scanner) {
-        String[] carNames;
 
         while (true) {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             carNames = scanner.nextLine().split(",");
-            System.out.println(Arrays.toString(carNames));
 
-            if (!isAvailableCarName(carNames)) {
+            if (haveBlankCarName() || !isValidSizeCarName() || isDuplicateName()) {
                 continue;
             }
             break;
@@ -27,10 +27,19 @@ public class RacingGame {
         for (String carName : carNames) {
             carList.add(new Car(carName));
         }
-
     }
 
-    private boolean isAvailableCarName(String[] carNames) {
+    private boolean haveBlankCarName() {
+        for (String carName : carNames) {
+            if (carName.contains(" ")) {
+                System.out.println("[ERROR] 자동차 이름에 공백은 사용이 불가능합니다.");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isValidSizeCarName() {
         for (String carName : carNames) {
             if (carName.length() <= 0 || carName.length() > 5) {
                 System.out.println("[ERROR] 자동차 이름은 0자이상 5자이하만 가능합니다.");
@@ -38,5 +47,22 @@ public class RacingGame {
             }
         }
         return true;
+    }
+
+    private boolean isDuplicateName() {
+        List<String> carList = new ArrayList<>();
+
+        for (String carName : carNames) {
+            if (!carList.contains(carName)) {
+                carList.add(carName);
+            }
+        }
+
+        if (carList.size() != carNames.length) {
+            System.out.println("[ERROR] 자동차 이름은 중복이 불가능합니다.");
+            return true;
+        }
+
+        return false;
     }
 }
