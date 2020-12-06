e42d852e5d558af8944c19a43874b07dd0c7b748
김주원 <kjw11077@naver.com>
2020-12-06 10:42:33 +0900

    refactor(Car): extract method playSingleGame

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static racingcar.GameResult.getResult;
+import static utils.PrintUtils.print;
 
 import java.util.List;
 import utils.RandomUtils;
@@ -12,27 +13,30 @@ public class Race {
     public static final int MOVE = 4;
 
     public static GameResult start(List<Car> cars, int tryCount) {
-
-        System.out.println("실행 결과");
+        print("실행 결과");
         for (int i = 0; i < tryCount; i++) {
-            for (Car car : cars) {
-                moveCar(car);
-            }
-            System.out.println();
+            playSingleGame(cars);
         }
 
         return getResult(cars);
     }
 
-    private static void moveCar(Car car) {
-        if (isMove()) {
+    private static void playSingleGame(List<Car> cars) {
+        for (Car car : cars) {
+            driveOrStopCar(car);
+        }
+        System.out.println();
+    }
+
+    private static void driveOrStopCar(Car car) {
+        if (isDrive()) {
             car.moveForward();
         }
         car.printStatus();
     }
 
 
-    private static boolean isMove() {
+    private static boolean isDrive() {
         // 4 이상일 경우 전진하고, 3 이하의 값이면 멈춘다
         return MOVE <= RandomUtils.nextInt(RANDOM_START, RANDOM_END);
     }
