9925ffaad0abacd75ae5be96413009890f668f28
zzudy <wngus1100@gmail.com>
2020-12-06 20:36:00 +0900

    feat(RacingGame) : 랜덤값이 4이상일 경우 전진하는 함수 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,9 +1,12 @@
 package racingcar;
 
 import utils.GameUtils;
+
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.ArrayList;
+
+import utils.RandomUtils;
 
 public class RacingGame {
     private Scanner scanner;
@@ -11,29 +14,32 @@ public class RacingGame {
     private GameUtils gameUtils;
     private int numberOfGames;
 
-    public RacingGame(Scanner scanner){
+    public RacingGame(Scanner scanner) {
         this.scanner = scanner;
         this.gameUtils = new GameUtils();
         this.Cars = new ArrayList<Car>();
     }
+
     public void playGame() {
         try {
             inputCars();
             inputNumberOfGames();
-            System.out.println(this.numberOfGames);
+            for (int i = 0; i < this.numberOfGames; i++) {
+                moveCars();
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void inputCars(){
+    public void inputCars() {
         final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
         final String ERROR_MESSAGE = "[ERROR] 자동차의 이름은 5자 이하여야 한다";
         System.out.println(INPUT_MESSAGE);
         String userCarsName = scanner.nextLine();
         String[] nameList = userCarsName.split(",");
-        for(int i=0; i<nameList.length; i++){
-            if(gameUtils.isRightCarName(nameList[i])) {
+        for (int i = 0; i < nameList.length; i++) {
+            if (gameUtils.isRightCarName(nameList[i])) {
                 registerCars(nameList[i]);
                 continue;
             }
@@ -41,28 +47,42 @@ public class RacingGame {
         }
     }
 
-    public void registerCars(String carName){
+    public void registerCars(String carName) {
         Car car = new Car(carName);
         this.Cars.add(car);
     }
 
-    public void inputNumberOfGames(){
+    public void inputNumberOfGames() {
         final String INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
         final String ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다";
         System.out.println(INPUT_MESSAGE);
         String userNumber = scanner.nextLine();
-        if(!gameUtils.isNumber(userNumber))
+        if (!gameUtils.isNumber(userNumber))
             throw new IllegalArgumentException(ERROR_MESSAGE);
         this.numberOfGames = Integer.parseInt(userNumber);
     }
 
-    public void getCarinfo(){
+    public void getCarinfo() {
 
     }
-    public void printResult(){
 
+    public void moveCars() {
+        for (int i = 0; i < this.Cars.size(); i++) {
+            Car nowCar = Cars.get(i);
+            int randNum = RandomUtils.nextInt(0, 9);
+            if (isGo(randNum)) nowCar.movePosition();
+        }
     }
-    public void printWinner(){
+
+    public boolean isGo(int num) {
+        if (num >= 4) return true;
+        return false;
+    }
+
+    public void printResult() {
+    }
+
+    public void printWinner() {
 
     }
 
