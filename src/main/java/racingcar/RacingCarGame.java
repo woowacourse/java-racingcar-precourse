56a4a6e22cad879b0df1346551ed2eabbd945d86
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-07 02:48:28 +0900

    feat(racing-car-game): 시도(Round) 객체 초기화 기능 추가

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -8,9 +8,12 @@ import java.util.Scanner;
 
 public class RacingCarGame {
     private final String INPUT_RACING_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)";
+    private final String INPUT_RACING_ROUND = "시도할 회수는 몇회인가요?";
 
     private final Scanner scanner;
     private Cars cars;
+    private Round round;
+
 
     public RacingCarGame(Scanner scanner) {
         this.scanner = scanner;
@@ -18,19 +21,32 @@ public class RacingCarGame {
 
     public void startGame() {
         try {
-            initRacingCars();
+            initialize();
         } catch (RacingCarException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    private void initialize() {
+        initRacingCars();
+        initRacingRound();
+    }
+
     private void initRacingCars() {
-        this.cars = new Cars();
-        cars.createCars(inputRacingCars());
+        this.cars = new Cars(inputRacingCars());
     }
 
     private String inputRacingCars() {
         System.out.println(INPUT_RACING_CARS);
+        return scanner.nextLine();
+    }
+
+    private void initRacingRound() {
+        this.round = new Round(inputRacingRound());
+    }
+
+    private String inputRacingRound() {
+        System.out.println(INPUT_RACING_ROUND);
         return scanner.nextLine();
     }
 }
