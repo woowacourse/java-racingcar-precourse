51e17b676b8e8e166b79a9930cfc6d32311f2d23
TaewanKimmmm <ktww88@gmail.com>
2020-12-07 02:24:58 +0900

    feat: 입력받은 자동차 이름 중복 예외처리 추가

--- src/main/java/validation/CarNameValidator.java
+++ src/main/java/validation/CarNameValidator.java
@@ -1,5 +1,9 @@
 package validation;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CarNameValidator {
     private static final int MINIMUM_CAR_NAME_LENGTH = 1;
     private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
@@ -11,6 +15,7 @@ public class CarNameValidator {
         String[] carNames = splitCarName(carName);
         validateCarNameCount(carNames);
         validateCarNameLength(carNames);
+        validateCarNameDuplication(carNames);
     }
 
     private static String[] splitCarName(String carNames) {
@@ -32,6 +37,14 @@ public class CarNameValidator {
             if (carName.length() < MINIMUM_CAR_NAME_LENGTH || MAXIMUM_CAR_NAME_LENGTH < carName.length()) {
                 throw new IllegalArgumentException("[ERROR] 경주할 자동차의 이름은 1 이상 5이하여야 합니다.");
             }
+        }
+    }
+
+    private static void validateCarNameDuplication(String[] carNames) {
+        int originalSize = carNames.length;
+        Set<String> carNameSet = new HashSet<String>(Arrays.asList(carNames));
+        if (originalSize != carNameSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 경주할 자동차의 이름이 중복됩니다.");
         }
     }
 }
