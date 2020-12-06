f954d812f6b4faa8e0a823ba65f990e0f6001635
Ji-Ha <xodhkd36@gmail.com>
2020-12-06 18:35:11 +0900

    feat(application.java): main에서 게임 시작.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,14 @@
 package racingcar;
 
+import inpututils.InputName;
+import inpututils.inputRepetitions;
+
 import java.util.Scanner;
 
 public class Application {
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		// TODO 구현 진행
+		GameManagement gameTest = new GameManagement(new InputName(scanner).inputPlayerName(), new inputRepetitions(scanner).inputanswer());
 	}
 }
