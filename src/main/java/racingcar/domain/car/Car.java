0e9a46445227a71d13f17bf8d2c9e546897f088c
yangfriendship <yangfriendship@naver.com>
2020-12-06 16:55:12 +0900

    refactor (Car) : Engine을 생성자를 통해서 선택할 수 있도록 변경, 상수 제거

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -2,6 +2,7 @@ package racingcar.domain.car;
 
 import racingcar.domain.engine.DefaultEngine;
 import racingcar.domain.engine.Engine;
+import racingcar.domain.setting.Message;
 
 import java.util.Objects;
 
@@ -9,10 +10,7 @@ public class Car implements Comparable<Car> {
 
     private final String name;
     private int position = 0;
-    private Engine engine = new DefaultEngine();
-
-    private static final char POSITION_MARK = '-';
-
+    private Engine engine;
 
     private Car(String name) {
         this.name = name;
@@ -20,9 +18,20 @@ public class Car implements Comparable<Car> {
 
     // 추가 기능 구현
 
+
+    public Car(String name, Engine engine) {
+        this.name = name;
+        this.engine = engine;
+    }
+
+    public static Car create(String name, Engine engine) {
+
+        return new Car(name, engine);
+    }
+
     public static Car createNameBy(String name) {
 
-        return new Car(name);
+        return new Car(name, new DefaultEngine());
     }
 
     public void changeEngine(Engine engine) {
@@ -57,7 +66,7 @@ public class Car implements Comparable<Car> {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < position; i++) {
-            stringBuilder.append(POSITION_MARK);
+            stringBuilder.append(Message.POSITION_MARK);
         }
 
         return stringBuilder.toString();
