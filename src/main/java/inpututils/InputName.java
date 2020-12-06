407a89998d24dec9fd1f85a80c6d5bf834b355ae
Ji-Ha <xodhkd36@gmail.com>
2020-12-06 17:01:41 +0900

    refactor(inputName.java): 메소드명 적절하게 변경 & code format

--- src/main/java/inpututils/InputName.java
+++ src/main/java/inpututils/InputName.java
@@ -1,43 +1,54 @@
 package inpututils;
 
-import controlexception.UserInputNameException;
+import controlexception.UserInputNameLengthException;
+import controlexception.UserInputPlayerNumberException;
 
 import java.util.Scanner;
 
 public class InputName extends Input {
 	private boolean CHECKEDERROR = false;
+
 	public InputName(Scanner scanner) {
 		super(scanner);
 	}
 
-	public String[] inputName() {
+	public String[] inputPlayerName() {
 		String[] name = new String[0];
 		do {
 			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 			try {
 				name = splitName(scanner.next());
-			}catch(UserInputNameException i ){
-				System.out.println(i.getMessage());
+			} catch (UserInputNameLengthException e) {
+				System.out.println(e.getMessage());
+			} catch (UserInputPlayerNumberException e) {
+				System.out.println(e.getMessage());
 			}
-		}while(CHECKEDERROR == false);
+		} while (CHECKEDERROR == false);
 		return name;
 	}
 
-	private String[] checkName(String[] name){
-		for(String playerName : name) {
+	private String[] checkPlayerName(String[] name) {
+		checkPersonnel(name.length);
+		for (String playerName : name) {
 			checkLength(playerName);
 		}
 		CHECKEDERROR = true;
 		return name;
 	}
 
-	private void checkLength(String playerName){
-		if(playerName.length() > 5){
-			throw new UserInputNameException(playerName.length());
+	private void checkLength(String playerName) {
+		if (playerName.length() > 5) {
+			throw new UserInputNameLengthException(playerName.length());
 		}
 	}
 
-	public String[] splitName(String name) {
-		return checkName(name.split(","));
+	private String[] splitName(String name) {
+		return checkPlayerName(name.split(","));
+	}
+
+	private void checkPersonnel(int playerNumber) {
+		if (playerNumber < 2) {
+			throw new UserInputPlayerNumberException(playerNumber);
+		}
 	}
 }
