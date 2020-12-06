644b25aa9cf63e141f81b5f8bcad6fed8d3c4dfc
DWL <DWL>
2020-12-06 17:36:48 +0900

    feat: Car들을 관리하기 위한 Cars 추가
    일급컬렉션 사용

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars = new ArrayList();
+
+    public Cars(List<Car> cars) {
+        this.cars.addAll(cars);
+    }
+
+    public void moveAll() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
