5f1e6d47aaad56b5e27abdb4250a0081f1992abd
zzudy <wngus1100@gmail.com>
2020-12-06 19:51:03 +0900

    feat(Car) : Car 객체 get메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,4 +9,12 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public String getCarName() {
+        return this.name;
+    }
+
+    public int getCarPosition() {
+        return this.position;
+    }
+
 }
