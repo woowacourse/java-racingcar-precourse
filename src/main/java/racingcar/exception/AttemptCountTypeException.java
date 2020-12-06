d0399191bac44cfeeb3ffbbd3c041453e843a320
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-06 18:56:41 +0900

    feat: 시도 횟수 입력 기능 및 예외 처리 구현

---
+++ src/main/java/racingcar/exception/AttemptCountTypeException.java
@@ -0,0 +1,9 @@
+package racingcar.exception;
+
+public class AttemptCountTypeException extends RuntimeException {
+    private static final String COUNT_TYPE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 integer 형식으로 입력해주세요.";
+
+    public AttemptCountTypeException() {
+        super(COUNT_TYPE_ERROR_MESSAGE);
+    }
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import racingcar.exception.NameLengthOutOfBoundsException;
 import racingcar.exception.EmptyNameException;
 import racingcar.exception.OverlapNameException;
+import racingcar.exception.AttemptCountTypeException;
 
 import java.util.Scanner;
 import java.util.Arrays;
@@ -11,6 +12,7 @@ import java.util.HashSet;
 
 public class InputView {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String REINPUT_MESSAGE = "다시 입력하십시오.";
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MIN_NAME_LENGTH = 1;
@@ -30,8 +32,22 @@ public class InputView {
         return nameList;
     }
 
+    public int scanAttemptCount() {
+        printAttemptCountInputMessage();
+        String attemptCount = scanUserInput();
+        while (!isValidAttemptCount(attemptCount)) {
+            printReInputMessage();
+            attemptCount = scanUserInput();
+        }
+        return Integer.parseInt(attemptCount);
+    }
+
     private void printNameInputMessage() {
         System.out.println(NAME_INPUT_MESSAGE);
+    }
+
+    private void printAttemptCountInputMessage() {
+        System.out.println(ATTEMPT_COUNT_INPUT_MESSAGE);
     }
 
     private String scanUserInput() {
@@ -48,6 +64,16 @@ public class InputView {
             validationNameList(nameList);
             return true;
         } catch(RuntimeException errorMessage) {
+            System.out.println(errorMessage);
+            return false;
+        }
+    }
+
+    private boolean isValidAttemptCount(String attemptCount) {
+        try{
+            validationCountNumber(attemptCount);
+            return true;
+        } catch (AttemptCountTypeException errorMessage) {
             System.out.println(errorMessage);
             return false;
         }
@@ -88,6 +114,15 @@ public class InputView {
             return true;
         }
         return false;
+    }
+
+    private boolean validationCountNumber(String attemptCount) {
+        try {
+            Integer.parseInt(attemptCount);
+            return true;
+        } catch (NumberFormatException e) {
+            throw new AttemptCountTypeException();
+        }
     }
 
     private void printReInputMessage() {
