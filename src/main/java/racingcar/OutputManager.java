5d73c849b482db938589835dd0aaa40f41ac2ca8
NewWisdom <wlgp2500@gmail.com>
2020-12-06 22:29:43 +0900

    refactor(output-manager): rename class printer to output-manager

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import java.util.List;
 
-public class Printer {
+public class OutputManager {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_NUMBER_MESSAGE = "시도할 횟수는 몇회인가요?";
     private static final String MOVING_RESULT_HEADER_MESSAGE = "실행 결과";
