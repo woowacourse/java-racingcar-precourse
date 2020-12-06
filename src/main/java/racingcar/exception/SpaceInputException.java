60ae7c914f5985880c1dff808ebe35143363f314
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-07 00:50:44 +0900

    refactor(space-input-exception): 상수 이름 변경

--- src/main/java/racingcar/exception/SpaceInputException.java
+++ src/main/java/racingcar/exception/SpaceInputException.java
@@ -1,9 +1,9 @@
 package racingcar.exception;
 
 public class SpaceInputException extends RacingCarException {
-    private static final String msg = "[ERROR] 입력 값은 공백이 아니여야 한다.";
+    private static final String DEFAULT_MESSAGE = "[ERROR] 입력 값은 공백이 아니여야 한다.";
 
     public SpaceInputException() {
-        super(msg);
+        super(DEFAULT_MESSAGE);
     }
 }
