688db6e5b213f012b0b2abcc575dbadbee3cc00b
yunjung <cairofaith@gmail.com>
2020-12-07 01:06:50 +0900

    feat(GameStatus): 우승자 출력 메소드 추가
    - 우승자를 (,) 기준으로 콘솔에 출력

--- src/main/java/racingcar/GameStatus.java
+++ src/main/java/racingcar/GameStatus.java
@@ -1,8 +1,22 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameStatus {
 
     public GameStatus() {
+    }
+
+    public static void printWinner(RacingCars racingCars) {
+        List<String> winners = new ArrayList<>();
+        for (int carIndex = 0; carIndex < racingCars.getTotalNumberOfCars(); carIndex++) {
+            if (racingCars.getCar(carIndex).isWinner(racingCars.maxPosition)) {
+                winners.add(racingCars.getCar(carIndex).getName());
+            }
+        }
+
+        System.out.println(String.join(",", winners));
     }
 
     public static void printStatus(RacingCars racingCars) {
@@ -14,11 +28,11 @@ public class GameStatus {
     }
 
     private static void printName(RacingCars racingCars, int carIndex) {
-        System.out.print(racingCars.getCarName(carIndex) + " : ");
+        System.out.print(racingCars.getCar(carIndex).getName() + " : ");
     }
 
     private static void printPosition(RacingCars racingCars, int carIndex) {
-        for (int pos = 0; pos < racingCars.getCarPosition(carIndex); pos++) {
+        for (int pos = 0; pos < racingCars.getCar(carIndex).getPosition(); pos++) {
             System.out.print('-');
         }
         System.out.print("\n");
