7647b549f36724a87ea3d62d3a1de9d4e7d3031f
Jinho <jh8579@gmail.com>
2020-12-07 03:38:22 +0900

    feat(Application) : add game runner class and use run method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,10 @@ package racingcar;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        GameRunner gameRunner = new GameRunner();
+        gameRunner.run(scanner);
     }
 }
