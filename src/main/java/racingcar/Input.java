dbbddaa8d78433bcd116645b6136e05afbff6a68
eoc940 <hgk0000@naver.com>
2020-12-06 16:17:17 +0900

    refactor(input): ','를 COMMA라는 constant로 대체

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -6,12 +6,12 @@ public class Input {
 
     static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     static final String INPUT_TRIAL_MESSAGE = "시도할 회수는 몇회인가요?";
-
+    static final String COMMA = ",";
 
     public String[] getCarNameArray(Scanner scanner) {
         System.out.println(INPUT_NAME_MESSAGE);
         String inputCars = scanner.next();
-        String[] carNames = inputCars.split(",");
+        String[] carNames = inputCars.split(COMMA);
         return carNames;
     }
 
