37b2f97157c7aee4c8e9ff244a03b2788665b306
baekchan1024 <dlqhs1024@naver.com>
2020-12-07 02:52:37 +0900

    feat (Cars) : 배열화된 객체들 position 이동 및 출력 메소드 구현
    메소드 구현 중 커밋 까먹음

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -5,5 +5,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
+	ArrayList<Car> carList = new ArrayList<Car>();
 	
+	public void arrayOfObjects(String[] carName){
+		for (int i = 0; i < carName.length; i++) {
+			Car car = new Car(carName[i]);
+			carList.add(i, car);
+		}
+		moveCarRun(carList);
+	}
+	
+	private void moveCarRun(ArrayList<Car> carList){
+		InputView inputView = new InputView();
+		int count = inputView.inputChooseRacingCount();
+		
+		for (int i = 0; i < carList.size(); i++) {
+			Car a = carList.get(i);
+			a.countMovePosition(count);
+		}
+		printMoveCars(carList);
+	}
+	
+	private void printMoveCars(ArrayList<Car> carList) {
+		Message.executionResult();
+		for (int i = 0; i < carList.size(); i++) {
+			Car a = carList.get(i);
+			a.printCarMoving();
+			System.out.println();
+		}
+	}
 }
