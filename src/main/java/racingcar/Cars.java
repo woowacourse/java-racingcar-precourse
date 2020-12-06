2f9ecdf9c74cfb2c3945f1d2b09852337f92ce18
yhh1056 <yhh1056@naver.com>
2020-12-06 21:14:50 +0900

    Refactor: Cars 일급 컬렉션으로 포장
    RacingGame 클래스에 있던 List<Car> cars 멤버 변수를 Cars 클래스로 분리하여 외부에서 추가하거나 수정할 수 없도록 일급컬렉션으로 포장하였습니다.

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,57 @@
+package racingcar;
+
+import static racingcar.Rule.END_RANGE;
+import static racingcar.Rule.START_RANGE;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import utils.RandomUtils;
+
+/**
+ * @author yhh1056
+ * @since 2020/12/05
+ */
+public class Cars {
+    private static final int FIRST_CAR = 0;
+
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void moves(final int gameCount) {
+        for (int i = 0; i < gameCount; i++) {
+            move();
+        }
+    }
+
+    private void move() {
+        for (Car car : cars) {
+            car.move(getRandomNumber());
+            System.out.println(car);
+        }
+        System.out.println();
+    }
+
+    private int getRandomNumber() {
+        return RandomUtils.nextInt(START_RANGE, END_RANGE);
+    }
+
+    private Car getWinner() {
+        Collections.sort(cars);
+        return cars.get(FIRST_CAR);
+    }
+
+    public List<String> findWinners() {
+        return findCoWinners(getWinner());
+    }
+
+    private List<String> findCoWinners(Car winner) {
+        return cars.stream()
+            .filter(winner::equalsPosition)
+            .map(Car::getName)
+            .collect(Collectors.toList());
+    }
+}
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,59 +1,26 @@
 package racingcar;
 
 import static racingcar.Message.RESULT_MESSAGE;
-import static racingcar.Rule.END_RANGE;
-import static racingcar.Rule.START_RANGE;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import utils.RandomUtils;
 
 /**
  * @author yhh1056
  * @since 2020/12/03
  */
 public class RacingGame {
-    private static final int FIRST_CAR = 0;
-
-    private final List<Car> cars;
+    private Cars cars;
 
     public RacingGame(List<Car> cars) {
-        this.cars = cars;
+        this.cars = new Cars(cars);
     }
 
     public void startRace(int gameCount) {
         System.out.println(RESULT_MESSAGE);
-        for (int i = 0; i < gameCount; i++) {
-            moveCars();
-        }
-    }
-
-    private void moveCars() {
-        for (Car car : cars) {
-            car.move(getRandomNumber());
-            System.out.println(car);
-        }
-        System.out.println();
-    }
-
-    private int getRandomNumber() {
-        return RandomUtils.nextInt(START_RANGE, END_RANGE);
+        cars.moves(gameCount);
     }
 
     public List<String> findWinners() {
-        return findCoWinners(getWinner());
-    }
-
-    private Car getWinner() {
-        Collections.sort(cars);
-        return cars.get(FIRST_CAR);
-    }
-
-    private List<String> findCoWinners(Car winner) {
-        return cars.stream()
-                   .filter(winner::equalsPosition)
-                   .map(Car::getName)
-                   .collect(Collectors.toList());
+        return cars.findWinners();
     }
 }
