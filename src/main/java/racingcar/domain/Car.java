4241a8b2c32fa07643225aa686f8bf6da4ba63f3
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-06 19:49:07 +0900

    feat: 0 ~ 9 사이의 random 숫자 생성 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,19 @@
 package racingcar.domain;
 
+import utils.RandomUtils;
+
 public class Car {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    private int generateRandomNumber() {
+        return RandomUtils.nextInt(MIN_NUMBER, MAX_NUMBER);
     }
 
     // 추가 기능 구현
