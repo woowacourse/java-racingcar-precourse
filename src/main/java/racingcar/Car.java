6b50ef169856be897b1a79d977d62366bb17e0c3
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-06 21:18:45 +0900

    feat(car): 전진하기 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,27 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
     private int position = 0;
+
+    private final int START_INCLUSIVE = 1;
+    private final int END_INCLUSIVE = 9;
+    private final int FORWARD_CRITERION = 4;
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    public void startTurn() {
+        if (RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE) >= FORWARD_CRITERION) {
+            goForward();
+        }
+    }
+
+    private void goForward() {
+        this.position++;
+    }
 }
