1dbc36151928f5df20b43d3758e71fb82c83df95
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-12-06 13:44:52 +0900

    feat(racing-car-game): generate random number between 0 and 9

--- src/main/java/racingcar/model/RacingCarGame.java
+++ src/main/java/racingcar/model/RacingCarGame.java
@@ -1,6 +1,15 @@
 package racingcar.model;
 
+import utils.RandomUtils;
+
 public class RacingCarGame {
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
     public RacingCarGame() {
+    }
+
+    private int generateRandomNumber() {
+        return RandomUtils.nextInt(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
     }
 }
