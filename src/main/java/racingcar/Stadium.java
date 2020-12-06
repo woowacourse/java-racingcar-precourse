b852cff05a1957a3f7967b922b545659c44f327a
DWL <DWL>
2020-12-06 17:39:52 +0900

    feat:  Race를 주최하는 Stadium 추가

---
+++ src/main/java/racingcar/Stadium.java
@@ -0,0 +1,43 @@
+package racingcar;
+
+import racingcar.exception.ParticipantNameSizeOverException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Stadium {
+    private Race race;
+    private ScoreBoard scoreBoard;
+    private ReceptionDesk receptionDesk;
+
+    public Stadium() {
+        receptionDesk = new ReceptionDesk();
+        scoreBoard = new ScoreBoard();
+    }
+
+    public void readyRace(Scanner scanner) throws ParticipantNameSizeOverException, InputMismatchException {
+        Cars participants = receptionDesk.applyCars(scanner);
+        RaceSetting raceSetting = new RaceSetting();
+        raceSetting.init();
+        raceSetting.setRound(scanner);
+        race = new Race(participants, raceSetting, new WinnerCondition(), raceStatusListener);
+    }
+
+    public void startRace() {
+        race.startRace();
+    }
+
+
+    private Race.RaceStatusListener raceStatusListener = new Race.RaceStatusListener() {
+
+        @Override
+        public void finishRound(Cars participants) {
+            scoreBoard.printCurrentRacingStatus(participants);
+        }
+
+        @Override
+        public void finishGame(Cars winner) {
+            scoreBoard.printWinner(winner);
+        }
+    };
+}
