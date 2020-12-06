92432adf75fc3dc073274b99e550bc6a71b3795c
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-06 16:54:59 +0900

    feat: 이름 길이 조건에 만 불만족 시 에러문구 출력 기능 구현

---
+++ src/main/java/racingcar/exception/NameLengthOutOfBoundsException.java
@@ -0,0 +1,9 @@
+package racingcar.exception;
+
+public class NameLengthOutOfBoundsException extends RuntimeException {
+    private static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
+
+    public NameLengthOutOfBoundsException() {
+        super(NAME_LENGTH_ERROR_MESSAGE);
+    }
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,9 +1,12 @@
 package racingcar.view;
 
+import racingcar.exception.NameLengthOutOfBoundsException;
+
 import java.util.Scanner;
 
 public class InputView {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REINPUT_MESSAGE = "다시 입력하십시오.";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -12,7 +15,12 @@ public class InputView {
 
     public String[] scanCarName() {
         printNameInputMessage();
-        return splitInputToName(scanUserInput());
+        String[] nameList = splitInputToName(scanUserInput());
+        while (!isValidNameList(nameList)) {
+            printReInputMessage();
+            nameList = splitInputToName(scanUserInput());
+        }
+        return nameList;
     }
 
     private void printNameInputMessage() {
@@ -28,10 +36,20 @@ public class InputView {
         return names;
     }
 
+    private boolean isValidNameList(String[] nameList) {
+        try {
+            validationNameList(nameList);
+            return true;
+        } catch(NameLengthOutOfBoundsException errorMessage) {
+            System.out.println(errorMessage);
+            return false;
+        }
+    }
+
     private boolean validationNameList(String[] nameList) {
         for (int idx = 0; idx < nameList.length; idx++) {
             if (!validationNameLength(nameList[idx])) {
-                return false;
+                throw new NameLengthOutOfBoundsException();
             }
         }
         return true;
@@ -42,5 +60,9 @@ public class InputView {
             return true;
         }
         return false;
+    }
+
+    private void printReInputMessage() {
+        System.out.println(REINPUT_MESSAGE);
     }
 }
