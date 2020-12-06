b85749de7b8ab5a6efb8536815e325dc21420977
Naeun <knae11@naver.com>
2020-12-06 11:12:42 +0900

    refactor(PlayerResponse): 상수 및 메소드 분리
    PlayerResponse 클래스 내부에서 메소드 분리, 상수선언 등으로 리팩토링

--- src/main/java/racingcar/PlayerResponse.java
+++ src/main/java/racingcar/PlayerResponse.java
@@ -5,35 +5,21 @@ import utils.ValidatorUtils;
 
 public class PlayerResponse {
 
-
-    private Scanner scanner;
+    private static final String ASKING_MOVES_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String ASKING_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NAMES_DELIMITER = ",";
+    private final Scanner scanner;
 
     public PlayerResponse(Scanner scanner) {
         this.scanner = scanner;
-    }
-
-    public int getHowManyMoves() {
-        while (true) {
-            try {
-                String movesBeforeCheck = askMoves();
-                ValidatorUtils.checkIsNumber(movesBeforeCheck);
-                return Integer.parseInt(movesBeforeCheck);
-            } catch (IllegalArgumentException i) {
-                System.out.println(i.getMessage());
-            }
-        }
-    }
-
-    private String askMoves() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        return scanner.nextLine().trim();
     }
 
     public String[] getCarNames() {
         String[] carNames;
         while (true) {
             try {
-                carNames = askCarNames();
+                System.out.println(ASKING_NAMES_MESSAGE);
+                carNames = responseOfCarNames();
                 ValidatorUtils.checkUnderFiveLetters(carNames);
                 return carNames;
             } catch (IllegalArgumentException i) {
@@ -42,13 +28,33 @@ public class PlayerResponse {
         }
     }
 
+    public int getHowManyMoves() {
+        while (true) {
+            try {
+                System.out.println(ASKING_MOVES_MESSAGE);
+                String movesBeforeCheck = responseOfMoves();
+                ValidatorUtils.checkIsNumber(movesBeforeCheck);
+                return Integer.parseInt(movesBeforeCheck);
+            } catch (IllegalArgumentException i) {
+                System.out.println(i.getMessage());
+            }
+        }
+    }
 
-    private String[] askCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNames = scanner.nextLine().split(",");
+    private String[] responseOfCarNames() {
+        String[] carNames = scanner.nextLine().split(NAMES_DELIMITER);
+        return deleteWhiteSpace(carNames);
+    }
+
+    private String[] deleteWhiteSpace(String[] carNames) {
         for (int i = 0; i < carNames.length; i++) {
             carNames[i] = carNames[i].trim();
         }
         return carNames;
     }
+
+    private String responseOfMoves() {
+        return scanner.nextLine().trim();
+    }
+
 }
