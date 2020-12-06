617ec0f3982f0572a58c29b5534747d6dc858819
최문경 <munkyung0324@gmail.com>
2020-12-07 03:25:23 +0900

    feat(Application, Game): get game count input, create Game class
    Application:
    - add createCars function
    - add get game count input
    - create game object
    - fix validCarNameList function param
    Game:
    - add Game class
    README:
    - fix typo in README.md

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,26 +1,41 @@
 package racingcar;
 
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    static String[] cars;
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        String[] cars;
+        List<Car> carList;
+
         do {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String carsInput = scanner.next();
             cars = carsInput.split(",");
-            System.out.println(Arrays.toString(cars));
-        } while (!validCarNameList());
+        } while (!validCarNameList(cars));
+
+        Game game = new Game();
+        carList = createCars(cars);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int cntInput = scanner.nextInt();
+        System.out.println();
 
     }
 
-    private static boolean validCarNameList() {
+    private static List<Car> createCars(String[] cars) {
+        List<Car> carList = new ArrayList<>();
+        for (String car: cars) {
+            carList.add(new Car(car));
+        }
+        return carList;
+    }
+
+    private static boolean validCarNameList(String[] cars) {
         CarValidator carValidator = new CarValidator();
-        for (String carName : cars) {
+        for (String carName: cars) {
             if(!carValidator.isValid(carName)) {
                 System.out.println("자동차 이름은 5자 이하로 입력하세요.");
                 return false;
