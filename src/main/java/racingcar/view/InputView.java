b6b4e5919067b95902cf662a2d80f7a6a1c672cf
tributetothemoon <kkm97351@gmail.com>
2020-12-07 05:03:05 +0900

    feat(InputView): 자동차 이름을 입력받는 기능 구현
    - 자동차 이름을 입력받기 위한 메소드 List<String> getCarNames()를 구현했습니다.
    - 입력에 포함된 모든 공백문자는 제거됩니다.
    - 빈 문자열, 공백만으로 이루어진 문자열 등은 공백 제거 후 InputView에서 예외로 처리되어 재입력을 요구합니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,43 @@
 package racingcar.view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
     private static Scanner scanner;
 
-    public static void setScanner(Scanner scanner){
+    public static void setScanner(Scanner scanner) {
         InputView.scanner = scanner;
+    }
+
+    public static List<String> getCarNames() {
+        System.out.println(REQUEST_CAR_NAMES_MESSAGE);
+        String rawString = deleteWhiteSpaces(scanner.nextLine());
+        try {
+            isEmptyStringOrThrowException(rawString);
+            return parseString(rawString);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getCarNames();
+        }
+    }
+
+    private static String deleteWhiteSpaces(String string) {
+        return string.replaceAll("\\s+", "");
+    }
+
+    private static List<String> parseString(String rawString) {
+        return new ArrayList<>(Arrays.asList(rawString.split(",")));
+    }
+
+    private static boolean isEmptyStringOrThrowException(String string) {
+        if (string.equals("")) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+        return true;
     }
 }
