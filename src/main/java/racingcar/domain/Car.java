a1ec2ba2859de607464c337c8727fbcd8c7e98e3
tributetothemoon <kkm97351@gmail.com>
2020-12-07 04:34:07 +0900

    feat(Car): Car객체 생성 로직 구현
    - Car객체는 정적 팩토리 메소드 Car from(String name)에 의해 생성됩니다.
    - 기본 생성자의 접근 제어자는 private로 변경되어, 잘못된 객체 생성을 방지합니다.
    - equals와 hashCode를 Override하는 코드가 작성되었습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,42 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
     private int position = 0;
 
-    public Car(String name) {
+    private Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    public static Car from(String name) {
+        isValidLengthOrThrowException(name);
+        return new Car(name);
+    }
+
+    public static boolean isValidLengthOrThrowException(String name) {
+        if (MAX_NAME_LENGTH < name.length()) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 " + MAX_NAME_LENGTH + "자를 넘어가서는 안 됩니다.");
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Car paramObj = (Car) obj;
+        return this.name.equals(paramObj.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode();
+    }
 }
