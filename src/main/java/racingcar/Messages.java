6b622ebd635ac636a79195e8020e89e978d466a4
김주원 <kjw11077@naver.com>
2020-12-06 10:57:15 +0900

    feat(Messages): add const MESSAGE_GAME_RESULT

--- src/main/java/racingcar/Messages.java
+++ src/main/java/racingcar/Messages.java
@@ -5,4 +5,5 @@ public class Messages {
     public static final String MESSAGE_ERROR = "[ERROR] ";
     public static final String TRY_COUNT_IS_NUMBER_ONLY = "시도 횟수는 숫자여야 한다.";
     public static final String FINAL_WINNER_IS = "최종 우승자: ";
+    public static final String MESSAGE_GAME_RESULT = "실행 결과";
 }
