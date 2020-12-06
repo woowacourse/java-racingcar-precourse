8e4e9b3ddd438f6f67d94ca579080fe3eae381ad
Ji-Ha <xodhkd36@gmail.com>
2020-12-06 16:17:08 +0900

    feat(inputRepetitions.java): 반복횟수에 숫자만 입력이 가능하게 예외처리

---
+++ src/main/java/controlexception/UserInputNameException.java
@@ -0,0 +1,15 @@
+package controlexception;
+
+public class UserInputNameException extends RuntimeException{
+	//입력에 관련된 예외를 처리하는 클래스.
+	static final private String LENGTHLIMIT = "5";
+	static private String userInputNumber;
+	public UserInputNameException(int userInputNumber){
+		this.userInputNumber = Integer.toString(userInputNumber);
+	}
+
+	@Override
+	public String getMessage() {
+		return "입력된 데이터의 길이가 "+ userInputNumber + "입니다. 다시 입력해주세요." + "(제한된 길이 : " + LENGTHLIMIT +")" ;
+	}
+}
--- src/main/java/inpututils/InputRepetitions.java
+++ src/main/java/inpututils/InputRepetitions.java
@@ -1,19 +1,29 @@
 package inpututils;
 
+import controlexception.UserInputNameException;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputRepetitions extends Input {
+	private boolean CHECKEDERROR = false;
+
 	public InputRepetitions(Scanner scanner) {
 		super(scanner);
 	}
 
-	@Override
-	Scanner getScanner() {
-		return super.scanner;
-	}
-
 	public int inputAnswer() {
-		System.out.println("시도할 회수는 몇회인가요?");
-		return getScanner().nextInt();
+		int repeat = 0;
+		do {
+			System.out.println("입력 횟수?");
+			try {
+				repeat =  scanner.nextInt();
+				CHECKEDERROR = true;
+			}catch(InputMismatchException i ){
+				System.out.println("숫자만 입력이 가능합니다. 숫자로 다시 입력해주세요!");
+				scanner = new Scanner(System.in);
+			}
+		}while(CHECKEDERROR == false);
+		return repeat;
 	}
 }
