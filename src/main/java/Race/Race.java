917bb70ac5539c5c05b5ee75cdda33b459d2e4e2
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-07 00:48:34 +0900

    [수정] 경주 클래스가 경기회차를 생성자에서 입력받도록 수정

--- src/main/java/Race/Race.java
+++ src/main/java/Race/Race.java
@@ -8,17 +8,23 @@ public class Race {
 
     private List<Car> Cars;
     private int carNum = 0;
+    private int roundNum = 0;
 
     Race(List<String> carNames) {
         createCars(carNames);
     }
 
-    void createCars(List<String> carNames) {
+    void createCars(List<String> carNames, int roundNum) {
         this.carNum = carNames.toArray().length;
+        this.roundNum = roundNum;
 
         for(int index = 0; index < this.carNum; index += 1) {
             this.Cars.add(new Car(carNames.get(index)));
         }
+    }
+
+    public void roundProcess(int roundNum) {
+
     }
 
 
