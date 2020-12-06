67390fd194a86759a29ac77eba17c79e6544af61
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-07 01:32:38 +0900

    style(exception): 공백 추가

--- src/main/java/racingcar/exception/DelimiterInputException.java
+++ src/main/java/racingcar/exception/DelimiterInputException.java
@@ -1,6 +1,6 @@
 package racingcar.exception;
 
-public class DelimiterInputException extends  RacingCarException{
+public class DelimiterInputException extends RacingCarException {
     private static final String DEFAULT_MESSAGE = "[ERROR] 구분 기호는 (,)이며, 적어도 1개 이상 입력해야 한다.";
 
     public DelimiterInputException() {
--- src/main/java/racingcar/exception/InputWithoutNameException.java
+++ src/main/java/racingcar/exception/InputWithoutNameException.java
@@ -1,6 +1,6 @@
 package racingcar.exception;
 
-public class InputWithoutNameException extends RacingCarException{
+public class InputWithoutNameException extends RacingCarException {
     private static final String DEFAULT_MESSAGE = "[ERROR] 자동차의 이름 값을 입력해야 한다.";
 
     public InputWithoutNameException() {
