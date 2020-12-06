d28681ba59df1996b2bfa7c90b23b75240490daa
byungwook <bbwwpark@naver.com>
2020-12-06 18:30:26 +0900

    refactor: 자동차 이름 입력값 예외처리 로직을 메서드로 분할 - parseCarNameInput 메서드에 포함되어있던 예외처리 로직을 메서드로 분리함 - 쉼표(,) 구분자가 입력에 포함되지 않았을 경우 예외처리 추가 - 자동차 이름에 특수 문자가 포함되는 경우 예외처리 추가 - 자동차 이름이 중복되는 경우 예외처리 추가 - 메서드명 가독성 좋게 수정

--- src/main/java/racingcar/io/InputView.java
+++ src/main/java/racingcar/io/InputView.java
@@ -1,9 +1,12 @@
 package racingcar.io;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Pattern;
 import racingcar.domain.Car;
 
 public class InputView {
@@ -12,6 +15,12 @@ public class InputView {
     private static final String RACING_CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String RACING_CAR_NAME_SEPARATOR = ",";
     private static final String RACING_MOVE_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String SEPARATE_ERROR_MESSAGE = "[ERROR] 자동차 이름은 쉼표(,)를 기준으로 구분되어야합니다.";
+    private static final String CAR_NAME_LENGTH_MESSAGE = "[ERROR] 자동차 이름은 5자 이하여야합니다.";
+    private static final String NOT_NUMBER_INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
+    private static final String UNDER_ZERO_INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 0보다 커야합니다.";
+    private static final String SPECIAL_CHARACTER_ERROR_MESSAGE = "[ERROR] 자동차 이름에 특수 문자는 허용되지 않습니다.";
+    private static final String DUPLICATED_CAR_NAME_ERROR_MESSAGE = "[ERROR] 중복된 자동차 이름이 존재합니다.";
     public static Scanner scanner;
 
     public static List<Car> getCars() {
@@ -31,28 +40,65 @@ public class InputView {
 
     private static List<String> parseCarNameInput(String input) {
         input = input.replaceAll(" ", "");
+        validateContainSeparator(input);
+        validateContainSpecialCharacters(input);
 
-        List<String> delimitedInputs = Arrays.asList(input.split(RACING_CAR_NAME_SEPARATOR, 0));
-        List<String> paredInputs = new LinkedList<>();
+        List<String> carNames = Arrays.asList(input.split(RACING_CAR_NAME_SEPARATOR, 0));
+        validateSeparateInput(carNames);
+        validateContainDuplicatedNames(carNames);
 
-        for (String delimitedInput : delimitedInputs) {
-            if (delimitedInput.length() == 0) {
-                throw new IllegalArgumentException("[ERROR] 자동차 이름은 쉼표(,)를 기준으로 구분되어야합니다.");
-            } else if (delimitedInput.length() > MAX_NAME_LENGTH) {
-                throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하여야합니다.");
-            }
-            paredInputs.add(delimitedInput);
+        List<String> paredCarNames = new LinkedList<>();
+        for (String carName : carNames) {
+            validateCarNameLength(carName);
+            paredCarNames.add(carName);
         }
-        return paredInputs;
+        return paredCarNames;
+    }
+
+    public static void validateContainSeparator(String input) {
+        if (!input.contains(RACING_CAR_NAME_SEPARATOR)) {
+            throw new IllegalArgumentException(SEPARATE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateContainSpecialCharacters(String name) {
+        if (!Pattern.matches("[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힝 | ,]*", name)) {
+            throw new IllegalArgumentException(SPECIAL_CHARACTER_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateSeparateInput(List<String> inputs) {
+        for (String input : inputs) {
+            if (input.length() == 0) {
+                throw new IllegalArgumentException(SEPARATE_ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public static void validateContainDuplicatedNames(List<String> names) {
+        Set<String> set = new HashSet<>();
+
+        for (String name : names) {
+            if (set.contains(name)) {
+                throw new IllegalArgumentException(DUPLICATED_CAR_NAME_ERROR_MESSAGE);
+            }
+            set.add(name);
+        }
+    }
+
+    public static void validateCarNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_MESSAGE);
+        }
     }
 
     public static int getRaceCount() {
         System.out.println(RACING_MOVE_COUNT);
         try {
             String input = scanner.nextLine();
-            validateIsNumber(input);
+            validateNumber(input);
             int raceCount = Integer.parseInt(input);
-            validateMoreThanZero(raceCount);
+            validateOverZero(raceCount);
             return raceCount;
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -60,20 +106,20 @@ public class InputView {
         }
     }
 
-    private static void validateIsNumber(String input) {
+    private static void validateNumber(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == '-' || input.charAt(i) == '+') {
                 continue;
             }
             if (!Character.isDigit(input.charAt(i))) {
-                throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+                throw new IllegalArgumentException(NOT_NUMBER_INPUT_ERROR_MESSAGE);
             }
         }
     }
 
-    private static void validateMoreThanZero(int number) {
+    private static void validateOverZero(int number) {
         if (number <= 0) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 0보다 커야합니다.");
+            throw new IllegalArgumentException(UNDER_ZERO_INPUT_ERROR_MESSAGE);
         }
     }
 
