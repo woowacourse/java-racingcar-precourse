c68dfc9d6d36438106b5cb7026614fe0f6796196
xrabcde <joara124@ewhain.net>
2020-12-06 21:41:27 +0900

    feat(Application): 경주할 자동차 이름들을 입력받는 inputCarNames 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,27 @@
 package racingcar;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         displayCarNameInputMessage();
+        inputCarNames(scanner);
         scanner.close();
     }
 
     private static void displayCarNameInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    private static void inputCarNames(Scanner kbd) {
+        String input = kbd.nextLine();
+        StringTokenizer tokens = new StringTokenizer(input, ", ");
+        int countTokens = tokens.countTokens();
+        String[] names = new String[countTokens];
+        for (int i = 0; i < countTokens; i++) {
+            names[i] = tokens.nextToken();
+        }
     }
 }
