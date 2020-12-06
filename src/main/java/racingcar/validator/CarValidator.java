be3a366dd94bf97d9e69480d35d280f6e28ee8c3
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-07 00:50:01 +0900

    feat(car-validator): 구분 기호가 없는 경우 에러 로직 추가

--- src/main/java/racingcar/validator/CarValidator.java
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -1,16 +1,19 @@
 package racingcar.validator;
 
+import racingcar.exception.DelimiterInputException;
 import racingcar.exception.RacingCarException;
 import racingcar.exception.SpaceInputException;
 
 public class CarValidator {
     private static final int ZERO = 0;
+    public static final String DELIMITER = ",";
 
     private CarValidator() {
     }
 
     static public void validate(String racingCarNames) {
         validateSpaceInput(racingCarNames);
+        validateDelimiterInput(racingCarNames);
     }
 
     static private void validateSpaceInput(String racingCarNames) {
@@ -19,4 +22,9 @@ public class CarValidator {
         }
     }
 
+    static private void validateDelimiterInput(String racingCarNames) {
+        if (!racingCarNames.contains(DELIMITER)) {
+            throw new DelimiterInputException();
+        }
+    }
 }
