8699c64235cf9abcd8689275b8f24107dd73ced7
yunjung <cairofaith@gmail.com>
2020-12-07 01:41:20 +0900

    feat(Application): 전체적인 게임 flow 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,39 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        RacingCars racingCars;
+        int trialsTimes;
+
+        try {
+            racingCars = readCarNames(scanner);
+            trialsTimes = readTrialNum(scanner);
+            takeRaceRounds(racingCars, trialsTimes);
+            GameStatus.printWinner(racingCars);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static RacingCars readCarNames(Scanner scanner) {
+        final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+        System.out.println(INPUT_CAR_NAME);
+        return new RacingCars(InputManager.readCarList(scanner.nextLine()));
+    }
+
+    private static int readTrialNum(Scanner scanner) {
+        final String INPUT_TRIAL_NUM = "시도할 회수는 몇회인가요?";
+
+        System.out.println(INPUT_TRIAL_NUM);
+        return InputManager.readTrialNum(scanner.nextLine());
+    }
+
+    private static void takeRaceRounds(RacingCars racingCars, int trialTimes) {
+        System.out.println("\n실행 결과");
+        for (int round = 0; round < trialTimes; round++) {
+            racingCars.takeOneRound();
+            GameStatus.printStatus(racingCars);
+        }
     }
 }
