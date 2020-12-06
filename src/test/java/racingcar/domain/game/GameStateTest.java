65a1feacf6743136655de5314764bf404729fce9
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-06 18:50:52 +0900

    test: GameState 경주 시도 횟수 차감 기능 테스트 추가

--- src/test/java/racingcar/domain/game/GameStateTest.java
+++ src/test/java/racingcar/domain/game/GameStateTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.exception.CannotRaceException;
 import racingcar.domain.exception.RacingTryCountsNumberFormatException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class GameStateTest {
@@ -33,5 +35,26 @@ class GameStateTest {
         GameState gameState = GameState.initiate(3);
 
         assertThat(gameState.isEnd()).isFalse();
+    }
+
+    @DisplayName("경주 시도 횟수 차감을 요청하면, 경주 시도 횟수가 1만큼 줄어든다")
+    @Test
+    public void decreaseRacingTryCounts_경주_시도_횟수가_1만큼_줄어든다() {
+        GameState gameState = GameState.initiate(1);
+
+        gameState.decreaseRacingTryCounts();
+
+        assertThat(gameState.isEnd()).isTrue();
+    }
+
+    @DisplayName("잔여 시도 횟수가 없을 때, 경주 시도 횟수 차감을 요청하면 예외 발생")
+    @Test
+    public void decreaseRacingTryCounts_예외_발생() {
+        GameState gameState = GameState.initiate(1);
+        gameState.decreaseRacingTryCounts();
+
+        assertThatCode(() -> {
+            gameState.decreaseRacingTryCounts();
+        }).isInstanceOf(CannotRaceException.class);
     }
 }
