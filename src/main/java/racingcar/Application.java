220b259c49b7c0b274cde257b44b082e18e5c70d
xrabcde <joara124@ewhain.net>
2020-12-06 21:24:19 +0900

    feat(Application): 자동차이름 입력받는 문구 출력해주는 displayCarNameInputMessage 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        displayCarNameInputMessage();
+        scanner.close();
+    }
+
+    private static void displayCarNameInputMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 }
