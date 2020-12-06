dea47e74cda250ae10e9d26d7fa7a5cc02aa3c6d
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-07 02:57:50 +0900

    refactor(cars): 생성자를 통해 객체를 초기화 하도록 변경

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -2,20 +2,16 @@ package racingcar;
 
 import racingcar.validator.CarValidator;
 
-import javax.xml.validation.Validator;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
-    public Cars() {
-    }
-
-    public void createCars(String racingCarNames) {
+    public Cars(String racingCarNames) {
         CarValidator.validate(racingCarNames);
-        cars = Arrays.stream(racingCarNames.split(CarValidator.DELIMITER))
+        this.cars = Arrays.stream(racingCarNames.split(CarValidator.DELIMITER))
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
