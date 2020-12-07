9243c7c67e124764f32d427f648f97d1b25e4601
minsugn Son <sns2831@gmail.com>
2020-12-08 08:32:56 +0900

    feat(racing car): 시도 횟수 입력 로직 구현
    isValid() 함수를 통해 Validators 에게 검증 요청 메세지를 전송하도록 함
    
    get 의 반환형은 Integer 이지만 scanner 는 String 으로 인풋을 받는다.
    단순히 Integer 로 변환하면 여러 예외사항이 발생할 수 있으나 isValid 에서
    입력값에 대한 검증이 이루어 졌기에 Integer.parseInt() 로 변환하여 출력

--- src/main/java/racingcar/domains/input/AttemptNumberInput.java
+++ src/main/java/racingcar/domains/input/AttemptNumberInput.java
@@ -1,8 +1,29 @@
 package racingcar.domains.input;
 
+import racingcar.domains.validate.Validator;
+import racingcar.domains.validate.Validators;
+
+import java.util.Scanner;
+
 public class AttemptNumberInput implements Input<Integer> {
+
+    Scanner scanner;
+    String attemptNumber;
+
+    public AttemptNumberInput(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     @Override
     public Integer get() {
-        return null;
+        attemptNumber = scanner.nextLine();
+        isValid();
+
+        return Integer.parseInt(attemptNumber);
+    }
+
+    public void isValid() {
+        Validator validator = Validators.getAttemptNumberInputValidator();
+        validator.isValid(attemptNumber);
     }
 }
