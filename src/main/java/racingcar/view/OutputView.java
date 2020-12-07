ae446e05385e6f83abdd5e9296607bb98b0bc80a
seunghyun <ggyool@naver.com>
2020-12-08 02:46:09 +0900

    feat: 최종 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,10 +3,14 @@ package racingcar.view;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
     private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_ATTEMPT_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자";
     private static final char HYPHEN = '-';
     private static final char COLON = ':';
     private static final char SPACE = ' ';
@@ -56,5 +60,19 @@ public class OutputView {
 
     private static void lineFeed() {
         System.out.println();
+    }
+
+
+    public static void printFinalWinners(Cars winners) {
+        List<String> carNames = winners.getCars()
+                .stream()
+                .map(car -> car.getName())
+                .collect(Collectors.toList());
+
+        String connectedCarNames = String.join(", ", carNames);
+        System.out.print(FINAL_WINNER_MESSAGE);
+        System.out.print(COLON);
+        System.out.print(SPACE);
+        System.out.println(connectedCarNames);
     }
 }
