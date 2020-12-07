64f5abc723d557c305e45b077d306977b64d6a9e
Junyoung Choi <cjy1705@naver.com>
2020-12-07 09:15:06 +0900

    feat : 게임 진행할 Racing 클래스 생성

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racing {
+
+    List<Car> cars = new ArrayList<>();
+
+    public void init(){
+
+    }
+
+}
