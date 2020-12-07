9b976543d822ebfb7f3c1987a783cf094048d4e9
operasystem6 <operasystem6@naver.com>
2020-12-07 14:44:12 +0900

    refactor: 숫자 판별 메소드 적용

--- src/main/java/racingcar/domain/validator/InputValidator.java
+++ src/main/java/racingcar/domain/validator/InputValidator.java
@@ -16,8 +16,9 @@ import java.util.ArrayList;
 public class InputValidator {
     private static final String OUT_OF_RANGE = "[ERROR] 자동차 이름의 길이는 5이하 여야 합니다.";
     private static final String DUPLICATE = "[ERROR] 중복된 자동차 이름이 있습니다.";
-    private static final String NUMBER_OF_ATTEMPTS = "[ERROR] 시도 횟수는 숫자여야 합니다.";
-    private static final String EXACT_FORMAT = "[ERROR] 정확한 형식을 입력하세요.";
+    private static final String NUMBER_OF_ATTEMPTS = "[ERROR] 시도 횟수는 숫자 여야 합니다.";
+    private static final String NOT_NEGATIVE = "[ERROR] 시도 횟수는 0보다 커야 합니다.";
+    private static final String EXACT_FORMAT = "[ERROR] 정확한 형식을 입력 하세요.";
 
     public static ArrayList<String> filterCarName(final ArrayList<String> carNames) {
         if (NameValidator.isOutOfRange(carNames)) {
@@ -33,8 +34,9 @@ public class InputValidator {
     }
 
     public static int filterNumberOfAttempts(final String input) {
-        if (CountValidator.isNumber(input)) {
-            throw new IllegalArgumentException(NUMBER_OF_ATTEMPTS);
+        CountValidator.checkNumeric(input, NUMBER_OF_ATTEMPTS);
+        if (!CountValidator.isCountZero(input)) {
+            throw new IllegalArgumentException(NOT_NEGATIVE);
         }
         return Integer.parseInt(input);
     }
