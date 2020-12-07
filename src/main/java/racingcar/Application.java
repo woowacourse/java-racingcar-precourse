94f876811b84261235709014ac68d880252037c8
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-12-08 01:52:22 +0900

    feat: 프로그램 동작 관론 콘솔 문구 추가
    프로그램 동작 안내 콘솔 문구 및 기능 추가
    에러시 종료하도록 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,23 @@
 package racingcar;
 
+
 import java.util.Scanner;
+import utils.UserInputData;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String[] carNames = UserInputData.carNameChoice(scanner);
+
+            System.out.println("시도할 회수는 몇회인가요?");
+            int matchTryNumber = UserInputData.matchCountChoice(scanner);
+
+            System.out.println("실행 결과");
+            GameController newGame = new GameController(carNames);
+            newGame.playGame(matchTryNumber);
+        } catch (IllegalArgumentException e) { return;}
     }
 }
