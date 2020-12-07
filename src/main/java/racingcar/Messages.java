4cd9794fedb59e934f64794c7ab68da5d3483e2b
김주원 <kjw11077@naver.com>
2020-12-07 17:15:31 +0900

    feat(Messages): 출력 메시지 추가

--- src/main/java/racingcar/Messages.java
+++ src/main/java/racingcar/Messages.java
@@ -5,4 +5,8 @@ public class Messages {
     public static final String FINAL_WINNER_IS = "최종 우승자: ";
     public static final String MESSAGE_GAME_RESULT = "실행 결과";
     public static final String HOW_MANY_TIME_TO_TRY = "시도할 회수는 몇회인가요?";
+    public static final String PLEASE_INPUT_NAMES_OF_CAR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String NAMES_CAN_NOT_DUPLICATE = "이름끼리는 중복될 수 없음.";
+    public static final String NAME_SHOULD_BE_LONGER_THAN_ONE = "이름의 길이는 1 이상이어야 함.";
+    public static final String NAME_SHOULD_BE_SHORTER_THAN_FIVE = "이름의 길이는 5를 넘지 못함.";
 }
