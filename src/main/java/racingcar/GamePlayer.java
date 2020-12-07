3bc8685f878284c102b63c3d51958e364f69ed45
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-07 20:15:00 +0900

    refactor: 상수 및 print 문 추가

--- src/main/java/racingcar/GamePlayer.java
+++ src/main/java/racingcar/GamePlayer.java
@@ -3,13 +3,17 @@ package racingcar;
 import java.util.Scanner;
 
 public class GamePlayer {
-    public static void playGame (Scanner scanner) {
+    final static String raceResult = "실행 결과";
+
+    public static void playGame(Scanner scanner) {
         String[] carsList = UserInput.makeCarsList(scanner);
         Car[] carInstances = CarMaker.makeCarInstances(carsList);
         int playTime = UserInput.makePlayTime(scanner);
+        System.out.println();
+        System.out.println(raceResult);
         for (int i = 0; i < playTime; i++) {
             Racing.playRace(carInstances);
         }
-        WinnerSelection.selectWinner(carInstances); 
+        WinnerSelection.selectWinner(carInstances);
     }
 }
