aab8375b105774026977c10c98e32c7809481a5e
minsugn Son <sns2831@gmail.com>
2020-12-08 08:41:15 +0900

    feat(racing car): 자동차 이름 입력 로직 구현
    CarNamesInput.java 구현
    List 로 결과값을 반환한다.

--- src/main/java/racingcar/domains/input/CarNamesInput.java
+++ src/main/java/racingcar/domains/input/CarNamesInput.java
@@ -1,10 +1,37 @@
 package racingcar.domains.input;
 
+import racingcar.domains.validate.Validator;
+import racingcar.domains.validate.Validators;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class CarNamesInput implements Input<List<String>> {
+
+    private static final String DELIMITER = ",";
+
+    private Scanner scanner;
+    private String carNames;
+
+    public CarNamesInput(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     @Override
     public List<String> get() {
-        return null;
+        carNames = scanner.nextLine();
+        isValid();
+        return toList();
+    }
+
+    private void isValid() {
+        Validator validator = Validators.getCarNameInputValidator();
+        validator.isValid(carNames);
+    }
+
+    private List<String> toList() {
+        String[] splitCarNames = carNames.split(DELIMITER);
+        return Arrays.asList(splitCarNames);
     }
 }
