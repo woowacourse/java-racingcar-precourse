fc61c2b3404854eba0ae8ef636843d97b58ff576
minsugn Son <sns2831@gmail.com>
2020-12-08 08:24:55 +0900

    feat(racing car): 검증 클래스 반환 메소드 추가
    Validators.java 의 검증 클래스 반환 메소드 추가.

--- src/main/java/racingcar/domains/validate/Validators.java
+++ src/main/java/racingcar/domains/validate/Validators.java
@@ -1,4 +1,13 @@
 package racingcar.domains.validate;
 
 public class Validators {
+
+    public static Validator getAttemptNumberInputValidator() {
+        return new AttemptNumberInputValidator();
+    }
+
+    public static Validator getCarNameInputValidator() {
+        return new CarNamesInputValidator();
+    }
+
 }
