b663022f6663635d66e816f954aa5c6141ae3428
Noh Hyeon Jin <hjforj@naver.com>
2020-12-07 18:09:08 +0900

    feat: 게임 진행 구현

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,49 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+    private Input input;
+    private Output output;
+    
+    private ArrayList<Car> cars = new ArrayList<Car>();
+    private int numberOfTry;
+    
+    public Game(Scanner scanner) {
+        input = new Input(scanner);
+    }
+    
+    public void run() {
+        inputCarName();
+        inputNumberOfTry();
+        printRoundOfGameResult();
+        printFinalWinner();
+    }
+    
+    public void inputCarName() {
+        String[] carNames = input.getCarNames();
+        input.checkCarNameLength(carNames);
+        for (int i = 0; i < carNames.length; i++) {
+            cars.add(new Car(carNames[i]));
+        }
+    }
+    public void inputNumberOfTry() {
+        numberOfTry = input.getNumberOfTry();
+        System.out.println();
+    }
+    
+    public void printRoundOfGameResult() {
+        System.out.println("실행결과");
+        for (int i = 0; i < numberOfTry; i++) {
+            for (Car car : cars) {
+            car.goOrStop();
+            }
+            output = new Output(cars);
+            output.printResult();
+        }
+    }
+    public void printFinalWinner() {
+        output.printFinalWinner();
+    }
+}
