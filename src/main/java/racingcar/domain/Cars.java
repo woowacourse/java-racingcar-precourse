5ccc99934bdbec6cdc9d3c90645e2d5a9c7f4e09
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-08 02:35:05 +0900

    style(cars): 코딩 컨벤션 적용

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +7,13 @@ public class Cars {
     private final List<Car> cars;
 
     public Cars(List<String> carNames) {
-        this.cars = carNames.stream().map(Car::new).collect(Collectors.toList());
+        this.cars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
     public void racing() {
-        for (Car car: cars) {
+        for (Car car : cars) {
             car.startTurn();
             car.printResult();
         }
@@ -20,7 +21,11 @@ public class Cars {
     }
 
     public List<Car> getFastestCars() {
-        Car fastestCar = cars.stream().max(Car::compareTo).orElseThrow(IllegalArgumentException::new);
-        return cars.stream().filter(car -> car.isSamePosition(fastestCar)).collect(Collectors.toList());
+        Car fastestCar = cars.stream()
+                .max(Car::compareTo)
+                .orElseThrow(IllegalArgumentException::new);
+        return cars.stream()
+                .filter(car -> car.isSamePosition(fastestCar))
+                .collect(Collectors.toList());
     }
 }
