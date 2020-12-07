e8bb65ae08a5e84e990d82771f28924307eecf38
TaewanKimmmm <ktww88@gmail.com>
2020-12-08 05:39:02 +0900

    feat: 경주할 자동차들 객체 생성 기능 추가

--- src/main/java/racingcar/CarRacingGame.java
+++ src/main/java/racingcar/CarRacingGame.java
@@ -1,19 +1,35 @@
 package racingcar;
 
+import utils.CompareUtils;
+import utils.SplitUtils;
 import view.InputView;
+import view.OutputView;
 
 import java.util.Scanner;
 
 public class CarRacingGame {
     private final Scanner scanner;
+    private static final int MOVE_FORWARD = 4;
 
     public CarRacingGame(Scanner scanner) {
         this.scanner = scanner;
     }
 
     public void start() {
-        String carName = InputView.getCarName(scanner);
-        System.out.println(carName);
+        String carNames = InputView.getCarName(scanner);
         int tryNumber = InputView.getTryNumber(scanner);
+        Car[] racingCars = makeRacingCars(carNames);
+    }
+
+    private Car[] makeRacingCars(String carNames) {
+        String[] racingCarNames = SplitUtils.splitCarName(carNames);
+
+        int numberOfRacingCar = racingCarNames.length;
+        Car[] racingCars = new Car[numberOfRacingCar];
+
+        for (int i = 0; i < numberOfRacingCar; i++) {
+            racingCars[i] = new Car(racingCarNames[i]);
+        }
+        return racingCars;
     }
 }
