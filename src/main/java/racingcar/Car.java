2f06cde48b2afd1a8e0c5dddb194d1d3e1bb0b83
taelee <taelee@student.42seoul.kr>
2020-12-08 00:51:19 +0900

    refactor(Car): change all primitive values to constants

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,6 +3,12 @@ package racingcar;
 import utils.RandomUtils;
 
 public class Car {
+    public static final int INT_RANDOM_LOWER_BOUND = 0;
+    public static final int INT_RANDOM_UPPER_BOUND = 9;
+    public static final int INT_MOVE_CRITERIA = 4;
+    public static final String DELIMITER_BETWEEN_NAME_AND_PROGRESS_MARK = " : ";
+    public static final String PROGRESS_MARK = "-";
+    public static final String NEXT_LINE = "\n";
     private final String name;
     private int position = 0;
 
@@ -12,18 +18,18 @@ public class Car {
 
     // 추가 기능 구현
     public void move() {
-        int random = RandomUtils.nextInt(0, 9);
-        if (random >= 4) {
+        int random = RandomUtils.nextInt(INT_RANDOM_LOWER_BOUND, INT_RANDOM_UPPER_BOUND);
+        if (random >= INT_MOVE_CRITERIA) {
             position++;
         }
     }
 
     public void printPosition() {
-        System.out.print(name + " : ");
+        System.out.print(name + DELIMITER_BETWEEN_NAME_AND_PROGRESS_MARK);
         for (int i = 0; i < position; i++) {
-            System.out.print("-");
+            System.out.print(PROGRESS_MARK);
         }
-        System.out.print("\n");
+        System.out.print(NEXT_LINE);
     }
 
     public String getName () {
