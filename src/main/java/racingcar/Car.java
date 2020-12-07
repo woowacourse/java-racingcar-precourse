5564c2097bf1f815a01a60c80fcf2d8239967f29
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-07 22:18:20 +0900

    refactor(car): 위치 리턴 기능 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,13 +2,16 @@ package racingcar;
 
 import utils.RandomUtils;
 
-public class Car {
-    private final String name;
-    private int position = 0;
+import java.util.Collections;
 
+public class Car {
     private final int START_INCLUSIVE = 1;
     private final int END_INCLUSIVE = 9;
     private final int FORWARD_CRITERION = 4;
+    private final String BAR = "-";
+
+    private final String name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
@@ -16,20 +19,24 @@ public class Car {
 
     // 추가 기능 구현
     public void startTurn() {
-        if (RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE) >= FORWARD_CRITERION) {
+        if (isGoForward()) {
             goForward();
         }
+    }
+
+    private boolean isGoForward() {
+        return RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE) >= FORWARD_CRITERION;
     }
 
     private void goForward() {
         this.position++;
     }
 
-    public String getName() {
-        return name;
+    public void printResult() {
+        System.out.printf("%s : %s\n", this.name, convertPositionToBar());
     }
 
-    public int getPosition() {
-        return position;
+    private String convertPositionToBar() {
+        return String.join("", Collections.nCopies(position, BAR));
     }
 }
