3ce8e82f4f05859bbb138d5b8363dadbec783a70
TaewanKimmmm <ktww88@gmail.com>
2020-12-08 01:46:24 +0900

    feat: 0과 9 사이 랜덤한 수 생성 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,20 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
+    private static final int MIN = 0;
+    private static final int MAX = 9;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    private int makeRandomNumber() {
+        return RandomUtils.nextInt(MIN, MAX);
     }
 
     // 추가 기능 구현
