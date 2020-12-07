a30769827559f267e82dc1e7abe13e9329077d27
김주원 <kjw11077@naver.com>
2020-12-07 17:23:27 +0900

    refactor(Race): inline, rename methods

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -10,35 +10,28 @@ import utils.RandomUtils;
 
 public class Race {
 
-    public static final int RANDOM_START = 0;
-    public static final int RANDOM_END = 9;
+    public static final int RANDOM_MIN = 0;
+    public static final int RANDOM_MAX = 9;
     public static final int MOVE = 4;
 
     public static GameResult start(List<Car> cars, int tryCount) {
         print(MESSAGE_GAME_RESULT);
         for (int i = 0; i < tryCount; i++) {
-            playSingleGame(cars);
+            cars.forEach(Race::moveCar);
+            printEmptyLine();
         }
-
         return getResult(cars);
     }
 
-    private static void playSingleGame(List<Car> cars) {
-        for (Car car : cars) {
-            driveOrStopCar(car);
-        }
-        printEmptyLine();
-    }
-
-    private static void driveOrStopCar(Car car) {
-        if (isDrive()) {
+    private static void moveCar(Car car) {
+        if (isMove()) {
             car.moveForward();
         }
         car.printStatus();
     }
 
-    private static boolean isDrive() {
+    private static boolean isMove() {
         // 4 이상일 경우 전진하고, 3 이하의 값이면 멈춘다
-        return MOVE <= RandomUtils.nextInt(RANDOM_START, RANDOM_END);
+        return MOVE <= RandomUtils.nextInt(RANDOM_MIN, RANDOM_MAX);
     }
 }
