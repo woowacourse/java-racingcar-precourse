05365fc0ac5f97c700f2a29802ea57570cb61ad2
TaewanKimmmm <ktww88@gmail.com>
2020-12-08 05:45:28 +0900

    style: 안 쓰는 라이브러리 import 제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,8 +2,6 @@ package view;
 
 import racingcar.Car;
 
-import java.util.List;
-
 public class OutputView {
     private static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
