b7e131b9dc5e2b04908138d112362a09011c193b
Hyeon9mak <jinha3507@gmail.com>
2020-12-07 12:54:19 +0900

    feat(Cars): add Cars (Car class list)

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,2 +1,47 @@
-package racingcar;public class Cars {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Cars {
+    private static final String COMMA = ",";
+
+    private final ArrayList<Car> cars;
+
+    private Cars(String carNames) {
+        validateEmpty(carNames);
+        validateDuplicate(carNames);
+        this.cars = enrollCars(carNames);
+    }
+
+    public static Cars makeCars(String carNames) {
+        return new Cars(carNames);
+    }
+
+
+    private void validateEmpty(String carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름을 입력해주세요.");
+        }
+    }
+
+    private void validateDuplicate(String carNames) {
+        ArrayList<String> listCarNames = new ArrayList<>(Arrays.asList(carNames.split(COMMA)));
+        Set<String> setCarNames = new HashSet<>(Arrays.asList(carNames.split(COMMA)));
+        if (listCarNames.size() != setCarNames.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 이름은 안됩니다.");
+        }
+    }
+
+    private ArrayList<Car> enrollCars(String carNames) {
+        ArrayList<Car> cars = new ArrayList<>();
+        for (String carName : carNames.split(COMMA)) {
+            cars.add(Car.makeRandomForwardCar(carName));
+        }
+        return cars;
+    }
+
+
 }
--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,2 +1,16 @@
-package racingcar;public class CarsTest {
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+
+    @Test
+    public void testCreate() {
+        Cars cars = Cars.makeCars("pobi,jun,joy");
+        
+        assertThat(cars).isNotNull();
+    }
+
 }
