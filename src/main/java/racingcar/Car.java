bb91656857db779f76e1e9a1f6fcf02ba33c1fdb
xrabcde <joara124@ewhain.net>
2020-12-07 18:26:52 +0900

    feat(Car): Car 객체에 필요한 메소드(getName, getPosition, moveForward, displayPosition) 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,8 +5,25 @@ public class Car {
     private int position = 0;
 
     public Car(String name) {
-        this.name = name;
+    this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void moveForward() {
+        this.position++;
+    }
+
+    public void displayPosition() {
+        System.out.print(this.name + " : ");
+        for (int i = 0; i < this.position; i++)
+            System.out.print("-");
+        System.out.println();
+    }
 }
