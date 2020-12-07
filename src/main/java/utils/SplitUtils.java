c714b61b43f26f230e956b8daf8de4b0fc8eccf9
TaewanKimmmm <ktww88@gmail.com>
2020-12-08 01:14:47 +0900

    style: 콤마 String 상수로 선언

--- src/main/java/utils/SplitUtils.java
+++ src/main/java/utils/SplitUtils.java
@@ -1,14 +1,15 @@
 package utils;
 
 public class SplitUtils {
+    private static final String COMMA = ",";
     private SplitUtils() {
     }
 
     public static String[] splitCarName(String carNames) {
-        if (!carNames.contains(",")) {
+        if (!carNames.contains(COMMA)) {
             String[] carName = {carNames};
             return carName;
         }
-        return carNames.split(",");
+        return carNames.split(COMMA);
     }
 }
