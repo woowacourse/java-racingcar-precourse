95d6f7d60f36ef41cef1c1e39aa569eb8b9909a0
TaewanKimmmm <ktww88@gmail.com>
2020-12-08 05:44:15 +0900

    feat: 각각의 라운드 결과 출력하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,9 @@
 package view;
 
+import racingcar.Car;
+
+import java.util.List;
+
 public class OutputView {
     private static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
@@ -8,8 +12,20 @@ public class OutputView {
     private static final String CAR_NAME_DUPLICATION_ERROR_MESSAGE = "[ERROR] 경주할 자동차의 이름이 중복됩니다.";
     private static final String TRY_NUMBER_TYPE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
     private static final String TRY_NUMBER_NEGATIVE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 양수여야 합니다.";
+    private static final String GAME_RESULT = "실행 결과";
 
     private OutputView() {
+    }
+
+    public static void showOneRoundResult(Car racingCar) {
+        String racingCarName = racingCar.getName();
+        int racingCarPosition = racingCar.getPosition();
+
+        System.out.print(racingCarName + " : ");
+        for (int i = 0 ; i < racingCarPosition; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     public static void showCarNameMessage() {
@@ -22,6 +38,10 @@ public class OutputView {
 
     public static void showTryNumberTypeErrorMessage() {
         System.out.println(TRY_NUMBER_TYPE_ERROR_MESSAGE);
+    }
+
+    public static void showAllRoundResultMessage() {
+        System.out.println(GAME_RESULT);
     }
 
     public static String getNegativeErrorMessage() {
