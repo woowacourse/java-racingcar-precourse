b2ae957ffa19ebf22698933b53eb4a77aa205fce
operasystem6 <operasystem6@naver.com>
2020-12-07 14:41:36 +0900

    style: 띄어쓰기 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,9 @@
 /*
- * InputView.java
+ * CountValidator.java
  *
- * version 1.0
+ * version 1.1
  *
- * 2020/12/04
+ * 2020/12/07
  *
  * Copyright (c) 2020 Junhyoung Park
  */
@@ -13,17 +13,17 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 횟수는 몇회인가요?";
+    private static final String START_MESSAGE = "경주할 자동차 이름을 입력 하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 횟수는 몇회 인가요?";
 
     public static String[] inputCarName(Scanner scanner) {
         System.out.println(START_MESSAGE);
         return dataSplit(scanner.nextLine());
     }
 
-    public static int inputNumberOfAttempts(Scanner scanner) {
+    public static String inputNumberOfAttempts(Scanner scanner) {
         System.out.println(NUMBER_OF_ATTEMPTS_MESSAGE);
-        return scanner.nextInt();
+        return scanner.next();
     }
 
     private static String[] dataSplit(String data) {
