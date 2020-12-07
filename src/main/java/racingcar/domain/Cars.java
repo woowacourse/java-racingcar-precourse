02c1b961041eb25c2794690569b3c307138f8042
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-08 02:33:43 +0900

    refactor(cars): 기본 생성자 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,7 +1,5 @@
 package racingcar.domain;
 
-import racingcar.validator.CarValidator;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,11 +7,8 @@ import java.util.stream.Collectors;
 public class Cars {
     private final List<Car> cars;
 
-    public Cars(String racingCarNames) {
-        CarValidator.validate(racingCarNames);
-        this.cars = Arrays.stream(racingCarNames.split(CarValidator.DELIMITER))
-                .map(Car::new)
-                .collect(Collectors.toList());
+    public Cars(List<String> carNames) {
+        this.cars = carNames.stream().map(Car::new).collect(Collectors.toList());
     }
 
     public void racing() {
