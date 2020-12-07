8ecda57e5036272d0bcb5322e00608017218cc3b
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-07 14:49:59 +0900

    style: 접근제한자 수정

--- src/main/java/utils/InputManager.java
+++ src/main/java/utils/InputManager.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputManager {
-    public static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String RACE_ROUND_INPUT = "시도할 회수는 몇회인가요?";
     private static final String COMMA = ",";
 
