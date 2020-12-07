3030429fced9aba6fddce65afebf02f5b9406581
baekchan1024 <dlqhs1024@naver.com>
2020-12-08 02:56:32 +0900

    feat (Game) : winner 찾아서 출력하는 로직 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,8 +1,38 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Game {
-	OutputView outputView = new OutputView();
+	InputView inputView = new InputView();
+	Cars cars = new Cars();
+
 	public void run() {
-		outputView.Racing();
+		Racing();
+	}
+
+	private void Racing() {
+		String[] carsNames = inputView.inputCarNames();
+		cars.arrayOfObjects(carsNames);
+	}
+
+	public void findWinner(ArrayList<Car> cars) {
+		int position = 0;
+
+		for (Car car : cars) {
+			if (car.getPosition() > position) {
+				position = car.getPosition();
+			}
+		}
+		printWinner(position, cars);
+	}
+
+	private void printWinner(int maxPosition, ArrayList<Car> cars) {
+		StringBuffer winner = new StringBuffer();
+		for (Car car : cars) {
+			if (car.getPosition() == maxPosition) {
+				winner.append(car.getName()+",");
+			}
+		}
+		Message.finalWinner(winner);
 	}
 }
