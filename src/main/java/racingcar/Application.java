63c0196594f962bb4dfd2ed0c8926f38db6d323f
xrabcde <joara124@ewhain.net>
2020-12-07 17:36:28 +0900

    fix(Application): 입력안내 문구를 출력해주는 displayCarNameInputMessage를 케이스별로 문구를 출력해도록 displayInputMessage로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,34 +12,41 @@ public class Application {
     private static final int SAME_NAME_ERROR = 3;
     private static final int MIN_CAR_LENGTH = 2;
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int CAR_NAME_CASE = 0;
+    private static final int GAME_COUNT_CASE = 1;
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        gameSetting(scanner);
-        System.out.println("makeCars success");
+        Car[] cars = gameSetting(scanner);
+        displayInputMessage(GAME_COUNT_CASE);
         scanner.close();
     }
 
-    private static void gameSetting(Scanner kbd) {
-        displayCarNameInputMessage();
+    private static Car[] gameSetting(Scanner kbd) {
+        displayInputMessage(CAR_NAME_CASE);
         String[] names = inputCarNames(kbd);
         boolean check = checkCarNames(names);
         while (!check) {
-            displayCarNameInputMessage();
+            displayInputMessage(CAR_NAME_CASE);
             names = inputCarNames(kbd);
             check = checkCarNames(names);
         }
-        makeCars(names);
+        Car[] cars = makeCars(names);
+        return cars;
     }
 
-    private static void makeCars(String[] names) {
+    private static Car[] makeCars(String[] names) {
         Car[] cars = new Car[names.length];
         for (int i = 0; i < names.length; i++) {
             cars[i] = new Car(names[i]);
         }
+        return cars;
     }
 
-    private static void displayCarNameInputMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    private static void displayInputMessage(int inputCase) {
+        if (inputCase == CAR_NAME_CASE)
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        if (inputCase == GAME_COUNT_CASE)
+            System.out.println("시도할 회수는 몇회인가요?");
     }
 
     private static String[] inputCarNames(Scanner kbd) {
