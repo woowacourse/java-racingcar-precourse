5f2d3467cc70518b118a8c4859c66e64d78b4822
DH Lim <dlarkfl2@gamil.com>
2020-12-07 12:13:02 +0900

    refactor(OutputView): printFinalWinner() 파라미터 변경, finalWinner 멤버변수 삭제

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,41 +1,31 @@
 package racingcar;
 
-import java.util.List;
-
 public class OutputView {
     private static final String RACING_START_MESSAGE = "실행 결과";
     private static final String RACING_FINISH_MESSAGE = "최종 우승자";
     private static final String MOVE_FORWARD = "-";
     private static final String COLON_AFTER_CAR_NAME = " : ";
 
-    private String finalWinner;
-    private final Racing racing;
-
-    public OutputView(Racing racing) {
-        this.racing = racing;
-    }
-
     public void printRacingStartMessage() {
         System.out.println(RACING_START_MESSAGE);
     }
 
-    public void printRoundResult() {
-        for (Car car : racing.getCarList()) {
-            System.out.println(car.getName() + COLON_AFTER_CAR_NAME + printMoveDistance(car.getPosition()));
+    public void printRoundResult(CarGroup carGroup) {
+        for (Car car : carGroup.getCarGroup()) {
+            System.out.println(car.getName() + COLON_AFTER_CAR_NAME + printMoveDistance(car));
         }
         System.out.println();
     }
 
-    private String printMoveDistance(int position) {
+    private String printMoveDistance(Car car) {
         String moveDistance = "";
-        for (int i = 0; i < position; i++) {
+        for (int i = 0; i < car.getPosition(); i++) {
             moveDistance = moveDistance.concat(MOVE_FORWARD);
         }
         return moveDistance;
     }
 
-    public void printFinalWinner(List<String> finalwinnerNameList) {
-        finalWinner = String.join(", ", finalwinnerNameList);
+    public void printFinalWinner(String finalWinner) {
         System.out.println(RACING_FINISH_MESSAGE + COLON_AFTER_CAR_NAME + finalWinner);
     }
 }
