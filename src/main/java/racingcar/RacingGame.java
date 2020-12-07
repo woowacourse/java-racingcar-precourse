fd5d48f0cbab9f946696f307f6b22680fe66596b
Gomding <backchida@gmail.com>
2020-12-07 18:27:35 +0900

    refactor : RacingGame 클래스 리팩토링
    기능 단위로 메서드 분리
    getCarNames() : 사용자에게 차의 이름을 입력받는 메서드
    makeCarList() : 입력받은 차의 이름들로 List를 만드는 메서드
    getNumberOfTimes() : 사용자에게 몇회 시도할지 입력받는 메서드
    printResultRacing() : 실행 결과를 출력하는 메서드
    printBestCarNames() : 최종 우승자를 출력하는 메서드

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,32 +7,51 @@ import java.util.Scanner;
 public class RacingGame {
     private String[] carNames;
     private String numberOfTimes;
+    private Scanner scanner;
+
+    private final List<Car> carList = new ArrayList<>();
 
     public void playGame(Scanner scanner) {
+        this.scanner = scanner;
+        getCarNames();
+        getNumberOfTimes();
+        printResultRacing();
+        printBestCarNames();
+    }
 
+    private void getCarNames() {
         while (true) {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            carNames = scanner.nextLine().split(",");
-
-            if (InputError.checkInputCarNames(carNames)) {
+            this.carNames = this.scanner.nextLine().split(",");
+            if (InputError.checkInputCarNames(this.carNames)) {
                 break;
             }
         }
+        makeCarList();
+    }
 
-        List<Car> carList = new ArrayList<>();
-        for (String carName : carNames) {
-            carList.add(new Car(carName));
+    private void makeCarList() {
+        for (String carName : this.carNames) {
+            this.carList.add(new Car(carName));
         }
+    }
 
+    private void sortCarList() {
+        this.carList.sort((o1, o2) -> o2.getPosition() - o1.getPosition());
+    }
+
+    private void getNumberOfTimes() {
         while (true) {
             System.out.println("시도할 회수는 몇회인가요?");
-            numberOfTimes = scanner.nextLine();
-            if (InputError.checkInputNumber(numberOfTimes)) {
+            this.numberOfTimes = this.scanner.nextLine();
+            if (InputError.checkInputNumber(this.numberOfTimes)) {
                 break;
             }
         }
         System.out.println();
+    }
 
+    private void printResultRacing() {
         for (int round = 0; round < Integer.parseInt(numberOfTimes); round++) {
             System.out.println("실행 결과");
             for (Car car : carList) {
@@ -41,19 +60,17 @@ public class RacingGame {
             }
             System.out.println();
         }
-        carList = sortCarList(carList);
+    }
+
+    private void printBestCarNames() {
+        sortCarList();
         StringBuilder sb = new StringBuilder("최종 우승자: ");
-        int bestPosition = carList.get(0).getPosition();
-        for (Car car : carList) {
+        int bestPosition = this.carList.get(0).getPosition();
+        for (Car car : this.carList) {
             if (car.getPosition() == bestPosition) {
-                sb.append(car.getName() + ", ");
+                sb.append(car.getName()).append(", ");
             }
         }
         System.out.println(sb.substring(0, sb.length() - 2));
-    }
-
-    private List<Car> sortCarList(List<Car> carList) {
-        carList.sort((o1, o2) -> o2.getPosition() - o1.getPosition());
-        return carList;
     }
 }
