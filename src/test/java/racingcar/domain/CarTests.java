8ddda031653515a3f60512fcababcf152c336eef
seunghyun <ggyool@naver.com>
2020-12-07 22:23:57 +0900

    test: Car 객체 생성에 대한 테스트 코드 추가

--- src/test/java/racingcar/domain/CarTests.java
+++ src/test/java/racingcar/domain/CarTests.java
@@ -2,11 +2,25 @@ package racingcar.domain;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CarTests {
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "woni", "jun", "abcde", "1 3 5"})
+    public void 객체생성_성공(String carName) {
+        assertThatCode(() -> new Car(carName))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"longname", ""})
+    public void 객체생성_실패_예외발생(String carName) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car(carName));
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 10, 100})
