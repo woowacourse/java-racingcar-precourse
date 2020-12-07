14718427a94bce16673de12effa983de759d5c02
tributetothemoon <kkm97351@gmail.com>
2020-12-07 19:18:14 +0900

    feat(Turn): 진행 회차(시도 회수)를 관리하는 Turn 클래스 생성
    - Turn 클래스는 시도할 회수를 정수로 받아 Turn from(int) 메소드로부터
      생성됩니다.
    - turnOver() 함수로 한번의 회차가 끝났음을 외부에서 알려주고, isEnd()메소드를 통해 게임이 종료되었는지를 판단해 외부에 알려줍니다.

---
+++ src/main/java/racingcar/domain/Turn.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+public class Turn {
+    private final int endTurn;
+    private int currentTurn;
+
+    private Turn(int endTurn) {
+        this.endTurn = endTurn;
+        this.currentTurn = 0;
+    }
+
+    public static Turn from(int endTurn) {
+        isValidValueOrThrowException(endTurn);
+        return new Turn(endTurn);
+    }
+
+    public static boolean isValidValueOrThrowException(int endTurn) {
+        if (endTurn <= 0) {
+            throw new IllegalArgumentException("회수는 양의 정수만 가능합니다.");
+        }
+        return true;
+    }
+
+    public void turnOver() {
+        this.currentTurn++;
+    }
+
+    public boolean isEnd() {
+        return this.endTurn <= this.currentTurn;
+    }
+}
