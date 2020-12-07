c5be78d09cbc8deccf3e6208cd2dddce0679a269
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-07 18:25:24 +0900

    [구현] 시도할 경기 횟수를 입력받는inputView 클래스의 멤버 함수인  inputRoundNum 함수 구현

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -9,10 +9,10 @@ public class InputView {
     private final int VALID_MINIMUM_CAR_NAMES_LENGTH = 2;
     private final String ERROR_TAG = "[Error]";
     private final String INVAlID_INPUT_MESSAGE = "You should input at least one car name";
-
+    private final String ASKING_ROUND_NUM = "시도할 횟수는 몇회인가요?";
     private final Scanner scanner;
 
-    InputView(Scanner scanner) {
+   public InputView(Scanner scanner) {
         this.scanner = scanner;
     }
 
@@ -20,13 +20,18 @@ public class InputView {
         String carNames = this.scanner.nextLine();
 
         if(!isValidInput(carNames)) {
-            Exception e = new Exception(ERROR_TAG + INVAlID_INPUT_LENGTH);
+            Exception e = new Exception(ERROR_TAG + INVAlID_INPUT_MESSAGE);
             System.out.println(e.toString());
         }
 
         return this.ConvertToList(carNames);
     }
 
+    public int inputRoundNum() {
+       System.out.println(this.ASKING_ROUND_NUM);
+       int roundNum = this.scanner.nextInt();
+       return roundNum;
+    }
     private List<String> ConvertToList(String target) {
         return Arrays.asList(target.split(","));
     }
@@ -37,5 +42,7 @@ public class InputView {
         }
         return true;
     }
+
+
 
 }
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,14 @@
 package racingcar;
+
 import RaceReferee.RaceReferee;
+import View.InputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+
     }
 }
