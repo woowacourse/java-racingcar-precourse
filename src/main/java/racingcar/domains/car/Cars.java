3330a5aeee67808d3b2a49cbf958aebee93c0f27
minsugn Son <sns2831@gmail.com>
2020-12-08 07:08:15 +0900

    feat(racing car): Car 도메인 관련 클래스 추가
    Cars.java : 자동차들의 그룹을 의미하는 일급 컬랙션.

---
+++ src/main/java/racingcar/domains/car/Cars.java
@@ -0,0 +1,26 @@
+package racingcar.domains.car;
+
+import racingcar.domains.output.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public Output getGameResult() {
+        return null;
+    }
+
+    public Output getGameStatus() {
+        return null;
+    }
+
+    public void move() {
+
+    }
+}
