ffeed48654bccc6f7dd9fa2c905acb819ab6ae3f
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-07 20:16:37 +0900

    refactor(Input) : Input 클래스와 Output 클래스에서의 의존성 제거

--- src/main/java/ui/Input.java
+++ src/main/java/ui/Input.java
@@ -6,12 +6,12 @@ import racingcar.ErrorMessage;
 public class Input {
 
     public static String receiveRacingCarNames(Scanner scanner) {
-        Output.printRequestRacingCarNames();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return scanner.nextLine();
     }
 
     public static String receiveAttemptsCount(Scanner scanner) {
-        Output.printRequestAttemptsCount();
+        System.out.println("시도할 회수는 몇회인가요?");
         return scanner.nextLine();
     }
 
--- src/main/java/ui/Output.java
+++ src/main/java/ui/Output.java
@@ -3,19 +3,9 @@ package ui;
 import java.util.List;
 
 public class Output {
-    private static final String REQUEST_RACING_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String REQUEST_ATTEMPTS_COUNT = "시도할 회수는 몇회인가요?";
     private static final String RESULT = "실행 결과";
     private static final String WINNER = "최종 우승자";
     private static final String ONE_STEP = "-";
-
-    public static void printRequestRacingCarNames() {
-        System.out.println(REQUEST_RACING_CAR_NAMES);
-    }
-
-    public static void printRequestAttemptsCount() {
-        System.out.println(REQUEST_ATTEMPTS_COUNT);
-    }
 
     public static void willPrintResult() {
         System.out.println(RESULT);
