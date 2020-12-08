3184bea8058cbfd23293d83806daba75e47bdf37
seunghyun <ggyool@naver.com>
2020-12-08 16:01:46 +0900

    refactor: InputView는 입력 기능만 수행하도록 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,25 +1,16 @@
 package racingcar.view;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class InputView {
-    private static final String COMMA = ",";
-
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public List<String> inputCarNames() {
-        String carNames = scanner.nextLine();
-
-        return Arrays.stream(carNames.split(COMMA))
-                .map(s -> s.trim())
-                .collect(Collectors.toList());
+    public String inputCarNames() {
+        return scanner.nextLine();
     }
 
     public String inputAttemptNumber() {
