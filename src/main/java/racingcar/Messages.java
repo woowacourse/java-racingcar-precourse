3cd5996433b09d05b46d4a6053646aabb8a254cf
김주원 <kjw11077@naver.com>
2020-12-08 19:37:34 +0900

    feat(Messages): add constant

--- src/main/java/racingcar/Messages.java
+++ src/main/java/racingcar/Messages.java
@@ -7,6 +7,7 @@ public class Messages {
     public static final String HOW_MANY_TIME_TO_TRY = "시도할 회수는 몇회인가요?";
     public static final String PLEASE_INPUT_NAMES_OF_CAR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String NAMES_CAN_NOT_DUPLICATE = "이름끼리는 중복될 수 없음.";
+    public static final String TRY_COUNT_IS_NATURAL_NUMBER_ONLY = "시도 횟수는 1이상의 자연수만 입력 가능합니다.";
     public static final String NAME_SHOULD_BE_LONGER_THAN_ONE = "이름의 길이는 1 이상이어야 함.";
     public static final String NAME_SHOULD_BE_SHORTER_THAN_FIVE = "이름의 길이는 5를 넘지 못함.";
 }
