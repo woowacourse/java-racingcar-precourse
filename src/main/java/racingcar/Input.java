a13efdc828bc3404a6102924b6bf2bf268f5749a
Noh Hyeon Jin <hjforj@naver.com>
2020-12-08 13:17:58 +0900

    feat: 공백인 자동차 이름이 입력으로 들어올 경우 예외 발생하도록 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 public class Input {
 	private static final String SPLIT_DELIMITER = ",";
+    private static final String BLANK_SPACE = "";
 	
     private static final String GET_CAR_NAMES_MESSAGE = "경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String GET_NUMBER_OF_TRY_MESSAGE = "시도할 회수는 몇회인가요?";
     
     private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
     private static final String CAR_NAME_DUPLICATE_ERROR_MESSAGE = "[ERROR] 자동차 이름이 중복됩니다.";
+    private static final String CAR_NAME_BLANK_ERROR_MESSAGE = "[ERROR] 자동차 이름이 공백입니다.";
     private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
     
     private final Scanner scanner;
@@ -24,7 +26,7 @@ public class Input {
     
     public String[] getCarNames() {
         System.out.println(GET_CAR_NAMES_MESSAGE);
-        String carNames = scanner.next();
+        String carNames = scanner.nextLine();
         this.carNames = carNames.split(SPLIT_DELIMITER);
         return this.carNames;
     }
@@ -52,6 +54,14 @@ public class Input {
             throw new IllegalArgumentException(CAR_NAME_DUPLICATE_ERROR_MESSAGE);
         }
     }
+    public void checkCarNameBlankSpace(String[] carNames) {
+        for (int i = 0; i < carNames.length; i++) {
+            if (carNames[i].equals(BLANK_SPACE)) {
+                throw new IllegalArgumentException(CAR_NAME_BLANK_ERROR_MESSAGE);
+            }
+        }
+    }
+    
     public void checkIsDigit(String numberOfTry) {
         for (int i = 0; i < numberOfTry.length(); i++) {
             if (!Character.isDigit(numberOfTry.charAt(i))) {
