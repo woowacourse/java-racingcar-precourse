99687ea3bd256c6d8a6480ebaffb290998b5fea1
amy choi <cgc8016@ewhain.net>
2020-12-08 21:34:01 +0900

    feat(Car): add ride method

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,9 +1,11 @@
 package racingcar;
 
+import static utils.RandomUtils.nextInt;
+
 public class Car {
 
     private final String name;
-    private final int position = 0;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
@@ -11,5 +13,18 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void ride() {
+        final int THRESHOLD = 4;
+
+        int randomNumber = nextInt(0, 9);
+        if (randomNumber >= THRESHOLD) {
+            position++;
+        }
     }
 }
