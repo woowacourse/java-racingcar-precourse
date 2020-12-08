97f332605e235ac112959283652164ef37819a27
minsugn Son <sns2831@gmail.com>
2020-12-08 11:40:11 +0900

    feat(racing car): 게임 상태 출력 기능 구현
    GameStatus.java
    게임 진행 시 전체 자동차의 상태를 출력하는 로직 구현

--- src/main/java/racingcar/domains/output/GameStatus.java
+++ src/main/java/racingcar/domains/output/GameStatus.java
@@ -1,8 +1,22 @@
 package racingcar.domains.output;
 
+import racingcar.domains.Game.Game;
+import racingcar.domains.car.Car;
+
+import java.util.List;
+
 public class GameStatus implements Output {
+    private List<Car> cars;
+
+    public GameStatus(List<Car> cars) {
+        this.cars = cars;
+    }
+
     @Override
     public void print() {
-
+        cars.forEach(car -> {
+            car.getStatus().print();
+            System.out.println();
+        });
     }
 }
