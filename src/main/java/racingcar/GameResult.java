dec088978c3ec2477fa9aa0826b0021814d35258
김주원 <kjw11077@naver.com>
2020-12-08 22:41:57 +0900

    docs(TODO): TODO 삭제

--- src/main/java/racingcar/GameResult.java
+++ src/main/java/racingcar/GameResult.java
@@ -6,7 +6,6 @@ import static utils.PrintUtils.print;
 import java.util.List;
 
 public class GameResult {
-    // todo 개선 필요: 하는일이 너무 없지 않나? 게임 로그를 얘가 가지게 할까?
 
     private final Winners winners;
 
--- src/main/java/racingcar/Winners.java
+++ src/main/java/racingcar/Winners.java
@@ -12,7 +12,6 @@ public class Winners {
     }
 
     public static Winners getWinners(List<Car> cars) {
-        // todo 개선 필요
         Car winner = cars.stream()
                 .max(Car::compareTo)
                 .get();
