cdb4a134d865d98b0ace4362866aa65a20c53ba9
taelee <taelee@student.42seoul.kr>
2020-12-08 11:02:27 +0900

    refactor(Validate): split [ERROR] to another constant

--- src/main/java/racingcar/Validate.java
+++ src/main/java/racingcar/Validate.java
@@ -5,19 +5,20 @@ import java.util.Arrays;
 
 public class Validate {
 
-    public static final String ERR_MSG_IS_EMPTY = "[Error] 글자를 입력해주세요.";
-    public static final String ERR_MSG_ENDS_WITH_COMMA = "[Error] 마지막이 쉼표로 끝납니다.";
-    public static final String ERR_MSG_LONGER_THAN_MAX_CHARACTER = "[Error] 차 이름은 5자 이하여야 합니다.";
-    public static final String ERR_MSG_HAS_EMPTY_NAME = "[Error] 비어있는 차 이름이 존재합니다.";
-    public static final String ERR_MSG_HAS_DUPLICATE = "[ERROR] 중복된 차 이름이 존재합니다.";
-    public static final String ERR_MSG_IS_NOT_INT = "[ERROR] 1 이상의 정수를 입력해주세요";
-    public static final String ERR_MSG_LESS_THAN_ONE = "[ERROR] 0보다 큰 정수를 입력해주세요";
-    public static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final String ERR_PREFIX = "[Error]";
+    private static final String ERR_MSG_IS_EMPTY = ERR_PREFIX + "글자를 입력해주세요.";
+    private static final String ERR_MSG_ENDS_WITH_COMMA = ERR_PREFIX + "마지막이 쉼표로 끝납니다.";
+    private static final String ERR_MSG_LONGER_THAN_MAX_CHARACTER = ERR_PREFIX + "차 이름은 5자 이하여야 합니다.";
+    private static final String ERR_MSG_HAS_EMPTY_NAME = ERR_PREFIX + "비어있는 차 이름이 존재합니다.";
+    private static final String ERR_MSG_HAS_DUPLICATE = ERR_PREFIX + "중복된 차 이름이 존재합니다.";
+    private static final String ERR_MSG_IS_NOT_INT = ERR_PREFIX + "1 이상의 정수를 입력해주세요";
+    private static final String ERR_MSG_LESS_THAN_ONE = ERR_PREFIX + "0보다 큰 정수를 입력해주세요";
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
-    public static ArrayList<String> validateCarName(String carNamesInput) throws IllegalArgumentException {
-        carInputStringHasException(carNamesInput);
-        ArrayList<String> tempCarList = new ArrayList<String>(Arrays.asList(carNamesInput.split(",")));
-        ArrayList<String> resultCarList = new ArrayList<String>();
+    public static ArrayList<String> validateCarName(String carString) throws IllegalArgumentException {
+        carStringHasError(carString);
+        ArrayList<String> tempCarList = new ArrayList<>(Arrays.asList(carString.split(",")));
+        ArrayList<String> resultCarList = new ArrayList<>();
 
         for (int i = 0; i < tempCarList.size(); i++) {
             String car = tempCarList.get(i);
@@ -27,7 +28,7 @@ public class Validate {
         return resultCarList;
     }
 
-    public static void carInputStringHasException(String carNamesInput) {
+    public static void carStringHasError(String carNamesInput) {
         isEmptyLine(carNamesInput);
         endsWithComma(carNamesInput);
     }
@@ -73,6 +74,7 @@ public class Validate {
         isBiggerThanZero(roundNumber);
         return roundNumber;
     }
+
     public static Integer stringToNumber(String numberString) {
         try {
             return Integer.parseInt(numberString);
@@ -80,6 +82,7 @@ public class Validate {
             throw new IllegalArgumentException(ERR_MSG_IS_NOT_INT);
         }
     }
+
     public static void isBiggerThanZero(Integer roundNumber) {
         if (roundNumber < 1) {
             throw new IllegalArgumentException(ERR_MSG_LESS_THAN_ONE);
