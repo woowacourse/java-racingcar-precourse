6b8dcf75231c948ab2cc3284424a1a518deae97a
yoon-chaejin <kevin3st@naver.com>
2020-12-08 20:39:54 +0900

    feat(car) : print the status of the car

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,6 +6,7 @@ public class Car {
     public static final int MINIMUM = 0;
     public static final int MAXIMUM = 9;
     public static final int CRITERION = 4;
+    public static final String UNIT_SYMBOL = "-";
 
     private final String name;
     private int position = 0;
@@ -17,10 +18,15 @@ public class Car {
     public void moveRandomly() {
         int randomNumber = RandomUtils.nextInt(MINIMUM, MAXIMUM);
 
-        if (randomNumber > CRITERION) {
+        if (randomNumber >= CRITERION) {
             this.position += 1;
         }
 
         return;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : " + UNIT_SYMBOL.repeat(this.position);
     }
 }
