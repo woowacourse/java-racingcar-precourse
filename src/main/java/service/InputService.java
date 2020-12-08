a511fbe86a581339a4f9ad8e60fd6a088003ed94
yoon-chaejin <kevin3st@naver.com>
2020-12-08 21:52:34 +0900

    feat(input) : get the number of game phases as input

--- src/main/java/service/InputService.java
+++ src/main/java/service/InputService.java
@@ -7,7 +7,6 @@ public class InputService {
 
     public static String[] getCarNames(Scanner scanner) {
         String[] carNames;
-
         while(true){
             try {
                 System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -31,5 +30,20 @@ public class InputService {
                 throw new IllegalArgumentException("자동차 이름은 공백이 아니어야 합니다.");
             }
         }
+    }
+
+    public static int getToTalPhaseNumber(Scanner scanner) {
+        int totalPhaseNumber;
+        while (true) {
+            try {
+                System.out.println("시도할 횟수는 몇회인가요?");
+                String input = scanner.nextLine();
+                totalPhaseNumber = Integer.parseInt(input);
+                break;
+            } catch (Exception e) {
+                System.out.println("[ERROR]시도 횟수는 숫자여야 합니다.");
+            }
+        }
+        return totalPhaseNumber;
     }
 }
