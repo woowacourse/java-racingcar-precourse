e9d0d76129a1997e67392a589b5e5471eab184bf
baekchan1024 <dlqhs1024@naver.com>
2020-12-08 15:15:02 +0900

    feat (ValidateUtils) : 사용자의 입력 예외처리 클래스 구현

---
+++ src/main/java/utils/ValidateUtils.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.util.HashSet;
+
+import racingcar.Message;
+
+public class ValidateUtils {
+	public static void checkCarNameLength(String[] carNameList) {
+		for (String name : carNameList) {
+			if (name.length() > 5) {
+				throw new IllegalArgumentException(Message.nameLengthExceeded);
+			}
+		}
+	}
+
+	public static void checkNameSpace(String[] carNameList) {
+		for (String name : carNameList) {
+			if (name.isEmpty()) {
+				throw new IllegalArgumentException(Message.nameSpacesExistence);
+			}
+		}
+	}
+
+	public static void checkDuplicate(String[] carNameList) {
+		HashSet<String> nameList = new HashSet<String>();
+
+		for (String name : carNameList) {
+			nameList.add(name);
+		}
+
+		if (nameList.size() != carNameList.length) {
+			throw new IllegalArgumentException(Message.nameDuplication);
+		}
+	}
+	
+	public static void checkZeroCount(int number) {
+		if (number == 0) {
+			throw new IllegalArgumentException(Message.inputCountZero);
+		}
+	}
+}
