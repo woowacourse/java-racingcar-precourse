a38d48dd84094c5b4840c42ababed3f122c19b2e
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-08 12:06:34 +0900

    [구현] Referee 클래스의 생성자 및 멤버 변수 선언

--- src/main/java/Race/Referee.java
+++ src/main/java/Race/Referee.java
@@ -1,5 +1,17 @@
 package Race;
 
+import View.OutputView;
+import racingcar.Car;
+import racingcar.Cars;
+
+import java.util.List;
+
 public class Referee {
+    private OutputView winnerBoard = new OutputView();
+
+    public Referee() {
+
+    }
+
 
 }
