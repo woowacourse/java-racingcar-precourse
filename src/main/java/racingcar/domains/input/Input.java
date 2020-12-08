1809013c165aa34fdcfa325df77d1116e08d6d6a
minsugn Son <sns2831@gmail.com>
2020-12-08 10:17:48 +0900

    refactor(racing car): 인풋 인터페이스 추상메소드 형식 변경
    불필요한 접근제어자 제거
    예외처리 추가

--- src/main/java/racingcar/domains/input/Input.java
+++ src/main/java/racingcar/domains/input/Input.java
@@ -1,5 +1,7 @@
 package racingcar.domains.input;
 
+import racingcar.domains.error.RacingCarGameException;
+
 public interface Input<T> {
-    public T get();
+    T get() throws RacingCarGameException;
 }
