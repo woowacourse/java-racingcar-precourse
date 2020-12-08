dc12c2fd2acc6ee1df8dac99f169b8de01c21302
minsugn Son <sns2831@gmail.com>
2020-12-08 11:33:29 +0900

    feat(racing car): 게임 진행 시 자동차 상태 출력 로직 구현
    CarStatus.java 구현

--- src/main/java/racingcar/domains/output/CarStatus.java
+++ src/main/java/racingcar/domains/output/CarStatus.java
@@ -1,8 +1,31 @@
 package racingcar.domains.output;
 
 public class CarStatus implements Output {
+    private static final String CAR_STATUS_PRINT_FORMAT = "%s : %s";
+    private static final String MOVE_POINTER = "-";
+
+    private String carName;
+    private int position;
+
+    public CarStatus(String carName, int position) {
+        this.carName = carName;
+        this.position = position;
+    }
+
     @Override
     public void print() {
+        System.out.println(
+                String.format(CAR_STATUS_PRINT_FORMAT, carName, getMovePointer())
+        );
+    }
 
+    private String getMovePointer() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < position; i++) {
+            stringBuilder.append(MOVE_POINTER);
+        }
+
+        return stringBuilder.toString();
     }
 }
