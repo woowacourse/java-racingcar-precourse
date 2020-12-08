6d99666a98ba81c8b662784c250242076c3a8982
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-08 14:39:55 +0900

    style: Application 메인 메서드 요구사항 주석 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        // TODO 구현 진행
         InputView inputView = new InputView(scanner);
         RacingGameController racingGameController = new RacingGameController(inputView);
         racingGameController.run();
