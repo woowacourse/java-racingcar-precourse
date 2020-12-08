c80fdec59f293e358edc5ffe21f333cf063f1c7a
minsugn Son <sns2831@gmail.com>
2020-12-08 11:04:44 +0900

    refactor(racing car): Inputs.java 반환시 Input 의 제네릭 타입 추가
    반환값 타입을 명시하지 않아 로직 중 버그 발생 가능성 농후

--- src/main/java/racingcar/domains/input/Inputs.java
+++ src/main/java/racingcar/domains/input/Inputs.java
@@ -1,13 +1,14 @@
 package racingcar.domains.input;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Inputs {
-    public static Input getAttemptNumberInput(Scanner scanner) {
+    public static Input<Integer> getAttemptNumberInput(Scanner scanner) {
         return new AttemptNumberInput(scanner);
     }
 
-    public static Input getCarNamesInput(Scanner scanner) {
+    public static Input<List<String>> getCarNamesInput(Scanner scanner) {
         return new CarNamesInput(scanner);
     }
 }
