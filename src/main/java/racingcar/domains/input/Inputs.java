f49ebac8c5619d0de2f90fcbc223c3251eb3cea1
minsugn Son <sns2831@gmail.com>
2020-12-08 10:58:44 +0900

    feat(racing car): Inputs.java 클래스 구현
    관련 Input 클래스를 리턴해 주는 로직 구현

--- src/main/java/racingcar/domains/input/Inputs.java
+++ src/main/java/racingcar/domains/input/Inputs.java
@@ -1,4 +1,13 @@
 package racingcar.domains.input;
 
+import java.util.Scanner;
+
 public class Inputs {
+    public static Input getAttemptNumberInput(Scanner scanner) {
+        return new AttemptNumberInput(scanner);
+    }
+
+    public static Input getCarNamesInput(Scanner scanner) {
+        return new CarNamesInput(scanner);
+    }
 }
