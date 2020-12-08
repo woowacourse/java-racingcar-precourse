71cd69ef1720bfa5ff2a776bfe607d4abfea678d
yoon-chaejin <kevin3st@naver.com>
2020-12-08 22:53:55 +0900

    feat(application) : run game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,27 @@
 package racingcar;
 
+import service.GameService;
+import service.InputService;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        InputService inputService = new InputService(scanner);
+        runGame(inputService);
+        scanner.close();
+    }
+
+    private static void runGame(InputService inputService) {
+        String[] carNames = inputService.getCarNames();
+        int totalPhaseNumber = inputService.getToTalPhaseNumber();
+        GameService gameService = new GameService(carNames);
+        System.out.println("\n실행 결과");
+        for (int i=0; i<totalPhaseNumber; i++) {
+            gameService.runPhase();
+        }
+        gameService.showResult();
     }
 }
