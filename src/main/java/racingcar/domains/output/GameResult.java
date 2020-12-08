bea125776521bcd814c26654772357625d216bb2
minsugn Son <sns2831@gmail.com>
2020-12-08 12:12:36 +0900

    feat(racing car): 최종 우승자 계산 및 게임 결과 출력 로직 구현

--- src/main/java/racingcar/domains/output/GameResult.java
+++ src/main/java/racingcar/domains/output/GameResult.java
@@ -1,8 +1,42 @@
 package racingcar.domains.output;
 
+import racingcar.domains.car.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameResult implements Output {
+    private static final String WINNER_PRINT_PREFIX = "최종 우승자: ";
+
+    private List<Car> cars;
+
+    public GameResult(List<Car> cars) {
+        this.cars = cars;
+    }
+
     @Override
     public void print() {
+        System.out.println(WINNER_PRINT_PREFIX
+                + String.join(", ", getWinnerName()));
+    }
 
+    private List<String> getWinnerName() {
+        return getWinner().stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private List<Car> getWinner() {
+        Car mostMovedCar = getMostMoved();
+
+        return cars.stream().
+                filter(car -> mostMovedCar.isSamePosition(car))
+                .collect(Collectors.toList());
+    }
+
+    private Car getMostMoved() {
+        return cars.stream()
+                .max(Car::compareTo)
+                .get();
     }
 }
