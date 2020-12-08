71f7cc5256f14d416ee81c832eff8543d0ffe745
100-yeeun <100yeeun@gmail.com>
2020-12-08 10:34:42 +0900

    feat: 입력 예외처리
    `racingCar/Game` 클래스에 아래 함수들 생성
    `enterName`: 이름 입력,
    `checkName`: 이름 유효성 확인,
    `enterRound`: 시도 횟수 입력 및 유효성 확인
    
    Breaks! 시도 횟수 변수명 `round` -> `tryCnt` 변경

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -3,16 +3,23 @@ package racingcar;
 import java.util.Scanner;
 
 public class Game {
+
+    static final int NAME_LENGTH_LIMIT = 5;
+    static final int UNSET_TRY_COUNT = -1;
+
     private static String[] carNames;
     private static Car[] cars;
-    private static int round;
-    public Game() {
+    private static int tryCnt = UNSET_TRY_COUNT;
+
+    private Game() {
     }
 
     public static void playGame(Scanner scanner) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = scanner.nextLine();
-        carNames = input.split(",") ;
+        while (carNames == null) {
+            enterName(scanner);
+        }
+
         cars = new Car[carNames.length];
 
         for (int i = 0; i < carNames.length; i++) {
@@ -20,6 +27,37 @@ public class Game {
         }
 
         System.out.println("시도할 회수는 몇회인가요?");
-        round = scanner.nextInt();
+        while (tryCnt < 0) {
+            enterRound(scanner);
+        }
+    }
+
+    private static void enterName(Scanner scanner) {
+        String input = scanner.nextLine();
+        carNames = input.split(",");
+        try {
+            checkName();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 자동차 이름은 쉼표(,) 기준으로 구분하며 이름은 5자 이하여야 한다.");
+            carNames = null;
+        }
+    }
+
+    private static void checkName() {
+        for (int i = 0; i < carNames.length; i++) {
+            if (carNames[i].length() > NAME_LENGTH_LIMIT) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static void enterRound(Scanner scanner) {
+        try {
+            String input = scanner.nextLine();
+            tryCnt = Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.");
+            tryCnt = UNSET_TRY_COUNT;
+        }
     }
 }
