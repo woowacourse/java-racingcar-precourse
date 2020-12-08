c235141df99846f74fcf0d800b4b5bfc9912f3ed
amy choi <cgc8016@ewhain.net>
2020-12-08 21:09:48 +0900

    feat(Car, Application): add makeCars methods, getName methods

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,5 +11,19 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         String[] carNames = getCarNames(scanner);
         int tryCount = getTryCount(scanner);
+        Car[] racingCars = makeCars(carNames);
+        // for debugging
+        for (Car car : racingCars) {
+            System.out.println(car.getName());
+        }
+    }
+
+    public static Car[] makeCars(String[] carNames) {
+        int carCount = carNames.length;
+        Car[] racingCars = new Car[carCount];
+        for (int i = 0; i < carCount; i++) {
+            racingCars[i] = new Car(carNames[i]);
+        }
+        return racingCars;
     }
 }
