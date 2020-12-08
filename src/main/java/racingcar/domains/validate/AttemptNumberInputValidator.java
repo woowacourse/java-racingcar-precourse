94fdaa8b6d53d5951b4e65be08c67aa5601c7f97
minsugn Son <sns2831@gmail.com>
2020-12-08 09:09:26 +0900

    feat(racing car): 시도 횟수 입력 검증 로직 구현
    입력이 숫자인지, 또한 입력이 0보다 큰지 확인하는 로직 추가.

--- src/main/java/racingcar/domains/validate/AttemptNumberInputValidator.java
+++ src/main/java/racingcar/domains/validate/AttemptNumberInputValidator.java
@@ -1,8 +1,35 @@
 package racingcar.domains.validate;
 
-public class AttemptNumberInputValidator implements Validator<String> {
-    @Override
-    public void isValid(String attemptNumber) {
+import racingcar.ErrorMessageGenerator;
+import racingcar.domains.error.RacingCarGameException;
 
+public class AttemptNumberInputValidator implements Validator<String> {
+    private static final String NOT_A_NUMBER_MESSAGE =
+            ErrorMessageGenerator.generateErrorMessage("입력이 숫자가 아닙니다.");
+    private static final String NOT_A_POSITIVE_NUMBER_MESSAGE =
+            ErrorMessageGenerator.generateErrorMessage("입력이 0보다 작거나 같습니다.");
+
+    private String attemptNumber;
+
+    @Override
+    public void isValid(String attemptNumber) throws RacingCarGameException {
+        this.attemptNumber = attemptNumber;
+
+        isNumeric();
+        isPositiveNumber();
+    }
+
+    private void isNumeric() throws RacingCarGameException {
+        try {
+            Integer.parseInt(attemptNumber);
+        } catch (NumberFormatException e) {
+            throw new RacingCarGameException(NOT_A_NUMBER_MESSAGE);
+        }
+    }
+
+    private void isPositiveNumber() throws RacingCarGameException {
+        if (Integer.parseInt(attemptNumber) <= 0) {
+            throw new RacingCarGameException(NOT_A_POSITIVE_NUMBER_MESSAGE);
+        }
     }
 }
