3289d09f86924bad2db598fbef9331a407895b88
xorud2592 <xorud2592@gmail.com>
2020-12-08 19:02:41 +0900

    마지막 점검후 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Application {
 	private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	private static final int INPUT_MAX_LENGTH = 5; 
+	private static final int INPUT_MAX_LENGTH = 5;
 	private static final String ERROR_NAME_MESSAGE = "[ERROR] 5자초과에 이름은 가질수없으므로 삭제됩니다";
 	private static final String INPUT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 	private static final String ERROR_INPUT_COUNT_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
 	private static final String GAME_START_MESSAGE = "\n실행 결과.";
 	private static final String RESULT_MESSAGE = "최종 우승자: ";
-	
+
 	static int topPosition = 0;
-	
+
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		// TODO 援ы쁽 吏꾪뻾
@@ -26,9 +26,9 @@ public class Application {
 		cars = createCars(cars, inputCars);
 
 		int count = checkCount(scanner);
-		
+
 		gameStart(cars, count);
-		
+
 	}
 
 	public static String[] checkInput(String[] inputCars) {
@@ -88,7 +88,7 @@ public class Application {
 			moveCar(cars);
 			System.out.println();
 		}
-		
+
 		resultGame(cars);
 	}
 
@@ -98,34 +98,30 @@ public class Application {
 			cars[carsCount].statusPrint();
 		}
 	}
-	
-	public static void resultGame(Car[] cars)
-	{
-		topPosition =0;
+
+	public static void resultGame(Car[] cars) {
+		topPosition = 0;
 		String resultMessage = "";
-		
+
 		for (int carsCount = 0; carsCount < cars.length; carsCount++) {
 			resultMessage = winCheck(cars[carsCount], resultMessage);
 		}
-		
-		topPosition =0;
+
+		topPosition = 0;
 		System.out.println(RESULT_MESSAGE + " " + resultMessage);
 	}
-	
-	public static String winCheck(Car car, String resultMessage)
-	{
-		
-		if(topPosition < car.getPosition())
-		{
+
+	public static String winCheck(Car car, String resultMessage) {
+
+		if (topPosition < car.getPosition()) {
 			topPosition = car.getPosition();
 			return car.getName();
 		}
-		
-		if(topPosition == car.getPosition())
-		{
+
+		if (topPosition == car.getPosition()) {
 			return resultMessage + ", " + car.getName();
 		}
-		
+
 		return resultMessage;
 	}
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,40 +3,34 @@ package racingcar;
 import utils.RandomUtils;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 異붽� 湲곕뒫 援ы쁽
+	// 異붽� 湲곕뒫 援ы쁽
 
-    public void statusPrint()
-    {
-    	String range = "";
-    	for(int i = 0; i<position;i++)
-    	{
-    		range+="-";
-    	}
-    	System.out.println(name + " : " + range);
-    }
-    
-    public void move()
-    {
-    	if(RandomUtils.nextInt(0, 9) >= 4)
-    	{
-    		position++;
-    	}
-    }
-    
-    public int getPosition()
-    {
-    	return position;
-    }
-    
-    public String getName()
-    {
-    	return name;
-    }
+	public void statusPrint() {
+		String range = "";
+		for (int i = 0; i < position; i++) {
+			range += "-";
+		}
+		System.out.println(name + " : " + range);
+	}
+
+	public void move() {
+		if (RandomUtils.nextInt(0, 9) >= 4) {
+			position++;
+		}
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
