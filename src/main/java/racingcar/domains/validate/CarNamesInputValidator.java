a9e836ec0726f2d0a1803d614c22b865b376d50a
minsugn Son <sns2831@gmail.com>
2020-12-08 10:13:30 +0900

    feat(racing car): 자동차 이름 입력 검증 로직 구현
    CarNamesInputValidator.java 구현
    isRightFrom : 입력 포멧 검증
    isRightLength : 자동차 이름 길이 검증

--- src/main/java/racingcar/domains/validate/CarNamesInputValidator.java
+++ src/main/java/racingcar/domains/validate/CarNamesInputValidator.java
@@ -1,10 +1,51 @@
 package racingcar.domains.validate;
 
-import java.util.List;
+import racingcar.ErrorMessageGenerator;
+import racingcar.domains.error.RacingCarGameException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.regex.Pattern;
 
 public class CarNamesInputValidator implements Validator<String> {
-    @Override
-    public void isValid(String carNames) {
+    private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
+    private static final String DELIMITER = ",";
 
+    private static final String INVALID_FORMAT_MESSAGE =
+            ErrorMessageGenerator.generateErrorMessage("입력 형식이 부정확 합니다.");
+    private static final String INVALID_CAR_NAME_LENGTH_MESSAGE =
+            ErrorMessageGenerator
+                    .generateErrorMessage("자동차 이름의 길이가" + MAXIMUM_CAR_NAME_LENGTH + "보다 깁니다.");
+
+    private static final String CAR_NAMES_INPUT_FORMAT =
+            "((( *)([가-힣]|\\w)+( *))+,?)+(.(( *)([가-힣]|\\w)+( *)))$";
+
+    private String carNames;
+
+    @Override
+    public void isValid(String carNames) throws RacingCarGameException {
+        this.carNames = carNames;
+
+        isRightFrom();
+        isRightLength();
+    }
+
+    private void isRightFrom() throws RacingCarGameException {
+        if (Pattern.matches(CAR_NAMES_INPUT_FORMAT, carNames)) {
+            throw new RacingCarGameException(INVALID_FORMAT_MESSAGE);
+        }
+    }
+
+    private void isRightLength() throws RacingCarGameException {
+        String[] splitCarNames = carNames.split(DELIMITER);
+
+        final long count = Arrays.stream(splitCarNames)
+                .map(String::trim)
+                .filter(carName -> carName.length() <= MAXIMUM_CAR_NAME_LENGTH)
+                .count();
+
+        if(splitCarNames.length != count) {
+            throw new RacingCarGameException(INVALID_CAR_NAME_LENGTH_MESSAGE);
+        }
     }
 }
