60a5f29a5d00fce8ef7dd2d74a8f16ee67b9fe02
yoon-chaejin <kevin3st@naver.com>
2020-12-08 21:37:32 +0900

    feat(input) : check the validity of car names
    Invalid Car Name Types
    - Name longer than 5
    - Blank name

--- src/main/java/service/InputService.java
+++ src/main/java/service/InputService.java
@@ -13,12 +13,23 @@ public class InputService {
                 System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
                 String input = scanner.nextLine();
                 carNames = input.split(SEPARATOR);
+                checkCarNames(carNames);
                 break;
             } catch (Exception e) {
-                System.out.println("[ERROR]"+e);
+                System.out.println("[ERROR]"+e.getMessage());
             }
         }
-
         return carNames;
+    }
+
+    private static void checkCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            if(carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            if(carName.isBlank()) {
+                throw new IllegalArgumentException("자동차 이름은 공백이 아니어야 합니다.");
+            }
+        }
     }
 }
