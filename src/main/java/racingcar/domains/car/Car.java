a801135a27d580e1b1d290f93de2fa3b0af17b07
minsugn Son <sns2831@gmail.com>
2020-12-08 12:11:55 +0900

    feat(racing car): Car.js 우승자 계산을 위한 메소드 추가
    compareTo : 다른 car 객체와 position 을 비교하기 위해 사용
    isSamePosition : 다른 car 객체와 position 이 같은지 비교하기 위해 사용
    getName : 자동차 이름을 얻어내기 위해 사용

--- src/main/java/racingcar/domains/car/Car.java
+++ src/main/java/racingcar/domains/car/Car.java
@@ -4,7 +4,7 @@ import racingcar.domains.output.CarStatus;
 import racingcar.domains.output.Output;
 import utils.RandomUtils;
 
-public class Car {
+public class Car implements Comparable<Car> {
     private static final int MINIMUM_RANDOM_NUMBER = 0;
     private static final int MAXIMUM_RANDOM_NUMBER = 9;
     private static final int MOVABLE_THRESHOLD = 4;
@@ -26,13 +26,26 @@ public class Car {
         return new CarStatus(name, position);
     }
 
-   private void move() {
+    private void move() {
         position++;
     }
 
     private boolean isMovable() {
         int randomNumber = RandomUtils.nextInt(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
         return MOVABLE_THRESHOLD <= randomNumber;
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        return position - car.position;
+    }
+
+    public boolean isSamePosition(Car car) {
+        return position == car.position;
+    }
+
+    public String getName() {
+        return name;
     }
 }
 
