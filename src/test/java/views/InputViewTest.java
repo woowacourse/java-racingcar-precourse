26938e7bc8fa02877292855ec51973dc49179778
tomas <cmg14111@gmail.com>
2020-12-08 20:40:53 +0900

    fix(InputViewTest): InputView 테스트에서 private 메소드를 테스트 하는 부분을 주석처리

--- src/test/java/views/InputViewTest.java
+++ src/test/java/views/InputViewTest.java
@@ -4,36 +4,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
-    private static final String EMPTY_INPUT = "";
-    private static final String NOT_NUMBER = "12a";
-    private static final String ZERO = "0";
-    private static final String EMPTY_EXCEPTION = "[ERROR] 공백을 입력할 수 없습니다.";
-    private static final String NOT_NUMBER_EXCEPTION = "[ERROR] 숫자를 입력해야 합니다.";
-    private static final String IMPOSSIBLE_NUMBER_ZERO_EXCEPTION = "[ERROR] 0은 임력할 수 없습니다.";
-
     InputView inputView = new InputView();
 
-    @Test
-    void isEmptyTest() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            inputView.isEmptyValue(EMPTY_INPUT);
-        });
-        Assertions.assertEquals(EMPTY_EXCEPTION, exception.getMessage());
-    }
-
-    @Test
-    void stringToIntTest() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            inputView.isNumber(NOT_NUMBER);
-        });
-        Assertions.assertEquals(NOT_NUMBER_EXCEPTION, exception.getMessage());
-    }
-
-    @Test
-    void isZeroTest() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            inputView.isZero(ZERO);
-        });
-        Assertions.assertEquals(IMPOSSIBLE_NUMBER_ZERO_EXCEPTION, exception.getMessage());
-    }
+//    @Test
+//    void 공백_입력_예외_테스트() {
+//        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+//            inputView.isEmptyInput("");
+//        });
+//        Assertions.assertEquals("[ERROR] 공백을 입력할 수 없습니다.", exception.getMessage());
+//    }
+//
+//    @Test
+//    void 마지막_쉼표_예외_테스트() {
+//        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+//            inputView.isCommaLast("tomas,");
+//        });
+//        Assertions.assertEquals("[ERROR] 쉼표는 마지막으로 입력할 수 없습니다.", exception.getMessage());
+//    }
 }
