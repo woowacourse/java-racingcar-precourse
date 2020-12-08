d6c665962115105695fc01484c58a3ca9bca0ed8
surfing2003 <surfing2003@naver.com>
2020-12-08 21:48:22 +0900

    feat(inputexceptioncntroller): 이름5자 초과확인 구현

--- src/main/java/racingcar/InputExceptionController.java
+++ src/main/java/racingcar/InputExceptionController.java
@@ -3,14 +3,15 @@ package racingcar;
 import java.util.ArrayList;
 
 public class InputExceptionController {
+    private static int MAX_NAME_LENGTH = 5;
     private static String ERROR_MARK = "[ERROR]";
 
     public static boolean isDuplication(ArrayList<Car> cars, String participationCar) {
         return cars.contains(participationCar);
     }
 
-    public static void isLengthExcess() {
-
+    public static boolean isLengthExcess(String participationCar) {
+        return participationCar.length() > MAX_NAME_LENGTH;
     }
 
     public static void checkInputCars(ArrayList<Car> cars, String participationCar) {
