c68d1da32f5118354a69fe5fd81c365a5a06169b
김주원 <kjw11077@naver.com>
2020-12-08 20:36:36 +0900

    refactor(Messages): 상수 순서 정렬

--- src/main/java/racingcar/Messages.java
+++ src/main/java/racingcar/Messages.java
@@ -6,9 +6,11 @@ public class Messages {
     public static final String GAME_RESULT_IS = "실행 결과";
     public static final String PLEASE_INPUT_NAMES_OF_CAR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String PLEASE_INPUT_TURNS = "게임의 턴 수는 몇 회인가요?";
-    public static final String NAMES_CAN_NOT_DUPLICATE = "차들의 이름끼리는 중복될 수 없습니다.";
-    public static final String TRY_COUNT_IS_INTEGER_ONLY = "턴 수는 정수만 가능합니다.";
-    public static final String TRY_COUNT_IS_NATURAL_NUMBER_ONLY = "턴 수는 1 이상의 자연수만 가능합니다.";
+
+    public static final String TURNS_SHOULD_BE_INTEGER = "턴 수는 정수만 가능합니다.";
+    public static final String TURNS_SHOULD_BE_GREATER_THAN_ONE = "턴 수는 1 이상의 자연수만 가능합니다.";
+
     public static final String NAME_SHOULD_BE_LONGER_THAN_ONE = "이름의 길이는 1 이상이어야 합니다.";
     public static final String NAME_SHOULD_BE_SHORTER_THAN_FIVE = "이름의 길이는 5 이하여야 합니다.";
+    public static final String NAMES_CAN_NOT_DUPLICATE = "차들의 이름끼리는 중복될 수 없습니다.";
 }
