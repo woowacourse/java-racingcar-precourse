451406d06ad255169c9b7ac5db128064966b0ab2
김주원 <kjw11077@naver.com>
2020-12-08 20:49:05 +0900

    feat(InputValidator): 턴 수 입력값 검증 객체

---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,55 @@
+package racingcar;
+
+import static racingcar.Messages.TURNS_SHOULD_BE_GREATER_THAN_ONE;
+import static racingcar.Messages.TURNS_SHOULD_BE_INTEGER;
+import static utils.PrintUtils.print;
+
+import exceptions.InvalidInputException;
+
+public class InputValidator {
+
+    private boolean valid;
+    private Integer turns;
+
+    public InputValidator() {
+        valid = false;
+        turns = null;
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public boolean isInValid() {
+        return !valid;
+    }
+
+    public void validate(String input) {
+        try {
+            validateIsInteger(input);
+            validateIsMoreThanOne();
+
+            validationSuccess();
+        } catch (InvalidInputException e) {
+            print(e.getMessage());
+        }
+    }
+
+    private void validateIsInteger(String input) {
+        try {
+            turns = Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(TURNS_SHOULD_BE_INTEGER);
+        }
+    }
+
+    private void validateIsMoreThanOne() {
+        if (turns < 1) {
+            throw new InvalidInputException(TURNS_SHOULD_BE_GREATER_THAN_ONE);
+        }
+    }
+
+    private void validationSuccess() {
+        valid = true;
+    }
+}
