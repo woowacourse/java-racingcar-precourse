0f35f9b4e0a693e04e3533e63d6b385939bb01f8
tomas <cmg14111@gmail.com>
2020-12-08 15:16:17 +0900

    feat: 마지막 입력이 쉼표일때, 이름이 하나만 입력됬을때의 예외 추가 구현

--- src/main/java/domain/RacingCars.java
+++ src/main/java/domain/RacingCars.java
@@ -8,9 +8,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingCars {
+    private static final String SINGLE_CAR_UNAVAILABLE = "[ERROR] 한대의 차량으로는 경주할 수 없습니다.";
+    private static final String COMMA_LAST_UNAVAILABLE = "[ERROR] 쉼표는 마지막으로 입력할 수 없습니다.";
     private static final int RANDOM_MINIMUM = 0;
     private static final int RANDOM_MAXIMUM = 9;
     private static final String SEPARATOR = ",";
+    private static final char SEPARATOR_CHAR = ',';
 
     List<Car> racingCarList = new ArrayList<>();
 
@@ -27,9 +30,24 @@ public class RacingCars {
         }
     }
 
-    public static List<String> namesToNameList(String str) {
-        return Arrays.stream(str.split(SEPARATOR))
+    public static List<String> namesToNameList(String carNames) {
+        isCommaLast(carNames);
+        List<String> carNameList = Arrays.stream(carNames.split(SEPARATOR))
                 .collect(Collectors.toList());
+        isSingleCar(carNameList);
+        return carNameList;
+    }
+
+    private static void isSingleCar(List<String> carNameList) {
+        if (carNameList.size() == 1) {
+            throw new IllegalArgumentException(SINGLE_CAR_UNAVAILABLE);
+        }
+    }
+
+    private static void isCommaLast(String carNames) {
+        if (carNames.charAt(carNames.length()-1) == SEPARATOR_CHAR) {
+            throw new IllegalArgumentException(COMMA_LAST_UNAVAILABLE);
+        }
     }
 
     public List<Car> getCarList() {
