64dccb007aef7e3da60082569328be15f318af10
minsugn Son <sns2831@gmail.com>
2020-12-08 12:29:58 +0900

    fix(racing car): 자동차 이름 입력 클래스와 시도 횟수 입력 클래스 안내메세지 교환
    AttemptNumberInput.java
    CarNamesInput.java
    안내 메시지가 서로 반대로 작성되어 변경

--- src/main/java/racingcar/domains/input/AttemptNumberInput.java
+++ src/main/java/racingcar/domains/input/AttemptNumberInput.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class AttemptNumberInput implements Input<Integer> {
 
-    private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private Scanner scanner;
     private String attemptNumber;
--- src/main/java/racingcar/domains/input/CarNamesInput.java
+++ src/main/java/racingcar/domains/input/CarNamesInput.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class CarNamesInput implements Input<List<String>> {
 
-    private static final String INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String DELIMITER = ",";
 
     private Scanner scanner;
