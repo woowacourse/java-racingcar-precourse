88dfeb89ef1997dd0ad44b92597d8eed6f59e67c
xorud2592 <xorud2592@gmail.com>
2020-12-08 17:39:29 +0900

    입력받은값 자른후 5글자 이하 체크

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,43 @@ package racingcar;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		// TODO 구현 진행
+
+		String input = scanner.nextLine();
+		String[] inputCars = input.split(",");
+		inputCars = checkInput(inputCars);
+
+		for (int i = 0; i < inputCars.length; i++) {
+			System.out.println(inputCars[i]);
+		}
+
+	}
+
+	public static String[] checkInput(String[] inputCars) {
+		int carCount = 0;
+		String[] beforeInputCars = new String[inputCars.length];
+
+		for (int number = 0; number < inputCars.length; number++) {
+			if (checkLength(inputCars[number])) {
+				beforeInputCars[carCount] = inputCars[number];
+				carCount++;
+			}
+		}
+
+		String[] affterInputCars = new String[carCount];
+		for (int beforeNumber = 0; beforeNumber < carCount; beforeNumber++) {
+			affterInputCars[beforeNumber] = beforeInputCars[beforeNumber];
+		}
+
+		return affterInputCars;
+	}
+
+	public static Boolean checkLength(String inputCar) {
+		if (inputCar.length() <= 5) {
+			return true;
+		}
+		return false;
+	}
 }
