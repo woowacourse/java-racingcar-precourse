4dd08c078fa2cf1e88d231f65b5fdd241e45cb9e
yoon-chaejin <kevin3st@naver.com>
2020-12-08 22:51:47 +0900

    feat(game) : find winners

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -2,23 +2,51 @@ package service;
 
 import racingcar.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameService {
     private List<Car> cars;
 
     public GameService(String[] carNames) {
+        cars = new ArrayList<>();
+
         for (String carName : carNames) {
             Car car = new Car(carName);
-            this.cars.add(car);
+            cars.add(car);
         }
     }
 
     public void runPhase() {
-        for (Car car : this.cars) {
+        for (Car car : cars) {
             car.moveRandomly();
             System.out.println(car);
         }
         System.out.println();
+    }
+
+    private Car findOneOfWinners() {
+        Car winner = cars.get(0);
+
+        for (Car car : cars) {
+            if (winner.compareTo(car) < 0) {
+                winner = car;
+            }
+        }
+
+        return winner;
+    }
+
+    private List<Car> findWinners() {
+        List<Car> winners = new ArrayList<>();
+
+        Car winner = findOneOfWinners();
+        for (Car car : cars) {
+            if (winner.compareTo(car) == 0) {
+                winners.add(car);
+            }
+        }
+
+        return winners;
     }
 }
