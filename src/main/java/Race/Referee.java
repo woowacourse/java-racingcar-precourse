915f86538e28be31a67f348de1c065200f074d49
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-08 12:22:15 +0900

    [구현] 모든 우승자를 판단해서 저장하는 saveAllWinner 함수 구현

--- src/main/java/Race/Referee.java
+++ src/main/java/Race/Referee.java
@@ -8,9 +8,23 @@ import java.util.List;
 
 public class Referee {
     private OutputView winnerBoard = new OutputView();
+    private List<String> winners = null;
 
     public Referee() {
 
+    }
+
+    private void saveAllWinner(Cars cars) {
+        int maxScore = cars.getMaxScore();
+        int carNum = cars.getCarNum();
+        Car currentCar;
+
+        for(int index = 0; index < carNum; index++) {
+            currentCar = cars.getCar(index);
+            if(this.isWinner(currentCar, maxScore)) {
+                this.winners.add(currentCar.getName());
+            }
+        }
     }
 
     private boolean isWinner(Car car, int winnerScore) {
