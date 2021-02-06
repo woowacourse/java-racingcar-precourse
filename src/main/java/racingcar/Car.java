e5cd970435ec301e213463f894896d0f028a9f9d
suhye0n <contact.suhyeon@gmail.com>
2021-02-06 21:00:57 +0900

    210206 - Car 클래스 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,23 @@
-package racingcar;
-
 public class Car {
     private final String name;
-    private int position = 0;
+    private int position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
-    // 추가 기능 구현
+    public void printResult() {
+        for (int i = 0; i < this.position ; i++) {
+            System.out.println("-");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
