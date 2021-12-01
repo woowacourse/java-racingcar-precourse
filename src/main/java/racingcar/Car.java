10b3ca87e48fd650ff64c7c33a61553952c18be1
jongwon <54282927+loosie@users.noreply.github.com>
2021-12-01 20:02:25 +0900

    feat: Car 메서드 기능 및 정렬 클래스 추가
    새로운 메서드 기능 추가
    - toString(): 차 이름(name)과 position 수에 따른 '-'을 찍어 값을 출력한다.
    - getPosition(): 해당 Car 객체의 position 변수 값을 반환한다.
    - getName(): 해당 Car 객체의 name을 반환한다.
    - moveOrStop(int randomNumber) :  randomNumber의 값을 입력받아 4 이상이면 move(position+1) 아니면 아무런 동작을 하지 않는다.
    
    정렬 클래스 및 메서드 추가
    - ComparatorByPosition 클래스 추가: Car객체의 position을 기준으로 내림차순을 해준다.
    - sortingByPosition(List<Car> carList): ComparatorByPosition을 참조받아 입력받은 carList를 정렬한다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,50 @@
 package racingcar;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(name + " : ");
+		for (int i = 0; i < position; i++) {
+			sb.append("-");
+		}
+		return sb.toString();
+	}
+
+	public void moveOrStop(int randomNumber) {
+		if (randomNumber >= 4) {
+			position++;
+		}
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static void sortingByPosition(List<Car> carList) {
+		Collections.sort(carList, new ComparatorByPosition());
+	}
+
+	static class ComparatorByPosition implements Comparator<Car> {
+		@Override
+		public int compare(Car o1, Car o2) {
+			return o2.position - o1.position;
+		}
+	}
+
 }
