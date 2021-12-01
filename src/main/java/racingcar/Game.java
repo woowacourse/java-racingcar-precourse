b95c8167006c4219cca0bd8149727a125a1a4dfc
DWL21 <76774809+DWL21@users.noreply.github.com>
2021-12-01 20:02:34 +0900

    feat: 자동차 이름 입력 - 잘못된 값이 입력될 경우 IllegalArgumentException 오류를 발생시키고 에러 메세지를 출력하는 기능

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -14,6 +14,29 @@ public class Game {
         return input;
     }
 
+    public void gameStart() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        boolean checking = false;
+        while (!checking) {
+            CarsInput();
+            checking = checkCars();
+        }
+
+    }
+
+    private boolean checkCars() {
+        try {
+            boolean checking = true;
+
+
+        } catch (IllegalArgumentException) {
+
+        }
+
+        return checking;
+    }
+
+    
 
 
 }
