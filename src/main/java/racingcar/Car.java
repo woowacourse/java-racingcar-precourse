465d4d51f0d7d3af10eabdb7f07688cd359e299c
Seungmin Lim <seungmin9066@gmail.com>
2021-12-01 16:30:18 +0900

    [추가] 자동차의 이름을 알려주는 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,17 @@
 package racingcar;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public String getCarName() {
+		return this.name;
+	}
+
+
+	// 추가 기능 구현
 }
