63543204f8842d3e80f2e6735cb31c9cfe22e00a
lymchgmk <lymchgmk@gmail.com>
2021-12-02 05:12:18 +0900

    feat (Car.java) : 자동차 객체 구현
    - getName() : class Car의 name 속성 return
    - getPosition() : class Car의 position 속성 return
    - race() : camp.nextstep.edu.missionutils.Randoms를 사용하여 레이싱 횟수 당 position 증가 혹은 유지 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +11,20 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void race() {
+        int startInclusive = Constants.RACE_DICE_START_INCLUSIVE;
+        int endInclusive = Constants.RACE_DICE_END_INCLUSIVE;
+        int diceEyes = Randoms.pickNumberInRange(startInclusive, endInclusive);
+        if (diceEyes >= 4) {
+            position++;
+        }
+    }
 }
