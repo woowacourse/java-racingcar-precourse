2fb4aeec2401e41a164662bb816f968100dc5534
wannagohome <iwwuf7@gmail.com>
2021-12-01 21:49:53 +0900

    feat(input): receive car input again when fail on validation

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,13 +7,27 @@ public class RacingGame {
 	private static final String CAR_INPUT_ERROR_MESSAGE = "[ERROR] 입력한 자동차 이름을 확인하세요";
 	
 	public void start() {
+		requestCarInput();
+	}
+
+	private String requestCarInput() {
 		System.out.println(CAR_INPUT_MESSAGE);
+		return receiveCarInput();
+	}
+
+	private String receiveCarInput() {
 		String carInput = Console.readLine();
 
 		try {
 			Validator.validateCarInput(carInput);
 		} catch (IllegalArgumentException e) {
 			System.out.println(CAR_INPUT_ERROR_MESSAGE);
+			return receiveCarInputAgain();
 		}
+		return carInput;
+	}
+
+	private String receiveCarInputAgain() {
+		return receiveCarInput();
 	}
 }
