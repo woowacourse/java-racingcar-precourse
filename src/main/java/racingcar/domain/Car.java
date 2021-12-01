231147f0fe4fe3594ae463af67507013ddcea374
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-01 21:20:23 +0900

    feat<Car>: 정적 팩토리 메소드 사용, 검증 기능 추가
    - 생성자를 private로 접근 제어하고 팩토리 메소드 패턴을 적용해서 createCar로만 객체 생성하도록 만듦
    - 자동차 이름에 아무것도 입력하지 않은 경우와 공백이 포함된 경우에 대한 검증 과정 추가
      - Matcher와 Patter.comile() 사용
    
    자동차 객체 생성을 위해서 정적 팩토리 메서드 사용했으며, 이름에 공백을 금지하며, 한글을 허용합니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,27 +1,51 @@
 package racingcar.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        validateName(name);
-        this.name = name;
-    }
+	private Car(String name) {
+		this.name = name;
+	}
 
-    public void goForward() {
-        this.position++;
-    }
+	public void goForward() {
+		this.position++;
+	}
 
-    // TODO ERROR문 상수로 처리하기
-    public static void validateName(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5글자 이하로 입력해주세요.");
-        }
+	// TODO ERROR문 상수로 처리하기
+	public static void validateName(String name) {
+		if (name.length() == 0) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름을 입력해주세요.");
+		}
 
-        String nameRegex = "[^A-Za-z0-9]+";
-        if (name.matches(nameRegex)) {
-            throw new IllegalArgumentException("[ERROR] 자동차 이름은 영어 혹은 숫자로만 입력해주세요.");
-        }
-    }
+		if (name.length() > 5) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름은 5글자 이하로 입력해주세요.");
+		}
+
+		// TODO 클래스 상수로 만들어 가져다쓰기
+		Matcher matchTest;
+		String spaceRegex = "[\\s]";
+		matchTest = Pattern.compile(spaceRegex).matcher(name);
+		if (matchTest.find()) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름에 공백은 입력 불가능합니다.");
+		}
+
+		String nameRegex = "[^a-zA-Z0-9ㄱ-힣]";
+		matchTest = Pattern.compile(nameRegex).matcher(name);
+		if (matchTest.find()) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름은 영어나 한글 혹은 숫자로만 입력해주세요.");
+		}
+	}
+
+	public static Car createCar(String name) {
+		validateName(name);
+		return new Car(name);
+	}
+
+	public int getPosition() {
+		return position;
+	}
 }
