6d3f7b9359bca7d2be78acfecaabdc94bfc52290
wilgur513 <wilgur513@naver.com>
2021-12-02 00:58:01 +0900

    refactor: extract constant value

--- src/main/java/racingcar/CarNameReader.java
+++ src/main/java/racingcar/CarNameReader.java
@@ -8,6 +8,7 @@ public class CarNameReader extends Reader<List<String>>{
 	public static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String ERROR_MESSAGE = "[ERROR] 자동차 이름은 5글자 이하여야 합니다.";
 	private static final int BOUNDARY_VALUE = 5;
+	private static final String DELIMITER = ",";
 
 	public CarNameReader(ConsoleDisplay consoleDisplay) {
 		super(consoleDisplay, INPUT_MESSAGE, ERROR_MESSAGE);
@@ -15,12 +16,12 @@ public class CarNameReader extends Reader<List<String>>{
 
 	@Override
 	protected boolean isValid(String input) {
-		return Stream.of(input.split(",")).anyMatch(name -> isLessThanBoundaryLength(name));
+		return Stream.of(input.split(DELIMITER)).anyMatch(name -> isLessThanBoundaryLength(name));
 	}
 
 	@Override
 	protected List<String> handleInputValue(String input) {
-		return Arrays.asList(input.split(","));
+		return Arrays.asList(input.split(DELIMITER));
 	}
 
 	private boolean isLessThanBoundaryLength(String name) {
