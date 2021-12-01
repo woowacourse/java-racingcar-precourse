7860b3b9ce30e74909ad92dcc33187163dac0dcb
KIM <km0192@naver.com>
2021-12-01 19:47:59 +0900

    refactor(Car) getName 추가, 변수/메소드 위치변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,19 +2,24 @@ package racingcar;
 
 public class Car {
     private final String name;
-    private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    String getName(){
+        return this.name;
+    }
+
+    private int position = 0;
+
+    int getPosition(){
+        return this.position;
     }
 
     void moveIfNumGTE4(int num){
         if (num >= 4){
             this.position++;
         }
-    }
-
-    int getPosition(){
-        return this.position;
     }
 }
