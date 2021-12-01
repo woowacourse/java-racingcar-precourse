de35df54bf13e1463a42b98371de48501ef09bc3
Seungmin Lim <seungmin9066@gmail.com>
2021-12-01 16:37:16 +0900

    [추가] 자동차 이름에서 예외처리 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,10 @@
 package racingcar;
 
 public class Car {
+
+	private static final String namingErrorMessage = "길이는 5글자 이내로 입력해야 합니다.";
+	private static final int lengthLimit = 5;
+
 	private final String name;
 	private int position = 0;
 
@@ -12,6 +16,10 @@ public class Car {
 		return this.name;
 	}
 
-
+	private void validateName(String name) {
+		if (name.length() > lengthLimit) {
+			throw new IllegalArgumentException(namingErrorMessage);
+		}
+	}
 	// 추가 기능 구현
 }
