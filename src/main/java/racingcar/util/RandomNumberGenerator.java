01b3323ff05ec37ad27efd1789f1591e4fb7b2e9
SeungHan <jk05018@naver.com>
2021-12-01 20:42:56 +0900

    feat : 0~9에 해당하는 무작위 수 생성 기능 추가

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,4 +1,9 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
+	public static int generateRandomNumber(){
+		return Randoms.pickNumberInRange(0, 9);
+	}
 }
