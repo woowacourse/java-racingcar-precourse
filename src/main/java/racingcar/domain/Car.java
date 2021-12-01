00324163514b8cd20b3ae246ae006234fec5aa5a
yoon <duddbs774@gmail.com>
2021-12-02 01:02:21 +0900

    refactor: 코드 리팩토링
    racingcar.domain.Car canIMove 메소드 추가(매개변수가 4 이상인지 체크)

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,21 @@
 package racingcar.domain;
 
 public class Car {
+    public static final int MIN_STANDARD_NUMBER = 4;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) { this.name = name; }
 
-    public void move() {
-        position++;
+    public void move(int value) {
+        if (canIMove(value)) {
+            position++;
+        }
+    }
+
+    private boolean canIMove(int value) {
+        return value >= MIN_STANDARD_NUMBER;
     }
 
     public int getPosition() {
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,18 +2,34 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CarTest {
-    @DisplayName("자동차 전진 테스트")
+    Car myCar;
+
+    @BeforeEach
+    void setUp() {
+        myCar = new Car("myCar");
+    }
+
+    @DisplayName("자동차 전진 및 정지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "3:0", "4:1", "9:1"}, delimiter = ':')
+    void moveTest(int input, int expected) {
+        myCar.move(input);
+        assertThat(myCar.getPosition()).isEqualTo(expected);
+    }
+
+    @DisplayName("자동차 연속 전진 테스트")
     @Test
-    void move() {
-        Car myCar = new Car("myCar");
-        assertThat(myCar.getPosition()).isEqualTo(0);
-        myCar.move();
+    void continuouslyMoveTest() {
+        myCar.move(4);
         assertThat(myCar.getPosition()).isEqualTo(1);
-        myCar.move();
+        myCar.move(4);
         assertThat(myCar.getPosition()).isEqualTo(2);
     }
 }
