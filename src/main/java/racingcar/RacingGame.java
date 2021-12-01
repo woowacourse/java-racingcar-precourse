2ad9b8ad958d669b1fdf478fc8d62880f5635615
wannagohome <iwwuf7@gmail.com>
2021-12-01 22:41:15 +0900

    feat(game): print 실행 결과 after input

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,8 +1,11 @@
 package racingcar;
 
 public class RacingGame {
+	private static final String RESULT_MESSAGE = "실행 결과";
+
 	public void start() {
 		Request.carInput();
 		Request.turnInput();
+		System.out.println(RESULT_MESSAGE);
 	}
 }
