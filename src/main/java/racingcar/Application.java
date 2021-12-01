f241ac25d77cb25f5af986bbc5e5544dee9dd209
soulfever <soulfever01@naver.com>
2021-12-01 18:01:43 +0900

    FEAT : 자동차가 앞으로 갈 것인지 판별하는 매소드 생성
    0~9까지의 수에서 random으로 한 개를 뽑았을 때,
    0~3까지는 false, 4~9까지는 true를 반환하는 매소드입니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 
 public class Application {
@@ -8,9 +9,18 @@ public class Application {
         String[] arrayCar = inputToArray();
         Car[] arrayCarObject = arrayCarObj(arrayCar);
         int Count = CountingGame();
-        
+        boolean randomGoStop = GoOrStop();
+    }
 
-
+    private static boolean GoOrStop() {
+        boolean randomGoStop;
+        int randomNum = Randoms.pickNumberInRange(0,9);
+        if (randomNum >= 4) {
+            randomGoStop = true;
+        }
+        else {
+            randomGoStop = false;
+        }
     }
 
     private static int CountingGame() {
@@ -20,13 +30,13 @@ public class Application {
         return Counting;
     }
 
-
     private static String[] inputToArray() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String inputCarName = Console.readLine();
         String[] arrayCar = inputCarName.split(",");
         return arrayCar;
     }
+
     private static Car[] arrayCarObj(String[] arrayCar) {
         Car[] arrayCarObj = new Car[arrayCar.length];
         for (int i = 0; i < arrayCar.length; i++) {
