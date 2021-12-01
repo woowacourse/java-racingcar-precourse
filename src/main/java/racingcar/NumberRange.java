a32db91835e87c82d6f098cc330451ca9a18a1a3
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-01 20:19:13 +0900

    refactor: 숫자 범위 enum class로 분리

---
+++ src/main/java/racingcar/NumberRange.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public enum NumberRange {
+	MIN_NO(0), MAX_NO(9);
+	private final int value;
+
+	NumberRange(int value) {
+		this.value = value;
+	}
+	public int getValue() {
+		return Integer.parseInt(String.valueOf(value));
+	}
+}
--- src/main/java/racingcar/ValidationUtils.java
+++ src/main/java/racingcar/ValidationUtils.java
@@ -2,14 +2,11 @@ package racingcar;
 
 public class ValidationUtils {
 
-	private static final int MIN_NO = 0;
-	private static final int MAX_NO = 9;
-
 	public static boolean validNo(int No) {
-		return No >= MIN_NO && No <= MAX_NO;
+		return No >= NumberRange.MIN_NO.getValue() && No <= NumberRange.MAX_NO.getValue();
 	}
 
-	public static boolean isNumber(String input) {
+	public static boolean validNumber(String input) {
 		try {
 			Integer.parseInt(input);
 		} catch (IllegalArgumentException e) {
