9a45603d91a384a877e1798feadaa8873beb5604
nailseong <nnis9857@gmail.com>
2021-12-02 06:29:47 +0900

    refactor(isValidCarNameList): 함수 추출

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -6,22 +6,33 @@ public class Validator {
 	private static final String CAR_NAME_REGEX = "[A-Za-z]+";
 
 	public static void isValidCarNameList(String[] nameList) throws IllegalArgumentException {
-		if (nameList.length == 0) {
+		checkNameListLength(nameList.length);
+
+		for (String name : nameList) {
+			checkNameLength(name.length());
+			checkNameCharacter(name);
+		}
+	}
+
+	private static void checkNameListLength(int length) {
+		if (length == 0) {
+			throw new IllegalArgumentException("자동차 이름이 존재하지 않습니다.");
+		}
+	}
+
+	private static void checkNameLength(int length) {
+		if (length == 0) {
 			throw new IllegalArgumentException("자동차 이름이 존재하지 않습니다.");
 		}
 
-		for (String name : nameList) {
-			if (name.length() == 0) {
-				throw new IllegalArgumentException("자동차 이름이 존재하지 않습니다.");
-			}
+		if (length > CAR_NAME_LENGTH_LIMIT) {
+			throw new IllegalArgumentException("자동차 이름의 길이는 5자 이하만 허용됩니다.");
+		}
+	}
 
-			if (!name.matches(CAR_NAME_REGEX)) {
-				throw new IllegalArgumentException("자동차 이름은 영어 대소문자만 허용됩니다.");
-			}
-
-			if (name.length() > CAR_NAME_LENGTH_LIMIT) {
-				throw new IllegalArgumentException("자동차 이름의 길이는 5자 이하만 허용됩니다.");
-			}
+	private static void checkNameCharacter(String name) {
+		if (!name.matches(CAR_NAME_REGEX)) {
+			throw new IllegalArgumentException("자동차 이름은 영어 대소문자만 허용됩니다.");
 		}
 	}
 
