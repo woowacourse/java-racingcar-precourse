6b0316a390a0c839bdfc4657abdef435cd56b46b
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-01 17:49:50 +0900

    feat(CarService) : CarService 생성
    1. saveCars : ','로 구분되어 들어온 값 list 변환 후 저장 (검증)
    2. updateCarPosition : 랜덤값에 따른 자동차 위치 변경
    3. findAllCar : 저장된 모든 자동차 정보 return

---
+++ src/main/java/racingcar/CarService.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static util.CarConstant.*;
+
+public class CarService {
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository){
+        this.carRepository = carRepository;
+    }
+
+    public void saveCars(String input){
+        String[] tempCars = input.split(DELIMITER);
+        CarValidator.isNameLowerThanFive(tempCars);
+        carRepository.saveAll(createCarList(tempCars));
+    }
+
+    private List<Car> createCarList(String[] tempCars) {
+        return Arrays.asList(tempCars)
+                .stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public void updateCarPosition(){
+        List<Car> cars = carRepository.findAll();
+        for (Car car : cars) {
+            moveForward(car);
+        }
+    }
+
+    private void moveForward(Car car) {
+        if(checkMoveForward()){
+            car.goForward();
+        }
+    }
+
+    private boolean checkMoveForward() {
+        int randomNumber = Randoms.pickNumberInRange(MIN, MAX);
+        if(randomNumber >= 4){
+            return true;
+        }
+        return false;
+    }
+
+    public List<Car> findAllCar(){
+        return carRepository.findAll();
+    }
+
+}
