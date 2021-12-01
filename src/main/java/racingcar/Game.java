f34456c327e90e018b20365e2cd9ac1667b35719
DWL21 <76774809+DWL21@users.noreply.github.com>
2021-12-01 20:34:36 +0900

    style: 경주할 자동차 이름 입력 받기
    입력 받는 기능의 범용성을 위해 CarsInput 메서드 이름을 Input으로 변경

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -10,7 +10,7 @@ public class Game {
 
     }
 
-    private void CarsInput() {
+    private void Input() {
         input = Console.readLine();
     }
 
@@ -18,9 +18,10 @@ public class Game {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         boolean checking = false;
         while (!checking) {
-            CarsInput();
+            Input();
             checking = checkCars();
         }
+        
 
     }
 
