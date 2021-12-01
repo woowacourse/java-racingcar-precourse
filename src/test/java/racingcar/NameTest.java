cb63173707bbf646a72a56ad0cd4fb28ae83b481
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-02 02:04:41 +0900

    refactor: 기능 삭제

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -2,9 +2,6 @@ package racingcar;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,18 +11,5 @@ public class NameTest {
 	void validCarNameTest() {
 		String testCarName = "sojukang";
 		assertThat(ValidationUtils.validCarName(testCarName)).isFalse();
-	}
-
-	@Test
-	@DisplayName("이름 \",\"로 구분하여 리스트로 저장 테스트")
-	void carNames() {
-		String cars = "abc,def,ghi,jkl";
-		List<String> arrCars = new ArrayList<>();
-		arrCars.add("abc");
-		arrCars.add("def");
-		arrCars.add("ghi");
-		arrCars.add("jkl");
-
-		assertThat(Car.generateCars(cars).containsAll(arrCars)).isTrue();
 	}
 }
