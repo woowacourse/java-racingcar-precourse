fb48cb2e3e49aa0da5478ad770e58b34dfae93fb
TaeHyeon Kim <kth990303@naver.com>
2021-12-01 20:15:11 +0900

    feat: User 클래스 setCarNames 메소드 구현

---
+++ src/main/java/racingcar/User.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class User {
+    static final String DIVISION_STRING = ",";
+    static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public List<String> setCarNames() {
+        System.out.println(CAR_NAME_MESSAGE);
+        String carNames = Console.readLine();
+        List<String> carNameList = Arrays.stream(carNames.split(DIVISION_STRING))
+                .collect(Collectors.toList());
+        try {
+            CarNameValidator.checkException(carNameList);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            carNameList = setCarNames();
+        }
+        return carNameList;
+    }
+}
