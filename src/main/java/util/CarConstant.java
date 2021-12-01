e86bae0004b49fcfc65fb56a3b90ea1f74b02dd4
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-01 17:14:58 +0900

    feat(CarConstant) : 상수 에러 메세지 추가

--- src/main/java/util/CarConstant.java
+++ src/main/java/util/CarConstant.java
@@ -5,6 +5,8 @@ public class CarConstant {
     public final static int MAX = 9;
     public final static int LIMIT_SIZE_OF_NAME = 5;
 
+    public final static String ERROR_PREFIX = "[ERROR]";
+    public final static String NUMBER_ERROR_MESSAGE = "시도 횟수는 숫자여야 한다.";
     public final static String REQUEST_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public final static String REQUEST_NUMBER_TO_TRY = "시도할 회수는 몇회인가요?";
     public final static String EXECUTION_RESULT = "실행 결과";
