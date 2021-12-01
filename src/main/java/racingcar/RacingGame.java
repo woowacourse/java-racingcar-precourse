028c2a93af8f9ffd31b26e7c57e07686523a86c1
wannagohome <iwwuf7@gmail.com>
2021-12-01 20:54:39 +0900

    feature(input): receive name of cars from user
     - 유저로부터 자동차 이름들을 입력 받는다.

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RacingGame {
+	public void start() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String carInput = Console.readLine();
+	}
+}
