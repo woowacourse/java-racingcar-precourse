16b5cd6e0187bd35f58456a3f7280e4aaff48b1c
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-01 16:56:36 +0900

    feat(CarConstant) : 상수값 관리 클래스 생성

---
+++ src/main/java/util/CarConstant.java
@@ -0,0 +1,18 @@
+package util;
+
+public class CarConstant {
+    public final static int MIN = 1;
+    public final static int MAX = 9;
+    public final static int LIMIT_SIZE_OF_NAME = 5;
+
+    public final static String REQUEST_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final static String REQUEST_NUMBER_TO_TRY = "시도할 회수는 몇회인가요?";
+    public final static String EXECUTION_RESULT = "실행 결과";
+    public final static String FINAL_WINNER = "최종 우승자";
+
+    public final static String DELIMITER = ", ";
+    public final static String SEPARATOR = " : ";
+    public final static String MARK = "-";
+
+
+}
