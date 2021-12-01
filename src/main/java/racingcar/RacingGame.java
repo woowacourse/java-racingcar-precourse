a580a1b26184ba72d40927f6ea45172cb33ad2e9
wannagohome <iwwuf7@gmail.com>
2021-12-01 20:59:05 +0900

    refactor(game): make car name request string as constant

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,8 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingGame {
+	static final String CAR_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	
 	public void start() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		System.out.println(CAR_INPUT_STRING);
 		String carInput = Console.readLine();
 	}
 }
