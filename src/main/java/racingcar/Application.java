1b0e35fe686785996d7074f6674f5b156e5f6966
KIM <km0192@naver.com>
2021-12-01 21:01:39 +0900

    feat(main) 자동차 리스트생성, 반복횟수 생성, 레이스를 수행클래스 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        CarListMaker carList = new CarListMaker();  // 자동차 리스트 생성
+        RepeatNumMaker repeatNum = new RepeatNumMaker(); // 숫자 생성
+        Race race = new Race(carList.getCarList(), repeatNum.getRepeatNum()); // 레이스 돌리기
+        // race.getCarList(); // 우승 결과 출력
     }
 }
