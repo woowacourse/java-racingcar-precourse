f8057fce47cb9df7d973ab25a5f9d6fe801eb2e5
soulfever <soulfever01@naver.com>
2021-12-01 17:31:02 +0900

    FEAT : Car 객체들로 묶은 배열을 만드는 매소드 생성
    inputToArray 매소드로 받은 자동차들의 이름들을
    Car[] 배열에 들어가는 Car 객체들로 만드는 매소드를 만들었습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,9 @@ import java.util.stream.Stream;
 public class Application {
     public static void main(String[] args) {
         String[] arrayCar = inputToArray();
+        Car[] arrayCarObject = arrayCarObj(arrayCar);
     }
+
 
     private static String[] inputToArray() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
@@ -15,6 +17,12 @@ public class Application {
         String[] arrayCar = inputCarName.split(",");
         return arrayCar;
     }
-
+    private static Car[] arrayCarObj(String[] arrayCar) {
+        Car[] arrayCarObj = new Car[arrayCar.length];
+        for (int i = 0; i < arrayCar.length; i++) {
+            arrayCarObj[i] = new Car(arrayCar[i]);
+        }
+        return arrayCarObj;
+    }
 
 }
