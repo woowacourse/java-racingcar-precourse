e22da0a2d13e044403c6e4aa196ecfca6e96607e
xi_jjun <rlawowns97@naver.com>
2021-12-01 20:16:05 +0900

    feat: add Constant(Car Random Number Condition)

---
+++ src/main/java/racingcar/constants/Constant.java
@@ -0,0 +1,7 @@
+package racingcar.constants;
+
+public class Constant {
+	// === Car Random Number Range === //
+	public static final int MIN_CONDITION = 0;
+	public static final int MAX_CONDITION = 9;
+}
