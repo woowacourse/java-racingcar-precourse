c9fe579d009ba06330e1778c636add7e30e33cd7
유영우 <fbduddn97@naver.com>
2021-12-01 22:29:47 +0900

    [refactor(Message)] 최종 우승자 message 변경("최종 우승자: " ->" 최종 우승자 : ")

--- src/main/java/config/Message.java
+++ src/main/java/config/Message.java
@@ -4,7 +4,7 @@ public class Message {
 	private final String ENTER_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private final String ASK_REPEAT_NUM = "시도할 회수는 몇회인가요?";
 	private final String PRINT_RESULT = "실행 결과";
-	private final String ENTER_WINNER = "최종 우승자: ";
+	private final String ENTER_WINNER = "최종 우승자 : ";
 
 	public String getENTER_CARS_NAME() {
 		return ENTER_CARS_NAME;
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,5 @@
 package racingcar;
 
-import java.util.ArrayList;
-
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Application {
     public static void main(String[] args) {
         new Game().play();
