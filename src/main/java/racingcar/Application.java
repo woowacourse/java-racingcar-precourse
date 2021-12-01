d322fe8085702070c22932748ed1b20005f50401
bugoverdose <bugod96@gmail.com>
2021-12-01 15:51:08 +0900

    feat: 사용자로부터 자동차 이름들과 시도할 회수 입력 받기
    - 사용자가 정상적인 형식으로 입력했다는 가정
    - 예외 처리 구현 필요

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = Console.readLine().split(",");
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        Integer trialNum = Integer.parseInt(Console.readLine());
     }
 }
