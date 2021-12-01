ea9c48d8a64b6fb2f6ce9365a28652beddeb062e
yxxnghwan <younghwan960@gmail.com>
2021-12-01 18:47:53 +0900

    feat(Car, Game): 자동차 이름 입력 validation 후 Car 객체 생성
    1. Car.generateCars에서 입력된 이름 리스트 validation 후 통과하면 객체 생성하여 List<Car> 형태로 반환
    2. validateName에서 각 자동차 이름 길이를 검사 후 5 초과시 IllegalArgumentException발생
    3. Game.readCars에서 Car.generateCars호출해서 경주할 자동차 객체 리스트를 생성하여 반환. IllegalArgumentException 예외 캐치시 재귀로 readCars() 다시 반환.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +12,19 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public static List<Car> generateCars(List<String> names) {
+        List<Car> result = new ArrayList<>();
+
+        for (String name : names) {
+            validateName(name);
+            result.add(new Car(name));
+        }
+
+        return result;
+    }
+
+    private static void validateName(String name) {
+        if(name.length() > Const.MAX_NAME_COUNT)
+            throw new IllegalArgumentException();
+    }
 }
--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -9,15 +9,23 @@ import camp.nextstep.edu.missionutils.Console;
 public class Game {
 	public static void play() {
 		System.out.println(Const.CAR_INPUT_MESSAGE);
-		readCars();
+
+		List<Car> cars = readCars();
+		System.out.println(cars);
 	}
 
 	private static List<Car> readCars() {
-		List<Car> result = new ArrayList<>();
+		List<Car> result;
 
 		String userInput = Console.readLine();
 		List<String> names = Arrays.asList(userInput.split(","));
-		System.out.println(names);
+
+		try {
+			result = Car.generateCars(names);
+		} catch (IllegalArgumentException e) {
+			System.out.println(Const.ERROR_INPUT_CAR_NAME_MESSAGE);
+			return readCars();
+		}
 
 		return result;
 	}
