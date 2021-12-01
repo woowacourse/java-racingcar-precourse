c224522946f1dfe873aec796be44ca180013a8cb
bugoverdose <bugod96@gmail.com>
2021-12-01 17:23:18 +0900

    feat(Validator): 자동차명이 중복되는 경우 예외 처리

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,10 +1,17 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
 
     private static final String ERROR = "[ERROR] ";
 
     public static void validateCarNames(String[] carNames) {
+        validateUniqueCarNames(carNames);
+
         for (String carName : carNames) {
             validateCarNameLength(carName);
             validateCarNameFormat(carName);
@@ -20,6 +27,15 @@ public class Validator {
     private static void validateCarNameFormat(String carName) {
         if (carName.contains(" ")) {
             throw new IllegalArgumentException(ERROR + "자동차명에 공백이 포함되어서는 안 된다.");
+        }
+    }
+
+    private static void validateUniqueCarNames(String[] carNames) {
+        List<String> carNameList = Arrays.asList(carNames);
+        Set<String> carNameSet = new HashSet<>(carNameList);
+
+        if (carNameList.size() > carNameSet.size()) {
+            throw new IllegalArgumentException(ERROR + "같은 자동차명을 중복 사용해서는 안 된다.");
         }
     }
 }
