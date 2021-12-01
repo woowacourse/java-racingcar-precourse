904c954f1bb3c46f4eb9e8c5794d0195983fb369
soulfever <soulfever01@naver.com>
2021-12-01 16:30:15 +0900

    FEAT : 자동차의 이름을 입력받아서 배열로 만드는 매소드 생성
    inputToArray 매소드를 추가했습니다.
    입력받은 자동차의 이름을 콤마를 기준으로 나눈 배열을
    return하는 매소드입니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        String[] arrayCar = inputToArray();
     }
+
+    private static String[] inputToArray() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String inputCarName = Console.readLine();
+        String[] arrayCar = inputCarName.split(",");
+        return arrayCar;
+    }
+
+
 }
