aceca0ce868977ae4778206a3606c3629d5c37c5
wannagohome <iwwuf7@gmail.com>
2021-12-01 22:13:04 +0900

    feat(input): receive turn input from user

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -5,9 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class RacingGame {
 	private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String CAR_INPUT_ERROR_MESSAGE = "[ERROR] 입력한 자동차 이름을 확인하세요";
+	private static final String TURN_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 	
 	public void start() {
 		requestCarInput();
+		requestTurnInput();
+	}
+
+	private int requestTurnInput() {
+		System.out.println(TURN_INPUT_MESSAGE);
+		return Integer.parseInt(Console.readLine());
 	}
 
 	private String requestCarInput() {
