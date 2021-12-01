c0f59793e92f89211647bd6aa38584837c706c15
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-02 02:05:51 +0900

    feat: 유저 입력 받아 carList 생성 기능

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,15 +1,32 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Game {
+	public static String generateUserInput() {
+		String userInput;
+		userInput = readLine();
+
+		return userInput;
+	}
+
 	public static List<String> strToList(String str) {
 		String[] strSplited = str.split(",");
 		List<String> list;
 		list = new ArrayList<>(Arrays.asList(strSplited));
 
 		return list;
+	}
+
+	public static Cars generateCars() {
+		List<String> userInput = strToList(generateUserInput());
+		ValidationUtils.validCarList(userInput);
+		Cars carList;
+		carList = new Cars(userInput);
+		return carList;
 	}
 }
