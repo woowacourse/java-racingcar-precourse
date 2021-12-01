61c764503410d8c267bad3feb575a5c35f754391
DWL21 <76774809+DWL21@users.noreply.github.com>
2021-12-01 19:36:15 +0900

    feat: 경주할 자동차 이름 입력 받기

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,19 @@
+package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class Game {
+
+    public Game() {
+
+    }
+
+    private String CarsInput() {
+        String input;
+        input = Console.readLine();
+        return input;
+    }
+
+
+
+}
