2425b95ef5630afba1079ca86d6da4c51106de29
Seungmin Lim <seungmin9066@gmail.com>
2021-12-01 18:12:28 +0900

    [추가] 경기의 진행을 출력하는 메서드 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -12,6 +12,9 @@ public class RacingGame {
 	private final String START_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준로 구분)";
 	private final String TRY_MESSAGE = "시도할 회수는 몇회인가요?";
 	private final String VALIDATE_MESSAGE = "숫자만 입력이 가능합니다.";
+	private final String GAME_RESULT_MESSAGE = "실행 결과";
+	private final String CAR_SHAPE = "-";
+
 	private final int START_INCLUSIVE = 0;
 	private final int END_INCLUSIVE = 9;
 	private final int STANDARD_OF_MOVEMENT = 4;
@@ -26,6 +29,7 @@ public class RacingGame {
 		} catch (IllegalArgumentException error) {
 			startGame();
 		}
+		System.out.println(GAME_RESULT_MESSAGE);
 		playRacingGame();
 	}
 
@@ -47,15 +51,25 @@ public class RacingGame {
 	private void playRacingGame() {
 		for (Car car : participantList) {
 			moveCar(car);
-			// 결과 출력
+			printCarLocation(car);
 		}
+		System.out.println();
 	}
 
 	private void moveCar(Car car) {
 		int distance = pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
 		if (distance >= STANDARD_OF_MOVEMENT) {
 			car.moveCar(distance);
+			printCarLocation(car);
 		}
+	}
+
+	private void printCarLocation(Car car) {
+		System.out.print(car.getCarName() + " : ");
+		for (int i = 0; i < car.getLocation(); i++) {
+			System.out.print(CAR_SHAPE);
+		}
+		System.out.println();
 	}
 
 	private void validateNumber(String number) {
