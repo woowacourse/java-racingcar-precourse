4f037f93abf209c8e0d286ec5be10cd6d7b1d876
steven <seokju73@gmail.com>
2021-12-01 18:38:28 +0900

    feat: Add input fucntion in view section

---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class View {
+    
+    public View () {}
+    
+    public String nameOfCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        
+        return Console.readLine();
+    }
+    
+    public String numOfAttempts() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        
+        return Console.readLine();
+    }
+}
