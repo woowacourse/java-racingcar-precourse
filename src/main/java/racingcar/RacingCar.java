b6675098228cf167f945a3bd3d28418ac5fe85da
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-12-01 17:17:39 +0900

    feat : 자동차를 입력받는 화면 및 기능 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.view.InputView;
+import racingcar.view.StartView;
+
 public class RacingCar {
+	InputView view;
 
 	public void racingCar() {
+		getUserCarList();
+	}
+
+	private void getUserCarList() {
+		view = new StartView();
+		String userCarList = view.getUserInput();
 	}
 }
--- src/main/java/racingcar/view/StartView.java
+++ src/main/java/racingcar/view/StartView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
-public class StartView {
+import camp.nextstep.edu.missionutils.Console;
+
+public class StartView implements InputView {
+	private final String START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+	public String getUserInput() {
+		System.out.println(START_MESSAGE);
+		String userInput = Console.readLine();
+		return userInput;
+	}
+
 }
