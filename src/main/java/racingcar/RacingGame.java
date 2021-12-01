24986189b3dc8704887eea7cd3c62340f43209fe
wannagohome <iwwuf7@gmail.com>
2021-12-01 21:25:27 +0900

    feat(validator): validate car name input, print error message
     - 자동자 이름 입력을 검증하고 검증에 실패하면 IllegalArgumentException 을 던진다
     - 검증에 실패하면 오류 메세지를 출력
     - 검증 메세지 변수명을 STRING에서 MESSAGE로 변경

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,10 +3,17 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingGame {
-	static final String CAR_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String CAR_INPUT_ERROR_MESSAGE = "[ERROR] 입력한 자동차 이름을 확인하세요";
 	
 	public void start() {
-		System.out.println(CAR_INPUT_STRING);
+		System.out.println(CAR_INPUT_MESSAGE);
 		String carInput = Console.readLine();
+
+		try {
+			Validator.validateCarInput(carInput);
+		} catch (IllegalArgumentException e) {
+			System.out.println(CAR_INPUT_ERROR_MESSAGE);
+		}
 	}
 }
---
+++ src/main/java/racingcar/Validator.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.Arrays;
+
+public class Validator {
+	public static void validateCarInput(String input) {
+		String[] cars = input.split(",");
+		for (String car : cars) {
+			checkCarNameLength(car);
+		}
+	}
+
+	private static void checkCarNameLength(String name) {
+		if (name.length() > 5 || name.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
