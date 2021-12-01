f053e7179f5403e8d5db88a148eb046503df5293
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-01 18:04:46 +0900

    test(CarServiceTest) : 승자 찾기 test 추가

--- src/test/java/racingcar/CarServiceTest.java
+++ src/test/java/racingcar/CarServiceTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
@@ -15,7 +17,8 @@ import static org.mockito.Mockito.*;
 
 class CarServiceTest {
 
-    CarService carService = new CarService(CarRepository.getInstance());
+    CarRepository carRepository = CarRepository.getInstance();
+    CarService carService = new CarService(carRepository);
 
     @DisplayName("자동차 저장_성공")
     @Test
@@ -46,6 +49,26 @@ class CarServiceTest {
         assertThat(findCars.get(0).getPosition()).isEqualTo(0);
         assertThat(findCars.get(1).getPosition()).isEqualTo(1);
         assertThat(findCars.get(2).getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("승자 확인")
+    @Test
+    void find_winners() {
+        carService.saveCars("jae, hun, choi");
+        try (final MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt())).thenReturn(3, 4, 5);
+            carService.updateCarPosition();
+        }
+
+        List<Car> winners = carService.findWinners();
+        assertThat(winners.size()).isEqualTo(2);
+        assertThat(winners.get(0).getName()).isEqualTo("hun");
+        assertThat(winners.get(1).getName()).isEqualTo("choi");
+    }
+
+    @AfterEach
+    void tearDown(){
+        carRepository.clear();
     }
 
 }
