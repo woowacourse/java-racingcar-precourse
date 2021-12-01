c74b8ce66bf10c8e6104fcba10267a18b6968427
Yunkeun <yunx2@naver.com>
2021-12-02 02:22:19 +0900

    feat: 자동차 이름 입력에 대한 비정상적인 입력 예외처리

--- src/main/java/racingcar/utils/InputUtils.java
+++ src/main/java/racingcar/utils/InputUtils.java
@@ -5,12 +5,38 @@ import java.util.List;
 
 public class InputUtils {
 
+	private static final int CAR_NAME_MINIMUM_LENGTH = 0;
+	private static final int CAR_NAME_MAXIMUM_LENGTH = 5;
 	private static final String CAR_NAME_DELIMITER = ",";
+	private static final String ERROR_HEADER = "[ERROR]";
+	private static final String LENGTH_ERROR_MESSAGE = "잘못된 차량의 길이가 입력되었습니다.";
+	private static final String DUPLICATE_ERROR_MESSAGE = "중복된 차량의 이름이 입력되었습니다.";
 
 	private InputUtils() {
 	}
 
-	public static List<String> getCarNames(String carNames) {
-		return Arrays.asList(carNames.split(CAR_NAME_DELIMITER));
+	public static List<String> getCarNames (String inputCarNames) {
+		List<String> carNames = Arrays.asList(inputCarNames.split(CAR_NAME_DELIMITER));
+		validateCarNames(carNames);
+		return Arrays.asList(inputCarNames.split(CAR_NAME_DELIMITER));
+	}
+
+	public static void validateCarNames(List<String> carNames) {
+		if (!checkDuplicatedCarName(carNames)) {
+			throw new IllegalArgumentException(ERROR_HEADER + DUPLICATE_ERROR_MESSAGE);
+		}
+		for (String carName: carNames) {
+			if (!checkCarNameLength(carName)) {
+				throw new IllegalArgumentException(ERROR_HEADER + LENGTH_ERROR_MESSAGE);
+			}
+		}
+	}
+
+	public static boolean checkCarNameLength(String carName) {
+		return carName.length() > CAR_NAME_MINIMUM_LENGTH && carName.length() <= CAR_NAME_MAXIMUM_LENGTH;
+	}
+
+	public static boolean checkDuplicatedCarName(List<String> carNames) {
+		return carNames.stream().distinct().count() == carNames.size();
 	}
 }
