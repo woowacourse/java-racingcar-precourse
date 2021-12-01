1a3e7f3d96147a306b833d044c85f6dd90c022a8
TaeHyeon Kim <kth990303@naver.com>
2021-12-01 20:56:23 +0900

    feat: User 클래스 setAttemptCounts 메소드 구현

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -9,6 +9,21 @@ import java.util.stream.Collectors;
 public class User {
     static final String DIVISION_STRING = ",";
     static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String COUNTS_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public int setAttemptCounts() {
+        System.out.println(COUNTS_MESSAGE);
+        String inputCountsByClient = Console.readLine();
+        int counts;
+        try {
+            AttemptCountValidator.checkException(inputCountsByClient);
+            counts = Integer.parseInt(inputCountsByClient);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            counts = setAttemptCounts();
+        }
+        return counts;
+    }
 
     public List<String> setCarNames() {
         System.out.println(CAR_NAME_MESSAGE);
