65af317b7388b66a774b001cf4215a0a790dd8ba
이민재 <lmj938@naver.com>
2021-12-02 00:40:55 +0900

    refactor : OutputView의 기능 InputView로 이전

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,11 @@
 package racingcar.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.constant.SystemMessage;
 
+public class InputView {
+    public static String printGetCarsNameMessage(){
+        System.out.println(SystemMessage.SET_CAR_NAME_MESSAGE.print());
+        return Console.readLine();
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,7 +3,5 @@ package racingcar.view;
 import racingcar.constant.SystemMessage;
 
 public class OutputView {
-    public static void printSetCarNameMessage(){
-        System.out.println(SystemMessage.SET_CAR_NAME_MESSAGE.print());
-    }
+
 }
