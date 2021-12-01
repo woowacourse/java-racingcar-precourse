04c6d211e28f93007c3fa71690d90a913c62d5cd
devHudi <devhudi@gmail.com>
2021-12-01 18:31:12 +0900

    feat: 개별 자동차 1회 전진 기능 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,11 +1,24 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
 	private final Name name;
-	private int position = 0;
+	private Position position = new Position();
 
 	public Car(Name name) {
 		this.name = name;
+	}
+
+	public void moveForward() {
+		if (willMove()) {
+			this.position = this.position.increase();
+		}
+	}
+
+	private boolean willMove() {
+		int randomNumber = Randoms.pickNumberInRange(0, 9);
+		return randomNumber >= 4;
 	}
 }
---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.Objects;
+
+public class Position {
+	private static final int MOVE_DISTANCE = 1;
+
+	private final int position;
+
+	public Position() {
+		this.position = 0;
+	}
+
+	private Position(int position) {
+		this.position = position;
+	}
+
+	public Position increase() {
+		return new Position(this.position + MOVE_DISTANCE);
+	}
+
+	public int getPosition() {
+		return this.position;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Position otherPosition = (Position)o;
+		return position == otherPosition.position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(position);
+	}
+}
