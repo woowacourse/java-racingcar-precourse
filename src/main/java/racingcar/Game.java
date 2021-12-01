fd03439e1130cc1205c69e0271bae0afe88b6e78
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-02 03:40:33 +0900

    feat: 전체 게임 진행 기능 추가

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,11 +7,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Game {
+	private static final String GET_CARS_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String GET_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
 	public static String generateUserInput() {
 		String userInput;
 		userInput = readLine();
 
 		return userInput;
+	}
+
+	public static int getGameCount() {
+		System.out.println(GET_COUNT_MESSAGE);
+		String input = generateUserInput();
+		if (ValidationUtils.validNumber(input)) {
+			return Integer.parseInt(input);
+		}
+		return 0;
 	}
 
 	public static List<String> strToList(String str) {
@@ -23,10 +35,12 @@ public class Game {
 	}
 
 	public static Cars generateCars() {
+		System.out.println(GET_CARS_MESSAGE);
 		List<String> userInput = strToList(generateUserInput());
 		ValidationUtils.validCarList(userInput);
 		Cars carList;
 		carList = new Cars(userInput);
+
 		return carList;
 	}
 
@@ -35,12 +49,34 @@ public class Game {
 		if (Car.isForward(randomNo)) {
 			drivingCar.forward();
 		}
+		System.out.println(drivingCar.getName() + " : " + drivingCar.getScoreString());
 	}
 
-	public static void runGameOneStep() {
-		Cars carList = generateCars();
+	public static void runGameOneStep(Cars carList) {
 		for (Car car : carList.cars) {
 			runGameByCar(car);
 		}
+		System.out.println();
+	}
+
+	public static void printWinners(Cars carList) {
+		List<String> winnerList = carList.getWinner();
+		String winnerMessage = "최종 우승자 : ";
+
+		for (String winner : winnerList) {
+			winnerMessage += winner;
+			winnerMessage += ",";
+		}
+		System.out.print(winnerMessage.substring(0, winnerMessage.length() - 1));
+	}
+
+	public static void runGame() {
+		Cars carList = generateCars();
+		int count = getGameCount();
+		System.out.println("\n실행 결과");
+		for (int i = 0; i < count; i++) {
+			runGameOneStep(carList);
+		}
+		printWinners(carList);
 	}
 }
