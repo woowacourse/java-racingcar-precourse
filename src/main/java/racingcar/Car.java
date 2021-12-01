fe2c787465a704e5d01aabc1c016aa8b972f0795
xi_jjun <rlawowns97@naver.com>
2021-12-01 20:12:20 +0900

    feat: add Car method(currentPosition/go)
    README 에 명시한 기능 추가
    currentPosition() : 현재 위치정보(position)를 return.
    go() : MIN_CONDITION ~ MAX_CONDITION 사이의 값 중 무작위로 1개를 뽑아서 4이상이면 position++.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constants.Constant;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public int currentPosition() {
+		return position;
+	}
+
+	public void go() {
+		int condition = Randoms.pickNumberInRange(Constant.MIN_CONDITION, Constant.MAX_CONDITION);
+
+		if (condition >= 4) {
+			position++;
+		}
+	}
 }
