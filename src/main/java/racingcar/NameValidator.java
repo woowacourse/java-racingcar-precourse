5060f72863975326dc764187719b54f445e0f7ac
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-01 21:42:41 +0900

    refactor : CarValidator를 NameValidator, NumberValidator로 분리

---
+++ src/main/java/racingcar/NameValidator.java
@@ -0,0 +1,50 @@
+package racingcar;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static util.CarConstant.*;
+
+public class NameValidator {
+
+    public static void isRightName(String[] cars){
+        isInputBlank(cars);
+        isNameLowerThanFive(cars);
+        isDuplicateName(cars);
+    }
+
+    private static void isInputBlank(String[] cars){
+        for(int i = 0 ; i < cars.length; i++){
+            isCarNameBlank(cars[i]);
+        }
+    }
+
+    private static void isCarNameBlank(String car) {
+        for(int i = 0 ; i < car.length(); i++){
+            char digit = car.charAt(i);
+            if(!Character.isWhitespace(digit)){
+                return;
+            }
+        }
+        throw new IllegalArgumentException(ERROR_PREFIX + SPACE + NAME_NOT_BLANK);
+    }
+
+    private static void isNameLowerThanFive(String[] cars){
+        for(int i = 0 ; i < cars.length; i++){
+            if(cars[i].length() > 5){
+                throw new IllegalArgumentException(ERROR_PREFIX + SPACE + NAME_LENGTH_ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private static void isDuplicateName(String[] car){
+        Set<String> tempSet = new HashSet<>();
+        for(int i = 0 ; i < car.length; i++){
+            tempSet.add(car[i]);
+        }
+        if(tempSet.size() < car.length){
+            throw new IllegalArgumentException(ERROR_PREFIX + SPACE + NAME_DUPLICATE);
+        }
+    }
+}
--- src/main/java/racingcar/NumberValidator.java
+++ src/main/java/racingcar/NumberValidator.java
@@ -1,17 +1,8 @@
 package racingcar;
 
-
 import static util.CarConstant.*;
 
-public class CarValidator {
-
-    public static void isNameLowerThanFive(String[] input){
-        for(int i = 0 ; i < input.length; i++){
-            if(input[i].length() > 5){
-                throw new IllegalArgumentException(ERROR_PREFIX + " " + NAME_LENGTH_ERROR_MESSAGE);
-            }
-        }
-    }
+public class NumberValidator {
 
     public static void isRightTryNumber(String input){
         isNumber(input);
