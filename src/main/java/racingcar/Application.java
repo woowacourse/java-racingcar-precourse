d615b2607bed442c3fd7f3694b5560b04fd3a12c
johrae99 <70756680+johrae99@users.noreply.github.com>
2021-12-01 17:48:06 +0900

    feat : 몇번의 이동을 할것인지 시도할 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,17 +3,11 @@ package racingcar;
 public class Application {
 	public static boolean continueGame = true;
 	public static String[] carsList;
+	public static int numberOfMove;
 
 	public static void main(String[] args) {
 		InputManager inputManager = new InputManager();
-		while (continueGame) {
-			try {
-				carsList = inputManager.scanCarsList();
-				continueGame = false;
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				e.printStackTrace();
-			}
-		}
+		inputManager.scanCars();
+		inputManager.scanNumber();
 	}
 }
--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -3,6 +3,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputManager {
+	public boolean carsScan = true;
+	public boolean numberScan = true;
+
 	public String[] scanCarsList() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String carsInput = Console.readLine();
@@ -11,10 +14,54 @@ public class InputManager {
 		return carsList;
 	}
 
+	public int scanNumberOfMove() {
+		System.out.println("시도할 횟수는 몇회인가요?");
+		String numberOfMove = Console.readLine();
+		isValidNumber(numberOfMove);
+		return toInt(numberOfMove);
+	}
+
 	public void isValidInput(String[] carsList) throws IllegalArgumentException {
 		for (int i = 0; i < carsList.length; i++) {
 			if (carsList[i].length() > 5) {
 				throw new IllegalArgumentException("[ERROR] 자동차 이름을 5자리 이하로 정해주세요.");
+			}
+		}
+		carsScan = false;
+	}
+
+	public void isValidNumber(String number) throws IllegalArgumentException {
+		if (number.length() == 0) {
+			throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+		}
+		for (int i = 0; i < number.length(); i++) {
+			if (!Character.isDigit((number.charAt(i)))) {
+				throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+			}
+		}
+		numberScan = false;
+	}
+
+	public int toInt(String number) {
+		return Integer.parseInt(number);
+	}
+
+	public void scanCars() {
+		while (carsScan) {
+			try {
+				scanCarsList();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public void scanNumber() {
+		while (numberScan) {
+			try {
+				scanNumberOfMove();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
 		}
 	}
