c23195a1a365459103b2d3ef9f4d971848a7584f
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-01 18:17:39 +0900

    feat(CarConstant) : 시도횟수 에러메시지 추가

--- src/main/java/util/CarConstant.java
+++ src/main/java/util/CarConstant.java
@@ -4,9 +4,14 @@ public class CarConstant {
     public final static int MIN = 1;
     public final static int MAX = 9;
     public final static int LIMIT_SIZE_OF_NAME = 5;
+    public final static int NUMBER_MOVE_FORWARD = 4;
+    public final static int MIN_INPUT = 0;
 
     public final static String ERROR_PREFIX = "[ERROR]";
-    public final static String NUMBER_ERROR_MESSAGE = "시도 횟수는 숫자여야 한다.";
+    public final static String NAME_LENGTH_ERROR_MESSAGE = "이름은 다섯자 이하여야 한다.";
+    public final static String TRY_NUMBER_NOT_NUMBER_MESSAGE = "시도 횟수는 숫자여야 한다.";
+    public final static String NUMBER_OUT_OF_RANGE_MESSAGE = "시도 횟수는 0보다 커야 한다.";
+
     public final static String REQUEST_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public final static String REQUEST_NUMBER_TO_TRY = "시도할 회수는 몇회인가요?";
     public final static String EXECUTION_RESULT = "실행 결과";
