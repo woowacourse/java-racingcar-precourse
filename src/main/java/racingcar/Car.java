c8a0ab3b40059db57fd331020cf11c4b2740d863
skullKim <kyk990328@gmail.com>
2021-12-01 19:13:46 +0900

    feat(Car.java): 레이싱게임에서 사용할 자동차에 대한 클래스 생성
    레이싱 게임에서 사용될 각 차량의 상태를 저장하는 인스턴스를 만들기 위해
    차량 인스턴스를 만드는 클래스 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,60 @@
 package racingcar;
 
+/**
+ * <h1>레이싱 게임에서 사용할 자동차에 정보</h1>
+ *
+ * @author yunki kim
+ * @version 1.0
+ * @since 2021-12-01(V1.0)
+ */
+
 public class Car {
+
+	/** 자동차 전진 여부를 결정한다. 난수가 이 값보다 크면 전진한다 */
+	public static final Integer SHOULD_FORWARD_CAR = 3;
+
+	/** 자동차 이름 */
     private final String name;
+
+    /** 자동차의 현재 위치 */
     private int position = 0;
 
+    /**
+	 * Car 인스턴스를 생성하는 생성자
+	 *
+	 * @param name 자동차 이름
+	 */
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+	/**
+	 * 자동차 이름을 반환한다
+	 *
+	 * @return String 자동차 이름
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * 자동차의 현재 위치를 반환한다
+	 *
+	 * @return int 자동차의 현재 위치
+	 */
+	public int getPosition() {
+		return this.position;
+	}
+
+	/**
+	 * 자동차가 앞으로 전진할지 여부를 판단한다
+	 * 전진할 조건이 되면 앞으로 전진한다
+	 */
+	public void forward() {
+		final RandomNumber randomNumber = new RandomNumber();
+		if(randomNumber.getRandomNumber() <= SHOULD_FORWARD_CAR) {
+			return;
+		}
+		this.position++;
+	}
 }
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Car 클래스")
+class CarTest {
+
+	private final Car carForwardTest = new Car("car");
+
+	@DisplayName("Car 클래스 인스턴스가 제대로 생성이 되었는지 확인")
+	@ParameterizedTest(name = "{index} {displayName} carName={0}")
+	@ValueSource(strings = {"12345", "carName", "testName", "1", "str!"})
+	void makeCarInstance(final String carName) {
+		Car car = new Car(carName);
+
+		assertEquals(carName, car.getName());
+	}
+
+	@DisplayName("Car 클래스 인스턴스가 제대로 전진되었는지 확인")
+	@RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+	void forWardCar() {
+		carForwardTest.forward();
+
+		final int currentPosition = carForwardTest.getPosition();
+		final int carCurrentPosition = 0;
+
+		assertTrue(currentPosition == carCurrentPosition
+			|| currentPosition == carCurrentPosition + 1,
+			"차량의 위치는 이전과 같거나 이전보다 1 증가되야 한다");
+	}
+}
