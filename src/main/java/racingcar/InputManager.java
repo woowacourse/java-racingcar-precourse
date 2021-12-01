e618bf020a32249c0bc0af50e1f39a6607f9447f
johrae99 <70756680+johrae99@users.noreply.github.com>
2021-12-01 17:56:57 +0900

    refactor : 메소드 이름 수정 및 변수 추가

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -5,23 +5,24 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputManager {
 	public boolean carsScan = true;
 	public boolean numberScan = true;
+	public String[] carsList;
+	public int numberOfMove;
 
-	public String[] scanCarsList() {
+	public void scanCarsList() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String carsInput = Console.readLine();
-		String[] carsList = carsInput.split(",");
-		isValidInput(carsList);
-		return carsList;
+		carsList = carsInput.split(",");
+		isValidCars(carsList);
 	}
 
-	public int scanNumberOfMove() {
+	public void scanNumberOfMove() {
 		System.out.println("시도할 횟수는 몇회인가요?");
 		String numberOfMove = Console.readLine();
 		isValidNumber(numberOfMove);
-		return toInt(numberOfMove);
+		this.numberOfMove = toInt(numberOfMove);
 	}
 
-	public void isValidInput(String[] carsList) throws IllegalArgumentException {
+	public void isValidCars(String[] carsList) throws IllegalArgumentException {
 		for (int i = 0; i < carsList.length; i++) {
 			if (carsList[i].length() > 5) {
 				throw new IllegalArgumentException("[ERROR] 자동차 이름을 5자리 이하로 정해주세요.");
