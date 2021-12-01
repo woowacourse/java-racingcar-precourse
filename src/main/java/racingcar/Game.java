1a034a089af44f377c6862a5ab25cdd911041684
TaeHyeon Kim <kth990303@naver.com>
2021-12-01 20:59:11 +0900

    feat: Game 클래스 setAttemptCounts 메소드 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -6,14 +6,20 @@ import java.util.List;
 public class Game {
     private final User user;
     private final List<Car> cars;
+    private int attemptCounts;
 
     public Game() {
         user = new User();
         cars = new ArrayList<>();
+        attemptCounts = 0;
     }
 
     public void setRacingCarList() {
         user.setCarNames()
                 .forEach(carName -> cars.add(new Car(carName)));
+    }
+
+    public void setAttemptCounts() {
+        attemptCounts = user.setAttemptCounts();
     }
 }
