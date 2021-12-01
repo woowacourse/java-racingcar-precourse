a6bac4c82f97912a9a1e4882ee9fa7e933c62a2b
KIM <km0192@naver.com>
2021-12-01 20:50:26 +0900

    feat(Race) 랜덤숫자만큼 차 전진, 라운드별 출력, 리스트 반환

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.stream.IntStream;
+
+
+public class Race {
+	ArrayList<Car> carList;
+
+	ArrayList<Car> getCarList(){
+		return this.carList;
+	}
+
+	Race (ArrayList<Car> carList, int repeatNum){
+		this.carList = carList;
+
+		for (int i = 0; i < repeatNum; i++){
+			playRound();
+		}
+	}
+
+	void playRound() {
+		for (Car c : carList) {
+			c.moveIfNumGTE4(randNum());
+		}
+		printRaceProgress();
+	}
+
+	int randNum(){
+		return Randoms.pickNumberInRange(1,9);
+	}
+
+	void printRaceProgress(){
+		for (Car car : carList){
+			printCarPosition(car);
+		}
+		System.out.println();
+	}
+
+	void printCarPosition(Car car){
+		String currPosition = PositionToStr(car.getPosition());
+		String carName = car.getName();
+
+		System.out.printf("%s : %s", carName, currPosition);
+		System.out.println();
+	}
+
+	String PositionToStr(int position){
+		StringBuilder raceLine = new StringBuilder();
+
+		IntStream.range(0, position).forEach(s -> raceLine.append("-"));
+
+		return raceLine.toString();
+	}
+}
