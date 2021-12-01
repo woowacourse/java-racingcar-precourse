c3bce2d0ecdb5f30d51e8cb6602b9577bf7c7b89
iamtaehoon <kimth9981@naver.com>
2021-12-01 20:24:18 +0900

    refactor: 차이름 검증 stream으로 로직 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,24 @@
 package racingcar;
 
+import java.util.Arrays;
+
 import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
         String[] everyCarName = InputView.enterEveryCarName();
-        for (String eachCarName : everyCarName) {
-            System.out.println(eachCarName);
-            if (eachCarName.length() > 5) {
-                throw new IllegalArgumentException("차 이름이 5글자가 넘어감.");
-            }
-            if (eachCarName.isEmpty()) {
-                throw new IllegalArgumentException("차 이름 중 null값 있음");
-            }
+        Arrays.stream(everyCarName).forEach(eachCarName -> validateCarName(eachCarName));
+
+    }
+
+    private static void validateCarName(String eachCarName) {
+        System.out.println(eachCarName);
+        if (eachCarName.length() > 5) {
+            throw new IllegalArgumentException("차 이름이 5글자가 넘어감.");
+        }
+        if (eachCarName.isEmpty()) {
+            throw new IllegalArgumentException("차 이름 중 null값 있음");
         }
     }
 }
