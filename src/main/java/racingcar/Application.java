8de12604d83cb81281210b5633d661e2fbb210fe
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-01 21:12:07 +0900

    feat(Application) : 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
+        CarController carController = new CarController(new CarService(CarRepository.getInstance()));
+        carController.play();
         // TODO 구현 진행
     }
 }
