93f448b5fb66604f8010af44fedd687d5ce9b5ae
iamtaehoon <kimth9981@naver.com>
2021-12-01 20:21:19 +0900

    feat: 차이름이 5글자초과, null인 경우 예외 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,15 @@ import racingcar.view.InputView;
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
-        InputView.enterEveryCarName();
+        String[] everyCarName = InputView.enterEveryCarName();
+        for (String eachCarName : everyCarName) {
+            System.out.println(eachCarName);
+            if (eachCarName.length() > 5) {
+                throw new IllegalArgumentException("차 이름이 5글자가 넘어감.");
+            }
+            if (eachCarName.isEmpty()) {
+                throw new IllegalArgumentException("차 이름 중 null값 있음");
+            }
+        }
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,11 +7,8 @@ public class InputView {
 	public static final String INPUT_EVERY_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String CAR_SPLIT_REGEX = ",";
 
-	public static void enterEveryCarName() {
+	public static String[] enterEveryCarName() {
 		System.out.println(INPUT_EVERY_CAR_NAME_MESSAGE);
-		String[] everyCarName = Console.readLine().split(CAR_SPLIT_REGEX);
-		for (String s : everyCarName) {
-			System.out.println("s = " + s);
-		}
+		return Console.readLine().split(CAR_SPLIT_REGEX);
 	}
 }
