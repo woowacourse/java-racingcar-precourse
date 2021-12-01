e0b95855fc674ebca44382b2561f309cdb5f470a
xi_jjun <rlawowns97@naver.com>
2021-12-02 00:55:46 +0900

    feat: add name check constant

--- src/main/java/racingcar/constants/Constant.java
+++ src/main/java/racingcar/constants/Constant.java
@@ -4,4 +4,10 @@ public class Constant {
 	// === Car Random Number Range === //
 	public static final int MIN_CONDITION = 0;
 	public static final int MAX_CONDITION = 9;
+
+	// === Check Proper Name Constants === //
+	public static final int MIN_NAME_LENGTH = 1;
+	public static final int MAX_NAME_LENGTH = 5;
+	public static final int IMPROPER_CHARACTER = ',';
+	public static final String SPLIT_STRING = ",";
 }
