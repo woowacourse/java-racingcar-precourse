3aba242f0349dc4965799dd5202bfa586889ba1c
jojogreen91 <iawbg13@gmail.com>
2021-12-02 12:59:21 +0900

    feat(Car): add drive method
    - 자동차 객체가 생성된 무작위 숫자에 따라서 position 을 변경하는 기능을 구현했다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,23 @@
 package racingcar;
 
 public class Car {
-    private final String name;
-    private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	private static final int DRIVE_STANDARD_NUMBER = 4;
 
-    // 추가 기능 구현
+	private final String name;
+	private int position = 0;
+
+	private RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+	public Car(String name) {
+		this.name = name;
+	}
+
+	public void drive() {
+		int randomNumber = randomNumberGenerator.generate();
+
+		if (randomNumber >= DRIVE_STANDARD_NUMBER) {
+			position++;
+		}
+	}
 }
