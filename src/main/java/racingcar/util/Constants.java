d09dd2ef501c53b66c166116b47b3734e4696474
pup-paw <wldusdl0310@gmail.com>
2021-12-03 01:54:24 +0900

    feat: InputView 에서 사용되는 상수 처리

---
+++ src/main/java/racingcar/util/Constants.java
@@ -0,0 +1,7 @@
+package racingcar.util;
+
+public class Constants {
+	public static final String CAR_NAME_LENGTH_ERROR = "[ERROR] 자동차 이름의 길이는 1이상 5이하여야 한다.";
+	public static final String ATTEMPTS_NUMBER_TYPE_ERROR = "[ERROR] 시도 횟수는 숫자여야 한다.";
+	public static final int CAR_NAME_MAX_LENGTH = 5;
+}
