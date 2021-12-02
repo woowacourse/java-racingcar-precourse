719cac8740185f3f1b324322c0bb36d4ea91f5d0
nailseong <nnis9857@gmail.com>
2021-12-02 22:54:24 +0900

    feat: 자동차 이름을 입력 받기

---
+++ src/main/java/racingcar/InputReader.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+import static racingcar.Validator.*;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputReader {
+
+	public static String[] returnCarNameList() {
+		String[] nameList = new String[0];
+
+		boolean isValid = false;
+		while (!isValid) {
+			String input = Console.readLine();
+			nameList = input.split(",", -1);
+
+			try {
+				isValidCarNameList(nameList);
+				isValid = true;
+			} catch (IllegalArgumentException exception) {
+				// TODO: 2021/12/02 메시지 출력 메서드로 추출
+				System.out.println("[ERROR] " + exception.getMessage());
+			}
+		}
+
+		return nameList;
+	}
+
+}
