486843b8e14042f65e908a6e6193774b214f189d
Jihyun <bjih1999@naver.com>
2021-12-02 17:22:34 +0900

    refactor(GameExecutor): 자동차 생성 로직 통합
    - getCarNames의 접근 지정자를 private으로 변경
    - getCarNames를 createCars에서 내부적으로 사용하도록 변경

--- src/main/java/racingcar/GameExecutor.java
+++ src/main/java/racingcar/GameExecutor.java
@@ -8,14 +8,17 @@ import java.util.List;
 
 public class GameExecutor {
 
-    public List<String> getCarNames() {
+    public GameExecutor() {}
+
+    private List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = Console.readLine().trim().split(",");
 
         return new ArrayList<>(Arrays.asList(carNames));
     }
 
-    public List<Car> createCars(List<String> carNameList) {
+    public List<Car> createCars() {
+        List<String> carNameList = getCarNames();
         List<Car> carList = new ArrayList<>();
 
         for (String name : carNameList) {
