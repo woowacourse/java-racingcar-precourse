df19a870823e3321b1bc5495cccd8ea3f810407c
nailseong <nnis9857@gmail.com>
2021-12-03 05:04:10 +0900

    feat: 시도할 횟수를 묻는 메시지를 출력

--- src/main/java/racingcar/MessagePrinter.java
+++ src/main/java/racingcar/MessagePrinter.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class MessagePrinter {
 
+	public static final String ASK_FOR_REPETITIONS = "시도할 회수는 몇회인가요?";
 	private static final String ASK_FOR_CAR_NAME_LIST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
 	public static void printError(IllegalArgumentException exception) {
@@ -10,6 +11,10 @@ public class MessagePrinter {
 
 	public static void askCarNameList() {
 		System.out.println(ASK_FOR_CAR_NAME_LIST);
+	}
+
+	public static void askForRepetitions() {
+		System.out.println(ASK_FOR_REPETITIONS);
 	}
 
 }
