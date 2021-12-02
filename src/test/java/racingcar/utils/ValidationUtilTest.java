3014a83b6589373e9ab898de37f933b23a8f24cd
yoon <duddbs774@gmail.com>
2021-12-03 03:15:39 +0900

    test: 자동차 이름 유효성 검사 테스트 케이스 이동
    racingcar/utils/ValidationUtilTest 생성 및 자동차 이름 유효성 검사
    테스트 케이스 추가
    racingcar/domain/CarsTest 에서 진행했던 테스트 케이스 일부를 이동 시킴

---
+++ src/test/java/racingcar/utils/ValidationUtilTest.java
@@ -0,0 +1,59 @@
+package racingcar.utils;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ValidationUtilTest {
+    @DisplayName("null값 예외발생 테스트")
+    @Test
+    void checkNull() {
+        assertThatThrownBy(() -> {
+            ValidationUtil.checkNull(null);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("빈값 예외발생 테스트")
+    @Test
+    void checkEmpty() {
+        assertThatThrownBy(() -> {
+            ValidationUtil.checkEmpty("");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("6자리 이상의 값 예외발생 테스트")
+    @Test
+    void checkInvalidLength() {
+        assertThatThrownBy(() -> {
+            ValidationUtil.checkLength("123456");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1자리 이상 5자리 이하의 값 통과 테스트")
+    @Test
+    void checkValidLength() {
+        ValidationUtil.checkLength("1");
+        ValidationUtil.checkLength("12345");
+    }
+
+    @DisplayName("공백을 제외한 첫자리가 쉼표일 경우 예외발생 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {",", " , ", " , 12345"})
+    void checkCommaInFirst(String value) {
+        assertThatThrownBy(() -> {
+            ValidationUtil.checkCommaInFirst(value);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("쉼표를 기준으로 나눈값 중 중복문자가 있을경우 예외발생 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,1", "1,2,1", "123, 123"})
+    void checkDuplicate(String value) {
+        assertThatThrownBy(() -> {
+            ValidationUtil.checkDuplicate(value);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
