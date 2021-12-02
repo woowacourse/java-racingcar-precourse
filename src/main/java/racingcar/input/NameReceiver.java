5c47bd6a56d69237620b045de0cffd7f28ef2c5f
jojogreen91 <iawbg13@gmail.com>
2021-12-02 18:17:33 +0900

    refactor: refactor NameReceiver & NameValidator class

--- src/main/java/racingcar/input/NameReceiver.java
+++ src/main/java/racingcar/input/NameReceiver.java
@@ -8,7 +8,6 @@ import racingcar.validator.NameValidator;
 public class NameReceiver {
 
 	private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	private static final String NAME_ERROR_MESSAGE = "[ERROR] 적절하지 않은 자동차 이름 입력되었습니다.";
 
 	NameValidator nameValidator = new NameValidator();
 
@@ -20,24 +19,18 @@ public class NameReceiver {
 			System.out.println(INPUT_NAME_MESSAGE);
 			String nameString = Console.readLine();
 
-			nameList = new ArrayList<>();
-			nameList = makeNameList(nameString, nameList);
+			nameList = makeNameList(nameString);
 
-			try {
-				nameValidator.validate(nameList);
-				errorSwitch = false;
-			} catch (IllegalArgumentException illegalArgumentException) {
-				System.out.println(NAME_ERROR_MESSAGE);
-				errorSwitch = true;
-			}
+			errorSwitch = nameValidator.validate(nameList);
 		}
 
 		return nameList;
 	}
 
-	private ArrayList<String> makeNameList(String nameString, ArrayList<String> nameList) {
-		String[] nameArr = nameString.split(",");
-		for (String str : nameArr) {
+	private ArrayList<String> makeNameList(String nameString) {
+		ArrayList<String> nameList = new ArrayList<>();
+
+		for (String str : nameString.split(",")) {
 			nameList.add(str);
 		}
 
--- src/main/java/racingcar/validator/NameValidator.java
+++ src/main/java/racingcar/validator/NameValidator.java
@@ -4,12 +4,22 @@ import java.util.ArrayList;
 
 public class NameValidator {
 
+	private static final String NAME_ERROR_MESSAGE = "[ERROR] 적절하지 않은 자동차 이름 입력되었습니다.";
+
 	private static final int NAME_LENGTH_MAX = 5;
 	private static final int NAME_LENGTH_MIN = 1;
 
-	public void validate(ArrayList<String> nameList) throws IllegalArgumentException {
-		for (String name : nameList) {
-			keepLengthLimit(name);
+	public boolean validate(ArrayList<String> nameList) {
+		try {
+			for (String name : nameList) {
+				keepLengthLimit(name);
+			}
+
+			return false;
+		} catch (IllegalArgumentException illegalArgumentException) {
+			System.out.println(NAME_ERROR_MESSAGE);
+
+			return true;
 		}
 	}
 
