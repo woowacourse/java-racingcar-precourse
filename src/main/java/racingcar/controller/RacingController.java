d2a0f2fa49f64f706be202613010b868028798e9
RIANAEH <ririhan217@gmail.com>
2021-12-02 16:20:59 +0900

    feat(racingController): add start racing
    자동차 경주를 전반적으로 실행하는 컨트롤러를 추가하였습니다. 리팩토링이 많이 필요한 부분이라고 생각합니다.

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,91 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.model.Car;
+import racingcar.model.CarName;
+import racingcar.utils.InputTryCountValidator;
+import racingcar.model.Winner;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+	private final InputView inputView;
+	private final OutputView outputView;
+	private String inputCarNames;
+	private String[] carNames;
+	private List<Car> carList;
+	private String inputTryCount;
+	private int tryCount;
+
+	public RacingController() {
+		inputView = new InputView();
+		outputView = new OutputView();
+	}
+
+	public void startRacing() {
+		getCarNames();
+		getTryCount();
+		createCars();
+
+		outputView.printResultMessage();
+		for (int i = 0; i < tryCount; i++) {
+			moveCars();
+			outputView.printResult(carList);
+		}
+
+		Winner winner = new Winner(carList);
+		String[] winners = winner.getWinners();
+		outputView.printWinners(winners);
+	}
+
+	private void getCarNames() {
+		do {
+			inputCarNames = inputView.getInputCarNames();
+		} while (!handleGetCarNamesException());
+	}
+
+	private boolean handleGetCarNamesException() {
+		try {
+			CarName carName = new CarName(inputCarNames);
+			carNames = carName.getCarNames();
+			return true;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
+	private void getTryCount() {
+		do {
+			inputTryCount = inputView.getInputTryCount();
+		} while (!handleGetTryCountException());
+
+		tryCount = Integer.parseInt(inputTryCount);
+	}
+
+	private boolean handleGetTryCountException() {
+		try {
+			InputTryCountValidator inputTryCountValidator = new InputTryCountValidator(inputTryCount);
+			inputTryCountValidator.validateInputTryCount();
+			return true;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
+	private void createCars() {
+		carList = new ArrayList<>();
+		for (String carName : carNames) {
+			carList.add(new Car(carName));
+		}
+	}
+
+	private void moveCars() {
+		for (Car car : carList) {
+			car.moveForward();
+		}
+	}
+}
