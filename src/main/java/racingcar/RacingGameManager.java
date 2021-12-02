80a7dc75ad3a62ab0a9bc2fa4e969e6fb4672286
wilgur513 <wilgur513@naver.com>
2021-12-02 17:28:56 +0900

    feat: implement RacingGameManager using other modules

--- src/main/java/racingcar/RacingGameManager.java
+++ src/main/java/racingcar/RacingGameManager.java
@@ -1,16 +1,31 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.model.ProceedResult;
+import racingcar.model.RacingGame;
 import racingcar.model.RacingInfo;
 import racingcar.reader.RacingInfoReader;
+import racingcar.writer.RacingWriter;
 
 public class RacingGameManager {
 	private final RacingInfoReader reader;
+	private final RacingWriter writer;
 
-	public RacingGameManager(RacingInfoReader reader) {
+	public RacingGameManager(RacingInfoReader reader, RacingWriter writer) {
 		this.reader = reader;
+		this.writer = writer;
 	}
 
 	public void run() {
 		RacingInfo info = reader.read();
+		RacingGame racingGame = RacingGame.of(info);
+
+		while (!racingGame.isFinished()) {
+			List<ProceedResult> proceedResults = racingGame.proceed();
+			writer.writeProceedResults(proceedResults);
+		}
+
+		List<String> winners = racingGame.getWinners();
+		writer.writeWinners(winners);
 	}
 }
