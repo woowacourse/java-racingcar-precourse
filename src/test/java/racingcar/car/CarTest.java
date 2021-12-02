678bd4f8392a3bfa8f21e32fd92ef15b1fefffb7
hyeonic <dev.hyeonic@gmail.com>
2021-12-02 20:35:46 +0900

    test: Car 전진 및 정지를 담당하는 move 검증을 위한 테스트 추가

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -3,6 +3,7 @@ package racingcar.car;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,9 +13,12 @@ class CarTest {
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "woni", "jun"})
     void constructor_nameLessThen5_create(String name) {
-        // given & when & then
+        // given
+        MovingPolicy randomMovingPolicy = new RandomMovingPolicy();
+
+        //  when & then
         assertThatCode(() -> {
-            new Car(name);
+            new Car(name, randomMovingPolicy);
         }).doesNotThrowAnyException();
     }
 
@@ -22,9 +26,40 @@ class CarTest {
     @ParameterizedTest
     @ValueSource(strings = {"pobiii", "woniii", "junnnn"})
     void constructor_nameMoreThen5_ExceptionThrown(String name) {
-        // given & when & then
+        // given
+        MovingPolicy randomMovingPolicy = new RandomMovingPolicy();
+
+        //  when & then
         assertThatThrownBy(() -> {
-            new Car(name);
+            new Car(name, randomMovingPolicy);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("movingPolicy의 isMoving이 true이면 position이 1 증가한다.")
+    @Test
+    void move_isMovingThenTrue_increasePosition() {
+        // given
+        MovingPolicy movingPolicy = () -> true;
+        Car car = new Car("pobi", movingPolicy);
+
+        // when
+        car.move();
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("movingPolicy의 isMoving이 false이면 position은 정지한다.")
+    @Test
+    void move_isMovingThenFalse_stopPosition() {
+        // given
+        MovingPolicy movingPolicy = () -> false;
+        Car car = new Car("pobi", movingPolicy);
+
+        // when
+        car.move();
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
