af4f0b94c6968fff281c9d39d1ba841f6e8df6a6
0chil <0@chll.it>
2021-12-02 19:32:14 +0900

    feat(Application): add game execution
    add loops for getting valid input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,42 @@
 package racingcar;
 
+import java.util.List;
+
+import racingcar.domain.Car;
+import racingcar.domain.Game;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		runGame();
+	}
+
+	public static void runGame() {
+		List<Car> userInputNameList = loopInputNameListUntilValid();
+		Game game = new Game(userInputNameList);
+		game.runMultiple(loopInputIterationNumberUntilValid());
+
+		OutputView.printGameResult(game);
+	}
+
+	private static List<Car> loopInputNameListUntilValid() {
+		while (true) {
+			try {
+				return InputView.inputNameList();
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	private static int loopInputIterationNumberUntilValid() {
+		while (true) {
+			try {
+				return InputView.inputIterationNumber();
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 }
