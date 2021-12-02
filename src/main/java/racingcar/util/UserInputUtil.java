d8016441b81201367abb3dcf73dc01d6f67fb717
Donggyu Kim <a29661498@gmail.com>
2021-12-02 16:09:06 +0900

    feat: 경주에 참여할 자동차 이름 입력 받는 기능 구현
    자동차 이름을 입력받고 이를 ','로 구분하도록 하는 메소드 getCarNames()를 구현하였습니다.
    해당 메소드 내부에서는 앞서 만든 checkCarName()을 통해서 각 자동차 이름 길이를 검사합니다.

--- src/main/java/racingcar/util/UserInputUtil.java
+++ src/main/java/racingcar/util/UserInputUtil.java
@@ -1,8 +1,22 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserInputUtil {
 
     private UserInputUtil() {
+    }
+
+    public static String[] getCarNames() {
+        String input = Console.readLine();
+
+        String[] carNames = input.split(",");
+
+        for(int i = 0; i < carNames.length; i++) {
+            checkCarName(carNames[i]);
+        }
+
+        return carNames;
     }
 
     private static void checkCarName(String input) {
