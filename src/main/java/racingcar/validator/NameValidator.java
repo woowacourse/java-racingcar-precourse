0dbacd8bbfde7af676982685a6eb6a9d99f66982
jojogreen91 <iawbg13@gmail.com>
2021-12-02 19:16:42 +0900

    feat(NameValidator): add keepUniqueName method
    - 중복되는 이름이 존재하는 경우 에러메시지를 출력하고 다시 입력을 받는 기능을 구현했다.

--- src/main/java/racingcar/validator/NameValidator.java
+++ src/main/java/racingcar/validator/NameValidator.java
@@ -1,6 +1,7 @@
 package racingcar.validator;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class NameValidator {
 
@@ -9,11 +10,13 @@ public class NameValidator {
 
 	private static final String LENGTH_LIMIT_ERROR_MESSAGE = "[ERROR] 이름의 길이는 1 ~ 5자 까지만 허용합니다.";
 	private static final String NOT_ONLY_BLANK_ERROR_MESSAGE = "[ERROR] 이름이 공백으로만 이루어져 있으면 안 됩니다.";
+	private static final String UNIQUE_NAME_ERROR_MESSAGE = "[ERROR] 중복되는 이름이 존재하면 안 됩니다.";
 
 	private String errorMessage = null;
 
 	public boolean validate(ArrayList<String> nameList) {
 		try {
+			keepUniqueName(nameList);
 			for (String name : nameList) {
 				keepNotOnlyBlank(name);
 				keepLengthLimit(name);
@@ -38,6 +41,16 @@ public class NameValidator {
 	private void keepNotOnlyBlank(String name) throws IllegalArgumentException {
 		if (name.trim().length() == 0) {
 			errorMessage = NOT_ONLY_BLANK_ERROR_MESSAGE;
+
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void keepUniqueName(ArrayList<String> nameList) throws IllegalArgumentException {
+		int distinctListSize = nameList.stream().distinct().collect(Collectors.toList()).size();
+
+		if (nameList.size() != distinctListSize) {
+			errorMessage = UNIQUE_NAME_ERROR_MESSAGE;
 
 			throw new IllegalArgumentException();
 		}
