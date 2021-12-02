e09ab6a68691217ff0b017acf077e7df9fdcd913
EastHShin <xldk78@gmail.com>
2021-12-03 03:28:08 +0900

    feat(CarNameInput): 자동차의 이름을 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.utils.CarNameInput;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		// TODO 구현 진행
+
+	}
 }
---
+++ src/main/java/racingcar/utils/CarNameInput.java
@@ -0,0 +1,48 @@
+package racingcar.utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Car;
+
+public class CarNameInput {
+	private static final int NAME_LIMIT_LENGTH = 5;
+
+	public void getCar() {
+		String readCars = Console.readLine();
+		String[] carArray = readCars.split(",");
+
+		if (isEmptyName(carArray)) {
+			throw new IllegalArgumentException("[ERROR] 자동차의 이름이 빈 문자열이어서는 안된다.");
+		}
+
+		if (isInvalidLength(carArray)) {
+			throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5글자 이하여야 한다.");
+		}
+
+		if (existsDuplicateName(carArray)) {
+			throw new IllegalArgumentException("[ERROR] 자동차에 중복된 이름이 포함되면 안된다.");
+		}
+
+	}
+
+	private boolean isEmptyName(String[] carArray) {
+		return !Arrays.stream(carArray)
+			.allMatch(car -> car.length() > 0);
+	}
+
+	private boolean isInvalidLength(String[] carArray) {
+		return !Arrays.stream(carArray)
+			.allMatch(car -> car.length() < NAME_LIMIT_LENGTH);
+	}
+
+	private boolean existsDuplicateName(String[] carArray) {
+		return !Arrays.stream(carArray)
+			.allMatch(new HashSet<>()::add);
+	}
+
+}
