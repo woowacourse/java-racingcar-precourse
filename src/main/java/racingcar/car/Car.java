eb4ebd6b48fa0140dc04fe97625679deb326a6ee
hyeonic <dev.hyeonic@gmail.com>
2021-12-02 21:44:48 +0900

    feat: Car 차수별 실행 결과 및 우승자 조회를 위한 메서드 추가

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -3,6 +3,9 @@ package racingcar.car;
 public class Car {
     private static final int DEFAULT_POSITION = 0;
     private static final int NAME_MAX_LENGTH = 5;
+    private static final String NAME_MESSAGE_FORMAT = "%s : ";
+    private static final int ZERO_INDEX = 0;
+    private static final String MOVING_STICK = "-";
 
     private final String name;
     private final MovingPolicy movingPolicy;
@@ -24,13 +27,32 @@ public class Car {
         }
     }
     
-    public void move() {
+    protected void move() {
         if (movingPolicy.isMoving()) {
             position++;
         }
     }
 
+    protected String getStateMessage() {
+        StringBuilder stringBuilder = new StringBuilder(String.format(NAME_MESSAGE_FORMAT, name));
+        for (int i = ZERO_INDEX; i < position; i++) {
+            stringBuilder.append(MOVING_STICK);
+        }
+        return stringBuilder.toString();
+    }
+
+    protected String getName() {
+        return name;
+    }
+
     protected int getPosition() {
         return position;
+    }
+
+    protected boolean isSamePosition(int maxPosition) {
+        if (this.position == maxPosition) {
+            return true;
+        }
+        return false;
     }
 }
