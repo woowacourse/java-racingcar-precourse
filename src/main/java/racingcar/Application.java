76afa731b89a598ecdf12850c8633b40a3949a3d
Donggyu Kim <a29661498@gmail.com>
2021-12-02 17:18:54 +0900

    feat: Application.java main()에 RacingGame 객체 생성 후 startGame() 호출
    프로그램의 시작점에 해당하는 main() 메소드에서 Baseball 객체 생성 이후 startGame() 메소드 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.game.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        RacingGame racingGame = new RacingGame();
+        racingGame.startGame();
     }
 }
