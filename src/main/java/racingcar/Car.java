b43fc163d89ac8213161375a043ebf253a883ffe
hyewoncc <hyewoncc@gmail.com>
2021-12-03 00:17:07 +0900

    feat: 랜덤값에 따라 Car 전진
    랜덤값을 구하고, 특정값 이상 시 전진(위치값 변경)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+	private static final int RANDOM_RANGE_START = 0;
+	private static final int RANDOM_RANGE_END = 9;
+	private static final int MINIMUM_MOVABLE_INT = 4;
+
 	private final String name;
 	private int position = 0;
 
@@ -8,5 +14,17 @@ public class Car {
 		this.name = name;
 	}
 
-	// 추가 기능 구현
+	public void goForwardByRandom() {
+		if (isMovable()) {
+			position += 1;
+		}
+	}
+
+	private boolean isMovable() {
+		int pick = Randoms.pickNumberInRange(RANDOM_RANGE_START, RANDOM_RANGE_END);
+		if (pick >= MINIMUM_MOVABLE_INT) {
+			return true;
+		}
+		return false;
+	}
 }
