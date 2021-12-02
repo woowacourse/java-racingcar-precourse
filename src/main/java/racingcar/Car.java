3fe4959dbb115792ab0e4404e43830138f6dc303
kanamycine <jinyeong5320@gmail.com>
2021-12-02 18:43:09 +0900

    feat(Car) : 매 턴마다, 진행 or 멈춤 기능 구현 feat(Car) : 진행 경과 출력 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,29 @@
 package racingcar;
 
+import racingcar.utils.RandomGenerator;
+import racingcar.utils.Constant;
+
 public class Car {
 	private final String driverName;
 	private int position = 0;
 
 	public Car(String driverName) {
 		this.driverName = driverName;
+	}
+
+	public void playTurn(){
+		RandomGenerator randomGenerator = new RandomGenerator();
+
+		if ( randomGenerator.getRandomNum() >= Constant.MOVE_VALUE){
+			this.position++;
+		}
+		this.printPosition();
+	}
+
+	public void printPosition(){
+		System.out.println(this.driverName + " : ");
+		for(int i = 0; i < position; i++) {
+			System.out.println("-");
+		}
 	}
 }
