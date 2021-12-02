a1c66b891f1a1b83fb183f29bfc42a42a6ee9b47
nailseong <nnis9857@gmail.com>
2021-12-03 06:16:53 +0900

    feat(Game): 기본 생성자가 멤버변수 numberOfRepetitions를 초기화

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,18 +1,26 @@
 package racingcar;
 
+import static racingcar.InputReader.*;
+import static racingcar.MessagePrinter.*;
+
 public class Game {
 
 	private final Car[] cars;
 	private final int numberOfCars;
+	private final int numberOfRepetitions;
 
 	public Game() {
-		String[] nameList = InputReader.returnCarNameList();
+		askCarNameList();
+		String[] nameList = returnCarNameList();
 		numberOfCars = nameList.length;
 		cars = new Car[numberOfCars];
 
 		for (int i = 0; i < numberOfCars; i++) {
 			cars[i] = new Car(nameList[i]);
 		}
+
+		askForRepetitions();
+		numberOfRepetitions = returnNumberOfRepetitions();
 	}
 
 }
