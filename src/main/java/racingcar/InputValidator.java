99df01f49e01644d787e0f2fe477891094785284
lymchgmk <lymchgmk@gmail.com>
2021-12-02 17:48:22 +0900

    fix (convention) : tap을 4 spaces로 reformat
    Tap을 그대로 사용하며 Code convention을 지키지 않아, 이를 4 spaces로 변환하여 고쳤습니다.

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -5,26 +5,26 @@ import java.util.regex.Pattern;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputValidator {
-	public String[] carNames() {
-		String userInput = Console.readLine();
-		String[] names = userInput.split(",");
-		String pattern = "^(\\w){1,5}$";
-		for (String name : names) {
-			boolean regex = Pattern.matches(pattern, name);
-			if (!regex) {
-				throw new IllegalArgumentException(Constants.ERROR + Constants.WRONG_CAR_NAMES);
-			}
-		}
-		return names;
-	}
+    public String[] carNames() {
+        String userInput = Console.readLine();
+        String[] names = userInput.split(",");
+        String pattern = "^(\\w){1,5}$";
+        for (String name : names) {
+            boolean regex = Pattern.matches(pattern, name);
+            if (!regex) {
+                throw new IllegalArgumentException(Constants.ERROR + Constants.WRONG_CAR_NAMES);
+            }
+        }
+        return names;
+    }
 
-	public int playTimes() {
-		String userInput = Console.readLine();
-		String pattern = "^[1-9]+[0-9]*$";
-		boolean regex = Pattern.matches(pattern, userInput);
-		if (!regex) {
-			throw new IllegalArgumentException(Constants.ERROR + Constants.WRONG_PLAY_TIMES);
-		}
-		return Integer.parseInt(userInput);
-	}
+    public int playTimes() {
+        String userInput = Console.readLine();
+        String pattern = "^[1-9]+[0-9]*$";
+        boolean regex = Pattern.matches(pattern, userInput);
+        if (!regex) {
+            throw new IllegalArgumentException(Constants.ERROR + Constants.WRONG_PLAY_TIMES);
+        }
+        return Integer.parseInt(userInput);
+    }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,98 +4,98 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class RacingGame {
-	String[] carNames;
-	Car[] cars;
-	int playTimes;
+    String[] carNames;
+    Car[] cars;
+    int playTimes;
 
-	public void start() {
-		inputCarNames();
-		readyCars(carNames);
+    public void start() {
+        inputCarNames();
+        readyCars(carNames);
 
-		inputPlayTimes();
+        inputPlayTimes();
 
-		printRaceResults(cars, playTimes);
+        printRaceResults(cars, playTimes);
 
-		printFinalWinnersName(cars);
-	}
+        printFinalWinnersName(cars);
+    }
 
-	private void printInputCarNames() {
-		System.out.println(Constants.INPUT_CAR_NAMES_MSG);
-	}
+    private void printInputCarNames() {
+        System.out.println(Constants.INPUT_CAR_NAMES_MSG);
+    }
 
-	private void printInputPlayTimes() {
-		System.out.println(Constants.INPUT_PLAY_TIMES_MSG);
-	}
+    private void printInputPlayTimes() {
+        System.out.println(Constants.INPUT_PLAY_TIMES_MSG);
+    }
 
-	private void printRaceResults(Car[] cars, int playTimes) {
-		System.out.println();
-		System.out.println(Constants.RACE_RESULT_MSG);
+    private void printRaceResults(Car[] cars, int playTimes) {
+        System.out.println();
+        System.out.println(Constants.RACE_RESULT_MSG);
 
-		for (int times = 0; times < playTimes; times++) {
+        for (int times = 0; times < playTimes; times++) {
 
-			for (Car car: cars) {
-				car.race();
-				String carName = car.getName();
-				String carPosMsg = String.join("", Collections.nCopies(car.getPosition(), "-"));
-				System.out.println(carName + " : " + carPosMsg);
-			}
+            for (Car car : cars) {
+                car.race();
+                String carName = car.getName();
+                String carPosMsg = String.join("", Collections.nCopies(car.getPosition(), "-"));
+                System.out.println(carName + " : " + carPosMsg);
+            }
 
-			System.out.println();
-		}
-	}
+            System.out.println();
+        }
+    }
 
-	private void printFinalWinnersName(Car[] cars) {
-		ArrayList<String> finalWinners = new ArrayList<String>();
-		int winnerPos = winnersPosition(cars);
+    private void printFinalWinnersName(Car[] cars) {
+        ArrayList<String> finalWinners = new ArrayList<String>();
+        int winnerPos = winnersPosition(cars);
 
-		for (Car car : cars) {
-			if (car.getPosition() == winnerPos) {
-				finalWinners.add(car.getName());
-			}
-		}
+        for (Car car : cars) {
+            if (car.getPosition() == winnerPos) {
+                finalWinners.add(car.getName());
+            }
+        }
 
-		String finalWinnersMsg = Constants.FINAL_WINNERS_MSG + " : " + String.join(", ", finalWinners);
-		System.out.println(finalWinnersMsg);
-	}
+        String finalWinnersMsg = Constants.FINAL_WINNERS_MSG + " : " + String.join(", ", finalWinners);
+        System.out.println(finalWinnersMsg);
+    }
 
-	private void inputCarNames() {
-		printInputCarNames();
+    private void inputCarNames() {
+        printInputCarNames();
 
-		try {
-			carNames = new InputValidator().carNames();
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-			inputCarNames();
-		}
-	}
+        try {
+            carNames = new InputValidator().carNames();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            inputCarNames();
+        }
+    }
 
-	private void readyCars(String[] carNames) {
-		cars = new Car[carNames.length];
-		for (int i = 0; i < carNames.length; i++) {
-			cars[i] = new Car(carNames[i]);
-		}
-	}
+    private void readyCars(String[] carNames) {
+        cars = new Car[carNames.length];
+        for (int i = 0; i < carNames.length; i++) {
+            cars[i] = new Car(carNames[i]);
+        }
+    }
 
-	private void inputPlayTimes() {
-		printInputPlayTimes();
+    private void inputPlayTimes() {
+        printInputPlayTimes();
 
-		try {
-			playTimes = new InputValidator().playTimes();
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-			inputPlayTimes();
-		}
-	}
+        try {
+            playTimes = new InputValidator().playTimes();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            inputPlayTimes();
+        }
+    }
 
-	private int winnersPosition(Car[] cars) {
-		int maxPos = 0;
+    private int winnersPosition(Car[] cars) {
+        int maxPos = 0;
 
-		for (Car car : cars) {
-			if (maxPos < car.getPosition()) {
-				maxPos = car.getPosition();
-			}
-		}
+        for (Car car : cars) {
+            if (maxPos < car.getPosition()) {
+                maxPos = car.getPosition();
+            }
+        }
 
-		return maxPos;
-	}
+        return maxPos;
+    }
 }
