22ca2de4c89709c34c4d6c4bcb2fa82e279a716a
Donggyu Kim <a29661498@gmail.com>
2021-12-02 16:37:02 +0900

    feat: 자동차의 이동 여부 판별 기능 구현
    이전에 public으로 선언하였던 RandomUtil의 getRandomNumber()를 private로 변경하고,
    checkMoveOrWait()메소드를 정의하여 자동차가 이동할지 말지에 대한 여부를 boolean형으로 반환해주는 메소드를 정의하였습니다.

--- src/main/java/racingcar/util/RandomUtil.java
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -7,7 +7,17 @@ public class RandomUtil {
     private RandomUtil() {
     }
 
-    public static int getRandomNumber() {
+    public static boolean checkMoveOrWait() {
+        int randomNumber = getRandomNumber();
+
+        if(randomNumber >= 4) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int getRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
 }
