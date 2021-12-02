96e44e68a62b92cdae434deea06f5bb6a66f92ff
jongwon <54282927+loosie@users.noreply.github.com>
2021-12-02 16:08:18 +0900

    feat(Output): 메서드 역핣 분리 및 변수 상수화
    - 메서드 역할 분리
    - String message 재생성 방지를 위해 constant pool에 등록

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -7,12 +7,11 @@ import java.util.List;
 import org.assertj.core.util.Lists;
 
 public class Output {
+	private static final String RESULT_MESSAGE = "실행 결과";
+	private static final String WINNER_MESSAGE = "최종 우승자 : ";
 
 	public static void outputCarPosition(Car[] cars) {
-		for (Car car : cars) {
-			System.out.println(car.toString());
-		}
-		System.out.println();
+		printCarStatus(cars);
 	}
 
 	public static void outputRaceResult(Car[] cars) {
@@ -20,17 +19,34 @@ public class Output {
 		sortingByPosition(carList);
 
 		StringBuilder sb = new StringBuilder();
-		System.out.println("실행 결과");
+		System.out.println(RESULT_MESSAGE);
 
+		getCarPositionPerRace(carList, sb);
+
+		System.out.println(WINNER_MESSAGE + trimResult(sb));
+	}
+
+	private static void printCarStatus(Car[] cars) {
+		StringBuilder sb = new StringBuilder();
+		for (Car car : cars) {
+			sb.append(car.toString()+"\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static void getCarPositionPerRace(List<Car> carList, StringBuilder sb) {
 		int max = carList.get(0).getPosition();
 		for (Car car : carList) {
 			if (max == car.getPosition()) {
 				sb.append(car.getName() + ", ");
 			}
 		}
+	}
 
+	private static String trimResult(StringBuilder sb) {
 		String result = sb.toString();
-		System.out.println("최종 우승자 : " + result.substring(0, result.length() - 2));
+		result = result.substring(0, result.length() - 2);
+		return result;
 	}
 
 }
