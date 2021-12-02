6dbdc8630a5f978aa13ef93a0558819f8e212a2c
BETTERFUTURE4 <bumkyu11@naver.com>
2021-12-02 20:56:43 +0900

    feat(Constants): 상수 클래스 구현

---
+++ src/main/java/racingcar/model/Constants.java
@@ -0,0 +1,15 @@
+package racingcar.model;
+
+public class Constants {
+	public static final int NAME_SIZE = 5;
+	public static final int MIN_RANDOM_NUM = 0;
+	public static final int MAX_RANDOM_NUM = 9;
+	public static final int GO_NUM = 4;
+	public static final String DELIMITER = ",";
+	public static final String ROUND_RESULT = "\n실행 결과";
+	public static final String CAR_NAME_WRONG_ERROR = "[ERROR] 자동차 목록은 쉼표로 구분된 비어있지 않은 문자여야 한다.";
+	public static final String CAR_NAME_SIZE_ERROR = "[ERROR] 자동차 목록은 1자~5자의 문자로 구성되어야 한다.";
+	public static final String CAR_NAME_DUPLICATE_ERROR = "[ERROR] 자동차 목록은 중복이 없어야 한다.";
+	public static final String ROUND_NUM_WRONG_ERROR = "[ERROR] 시도 횟수는 1 이상의 숫자여야 한다.";
+
+}
