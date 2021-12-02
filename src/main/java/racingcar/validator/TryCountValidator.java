095c7194e4943cc68f7afc76cbcc7505b072f311
jojogreen91 <iawbg13@gmail.com>
2021-12-02 23:12:30 +0900

    feat(TryCountValidator): add keepDigitValue method
    - 입력받은 시도 횟수의 값이 자연수가 아닌경우 에러 메시지를 출력하고 다시 입력을 받는 기능을 구현했다.

--- src/main/java/racingcar/validator/TryCountValidator.java
+++ src/main/java/racingcar/validator/TryCountValidator.java
@@ -3,12 +3,14 @@ package racingcar.validator;
 public class TryCountValidator {
 
 	private static final String DIGIT_VALUE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 단 하나의 올바른 숫자만 허용합니다.";
+	private static final String EXCEPT_ZERO_ERROR_MESSAGE = "[ERROR] 시도 횟수는 자연수만 가능합니다.";
 
 	private String errorMessage = null;
 
 	public boolean validate(String tryCountString) {
 		try {
 			keepDigitValue(tryCountString);
+			keepDigitExceptZero(tryCountString);
 
 			// 공백 라인 출력
 			System.out.println();
@@ -30,6 +32,15 @@ public class TryCountValidator {
 
 				throw new IllegalArgumentException();
 			}
+		}
+	}
+
+	private void keepDigitExceptZero(String tryCountString) throws IllegalArgumentException {
+		int number = Integer.parseInt(tryCountString);
+		if (number <= 0) {
+			errorMessage = EXCEPT_ZERO_ERROR_MESSAGE;
+
+			throw new IllegalArgumentException();
 		}
 	}
 }
