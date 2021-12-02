c7fdae5a639887a8fdd817558019eeeeb0af5d2a
jojogreen91 <iawbg13@gmail.com>
2021-12-02 19:06:12 +0900

    feat(NameValidator): add keepNotOnlyBlank method
    - 이름이 공백으로만 이루어져 있는 경우 에러메시지를 출력하고 다시 입력을 받는 기능을 구현했다.
    - 예외 상황별 고유한 에러메시지 출력하기 위해 기존 메서드 방식을 수정 했다.

--- src/main/java/racingcar/validator/NameValidator.java
+++ src/main/java/racingcar/validator/NameValidator.java
@@ -4,20 +4,24 @@ import java.util.ArrayList;
 
 public class NameValidator {
 
-	private static final String NAME_ERROR_MESSAGE = "[ERROR] 적절하지 않은 자동차 이름 입력되었습니다.";
-
 	private static final int NAME_LENGTH_MAX = 5;
 	private static final int NAME_LENGTH_MIN = 1;
+
+	private static final String LENGTH_LIMIT_ERROR_MESSAGE = "[ERROR] 이름의 길이는 1 ~ 5자 까지만 허용합니다.";
+	private static final String NOT_ONLY_BLANK_ERROR_MESSAGE = "[ERROR] 이름이 공백으로만 이루어져 있으면 안 됩니다.";
+
+	private String errorMessage = null;
 
 	public boolean validate(ArrayList<String> nameList) {
 		try {
 			for (String name : nameList) {
+				keepNotOnlyBlank(name);
 				keepLengthLimit(name);
 			}
 
 			return false;
 		} catch (IllegalArgumentException illegalArgumentException) {
-			System.out.println(NAME_ERROR_MESSAGE);
+			System.out.println(errorMessage);
 
 			return true;
 		}
@@ -25,6 +29,16 @@ public class NameValidator {
 
 	private void keepLengthLimit(String name) throws IllegalArgumentException {
 		if (name.length() > NAME_LENGTH_MAX || name.length() < NAME_LENGTH_MIN) {
+			errorMessage = LENGTH_LIMIT_ERROR_MESSAGE;
+
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void keepNotOnlyBlank(String name) throws IllegalArgumentException {
+		if (name.trim().length() == 0) {
+			errorMessage = NOT_ONLY_BLANK_ERROR_MESSAGE;
+
 			throw new IllegalArgumentException();
 		}
 	}
