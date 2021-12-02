e07dce5d4fa40fe7c6163b8bc7ce79086f867127
iamsseaa <homingsea@gmail.com>
2021-12-02 23:24:30 +0900

    feat: 유저의 차 이름 입력 메서드

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -1,4 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
+    private String[] names;
+
+    public void enterCarName() {
+        String name = Console.readLine();
+    }
 }
