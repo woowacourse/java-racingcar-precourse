b1a4827fccc46385aa5283f92aafd5840f677fcc
shindong96 <2006peter@naver.com>
2021-12-02 21:01:03 +0900

    feat(Car) : GoOrStay() 함수 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 	private final String name;
 	private int position = 0;
 
 	public Car(String name) {
 		this.name = name;
+	}
+
+	public void GoOrStay() {
+		int randomNum = Randoms.pickNumberInRange(0, 9);
+
+		if (randomNum >= 4) {
+			position++;
+		}
 	}
 
 	// 추가 기능 구현
