96a48cc73976fb79714dcef06094cc53526e7f51
Hyunta <mohai2618@naver.com>
2021-12-02 20:08:30 +0900

    feat(moveCount) : 이동횟수를 입력받아오는 기능 구현

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -2,27 +2,29 @@ package racingcar.controller;
 
 import java.util.ArrayList;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RaceController {
-	private static InputView inputView;
+	public static final int MIN_RANDOM = 0;
+	public static final int MAX_RANDOM = 9;
+	public static final int MOVE_CONDITION = 4;
+
+	private InputView inputView;
+	private OutputView outputView;
 
 	public RaceController() {
 		inputView = new InputView();
+		outputView = new OutputView();
 	}
 
 	public void start() {
 		ArrayList<String> names = getProperNames();
 		Car[] cars = enrollCars(names);
-	}
+		int moveCount = getProperMoveCount();
 
-	private Car[] enrollCars(ArrayList<String> names) {
-		Car[] cars = new Car[names.size()];
-		for (int i = 0; i < names.size(); i++) {
-			cars[i] = new Car(names.get(i));
-		}
-		return cars;
 	}
 
 	private ArrayList<String> getProperNames() {
@@ -35,4 +37,19 @@ public class RaceController {
 		}
 		return names;
 	}
+
+	private Car[] enrollCars(ArrayList<String> names) {
+		Car[] cars = new Car[names.size()];
+		for (int i = 0; i < names.size(); i++) {
+			cars[i] = new Car(names.get(i));
+		}
+		return cars;
+	}
+
+	private int getProperMoveCount() {
+		int moveCount = inputView.enterMovesWithValidation();
+		return moveCount;
+	}
+
+
 }
