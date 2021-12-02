f90bc62a0a93cfcb052544f0bf53f927980777a8
jojogreen91 <iawbg13@gmail.com>
2021-12-02 10:01:54 +0900

    feat(GameHost): update run method
    - run method 에 시도 횟수 입력 기능을 update 했다.

--- src/main/java/racingcar/GameHost.java
+++ src/main/java/racingcar/GameHost.java
@@ -3,13 +3,18 @@ package racingcar;
 import java.util.ArrayList;
 
 import racingcar.input.NameReceiver;
+import racingcar.input.TryCountReceiver;
 
 public class GameHost {
 
 	NameReceiver nameReceiver = new NameReceiver();
+	TryCountReceiver tryCountReceiver = new TryCountReceiver();
 
 	public void run() {
 		// 자동차 이름 입력
 		ArrayList<String> nameList = nameReceiver.receive();
+
+		// 시도 횟수 입력
+		Integer tryCount = tryCountReceiver.receive();
 	}
 }
