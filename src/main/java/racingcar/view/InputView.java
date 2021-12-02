fa34b3d970fc8b4281fd0510e2be4551c9ab9aeb
jochungbeom <cndqjacndqja@gmail.com>
2021-12-02 13:48:28 +0900

    refactor: 자동차 이름 입력에서 입력 문구 출력 추가 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
 
 	public static String[] inputCarNames() {
 		try {
+			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 			String[] names = Console.readLine().split(",");
 			validateLengthOfCarName(names);
 			return names;
