46185d7901fa41af8a9d24f9e283630a24743ead
0chil <0@chll.it>
2021-12-02 18:33:11 +0900

    refactor(InputView): change return object to Car list

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,16 +5,19 @@ import static racingcar.constant.GameConstants.UserInputHint.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
 import racingcar.validator.UserInputValidator;
 
 public class InputView {
-	public static List<String> inputNameList() throws IllegalArgumentException {
+	public static List<Car> inputNameList() throws IllegalArgumentException {
 		System.out.println(NAME_INPUT_HINT.getString());
+
 		List<String> userInputNameList = Arrays.asList(
 			Console.readLine().split(USER_NAME_INPUT_DELIMITER.getString()));
 		userInputNameList.forEach(UserInputValidator::validateUserInputNameLength);
-		return userInputNameList;
+		return userInputNameList.stream().map(Car::new).collect(Collectors.toList());
 	}
 }
