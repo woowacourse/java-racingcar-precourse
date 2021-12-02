84a738afbd7def644c4f8978355946b3bb280177
jojogreen91 <iawbg13@gmail.com>
2021-12-02 22:58:06 +0900

    refactor: refactor TryCountValidator class
    - 에러메시지를 수정하였다.

--- src/main/java/racingcar/validator/TryCountValidator.java
+++ src/main/java/racingcar/validator/TryCountValidator.java
@@ -2,7 +2,9 @@ package racingcar.validator;
 
 public class TryCountValidator {
 
-	private static final String TRY_COUNT_ERROR_MESSAGE = "[ERROR] 시도 횟수의 값이 적절하지 않습니다.";
+	private static final String DIGIT_VALUE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 단 하나의 올바른 숫자만 허용합니다.";
+
+	private String errorMessage = null;
 
 	public boolean validate(String tryCountString) {
 		try {
@@ -13,7 +15,7 @@ public class TryCountValidator {
 
 			return false;
 		} catch (IllegalArgumentException illegalArgumentException) {
-			System.out.println(TRY_COUNT_ERROR_MESSAGE);
+			System.out.println(DIGIT_VALUE_ERROR_MESSAGE);
 
 			return true;
 		}
@@ -24,6 +26,8 @@ public class TryCountValidator {
 			char c = tryCountString.charAt(i);
 
 			if (!Character.isDigit(c)) {
+				errorMessage = DIGIT_VALUE_ERROR_MESSAGE;
+
 				throw new IllegalArgumentException();
 			}
 		}
