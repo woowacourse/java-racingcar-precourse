bdbbc79dadf30c6f14cdd6db5a31e6f770c60003
jojogreen91 <iawbg13@gmail.com>
2021-12-02 12:39:25 +0900

    feat: add RandomNumberGenerator class and its method
    - 0 ~ 9 사이에 무작위 값을 생성하는 기능을 구현했다.

---
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+	private static final int RANDOM_NUMBER_RANGE_MIN = 0;
+	private static final int RANDOM_NUMBER_RANGE_MAX = 9;
+
+	public int generate() {
+		int randomNumber = Randoms.pickNumberInRange(RANDOM_NUMBER_RANGE_MIN, RANDOM_NUMBER_RANGE_MAX);
+		return randomNumber;
+	}
+}
