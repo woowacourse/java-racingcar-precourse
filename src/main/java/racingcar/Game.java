79dca3fb2d1b1fad5d5daac1e5b26f0b52feb49b
shindong96 <2006peter@naver.com>
2021-12-02 22:28:31 +0900

    feat(Game) : playGame() 함수 추가
    setCars(), setGameTurn(), printWinner() 생성, Car 클래스에 move() 생성 필요

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,35 @@
 package racingcar;
 
+import java.util.Vector;
+
 public class Game {
+	private Vector<Car> cars;
+	private int gameTurn;
+
+	public Game() {
+		cars = new Vector<Car>();
+		gameTurn = 0;
+	}
+
+	public void playGame() {
+		setCars();
+		setGameTurn();
+
+		for (int i = 0; i < gameTurn; i++) {
+			for (int j = 0; j < cars.size(); j++) {
+				cars.get(j).move();
+			}
+		}
+
+		printWinner();
+	}
+
+	public void setCars() {
+	}
+
+	public void setGameTurn() {
+	}
+
+	public void printWinner() {
+	}
 }
