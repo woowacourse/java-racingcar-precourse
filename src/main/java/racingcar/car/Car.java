85af773dd3e19944c8748b887636e680a8bb121c
hyeonic <dev.hyeonic@gmail.com>
2021-12-02 20:06:22 +0900

    feat: Car 이동 관련 move 추가

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,8 +1,13 @@
 package racingcar.car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private static final int DEFAULT_POSITION = 0;
     private static final int NAME_MAX_LENGTH = 5;
+    private static final int MIN_NUMBER_RANGE = 0;
+    private static final int MAX_NUMBER_RANGE = 9;
+    private static final int MOVE_CONDITION = 4;
 
     private final String name;
     private int position = DEFAULT_POSITION;
@@ -20,5 +25,18 @@ public class Car {
         if (name.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException();
         }
+    }
+    
+    public void move() {
+        if (isMoving()) {
+            position++;
+        }
+    }
+
+    private boolean isMoving() {
+        if (Randoms.pickNumberInRange(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE) >= MOVE_CONDITION) {
+            return true;
+        }
+        return false;
     }
 }
