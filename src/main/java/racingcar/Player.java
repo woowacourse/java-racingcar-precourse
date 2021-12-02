bb61ede501697cdf3cddf0f499730ed59be81b1b
HyeonbinSa <shb1833@gmail.com>
2021-12-02 21:15:06 +0900

    feat: 입력값(자동차 이름, 시도 횟수) 검증 기능 구현 및 수정

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -8,49 +8,81 @@ public class Player {
     private static final int CAR_NAME_LENGTH = 6;
     private static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INSERT_COIN_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String ERROR = "[ERROR]";
     private static int gameCoin;
     private static String carNames;
     private static String[] carNamesArray;
 
     public String[] startGame() {
-        while (!this.inputCarNames()) {
+        while (true) {
+            this.inputCarNames();
+            try {
+                this.validateCarNamesLength();
+                this.validateCarNamesDuplicate();
+                break;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception);
+            }
         }
-        this.insertCoin();
+        while (true) {
+            String coin = this.insertCoin();
+            try{
+                this.validateOnlyInteger(coin);
+                this.validateMoreThanOne();
+                break;
+            }catch (IllegalArgumentException exception){
+                System.out.println(exception);
+            }
+        }
         return carNamesArray;
     }
 
-    public boolean remainCoin(){
+    public boolean remainCoin() {
         return gameCoin-- != 0;
     }
 
-    private boolean inputCarNames() {
+    private void inputCarNames() {
         System.out.println(INPUT_CAR_MESSAGE);
         carNames = Console.readLine();
         carNamesArray = carNames.split(",");
-        return validateCarNamesLength();
     }
 
-    private void insertCoin() {
+    private String insertCoin() {
         System.out.println(INSERT_COIN_MESSAGE);
         String coin = Console.readLine();
-        gameCoin = Integer.parseInt(coin);
+        return coin;
     }
 
-    private boolean validateCarNamesDuplicate() {
+    private void validateCarNamesDuplicate() {
         HashSet<String> carNameSet = new HashSet<>();
         for (String carName : carNamesArray) {
             carNameSet.add(carName);
         }
-        return carNameSet.size() == carNamesArray.length;
+        if (carNameSet.size() != carNamesArray.length) {
+            throw new IllegalArgumentException(ERROR + " : 중복된 자동차 이름을 입력하였습니다.");
+        }
     }
 
-    private boolean validateCarNamesLength() {
+    private void validateCarNamesLength() {
         for (String carName : carNamesArray) {
             if (carName.length() >= CAR_NAME_LENGTH) {
-                return false;
+                throw new IllegalArgumentException(ERROR + " : 자동차 이름은 최대 5자까지 입력 가능합니다.");
             }
         }
-        return true;
     }
 
+    private void validateMoreThanOne(){
+        if(gameCoin <= 0){
+            throw new IllegalArgumentException(ERROR + " : 1 이상의 정수만 입력 가능합니다.");
+        }
+    }
+
+    private void validateOnlyInteger(String coin){
+        for(String digit : coin.split("")){
+            if(digit.charAt(0) < '0' || digit.charAt(0)  > '9'){
+                throw new IllegalArgumentException(ERROR + "숫자만 입력이 가능합니다.");
+            }
+        }
+        gameCoin = Integer.parseInt(coin);
+    }
 }
