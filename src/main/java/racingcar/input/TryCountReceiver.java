8e0d62ec39dfb13dd06fe74324a82a93a1f158e0
jojogreen91 <iawbg13@gmail.com>
2021-12-02 10:00:05 +0900

    feat: add TryCountReceiver class and its receive method
    - 총 레이싱 시도 횟수를 입력받는 기능을 구현했다.
    - 총 레이싱 시도 횟수를 입력받아 적절한지 검증하고 Integer 로 반환하도록 구현했다.

---
+++ src/main/java/racingcar/input/TryCountReceiver.java
@@ -0,0 +1,32 @@
+package racingcar.input;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.TryCountValidator;
+
+public class TryCountReceiver {
+
+	private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+	private static final String TRY_COUNT_ERROR_MESSAGE = "[ERROR] 시도 횟수의 값이 적절하지 않습니다.";
+
+	TryCountValidator tryCountValidator = new TryCountValidator();
+
+	public Integer receive() {
+		String tryCountString = null;
+
+		boolean errorSwitch = true;
+		while (errorSwitch) {
+			System.out.println(INPUT_TRY_COUNT_MESSAGE);
+			tryCountString = Console.readLine();
+
+			try {
+				tryCountValidator.validate(tryCountString);
+				errorSwitch = false;
+			} catch (IllegalArgumentException illegalArgumentException) {
+				System.out.println(TRY_COUNT_ERROR_MESSAGE);
+				errorSwitch = true;
+			}
+		}
+
+		return Integer.parseInt(tryCountString);
+	}
+}
