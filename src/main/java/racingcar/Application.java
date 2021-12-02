c19c72d6a20a0cb99fe4f12738234444afadd9bc
shindong96 <2006peter@naver.com>
2021-12-03 00:06:13 +0900

    refact(Application) : refactor main()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.playGame();
+		// TODO 구현 진행
+	}
 }
