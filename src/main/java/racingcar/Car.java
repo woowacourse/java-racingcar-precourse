136aad68c95100cb2fa6215bbfbc0d69f59c1c40
Hongdonggeon <ghd700@naver.com>
2021-12-03 01:35:48 +0900

    feat(Car,Constant) : 자동차 이름 5글자 초과하는지 검사하기

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,18 @@
 package racingcar;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		validateNameLength(name);
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	// 추가 기능 구현
+	public void validateNameLength(String name) {
+		if (name.length() > Constant.NAME_MAX_LENGTH) {
+			throw new IllegalArgumentException(Constant.NAME_MAX_LENGTH_EXCEED_ERROR_MESSAGE);
+		}
+	}
 }
---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Constant {
+	public static int NAME_MAX_LENGTH = 5;
+
+	// 에러문
+	public static String NAME_MAX_LENGTH_EXCEED_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하만 가능하다.";
+}
