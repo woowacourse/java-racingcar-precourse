9159bdde8df6050ef655e51168f050b7f7f9d31a
Inhyeok Jo <alzpqm@naver.com>
2021-12-02 21:58:30 +0900

    feature: 자동차 이름을 입력받는 getCarNames 메소드 구현

--- src/main/java/racingcar/RacingGameView.java
+++ src/main/java/racingcar/RacingGameView.java
@@ -1,4 +1,32 @@
 package racingcar;
 
+import static util.Constants.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingGameView {
+
+	public ArrayList<String> getCarNames() {
+		ArrayList<String> carNamesList;
+		do {
+			System.out.println(CAR_NAME_INPUT_MESSAGE);
+			carNamesList = new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
+		} while (checkLongNameException(carNamesList));
+		return carNamesList;
+	}
+
+	private boolean checkLongNameException(ArrayList<String> carNamesList) {
+		try {
+			if (carNamesList.stream().anyMatch(carName -> carName.length() > MAXIMUM_NAME_LENGTH)) {
+				System.out.println(CAR_NAME_INPUT_ERROR_MESSAGE);
+				throw new IllegalArgumentException();
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
 }
--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -1,4 +1,7 @@
 package util;
 
 public class Constants {
+	public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+	public static final String CAR_NAME_INPUT_ERROR_MESSAGE = "[ERROR] 이름은 5글자 이하여야 한다.";
+	public static final int MAXIMUM_NAME_LENGTH = 5;
 }
