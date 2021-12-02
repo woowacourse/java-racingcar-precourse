f5cf2630c56efabd50eea1d60fcc04702b4dedef
최창준 <aowert6518@gmail.com>
2021-12-02 22:35:45 +0900

    [feat] 몇번 이동할 건지 입력받기
    - 경주에서 자동차가 전진할 횟수 : moveCount
    
    - getMoveCount() : 사용자에게 moveCount 받아오기

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Race {
     private static ArrayList<Car> cars = new ArrayList<>();
-
+    private static int moveCount;
     public static String getUserInput() {
         return Console.readLine();
     }
@@ -18,5 +18,10 @@ public class Race {
             Car car = new Car(carName);
             cars.add(car);
         }
+    }
+
+    public static void getMoveCount() {
+        String userInput = getUserInput();
+        moveCount = Integer.parseInt(userInput);
     }
 }
