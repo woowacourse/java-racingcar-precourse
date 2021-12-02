0250b8e023ba4f6998a66d66f24bc80a281376d0
Hyunta <mohai2618@naver.com>
2021-12-02 19:05:49 +0900

    feat(InputValidator) : 이름이 비어있을 때 검증하는 기능 구현

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -9,18 +9,21 @@ public class InputValidator {
 	public static final String ERROR_ONLY_NAME = "[ERROR] 이름을 2개 이상 입력해주세요.";
 	public static final String ERROR_LONG_NAME = "[ERROR] 이름을 5자 이하로 등록해주세요";
 	public static final String ERROR_DUPLICATE_NAME = "[ERROR] 중복된 이름이 있습니다.";
+	public static final String ERROR_EMPTY_NAME = "[ERROR] 빈 이름이 있습니다";
 
 	public boolean validateNames(ArrayList<String> names) {
 		try {
 			isOnlyName(names);
 			isLongName(names);
 			isDuplicateName(names);
+			isEmptyName(names);
 			return false;
 		} catch (IllegalArgumentException exception) {
 			System.out.println(exception.getMessage());
 			return true;
 		}
 	}
+
 
 	private void isOnlyName(ArrayList<String> names) {
 		if (names.size() == 1) {
@@ -43,6 +46,14 @@ public class InputValidator {
 		}
 		if (duplicateCheckSet.size() != names.size()) {
 			throw new IllegalArgumentException(ERROR_DUPLICATE_NAME);
+		}
+	}
+
+	private void isEmptyName(ArrayList<String> names) {
+		for (String name : names) {
+			if (name.trim().isEmpty()) {
+				throw new IllegalArgumentException(ERROR_EMPTY_NAME);
+			}
 		}
 	}
 }
