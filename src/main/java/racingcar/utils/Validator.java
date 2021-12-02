78b178abfd209d2f6afcd7757cd913666b7a5871
jongwon <54282927+loosie@users.noreply.github.com>
2021-12-02 17:23:15 +0900

    feat: Validator 역할 생성 및 메서드 추가
    Input클래스 Validate 메서드
    - validateCarNames :입력받은 문자열 길이가  NAME_LENGTH_LIMIT(=5)를 초과했는지 여부를 반환
    - validateNumberFormat: 게임 횟수의 입력이 NumberFormat(0~9)인지 여부를 반환
    
    Car클래스 Validate 메서드
    - isCarMoveCondition: Car가 입력받은 randomNumber가 4이상인지 아닌지에 대한 여부를 반환

---
+++ src/main/java/racingcar/utils/Validator.java
@@ -0,0 +1,53 @@
+package racingcar.utils;
+
+import java.util.regex.Pattern;
+
+import racingcar.exception.CarNameNotValidException;
+import racingcar.exception.NumberFormatNotValidException;
+
+public class Validator {
+	private static final Pattern NUMBER = Pattern.compile("[0-9]+");
+	private static final int NAME_LENGTH_LIMIT = 5;
+	private static final int CAR_MOVE_CONDITION_MIN_VALUE = 4;
+
+	public static boolean validateCarNames(String[] carNames, boolean flag) {
+		try {
+			for (int i = 0; i < carNames.length; i++) {
+				String name = carNames[i];
+				isNameLengthMoreOverThanLimit(name);
+			}
+		} catch (IllegalArgumentException ex) {
+			flag = false;
+		}
+		return flag;
+	}
+
+	public static boolean validateNumberFormat(String input, boolean flag) {
+		try {
+			isNumberFormat(input);
+		} catch (IllegalArgumentException ex) {
+			flag = false;
+		}
+		return flag;
+	}
+
+	public static boolean isCarMoveCondition(int randomNumber) {
+		if (randomNumber >= CAR_MOVE_CONDITION_MIN_VALUE) {
+			return true;
+		}
+		return false;
+	}
+
+	private static void isNumberFormat(String input) {
+		if (!NUMBER.matcher(input).matches()) {
+			throw new NumberFormatNotValidException();
+		}
+	}
+
+	private static void isNameLengthMoreOverThanLimit(String name) {
+		if (name.length() > NAME_LENGTH_LIMIT) {
+			throw new CarNameNotValidException();
+		}
+	}
+
+}
