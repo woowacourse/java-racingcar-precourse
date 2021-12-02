7150c10cd7bc9081b88ffff9123addb34dd56082
iamtaehoon <kimth9981@naver.com>
2021-12-02 16:21:01 +0900

    refactor: Racing 게임 playGame -> run 으로 메서드 이름 변경, 내부구조 메서드 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,6 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         RacingGame racingGame = new RacingGame();
-        racingGame.startGame();
+        racingGame.run();
     }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,15 +4,24 @@ import racingcar.view.InputView;
 
 public class RacingGame {
 	private EveryCar everyCar;
+	private int tryCntAboutCarMoving;
 
-	public void startGame() {
-		String[] everyCarName = InputView.enterEveryCarName();
-		everyCar = new EveryCar(everyCarName);
-		int tryCntAboutCarMoving = InputView.enterTryCnt();
+	public void run() {
+		initializeRace();
+		playGame();
+	}
+
+	private void playGame() {
 		System.out.println("\n실행 결과");
 		for (int i = 0; i < tryCntAboutCarMoving; i++) {
 			everyCar.moveEveryCarThisTiming();
 		}
 		everyCar.showThisGameWinner();
+	}
+
+	private void initializeRace() {
+		String[] everyCarName = InputView.enterEveryCarName();
+		everyCar = new EveryCar(everyCarName);
+		tryCntAboutCarMoving = InputView.enterTryCnt();
 	}
 }
