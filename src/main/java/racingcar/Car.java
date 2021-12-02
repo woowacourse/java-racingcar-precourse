2b10c032db7195d1ed398697c0df961fa722d9c8
TaeHyeon Kim <kth990303@naver.com>
2021-12-02 19:07:26 +0900

    feat: Car 클래스의 전진 메소드, printInfo 메소드, getter 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    static final int MIN_RANGE_NUMBER = 0;
+    static final int MAX_RANGE_NUMBER = 9;
+    static final int PROCEED_MIN_NUMBER = 4;
+
     private final String name;
     private int position = 0;
 
@@ -8,5 +14,29 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void turn() {
+        if (decideToProceed()) {
+            this.position++;
+        }
+    }
+
+    private boolean decideToProceed() {
+        return Randoms.pickNumberInRange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER) >= PROCEED_MIN_NUMBER;
+    }
+
+    public void printInfo() {
+        System.out.print(name + " : ");
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
