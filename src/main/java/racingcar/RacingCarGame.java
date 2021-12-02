8448f3250a2d143808e2b35136768f978238b9bd
YJGwon <yj970125@gmail.com>
2021-12-02 16:48:17 +0900

    feat: [예외처리]입력받은 이름이 5자를 초과하는 경우
    (수정)RacingCarGame.java
       methods
       -(추가)validateCarName: 자동차 이름이 5자를 초과하면 Exception throw
       -(수정)setCarList: validateCarName 실행하고 Exception throw
       -(수정)start: Exception catch되지 않을 때 까지 자동차 이름 입력 실행
    
    입력받은 자동차 이름의 배열에 대해 반복문을 한 번만 실행하기 위해 setCarList 안에서 validation 실행

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -10,17 +10,33 @@ import java.util.ArrayList;
  * @since 1.0
  */
 public class RacingCarGame {
-	private ArrayList<Car> carList;
+	private ArrayList<Car> carList = new ArrayList<>();
 
 	public void start() {
-		setCarList(Computer.askCarNames());
+		boolean validated;
+		do {
+			try {
+				setCarList(Computer.askCarNames());
+				validated = true;
+			} catch (IllegalArgumentException e) {
+				System.out.println("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
+				validated = false;
+			}
+		} while (!validated);
 	}
 
-	private void setCarList(String[] carNames) {
-		carList = new ArrayList<>();
-		for(String carName : carNames) {
-			//TODO 예외처리(5자 초과일 때)
+	private void setCarList(String[] carNames) throws IllegalArgumentException {
+		carList.clear();
+		for (String carName : carNames) {
+			validateCarName(carName);
 			carList.add(new Car(carName));
 		}
+	}
+
+	private void validateCarName(String carName) throws IllegalArgumentException {
+		if (carName.length() <= 5) {
+			return;
+		}
+		throw new IllegalArgumentException("자동차 이름은 5자 이하여야 한다");
 	}
 }
