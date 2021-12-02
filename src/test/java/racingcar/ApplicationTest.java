2e0e18053dd4bf3a17278cf63d6d3577105b9ddf
jongwon <54282927+loosie@users.noreply.github.com>
2021-12-02 16:36:47 +0900

    test: 복수 우승, 숫자 입력 값 테스트 케이스 추가
    게임 시뮬레이션 테스트 케이스
    - 단독 우승일 경우  (기존 TC)
    - 복수 우승일 경우  (추가된 TC)
    
    에러 테스트 케이스
    - Car 이름 입력 길이 초과에 대한 예외 처리 (기존 TC)
    - 숫자가 아닌 다른 텍스트를 입력할 경우 예외 처리 (추가된 TC)

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
-    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String NAME_ERROR_MESSAGE = "[ERROR] 이름은 5자 이하만 가능하다.";
+    private static final String NUMBER_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
 
     @Test
-    void 전진_정지() {
+    void 전진_정지_단독우승() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
@@ -26,13 +26,34 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 전진_정지_복수우승() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni", "1");
+                assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+            },
+            MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
          assertSimpleTest(
              () -> {
                  runException("pobi,javaji");
-                 assertThat(output()).contains(ERROR_MESSAGE);
+                 assertThat(output()).contains(NAME_ERROR_MESSAGE);
             }
          );
+    }
+
+    @Test
+    void 숫자에_대한_예외_처리() {
+        assertSimpleTest(
+            () -> {
+                runException("a,b,c,d,e","1abd");
+                assertThat(output()).contains(NUMBER_ERROR_MESSAGE);
+            }
+        );
     }
 
     @Override
