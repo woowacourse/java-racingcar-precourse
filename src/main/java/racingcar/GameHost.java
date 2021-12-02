13b1df49d9c17cd5e64c911d1ca35da51b7d3245
jojogreen91 <iawbg13@gmail.com>
2021-12-02 19:37:59 +0900

    refactor: refactor GameHost class

--- src/main/java/racingcar/GameHost.java
+++ src/main/java/racingcar/GameHost.java
@@ -7,22 +7,21 @@ import racingcar.input.TryCountReceiver;
 
 public class GameHost {
 
-	NameReceiver nameReceiver = new NameReceiver();
-	TryCountReceiver tryCountReceiver = new TryCountReceiver();
-	RaceTrack raceTrack = new RaceTrack();
-	Judge judge = new Judge();
-
 	public void run() {
 		// 자동차 이름 입력
+		NameReceiver nameReceiver = new NameReceiver();
 		ArrayList<String> nameList = nameReceiver.receive();
 
 		// 시도 횟수 입력
+		TryCountReceiver tryCountReceiver = new TryCountReceiver();
 		Integer tryCount = tryCountReceiver.receive();
 
 		// 레이싱 실행
+		RaceTrack raceTrack = new RaceTrack();
 		ArrayList<Car> carList = raceTrack.start(nameList, tryCount);
 
 		// 최종 우승자 발표
+		Judge judge = new Judge();
 		judge.announceWinner(nameList, carList);
 	}
 }
