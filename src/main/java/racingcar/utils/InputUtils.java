c8e6804ae954a356367677204bb52a9459a67789
Yunkeun <yunx2@naver.com>
2021-12-02 15:42:10 +0900

    feat: 자동차 이름 입력 예외처리 중 공백 입력에 대한 예외처리

--- src/main/java/racingcar/utils/InputUtils.java
+++ src/main/java/racingcar/utils/InputUtils.java
@@ -2,15 +2,17 @@ package racingcar.utils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class InputUtils {
 
 	private static final int CAR_NAME_MINIMUM_LENGTH = 0;
 	private static final int CAR_NAME_MAXIMUM_LENGTH = 5;
 	private static final String CAR_NAME_DELIMITER = ",";
-	private static final String ERROR_HEADER = "[ERROR]";
+	private static final String ERROR_HEADER = "[ERROR] ";
 	private static final String LENGTH_ERROR_MESSAGE = "잘못된 차량의 길이가 입력되었습니다.";
 	private static final String DUPLICATE_ERROR_MESSAGE = "중복된 차량의 이름이 입력되었습니다.";
+	private static final String SPACE_ERROR_MESSAGE = "공백이 입력되었습니다.";
 
 	private InputUtils() {
 	}
@@ -29,6 +31,9 @@ public class InputUtils {
 			if (!checkCarNameLength(carName)) {
 				throw new IllegalArgumentException(ERROR_HEADER + LENGTH_ERROR_MESSAGE);
 			}
+			if (!checkCarNameContainsSpace(carName)) {
+				throw new IllegalArgumentException(ERROR_HEADER + SPACE_ERROR_MESSAGE);
+			}
 		}
 	}
 
@@ -38,5 +43,9 @@ public class InputUtils {
 
 	public static boolean checkDuplicatedCarName(List<String> carNames) {
 		return carNames.stream().distinct().count() == carNames.size();
+	}
+
+	public static boolean checkCarNameContainsSpace(String carName) {
+		return Objects.equals(carName, carName.replace(" ", ""));
 	}
 }
