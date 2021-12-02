352abb06c27ebb46855412161aca628a59db08df
Donggyu Kim <a29661498@gmail.com>
2021-12-02 17:04:25 +0900

    feat: 자동차 경주 전체 진행 메소드(startGame) 구현
    경주할 자동차를 만들고, 시도할 횟수만큼 각 자동차에 대해서 진행하는 startGame() 메소드 구현

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar.game;
 
 import racingcar.domain.Car;
+import racingcar.util.UserInputUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,20 @@ import java.util.List;
 import static racingcar.util.UserInputUtil.*;
 
 public class RacingGame {
+
+    public void startGame() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<Car> carList = getCarList();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = getIntNum();
+
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < count; i++) {
+            progressGame(carList);
+        }
+    }
 
     private void progressGame(List<Car> carList) {
         for (Car car : carList) {
