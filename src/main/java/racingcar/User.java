a1f7c2eaa78e06a6fa56cddd65af1d94e033c2ff
iamsseaa <homingsea@gmail.com>
2021-12-02 23:27:21 +0900

    feat: 경기 진행의 시도 횟수를 입력하는 메소드

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -4,8 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
     private String[] names;
+    private int times;
 
     public void enterCarName() {
         String name = Console.readLine();
+    }
+
+    public void enterTimes() {
+        times = Integer.parseInt(Console.readLine());
     }
 }
