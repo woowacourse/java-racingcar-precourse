807bd339e4031718db0448e0dffdabdd118da4d2
iamsseaa <homingsea@gmail.com>
2021-12-02 23:13:03 +0900

    feat: 랜덤 값이 4 이상인지 확인(전진인지 아닌지 확인)하는 메소드

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    public static final int STANDARD = 4;
     private final String name;
     private int position = 0;
 
@@ -12,5 +13,12 @@ public class Car {
 
     public int makeRandomForAdvance() {
         return Randoms.pickNumberInRange(0,9);
+    }
+
+    public boolean predicateAdvance(int randomNumber) {
+        if (randomNumber >= STANDARD) {
+            return true;
+        }
+        return false;
     }
 }
