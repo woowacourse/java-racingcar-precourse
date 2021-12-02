f4c9b4a76f6a19adb4ced851ffa03c32e38902ff
johrae99 <70756680+johrae99@users.noreply.github.com>
2021-12-02 16:10:14 +0900

    refactor : GameManager 함수 작은 단위로 나누어 수정

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class GameManager {
 	public InputManager inputManager;
 	public Car[] cars;
+	ArrayList<String> winner = new ArrayList<>();
 
 	public GameManager(InputManager inputManager) {
 		this.inputManager = inputManager;
@@ -16,14 +17,19 @@ public class GameManager {
 	public void startGame() {
 		inputManager.scanCarsList();
 		inputManager.scanNumberOfMove();
+		saveCars();
+		for (int i = 0; i < inputManager.numberOfMove; i++) {
+			playRound();
+		}
+		calculateWinner();
+		printWinner();
+	}
+
+	public void saveCars() {
 		cars = new Car[inputManager.carsList.length];
 		for (int i = 0; i < cars.length; i++) {
 			cars[i] = new Car(inputManager.carsList[i]);
 		}
-		for (int i = 0; i < inputManager.numberOfMove; i++) {
-			playRound();
-		}
-		printWinner();
 	}
 
 	public void playRound() {
@@ -53,29 +59,37 @@ public class GameManager {
 		System.out.println();
 	}
 
-	public void printWinner() {
-		int[] position = new int[cars.length];
-		ArrayList<String> winner = new ArrayList<>();
-		for (int i = 0; i < position.length; i++) {
-			position[i] = cars[i].getPosition();
-		}
-		Arrays.sort(position);
+	public int printWinner() {
 		System.out.print("최종 우승자 : ");
+		if (winner.size() == 1) {
+			System.out.print(winner.get(0));
+			return 0;
+		}
+		for (int i = 0; i < winner.size(); i++) {
+			System.out.print(winner.get(i));
+			if (i != winner.size() - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println();
+		return 0;
+	}
+
+	public void calculateWinner() {
+		int[] position = getWinnerPosition();
 		for (int i = 0; i < position.length; i++) {
 			if (cars[i].getPosition() == position[cars.length - 1]) {
 				winner.add(cars[i].getName());
 			}
 		}
-		if (winner.size() == 1) {
-			System.out.print(winner.get(0));
-		} else if (winner.size() != 1) {
-			for (int i = 0; i < winner.size(); i++) {
-				System.out.print(winner.get(i));
-				if (i != winner.size() - 1) {
-					System.out.print(", ");
-				}
-			}
+	}
+
+	public int[] getWinnerPosition() {
+		int[] position = new int[cars.length];
+		for (int i = 0; i < position.length; i++) {
+			position[i] = cars[i].getPosition();
 		}
-		System.out.println();
+		Arrays.sort(position);
+		return position;
 	}
 }
