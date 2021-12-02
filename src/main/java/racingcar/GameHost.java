7d029c5f98a62d9d2eb36d65474661786689fd5a
jojogreen91 <iawbg13@gmail.com>
2021-12-02 09:47:20 +0900

    feat(GameHost): update run method
    - run method 에 자동차 이름 입력 기능을 update 했다.

--- src/main/java/racingcar/GameHost.java
+++ src/main/java/racingcar/GameHost.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import java.util.ArrayList;
+
+import racingcar.input.NameReceiver;
+
 public class GameHost {
 
+	NameReceiver nameReceiver = new NameReceiver();
+
 	public void run() {
+		// 자동차 이름 입력
+		ArrayList<String> nameList = nameReceiver.receive();
 	}
 }
