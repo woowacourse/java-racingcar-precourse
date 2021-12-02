1f51593fae504a99210ec063f8d34a4f2ca0dea9
xi_jjun <rlawowns97@naver.com>
2021-12-02 16:54:04 +0900

    feat: add MOVING_FORWARD constant for Car

--- src/main/java/racingcar/constants/Constant.java
+++ src/main/java/racingcar/constants/Constant.java
@@ -1,9 +1,9 @@
 package racingcar.constants;
 
 public class Constant {
-	// === Car Random Number Range === //
 	public static final int MIN_CONDITION = 0;
 	public static final int MAX_CONDITION = 9;
+	public static final int MOVING_FORWARD = 4;
 
 	// === Check Proper Name Constants === //
 	public static final int MIN_NAME_LENGTH = 1;
