6d873f59175a3c8b001bfc59f00fe4479b953300
Sanggu <rkdtkdrn93@naver.com>
2021-12-02 20:29:06 +0900

    feat: 시도할 횟수를 입력받는 기능 추가
    GameMachine.java
    - inputRound() : 시도할 횟수를 입력받는 함수

--- src/main/java/racingcar/GameMachine.java
+++ src/main/java/racingcar/GameMachine.java
@@ -16,14 +16,23 @@ public class GameMachine {
 
 	public void racingGame() {
 		lineUp();
+		this.round = inputRound();
 	}
 
 	public void lineUp() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String userInput = Console.readLine();
 		String[] names = userInput.split(",");
+		//null, 중복 이름 검사 추가 예정
 		for (String name : names) {
 			cars.add(new Car(name.trim()));
 		}
+	}
+
+	public int inputRound() {
+		System.out.println("시도할 횟수는 몇회인가요?");
+		String userInput = Console.readLine();
+		//숫자인지, 1보다 큰지 유효성 검사 추가 예정
+		return Integer.parseInt(userInput);
 	}
 }
