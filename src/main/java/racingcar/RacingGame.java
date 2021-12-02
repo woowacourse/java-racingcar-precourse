75b539cc560e429ec8b7edfcd63096b80d5efa02
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-12-02 20:01:23 +0900

    feat: RacingGame 클래스에 사용자 콘솔 입력부(자동차 이름, 시도할 횟수) 구현

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RacingGame {
+	private static final String MSG_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String MSG_INPUT_ATTEMPTS_NUM = "시도할 회수는 몇회인가요?";
+
+	public void play() {
+		List<String> carNameList = this.inputCarNames();
+		int numberOfAttempts = this.inputNumberOfAttempts();
+	}
+
+	private List<String> inputCarNames() {
+		System.out.println(MSG_INPUT_CAR_NAMES);
+		return Arrays.asList(Console.readLine().split(","));
+	}
+
+	private int inputNumberOfAttempts() {
+		System.out.println(MSG_INPUT_ATTEMPTS_NUM);
+		return Integer.parseInt(Console.readLine());
+	}
+}
