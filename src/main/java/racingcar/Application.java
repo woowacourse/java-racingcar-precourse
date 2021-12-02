0c7680817baf4c55f17a977916ded5d3896f2b76
KIM <km0192@naver.com>
2021-12-02 09:29:07 +0900

    recator(Application) 메소드의 중복선언 삭제, Referee 판정실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,13 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        CarListMaker carList = new CarListMaker();  // 자동차 리스트 생성
-        RepeatNumMaker repeatNum = new RepeatNumMaker(); // 숫자 생성
-        Race race = new Race(carList.getCarList(), repeatNum.getRepeatNum()); // 레이스 돌리기
-        // race.getCarList(); // 우승 결과 출력
+        ArrayList<Car> carList = new CarListMaker().getCarList();
+        int repeatNum = new RepeatNumMaker().getRepeatNum();
+        Race race = new Race(carList, repeatNum);
+        Referee referee = new Referee(carList);
+        referee.makeJudgement();
     }
 }
