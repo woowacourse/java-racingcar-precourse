4518bdb5cf2369fe0e31b8f4aed53c796585ce12
TaeHyeon Kim <kth990303@naver.com>
2021-12-02 17:27:03 +0900

    feat: CarNameValidator 클래스 중복이름 검증 메소드 구현

--- src/main/java/racingcar/CarNameValidator.java
+++ src/main/java/racingcar/CarNameValidator.java
@@ -5,16 +5,26 @@ import java.util.List;
 public class CarNameValidator {
     static final int NAME_SIZE = 5;
     static final String NAME_SIZE_ERROR_MESSAGE = "[ERROR] 이름은 1자 이상 5자 이하여야 한다.";
+    static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 이름은 중복될 수 없다.";
 
     public static void checkException(List<String> carNameList) {
         if (!checkCarNameLength(carNameList)) {
             throw new IllegalArgumentException(NAME_SIZE_ERROR_MESSAGE);
+        }
+        if (!checkDuplicateName(carNameList)) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
 
     public static boolean checkCarNameLength(List<String> carNameList) {
         return carNameList.stream()
                 .filter(carName -> carName.length() > 0 && carName.length() <= NAME_SIZE)
+                .count() == carNameList.size();
+    }
+
+    public static boolean checkDuplicateName(List<String> carNameList) {
+        return carNameList.stream()
+                .distinct()
                 .count() == carNameList.size();
     }
 }
