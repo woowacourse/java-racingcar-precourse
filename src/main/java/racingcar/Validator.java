f596aa3533e8e42fdbdc22d0da9a97ef84a81d8c
Jihyun <bjih1999@naver.com>
2021-12-02 16:16:31 +0900

    style(Validator) : 멤버 변수와 메소드 사이 빈줄 삽입

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -4,9 +4,11 @@ public class Validator {
 
     public static final int MAXIMUM_NAME_LENGTH = 5;
     public static final String ERROR_PREFIX = "[ERROR] ";
+
     public static void validateCarName(String name) {
         if (name.length() > MAXIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(ERROR_PREFIX + "자동차의 이름은 5자 이하여야합니다.");
         }
     }
+
 }
