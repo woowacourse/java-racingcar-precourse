dd537bc15e27d2f67e168c1fe8f15c150d2bef5e
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-02 18:38:30 +0900

    feat: 자동차 전진 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Objects;
 
 public class Car {
+    private static final int STANDARD_NUMBER = 4;
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     private final String name;
     private int position = 0;
 
@@ -12,5 +18,19 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void go(int random) {
+        if (isBiggerThanStandard(random)) {
+            position++;
+        }
+    }
+
+    private boolean isBiggerThanStandard(int num) {
+        return STANDARD_NUMBER <= num;
     }
 }
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    @Test
+    void 자동차_전진() {
+        Car car = new Car("car1");
+        car.go(5);
+        assertEquals(1, car.getPosition());
+    }
+
+    @Test
+    void 자동차_멈춤() {
+        Car car = new Car("car1");
+        car.go(2);
+        assertEquals(0, car.getPosition());
+    }
+}
