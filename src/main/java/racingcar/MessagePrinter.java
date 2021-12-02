a731b432903301aad12701b9d567f748932d743d
nailseong <nnis9857@gmail.com>
2021-12-03 04:50:36 +0900

    feat: 자동차 이름을 묻는 메시지를 출력

--- src/main/java/racingcar/MessagePrinter.java
+++ src/main/java/racingcar/MessagePrinter.java
@@ -2,8 +2,14 @@ package racingcar;
 
 public class MessagePrinter {
 
+	private static final String ASK_FOR_CAR_NAME_LIST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
 	public static void printError(IllegalArgumentException exception) {
 		System.out.println("[ERROR] " + exception.getMessage());
+	}
+
+	public static void askCarNameList() {
+		System.out.println(ASK_FOR_CAR_NAME_LIST);
 	}
 
 }
