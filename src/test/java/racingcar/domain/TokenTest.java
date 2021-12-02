be2db3f8f3c1029b039992cca9c44cd207b5e478
yoon <duddbs774@gmail.com>
2021-12-03 05:20:06 +0900

    test: 테스트 코드 수정
    racingcar.domain.TokenTest 예외발생 테스트 케이스에
    hasMessageContaining 메서드 추가함으로써 정확한 예외발생 테스트를
    할 수 있도록 함

--- src/test/java/racingcar/domain/TokenTest.java
+++ src/test/java/racingcar/domain/TokenTest.java
@@ -8,17 +8,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TokenTest {
+    public static final String ERROR_STRING = "[ERROR]";
+
     @DisplayName("토큰 생성시 유효하지 않은 값에 대한 예외발생")
     @ParameterizedTest
     @ValueSource(strings = {"", "a", "!", "%"})
     void tokenFromInvalidValue(String value) {
-        assertThatThrownBy(() -> Token.from(value)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Token.from(value)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_STRING);
     }
 
     @DisplayName("토큰 생성시 null값에 대한 예외발생")
     @Test
     void tokenFromNull() {
-        assertThatThrownBy(() -> Token.from(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Token.from(null)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_STRING);
     }
 
     @DisplayName("토큰 생성시 유효한 값에 대한 통과 테스트")
