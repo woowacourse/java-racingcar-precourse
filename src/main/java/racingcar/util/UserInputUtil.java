0a4e8183cf9e1aa00a1be2c12784c3dedc18ac09
Donggyu Kim <a29661498@gmail.com>
2021-12-02 16:21:02 +0900

    feat: 이동 횟수(시도 횟수)를 입력받는 기능 구현
    사용자로부터 이동 횟수(시도 횟수)를 입력받는 기능을 UserInputUtil 클래스에 정의하였습니다.
    내부적으로 private로 선언된 toInteger() 메소드를 통해서 정수 입력인지를 검증합니다.

--- src/main/java/racingcar/util/UserInputUtil.java
+++ src/main/java/racingcar/util/UserInputUtil.java
@@ -7,12 +7,20 @@ public class UserInputUtil {
     private UserInputUtil() {
     }
 
+    public static int getIntNum() {
+        String input = Console.readLine();
+
+        int number = toInteger(input);
+
+        return number;
+    }
+
     public static String[] getCarNames() {
         String input = Console.readLine();
 
         String[] carNames = input.split(",");
 
-        for(int i = 0; i < carNames.length; i++) {
+        for (int i = 0; i < carNames.length; i++) {
             checkCarName(carNames[i]);
         }
 
@@ -22,6 +30,14 @@ public class UserInputUtil {
     private static void checkCarName(String input) {
         if(input.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5글자 이하여야 합니다.");
+        }
+    }
+
+    private static int toInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
         }
     }
 }
