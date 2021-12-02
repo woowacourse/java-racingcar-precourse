9df091634bc787901e7c01c6d6bba809f746f220
iamtaehoon <kimth9981@naver.com>
2021-12-02 17:10:58 +0900

    refactor: OutputView클래스의 showThisGameWinner 메서드 하위로직 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,10 +2,11 @@ package racingcar.view;
 
 import java.util.ArrayList;
 
-import racingcar.Car;
 import racingcar.dto.CarDTO;
 
 public class OutputView {
+	public static ArrayList<CarDTO> carDTOArrayList;
+
 	public static void showSentenceBeforeResult() {
 		System.out.println("\n실행 결과");
 	}
@@ -25,24 +26,36 @@ public class OutputView {
 		return movingMark;
 	}
 
-	public static void showThisGameWinner(ArrayList<CarDTO> carsDTO) {
-		int winnerMovingDistance = carsDTO.stream()
-			.mapToInt(carDTO -> carDTO.getMovingDistance())
-			.max()
-			.orElseThrow(() -> new IllegalArgumentException("자동차의 최댓값이 존재하지 않습니다. 로직이 잘못되었습니다."));
+	public static void showThisGameWinner(ArrayList<CarDTO> carDTOArrayList) {
+		OutputView.carDTOArrayList = carDTOArrayList;
+		int winnerMovingDistance = findWinnerMovingDistance(carDTOArrayList);
+		CarDTO[] winners = findAllWinners(winnerMovingDistance);
+		makeFinalResultSentence(winners);
+	}
 
-		CarDTO[] carsDTOArray = carsDTO.stream() //cars 일므 변경 car[] 타입도.
-			.filter(eachCar -> eachCar.getMovingDistance() == winnerMovingDistance)
-			.toArray(CarDTO[]::new);
-
+	private static void makeFinalResultSentence(CarDTO[] winners) {
 		String result = "최종 우승자 : ";
-		for (CarDTO carDTO : carsDTOArray) {
-			result += carDTO.getName();
-			if (carDTO != carsDTOArray[carsDTOArray.length-1]) {
+		for (CarDTO winner : winners) {
+			result += winner.getName();
+			if (winner != winners[winners.length-1]) {
 				result += ", ";
 			}
 		}
 		System.out.println(result);
+	}
 
+	private static CarDTO[] findAllWinners(int winnerMovingDistance) {
+		CarDTO[] winners = carDTOArrayList.stream()
+			.filter(eachCar -> eachCar.getMovingDistance() == winnerMovingDistance)
+			.toArray(CarDTO[]::new);
+		return winners;
+	}
+
+	private static int findWinnerMovingDistance(ArrayList<CarDTO> carsDTO) {
+		int winnerMovingDistance = carsDTO.stream()
+			.mapToInt(carDTO -> carDTO.getMovingDistance())
+			.max()
+			.orElseThrow(() -> new IllegalArgumentException("자동차의 최댓값이 존재하지 않습니다. 로직이 잘못되었습니다."));
+		return winnerMovingDistance;
 	}
 }
