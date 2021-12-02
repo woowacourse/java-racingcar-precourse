78c2a79a3e2287e7762d8453afcf20166fe4d1c8
최창준 <aowert6518@gmail.com>
2021-12-02 22:59:46 +0900

    [feat] 자동차가 전진할지 말지 결정하기
    - Car 클래스 내부 함수에서 position 증가하는 함수로 구현
    - 랜덤 넘버를 만들고 4이상이면 전진 position + 1

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +11,11 @@ public class Car {
     }
 
     // 추가 기능 구현
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
 }
