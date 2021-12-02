25803fac3dd49fc99861ccc3c0c0c49598936cec
jochungbeom <cndqjacndqja@gmail.com>
2021-12-02 13:08:00 +0900

    feat: [ERROR] 5글자 이상 초과 시 에러 발생 후, 다시 입력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,25 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+	private static final int MAX_CAR_NAME_LENGTH = 5;
+	private static final String ERROR_MESSAGE = "[ERROR]";
 
 	public static String[] inputCarNames() {
-		return Console.readLine().split(",");
+		try {
+			String[] names = Console.readLine().split(",");
+			validateLengthOfCarName(names);
+			return names;
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+			return inputCarNames();
+		}
+	}
+
+	private static void validateLengthOfCarName(String[] names) {
+		for (String name : names) {
+			if (name.length() > MAX_CAR_NAME_LENGTH) {
+				throw new IllegalArgumentException(ERROR_MESSAGE + "입력한 이름이 5글자를 초과합니다.");
+			}
+		}
 	}
 }
