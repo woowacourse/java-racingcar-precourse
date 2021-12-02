d8879486cdda480fe052edec7c6450b411d2e609
KIM <km0192@naver.com>
2021-12-02 16:56:15 +0900

    refactor(InputValidator) : 입력 오류체크/리스트화 에러체크 함수 분리

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -3,30 +3,30 @@ package racingcar;
 import java.util.HashSet;
 
 public class InputValidator {
-	String carName;
+	private String carName;
 	HashSet<String> carNameSet = new HashSet<>();
 
-	boolean checkCarListInput(String carName) {
+	static boolean isStrContainOnlyAlphabetOrComma(String str){
+		if (str.matches("[a-zA-z,]+")){
+			return true;
+		}
+		throw new IllegalArgumentException(ErrorMessageText.ENGLISH_AND_COMMA_ONLY);
+	}
+
+	public boolean hasValidLengthAndNoDuplication(String carName) {
 		this.carName = carName;
-		return isValidLength() && containsNoBlank() && hasNoDuplication();
+		return isValidLength() && hasNoDuplication();
 	}
 
-	boolean isValidLength(){
+	private boolean isValidLength(){
 		int len = carName.length();
-		if (len == 0 || len > 5 ){
-			throw new IllegalArgumentException(ErrorMessageText.NOT_ALLOWED_LENGTH_RANGE);
+		if (len < 0 && len <= 5){
+			return true;
 		}
-		return true;
+		throw new IllegalArgumentException(ErrorMessageText.NOT_ALLOWED_LENGTH_RANGE);
 	}
 
-	boolean containsNoBlank(){
-		if (carName.contains(" ")){
-			throw new IllegalArgumentException(ErrorMessageText.BLANK_NOT_ALLOWED);
-		}
-		return true;
-	}
-
-	boolean hasNoDuplication(){
+	private boolean hasNoDuplication(){
 		if (carNameSet.contains(carName)) {
 			throw new IllegalArgumentException(ErrorMessageText.DUPLICATION_NOT_ALLOWED);
 		}
@@ -37,7 +37,7 @@ public class InputValidator {
 
 class ErrorMessageText{
 	static final String START = "[ERROR]";
-	static final String BLANK_NOT_ALLOWED = START + "입력에 공백은 허용되지 않습니다.";
+	static final String ENGLISH_AND_COMMA_ONLY = START + "입력값은 영문과 쉼표만 허용됩니다.";
 	static final String DUPLICATION_NOT_ALLOWED = START + "입력에 중복은 허용되지 않습니다.";
 	static final String NOT_ALLOWED_LENGTH_RANGE = START + "이름의 길이는 5이하만 허용됩니다.";
 }
