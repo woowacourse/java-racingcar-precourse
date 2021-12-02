d8c7a13c8886f88a7ee27b1764829b7e44051d18
KIM <km0192@naver.com>
2021-12-02 16:55:05 +0900

    refactor(CarListMaker) : 입력/리스트화 클래스 분리, 에러체크 분리

--- src/main/java/racingcar/CarListMaker.java
+++ src/main/java/racingcar/CarListMaker.java
@@ -1,29 +1,83 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class CarListMaker {
 	ArrayList<Car> carList = new ArrayList<>();
 
+	ArrayList<Car> getCarList(){
+		return this.carList;
+	}
+
 	CarListMaker(){
 		makeList();
 	}
 
-	void makeList(){
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
-		String carListStr = Console.readLine();
-
-		String[] carListArr = carListStr.split(",");
-
-		for (String carName : carListArr){
-			// 검사 : 1.길이제한, 2.공백존재 3.중복
-			carList.add(new Car(carName));
+	private void makeList(){
+		boolean passed = false;
+		while (!passed) {
+			String chunkOfCarNames = new NameStringMaker().getNameString();
+			passed = isValidList(chunkOfCarNames);
 		}
 	}
 
-	ArrayList<Car> getCarList(){
-		return this.carList;
+	private boolean isValidList(String chunkOfCarNames){
+		try {
+			carList = getCarList(chunkOfCarNames);
+		} catch (IllegalArgumentException error) {
+			carList = new ArrayList<>();
+			System.out.println(error.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	private ArrayList<Car> getCarList(String chunkOfCarNames) throws IllegalArgumentException {
+		InputValidator checker = new InputValidator();
+		for (String carName : splitStrToListByComma(chunkOfCarNames)) {
+			checker.hasValidLengthAndNoDuplication(carName);
+			carList.add(new Car(carName));
+		}
+		return carList;
+	}
+
+	private ArrayList<String> splitStrToListByComma(String str){
+		String[] arr = str.split(",");
+		List<String> myList =  Stream.of(arr).collect(Collectors.toList());
+		return (new ArrayList<String>(myList));
+	}
+}
+
+class NameStringMaker{
+	private String chunkOfCarNames;
+
+	String getNameString(){
+		return chunkOfCarNames;
+	}
+
+	NameStringMaker(){
+		input();
+	}
+
+	private String input(){
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		do {
+			String chunkOfCarNames = Console.readLine();
+		} while (!isValidInput(chunkOfCarNames));
+		return chunkOfCarNames;
+	}
+
+	private boolean isValidInput(String input){
+		try {
+			return InputValidator.isStrContainOnlyAlphabetOrComma(input);
+		} catch (Exception error) {
+			System.out.println(error.getMessage());
+			return false;
+		}
 	}
 }
