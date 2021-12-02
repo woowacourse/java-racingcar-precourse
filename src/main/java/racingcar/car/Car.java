0ee5e2fcff873e31b021ef96c91d835d82e50315
hyeonic <dev.hyeonic@gmail.com>
2021-12-02 20:19:32 +0900

    refactor: Car MovingPolicy 추가 및 move 메서드 수정

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,20 +1,17 @@
 package racingcar.car;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Car {
     private static final int DEFAULT_POSITION = 0;
     private static final int NAME_MAX_LENGTH = 5;
-    private static final int MIN_NUMBER_RANGE = 0;
-    private static final int MAX_NUMBER_RANGE = 9;
-    private static final int MOVE_CONDITION = 4;
 
     private final String name;
+    private final MovingPolicy movingPolicy;
     private int position = DEFAULT_POSITION;
 
-    public Car(String name) {
+    public Car(String name, MovingPolicy movingPolicy) {
         validateName(name);
         this.name = name;
+        this.movingPolicy = movingPolicy;
     }
 
     private void validateName(String name) {
@@ -28,15 +25,8 @@ public class Car {
     }
     
     public void move() {
-        if (isMoving()) {
+        if (movingPolicy.isMoving()) {
             position++;
         }
-    }
-
-    private boolean isMoving() {
-        if (Randoms.pickNumberInRange(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE) >= MOVE_CONDITION) {
-            return true;
-        }
-        return false;
     }
 }
