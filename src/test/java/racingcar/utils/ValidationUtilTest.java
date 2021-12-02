13163966877c8f9a060a5612fb8abc8037ff2b1f
yoon <duddbs774@gmail.com>
2021-12-03 04:44:31 +0900

    test: 테스트 코드 수정
    racingcar.utils.ValidationUtilTest 예외발생 테스트 코드에
    hasMessageContaining 메서드 추가함으로써 정확한 예외발생 테스트를
    할 수 있도록 함

--- src/test/java/racingcar/utils/ValidationUtilTest.java
+++ src/test/java/racingcar/utils/ValidationUtilTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidationUtilTest {
+    public static final String ERROR_STRING = "[ERROR]";
+
     @DisplayName("null값 예외발생 테스트")
     @Test
     void checkNull() {
         assertThatThrownBy(() -> {
             ValidationUtil.checkNull(null);
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_STRING);
     }
 
     @DisplayName("빈값 예외발생 테스트")
@@ -21,7 +24,8 @@ public class ValidationUtilTest {
     void checkEmpty() {
         assertThatThrownBy(() -> {
             ValidationUtil.checkEmpty("");
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_STRING);
     }
 
     @DisplayName("6자리 이상의 값 예외발생 테스트")
@@ -29,7 +33,8 @@ public class ValidationUtilTest {
     void checkInvalidLength() {
         assertThatThrownBy(() -> {
             ValidationUtil.checkLength("123456");
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_STRING);
     }
 
     @DisplayName("1자리 이상 5자리 이하의 값 통과 테스트")
@@ -45,7 +50,8 @@ public class ValidationUtilTest {
     void checkCommaInFirst(String value) {
         assertThatThrownBy(() -> {
             ValidationUtil.checkCommaInFirst(value);
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_STRING);
     }
 
     @DisplayName("쉼표를 기준으로 나눈값 중 중복문자가 있을경우 예외발생 테스트")
@@ -54,6 +60,7 @@ public class ValidationUtilTest {
     void checkDuplicate(String value) {
         assertThatThrownBy(() -> {
             ValidationUtil.checkDuplicate(value);
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_STRING);
     }
 }
