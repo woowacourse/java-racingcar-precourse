d6d8f627760fc4a309daebd2880d51a5c92fe54a
yoon <duddbs774@gmail.com>
2021-12-03 02:28:38 +0900

    test: 테스트 코드 수정
    racingcar.domain.Car의 move메소드를 단순 전진 기능으로 수정함에 따라
    테스트코드 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,34 +2,18 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class CarTest {
-    Car myCar;
-
-    @BeforeEach
-    void setUp() {
-        myCar = new Car("myCar");
-    }
-
-    @DisplayName("자동차 전진 및 정지 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"0:0", "3:0", "4:1", "9:1"}, delimiter = ':')
-    void moveTest(int input, int expected) {
-        myCar.move(input);
-        assertThat(myCar.getPosition()).isEqualTo(expected);
-    }
-
     @DisplayName("자동차 연속 전진 테스트")
     @Test
     void continuouslyMoveTest() {
-        myCar.move(4);
+        Car myCar = new Car("myCar");
+        assertThat(myCar.getPosition()).isEqualTo(0);
+        myCar.move();
         assertThat(myCar.getPosition()).isEqualTo(1);
-        myCar.move(4);
+        myCar.move();
         assertThat(myCar.getPosition()).isEqualTo(2);
     }
 }
