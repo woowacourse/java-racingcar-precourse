bcef69fff191f5024e86418efaa0fa8a22fa101f
iamtaehoon <kimth9981@naver.com>
2021-12-02 16:02:21 +0900

    refactor: InputView 객체 tryCnt 검증 메세지 상수 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,8 @@ public class InputView {
 	public static final String CAR_NAME_OVER_STANDARD_ERROR_MESSAGE = "차의 이름은 %d자 이하만 가능하다.";
 	public static final String CAR_NAME_NOT_NULL_ERROR_MESSAGE = "차의 이름에 null 값이 들어갔다.";
 	public static final String SHOW_ERROR_MESSAGE = "[ERROR] %s";
+	public static final String TRY_CNT_IS_NOT_NEGATIVE_MESSAGE = "시도 횟수는 음수가 될 수 없다.";
+	public static final String TRY_CNT_IS_NOT_NUMBER_MESSAGE = "시도 횟수는 숫자여야 한다.";
 
 	public static final String INPUT_EVERY_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String INPUT_TRY_CAR_MOVING_CNT = "시도할 회수는 몇회인가요?";
@@ -68,13 +70,13 @@ public class InputView {
 
 	private static void validateTryCntIsPositive(int tryCnt) {
 		if (tryCnt < 0) {
-			throw new IllegalArgumentException("시도 횟수는 음수가 될 수 없다.");
+			throw new IllegalArgumentException(TRY_CNT_IS_NOT_NEGATIVE_MESSAGE);
 		}
 	}
 
 	private static void validateTryCntIsNumber(String tryCntString) {
 		if (!tryCntString.matches(NUMBER_REGEX)) {
-			throw new IllegalArgumentException("시도 횟수는 숫자여야 한다.");
+			throw new IllegalArgumentException(TRY_CNT_IS_NOT_NUMBER_MESSAGE);
 		}
 	}
 }
