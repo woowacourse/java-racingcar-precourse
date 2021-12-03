5fee10f408590d028a86ab2b3b513afbe7666f78
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-03 14:15:01 +0900

    refactor: 매직 넘버 상수로 대체

--- src/main/java/racingcar/util/ValidChecker.java
+++ src/main/java/racingcar/util/ValidChecker.java
@@ -7,7 +7,7 @@ public class ValidChecker {
 	public static void isValidCarNameLength(String carName) {
 		if (carName.length() > MAX_CAR_NAME_LENGTH) {
 			throwIllegalArgumentException(ERROR_CAR_NAME_LENGTH_OVER);
-		} else if (carName.length() < 1) {
+		} else if (carName.length() < MIN_CAR_NAME_LENGTH) {
 			throwIllegalArgumentException(ERROR_CAR_NAME_LENGTH_UNDER);
 		}
 	}
