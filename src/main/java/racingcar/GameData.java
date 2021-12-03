64bda4cad5f0e8ccb8adbe1f2100a31eec6e7346
skennetho <thrkd613@naver.com>
2021-12-04 01:01:49 +0900

    feat(GameData): 게임에 필요한 정보를 갖는 클래스 생성
    게임에 필요한 데이터들을 한곳에 두기위한 클래스를 생성하였다. 이제 다른곳에서 필요할 시 해당 클래스를 통해 가져오면 되고, 추가로 변경사항이 생겼을시 이 GameData 내부에서 수정하면 된다.
    static final 로 선언하여 다른곳에서 접근 할 수 있게 하였고, 값에 무결성을 추가하였다.

---
+++ src/main/java/racingcar/GameData.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class GameData {
+    public static final int MAX_CAR_NAME_LENGTH =5;
+
+    public static final String MESSAGE_ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String MESSAGE_ASK_TRY_NUMBER = "시도할 회수는 몇회인가요?";
+    public static final String MESSAGE_ERROR = "[ERROR] ";
+}
