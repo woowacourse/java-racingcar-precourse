58feff9903ae8d2d5437066c9cecda9a726b487a
meatsby <meatsby@gmail.com>
2021-12-03 11:19:54 +0900

    feat: 자동차 이름, 위치 반환 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -19,4 +19,12 @@ public class Car {
             position++;
         }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
