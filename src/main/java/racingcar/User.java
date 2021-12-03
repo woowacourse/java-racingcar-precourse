42411c0d68462d90e4fc21b0aecebfa7cf657040
kanamycine <jinyeong5320@gmail.com>
2021-12-03 17:27:00 +0900

    fix(User) : 인풋 받을 때, 개행 알맞게 수정

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -1,17 +1,23 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.utils.Constant;
 
 public class User {
 	public static String [] driverNameInput(){
 		String [] driverNames = new String[3];
+
+		System.out.println(Constant.ASK_CARS_NAME);
 		String driverNameInput = Console.readLine();
 		driverNames = driverNameInput.split(",");
+
 		return driverNames;
 	}
 
 	public static int gameCountInput(){
+		System.out.println(Constant.ASK_GAME_COUNT);
 		int gameCount = Integer.parseInt(Console.readLine());
+		System.out.println();
 		return gameCount;
 	}
 }
