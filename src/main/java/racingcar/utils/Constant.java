2ee133d190ac404b065f402b1bf85f7c54e44447
kanamycine <jinyeong5320@gmail.com>
2021-12-03 17:01:32 +0900

    feat(Constant) : 기호 상수 클래스 작성

--- src/main/java/racingcar/utils/Constant.java
+++ src/main/java/racingcar/utils/Constant.java
@@ -2,4 +2,9 @@ package racingcar.utils;
 
 public class Constant {
 	public static final int MOVE_VALUE = 4;
+
+	public static String ASK_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static String ASK_GAME_COUNT = "시도할 회수는 몇회인가요?";
+	public static String PLAY_RESULT = "실행 결과";
+	public static String WINNER = "최종 우승자";
 }
