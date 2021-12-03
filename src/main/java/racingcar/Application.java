2dcf890cf4c5b70f80d268df34a269cdd89cf6c3
SheepEatLion <gowjr207@gmail.com>
2021-12-03 22:06:23 +0900

    feat: Application.java
    - 사용자에게 입력해야할 값 안내문구 출력
    - 사용자로부터 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        String carName;                         // 입력받을 차 이름
+        String numOfTry;                        // 입력받을 경기 시도 횟수
+
+        System.out.print("경주할 자동차 이름을 입력하세요.");
+        System.out.println("(이름은 쉼표(,) 기준으로 구분)");
+        carName = Console.readLine();
+        System.out.println("시도할 횟수는 몇회인가요?");
+        numOfTry = Console.readLine();
     }
 }
