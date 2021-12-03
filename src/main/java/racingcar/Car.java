b1570c2bd45e92670d49f9ea2717a6f64faa43b1
SheepEatLion <gowjr207@gmail.com>
2021-12-03 23:35:01 +0900

    feat: Car.java
    added new method name of 'getName' and 'getPosition'
    - private 변수인 name 과 position 을 읽어오기 위해 getter 를 생성했다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,8 +8,15 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
     public void moveForward(){
         this.position++;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPosition(){
+        return this.position;
     }
 }
