94ae3034d5ef8b22bd898910507c3dafedf8bb55
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-03 19:58:49 +0900

    feat<Message>: 출력 메시지를 모아놓은 클래스
    상수로 지정해놓았습니다.

---
+++ src/main/java/racingcar/message/GuideMessage.java
@@ -0,0 +1,8 @@
+package racingcar.message;
+
+public class GuideMessage {
+	public static final String ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String ENTER_NUMBER = "시도할 회수는 몇회인가요?";
+	public static final String RESULT_MESSAGE = "\n실행 결과";
+	public static final String ERR_NAME_DUPLICATION = "[ERROR] 자동차 이름이 중복되지 않게 입력해주세요.";
+}
