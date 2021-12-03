28f6e71c6cf9d2be0d4aaede22011224f4bd5e21
iamsseaa <homingsea@gmail.com>
2021-12-03 14:41:53 +0900

    feat: 차 객체를 리스트에 저장하는 메소드

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    public List<Car> makeCarObject(String[] allCarName) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : allCarName) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+
+        return cars
+    }
 }
