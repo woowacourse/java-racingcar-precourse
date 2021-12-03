b42bfee75fa74126e80aedfa2902c37a4f4fb3c7
yeongunheo <gjduddns@gmail.com>
2021-12-03 11:44:04 +0900

    feat: 하나의 자동차를 이동하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	// 추가 기능 구현
+	public int move() {
+		if (Randoms.pickNumberInRange(0, 10) >= 4) {
+			this.position++;
+		}
+		return position;
+	}
 }
