fb4353cfc41c355d51b86c4fd9476ac4164b0d64
delphox60 <delphox60@gmail.com>
2021-12-03 10:43:11 +0900

    feat(Car): 임의의 수를 생성하여 그 값에 따라  전진 또는 정지하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    static final int RANDOM_RANGE_MIN = 0;
+    static final int RANDOM_RANGE_MAX = 9;
+
     private final String name;
     private int position = 0;
 
@@ -9,4 +14,11 @@ public class Car {
     }
 
     // 추가 기능 구현
+
+    public void progressOrStop() {
+        int randNum = Randoms.pickNumberInRange(RANDOM_RANGE_MIN, RANDOM_RANGE_MAX);
+        if (randNum >= 4) {
+            position++;
+        }
+    }
 }
