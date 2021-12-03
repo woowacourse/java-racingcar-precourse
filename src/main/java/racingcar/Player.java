bb213cf023c4cc74a48f5030075746becc4b0931
HyeonbinSa <shb1833@gmail.com>
2021-12-03 20:21:47 +0900

    refactor: 불필요한 import 제거 및 컨벤션에 맞게 수정

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -2,8 +2,6 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.HashSet;
-
 public class Player {
     private static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INSERT_COIN_MESSAGE = "시도할 회수는 몇회인가요?";
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,9 +1,8 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
-import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingGame {
     private static final int MIN = 0;
