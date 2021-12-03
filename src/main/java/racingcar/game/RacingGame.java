69b0fda2f6b3c9bbee72864480018435153e1369
EastHShin <xldk78@gmail.com>
2021-12-04 05:00:49 +0900

    feat(game): 자동차마다 랜덤값을 구하는 기능 구현

--- src/main/java/racingcar/game/RacingGame.java
+++ src/main/java/racingcar/game/RacingGame.java
@@ -1,2 +1,28 @@
-package racingcar.game;public class RacingGame {
+package racingcar.game;
+
+import java.util.List;
+
+import racingcar.Car;
+import racingcar.utils.CarNameInput;
+import racingcar.utils.GameTimesInput;
+
+public class RacingGame {
+	private CarNameInput carNameInput;
+	private GameTimesInput gameTimesInput;
+	private List<Car> cars;
+	private int numOfIteration;
+
+	public RacingGame() {
+		carNameInput = new CarNameInput();
+		gameTimesInput = new GameTimesInput();
+	}
+
+	public void init() {
+		cars = carNameInput.getCar();
+		numOfIteration = gameTimesInput.getGameTimes();
+	}
+
+	public void gameStart(){
+
+	}
 }
--- src/main/java/racingcar/game/RacingGameReferee.java
+++ src/main/java/racingcar/game/RacingGameReferee.java
@@ -1,2 +1,15 @@
-package racingcar.game;public class RacingGameReferee {
+package racingcar.game;
+
+import java.util.List;
+
+import racingcar.Car;
+import racingcar.utils.RandomNumbers;
+
+public class RacingGameReferee {
+	private static final int THRESHOLD = 4;
+
+	public void race(List<Car> cars) {
+		cars.stream()
+			.forEach(car -> car.moveOrStop(RandomNumbers.getRandomNumber(), THRESHOLD));
+	}
 }
