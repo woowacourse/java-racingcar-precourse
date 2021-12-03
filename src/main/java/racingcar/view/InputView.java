7975f9bf359dfc733a1ba0ffbe8ed5cedc9f31d6
Seungmin Lim <seungmin9066@gmail.com>
2021-12-03 13:46:29 +0900

    [추가] MVC패턴에 맞게 inputView 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,20 @@
-package racingcar.view;public class InputView {
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+import racingcar.util.Validator;
+
+public class InputView {
+
+	static Validator validator = new Validator();
+
+	public static String getStringInput() {
+		return readLine();
+	}
+
+	public static int getIntInput() {
+		String input = readLine();
+		validator.validateNumber(input);
+		return Integer.parseInt(input);
+	}
 }
