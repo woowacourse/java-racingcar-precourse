2b8f2fdbebd62d0db8007c9e2dce75520901ab70
Seungmin Lim <seungmin9066@gmail.com>
2021-12-03 13:47:35 +0900

    [추가] 에러를 검사하는 클래스 추가

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,2 +1,19 @@
-package racingcar.util;public class Validator {
+package racingcar.util;
+
+import static racingcar.constant.GameConstants.*;
+
+public class Validator {
+
+	public void validateNumber(String number) {
+		boolean isNumeric = number.matches(REGEX_EXPRESSION_OF_NUMBER);
+		if (!isNumeric) {
+			throw new IllegalArgumentException(VALIDATE_MESSAGE);
+		}
+	}
+
+	public void validateName(String name) {
+		if (name.length() > LENGTH_LIMIT) {
+			throw new IllegalArgumentException(NAMING_ERROR_MESSAGE);
+		}
+	}
 }
