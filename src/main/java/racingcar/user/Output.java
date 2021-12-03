bbb4b8491517233bafe17e92e35d7b3aaf516356
meatsby <meatsby@gmail.com>
2021-12-03 11:39:04 +0900

    feat: 자동차 이름을 포함한 이동 거리 출력 기능 구현

--- src/main/java/racingcar/user/Output.java
+++ src/main/java/racingcar/user/Output.java
@@ -1,7 +1,16 @@
 package racingcar.user;
 
+import racingcar.Car;
+
 public class Output {
 	private static final String DISTANCE = "-";
+	private static final String COLON = " : ";
+
+	private void printResult(Car car) {
+		System.out.print(car.getName() + COLON);
+		printDistance(car.getPosition());
+		System.out.println();
+	}
 
 	private void printDistance(int result) {
 		for (int i = 0; i < result; i++) {
