c6f86d70d2aa2e25e58f25b9e095118b684ea758
yoon <duddbs774@gmail.com>
2021-12-04 04:09:47 +0900

    refactor: 코드 리팩토링
    domain.Engines
     - 기본 생성자 코드 리팩토링(chars메서드로 스트림 반환 받아 바로 사용 코드 짧아짐)

--- src/main/java/racingcar/domain/Engines.java
+++ src/main/java/racingcar/domain/Engines.java
@@ -1,17 +1,13 @@
 package racingcar.domain;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Engines {
-    public static final String EMPTY_STRING = "";
-
     private final List<Engine> engines;
 
     public Engines(String value) {
-        this.engines = Arrays.stream(value.split(EMPTY_STRING)).mapToInt(Integer::parseInt).boxed()
-            .map(Engine::new).collect(Collectors.toList());
+        this.engines = value.chars().map(Character::getNumericValue).mapToObj(Engine::new).collect(Collectors.toList());
     }
 
     public boolean canOperate(int index) {
