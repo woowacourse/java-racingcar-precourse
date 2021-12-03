fa4369cbcdd6ff149b446e33e30a8bfe3f8c8bf6
hyeonic <dev.hyeonic@gmail.com>
2021-12-03 22:18:15 +0900

    test: InputView getNames 변경에 맞춰 검증 테스트 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,13 +1,17 @@
 package racingcar.view;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
@@ -20,10 +24,20 @@ class InputViewTest {
         return new InputView();
     }
 
+    static Stream<Arguments> getNamesMethodSourceProvider() {
+        return Stream.of(
+            arguments("pobi,woni,jun", 3),
+            arguments(",,,,", 5),
+            arguments(",pobi,", 3),
+            arguments(" , ,, , ", 5),
+            arguments("pobi", 1)
+        );
+    }
+
     @DisplayName("경주할 자동차 이름을 입력 받으면 쉼표를 기준으로 나눠 리스트를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"pobi,woni,jun"})
-    void getNames_inputNames_ListReturn(String inputNames) {
+    @MethodSource("getNamesMethodSourceProvider")
+    void getNames_inputNames_ListReturn(String inputNames, int size) {
         // given
         System.setIn(generateStream(inputNames));
         InputView inputView = generateInputView();
@@ -32,7 +46,9 @@ class InputViewTest {
         List<String> names = inputView.getNames();
 
         // then
+        System.out.println(names);
         assertThat(names).isInstanceOf(List.class);
+        assertThat(names.size()).isEqualTo(size);
     }
 
     @DisplayName("시도할 회수를 입력하면 검증을 거쳐 시도 회수를 반환한다.")
