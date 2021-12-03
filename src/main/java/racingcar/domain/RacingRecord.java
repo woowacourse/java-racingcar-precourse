36ddf37bc0fe3f616e3e4d7698e2f80899d45453
Wishoon <heonga2@gmail.com>
2021-12-03 22:46:45 +0900

    feat: 자동차 경주 게임 각 라운드 결과를 반환하는 기능 추가
    RacingRecord
    - getRacingRecord(): 기록 반환
    - driveRecord -> racingRecord로 이름 변경

--- src/main/java/racingcar/domain/RacingRecord.java
+++ src/main/java/racingcar/domain/RacingRecord.java
@@ -3,9 +3,13 @@ package racingcar.domain;
 import java.util.List;
 
 public class RacingRecord {
-	private List<Car> driveRecord;
+	private List<Car> racingRecord;
 
 	public RacingRecord(List<Car> driveRecord) {
-		this.driveRecord = driveRecord;
+		this.racingRecord = driveRecord;
+	}
+
+	public List<Car> getRacingRecord() {
+		return racingRecord;
 	}
 }
