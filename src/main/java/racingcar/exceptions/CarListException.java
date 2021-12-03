aec37e07e7a6f4914ae38a8516be22d88c01a1b3
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-12-03 11:14:15 +0900

    feat(CarListException) : 중복 입력된 자동차 이름에 대한 예외처리 기능 추가

--- src/main/java/racingcar/exceptions/CarListException.java
+++ src/main/java/racingcar/exceptions/CarListException.java
@@ -1,12 +1,27 @@
 package racingcar.exceptions;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CarListException {
 	private static String ERROR_MESSAGE_OVER = "[ERROR] 자동차 이름은 공백을 포함하여 5자리를 넘을 수 없습니다.\n";
 	private static String ERROR_MESSAGE_NULL = "[ERROR] 자동차 이름은 공백을 제외하고 최소 한자리 이상이어야 합니다.\n";
+	private static String ERROR_MESSAGE_OVERLAPPED = "[ERROR] 자동차 이름은 중복될 수 없습니다.\n";
+
 
 	public static void carListException(String[] carArray) {
+		isEmpty(carArray);
 		for (String car : carArray) {
 			checkCarNameLength(car);
+		}
+
+		isCarNameOverlapped(carArray);
+	}
+
+	//"", ",,"와 같은 경우 예외처리
+	private static void isEmpty(String[] carArray) {
+		if (carArray.length == 0) {
+			throw new IllegalArgumentException(ERROR_MESSAGE_NULL);
 		}
 	}
 
@@ -18,15 +33,27 @@ public class CarListException {
 			throw new IllegalArgumentException(ERROR_MESSAGE_OVER);
 		}
 
-		isEmpty(car);
+		isCarNameEmpty(car);
 	}
 
 	//"   "와 같은 입력은, 5자리 이하의 입력이지만 유효값(body)가 없어서 예외처리 필요
-	private static void isEmpty(String car) {
+	private static void isCarNameEmpty(String car) {
 		int carLengthWithoutSpace = car.trim().length();
 		if (carLengthWithoutSpace == 0) {
 			throw new IllegalArgumentException(ERROR_MESSAGE_NULL);
 		}
 	}
 
+	private static void isCarNameOverlapped(String[] carArray) {
+		int originLength = carArray.length;
+		int distinctLength =
+			Stream.of(carArray)
+				.map(carName -> carName.trim())
+				.distinct()
+				.collect(Collectors.toList())
+				.size();
+		if (originLength != distinctLength) {
+			throw new IllegalArgumentException(ERROR_MESSAGE_OVERLAPPED);
+		}
+	}
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,14 +1,11 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import racingcar.exceptions.CarListException;
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
