124623a7e75a2d4764586b45e8d42ed5dd0f0172
iamsseaa <homingsea@gmail.com>
2021-12-03 16:14:03 +0900

    feat: main 클래스에서 사용자에게 차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,14 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+
+        User user = new User();
+
+        System.out.println("경주할 자동차 이름은 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        user.enterCarName();
+        user.toNameArray();
+
+
     }
 }
