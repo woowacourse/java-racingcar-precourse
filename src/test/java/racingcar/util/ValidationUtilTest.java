4a16d949390dc11c1ad4c170858e0167b3ddd1f5
Wishoon <heonga2@gmail.com>
2021-12-03 20:07:17 +0900

    test: 자동차 이름들 입력에 대한 유효성 테스트 작성

---
+++ src/test/java/racingcar/util/ValidationUtilTest.java
@@ -0,0 +1,43 @@
+package racingcar.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ValidationUtilTest {
+
+	@ParameterizedTest
+	@ValueSource(strings = {"w ", "wq r", " w", " "})
+	void 입력값에_대한_빈칸_예외_처리(String value) {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ValidationUtil.isValidateBlank(value);
+		});
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {""})
+	void 입력값에_대한_공백_예외_처리(String value) {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ValidationUtil.isValidateNotInput(value);
+		});
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"abcdef", "abcdefg"})
+	void 입력값에_대한_길이_예외_처리(String value) {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ValidationUtil.isValidateLength(value);
+		});
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"abcd,abcd"})
+	void 입력값이_중복_된_경우_예외_처리(String value) {
+		String[] inputArr = value.split(",");
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			ValidationUtil.isValidateDuplicated(inputArr);
+		});
+	}
+}
