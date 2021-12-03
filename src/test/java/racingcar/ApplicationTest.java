f083b9efd49f7ad07a6dda03a205a85fcb9d9a9d
jongwon <54282927+loosie@users.noreply.github.com>
2021-12-03 18:27:36 +0900

    test: 이름에 대한 중복 예외 처리 테스트 추가
    Car 이름 중복 방지 기능 테스트 추가
    - 중복된 이름 입력시 NAME_DUPLICATED_ERROR_MESSAG 출력 테스트 성공

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -11,7 +11,8 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
-    private static final String NAME_ERROR_MESSAGE = "[ERROR] 이름은 5자 이하만 가능하다.";
+    private static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 이름은 5자 이하만 가능하다.";
+    private static final String NAME_DUPLICATED_ERROR_MESSAGE = "[ERROR] 중복된 이름은 사용할 수 없다.";
     private static final String NUMBER_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
 
     @Test
@@ -29,8 +30,8 @@ class ApplicationTest extends NsTest {
     void 전진_정지_복수우승() {
         assertRandomNumberInRangeTest(
             () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                run("pobi2,woni2", "1");
+                assertThat(output()).contains("pobi2 : -", "woni2 : -", "최종 우승자 : pobi2, woni2");
             },
             MOVING_FORWARD, MOVING_FORWARD
         );
@@ -40,10 +41,20 @@ class ApplicationTest extends NsTest {
     void 이름에_대한_예외_처리() {
          assertSimpleTest(
              () -> {
-                 runException("pobi,javaji");
-                 assertThat(output()).contains(NAME_ERROR_MESSAGE);
+                 runException("pobi3,javaji3");
+                 assertThat(output()).contains(NAME_LENGTH_ERROR_MESSAGE);
             }
          );
+    }
+
+    @Test
+    void 이름에_대한_중복_예외_처리() {
+        assertSimpleTest(
+            () -> {
+                runException("test,test");
+                assertThat(output()).contains(NAME_DUPLICATED_ERROR_MESSAGE);
+            }
+        );
     }
 
     @Test
