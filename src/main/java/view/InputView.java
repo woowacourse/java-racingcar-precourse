a6ebe21cb6b66c45ba9eebb07dfd08132dce97e7
Byeongju-Kong <bjuuuu98@gmail.com>
2021-12-03 19:53:19 +0900

    feat(InputView): 입력 안내문 출력 기능 구현
    기능 1. 입력 안내문을 출력한다.

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+public class InputView {
+    public void alertInputNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void alertInputCountOfAttempt() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+}
