56e20bbe2a55e2018ceed97c9a8f38293eb77d82
CptBluebear <cptbluebear@naver.com>
2021-12-04 01:38:12 +0900

    feat(Car.java): 무작위로 전진 성공여부 판정 기능 추가
    camp.nextstep.edu.missionutils.Randoms 클래스를 이용하여
    무작위의 숫자를 뽑아 해당 숫자를 기반으로
    자동차 전진 여부를 판별한다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public boolean isMoveSuccess() {
+		return Randoms.pickNumberInRange(GameData.START_NUMBER, GameData.END_NUMBER) >= GameData.CRITERIA_NUMBER;
+	}
 }
