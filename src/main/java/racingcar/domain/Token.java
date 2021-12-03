503080b3382640f97c146bf7b90683ab69182c0c
yoon <duddbs774@gmail.com>
2021-12-04 00:29:16 +0900

    feat: 예외발생 케이스 추가
    domain.Token
     - checkLessThanThreshold 메서드 구현
    domain.TokenTest
     - 토큰 생성시 임계값 보다 낮은 값에 대한 예외발생 테스트 추가

--- src/main/java/racingcar/domain/Token.java
+++ src/main/java/racingcar/domain/Token.java
@@ -1,11 +1,15 @@
 package racingcar.domain;
 
 public class Token {
+    public static final int ITERATION_MODIFIER = -1;
+    public static final String EMPTY_STRING = "";
+    public static final int THRESHOLD_NUMBER = 1;
+
     public static final String ERROR_STRING = "[ERROR]";
     public static final String ERROR_NULL_MENTION = ERROR_STRING + " 값을 입력해주세요.";
     public static final String ERROR_TYPE_MENTION = ERROR_STRING + " 시도 횟수는 숫자여야 한다.";
-    public static final int ITERATION_MODIFIER = -1;
-    public static final String EMPTY_STRING = "";
+    public static final String ERROR_LESS_THAN_THRESHOLD_MENTION = ERROR_STRING + " 입력값은 "
+        + THRESHOLD_NUMBER + " 이상이어야 합니다.";
 
     private int count;
 
@@ -22,6 +26,17 @@ public class Token {
         checkNull(value);
         checkEmpty(value.trim());
         checkType(value.trim());
+        checkLessThanThreshold(value.trim());
+    }
+
+    private void checkLessThanThreshold(String value) {
+        if(isLessThanThreshold(value)){
+            throw new IllegalArgumentException(ERROR_LESS_THAN_THRESHOLD_MENTION);
+        }
+    }
+
+    private boolean isLessThanThreshold(String value) {
+        return Integer.parseInt(value) < THRESHOLD_NUMBER;
     }
 
     private void checkEmpty(String value) {
--- src/test/java/racingcar/domain/TokenTest.java
+++ src/test/java/racingcar/domain/TokenTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
+import static racingcar.domain.Token.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,8 +9,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TokenTest {
-    public static final String ERROR_STRING = "[ERROR]";
-
     @DisplayName("토큰 생성시 유효하지 않은 값에 대한 예외발생")
     @ParameterizedTest
     @ValueSource(strings = {"", "a", "!", "%"})
@@ -30,5 +29,13 @@ public class TokenTest {
     @ValueSource(strings = {"1", "12", "123", "1234"})
     void tokenFromValidValue(String value) {
         Token.from(value);
+    }
+
+    @DisplayName("토큰 생성시 임계값 보다 낮을 경우 예외발생 테스트")
+    @Test
+    void tokenFromLessThanThreshold() {
+        String value = Integer.toString(THRESHOLD_NUMBER - 1);
+        assertThatThrownBy(() -> Token.from(value)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ERROR_LESS_THAN_THRESHOLD_MENTION);
     }
 }
