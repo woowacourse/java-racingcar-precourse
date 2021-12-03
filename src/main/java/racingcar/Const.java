4365895817d998262704844fc62d2d53e21771bb
yxxnghwan <younghwan960@gmail.com>
2021-12-04 03:03:47 +0900

    feat(Const, Validator): 예외케이스에 따라 메시지 다르게 출력하도록 변경

--- src/main/java/racingcar/Const.java
+++ src/main/java/racingcar/Const.java
@@ -4,7 +4,8 @@ public class Const {
 	private static final String ERROR_MESSAGE = "[ERROR]";
 
 	public static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	public static final String ERROR_INPUT_CAR_NAME_MESSAGE = ERROR_MESSAGE + " 자동차 이름은 5글자 이하여야 합니다.";
+	public static final String ERROR_INPUT_CAR_NAME_LENGTH = ERROR_MESSAGE + " 자동차 이름은 5글자 이하여야 합니다.";
+	public static final String ERROR_INPUT_CAR_NAME_EMPTY_OR_BLANK = ERROR_MESSAGE + " 자동차 이름은 빈 문자열이거나 공백만 있을 수 없습니다.";
 	public static final String NAME_SEPARATOR = ",";
 	public static final String ROUND_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 	public static final String ERROR_INPUT_ROUND_MESSAGE = ERROR_MESSAGE + " 시도 횟수는 숫자여야 합니다.";
--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -3,10 +3,10 @@ package racingcar;
 public class Validator {
 	public static String validateName(String name) {
 		if(isHighLength(name))
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(Const.ERROR_INPUT_CAR_NAME_LENGTH);
 
 		if(isEmptyOrBlank(name))
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(Const.ERROR_INPUT_CAR_NAME_EMPTY_OR_BLANK);
 
 		return name;
 	}
