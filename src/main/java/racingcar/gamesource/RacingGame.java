54413b3e8e5d59746e65c72bb50bf7807a9a7613
overnew <ehdls1303@gmail.com>
2021-12-03 18:04:11 +0900

    feat: RacingGame getMaxPosition(), getWinner(), printWinner() 구현
    private int getMaxPosition()
     - Car객체 중에 가장 큰 position 반환
    
    private List<Car> getWinners()
     - 우승한 Car객체를 List로 반환
    
    private void printWinners()
     - 최종 우승자를 출력

--- src/main/java/racingcar/gamesource/RacingGame.java
+++ src/main/java/racingcar/gamesource/RacingGame.java
@@ -7,17 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.InputCheck;
 
 public class RacingGame {
-    private List<Car> cars = new ArrayList<>();
-
-    private void gameInit(){
-        String input = Console.readLine();
-        List<String> names = InputCheck.parseNames(input);
-
-        for(String carName: names){
-            cars.add(new Car(carName));
-        }
-
-    }
+    private List<Car> racingCars = new ArrayList<>();
 
     public void playStart(){
         gameInit();
@@ -26,9 +16,19 @@ public class RacingGame {
         //추가 구현 예정
     }
 
+    private void gameInit(){
+        String input = Console.readLine();
+        List<String> names = InputCheck.parseNames(input);
+
+        for(String carName: names){
+            racingCars.add(new Car(carName));
+        }
+
+    }
+
     private void playNextRound(){
 
-        for(Car car: cars){
+        for(Car car: racingCars){
             car.drive();
         }
 
@@ -36,11 +36,51 @@ public class RacingGame {
 
     private void printAllPosition(){
 
-        for(Car car: cars){
+        for(Car car: racingCars){
             System.out.println(car.toString());
         }
 
         System.out.print('\n');
+    }
+
+    private void printWinners(){
+        List<Car> winners = getWinners();
+        StringBuilder winnersName = new StringBuilder("");
+
+        for(int i=0; i<winners.size()-1 ; ++i){
+            winnersName.append(winners.get(i).getName());
+            winnersName.append(", ");
+        }
+
+        String lastWinnerName = winners.get(winners.size()-1).getName();
+        winnersName.append(lastWinnerName);
+
+        System.out.println("최종 우승자 : " + winnersName);
+    }
+
+    private List<Car> getWinners(){
+        List<Car> winners = new ArrayList<>();
+        int maxPosition = getMaxPosition();
+
+        for(Car car: racingCars){
+
+            if(car.getPosition() == maxPosition){
+                winners.add(car);
+            }
+
+        }
+
+        return winners;
+    }
+
+    private int getMaxPosition(){
+        int maxPosition = 0;
+
+        for(Car car: racingCars){
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+
+        return maxPosition;
     }
 
 }
