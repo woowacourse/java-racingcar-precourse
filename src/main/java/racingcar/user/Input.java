f61a59c167b8aca5247cb86e25d30f9874b305d3
meatsby <meatsby@gmail.com>
2021-12-03 11:31:06 +0900

    feat: 라운드 횟수를 받는 기능 구현

--- src/main/java/racingcar/user/Input.java
+++ src/main/java/racingcar/user/Input.java
@@ -5,6 +5,7 @@ import racingcar.racinggame.Validation;
 
 public class Input {
 	private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String ROUND_INPUT_MESSAGE = "시도할 횟수는 몇회인가요?";
 	private static final String COMMA = ",";
 
 	private final Validation validation;
@@ -20,6 +21,19 @@ public class Input {
 				String[] carNames = Console.readLine().split(COMMA);
 				validation.isValid(carNames);
 				return carNames;
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public String inputRound() {
+		while (true) {
+			System.out.println(ROUND_INPUT_MESSAGE);
+			try {
+				String round = Console.readLine();
+				validation.isValid(round);
+				return round;
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
 			}
