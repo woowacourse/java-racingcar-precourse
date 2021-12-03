49f1f42a4722a4811eedda2e5661e4aa60a1d4b4
delphox60 <delphox60@gmail.com>
2021-12-03 12:53:57 +0900

    fix(CarEntry): CarEntry 클래스의 멤버 변수 carList 타입을List<Car>로 수정

--- src/main/java/racingcar/CarEntry.java
+++ src/main/java/racingcar/CarEntry.java
@@ -1,21 +1,22 @@
 package racingcar;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class CarEntry {
-    private final List<String> carList;
+    private final List<Car> carList;
     private final int nOfCars;
 
     public CarEntry(String carNames) {
         String[] carNameArray = getCarNameArray(carNames);
         nOfCars = carNameArray.length;
-        carList = new ArrayList<>(nOfCars);
+        carList = new ArrayList<Car>(nOfCars);
         for (int i = 0; i < nOfCars; i++) {
-            carList.add(i, carNameArray[i]);
+            carList.add(i, new Car(carNameArray[i]));
         }
     }
-
+    
     private String[] getCarNameArray(String carNames) {
         String[] carNameArray = carNames.split(",");
         for (int i = 0; i < nOfCars; i++) {
