f00a5bc62d0319fd72c36ed9b069ef606549198a
kanamycine <jinyeong5320@gmail.com>
2021-12-03 18:32:38 +0900

    fix(Constant) : 최종 우승 출력 텍스트 수정

--- src/main/java/racingcar/utils/Constant.java
+++ src/main/java/racingcar/utils/Constant.java
@@ -6,5 +6,5 @@ public class Constant {
 	public static String ASK_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static String ASK_GAME_COUNT = "시도할 회수는 몇회인가요?";
 	public static String PLAY_RESULT = "실행 결과";
-	public static String WINNER = "최종 우승자";
+	public static String WINNER = "최종 우승자 : ";
 }
