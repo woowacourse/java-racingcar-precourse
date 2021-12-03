5a2f284ca8350eb6d4167224528c79e988f5b8c0
hyeonic <dev.hyeonic@gmail.com>
2021-12-03 23:29:15 +0900

    feat: 에러 메시지 관리를 위한 ErrorMessage 추가

---
+++ src/main/java/racingcar/error/ErrorMessage.java
@@ -0,0 +1,18 @@
+package racingcar.error;
+
+public enum ErrorMessage {
+
+    CAR_NAME_NOT_VALID("[ERROR] 차 이름을 잘못 입력하였습니다."),
+    NUMBER_FORMAT_NOT_VALID("[ERROR] 시도 횟수는 숫자여야 한다."),
+    NEGATIVE_NUMBER_NOT_VALID("[ERROR] 시도 횟수는 음수가 아니어야 한다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
