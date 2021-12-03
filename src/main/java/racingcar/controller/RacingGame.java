52e1cef06db54cd284f4a909987028b721c6b282
yezyvibe <yeji0501@gmail.com>
2021-12-03 09:43:47 +0900

    feat(RandomNumber): pick random number and move the car forward

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,15 +1,51 @@
 package racingcar.controller;
 
 import static racingcar.view.InputView.*;
+import static racingcar.view.OutputView.*;
 
+
+import java.util.List;
+
+import racingcar.domain.Car;
+import racingcar.domain.RandomNumber;
 
 public class RacingGame {
 
 	public void run() {
 		printCarNameInputMessage();
-		inputCarName();
+		Car[] carName = createCarInstance(inputCarName());
 		printAttemptInputMessage();
-		inputAttemptNumber();
+		int attemptNumber = inputAttemptNumber();
+		printExecutionResult();
+		for (int i = 0; i < attemptNumber; i++) {
+			System.out.println(i + "번째 시도");
+			makeCarMove(carName);
+		}
+	}
+
+	public static Car[] createCarInstance(List<String> carNameList) {
+		Car[] carName = new Car[carNameList.size()];
+		for (int i = 0; i < carNameList.size(); i++) {
+			carName[i] = new Car(carNameList.get(i));
+		}
+		return carName;
+	}
+
+	public static void makeCarMove(Car[] carName) {
+		for (Car car : carName) {
+			if (createRandomNumberForCarMovement()) {
+				car.moveForward();
+				System.out.println(car.getName()+": " + car.getPosition() + "이동");
+			}
+		}
+	}
+
+	public static boolean createRandomNumberForCarMovement() {
+		int randomNumber = RandomNumber.pickRandomNumber();
+		if (randomNumber >= 4) {
+			return true;
+		}
+		return false;
 	}
 
 }
---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveForward() {
+        ++position;
+    }
+}
