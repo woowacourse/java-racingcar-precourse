d5410cddde7d91c387a1daa7d2e24a1846ef16dd
kwonchanmi <kwoncm1999@naver.com>
2021-12-04 01:51:16 +0900

    feat: n대의 자동차 입력 예외 처리
    n대의 자동차 입력시 발생하는 예외 처리
    - 조건 위배 시 예외 발생
    - 예외 발생 시 ERROR 문구 출력 후 다시 입력 진행

--- src/main/java/racingcar/domain/InputValidation.java
+++ src/main/java/racingcar/domain/InputValidation.java
@@ -5,15 +5,17 @@ import java.util.Arrays;
 public class InputValidation {
 
 	public static final long ZERO = 0;
+	public static final int CAR_NAME_LENGTH_MIN = 1;
+	public static final int CAR_NAME_LENGTH_MAX = 5;
 	public static final long CAR_NUM_MIN = 1;
 	public static final long CAR_NUM_MAX = 1000;
-	public static final String ERROR_CAR_NAME_LENGTH = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+	public static final String ERROR_CAR_NAME_LENGTH = "[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.";
 	public static final String ERROR_CAR_NUMBER = "[ERROR] 자동차 개수는 1개 이상 1,000개 이하만 가능합니다.";
 	public static final String ERROR_LAST_CHARACTER_REGEX = "[ERROR] 입력한 자동차 이름은 쉼표로 끝날 수 없습니다.";
 
 	public static void checkCarNameValidation(String input, String splitRegex) {
 		long carNameLengthValidation = Arrays.stream(input.split(splitRegex))
-			.filter(carName -> carName.length() > 5 || carName.length() < 1)
+			.filter(carName -> carName.length() > CAR_NAME_LENGTH_MAX || carName.length() < CAR_NAME_LENGTH_MIN)
 			.count();
 		long carNumberValidation = Arrays.stream(input.split(splitRegex)).count();
 		boolean lastCharacterRegex = input.endsWith(splitRegex);
