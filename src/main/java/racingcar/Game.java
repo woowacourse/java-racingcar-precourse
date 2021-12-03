5a29a42812f6ca6487fe48b4cb8657cfd8bdbc40
nailseong <nnis9857@gmail.com>
2021-12-04 03:53:50 +0900

    feat: 최종 우승자 리스트 생성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -3,6 +3,8 @@ package racingcar;
 import static racingcar.InputReader.*;
 import static racingcar.MessagePrinter.*;
 
+import java.util.ArrayList;
+
 public class Game {
 
 	private final Car[] cars;
@@ -31,12 +33,28 @@ public class Game {
 			proceedTurn();
 			printTurnResult(cars);
 		}
+
+		ArrayList<String> winnerList = createWinnerList();
+		printWinners(winnerList);
 	}
 
 	private void proceedTurn() {
 		for (Car car : cars) {
 			car.moveOrStay();
 		}
+	}
+
+	private ArrayList<String> createWinnerList() {
+		int winnerPosition = getWinnerPosition();
+
+		ArrayList<String> winnerList = new ArrayList<>();
+		for (Car car : cars) {
+			if (winnerPosition == car.getPosition()) {
+				winnerList.add(car.getName());
+			}
+		}
+
+		return winnerList;
 	}
 
 	private int getWinnerPosition() {
