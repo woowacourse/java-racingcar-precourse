be8d864f1d2df346272674cb6ad2d976a8f5d5e0
meatsby <meatsby@gmail.com>
2021-12-03 11:26:27 +0900

    feat: 자동차 이름이 중복인 예외 처리 기능 구현

--- src/main/java/racingcar/racinggame/Validation.java
+++ src/main/java/racingcar/racinggame/Validation.java
@@ -1,18 +1,32 @@
 package racingcar.racinggame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Validation {
 	private static final String ERROR_MESSAGE = "[ERROR] ";
 	private static final String BLANK_NAME_ERROR = "자동차 이름은 빈칸일 수 없습니다.";
+	private static final String DUPLICATE_NAME_ERROR = "자동차 이름은 중복될 수 없습니다.";
 
 	private static final int MIN_NUMBER_OF_CARS = 1;
 
 	public void isValid(String[] carNames) {
 		isEmpty(carNames);
+		isUnique(carNames);
 		}
 
 	private void isEmpty(String[] carNames) {
 		if (carNames.length < MIN_NUMBER_OF_CARS) {
 			throw new IllegalArgumentException(ERROR_MESSAGE + BLANK_NAME_ERROR);
+		}
+	}
+
+	private void isUnique(String[] carNames) {
+		ArrayList<String> names = new ArrayList<>(Arrays.asList(carNames));
+		HashSet<String> unique = new HashSet<>(names);
+		if (names.size() != unique.size()) {
+			throw new IllegalArgumentException(ERROR_MESSAGE + DUPLICATE_NAME_ERROR);
 		}
 	}
 }
