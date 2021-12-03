3d273aaf426942061dd7463a1c7ada211d253eff
yoon <duddbs774@gmail.com>
2021-12-04 03:52:57 +0900

    test: 테스트 코드 리팩토링
    domain.TrackTest
     - n 대신 System.lineSeparator() 사용(운영체제 호환문제)

--- src/test/java/racingcar/domain/TrackTest.java
+++ src/test/java/racingcar/domain/TrackTest.java
@@ -9,26 +9,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TrackTest {
+    public static final String NEW_LINE = System.lineSeparator();
+
     Track track;
 
     @BeforeEach
     void setUp() {
-        this.track = new Track("1,2,3");
+        this.track = Track.createByCarsName("1,2,3");
     }
 
     @DisplayName("1라운드 경기내용 문자열 반환 테스트")
     @Test
     void getOneRoundGameRecord() {
-        track.play(new EngineRepository(Arrays.asList("444")));
-        assertThat(track.getGameRecord()).isEqualTo("1 : -\n2 : -\n3 : -\n\n");
+        track.play(new EngineRepository(Arrays.asList("034")));
+        assertThat(track.getGameRecord())
+            .isEqualTo("1 : " + NEW_LINE + "2 : " + NEW_LINE + "3 : -" + NEW_LINE + NEW_LINE);
+
     }
 
     @DisplayName("2라운드 경기내용 문자열 반환 테스트")
     @Test
     void getTwoRoundsGameRecord() {
-        track.play(new EngineRepository(Arrays.asList("456", "999")));
+        track.play(new EngineRepository(Arrays.asList("345", "980")));
         assertThat(track.getGameRecord())
-            .isEqualTo("1 : -\n2 : -\n3 : -\n\n1 : --\n2 : --\n3 : --\n\n");
+            .isEqualTo("1 : " + NEW_LINE + "2 : -" + NEW_LINE + "3 : -" + NEW_LINE + NEW_LINE
+                + "1 : -" + NEW_LINE + "2 : --" + NEW_LINE + "3 : -" + NEW_LINE + NEW_LINE);
     }
 
     @DisplayName("3라운드 경기내용 문자열 반환 테스트")
@@ -36,6 +41,8 @@ public class TrackTest {
     void getThreeRoundsGameRecord() {
         track.play(new EngineRepository(Arrays.asList("456", "789", "012")));
         assertThat(track.getGameRecord())
-            .isEqualTo("1 : -\n2 : -\n3 : -\n\n1 : --\n2 : --\n3 : --\n\n1 : --\n2 : --\n3 : --\n\n");
+            .isEqualTo("1 : -" + NEW_LINE + "2 : -" + NEW_LINE + "3 : -" + NEW_LINE + NEW_LINE
+                + "1 : --" + NEW_LINE + "2 : --" + NEW_LINE + "3 : --" + NEW_LINE + NEW_LINE
+                + "1 : --" + NEW_LINE + "2 : --" + NEW_LINE + "3 : --" + NEW_LINE + NEW_LINE);
     }
 }
