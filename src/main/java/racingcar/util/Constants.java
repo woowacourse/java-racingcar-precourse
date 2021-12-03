86b97f725f7dcaaa7e739b9c8be704d739da2dab
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-03 14:07:14 +0900

    feat: 자동차 이름 길이가 1 이하 예외 처리 기능

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -2,8 +2,10 @@ package racingcar.util;
 
 public class Constants {
 	public static final int MAX_CAR_NAME_LENGTH = 5;
+	public static final int MIN_CAR_NAME_LENGTH = 1;
 	public static final String COMMA = ",";
 
 	public static final String ERROR = "[ERROR]";
 	public static final String ERROR_CAR_NAME_LENGTH_OVER = ERROR + " 자동차 이름은 " + MAX_CAR_NAME_LENGTH + "이하여야 합니다.";
+	public static final String ERROR_CAR_NAME_LENGTH_UNDER = ERROR + " 자동차 이름은 " + MIN_CAR_NAME_LENGTH + "이상이어야 합니다.";
 }
--- src/main/java/racingcar/util/ValidChecker.java
+++ src/main/java/racingcar/util/ValidChecker.java
@@ -7,6 +7,8 @@ public class ValidChecker {
 	public static void isValidCarNameLength(String carName) {
 		if (carName.length() > MAX_CAR_NAME_LENGTH) {
 			throwIllegalArgumentException(ERROR_CAR_NAME_LENGTH_OVER);
+		} else if (carName.length() < 1) {
+			throwIllegalArgumentException(ERROR_CAR_NAME_LENGTH_UNDER);
 		}
 	}
 
