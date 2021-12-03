9cda319b436c7b9b627c8827b96e9ed6bc8cf1a6
awesomeo184 <awesomeo184@gmail.com>
2021-12-04 00:38:46 +0900

    refactor(Car): Car 클래스에서 이름 검증 책임을 제거한다.
    현재 Car 인스턴스를 생성하는 책임은 CarFactory가 지고 있으므로 차 생성의 제약조건도 CarFactory에 존재한다. 따라서 제약 조건이 중복해서 나타나는 것을 방지하기 위해 Car 객체에서 이름의 제약조건과 검증 로직을 제거한다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,20 +1,11 @@
 package racingcar.domain;
 
 public class Car {
-    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
-        checkNameLength(name);
         this.name = name;
-    }
-
-    // 추가 기능 구현
-    private void checkNameLength(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하여야 합니다. 현재 자동차 이름 = " + name);
-        }
     }
 
     public boolean attemptMove(int number) {
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -7,14 +7,6 @@ import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
-    @Test
-    @DisplayName("자동차의 이름 길이가 5를 초과하면 예외를 던진다.")
-    void name_length_over_max_length_throws_Exception() {
-        String name = "overMax";
-        assertThatThrownBy(() -> {
-            new Car(name);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
 
     @Test
     @DisplayName("입력된 숫자가 4이상인 경우 position을 증가시킨다.")
