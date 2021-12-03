48ba510213f4461ce28713ae87ad889a1ccb352f
Dong Geun <dlehdrms978@gmail.com>
2021-12-03 18:10:27 +0900

    경주를 진행하는 기능 : '자동차의 전진을 결정' 구현 완료

---
+++ src/main/java/racingcar/RaceStarter.java
@@ -0,0 +1,63 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceStarter {
+    InputValidator inputValidator = new InputValidator();
+    List<Car> carList;
+    int tryNumber=0;
+    public RaceStarter() {
+
+    }
+    public void start(){
+        List<String> carNameList = getCarNamesFromUser();
+        tryNumber = getTryNumberFromUser();
+        initCarList(carNameList);
+        play();
+
+    }
+    private void play(){
+        for(int i = 0 ; i < tryNumber ; i++ ){
+            increaseCarsPosition();
+
+        }
+    }
+
+    private void increaseCarsPosition(){
+        carList.forEach(car -> {
+            int random = Randoms.pickNumberInRange(0,9);
+            if(random >=4) car.increasePosition();
+        });
+    }
+    private void initCarList(List<String> carNamesList){
+        List<Car> cars = new ArrayList<>();
+        carNamesList.forEach((carName)->{
+            Car car = new Car(carName);
+            cars.add(car);
+        });
+    }
+    private List<String> getCarNamesFromUser(){
+        while (true){
+            String inputCarNames = Console.readLine();
+            try{
+                return  inputValidator.validateCarName(inputCarNames);
+            }catch (Exception e){
+                System.out.println("[ERROR] 잘못된 자동차 이름입니다!");
+            }
+        }
+    }
+    private int getTryNumberFromUser(){
+        while (true){
+            String tryNumber = Console.readLine();
+            try{
+                return inputValidator.validateTryNumber(tryNumber);
+            }catch (Exception e){
+                System.out.println("[ERROR] 잘못된 자동차 이름입니다!");
+            }
+        }
+    }
+}
