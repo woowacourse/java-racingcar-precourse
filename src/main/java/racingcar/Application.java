b9ee5ad850282d40c14c668fc8459b850c333e63
EastHShin <xldk78@gmail.com>
2021-12-04 06:47:53 +0900

    feat(racingcar): 게임 진행 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,18 @@
 package racingcar;
 
+import java.util.List;
+
+import racingcar.game.RacingGame;
 import racingcar.utils.CarNameInput;
+import racingcar.utils.GameTimesInput;
+import racingcar.utils.validator.CarNameValidator;
 
 public class Application {
 	public static void main(String[] args) {
 		// TODO 구현 진행
-
+		RacingGame racingGame = new RacingGame();
+		racingGame.init();
+		racingGame.gameStart();
+		racingGame.awards();
 	}
 }
