6f80674d2d3ced9e6f5dede1a534347c9a16acd2
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-03 20:05:22 +0900

    test<NamingProcessTest>: 클래스내 메소드 모두 테스트
    - 이름을 제대로 파싱하여 문자열 배열을 반환하는지
    - 이름이 중복된 경우 제대로 예외를 던지는지
    - 입력값을 넣었을 때 Car 배열을 반환하는지
    
    모두 테스트 검증 완료했습니다.

---
+++ src/test/java/racingcar/service/NamingProcessTest.java
@@ -0,0 +1,47 @@
+package racingcar.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import racingcar.domain.Car;
+
+class NamingProcessTest {
+
+	@Test
+	void 이름_파싱() {
+		// given
+		String input = "pobi,woni,jun";
+
+		// when
+		String[] parsedName = NamingProcess.parseString(input);
+
+		// then
+		assertArrayEquals(new String[] {"pobi", "woni", "jun"}, parsedName);
+	}
+
+	@Test
+	void 이름_중복_예외() {
+		// given
+		String input = "pobi, jun, jun";
+
+		// then
+		assertThrows(IllegalArgumentException.class, () -> NamingProcess.parseString(input));
+	}
+
+	@Test
+	void 자동차_이름_리스트반환() {
+		// given
+		String input = "pobi,woni,jun";
+
+		// when
+		Car[] cars = NamingProcess.StringToArray(input);
+
+		// then
+		assertThat(cars.length).isEqualTo(3);
+		assertThat(cars[0].getName()).isEqualTo("pobi");
+		assertThat(cars[1].getName()).isEqualTo("woni");
+		assertThat(cars[2].getName()).isEqualTo("jun");
+	}
+}
