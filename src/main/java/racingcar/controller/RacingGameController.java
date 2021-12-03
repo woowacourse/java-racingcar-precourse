d76a94d716b7d98d6e90a3d31dd8a65a260a5788
Seungmin Lim <seungmin9066@gmail.com>
2021-12-03 13:51:01 +0900

    [추가] MVC 패턴에 맞게 컨트롤러 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,2 +1,42 @@
-package racingcar.controller;public class RacingGameController {
+package racingcar.controller;
+
+import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingGameController {
+
+	private final RacingGame racingGame = new RacingGame();
+	private int rounds;
+
+	public void startGame() {
+		initialize();
+		racingGame.run(rounds);
+		racingGame.getResult();
+	}
+
+	private void initialize() {
+		setUpParticipants();
+		setupRounds();
+	}
+
+	private void setUpParticipants() {
+		try {
+			OutputView.printAskingCarNames();
+			racingGame.addParticipants(InputView.getStringInput());
+		} catch (IllegalArgumentException error) {
+			System.out.println(error.getMessage());
+			setUpParticipants();
+		}
+	}
+
+	private void setupRounds() {
+		try {
+			OutputView.printAskingRounds();
+			rounds = InputView.getIntInput();
+		} catch (IllegalArgumentException error) {
+			System.out.println(error.getMessage());
+			setupRounds();
+		}
+	}
 }
