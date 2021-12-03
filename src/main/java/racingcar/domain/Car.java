766a3674f2d3f6b5bd0ed1d44c0c6e47f38fc2d7
yoon <duddbs774@gmail.com>
2021-12-04 04:32:44 +0900

    refactor: compareTo 메소드 코드 리팩토링
    Integer.compare 메서드로 position값 비교하도록 함 불필요한 상수 제거 및 코드 간결해짐

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,14 +6,13 @@ public class Car implements Comparable<Car> {
     public static final String DELIMITER = " : ";
     public static final int START_INDEX = 0;
     public static final String STRING_POSITION = "-";
-    public static final int SAME_POSITION = 0;
-    public static final int HIGHER_POSITION = 1;
-    public static final int LOWER_POSITION = -1;
 
     private final String name;
     private int position = 0;
 
-    public Car(String name) { this.name = name; }
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void move() {
         position++;
@@ -33,19 +32,13 @@ public class Car implements Comparable<Car> {
         return stringBuilder.toString();
     }
 
-    public boolean isMaxPosition(int maxPosition) {
-        return position == maxPosition;
+    public boolean isSamePosition(int position) {
+        return this.position == position;
     }
 
     @Override
     public int compareTo(Car car) {
-        if (this.position > car.getPosition()) {
-            return HIGHER_POSITION;
-        }
-        if (this.position < car.getPosition()) {
-            return LOWER_POSITION;
-        }
-        return SAME_POSITION;
+        return Integer.compare(this.position, car.getPosition());
     }
 
     @Override
