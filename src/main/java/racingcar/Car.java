589e006d302666c5365be2f785bfd651ff862c2c
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-12-03 13:31:29 +0900

    feat: 자동차 전진기능 및 각 차수별 출력기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +10,21 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void moveForward() {
+        if (canMove()) {
+            this.position += 1;
+        }
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,8 +9,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class RacingGame {
     private static final String MSG_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MSG_INPUT_ATTEMPTS_NUM = "시도할 회수는 몇회인가요?";
+    private static final String MSG_PRINT_EXECUTE_RESULT = "실행 결과";
 
-    private List<Car> cars;
+    private final List<Car> cars;
 
     public RacingGame() {
         this.cars = new ArrayList<>();
@@ -19,6 +20,22 @@ public class RacingGame {
     public void play() {
         this.createCarObjByName(this.inputCarNames());
         int numberOfAttempts = this.inputNumberOfAttempts();
+        System.out.println(MSG_PRINT_EXECUTE_RESULT);
+        while (numberOfAttempts-- > 0) {
+            for (Car car : this.cars) {
+                car.moveForward();
+                this.printEachAttempts(car.getName(), car.getPosition());
+            }
+            System.out.println();
+        }
+    }
+
+    private void printEachAttempts(String name, int position) {
+        System.out.print(name + " : ");
+        while (position-- > 0) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     private void createCarObjByName(List<String> carNameList) {
