b9203728f7cf73b254a66106cd417a36f06f72da
CptBluebear <cptbluebear@naver.com>
2021-12-04 01:26:00 +0900

    feat(GameData.java): 프로그램에 필요한 상수 및 문자열 추가

---
+++ src/main/java/racingcar/GameData.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class GameData {
+
+	public static final int START_NUMBER = 0;
+	public static final int END_NUMBER = 9;
+	public static final int CRITERIA_NUMBER = 4;
+
+	public static final String WINNER_MESSAGE = "최종 우승자 : ";
+	public static final String CARNAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String TRY_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+	public static final String INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
+
+}
