a722f0a2744cfa4d423cc0acabc2f3d3b4b18036
overnew <ehdls1303@gmail.com>
2021-12-03 20:28:41 +0900

    feat: ConstMessage class 구현
    출력에 사용되는 메시지들을 상수화하여 저장

---
+++ src/main/java/racingcar/constants/ConstMessage.java
@@ -0,0 +1,13 @@
+package racingcar.constants;
+
+public class ConstMessage {
+    //입력 요청
+    public static final String NAMES_INPUT_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ROUND_NUMBER_INPUT_REQUEST = "시도할 회수는 몇회인가요?";
+
+    //게임 정보
+    public static final String PLAY_RESULT_MESSAGE = "실행 결과\n";
+    public static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    public static final String NAME_POSITION_DELIMITER = " : ";
+    public static final String NAME_AND_NAME_DELIMITER = ", ";
+}
