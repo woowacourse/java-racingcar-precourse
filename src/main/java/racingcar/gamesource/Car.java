f0e445e6014f06571bc8ab71b9379e7b20ee35d9
overnew <ehdls1303@gmail.com>
2021-12-03 13:12:35 +0900

    feat: Car class getter함수, drive(), toString() 구현
    객체의 필드값을 가져우는 getter함수 구현
     - getName(), getPosition()
    
    position을 전진시키는 drive() 함수 구현
     - random한 수가 특정값 이상일때 position을 1 증가
    
    toString() 오버라이드
     - 객체의 이름과 position을 '-'로 표현해 반환

--- src/main/java/racingcar/gamesource/Car.java
+++ src/main/java/racingcar/gamesource/Car.java
@@ -1,4 +1,6 @@
-package racingcar;
+package racingcar.gamesource;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private final String name;
@@ -8,5 +10,31 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPosition(){
+        return this.position;
+    }
+
+    public void drive(){
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+
+        if(randomNumber >=4){
+            ++position;
+        }
+
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder positionToHyphens = new StringBuilder("");
+
+        for(int i=0; i<position ; ++i){
+            positionToHyphens.append('-');
+        }
+
+        return this.name + " : " + positionToHyphens;
+    }
 }
