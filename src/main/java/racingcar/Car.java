d8f7435dab7555ae979d4477dab3015f5f30b778
Dong Geun <dlehdrms978@gmail.com>
2021-12-03 18:27:36 +0900

    getter 및 포지션 증가 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,6 +7,14 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
-
+    public void increasePosition(){
+        position++;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getPosition(){
+        return  this.position;
+    }
     // 추가 기능 구현
 }
