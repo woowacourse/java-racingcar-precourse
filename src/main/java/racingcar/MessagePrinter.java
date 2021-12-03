209a2b39fa0adde74fb0ba09affdaadb009e1018
nailseong <nnis9857@gmail.com>
2021-12-03 20:59:10 +0900

    feat: 자동차의 포지션을 문자열로 반환한다.

--- src/main/java/racingcar/MessagePrinter.java
+++ src/main/java/racingcar/MessagePrinter.java
@@ -3,8 +3,9 @@ package racingcar;
 public class MessagePrinter {
 
 	public static final String ASK_FOR_REPETITIONS = "시도할 회수는 몇회인가요?";
-	private static final String ASK_FOR_CAR_NAME_LIST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String START_MESSAGE = "실행 결과";
+	private static final String ASK_FOR_CAR_NAME_LIST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String POSITION_INDICATOR = "-";
 
 	public static void printError(IllegalArgumentException exception) {
 		System.out.println("[ERROR] " + exception.getMessage());
@@ -20,6 +21,18 @@ public class MessagePrinter {
 
 	public static void printStartMessage() {
 		System.out.println(START_MESSAGE);
+	}
+
+	private static String createPositionMessage(Car car) {
+		StringBuilder positionMessage = new StringBuilder();
+
+		positionMessage.append(car.getName());
+		positionMessage.append(" : ");
+		for (int i = 0; i < car.getPosition(); i++) {
+			positionMessage.append(POSITION_INDICATOR);
+		}
+
+		return positionMessage.toString();
 	}
 
 }
