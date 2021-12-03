d7a4f6b7199aa24addeeed768df5d28e0d51345a
dls4585 <dls4585@naver.com>
2021-12-03 14:58:13 +0900

    test: 횟수 입력 테스트
    시도 횟수 빈칸, 숫자가 아닌 것 테스트

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -17,13 +17,39 @@ public class RacingCarTest {
 //        }).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 횟수_입력_성공() {
+        assertThat(validateCount("5"))
+                .contains("true");
+    }
+
+    @Test
+    void 횟수_입력_예외() {
+        assertThat(validateCount("다섯번"))
+                .startsWith("[ERROR]")
+                .contains("[ERROR]");
+        
+        assertThat(validateCount(""))
+                .startsWith("[ERROR]")
+                .contains("[ERROR]");
+    }
+
+    public String validateCount(String count) {
+        try {
+            Integer.parseInt(count);
+        } catch (NumberFormatException e) {
+            return "[ERROR] 시도 횟수는 숫자여야 한다.";
+        }
+        return "true";
+    }
+
     public String validateName(String[] names) {
         for (String name : names) {
-            if(name.length() > 5) {
+            if (name.length() > 5) {
 //                throw new IllegalArgumentException();
                 return "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
             }
-            if(name.length() == 0) {
+            if (name.length() == 0) {
 //                throw new IllegalArgumentException();
                 return "[ERROR] 자동차 이름은 빈 문자열일 수 없습니다.";
             }
