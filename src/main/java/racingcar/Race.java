3f7baf134efc85f55455d8f88f9a800f837ff49f
최창준 <aowert6518@gmail.com>
2021-12-03 17:29:59 +0900

    [feat] 사용자 입력 안내 문구 출력하기
    - 함수 사이 출력문 삽입

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -16,6 +16,7 @@ public class Race {
     }
 
     public static void getCarByName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = getUserInput();
         String[] carNames = userInput.split(",");
         for (String carName : carNames) {
@@ -25,6 +26,7 @@ public class Race {
     }
 
     public static void getMoveCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
         String userInput = getUserInput();
         moveCount = Integer.parseInt(userInput);
     }
@@ -32,11 +34,14 @@ public class Race {
     public void run() {
         getCarByName();
         getMoveCount();
+        System.out.println();
+        System.out.println("실행 결과");
         while (moveCount > 0) {
             for (Car car : cars) {
                 car.move();
                 car.printPosition();
             }
+            System.out.println();
             moveCount--;
         }
         calculateMaxDistance();
@@ -64,7 +69,7 @@ public class Race {
         for (int i = 0; i < winnerCount; i++) {
             winnerCarNames[i] = winnerCars.get(i).getName();
         }
-        System.out.println("최종 우승자 : " + String.join(",", winnerCarNames));
+        System.out.println("최종 우승자 : " + String.join(", ", winnerCarNames));
     }
 
 }
