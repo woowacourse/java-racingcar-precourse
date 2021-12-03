2b34c2d591c9b0daff869058c52a53fef203e566
yoon <duddbs774@gmail.com>
2021-12-04 02:31:27 +0900

    feat: 입출력값과 연동해서 게임 실행 기능 추가
    Application
     - init 메서드 구현(domain.GameBoy 생성시 예외 처리 기능)
     - createToken 메서드 구현(domain.Token 생성시 예외 처리 기능)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import racingcar.domain.GameBoy;
+import racingcar.domain.Token;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        GameBoy gameBoy = init();
+        Token token = createToken();
+        gameBoy.insertToken(token);
+        gameBoy.play();
+        OutputView.print(gameBoy.showResult());
+    }
+
+    private static GameBoy init() {
+        try {
+            return new GameBoy(InputView.inputNames());
+        } catch (IllegalArgumentException e) {
+            OutputView.print(e.getMessage());
+            return init();
+        }
+    }
+
+    private static Token createToken() {
+        try {
+            return Token.from(InputView.inputCount());
+        } catch (IllegalArgumentException e) {
+            OutputView.print(e.getMessage());
+            return createToken();
+        }
     }
 }
