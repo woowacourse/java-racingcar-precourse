94a9836656c9fb1df6e9fe398081cf2052f739d1
awesomeo184 <awesomeo184@gmail.com>
2021-12-03 23:47:33 +0900

    feat(Application): 사용자 입력을 받고 검증하는 기능 추가
    - 자동차 이름을 입력받고 각 이름의 길이가 5를 초과할 경우 에러 메시지를 출력하고 입력을 다시 받는다.
    - 시도횟수를 입력받고 시도횟수가 숫자가 아니거나 음수일 경우 에러 메시지를 출력하고 입력을 다시 받는다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,62 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        String[] seperatedNames;
+        String trialInput;
+        do {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carNamesInput = Console.readLine();
+            seperatedNames = carNamesInput.split(",");
+        } while (isIllegalName(seperatedNames));
+
+        do {
+            System.out.println("시도할 횟수는 몇회인가요?");
+            trialInput = Console.readLine();
+        } while (isIllegalTrial(trialInput));
+    }
+
+    private static boolean isIllegalTrial(String trialInput) {
+        try {
+            assertNumericOtherwiseRethrowException(trialInput);
+            assertPositiveValue(trialInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
+        }
+        return false;
+    }
+
+    private static void assertPositiveValue(String trialInput) {
+        int intValue = Integer.parseInt(trialInput);
+        if (intValue < 0) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 양수여야 합니다.");
+        }
+    }
+
+    private static void assertNumericOtherwiseRethrowException(String trialInput) {
+        try {
+            Integer.parseInt(trialInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+        }
+
+    }
+
+    private static boolean isIllegalName(String[] seperatedNames) {
+        try {
+            for (String name : seperatedNames) {
+                if (name.length() > 5) {
+                    throw new IllegalArgumentException("[ERROR] 자동차 이름의 크기는 5를 초과할 수 없습니다. 현재 자동차 이름 = " + name);
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
+        }
+        return false;
     }
 }
