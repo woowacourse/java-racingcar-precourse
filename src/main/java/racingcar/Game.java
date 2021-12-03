273d95b2aecc2adfe86cb68758f0ae0db50eb9f2
June <tkdwns159@naver.com>
2021-12-03 14:05:47 +0900

    feat(Game): 자동차 생성시키기, 시도 회수 입력받기 기능 추가
    시도 회수 입력 받을 시 예외 상황:
    - 입력 String의 각 character가 0~9가 아닐 때
    
    099와 같이 숫자 가장 앞자리에 0이 등장하여도 숫자로 변환했을 시
    0이 사라지고 정상적인 숫자로 변환되는 것을 확인하였음.

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -4,14 +4,22 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class Game {
-	private List<Car> participants;
+	private final List<Car> participants;
+	private final int numberOfTrials;
 
 	Game() {
 		List<String> carNames = getCarNamesFromUser();
+		if (!validateCarNames(carNames)) {
+			throw new IllegalArgumentException();
+		}
+
+		participants = carNames.stream().map(Car::new).collect(Collectors.toList());
+		numberOfTrials = getNumberOfTrials();
 	}
 
 	private List<String> getCarNamesFromUser() {
@@ -19,6 +27,16 @@ public class Game {
 		String inputOfCarNames = Console.readLine();
 		String[] names = inputOfCarNames.split(",");
 		return Arrays.asList(names);
+	}
+
+	private int getNumberOfTrials() {
+		System.out.println("시도할 회수는 몇회인가요?");
+		String inputOfNumberOfTrials = Console.readLine();
+		if (!validateNumberOfTrials(inputOfNumberOfTrials)) {
+			throw new IllegalArgumentException();
+		}
+
+		return Integer.parseInt(inputOfNumberOfTrials);
 	}
 
 	private boolean validateCarNames(List<String> names) {
@@ -32,6 +50,19 @@ public class Game {
 				return false;
 			}
 		}
+
 		return true;
 	}
+
+	private boolean validateNumberOfTrials(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			int number = input.charAt(i) - '0';
+			if (number < 0 || number > 9) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 }
