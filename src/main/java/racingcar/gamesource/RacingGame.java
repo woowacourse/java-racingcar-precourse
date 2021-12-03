7cee9c83938548c9172735f6d0dbd04edde3f912
overnew <ehdls1303@gmail.com>
2021-12-03 18:31:00 +0900

    feat: RacingGame playStart() 내부 구현
    public void playStart()
     - 구현해 놓은 함수를 이용해 내부 구현
     - 시도 횟수를 입력받음 -> 추후 다른 함수로 추출 예정

--- src/main/java/racingcar/gamesource/RacingGame.java
+++ src/main/java/racingcar/gamesource/RacingGame.java
@@ -11,12 +11,24 @@ public class RacingGame {
 
     public void playStart(){
         gameInit();
-        int roundNum =0;
 
-        //추가 구현 예정
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = Console.readLine();
+        int roundNum = Integer.parseInt(input);
+
+        System.out.println("실행 결과\n");
+
+        while((roundNum--) != 0){
+            playNextRound();
+            printAllPosition();
+        }
+
+        printWinners();
     }
 
     private void gameInit(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         String input = Console.readLine();
         List<String> names = InputCheck.parseNames(input);
 
