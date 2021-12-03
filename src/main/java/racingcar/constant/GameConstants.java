ec90e123fd34b0f335815213bc872a12cc7842d0
Seungmin Lim <seungmin9066@gmail.com>
2021-12-03 13:50:19 +0900

    [추가] 상수를 관리하는 클래스 추가

--- src/main/java/racingcar/constant/GameConstants.java
+++ src/main/java/racingcar/constant/GameConstants.java
@@ -1,2 +1,22 @@
-package racingcar.constant;public class GameConstants {
+package racingcar.constant;
+
+public class GameConstants {
+
+	public static final String ASKING_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준로 구분)";
+	public static final String ASKING_TRY_ROUNDS_MESSAGE = "시도할 회수는 몇회인가요?";
+	public static final String NAMING_ERROR_MESSAGE = "[ERROR] 길이는 5글자 이내로 입력해야 합니다.";
+	public static final int LENGTH_LIMIT = 5;
+	public static final String VALIDATE_MESSAGE = "[ERROR] 숫자만 입력이 가능합니다.";
+	public static final String GAME_EXECUTION_RESULT_MESSAGE = "실행 결과";
+	public static final String WINNER_MESSAGE = "최종 우승자 : ";
+	public static final String CAR_SHAPE = "-";
+	public static final String WINNER_DISTINGUISH = ", ";
+	public static final String NAME_DISTINGUISH = ",";
+	public static final String REGEX_EXPRESSION_OF_NUMBER = "[+-]?\\d*(\\.\\d+)?";
+
+	public static final int START_INCLUSIVE = 0;
+	public static final int END_INCLUSIVE = 9;
+	public static final int STANDARD_OF_MOVEMENT = 4;
+	public static final int MOVING_DISTANCE = 1;
+
 }
