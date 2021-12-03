df5fb5fe5620b7cfd8109e6cf454a0ac73f8eb06
overnew <ehdls1303@gmail.com>
2021-12-03 20:31:05 +0900

    feat: ConstValue class 구현
    게임에 상용되는 불변 값을 상수화하여 저장

---
+++ src/main/java/racingcar/constants/ConstValue.java
@@ -0,0 +1,8 @@
+package racingcar.constants;
+
+public class ConstValue {
+    public static final int RAMDOM_NUMBER_BEGIN = 0;
+    public static final int RAMDOM_NUMBER_END = 9;
+    public static final int DRIVE_POSSIBLE_BOUND = 4;
+
+}
