b880384c9accbc8921b494489d9d1ce2ca9782b8
Dong Geun <dlehdrms978@gmail.com>
2021-12-03 17:15:58 +0900

    사용자의 입력이 유효한지 처리하는 기능 구현 완료

---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,41 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class InputValidator {
+    static final int MAX_CAR_NAME_LENGTH = 5;
+    static final int MIN_CAR_NAME_LENGTH = 1;
+    public InputValidator(){
+
+    }
+    public void validateCarName(String inputCarNames){
+        List<String> carNames = splitAndTrim(inputCarNames);
+        if(checkCarNameLength(carNames) == false) throw new IllegalArgumentException();
+    }
+    public void validateTryNumber(String inputTryNumber){
+        try {
+            int number = Integer.parseInt(inputTryNumber);
+            if( number < 0 ) throw new IllegalArgumentException();
+        }catch (Exception e){
+            throw new IllegalArgumentException();
+        }
+    }
+    private List<String> splitAndTrim(String inputCarNames){
+        String carNames[] = inputCarNames.split(",");
+        for (int i = 0; i < carNames.length ; i++ ){
+            String carName = carNames[i];
+            carNames[i] = carName.trim();
+        }
+        System.out.println("carNames.toString() = " + carNames.toString());
+        return Arrays.asList(carNames);
+    }
+    private boolean checkCarNameLength(List<String> carNames){
+        long errorNumber = carNames.stream().filter((carName)->
+                carName.length() >= MAX_CAR_NAME_LENGTH || carName.length() < MIN_CAR_NAME_LENGTH ).count();
+        if(errorNumber != 0 ) return false;
+        return true;
+    }
+
+}
