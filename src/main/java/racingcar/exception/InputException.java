c021dbf531bfa8206e4f372fa9c8207ba2b14e62
Gyuchool <balance0591@naver.com>
2021-12-04 15:09:34 +0900

    feat(exception): 6자 이상인 자동차 이름 예외 처리

--- src/main/java/racingcar/exception/InputException.java
+++ src/main/java/racingcar/exception/InputException.java
@@ -6,13 +6,16 @@ import java.util.Set;
 
 public class InputException {
 	private static final String CAR_LAST_NAME_IS_COMMA = "[ERROR] 마지막 자동차 이름을 입력하지 않았습니다.";
+	private static final String CAR_NAME_SIZE = "[ERROR] 자동차 이름은 1자이상 5자이하만 가능합니다.";
 	private static final String CAR_DUPLICATE_NAME = "[ERROR] 자동차 이름은 중복이 불가능합니다.";
+	private static final int MIN_CAR_NAME_SIZE = 1;
+	private static final int MAX_CAR_NAME_SIZE = 5;
 
 	public static String[] isValidCarNames(String inputCarNames) {
 
 		haveLastNameComma(inputCarNames);
 		String[] carNames = inputCarNames.split(",");
-		if (!haveDuplicateName(carNames)) {
+		if (!haveDuplicateName(carNames) && isValidCarNameSize(carNames)) {
 			return carNames;
 		}
 		throw new IllegalArgumentException();
@@ -24,6 +27,16 @@ public class InputException {
 		}
 	}
 
+	private static boolean isValidCarNameSize(String[] carNames) {
+		for (String carName : carNames) {
+			if (carName.length() < MIN_CAR_NAME_SIZE || carName.length() > MAX_CAR_NAME_SIZE) {
+				System.out.println(CAR_NAME_SIZE);
+				return false;
+			}
+		}
+		return true;
+	}
+
 	private static boolean haveDuplicateName(String[] carNames) {
 		Set<String> names = new HashSet<>(Arrays.asList(carNames));
 		if (names.size() != carNames.length) {
@@ -31,4 +44,5 @@ public class InputException {
 		}
 		return false;
 	}
+
 }
