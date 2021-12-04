651c00ff3f5e51b9828daad454c9f93d973a54d8
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-04 11:41:55 +0900

    refactor: 매직 넘버, 리터럴 상수 변환

--- src/main/java/racingcar/strategy/RandomMoveStrategy.java
+++ src/main/java/racingcar/strategy/RandomMoveStrategy.java
@@ -4,9 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
 
+    private static final int MIN_COMMAND = 1;
+    private static final int MAX_COMMAND = 9;
+    private static final int MIN_MOVEABLE_COMMAND = 4;
+
     @Override
     public boolean isMoveable() {
-        int command = Randoms.pickNumberInRange(1, 9);
-        return command >= 4;
+        int command = Randoms.pickNumberInRange(MIN_COMMAND, MAX_COMMAND);
+        return command >= MIN_MOVEABLE_COMMAND;
     }
 }
