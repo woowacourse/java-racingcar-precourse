d6d108b41aedf00aa2152e53c3f83d364b9ec33b
Gyuchool <balance0591@naver.com>
2021-12-04 16:03:23 +0900

    test(cars): 입력값에 따라 join로직 정상 작동 테스트

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,40 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarsTest {
+
+	private Cars cars;
+	private String[] input;
+	private static final String participant1 = "kia";
+	private static final String participant2 = "Hyundai";
+	private static int participantSize;
+	@BeforeEach
+	void init() {
+		this.cars = new Cars();
+		this.input = new String[] {participant1, participant2};
+		participantSize = input.length;
+	}
+
+	@Test
+	@DisplayName("입력에 따른 참가 자동차들 참가되었는지 테스트 - 성공")
+	void join() {
+
+		cars.join(input);
+
+		List<Car> carList = this.cars.getCars();
+		assertAll(
+			() -> assertThat(carList.get(0).getName()).isEqualTo(participant1),
+			() -> assertThat(carList.get(1).getName()).isEqualTo(participant2),
+			() -> assertThat(carList.size()).isEqualTo(participantSize)
+		);
+
+	}
+}
