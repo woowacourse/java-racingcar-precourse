cebe6bfd79de66a8cef77797137e4e1f500536a8
Ohzzi <clover7even@naver.com>
2021-12-04 23:20:50 +0900

    fix: 이름 입력 시 중복 여부 검사
    이름을 입력했을 때 같은 이름을 가진 경우도 정상적으로 작동하던 것을 중복을 검사하여 예외를 던지도록 수정

--- src/main/java/racingcar/utils/Validator.java
+++ src/main/java/racingcar/utils/Validator.java
@@ -1,17 +1,19 @@
 package racingcar.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
 	private static final int NAME_LENGTH_LIMIT = 5;
 
 	public static final String ERROR_LENGTH_MSG = "[ERROR] 자동차 이름은 5자 이하여야 한다.";
 	public static final String ERROR_NUMBER_MSG = "[ERROR] 시도 횟수는 숫자여야 한다.";
+	public static final String ERROR_DUP_MSG = "[ERROR] 자동차 이름이 중복되었다.";
 
-	public static void validateNameInput(String[] names) {
-		for (String name : names) {
-			if (name.trim().length() > NAME_LENGTH_LIMIT || name.trim().length() == 0) {
-				throw new IllegalArgumentException(ERROR_LENGTH_MSG);
-			}
-		}
+	public static void validateNameInput(String[] names) throws IllegalArgumentException {
+		validateNameLength(names);
+		validateDuplicatedName(names);
 	}
 
 	public static void validateNumberInput(String input) {
@@ -19,6 +21,20 @@ public class Validator {
 			Integer.parseInt(input);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(ERROR_NUMBER_MSG);
+		}
+	}
+
+	private static void validateNameLength(String[] names) {
+		Arrays.stream(names).forEach(name -> {
+			if (name.trim().length() > NAME_LENGTH_LIMIT || name.trim().length() == 0) {
+				throw new IllegalArgumentException(ERROR_LENGTH_MSG);
+			}
+		});
+	}
+
+	private static void validateDuplicatedName(String[] names) {
+		if (names.length != Arrays.stream(names).distinct().count()) {
+			throw new IllegalArgumentException(ERROR_DUP_MSG);
 		}
 	}
 }
--- src/test/java/racingcar/utils/ValidatorTest.java
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -7,19 +7,23 @@ import org.junit.jupiter.api.Test;
 
 class ValidatorTest {
 	@Test
-	void 이름_예외_검사() {
+	void 이름_검사() {
 		// given
 		String[] longName = "LongName".split(",");
 		String[] startWithComma = ",Kim,Lee".split(",");
 		String[] endWithComma = "Kim,Lee,".split(",", -1);
 		String[] containBlank = "Kim, ,Lee".split(",");
+		String[] duplicatedNames = "Kim,Kim".split(",");
+		String[] rightNames = "Kim, Park".split(",");
 
 		// when, then
 		assertAll(
 			() -> assertThrows(IllegalArgumentException.class, () -> validateNameInput(longName)),
 			() -> assertThrows(IllegalArgumentException.class, () -> validateNameInput(startWithComma)),
 			() -> assertThrows(IllegalArgumentException.class, () -> validateNameInput(endWithComma)),
-			() -> assertThrows(IllegalArgumentException.class, () -> validateNameInput(containBlank))
+			() -> assertThrows(IllegalArgumentException.class, () -> validateNameInput(containBlank)),
+			() -> assertThrows(IllegalArgumentException.class, () -> validateNameInput(duplicatedNames)),
+			() -> validateNameInput(rightNames)
 		);
 	}
 
