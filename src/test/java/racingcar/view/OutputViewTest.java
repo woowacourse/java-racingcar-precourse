92461ec371c41bbd20465a028ec73cd666cc8275
Wishoon <heonga2@gmail.com>
2021-12-04 19:46:39 +0900

    test: 자동차 경주 게임의 출력 테스트 작성
    OutputView
    - 승리자가 2이상인 경우 콤마 생성여부 확인
    - 게임결과 출력시 position 값을 '-'로 변환 여부 확인

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,60 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.*;
+import static racingcar.util.SymbolicConstantUtil.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import racingcar.domain.Car;
+import racingcar.domain.RacingRecord;
+import racingcar.domain.RacingResult;
+
+class OutputViewTest {
+	private static final int MOVING_FORWARD = 4;
+
+	private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+	private List<Car> cars = new ArrayList<>();
+
+	@BeforeEach
+	public void setUpStreams() {
+		System.setOut(new PrintStream(output));
+	}
+
+	@BeforeEach
+	public void setUpCars() {
+		cars.add(new Car("hoon"));
+		cars.add(new Car("wish"));
+	}
+
+	@Test
+	void 승리자가_2이상인_경우_콤마_생성() {
+		// given
+		cars.forEach(car -> car.drive(MOVING_FORWARD));
+		RacingResult racingResult = new RacingResult(cars);
+
+		// when
+		OutputView.printRacingWinners(racingResult);
+
+		// then
+		assertThat(output.toString()).contains(COMMA);
+	}
+
+	@Test
+	void 게임결과_출력시_전진값에_대한_대쉬기호_생성() {
+		// given
+		cars.forEach(car -> car.drive(MOVING_FORWARD));
+		RacingRecord racingRecord = new RacingRecord(cars);
+
+		// when
+		OutputView.printRacingRecord(racingRecord);
+
+		// then
+		assertThat(output.toString()).contains(DASH);
+	}
+}
