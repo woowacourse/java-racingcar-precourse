3490a0c9dab3e86409487b46e2c6af108290ee61
June <tkdwns159@naver.com>
2021-12-04 18:10:19 +0900

    feat(Application): 게임 시작 실행문 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,8 @@
 package racingcar;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
 }
