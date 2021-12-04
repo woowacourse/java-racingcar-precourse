632b5659c747ba2effe4e0cf8c8b15c9987561f4
June <tkdwns159@naver.com>
2021-12-04 15:47:23 +0900

    feat(Car): 자동차 이동시키기 기능 추가
    - 랜덤으로 생성한 숫자가 4이상일때만 해당 Car 객체를 1만큼 이동
    - 이동을 시도할때 마다 그 결과 메세지를 출력

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public void printCarPosition() {
+		String name = String.format("%s : ", this.name);
+		String distance = new String(new char[position]).replace("\0", "-");
+
+		System.out.println(name + distance);
+	}
+
+	private boolean flipCoin() {
+		int result = Randoms.pickNumberInRange(0, 9);
+		return result >= 4;
+	}
+
+	public void move() {
+		if (flipCoin()) {
+			this.position += 1;
+		}
+
+		printCarPosition();
+	}
+
 }
