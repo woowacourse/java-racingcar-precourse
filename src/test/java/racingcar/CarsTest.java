45a262b74fcdb15f2a7469d757a08e6be8a5c115
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-05 00:14:41 +0900

    test(CarsTest) : 기존 Service, Repository를 모두 커버하는 test 작성

---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,94 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.*;
+import org.mockito.MockedStatic;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
+import static racingcar.TestConstant.*;
+import static racingcar.TestConstant.MOVED;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CarsTest {
+
+    Cars cars = Cars.getInstance();
+
+    @BeforeAll
+    void setUp() {
+        cars.saveCars("jae,hun,choi");
+        try (final MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt())).thenReturn(STOP, MOVING_FORWARD, MOVING_FORWARD);
+            cars.updateCarPosition();
+        }
+    }
+
+    @DisplayName("저장된 Cars 값을 가져온다")
+    @Test
+    void getAll(){
+        List<Car> findCars = cars.getAllCars();
+
+        SoftAssertions.assertSoftly(soft -> {
+            assertThat(findCars.size()).isEqualTo(3);
+            assertThat(findCars.get(0).getName()).isEqualTo("jae");
+            assertThat(findCars.get(1).getName()).isEqualTo("hun");
+            assertThat(findCars.get(2).getName()).isEqualTo("choi");
+        });
+    }
+
+    @DisplayName("숫자에 따라 차량 포지션을 업데이트한다")
+    @Test
+    void updateCarPosition() {
+        List<Car> findCars = cars.getAllCars();
+
+        SoftAssertions.assertSoftly(soft -> {
+            assertThat(findCars.get(0).getPosition()).isEqualTo(STAY);
+            assertThat(findCars.get(1).getPosition()).isEqualTo(MOVED);
+            assertThat(findCars.get(2).getPosition()).isEqualTo(MOVED);
+        });
+    }
+
+    @DisplayName("승자 찾기")
+    @Test
+    void findWinners() {
+        List<Car> winners = cars.getWinners();
+
+        SoftAssertions.assertSoftly(soft -> {
+            assertThat(winners.size()).isEqualTo(2);
+            assertThat(winners.get(0).getName()).isEqualTo("hun");
+            assertThat(winners.get(1).getName()).isEqualTo("choi");
+        });
+
+    }
+
+    @DisplayName("자동차 저장 길이초과_실패")
+    @Test
+    void save_cars_range_false() {
+        assertThrows(IllegalArgumentException.class, ()
+                -> cars.saveCars("jae,hun,choizzz"));
+    }
+
+    @DisplayName("자동차 저장 중복이름_실패")
+    @Test
+    void save_cars_duplicate_false() {
+        assertThrows(IllegalArgumentException.class, ()
+                -> cars.saveCars("jae,hun,hun"));
+    }
+
+    @DisplayName("자동차 저장 빈칸입력_실패")
+    @Test
+    void save_cars_blank_false() {
+        assertThrows(IllegalArgumentException.class, ()
+                -> cars.saveCars("jae,hun, "));
+    }
+
+    @AfterAll
+    void tearDown() {
+        cars.removeAll();
+    }
+}
