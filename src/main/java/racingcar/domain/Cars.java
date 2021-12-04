e106be58b22cf5509f329342cfa64f32cfd4c6af
her0807 <her0807@naver.com>
2021-12-04 21:59:09 +0900

    feat: 우승자 판별/출력 기능 구현
    - play() 전체 자동차를 움직이기 시도
    - recordingScore() 경기 결과를 기록
    - findMaxPosition() 가장 멀리간 자동차를 찾는 로직
    - findSamePosition() 가장 멀리간 거리를 가지고 있는 자동차들을 찾음
    - findWinnerCars() 우승자를 찾기 위한 로직, findMaxPosition, findSamePosition 를 실행

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,18 +2,21 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 import racingcar.domain.exception.CarNameValidator;
 import racingcar.domain.exception.Validator;
+import racingcar.view.Output;
 
 public class Cars {
-    private static final String COMMA = ",";
+    private static final String DELIMITER = ",";
     private List<Car> cars;
 
     public Cars(String names) {
         cars = new ArrayList<>();
         CarNameValidator validator = new Validator();
-        String[] nameArr = names.split(COMMA);
+        String[] nameArr = names.split(DELIMITER);
         if (validator.isAllValid(nameArr)) {
             for (String name : nameArr) {
                 cars.add(createCar(name));
@@ -25,9 +28,32 @@ public class Cars {
         return new Car(name);
     }
 
-    public void play(RepetitionNumber distance) {
+    public void play() {
         for (Car car : cars) {
-            car.drive(distance.getRepetitionNumber());
+            car.drive();
         }
+        recordingScore();
+    }
+
+    private void recordingScore() {
+        Output.roundResult(cars);
+    }
+
+    public List<String> findWinnerCars() {
+        Car maxPosition = findMaxPosition();
+        return findSamePosition(maxPosition);
+    }
+
+    private Car findMaxPosition() {
+        return cars.stream()
+            .max(Car::compareTo)
+            .orElseThrow(() -> new NoSuchElementException(Output.maxPositionCarErrorMessage()));
+    }
+
+    private List<String> findSamePosition(Car maxPositionCar) {
+        return cars.stream()
+            .filter(car -> car.isMaxPosition(maxPositionCar))
+            .map(Car::getName)
+            .collect(Collectors.toList());
     }
 }
--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,19 +1,53 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import racingcar.domain.Car;
+
 public class Output {
     private static final String CARS_NAME_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력해주세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REPETITION_NUMBER_INPUT_GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static StringBuilder stringBuilder;
+    private static final String NOT_FOUND_MAX_POSITION_CAR = "가장 멀리간 차를 찾을 수 없습니다.";
+    private static final String NEXT_LINE = "\n";
+    private static final String DELIMITER = ", ";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String FINAL_WINNER_RESULT_MESSAGE = "최종 우승자";
+    private static StringBuilder stringBuilder = new StringBuilder();
 
     private Output() {
-        stringBuilder = new StringBuilder();
     }
 
     public static void carNameInputGuideMessage() {
-        System.out.println(CARS_NAME_INPUT_GUIDE_MESSAGE);
+        println(CARS_NAME_INPUT_GUIDE_MESSAGE);
     }
 
     public static void numberInputGuideMessage() {
-        System.out.println(REPETITION_NUMBER_INPUT_GUIDE_MESSAGE);
+        println(REPETITION_NUMBER_INPUT_GUIDE_MESSAGE);
+    }
+
+    public static String maxPositionCarErrorMessage() {
+        return NOT_FOUND_MAX_POSITION_CAR;
+    }
+
+    public static void roundResult(List<Car> cars) {
+        for (Car carResult : cars) {
+            stringBuilder.append(carResult.toString()).append(NEXT_LINE);
+        }
+        stringBuilder.append(NEXT_LINE);
+    }
+
+    public static void printAllRoundResult() {
+        println(RESULT_MESSAGE);
+        System.out.print(stringBuilder);
+    }
+
+    public static void printWinners(List<String> winners) {
+        println(FINAL_WINNER_RESULT_MESSAGE);
+        println(winners.stream().collect(Collectors.joining(DELIMITER)));
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
     }
 }
