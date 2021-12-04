a4667f612418b4b4fc506ac51c405c7b9fc2dc27
yhh1056 <yhh1056@naver.com>
2021-12-05 01:10:30 +0900

    feat: 자동차 이름을 쉼표로 구분

--- src/main/java/racingcar/CarGame.java
+++ src/main/java/racingcar/CarGame.java
@@ -4,10 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class CarGame implements Runnable {
 	private static final String GUIDE_INPUT_CAR_NAME = "String x = 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String COMMA = ",";
 
 	@Override
 	public void run() {
-		String carNames = trim(inputCarNames());
+		String[] carNames = splitComma(trim(inputCarNames()));
+	}
+
+	private String[] splitComma(String carNames) {
+		return carNames.split(COMMA);
 	}
 
 	private String trim(String inputCarNames) {
