f865f5990847d5706fd4701851f510aac794e510
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-04 22:33:17 +0900

    refactor(User.java): carNames에서 names로 이름 수정
    -inputCarNames()에서 inputNames()로 함수명 수정
    -String carNames에서 String[] names로 수정

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -4,13 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
-	private String INPUT_CARNAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private String DELIMITER = ",";
+	private String INPUT_MESSAGE_CARNAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(" + DELIMITER + ") 기준으로 구분)";
 
-	private String carNames;
+	private String[] names;
 	private int numberOfTimes;
 
-	public void inputCarNames() {
-		System.out.println(INPUT_CARNAMES_MESSAGE);
-		carNames = Console.readLine();
+	public void inputNames() {
+		System.out.println(INPUT_MESSAGE_CARNAMES);
+		String input = Console.readLine();
+		names = input.split(DELIMITER);
 	}
 }
