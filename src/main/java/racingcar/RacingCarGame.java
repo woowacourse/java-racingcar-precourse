c6b75c3b5d000508e6af2df553503430e1aacf63
zmfl1230 <zmfl123097@gmail.com>
2021-12-04 15:02:53 +0900

    feat: design RacingCarGame class structure

---
+++ src/main/java/racingcar/RacingCarGame.java
@@ -0,0 +1,30 @@
+package racingcar;
+import java.util.ArrayList;
+
+public class RacingCarGame  implements Game{
+    ArrayList<Car> racingCars;
+    int numberOfTrial;
+
+    @Override
+    public void play() {
+
+    }
+
+    private void getRacingCarsNameFromUser() {
+
+    }
+
+    private void seperatedByMark(char c) {
+
+    }
+
+    private void createRacingCarEntity(String carName) {
+
+    }
+
+    private void getNumberOfTrialFromUser() {
+
+    }
+
+
+}
