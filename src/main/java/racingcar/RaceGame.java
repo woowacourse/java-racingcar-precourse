e949bdb3a82a51db083d5fa148e03a31af0a7f9e
skennetho <thrkd613@naver.com>
2021-12-04 23:44:25 +0900

    feat: 자동차이름과 시도횟수를 입력받는 기능 구현
    질문과 함께 readLine()을 수행하게 구현하였고 입력받은 직후 inputChecker를 통해 검증을 하게 된다.
    Application에서 입력을 받을 시 Application에서 RaceGame의 예외를 처리 해야하는 문제가 생기기 때문에 예외 발생시 스스로 처리할 수 있게 하기 위해 RaceGame에서 사용자의 입력을 받는 기능을 구현하였다.

--- src/main/java/racingcar/RaceGame.java
+++ src/main/java/racingcar/RaceGame.java
@@ -1,7 +1,45 @@
 package racingcar;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class RaceGame {
-    private final InputChecker inputChecker = new GameRule();
+    private InputChecker inputChecker;
+    private ArrayList<Car> carArrayList;
+
+    private int tryNumber;
+
+    public RaceGame(InputChecker inputChecker) {
+        this.inputChecker = inputChecker;
+    }
+
+    public void startGame() {
+        askCarNames();
+        askTryNumber();
+    }
+
+    private void askCarNames() {
+        System.out.println(GameData.MESSAGE_ASK_CAR_NAMES);
+        String[] carNames = readLine().split(",");
+
+        for (String carName : carNames) {
+            inputChecker.checkCarName(carName);
+            addNewCar(carName);
+        }
+    }
+
+    private void askTryNumber() {
+        System.out.println(GameData.MESSAGE_ASK_TRY_NUMBER);
+        String tryNumber = readLine();
+
+        inputChecker.checkTryNumber(tryNumber);
+        this.tryNumber = Integer.parseInt(tryNumber);
+    }
+
+    private void addNewCar(String carName) {
+        //TODO: 자동차 추가 기능 구현
+    }
 
     public interface InputChecker {
         public void checkCarName(String input);
