25c1a7718eb61d24e01b43f60f2f170c5f314e0c
meatsby <meatsby@gmail.com>
2021-12-04 16:30:57 +0900

    refactor: getter 제거 및 메서드 분리
    중복되는 문맥 수정
    캡슐화를 위한 getter 제거
    공동 우승자용 메서드를 따로 분리

--- src/main/java/racingcar/user/Output.java
+++ src/main/java/racingcar/user/Output.java
@@ -5,32 +5,34 @@ import java.util.ArrayList;
 import racingcar.Car;
 
 public class Output {
-	private static final String DISTANCE = "-";
 	private static final String COLON = " : ";
+	private static final String DISTANCE = "-";
 	private static final String SEPARATOR = ", ";
 	private static final String ROUND_RESULT_MESSAGE = "실행 결과";
 	private static final String CHAMPION_MESSAGE = "최종 우승자 : ";
 	private static final int SINGLE_CHAMPION = 0;
+	private static final int CAR_NAME = 0;
+	private static final int CAR_POSITION = 1;
 
 	public void printRoundResult(String round, Car[] cars) {
 		System.out.println();
 		System.out.println(ROUND_RESULT_MESSAGE);
 		for (int i = 0; i < Integer.parseInt(round); i++) {
-			moveCars(cars);
+			move(cars);
 			System.out.println();
 		}
 	}
 
-	private void moveCars(Car[] cars) {
+	private void move(Car[] cars) {
 		for (Car car : cars) {
-			car.move();
-			printResult(car);
+			ArrayList<String> result = car.move();
+			printResult(result.get(CAR_NAME), result.get(CAR_POSITION));
 		}
 	}
 
-	private void printResult(Car car) {
-		System.out.print(car.getName() + COLON);
-		printDistance(car.getPosition());
+	private void printResult(String name, String position) {
+		System.out.print(name + COLON);
+		printDistance(Integer.parseInt(position));
 		System.out.println();
 	}
 
@@ -42,6 +44,10 @@ public class Output {
 
 	public void printChampionList(ArrayList<String> champion) {
 		System.out.print(CHAMPION_MESSAGE + champion.get(SINGLE_CHAMPION));
+		printMultipleChampion(champion);
+	}
+
+	private void printMultipleChampion(ArrayList<String> champion) {
 		for (int i = 1; i < champion.size(); i++) {
 			System.out.print(SEPARATOR + champion.get(i));
 		}
