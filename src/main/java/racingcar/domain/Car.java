69efabf37803d59c300e697bfe49faf5894ce722
her0807 <her0807@naver.com>
2021-12-04 21:52:15 +0900

    feat: 자동차 이동과 위치에 관한 기능 추가
    - hasNextStep() 랜덤으로 숫자를 생성하고, 그 값이 4인지 판별
    - drive() hasNextStep이 true 이면 한칸 전진
    - toString() 현재 위치를 출력하기 위한 문자열 생성
    - isMaxPosition() 우승자인지 판별하기 위한 로직
    - compareTo() position 을 드러내지 않고, 위치를 비교하기 위해 Comparable 를 상속받아 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,10 +2,11 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Car {
+public class Car implements Comparable<Car> {
     private static final int MIN_RANDOM_VALUE = 0;
     private static final int MAX_RANDOM_VALUE = 9;
     private static final int MOVE_STANDARD_VALUE = 4;
+    private static final String STEP = "-";
     private final String name;
     private int position = 0;
 
@@ -21,19 +22,31 @@ public class Car {
         return name;
     }
 
-    private boolean hasNext() {
+    private boolean hasNextStep() {
         return Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE) >= MOVE_STANDARD_VALUE;
     }
 
-    public void drive(int distance) {
-        for (int i = 0; distance > i; i++) {
-            if (hasNext()) {
-                moveStepForward();
-            }
+    public void drive() {
+        if (hasNextStep()) {
+            moveStepForward();
         }
     }
 
-    public int getPosition() {
-        return position;
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < this.position; i++) {
+            stringBuilder.append(STEP);
+        }
+        return String.format("%s : %s", this.name, stringBuilder);
+    }
+
+    public boolean isMaxPosition(Car maxPositionCar) {
+        return maxPositionCar.position == this.position;
+    }
+
+    @Override
+    public int compareTo(Car otherCar) {
+        return this.position - otherCar.position;
     }
 }
