5be0cae2580589d8e7c254d1e14046a02dd99ba4
hyewoncc <hyewoncc@gmail.com>
2021-12-04 18:59:19 +0900

    feat: 경주 실행 회수 입력과 숫자 검증
    경주 실행 회수를 입력
    숫자가 아닐 시 예외 처리하고 메시지 생성

--- src/main/java/racingcar/PlayerInput.java
+++ src/main/java/racingcar/PlayerInput.java
@@ -9,7 +9,9 @@ import java.util.StringTokenizer;
 public class PlayerInput {
 	private static final int MAX_NAME_SIZE = 5;
 	private static final String INPUT_MESSAGE_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_MESSAGE_MOVE_TRIAL_COUNT = "시도할 회수는 몇회인가요?";
 	private static final String ERROR_MESSAGE_NAME_SIZE = "[ERROR] 이름은 최대 5자 입력 가능.";
+	private static final String ERROR_MESSAGE_NOT_INT = "[ERROR] 시도 회수는 숫자만 입력 가능.";
 
 	public List<String> getCarNames() {
 		String input = getCarNamesInput();
@@ -17,8 +19,20 @@ public class PlayerInput {
 		return carNames;
 	}
 
+	public int getMoveTrialCount() {
+		String input = getMoveTrialCountInput();
+		int count = convertInputToCount(input);
+		return count;
+	}
+
 	private String getCarNamesInput() {
 		System.out.println(INPUT_MESSAGE_CAR_NAMES);
+		String input = Console.readLine();
+		return input;
+	}
+
+	private String getMoveTrialCountInput() {
+		System.out.println(INPUT_MESSAGE_MOVE_TRIAL_COUNT);
 		String input = Console.readLine();
 		return input;
 	}
@@ -34,9 +48,22 @@ public class PlayerInput {
 		return carNames;
 	}
 
+	private int convertInputToCount(String input) {
+		validateInputIsInt(input);
+		return Integer.parseInt(input);
+	}
+
 	private void validateNameSize(String name) {
 		if (name.length() > MAX_NAME_SIZE) {
 			throw new IllegalArgumentException(ERROR_MESSAGE_NAME_SIZE);
+		}
+	}
+
+	private void validateInputIsInt(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			if (!Character.isDigit(input.charAt(i))) {
+				throw new IllegalArgumentException(ERROR_MESSAGE_NOT_INT);
+			}
 		}
 	}
 }
