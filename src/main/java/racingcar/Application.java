4eb92c1c839a9b6a3c99e7b715bf5f0a9c747798
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-05 05:19:08 +0900

    feat<Application>: 입력값 검증 및 게임 시작
    - getNameList()
      - 자동차 이름의 길이와 중복을 검증해주는 메소드
      - 검증에 실패하면 에러 메세지를 출력하고 다시 입력받도록 함
    - getNumber()
      - 시도 횟수가 1 ~ 9 사이의 숫자인지 검증해주는 메소드
      - 검증에 실패하면 에러 메세지를 출력하고 다시 입력받도록 함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,48 @@
 package racingcar;
 
+import static racingcar.message.GuideMessage.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.service.Game;
+import racingcar.service.NameValidation;
+import racingcar.service.NamingProcess;
+import racingcar.service.NumberValidation;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+
+	public static void main(String[] args) {
+
+		System.out.println(ENTER_CAR_NAME);
+		String[] nameList = getNameList();
+		System.out.println(ENTER_NUMBER);
+		int trials = Integer.parseInt(getNumber());
+
+		Game.start(nameList, trials);
+	}
+
+	private static String[] getNameList() {
+		while (true) {
+			String[] nameList = NamingProcess.parseString(Console.readLine());
+			try {
+				NameValidation.validateName(nameList);
+			} catch (IllegalArgumentException exception) {
+				System.out.println(exception.getMessage());
+				return getNameList();
+			}
+			return nameList;
+		}
+	}
+
+	private static String getNumber() {
+		while (true) {
+			String inputNumber = Console.readLine();
+			try {
+				NumberValidation.validateInputNumber(inputNumber);
+			} catch (IllegalArgumentException exception) {
+				System.out.println(exception.getMessage());
+				return getNumber();
+			}
+			return inputNumber;
+		}
+	}
 }
