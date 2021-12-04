4a2569439bf52d0753e7dc1ee9c5b75244e48f2e
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-04 12:42:59 +0900

    [feat] main 로직의 큰 흐름 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Application {
+        public static ArrayList<Car> carList;
+
     public static void main(String[] args) {
-        // TODO 구현 진행
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carList = Reader.inputCarName();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numberOfTry = Reader.inputNumberOfTry();
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < numberOfTry; i++) {
+            Race.doOneIterationRace(carList);
+        }
+        Race.determineWinner(carList);
     }
 }
