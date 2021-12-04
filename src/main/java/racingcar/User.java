694e52c3732ccad2b9db5e2cc706ab7cf828d10f
kanamycine <jinyeong5320@gmail.com>
2021-12-04 18:10:10 +0900

    feat(User) : 이름 입력 예외 처리 기능 구현

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -2,16 +2,18 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.Constant;
+import racingcar.utils.Validation;
 
 public class User {
-	public static String [] driverNameInput(){
-		String [] driverNames = new String[3];
+	public static String [] carNameInput(){
+		String[] carNames = new String[3];
+		do {
+			System.out.println(Constant.ASK_CARS_NAME);
+			String carNameInput = Console.readLine();
+			carNames = carNameInput.split(",");
+		} while (!checkRightNames(carNames));
 
-		System.out.println(Constant.ASK_CARS_NAME);
-		String driverNameInput = Console.readLine();
-		driverNames = driverNameInput.split(",");
-
-		return driverNames;
+		return carNames;
 	}
 
 	public static int gameCountInput(){
@@ -19,5 +21,18 @@ public class User {
 		int gameCount = Integer.parseInt(Console.readLine());
 		System.out.println();
 		return gameCount;
+	}
+
+	public static boolean checkRightNames(String [] carNames){
+		boolean isRightNames = true;
+		try{
+			Validation.CONTAIN_SAME_NAME_ERROR(carNames);
+			Validation.WHITE_SPACE_NAME_ERROR(carNames);
+			Validation.LESS_MORE_NAME_ERROR(carNames);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			isRightNames = false;
+		}
+		return isRightNames;
 	}
 }
