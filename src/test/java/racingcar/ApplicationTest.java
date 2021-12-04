098455ede67b020d2746fcfbc4bf8da9e2048c43
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-12-05 00:45:04 +0900

    test: 공동 우승 테스트 추가
    - 공동 우승 시 출력 메시지에 대한 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -25,6 +25,17 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 공동_우승() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni", "1");
+                assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+            },
+            MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(
             () -> {
