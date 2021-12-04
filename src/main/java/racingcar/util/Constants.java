acdc021cf6f96860d5341a980a9bc684004802c6
pup-paw <wldusdl0310@gmail.com>
2021-12-05 03:10:14 +0900

    feat: OutputView 에서 사용되는 상수 처리

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -2,6 +2,8 @@ package racingcar.util;
 
 public class Constants {
 	public static final String CAR_NAME_LENGTH_ERROR = "[ERROR] 자동차 이름의 길이는 1이상 5이하여야 한다.";
-	public static final String ATTEMPTS_NUMBER_TYPE_ERROR = "[ERROR] 시도 횟수는 숫자여야 한다.";
+	public static final String ATTEMPTS_NUMBER_TYPE_ERROR = "[ERROR] 시도 회수는 숫자여야 한다.";
 	public static final int CAR_NAME_MAX_LENGTH = 5;
+	public static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String ASK_ATTEMPTS_NUMBER = "시도할 회수는 몇회인가요?";
 }
