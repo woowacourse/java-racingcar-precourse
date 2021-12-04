e356d56391d0bc222868e620cefaf6d30420baf5
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-04 23:19:35 +0900

    [refactor] carList의 초깃값으로 null값을 부여

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,13 +3,12 @@ package racingcar;
 import java.util.ArrayList;
 
 public class Application {
-        public static ArrayList<Car> carList;
-        public static int numberOfTry;
+    public static ArrayList<Car> carList = null;
+    public static int numberOfTry;
 
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         Generator.inputCarName();
-
         System.out.println("시도할 회수는 몇회인가요?");
         Generator.inputNumberOfTry();
 
