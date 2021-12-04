ffa389ca88e143bb81492ccd60b5a4fe52566e60
zmfl1230 <zmfl123097@gmail.com>
2021-12-04 15:00:20 +0900

    feat: design Car class structure

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,9 +4,28 @@ public class Car {
     private final String name;
     private int position = 0;
 
+    private final String MARK = "-";
+
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public int generateRandomValue() {
+
+        return 0;
+    }
+
+    public void updatePosition() {
+
+    }
+
+    private String indicatePosition() {
+
+        return "";
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
