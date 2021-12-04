b21ba8307da00b681ff558d489e3504827ed57fa
hyewoncc <hyewoncc@gmail.com>
2021-12-04 21:48:05 +0900

    feat: 입력 회수만큼 자동차 전진 실행
    입력값만큼 전진을 실행하고 상태를 출력

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -10,12 +10,25 @@ public class RacingGame {
 	public void runGame() {
 		List<String> carNames = playerInput.getCarNames();
 		makeCars(carNames);
+		int moveCount = playerInput.getMoveTrialCount();
+		moveCarsAndShowStatus(moveCount);
 	}
 
 	private void makeCars(List<String> carNames) {
 		for (String carName : carNames) {
 			Car car = new Car(carName);
 			cars.add(car);
+		}
+	}
+
+	private void moveCarsAndShowStatus(int count) {
+		System.out.println("\n실행 결과");
+		for (int i = 0; i < count; i++) {
+			for (int j = 0; j < cars.size(); j++) {
+				cars.get(j).goForwardByRandom();
+				cars.get(j).printStatus();
+			}
+			System.out.println();
 		}
 	}
 }
