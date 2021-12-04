c08d222f628ce32f8e761957f0d50b9d74085fdc
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-12-04 14:45:31 +0900

    feat: 사용자로부터 받는 입력 예외처리 구현, IllegalArgumentException 발생시키도록

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,10 +7,14 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingGame {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 1;
     private static final String MSG_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MSG_INPUT_ATTEMPTS_NUM = "시도할 회수는 몇회인가요?";
     private static final String MSG_PRINT_EXECUTE_RESULT = "실행 결과";
     private static final String MSG_PRINT_WINNER = "최종 우승자 : ";
+    private static final String ERROR_INVALID_NAME_LENGTH = "[ERROR] 자동차 이름의 길이는 1이상 5이하여야 한다.";
+    private static final String ERROR_ATTEMPT_NOT_NUMBER = "[ERROR] 시도 횟수는 숫자여야 한다.";
 
     private final List<Car> cars;
     private int maxPosition;
@@ -65,14 +69,43 @@ public class RacingGame {
         }
     }
 
-    // TODO: 입력에 대한 예외처리 하기
     private List<String> inputCarNames() {
-        System.out.println(MSG_INPUT_CAR_NAMES);
-        return Arrays.asList(Console.readLine().split(","));
+        while (true) {
+            try {
+                System.out.println(MSG_INPUT_CAR_NAMES);
+                List<String> names = Arrays.asList(Console.readLine().split(","));
+                validateNameLength(names);
+                return names;
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_INVALID_NAME_LENGTH);
+            }
+        }
+    }
+
+    private void validateNameLength(List<String> names) throws IllegalArgumentException{
+        for (String name : names) {
+            if (name.length() > MAX_NAME_LENGTH || name.length() < MIN_NAME_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     private int inputNumberOfAttempts() {
-        System.out.println(MSG_INPUT_ATTEMPTS_NUM);
-        return Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                System.out.println(MSG_INPUT_ATTEMPTS_NUM);
+                return convertToInteger(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_ATTEMPT_NOT_NUMBER);
+            }
+        }
+    }
+
+    private int convertToInteger(String attempts) throws IllegalArgumentException{
+        try {
+            return Integer.parseInt(attempts);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
