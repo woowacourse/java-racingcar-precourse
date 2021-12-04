1810059b3acef5c9a0ec9ef852c758a06ffb0bce
Gyuchool <balance0591@naver.com>
2021-12-04 14:19:31 +0900

    feat(car): 0~9까지 랜덤 값중 4이상이면 전진하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,11 @@
 package racingcar;
 
-public class Car {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class Car {
+	private static final int RANDOM_MIN_NUMBER = 0;
+	private static final int RANDOM_MAX_NUMBER = 9;
+	private static final int STANDARD = 4;
 	private final String name;
 	private int position = 0;
 
@@ -16,5 +20,18 @@ public class Car {
 
 	public int getPosition() {
 		return this.position;
+	}
+
+	public void moveOrStop() {
+		if (canGo()) {
+			position++;
+		}
+	}
+
+	private boolean canGo() {
+		if (Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER) >= STANDARD) {
+			return true;
+		}
+		return false;
 	}
 }
