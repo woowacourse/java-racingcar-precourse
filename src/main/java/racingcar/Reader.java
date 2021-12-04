1e4e32d0d1a0aa3b7545aced50d078034b5794a4
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-04 12:40:28 +0900

    [refactor] 클래스명 변경 : InputFromUser -> Reader
    - main에서 해당 클래스가 쓰이는 모양새가
    InputFromUser.inputCarName() 처럼 중복된 단어가 있어 가독성이 떨어져 바꿈

--- src/main/java/racingcar/Reader.java
+++ src/main/java/racingcar/Reader.java
@@ -1,29 +1,31 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 
-public class InputFromUser {
+public class Reader {
     static FastReader scan = new FastReader();
 
     public static ArrayList<Car> inputCarName() {
-        String inputText = scan.nextLine();
+        String inputText = Console.readLine();
         String[] carNames = inputText.split(",");
 
         ArrayList<Car> carList = new ArrayList<>();
         for (int i = 0; i < carNames.length; i++) {
-            // --> validation 실행 //
+            // TODO --> validation 실행 //
 
             carList.add(new Car(carNames[i]));
         }
-        System.out.println(carList);
-
         return carList;
     }
 
     public static int inputNumberOfTry() {
-        int numberOfTry = scan.nextInt();
-        // --> validation 실행 //
+        String inputText = Console.readLine();
 
+        // TODO --> validation 실행 //
+
+        int numberOfTry = Integer.parseInt(inputText);
         return numberOfTry;
     }
 }
