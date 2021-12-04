1497530e5cd28f35ab38724e460155b4c32eceb2
Gyuchool <balance0591@naver.com>
2021-12-04 14:43:42 +0900

    feat(cars): 자동차를 관리하는 클래스 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Cars {
+	List<Car> cars;
+
+	public Cars() {
+		this.cars = new ArrayList<>();
+	}
+
+	public void join(String[] carNames) {
+
+		for (String carName : carNames) {
+			cars.add(new Car(carName));
+		}
+	}
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+}
