357a57f2cf4ae6322ce06b14b7ea2a3ee02d1915
DK <ldk980130@gmail.com>
2021-12-04 20:58:46 +0900

    feat : 한 시도 동안 자동차들이 레이스하는 기능 구현

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -2,6 +2,7 @@ package racingcar.service;
 
 import java.util.stream.Stream;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 import racingcar.repository.CarRepository;
 
@@ -9,8 +10,21 @@ public class RacingService {
 
 	private static final CarRepository carRepository = new CarRepository();
 
+	private static final int MIN_RANDOM_VALUE = 0;
+	private static final int MAX_RANDOM_VALUE = 9;
+
 	public void registerCars(String[] carNames) {
 		Stream.of(carNames)
 			.forEach(n -> carRepository.save(new Car(n)));
+	}
+
+	public void race() {
+		carRepository.findAll()
+			.stream()
+			.forEach(car -> car.action(getRandomValue()));
+	}
+
+	private int getRandomValue() {
+		return Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
 	}
 }
