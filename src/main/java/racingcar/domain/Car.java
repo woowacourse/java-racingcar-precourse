72ff6419d39b1d070acc53530c6aa789c47a04eb
HyoTaek-Jang <hyotaek9812@gmail.com>
2021-12-04 11:20:20 +0900

    feat: 자동차 전진 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,23 +5,29 @@ import static racingcar.util.Constant.*;
 import racingcar.util.Util;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    private boolean isGoing() {
-        return false;
-    }
+	private boolean isGoing(int value) {
+		if (value >= 4) {
+			return true;
+		}
+		return false;
+	}
 
-    public void move() {
-        int random = Util.getRandomNumber(RANDOM_START, RANDOM_END);
-    }
+	public void move() {
+		int random = Util.getRandomNumber(RANDOM_START, RANDOM_END);
+		if (isGoing(random)) {
+			this.position++;
+		}
+	}
 
-    @Override
-    public String toString() {
-        return "123";
-    }
+	@Override
+	public String toString() {
+		return "123";
+	}
 }
