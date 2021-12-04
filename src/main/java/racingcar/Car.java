a974832fc2a338aa8faf5035fdbc670873fd4a3f
Mo0nl19ht <mool2480@gmail.com>
2021-12-04 21:27:09 +0900

    feat: canMove 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,20 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public boolean canMove() {
+		if (pickNumberInRange(0, 9) >= 4) {
+			return true;
+		}
+		return false;
+	}
+
 }
