8a5f44127f54c21521e247712ba349370be18de7
Yunkeun <yunx2@naver.com>
2021-12-04 18:32:02 +0900

    refactor: Application에서 입력을 받아 함수 실행하게 로직 변경 GameController 클래스에서 함수를 줄일 수 있게 된다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,19 @@
 package racingcar;
 
 import racingcar.controller.GameController;
+import racingcar.model.Cars;
+import racingcar.utils.CarUtils;
+import racingcar.view.InputView;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO 구현 진행
-		GameController.playGame();
+		Cars cars;
+		int numberOfAttempts;
+
+		cars = CarUtils.makeCars(InputView.writeCarNames());
+		numberOfAttempts = InputView.writeNumberOfAttempts();
+		GameController.playRacing(cars, numberOfAttempts);
 	}
 }
--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,23 +1,13 @@
 package racingcar.controller;
 
-import java.util.List;
 import racingcar.model.Cars;
-import racingcar.utils.CarUtils;
-import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
 
 	private static final int MINIMUM_ATTEMPT_NUMBER = 0;
 
-	public static void playGame() {
-		List<String> carNames = InputView.writeCarNames();
-		Cars cars = CarUtils.makeCars(carNames);
-		int numberOfAttempts = InputView.writeNumberOfAttempts();
-		racing(cars, numberOfAttempts);
-	}
-
-	public static void racing(Cars cars, int numberOfAttempts) {
+	public static void playRacing(Cars cars, int numberOfAttempts) {
 		OutputView.printExecutionResult();
 		for (int i = MINIMUM_ATTEMPT_NUMBER; i < numberOfAttempts; i++) {
 			cars.showCarsPosition();
