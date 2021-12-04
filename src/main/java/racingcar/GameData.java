0a5371054975e0f05a10e4a64a7cd4e8450d678f
CptBluebear <cptbluebear@naver.com>
2021-12-04 16:09:07 +0900

    feat(GameEngine.java): 자동차 생성 기능 추가
    자동차를 생성해 리스트에 저장하는 기능을 추가함.
    이 과정에서 에러 메세지 출력을 위해
    GameData.java에 에러 메세지 문자열을 추가하였음.

--- src/main/java/racingcar/GameData.java
+++ src/main/java/racingcar/GameData.java
@@ -11,5 +11,6 @@ public class GameData {
 	public static final String CARNAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String TRY_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 	public static final String INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
+	public static final String CARNAME_LIMIT_ERROR_MESSAGE = "[ERROR] 자동차 이름은 %d 이하여야 한다.";
 
 }
--- src/main/java/racingcar/GameEngine.java
+++ src/main/java/racingcar/GameEngine.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameEngine {
 
 	InputHandler inputHandler;
@@ -9,7 +12,25 @@ public class GameEngine {
 	}
 
 	public void start() {
-		//구현 예정
+		System.out.println(GameData.CARNAME_INPUT_MESSAGE);
+		List<Car> carList = generateCars(readCarNames());
 	}
 
+	public List<String> readCarNames() {
+		while (true) {
+			try {
+				return inputHandler.readRacingCarNames();
+			} catch (IllegalArgumentException iae) {
+				System.err.println(String.format(GameData.CARNAME_LIMIT_ERROR_MESSAGE, GameData.CARNAME_LIMIT));
+			}
+		}
+	}
+
+	public List<Car> generateCars(List<String> carNames) {
+		List<Car> results = new ArrayList<Car>();
+		for (String carName : carNames) {
+			results.add(new Car(carName));
+		}
+		return results;
+	}
 }
