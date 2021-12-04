34f0df93f79fbbf2aaeb8aa49c2d9d3519178555
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-05 00:15:07 +0900

    test : test용 상수 클래스 생성

---
+++ src/test/java/racingcar/TestConstant.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+class TestConstant {
+
+    public static final int MOVING_FORWARD = 4;
+    public static final int STOP = 3;
+    public static final int MOVED = 1;
+    public static final int STAY = 0;
+    public static final int SAVED = 3;
+
+    public static final String ERROR_MESSAGE = "[ERROR]";
+
+}
