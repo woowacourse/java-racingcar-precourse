c812ed8a792523278ac883bf70af1c854d6ddf65
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-04 10:31:55 +0900

    test: 이름길이 예외케이서 1자 이상 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,20 +3,19 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 class CarTest {
 
-    @Test
-    @DisplayName("이름이 5자 이하가 아닐 경우 Exception이 발생해야 한다.")
-    void createExceptionByLargeNameLengthTest() {
-        // given
-        String name = "abcdef";
-
+    @ParameterizedTest
+    @ValueSource(strings = {"", "abcdef"})
+    @DisplayName("이름이 1자 이상 5자 이하가 아닐 경우 Exception이 발생해야 한다.")
+    void createExceptionByLargeNameLengthTest(String input) {
         // when & then
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new Car(name))
+            .isThrownBy(() -> new Car(input))
             .withMessage("[ERROR] Car의 이름은 5자 이하만 입력되어야 합니다.");
     }
 }
