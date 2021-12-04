9d3460f05df7f440ab267cabb5fcabfa1760a0d3
HyoTaek-Jang <hyotaek9812@gmail.com>
2021-12-04 12:45:11 +0900

    feat: 게임 시작 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,12 @@ package racingcar;
 
 import racingcar.domain.Car;
 import racingcar.domain.User;
+import racingcar.service.Game;
 
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
-
+        Game game = new Game(new User());
+        game.start();
     }
 }
