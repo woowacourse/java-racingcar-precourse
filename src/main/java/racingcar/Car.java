fecb69e811229eed68d90a2ea06db445390f3621
zmfl1230 <zmfl123097@gmail.com>
2021-12-04 15:04:31 +0900

    Revert "feat: design Car class structure"
    This reverts commit ffa389ca88e143bb81492ccd60b5a4fe52566e60.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,28 +4,9 @@ public class Car {
     private final String name;
     private int position = 0;
 
-    private final String MARK = "-";
-
     public Car(String name) {
         this.name = name;
     }
 
-    public int generateRandomValue() {
-
-        return 0;
-    }
-
-    public void updatePosition() {
-
-    }
-
-    private String indicatePosition() {
-
-        return "";
-    }
-
-    @Override
-    public String toString() {
-        return "";
-    }
+    // 추가 기능 구현
 }
