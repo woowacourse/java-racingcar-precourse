50030af026df3e38fb976f3838b59d98b0f66e24
leokim <kunga077@gmail.com>
2021-12-05 00:13:50 +0900

    feat: 사용자로부터 경주 차 이름 입력과 유효성 검사 및 등록
    - 경주할 차 이름들을 ,로 구분하여 입력을 받는다.
    - 각각의 차 이름 글자수가 5 이하인지 확인한다.
    - 글자수가 5를 넘어가면 에러를 발생시키고 다시 입력받는다.
    - 위의 유효성이 확인되면 List 형태로 차를 등록한다.

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,8 +1,42 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Racing {
 
-    public void start() {
+    private List<Car> lineup;
 
+    public Racing() {
+        this.lineup = new ArrayList<>();
+    }
+
+    public void start() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] waitingCarNames = Console.readLine().split(",");
+        enroll(waitingCarNames);
+    }
+
+    private void enroll(String[] waitingCarNames) {
+        for (int i = 0; i < waitingCarNames.length; i++) {
+            String carName = waitingCarNames[i].trim();
+            try {
+                validateCarName(carName);
+                Car car = new Car(carName);
+                lineup.add(car);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                this.lineup = new ArrayList<>();
+                String[] retriedWaitingCarNames = Console.readLine().split(",");
+                enroll(retriedWaitingCarNames);
+            }
+        }
+    }
+
+    private void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 이름의 길이가 5를 넘습니다");
+        }
     }
 }
