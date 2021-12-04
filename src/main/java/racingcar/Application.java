557714dbbcfd022083572a370587d970b0851904
KIM <km0192@naver.com>
2021-12-04 18:02:58 +0900

    feat(Race) : 입력받은 숫자와 자동차 리스트로 라운드 진행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Application {
 	public static void main(String[] args) {
-		List carList = new CarList().getList();
-		// 라운드 반복 진행 with 숫자생성
+		List<Car> carList = new CarList().getList();
+		Race race = new Race(carList);
 		// 우승자 인스턴스 생성
 		// 우승자 출력
 	}
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,17 +1,38 @@
 package racingcar;
 
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Race {
 	int repeatNum;
+	List<Car> carList;
 
-	Race() {
-
+	Race(List carList) {
+		this.carList = carList;
+		System.out.println("반복횟수 입력하세요 !");
+		repeatNum = new NumberInitiator().getNumber();
+		for (int i = 0; i < repeatNum; i++) {
+			new Round().play();
+		}
 	}
 
 	class Round {
 		void play() {
+			carList.forEach(car -> rollDice(car));
+			System.out.println();
+		}
 
+		void rollDice(Car car) {
+			if (4 <= rand()) {
+				car.moveNext();
+			}
+			car.printPosition();
+		}
+
+		int rand() {
+			return Randoms.pickNumberInRange(1, 9);
 		}
 	}
 
