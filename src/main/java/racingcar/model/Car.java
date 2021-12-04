f9d9dc60ff8d7e8ae313830a72dd520221df6e5b
RIANAEH <ririhan217@gmail.com>
2021-12-04 22:42:04 +0900

    refactor(car): refactor car
    따로 분리해서 작성했던 난수 생성 클래스를 없애고 해당 기능을 Car 클래스에 부여해주었습니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,9 +1,11 @@
 package racingcar.model;
 
-import racingcar.utils.RandomNumberPicker;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 	private static final int MOVE_FORWARD_START_NUMBER = 4;
+	private static final int START_INCLUSIVE = 0;
+	private static final int END_INCLUSIVE = 9;
 
 	private final String name;
 	private int position = 0;
@@ -12,14 +14,18 @@ public class Car {
 		this.name = name;
 	}
 
-	public String getCarName() {
-		return name;
-	}
-
 	public void moveForward() {
-		if (RandomNumberPicker.pick() >= MOVE_FORWARD_START_NUMBER) {
+		if (pickRandomNumber() >= MOVE_FORWARD_START_NUMBER) {
 			position++;
 		}
+	}
+
+	private int pickRandomNumber() {
+		return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+	}
+
+	public String getCarName() {
+		return name;
 	}
 
 	public int getPosition() {
