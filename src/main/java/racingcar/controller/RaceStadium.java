80d11a909ee78a79c6c451fc2c39c976a86b92fd
Gyuchool <balance0591@naver.com>
2021-12-04 16:23:46 +0900

    feat:(outputView): 매 횟수마다 자동차들의 이름과 위치를 출력하는 기능 구현

--- src/main/java/racingcar/controller/RaceStadium.java
+++ src/main/java/racingcar/controller/RaceStadium.java
@@ -1,14 +1,31 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RaceStadium {
 	private final Cars cars = new Cars();
 
 	private int trial;
+
 	public void start() {
 		cars.join(InputView.getInputRaceCar());
 		trial = InputView.getInputTrial();
+		racing();
+	}
+
+	private void racing() {
+		OutputView.printGameStartMessage();
+		for (int round = 0; round < trial; round++) {
+			moveTry();
+			OutputView.printRacingCurrentSituation(cars);
+		}
+	}
+	private void moveTry(){
+		for (Car participant : cars.getCars()) {
+			participant.moveOrStop();
+		}
 	}
 }
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,34 @@
+package racingcar.view;
+
+import java.util.List;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+public class OutputView {
+	private static final String TRIAL_RESULT = "실행 결과";
+	private static final String CAR_POSITION_PARTITION_FORMAT = " : ";
+	private static final String POSITION_INDICATOR = "-";
+
+	public static void printGameStartMessage() {
+		System.out.println(TRIAL_RESULT);
+	}
+
+	public static void printRacingCurrentSituation(Cars cars) {
+		List<Car> participants = cars.getCars();
+		for (Car car : participants) {
+			String position = currentPosition(car);
+			System.out.println(car.getName() + CAR_POSITION_PARTITION_FORMAT + position);
+		}
+		System.out.println();
+	}
+
+	private static String currentPosition(Car car) {
+		StringBuilder sb = new StringBuilder();
+		int position = car.getPosition();
+		for (int i = 0; i < position; i++) {
+			sb.append(POSITION_INDICATOR);
+		}
+		return sb.toString();
+	}
+}
