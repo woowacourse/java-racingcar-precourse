9e9eb3b3e44495a843a6403e76dbdea5db91714d
Wishoon <heonga2@gmail.com>
2021-12-04 23:35:42 +0900

    test: 등록된 자동차 전체의 운전 수행, 레이싱 게임 최종 라운드 후 우승자 판별 테스트 작성 리팩토링

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,53 +1,59 @@
 package racingcar.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.util.SymbolicConstantUtil.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import racingcar.util.NumberGeneratePolicy;
 
 class CarsTest {
+	private static final int MOVING_FORWARD = 5;
+	private NumberGeneratePolicy numberGeneratePolicy;
+
+	@BeforeEach
+	void 테스트용_숫자_생성_정책() {
+		numberGeneratePolicy = new NumberGeneratePolicy() {
+			@Override
+			public int generateNumber() {
+				return MOVING_FORWARD;
+			}
+		};
+	}
 
 	@ParameterizedTest
 	@ValueSource(strings = "hoon,ji,choi")
 	void 등록된_자동차_전체의_운전_수행(String value) {
 		// given
 		Cars cars = Cars.fromString(value);
-		NumberGeneratePolicy numberGeneratePolicy = new NumberGeneratePolicy() {
-			@Override
-			public int generateNumber() {
-				return 4;
-			}
-		};
 
 		// when
 		cars.driveAll(numberGeneratePolicy);
 
 		// then
-		for (Car car : cars.getDriveRecord()) {
-			assertEquals(car.getPosition(), 1);
+		String[] carsName = value.split(COMMA);
+		for (int index = 0; index < carsName.length; index++) {
+			assertEquals(cars.getDriveRecord().get(index).getPosition(), 1);
+			assertEquals(cars.getDriveRecord().get(index).getName(), carsName[index]);
 		}
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = "hoon,ji,choi")
+	@ValueSource(strings = "hoon,ji")
 	void 레이싱_게임_최종_라운드_후_우승자_판별(String value) {
 		// given
 		Cars cars = Cars.fromString(value);
-		NumberGeneratePolicy numberGeneratePolicy = new NumberGeneratePolicy() {
-			@Override
-			public int generateNumber() {
-				return 4;
-			}
-		};
 
 		// when
 		cars.driveAll(numberGeneratePolicy);
 
 		// then
-		for (Car winner : cars.getWinners()) {
-			assertEquals(winner.getPosition(), 1);
+		String[] carsName = value.split(COMMA);
+		for (int index = 0; index < carsName.length; index++) {
+			assertEquals(cars.getWinners().get(index).getPosition(), 1);
+			assertEquals(cars.getWinners().get(index).getName(), carsName[index]);
 		}
 	}
 }
