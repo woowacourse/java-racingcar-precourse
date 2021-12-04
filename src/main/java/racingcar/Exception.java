2b004ff7f5ce408683b8a499409c7ceb36eb6a6f
iamsseaa <homingsea@gmail.com>
2021-12-04 16:56:40 +0900

    feat: 차 이름 입력에 공백이 존재하면 예외 발생

--- src/main/java/racingcar/Exception.java
+++ src/main/java/racingcar/Exception.java
@@ -1,4 +1,11 @@
 package racingcar;
 
 public class Exception {
+    public static void isEmptyName(String[] allCarName) {
+        for (String name : allCarName) {
+            if (name.contains(" ")) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
