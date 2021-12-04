8ad53e5265a06473f6887a7218d79f444985cd9d
meatsby <meatsby@gmail.com>
2021-12-04 16:20:52 +0900

    refactor: 자동차 객체의 역할 재구성 및 상수화
    상수 DISPLACEMENT 를 통해 정확한 의미 전달하도록 재구성
    객체의 캡슐화를 위해 getter 메서드 제거
    - move 메서드를 통해 이름과 위치를 반환하도록 재구성
    - 최고기록을 확인할 때 해당 객체와 비교하도록 구현
    - 해당 객체의 기록이 최고기록인지 확인 후 이름 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 	private static final int MIN_POWER = 0;
 	private static final int MAX_POWER = 9;
 	private static final int THRESHOLD = 4;
+	private static final int DISPLACEMENT = 1;
 
 	private final String name;
 	private int position = 0;
@@ -14,17 +17,23 @@ public class Car {
 		this.name = name;
 	}
 
-	public void move() {
+	public ArrayList<String> move() {
+		ArrayList<String> result = new ArrayList<>();
 		if (Randoms.pickNumberInRange(MIN_POWER, MAX_POWER) >= THRESHOLD) {
-			position++;
+			position += DISPLACEMENT;
 		}
+		result.add(name);
+		result.add(Integer.toString(position));
+		return result;
 	}
 
-	public String getName() {
-		return name;
+	public int updateHighScore(int highScore) {
+		return Math.max(position, highScore);
 	}
 
-	public int getPosition() {
-		return position;
+	public void isChampion(ArrayList<String> champion, int highScore) {
+		if (position == highScore) {
+			champion.add(name);
+		}
 	}
 }
