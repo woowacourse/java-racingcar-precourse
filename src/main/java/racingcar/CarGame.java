34bc2075c81e62e9c1a34c4d4046348e09f7cb9c
yhh1056 <yhh1056@naver.com>
2021-12-05 01:20:02 +0900

    fix: 자동차 이름 입력 안내문구 수정

--- src/main/java/racingcar/CarGame.java
+++ src/main/java/racingcar/CarGame.java
@@ -3,7 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class CarGame implements Runnable {
-	private static final String GUIDE_INPUT_CAR_NAME = "String x = 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String GUIDE_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String COMMA = ",";
 
 	@Override
