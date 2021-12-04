e5dd08e57ce933a9c2c2ad7731756967470bd95b
xi_jjun <rlawowns97@naver.com>
2021-12-04 22:35:04 +0900

    refactor: modify Error message
    ERROR_INVALID_NAME_FORMAT 를 이름이 중복되는 상황을 고려하여
    [ERROR] 자동차의 이름은 중복이 안되는 1~5글자 입니다
    으로 변경.

--- src/main/java/racingcar/constants/Constant.java
+++ src/main/java/racingcar/constants/Constant.java
@@ -5,11 +5,16 @@ public class Constant {
 	public static final int MAX_CONDITION = 9;
 	public static final int MOVING_FORWARD = 4;
 
-	// === Check Proper Name Constants === //
 	public static final int MIN_NAME_LENGTH = 1;
 	public static final int MAX_NAME_LENGTH = 5;
 	public static final int IMPROPER_CHARACTER = ',';
 	public static final String SPLIT_STRING = ",";
+
+	public static final int MIN_STAGE_NUMBER = 1;
+	public static final int INVALID_STAGE_NUMBER = -1;
+
+	public static final String EMPTY = "";
+	public static final String SPACE = " ";
 
 	private Constant() {
 	}
--- src/main/java/racingcar/constants/Message.java
+++ src/main/java/racingcar/constants/Message.java
@@ -5,7 +5,7 @@ public class Message {
 	public static final String ASK_STAGES = "시도할 회수는 몇회인가요?";
 	public static final String EXECUTE_RESULT = "실행 결과";
 	public static final String WINNERS = "최종 우승자 : ";
-	public static final String ERROR_INVALID_NAME_FORMAT = "[ERROR] 자동차의 이름은 1~5글자 입니다";
+	public static final String ERROR_INVALID_NAME_FORMAT = "[ERROR] 자동차의 이름은 중복이 안되는 1~5글자 입니다";
 	public static final String ERROR_INVALID_STAGE_VALUE = "[ERROR] 시도할 횟수는 0이 아닌 숫자여야 합니다";
 
 	private Message() {
