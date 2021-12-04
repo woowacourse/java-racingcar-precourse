a113851f71aeccb3b23ecb109d43c8b85801b718
her0807 <her0807@naver.com>
2021-12-04 16:26:45 +0900

    feat: 자동차 리스트에 대한 책임이 있는 Cars 클래스 생성
    - 인터페이스 분리 원칙을 적용한 Validator 호출

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.domain.exception.CarNameValidator;
+import racingcar.domain.exception.Validator;
+
+public class Cars {
+    private static final String COMMA = ",";
+    private List<Car> cars;
+
+    public Cars(String names) {
+        cars = new ArrayList<>();
+        CarNameValidator validator = new Validator();
+        String[] nameArr = names.split(COMMA);
+        if (validator.isAllValid(nameArr)) {
+            for (String name : nameArr) {
+                cars.add(createCar(name));
+            }
+        }
+    }
+
+    private Car createCar(String name) {
+        return new Car(name);
+    }
+}
