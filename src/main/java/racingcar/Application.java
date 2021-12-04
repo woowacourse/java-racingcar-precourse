9f4c582c7ca1997a04e72ec4e83c076f1f122917
Gyuchool <balance0591@naver.com>
2021-12-04 14:23:33 +0900

    feat(raceStadium): 경기장 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.RaceStadium;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		// TODO 구현 진행
+		RaceStadium race = new RaceStadium();
+		race.start();
+	}
 }
