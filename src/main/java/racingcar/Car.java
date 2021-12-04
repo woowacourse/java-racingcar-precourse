2c4d89cc45a60a9da95a8b185ca65e5e7a2941be
KIM <km0192@naver.com>
2021-12-04 13:42:54 +0900

    refactor(README) - 출력기능 병합

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,23 @@
 package racingcar;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	// 추가 기능 구현
+	String getName() {
+		return name;
+	}
+
+	int getPosition() {
+		return position;
+	}
+
+	int moveNext() {
+        
+	}
 }
