69fd99befca090732367598902bca853e15b5e57
iamsseaa <homingsea@gmail.com>
2021-12-04 20:13:33 +0900

    feat: 차 이름 중복될 경우 예외 발생

--- src/main/java/racingcar/Exception.java
+++ src/main/java/racingcar/Exception.java
@@ -1,11 +1,22 @@
 package racingcar;
 
+import java.util.*;
+
 public class Exception {
     public static void isEmptyName(String[] allCarName) {
         for (String name : allCarName) {
             if (name.contains(" ")) {
                 throw new IllegalArgumentException();
             }
+        }
+    }
+
+    public static void isOverlap(String[] allCarName) {
+        List<String> list = Arrays.asList(allCarName);
+        Set<String> set = new HashSet<>(list);
+
+        if (list.size() != set.size()) {
+            throw new IllegalArgumentException();
         }
     }
 }
