6be5955b4af748b68bcb0705365b44af3e2a0fa3
CptBluebear <cptbluebear@naver.com>
2021-12-04 14:40:40 +0900

    feat(GameEngine.java): 게임 진행 처리 클래스 추가

---
+++ src/main/java/racingcar/GameEngine.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class GameEngine {
+
+	InputHandler inputHandler;
+
+	public GameEngine() {
+		inputHandler = new InputHandler();
+	}
+
+	public void start() {
+		//구현 예정
+	}
+
+}
