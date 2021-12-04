4c17cf49d74e0a510aabe80621d94a8b9c86c1f2
Hongdonggeon <ghd700@naver.com>
2021-12-04 22:57:57 +0900

    refactor(Referee) : Referee 클래스 구조 변경 -> Referee 객체가 생성  생성될 때 cars 객체와 연관성 x

--- src/main/java/racingcar/Referee.java
+++ src/main/java/racingcar/Referee.java
@@ -5,19 +5,13 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Referee implements Comparator<Car> {
-	private Cars cars;
-
-	public Referee(Cars cars) {
-		this.cars = cars;
-	}
-
 	@Override
 	public int compare(Car car1, Car car2) {
 		return car2.getPosition() - car1.getPosition();
 	}
 
-	public List<Car> selectWinners() {
-		List<Car> carList = this.cars.getCars();
+	public List<Car> selectWinners(Cars cars) {
+		List<Car> carList = cars.getCars();
 		int winnerPosition = carList.get(0).getPosition();
 		List<Car> winnerCars = new ArrayList<>();
 		for (Car car : carList) {
