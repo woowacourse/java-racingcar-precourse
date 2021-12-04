05be3de1e8f684aaf10c478d8c00adf49fed6f19
Gyuchool <balance0591@naver.com>
2021-12-04 18:37:03 +0900

    refactor(cars): lamda함수 간결화

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -23,8 +23,7 @@ public class Cars {
 	}
 
 	public Car getCarWithMaxPosition() {
-		Comparator<Car> carComparatorByPosition = Comparator.comparingInt(Car::getPosition);
-		return cars.stream().max(carComparatorByPosition).orElseThrow(IllegalArgumentException::new);
+		return cars.stream().max(Comparator.comparingInt(Car::getPosition)).get();
 	}
 
 }
