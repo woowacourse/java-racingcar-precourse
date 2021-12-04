a0184fffdb54d32199ab8d65ba09bb2d2f3a5511
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-05 05:40:01 +0900

    feat<GuideMessage>: 메세지 추가
    최종 우수자 및 에러 메세지 추가

--- src/main/java/racingcar/message/GuideMessage.java
+++ src/main/java/racingcar/message/GuideMessage.java
@@ -4,5 +4,8 @@ public class GuideMessage {
 	public static final String ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String ENTER_NUMBER = "시도할 회수는 몇회인가요?";
 	public static final String RESULT_MESSAGE = "\n실행 결과";
+	public static final String FINAL_WINNER = "최종 우승자 : ";
 	public static final String ERR_NAME_DUPLICATION = "[ERROR] 자동차 이름이 중복되지 않게 입력해주세요.";
+	public static final String ERR_NAME_LENGTH = "[ERROR] 자동차 이름은 5글자 이하로 입력해주세요";
+	public static final String ERR_INTEGER = "[ERROR] 숫자를 입력해주세요.";
 }
