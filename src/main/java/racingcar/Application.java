381b054d225fabcf33f302f4b69cb70f0160ef5d
eonju97 <eonju97.dev@gmail.com>
2021-12-04 11:27:54 +0900

    feat : 게임 진행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO 구현 진행
+
+        Game game = new Game();
+
+        game.run();
     }
 }
---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,71 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Game {
+
+    private static final int MINIMUM_RANDOM_NUMBER = 0;
+    private static final int MAXIMUM_RANDOM_NUMBER = 9;
+
+    private final Printer printer;
+    private List<Car> cars;
+    private int round;
+
+    public Game() {
+        this.printer = new Printer();
+        this.cars = new ArrayList<>();
+        this.round = 0;
+    }
+
+    public void run() {
+        Input input = new Input();
+
+        printer.printInputNameInfo();
+
+        List<String> names = input.inputCarNames();
+        this.cars = names.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+
+        printer.printInputRoundInfo();
+        this.round = input.inputRoundNumber();
+
+        for (int i = 0; i < round; i++) {
+            runRound();
+            printer.printMiddleResult(cars);
+        }
+
+        printTotalResult();
+    }
+
+    public void runRound() {
+        for (Car car : cars) {
+            car.goOrStop(Randoms.pickNumberInRange(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER));
+        }
+    }
+
+    private void printTotalResult() {
+        List<String> winners = findWinners();
+        printer.printWinner(winners);
+    }
+
+    private List<String> findWinners() {
+        int maxPosition = findMaxPosition();
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+}
