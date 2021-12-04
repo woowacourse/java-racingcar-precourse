a42e67c5b4d92eecbb34dd2c60536f5746a6ae20
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-05 05:31:49 +0900

    fix: 숫자 검증 버그 수정
    "[1-9]+"를 "[1-9]"로 수정했습니다. 한자리 수만 입력받아야 하기 때문입니다.
    또한, if 문에서 ! 기호를 붙여서 제대로 로직이 작동하도록 버그를 수정했습니다.

--- src/main/java/racingcar/validation/NumberValidation.java
+++ src/main/java/racingcar/validation/NumberValidation.java
@@ -5,9 +5,9 @@ import static racingcar.message.GuideMessage.*;
 public class NumberValidation {
 
 	public static void validateInputNumber(String inputString) {
-		String regex = "[1-9]+";
+		String regex = "[1-9]";
 
-		if (inputString.matches(regex)) {
+		if (!inputString.matches(regex)) {
 			throw new IllegalArgumentException(ERR_INTEGER);
 		}
 	}
