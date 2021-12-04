3e0f082dbea48990c0fa6ae975fdbf79ed8ec63d
Gyuchool <balance0591@naver.com>
2021-12-04 15:06:16 +0900

    feat(exception): 중복되는 이름 예외 처리

--- src/main/java/racingcar/exception/InputException.java
+++ src/main/java/racingcar/exception/InputException.java
@@ -1,12 +1,21 @@
 package racingcar.exception;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputException {
 	private static final String CAR_LAST_NAME_IS_COMMA = "[ERROR] 마지막 자동차 이름을 입력하지 않았습니다.";
+	private static final String CAR_DUPLICATE_NAME = "[ERROR] 자동차 이름은 중복이 불가능합니다.";
 
-	public static String isValidCarNames(String inputCarNames) {
+	public static String[] isValidCarNames(String inputCarNames) {
 
 		haveLastNameComma(inputCarNames);
-		return inputCarNames;
+		String[] carNames = inputCarNames.split(",");
+		if (!haveDuplicateName(carNames)) {
+			return carNames;
+		}
+		throw new IllegalArgumentException();
 	}
 
 	private static void haveLastNameComma(String carNames) {
@@ -15,4 +24,11 @@ public class InputException {
 		}
 	}
 
+	private static boolean haveDuplicateName(String[] carNames) {
+		Set<String> names = new HashSet<>(Arrays.asList(carNames));
+		if (names.size() != carNames.length) {
+			throw new IllegalArgumentException(CAR_DUPLICATE_NAME);
+		}
+		return false;
+	}
 }
