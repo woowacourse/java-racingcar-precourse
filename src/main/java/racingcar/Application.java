fc58a1c67d5677745fdd8f113c93353a6807eb4b
pup-paw <wldusdl0310@gmail.com>
2021-12-05 04:57:30 +0900

    feat: 게임 실행을 위한 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		GameController.run();
+	}
 }
