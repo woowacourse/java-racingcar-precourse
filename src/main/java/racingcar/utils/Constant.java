daefb73d6ffaa599a037c61905025a16d86ba986
kanamycine <jinyeong5320@gmail.com>
2021-12-04 18:10:34 +0900

    feat(Constant) : 예외 처리 메세지 추가

--- src/main/java/racingcar/utils/Constant.java
+++ src/main/java/racingcar/utils/Constant.java
@@ -3,8 +3,12 @@ package racingcar.utils;
 public class Constant {
 	public static final int MOVE_VALUE = 4;
 
-	public static String ASK_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	public static String ASK_GAME_COUNT = "시도할 회수는 몇회인가요?";
-	public static String PLAY_RESULT = "실행 결과";
-	public static String WINNER = "최종 우승자 : ";
+	public static final String ASK_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String ASK_GAME_COUNT = "시도할 회수는 몇회인가요?";
+	public static final String PLAY_RESULT = "실행 결과";
+	public static final String WINNER = "최종 우승자 : ";
+
+	public static final String LESS_MORE_NAME_ERROR = "[ERROR] : 모든 이름은 1~5자리 문자여야 합니다.";
+	public static final String CONTAIN_SAME_NAME_ERROR = "[ERROR] : 모든 이름은 중복을 허용하지 않습니다.";
+	public static final String WHITE_SPACE_NAME_ERROR = "[ERROR] : 공백으로만 이루어진 이름은 허용하지 않습니다.";
 }
