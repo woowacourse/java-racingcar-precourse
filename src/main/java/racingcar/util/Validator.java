9f6dbfae2452506e099c169cc631ece0e869fd87
dongho <dongho108@naver.com>
2021-12-04 17:27:07 +0900

    refactor: add validation (check space)

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -3,18 +3,28 @@ package racingcar.util;
 public class Validator {
 	private static final int CAR_NAME_RESTRICTIONS_MIN = 1;
 	private static final int CAR_NAME_RESTRICTIONS_MAX = 5;
+	private static final String SPACE = " ";
 	private final Parser parser = new Parser();
 
 	public void checkCarNamesInput(String input) throws IllegalArgumentException {
 		for (String carName : parser.parseCarNames(input)) {
-			if (CAR_NAME_RESTRICTIONS_MAX < carName.length()) {
-				throw new IllegalArgumentException(
-					"[ERROR] 자동차 이름은 " + CAR_NAME_RESTRICTIONS_MAX + "글자 이하여야 한다.");
-			}
-			if (carName.length() < CAR_NAME_RESTRICTIONS_MIN) {
-				throw new IllegalArgumentException(
-					"[ERROR] 자동차 이름은 " + CAR_NAME_RESTRICTIONS_MIN + "글자 이상이어야 한다.");
-			}
+			checkLength(carName);
+			checkSpace(carName);
+		}
+	}
+
+	private void checkSpace(String carName) throws IllegalArgumentException {
+		if (carName.contains(SPACE)) {
+			throw new IllegalArgumentException("[ERROR] 자농차 이름안에는 공백이 있어서는 안됩니다.");
+		}
+	}
+
+	private void checkLength(String carName) throws IllegalArgumentException {
+		if (CAR_NAME_RESTRICTIONS_MAX < carName.length()) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름은 " + CAR_NAME_RESTRICTIONS_MAX + "글자 이하여야 한다.");
+		}
+		if (carName.length() < CAR_NAME_RESTRICTIONS_MIN) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름은 " + CAR_NAME_RESTRICTIONS_MIN + "글자 이상이어야 한다.");
 		}
 	}
 
