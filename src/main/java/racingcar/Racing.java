c56192e607e973ead0de65d37bf314059b8c7a48
leokim <kunga077@gmail.com>
2021-12-05 01:13:24 +0900

    feat: 반복할 횟수 입력 및 유효성 확인
    - 사용자로부터 반복할 횟수를 입력받는다.
    - 숫자가 아니면 에러를 발생시킨다.
    - 0 이하면 에러를 발생시킨다.
    - 에러가 발생하면 에러 메시지를 출력한 뒤 다시 입력받는다.

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -16,6 +16,9 @@ public class Racing {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] waitingCarNames = Console.readLine().split(",");
         enroll(waitingCarNames);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        Integer repeatTimes = getRepeatTimes();
     }
 
     private void enroll(String[] waitingCarNames) {
@@ -37,6 +40,33 @@ public class Racing {
     private void validateCarName(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 이름의 길이가 5를 넘습니다");
+        }
+    }
+
+    private Integer getRepeatTimes() {
+        String rawRepeatTimes = Console.readLine();
+        try {
+            Integer repeatTimes = convertToNumber(rawRepeatTimes);
+            validateRepeatTimes(repeatTimes);
+            return repeatTimes;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.out.println("다시 입력해주세요.");
+            return getRepeatTimes();
+        }
+    }
+
+    private Integer convertToNumber(String rawRepeatTimes) {
+        try {
+            return Integer.parseInt(rawRepeatTimes);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private void validateRepeatTimes(Integer repeatTimes) {
+        if (repeatTimes <= 0) {
+            throw new IllegalArgumentException("[ERROR] 1 이상의 수를 입력해야 합니다");
         }
     }
 }
