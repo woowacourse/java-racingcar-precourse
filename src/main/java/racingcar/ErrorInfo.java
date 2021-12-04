2dfc68d6a73dc9110741d782147a2a14616cea51
yhh1056 <yhh1056@naver.com>
2021-12-05 02:53:03 +0900

    feat: 시도 횟수가 0 이하인 경우 예외 처리

--- src/main/java/racingcar/ErrorInfo.java
+++ src/main/java/racingcar/ErrorInfo.java
@@ -4,7 +4,8 @@ public enum ErrorInfo {
 	LENGTH("[ERROR] 이름은 5글자를 넘길 수 없습니다."),
  	OVERLAP("[ERROR] 이름이 중복됩니다."),
  	NUMBER("[ERROR] 시도 횟수는 숫자여야합니다."),
-	MAX("[ERROR] 시도 횟수가 100을 넘길 수 없습니다.");
+	MAX("[ERROR] 시도 횟수가 100을 넘길 수 없습니다."),
+	MIN("[ERROR] 시도 횟수는 1이상을 입력해주세요.");
 
 	private final String message;
 
--- src/main/java/racingcar/MoveCount.java
+++ src/main/java/racingcar/MoveCount.java
@@ -5,11 +5,13 @@ import racingcar.util.Assertions;
 
 public class MoveCount {
 	private static final int MAX_COUNT = 100;
+	private static final int MIN_COUNT = 0;
 
 	private final int count;
 
 	public MoveCount(int count) {
 		Assertions.throwExceptionIsTrue(count > MAX_COUNT, ErrorInfo.MAX);
+		Assertions.throwExceptionIsTrue(count <= MIN_COUNT, ErrorInfo.MIN);
 		this.count = count;
 	}
 
