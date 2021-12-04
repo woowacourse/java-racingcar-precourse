d3d5f6d17ee406fd474665845c7b82ef71e68303
Byeongju-Kong <bjuuuu98@gmail.com>
2021-12-04 13:19:51 +0900

    feat(InputView): 에러 메시지 출력 기능 추가 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,11 +1,17 @@
 package view;
 
 public class InputView {
+    private static final String ERROR_SYMBOL = "[ERROR] ";
+
     public void alertInputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void alertInputCountOfAttempt() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printErrorMessage(final String message) {
+        System.out.println(ERROR_SYMBOL + message);
     }
 }
