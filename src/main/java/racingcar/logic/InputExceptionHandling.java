9fec12efb7a87011948383332286cd131f1c80c8
YuJeong <yjfflq4872@gmail.com>
2021-12-05 15:34:31 +0900

    feat: 자동차 이름 입력 기능 구현

---
+++ src/main/java/racingcar/logic/InputExceptionHandling.java
@@ -0,0 +1,49 @@
+package racingcar.logic;
+
+import java.util.Arrays;
+
+public class InputExceptionHandling {
+	private static final String COUNT_ERROR_MSG = "하나 이상의 이름을 입력해야 한다.";
+	private static final String LENGTH_ERROR_MSG = "이름은 5글자 이하여야 한다.";
+	private static final String DUP_ERROR_MSG = "이름은 중복되지 않아야 한다.";
+	private static final String SPACE_ERROR_MSG = "공백은 이름으로 설정할 수 없다.";
+	private static final String SPACE_STRING = "";
+	private static final int MAX_NAME_LENGTH = 5;
+
+	public String[] validNameInput(String[] names) throws IllegalArgumentException {
+		validCount(names);
+		validLength(names);
+		validDuplication(names);
+		validIsSpace(names);
+
+		return names;
+	}
+
+	private void validCount(String[] names) throws IllegalArgumentException {
+		if (names.length <= 0) {
+			throw new IllegalArgumentException(COUNT_ERROR_MSG);
+		}
+	}
+
+	private void validLength(String[] names) throws IllegalArgumentException {
+		for (String name : names) {
+			if (name.length() > MAX_NAME_LENGTH) {
+				throw new IllegalArgumentException(LENGTH_ERROR_MSG);
+			}
+		}
+	}
+
+	private void validDuplication(String[] names) throws IllegalArgumentException {
+		if (Arrays.stream(names).distinct().count() != names.length) {
+			throw new IllegalArgumentException(DUP_ERROR_MSG);
+		}
+	}
+
+	private void validIsSpace(String[] names) throws IllegalArgumentException {
+		for (String name : names) {
+			if (name.equals(SPACE_STRING)) {
+				throw new IllegalArgumentException(SPACE_ERROR_MSG);
+			}
+		}
+	}
+}
---
+++ src/main/java/racingcar/view/InputFromUser.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.logic.InputExceptionHandling;
+
+public class InputFromUser {
+	private static final String INPUT_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_COUNT_MSG = "시도할 회수는 몇회인가요?";
+	private static final String DELIMITER = ",";
+	
+	private final InputExceptionHandling inputExceptionHandling;
+
+	public InputFromUser() {
+		inputExceptionHandling = new InputExceptionHandling();
+	}
+
+	public String[] inputCarName() {
+		System.out.println(INPUT_NAME_MSG);
+		String names = Console.readLine();
+		String[] carNames = names.split(DELIMITER);
+
+		try {
+			return inputExceptionHandling.validNameInput(carNames);
+		} catch (IllegalArgumentException e) {
+			System.out.println("[ERROR] " + e);
+			return inputCarName();
+		}
+	}
+}
