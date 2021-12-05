1719dcc0caf5fd448f36a1b867f5dfeb324d4fff
delphox60 <delphox60@gmail.com>
2021-12-05 11:04:39 +0900

    feat(GameSystem): 콘솔에 입력 메시지, 실행 결과 메시지를 출력하는 기능 구현

--- src/main/java/racingcar/GameSystem.java
+++ src/main/java/racingcar/GameSystem.java
@@ -6,12 +6,15 @@ public class GameSystem {
     CarEntry carEntry;
 
     public GameSystem() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         carEntry = new CarEntry(Console.readLine());
     }
 
     public void executeRace() {
         System.out.println("시도할 회수는 몇회인가요?");
         int nOfRuns = Integer.parseInt(Console.readLine());
+
+        System.out.println("실행 결과");
         for (int i = 0; i < nOfRuns; i++){
             runRaceOnce();
         }
