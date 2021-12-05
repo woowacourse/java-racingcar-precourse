eadba609e78427071a2323d12e9d4763043160d7
SuyeonChoi <tndus970120@gmail.com>
2021-12-05 23:23:42 +0900

    feat(InputView): add a function to validate if car name exceeds 5 characters

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,12 +3,28 @@ package view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+	private static final int MAX_CAR_NAME_LENGTH = 5;
 
 	private InputView() {
 	}
 
-	private String getCarNames() {
+	private String[] getCarNames() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		return Console.readLine();
+		String[] carNames = Console.readLine().split(",");
+		try {
+			validateCarName(carNames);
+		} catch (Exception exception) {
+			System.out.println("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+			getCarNames();
+		}
+		return carNames;
+	}
+
+	private void validateCarName(String[] carNames) {
+		for (String car : carNames) {
+			if (car.length() >= MAX_CAR_NAME_LENGTH) {
+				throw new IllegalArgumentException();
+			}
+		}
 	}
 }
