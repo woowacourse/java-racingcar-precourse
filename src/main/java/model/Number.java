369b01cef2d3f9c182bc790eb2d5bcea78c2ad99
bcc0830 <bcc0830@naver.com>
2021-12-05 20:40:15 +0900

    feat(Number) : 열거형 클래스인 IntEnums 추가 및 생성자 추가

---
+++ src/main/java/model/Number.java
@@ -0,0 +1,23 @@
+package model;
+
+enum NumberIntEnums {
+	RANDOM_NUMBER_START(0), RANDOM_NUMBER_END(9);
+	private final int value;
+
+	NumberIntEnums(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+}
+
+public class Number {
+	private int randomNumber;
+
+	public Number() {
+		this.randomNumber = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(
+			NumberIntEnums.RANDOM_NUMBER_START.getValue(), NumberIntEnums.RANDOM_NUMBER_END.getValue());
+	}
+}
