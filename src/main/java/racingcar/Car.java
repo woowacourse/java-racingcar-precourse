d323845841dee849d3445628ac34955dc0fb510d
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-12-05 22:49:19 +0900

    feat: 경주할 자동차 이름을 입력하는 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,7 +10,9 @@ public class Car {
         this.name = name;
     }
 
-    public void getCarName() {
+    public String getCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        return carNames;
     }
 }
