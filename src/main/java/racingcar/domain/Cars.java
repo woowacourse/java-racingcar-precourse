9716a777fb8cb0ca1d8e97e9bc88bfe0c82b8784
OzRagwort <ozragwort@gmail.com>
2021-12-05 16:53:26 +0900

    feat(Car): move all car forward

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cars {
+	private List<Car> cars = new LinkedList<>();
+
+	public Cars(String[] names) {
+		for (String name : names) {
+			cars.add(new Car(name));
+		}
+	}
+
+	public void racing() {
+		cars.forEach(Car::moveForward);
+	}
+}
