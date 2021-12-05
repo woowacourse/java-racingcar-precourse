956296800a016ce8a5b983780889423f6ec7d225
hoho00 <jhoya963@gmail.com>
2021-12-05 18:08:23 +0900

    feat($constants): add PromptConstants.java and ExceptionsConstants.java

---
+++ src/main/java/racingcar/common/constants/ExceptionConstants.java
@@ -0,0 +1,6 @@
+package racingcar.common.constants;
+
+public final class ExceptionConstants {
+	public static final String OVER_FIVE_WORDS_ERROR_MESSAGE = "[ERROR] 이름은 5자 이하만 가능하다.";
+	public static final String NOT_NUMBER_COUNT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
+}
---
+++ src/main/java/racingcar/common/constants/PromptConstants.java
@@ -0,0 +1,8 @@
+package racingcar.common.constants;
+
+public final class PromptConstants {
+	public static final String ASKING_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String ASKING_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+	public static final String SHOWING_RESULTS_MESSAGE = "실행 결과";
+	public static final String SHOWING_FINAL_WINNERS_MESSAGE = "최종 우승자";
+}
