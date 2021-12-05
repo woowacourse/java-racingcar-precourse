7569cbab8ddfc7f0728538f3b2b7473634165fa3
DWL21 <76774809+DWL21@users.noreply.github.com>
2021-12-05 17:29:31 +0900

    feat: 자동차들이 전진할 지 정해주는 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void stopOrGo() {
+
+    }
+
+    public boolean random() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            return true;
+        }
+        return false;
     }
 
     // 추가 기능 구현
