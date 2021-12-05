03c637c736f3d38d3a35fcb311e13254f8d3f525
OzRagwort <ozragwort@gmail.com>
2021-12-05 16:35:10 +0900

    feat(Car): move car forward by condition

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.enums.DomainConditions;
+
+public class Car {
+	private static final int RANDOM_START_NUMBER = DomainConditions.RANDOM_START_NUMBER.get();
+	private static final int RANDOM_END_NUMBER = DomainConditions.RANDOM_END_NUMBER.get();
+	private static final int MOVING_POINT_NUMBER = DomainConditions.MOVING_POINT_NUMBER.get();
+
+	private final String name;
+	private int position = 0;
+
+	public Car(String name) {
+		this.name = name;
+	}
+
+	public void moveForward() {
+		int randomValue = Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER);
+		if (randomValue >= MOVING_POINT_NUMBER) {
+			position++;
+		}
+	}
+}
--- src/main/java/racingcar/enums/DomainConditions.java
+++ src/main/java/racingcar/enums/DomainConditions.java
@@ -3,7 +3,10 @@ package racingcar.enums;
 public enum DomainConditions {
 	MAX_CAR_NAME_LENGTH(5),
 	EMPTY_CAR_NAME_LENGTH(0),
-	EMPTY_RACING_ROUND(0);
+	EMPTY_RACING_ROUND(0),
+	RANDOM_START_NUMBER(0),
+	RANDOM_END_NUMBER(9),
+	MOVING_POINT_NUMBER(4);
 
 	private final int value;
 
