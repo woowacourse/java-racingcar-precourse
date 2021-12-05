547fe16c724a1df22dc10c05a89ea085d6e5f4e9
skennetho <thrkd613@naver.com>
2021-12-05 19:53:34 +0900

    feat(Car): 자동차의 무작위값을 통한 전진 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,27 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
 public class Car {
     private final String name;
+    private final int CONDITION_TO_MOVE = 4;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void tryMove() {
+        if (isMovable()) {
+            position++;
+        }
+    }
+
+    private boolean isMovable() {
+        return pickRandomNumber() >= CONDITION_TO_MOVE;
+    }
+
+    private int pickRandomNumber() {
+        return pickNumberInRange(0, 9);
+    }
 }
