ea02a51eae3dc6efe63212bfd8d809dcfcff2b46
khj1 <dhepd8849@gmail.com>
2021-12-05 20:06:12 +0900

    feat: 자동차 경주 게임 애플리케이션 구현
    - 구현된 모든 기능을 종합하여 콘솔 상에서 게임을 진행할 수 있도록 애플리케이션을 구현했습니다.
    - getCarsWithUserInput() 메소드를 통해 사용자가 잘못된 자동차 이름 값을 입력하면 에러 메시지를 출력하고, 이름 값을 다시 입력받도록 구현했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final String ERROR = "[ERROR]";
+
     public static void main(String[] args) {
-        // TODO 구현 진행
+        Cars cars = getCarsWithUserInput();
+        int numberOfAttempts = Integer.parseInt(inputView.enterNumberOfAttempts());
+
+        outputView.printResult(cars, numberOfAttempts);
+        outputView.printWinners(cars);
+    }
+
+    private static Cars getCarsWithUserInput() {
+        Cars cars;
+
+        try {
+            cars = new Cars(inputView.enterCarNames());
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR + " " + e.getMessage());
+            cars = getCarsWithUserInput();
+        }
+
+        return cars;
     }
 }
