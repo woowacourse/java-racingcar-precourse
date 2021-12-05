609be4b3d3661d4c9890502f7b78dcecfb2c4dd1
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-12-05 22:20:24 +0900

    feat: 경기 시도회수를 입력하라는 문구 추가

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Racing {
+
+    private int times;
+
+    public void getTimes() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+}
