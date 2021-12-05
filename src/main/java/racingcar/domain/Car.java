64a5b1bac80d93a3514a3458bd95beaae04ff156
jochungbeom <cndqjacndqja@gmail.com>
2021-12-05 17:16:58 +0900

    refactor: Car 클래스 구현 순서 코딩 컨벤션 적용

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,13 +3,13 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car implements Comparable<Car> {
-	private final String name;
-	private int position = 0;
 	private static final int CONDITION_FOR_MOVE = 4;
 	private static final int MIX_RANDOM_NUMBER = 0;
 	private static final int MAX_RANDOM_NUMBER = 9;
 	private static final String CAR_POSITION_BY_FORMATTER = "-";
 	private static final String CAR_STATUS_DELIMITER = " : ";
+	private final String name;
+	private int position = 0;
 
 	public Car(String name) {
 		this.name = name;
