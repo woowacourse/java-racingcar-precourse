0e4c2639cdcda6f440d159cbe6a38cc2f84ccc89
swcho <progress0407@gmail.com>
2021-12-06 03:58:34 +0900

    style(RandomMovingStrategy): 매직넘버 제거

--- src/main/java/racingcar/domain/RandomMovingStrategy.java
+++ src/main/java/racingcar/domain/RandomMovingStrategy.java
@@ -5,10 +5,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomMovingStrategy implements MovingStrategy {
 
 	public static final int MOVING_THRESHOLD = 4;
+	public static final int MINIMUM_MOVING_VALUE = 0;
+	public static final int MAXIMUM_MOVING_VALUE = 9;
 
 	@Override
 	public boolean isMovable() {
-		int randomNumbers = Randoms.pickNumberInRange(0, 9);
+		int randomNumbers = Randoms.pickNumberInRange(MINIMUM_MOVING_VALUE, MAXIMUM_MOVING_VALUE);
 		if (randomNumbers >= MOVING_THRESHOLD) {
 			return true;
 		}
