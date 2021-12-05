21bd51530c03e7f4c62359c8b6aa03df03eae7d7
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-05 21:38:19 +0900

    feat(Car.java): 주어진 범위 사이 무작위 값이 기준 이상인 경우 position이 1크게 하는 goOrStop() 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+
+	private final int START_NUMBER_OF_RANGE = 0;
+	private final int END_NUMBER_OF_RANGE = 9;
+	private final int STANDARD_NUMBER_OF_GO = 4;
+
 	private final String name;
 	private int position = 0;
 
 	public Car(String name) {
 		this.name = name;
 	}
-	// 추가 기능 구현
+
+	public void goOrStop() {
+		int randomNumber = Randoms.pickNumberInRange(START_NUMBER_OF_RANGE, END_NUMBER_OF_RANGE);
+		if (randomNumber >= STANDARD_NUMBER_OF_GO) {
+			position++;
+		}
+	}
 }
