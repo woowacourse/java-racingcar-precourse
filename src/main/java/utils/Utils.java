58303b323376c80fbcbe6430af5288a9128b71a3
bcc0830 <bcc0830@naver.com>
2021-12-05 16:45:24 +0900

    feat(Utils) : 특정 문자열이 특정 길이조건을 만족하는지 알려주는 기능 추가

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -4,12 +4,19 @@ import java.lang.IllegalArgumentException;
 
 public class Utils {
 	private static final String specialCharMessage = "[ERROR] 자동차의 이름에는 특수문자 혹은 공백이 들어갈 수 없습니다.\n";
+	private static final String lengthMessage = "[ERROR] 자동차의 이름의 글자는 5글자를 초과할 수 없습니다.\n";
 
 	public static void checkSpecialChar(String inputToken, char start, char end) throws IllegalArgumentException {
 		for (int index = 0; index < inputToken.length(); index++) {
 			if (!(start <= inputToken.charAt(index) && inputToken.charAt(index) <= end)) {
 				throw new IllegalArgumentException(specialCharMessage);
 			}
+		}
+	}
+
+	public static void checkLength(String inputToken, int size) throws IllegalArgumentException {
+		if (inputToken.length() > size) {
+			throw new IllegalArgumentException(lengthMessage);
 		}
 	}
 }
