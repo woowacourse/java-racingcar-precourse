ca76c3afe4afd713a193c11ceaabe50db2db00aa
iamsseaa <homingsea@gmail.com>
2021-12-05 15:46:01 +0900

    feat: 시도 횟수가 숫자(정수)가 아닌 경우 예외 발생

--- src/main/java/racingcar/RoundTimesException.java
+++ src/main/java/racingcar/RoundTimesException.java
@@ -1,4 +1,10 @@
 package racingcar;
 
 public class RoundTimesException {
+
+    public static void isNumber(String times) {
+        if (!times.matches("-?\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 정수여야 합니다.");
+        }
+    }
 }
