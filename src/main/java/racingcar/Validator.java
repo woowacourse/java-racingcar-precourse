8d9534f51e4f76d6b7507865cdc08a42b4d8219d
Sanggu <rkdtkdrn93@naver.com>
2021-12-05 23:53:00 +0900

    refactor: ERROR MSG 이름 변경 및 TEST 코드 코드 컨벤션 적용

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -7,17 +7,17 @@ import java.util.List;
 
 public class Validator {
 	private static final int MAX_LENGTH_OF_NAME = 5;
-	private static final String ERROR = "[ERROR] ";
+	private static final String ERROR_MSG = "[ERROR] ";
 
 	public static void isNull(String input) {
 		if (input.isEmpty()) {
-			throw new IllegalArgumentException(ERROR + "입력 값이 공백입니다.");
+			throw new IllegalArgumentException(ERROR_MSG + "입력 값이 공백입니다.");
 		}
 	}
 
 	public static void nameLength(String input) {
 		if (input.length() > MAX_LENGTH_OF_NAME) {
-			throw new IllegalArgumentException(ERROR + "자동차 이름은 최대 5글자입니다.");
+			throw new IllegalArgumentException(ERROR_MSG + "자동차 이름은 최대 5글자입니다.");
 		}
 	}
 
@@ -28,7 +28,7 @@ public class Validator {
 		}
 		for (String name : nameList) {
 			if (Collections.frequency(nameList, name) != 1) {
-				throw new IllegalArgumentException(ERROR + "중복된 이름을 입력했습니다.");
+				throw new IllegalArgumentException(ERROR_MSG + "중복된 이름을 입력했습니다.");
 			}
 		}
 	}
@@ -37,7 +37,7 @@ public class Validator {
 		try {
 			Integer.parseInt(input);
 		} catch (Exception e) {
-			throw new IllegalArgumentException(ERROR + "시도 횟수는 숫자여야 합니다.");
+			throw new IllegalArgumentException(ERROR_MSG + "시도 횟수는 숫자여야 합니다.");
 		}
 	}
 
