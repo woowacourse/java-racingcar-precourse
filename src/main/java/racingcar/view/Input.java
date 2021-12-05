d01289d803bb0704e67cf6e36d95026c7a0ce6ba
jurlring <78091011+jurlring@users.noreply.github.com>
2021-12-06 02:19:41 +0900

    feat : 예외 발생 시 재입력 기능 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -2,19 +2,25 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 import static racingcar.exception.CarNameException.*;
-
-import java.util.Arrays;
-import java.util.List;
+import static racingcar.exception.CountException.*;
 
 public class Input {
 
 	static final String CAR_NAME_DISTINGUISH_STRING = ",";
+	private static final String CARS_NAME_INPUT_MASSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-	public List<String> requestCarsName() {
-		String carsName = readLine();
-		String[] carsNameList = stringToArraysByComma(carsName);
-		checkCarNameException(carsNameList);
-		return Arrays.asList(carsNameList);
+	public String[] requestCarsName() {
+		System.out.println(CARS_NAME_INPUT_MASSAGE);
+		while (true) {
+			String carsName = readLine();
+			String[] carsNameList = stringToArraysByComma(carsName);
+			try {
+				checkCarNameException(carsNameList);
+				return carsNameList;
+			} catch (Exception exception) {
+				System.out.println(exception.getMessage());
+			}
+		}
 	}
 
 	public String[] stringToArraysByComma(String carsName) {
