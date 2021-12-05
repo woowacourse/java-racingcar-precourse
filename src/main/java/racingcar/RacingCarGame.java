d1e2f3a23f2dea4b3f620060f3dec2a7e70cca20
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-05 23:09:30 +0900

    feat(RacingCarGame.java): play()안에서 게임 실행 결과 출력 구현

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class RacingCarGame {
 
+	private final String RESULT_MESSAGE = "실행 결과";
+
 	User user;
 	List<Car> carList;
 
@@ -17,12 +19,24 @@ public class RacingCarGame {
 		user.askNamesUntilCorrect();
 		user.askNumberOfTimesUntilCorrect();
 		makeCarList();
+		printResult();
 	}
 
 	private void makeCarList() {
 		for (String name : user.getNames()) {
 			Car car = new Car(name);
 			carList.add(car);
+		}
+	}
+
+	private void printResult() {
+		System.out.println(RESULT_MESSAGE);
+		for (int i = 0; i < user.getNumberOfTimes(); i++) {
+			for (Car car : carList) {
+				car.goOrStop();
+				car.printState();
+			}
+			System.out.println();
 		}
 	}
 }
