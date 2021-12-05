6e60165686988027c5bbfd895494f8d8e0b5f252
YuJeong <yjfflq4872@gmail.com>
2021-12-05 15:53:13 +0900

    feat: 랜덤수 생성 구현

---
+++ src/main/java/racingcar/utill/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.utill;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+	private static final int START_RANGE = 0;
+	private static final int END_RANGE = 9;
+
+	public int generateRandomNumber() {
+		return Randoms.pickNumberInRange(START_RANGE, END_RANGE);
+	}
+}
