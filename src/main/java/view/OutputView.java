760428bb1083dec4a4c7be670704ad07ce6eaeb4
bcc0830 <bcc0830@naver.com>
2021-12-05 20:19:57 +0900

    feat(OutputView) : 사용자의 전체 게임 횟수 입력을 요구하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
 	public void noticeNameInput() {
 		System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
 	}
+
+	public void noticeTurnInput() {
+		System.out.print("시도할 회수는 몇회인가요?\n");
+	}
 }
