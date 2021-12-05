11e1ed456f632dae6edff0a51318b33e2e249b75
DWL21 <76774809+DWL21@users.noreply.github.com>
2021-12-05 17:03:58 +0900

    fix: 자동차이름과 횟수를 입력받는 기능 메세지 실행순서 변경

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -24,18 +24,18 @@ public class Game {
     }
 
     public void carInput() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         checking = false;
         while (!checking) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             Input();
             checking = checkCars();
         }
     }
 
     public void timesInput() {
-        System.out.println("시도할 회수는 몇회인가요?");
         checking = false;
         while (!checking) {
+            System.out.println("시도할 회수는 몇회인가요?");
             Input();
             checking = checkTimes();
         }
