d7f68c77a1cfc20fbc72aa1ea02a0860854ea465
jurlring <78091011+jurlring@users.noreply.github.com>
2021-12-06 02:22:50 +0900

    feat : 시도할 횟수 입력 기능 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -8,6 +8,7 @@ public class Input {
 
 	static final String CAR_NAME_DISTINGUISH_STRING = ",";
 	private static final String CARS_NAME_INPUT_MASSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String COUNT_INPUT_MASSAGE = "시도할 회수는 몇회인가요?";
 
 	public String[] requestCarsName() {
 		System.out.println(CARS_NAME_INPUT_MASSAGE);
@@ -27,6 +28,18 @@ public class Input {
 		return carsName.split(CAR_NAME_DISTINGUISH_STRING);
 	}
 
-	
+	public int requestCount() {
+		System.out.println(COUNT_INPUT_MASSAGE);
+		while (true) {
+			String countInput = readLine();
+			try {
+				checkCountException(countInput);
+				return Integer.parseInt(countInput);
+			} catch (Exception exception) {
+				System.out.println(exception.getMessage());
+			}
+		}
+
+	}
 
 }
