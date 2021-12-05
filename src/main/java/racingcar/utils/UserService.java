9cf0cd1b1031ba59dc67d472fdd6e680ed9d5b81
xi_jjun <rlawowns97@naver.com>
2021-12-06 06:37:20 +0900

    refactor: Verifier 생성으로 인한 코드 리팩토링

--- src/main/java/racingcar/utils/UserService.java
+++ src/main/java/racingcar/utils/UserService.java
@@ -1,17 +1,13 @@
 package racingcar.utils;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.constants.Constant;
 import racingcar.constants.Message;
 
 public class UserService {
 	public String[] register() {
-		String[] names = new String[]{Constant.EMPTY};
-
-		while (names[0].equals(Constant.EMPTY)) {
+		String[] names = new String[]{};
+		while (names.length == 0) {
 			names = getCarNames();
 		}
 
@@ -19,44 +15,22 @@ public class UserService {
 	}
 
 	private String[] getCarNames() {
-		String[] names;
 		try {
-			names = getValidName();
+			String input = Console.readLine();
+			return getValidName(input);
 		} catch (IllegalArgumentException e) {
 			System.out.println(Message.ERROR_INVALID_NAME_FORMAT);
-			return new String[]{Constant.EMPTY};
+			return new String[]{};
 		}
-
-		return names;
 	}
 
-	private String[] getValidName() {
-		String input = Console.readLine();
+	private String[] getValidNames(String input) {
 		String[] names = input.split(Constant.SPLIT_STRING);
 
-		checkLastIndex(input);
-		checkValidName(names);
+		Verifier.checkLastIndex(input);
+		Verifier.checkValidNames(names);
 
 		return names;
-	}
-
-	private void checkValidName(String[] names) {
-		Set<String> checker = new HashSet<>();
-		for (String name : names) {
-			if (!isValidNameLength(name) || !checker.add(name)) {
-				throw new IllegalArgumentException();
-			}
-		}
-	}
-
-	private boolean isValidNameLength(String name) {
-		return Constant.MIN_NAME_LENGTH <= name.length() && name.length() <= Constant.MAX_NAME_LENGTH;
-	}
-
-	private void checkLastIndex(String userInput) {
-		if (userInput.lastIndexOf(Constant.IMPROPER_CHARACTER) == userInput.length() - 1) {
-			throw new IllegalArgumentException();
-		}
 	}
 
 	public int howManyTimes() {
@@ -64,25 +38,17 @@ public class UserService {
 		while (stage == Constant.INVALID_STAGE_NUMBER) {
 			stage = getStage();
 		}
+
 		return stage;
 	}
 
 	private int getStage() {
-		int stage;
 		try {
 			String input = Console.readLine();
-			stage = getValidStage(input);
+			return Verifier.checkValidStage(input);
 		} catch (IllegalArgumentException e) {
 			System.out.println(Message.ERROR_INVALID_STAGE_VALUE);
 			return Constant.INVALID_STAGE_NUMBER;
 		}
-		return stage;
-	}
-
-	private int getValidStage(String input) {
-		if (input.chars().allMatch(Character::isDigit)) {
-			return Integer.parseInt(input);
-		}
-		return Constant.INVALID_STAGE_NUMBER;
 	}
 }
