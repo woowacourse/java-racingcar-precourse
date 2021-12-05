9cd9f35784b2e0f0f9562d8eba084cfb33b4da70
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-05 21:50:04 +0900

    feat(Car.java): name과 position을 출력하는 printState() 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,7 +6,7 @@ public class Car {
 
 	private final int START_NUMBER_OF_RANGE = 0;
 	private final int END_NUMBER_OF_RANGE = 9;
-	private final int STANDARD_NUMBER_OF_GO = 4;
+	private final int START_NUMBER_OF_GO = 4;
 
 	private final String name;
 	private int position = 0;
@@ -17,8 +17,16 @@ public class Car {
 
 	public void goOrStop() {
 		int randomNumber = Randoms.pickNumberInRange(START_NUMBER_OF_RANGE, END_NUMBER_OF_RANGE);
-		if (randomNumber >= STANDARD_NUMBER_OF_GO) {
+		if (randomNumber >= START_NUMBER_OF_GO) {
 			position++;
 		}
+	}
+
+	public void printState() {
+		String state = name + " : ";
+		for (int i = 0; i < position; i++) {
+			state += "-";
+		}
+		System.out.println(state);
 	}
 }
