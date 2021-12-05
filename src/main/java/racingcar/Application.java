e3a7b16668b8dab7381b1e8ff32934e920a55d08
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-06 03:48:55 +0900

    feat: 시도횟수 입력기능 추가 및 시도횟수 오류 시 반복 입력 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,33 @@
 package racingcar;
 
 import static racingcar.view.InputView.inputCarNames;
+import static racingcar.view.InputView.inputRound;
 
 import racingcar.domain.Cars;
+import racingcar.domain.Round;
 
 public class Application {
+
     public static void main(String[] args) {
-        Cars cars = createRandomCars();
+        Cars cars = getInputRandomCars();
+        Round round = getInputRound();
     }
 
-    private static Cars createRandomCars() {
+    private static Cars getInputRandomCars() {
         try {
             return Cars.createRandomCars(inputCarNames());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return createRandomCars();
+            return getInputRandomCars();
+        }
+    }
+
+    private static Round getInputRound() {
+        try {
+            return Round.createByStringRound(inputRound());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getInputRound();
         }
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ROUND = "시도할 회수는 몇회인가요?";
 
     private InputView() {
     }
@@ -16,5 +17,10 @@ public class InputView {
         System.out.println(INPUT_CAR_NAMES);
         return Arrays.stream(Console.readLine().split(","))
             .collect(Collectors.toList());
+    }
+
+    public static String inputRound() {
+        System.out.println(INPUT_ROUND);
+        return Console.readLine();
     }
 }
