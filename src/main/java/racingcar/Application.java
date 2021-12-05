10e57709194bdf1bdedc23093eec96a920a869b3
OzRagwort <ozragwort@gmail.com>
2021-12-05 17:40:31 +0900

    feat: integrate feature

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGameController;
+
 public class Application {
 	public static void main(String[] args) {
-		// TODO 구현 진행
+		RacingCarGameController gameController = new RacingCarGameController();
+		gameController.play();
 	}
 }
---
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -0,0 +1,32 @@
+package racingcar.controller;
+
+import static racingcar.view.InputView.*;
+import static racingcar.view.OutputView.*;
+
+import java.io.InputStream;
+import java.util.List;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+public class RacingCarGameController {
+	private Cars cars;
+	private int racingRound = 0;
+
+	public RacingCarGameController() {
+	}
+
+	public void play() {
+		cars = new Cars(readCarNames());
+		racingRound = readRacingRound();
+		System.out.println();
+
+		printStartRoundMessage();
+		for (int i = 0; i < racingRound; i++) {
+			cars.racing();
+		}
+
+		List<Car> winners = cars.findWinners();
+		printWinners(winners);
+	}
+}
