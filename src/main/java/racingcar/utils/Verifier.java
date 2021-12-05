e04271e6c32e4a3c3c9efa2115fd243d8e020ed9
xi_jjun <rlawowns97@naver.com>
2021-12-06 06:24:02 +0900

    feat: add Verifier functions
    사용자의 입력이 알맞은지 판단해주는 클래스
    checkLastIndex(userInput) : userInput의 맨 마지막 문자가 ','라면 IllegalArgumentException 발생.
    checkValidNames(names[]) : names 배열의 값들이 적절한지 판단.
    	ㄴisValidNameLength(name) : name이 1~5 글자 수를 가졌다면 return true.
    	ㄴisDuplicated(Set<String> set, name) : name이 set안에 이미 있다면 return true.
    checkValidStage(input) : input 이 적절하면 int type으로 변환한 input을 반환.
    	ㄴisNumber(input) : input이 숫자면 return true. 아니면 IllegalArgumentException 발생.

---
+++ src/main/java/racingcar/utils/Verifier.java
@@ -0,0 +1,50 @@
+package racingcar.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import racingcar.constants.Constant;
+
+public class Verifier {
+
+	private Verifier() {
+	}
+
+	public static void checkLastIndex(String userInput) {
+		if (userInput.lastIndexOf(Constant.IMPROPER_CHARACTER) == userInput.length() - 1) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void checkValidName(String[] names) {
+		Set<String> namesSet = new HashSet<>();
+		for (String name : names) {
+			if (!isValidNameLength(name) || isDuplicated(namesSet, name)) {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
+
+	private static boolean isDuplicated(Set<String> names, String name) {
+		return !names.add(name);
+	}
+
+	private static boolean isValidNameLength(String name) {
+		return Constant.MIN_NAME_LENGTH <= name.length() && name.length() <= Constant.MAX_NAME_LENGTH;
+	}
+
+	public static int checkValidStage(String input) {
+		if (isNumber(input)) {
+			int stage = Integer.parseInt(input);
+			if (stage != 0) {
+				return stage;
+			}
+		}
+
+		throw new IllegalArgumentException();
+	}
+
+	private static boolean isNumber(String input) {
+		return input.chars().allMatch(Character::isDigit);
+	}
+}
