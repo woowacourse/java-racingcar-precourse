757dff2bb6a6ddbd86605012bbe2ef9043bab861
delphox60 <delphox60@gmail.com>
2021-12-05 11:00:49 +0900

    feat(GameSystem): 시도할 회수를 입력받는 기능 구현
    - executeGame(): 경주 게임을 실행하는 method 스켈레톤 코드 작성
    - runRaceOnce(): 경주를 1회 시도하는 method 선언

--- src/main/java/racingcar/GameSystem.java
+++ src/main/java/racingcar/GameSystem.java
@@ -8,4 +8,17 @@ public class GameSystem {
     public GameSystem() {
         carEntry = new CarEntry(Console.readLine());
     }
+
+    public void executeRace() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int nOfRuns = Integer.parseInt(Console.readLine());
+        for (int i = 0; i < nOfRuns; i++){
+            runRaceOnce();
+        }
+        carEntry.printWinners();
+    }
+
+    private void runRaceOnce() {
+        //TODO
+    }
 }
