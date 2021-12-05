2fb55192c2a9c2a5e15c540fc247e87e37c851d3
hyunrrr <whgusrms96@gmail.com>
2021-12-05 20:45:39 +0900

    refactor(Car): 자동차 움직이는 기능을 Car클래스 내부로 이동

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,10 @@
 package racingcar;
 
 public class Car {
+	private static final int RANDOM_NUMBER_MINIMUM = 0;
+	private static final int RANDOM_NUMBER_MAXIMUM = 9;
+	private static final int MINIMUM_CONDITION_MOVE_FORWARD = 4;
+
 	private final String name;
 	private int position = 0;
 
@@ -8,8 +12,18 @@ public class Car {
 		this.name = name;
 	}
 
-	// 추가 기능 구현
-	protected void moveCar(int forwardStep) {
-		position += forwardStep;
+	private int makeRandomOneDigitNum() {
+		return camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(RANDOM_NUMBER_MINIMUM, RANDOM_NUMBER_MAXIMUM);
+	}
+
+	private int convertRandomNumToForwardStep(int randomNum) {
+		if (randomNum > MINIMUM_CONDITION_MOVE_FORWARD) {
+			return 1;
+		}
+		return 0;
+	}
+
+	protected void moveCar() {
+		position += convertRandomNumToForwardStep(makeRandomOneDigitNum());
 	}
 }
