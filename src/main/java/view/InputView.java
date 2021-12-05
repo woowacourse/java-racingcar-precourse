c4c2fe5893fe52375a73f62c1c2f49a9daab96d3
SuyeonChoi <tndus970120@gmail.com>
2021-12-05 22:41:01 +0900

    feat(InputView): add car name input function

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,14 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+	private InputView() {
+	}
+
+	private String getCarNames() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		return Console.readLine();
+	}
+}
