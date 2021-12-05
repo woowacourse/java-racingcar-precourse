2a8fee7d431713e9dcfde5320a5808cd12e8c9d8
swcho <progress0407@gmail.com>
2021-12-06 03:52:33 +0900

    style(OutputView): 출력부 멘트 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,9 +7,10 @@ import racingcar.domain.Cars;
 
 public class OutputView {
 
-	public static final String OUTPUT_RESULT = "실행 결과";
+	public static final String OUTPUT_RESULT = "\n실행 결과";
 
 	public static void printResult(ViewDto viewDto) {
+		System.out.println(OUTPUT_RESULT);
 		Cars cars = viewDto.getCars();
 		int tryNumber = viewDto.getTryNumber();
 		while (!cars.isEndGame(tryNumber)) {
