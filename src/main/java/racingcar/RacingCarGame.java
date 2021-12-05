fc2400285e4208fb687f651f0fb17684c5176974
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-05 23:10:49 +0900

    feat(RacingCarGame.java): play()안에서 최종 우승자 출력 구현

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class RacingCarGame {
 
 	private final String RESULT_MESSAGE = "실행 결과";
+	private final String WINNER_MESSAGE = "최종 우승자 : ";
 
 	User user;
 	List<Car> carList;
@@ -38,5 +39,17 @@ public class RacingCarGame {
 			}
 			System.out.println();
 		}
+	}
+
+	private void printWinner() {
+		carList.sort(Car.winComparator);
+		Car winner = carList.get(0);
+		String winnerNames = winner.getName();
+		for (int i = 1; i < carList.size(); i++) {
+			if (winner.draw(carList.get(i))) {
+				winnerNames += ", " + carList.get(i).getName();
+			}
+		}
+		System.out.println(WINNER_MESSAGE + winnerNames);
 	}
 }
