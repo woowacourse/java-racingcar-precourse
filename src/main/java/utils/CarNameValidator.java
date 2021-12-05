5f459116ba5ef48766f34bfad669d1098fa5bfc1
nayoung <evelyn82ny@gmail.com>
2021-12-05 11:17:22 +0900

    refactor: 에러 메시지 생성 방식 변경

--- src/main/java/utils/CarNameValidator.java
+++ src/main/java/utils/CarNameValidator.java
@@ -2,7 +2,7 @@ package utils;
 
 public class CarNameValidator extends RuntimeException {
 
-    public static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+    public static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차 이름은 length 자 이하만 가능합니다. name 이전까지의 자동차가 생성되었습니다.";
     public static final int CAR_NAME_LENGTH = 5;
 
     public static void validateLengthInRange(String data) {
@@ -13,6 +13,8 @@ public class CarNameValidator extends RuntimeException {
     }
 
     private static String getLengthErrorMessage(String data) {
-        return NAME_LENGTH_ERROR_MESSAGE + " "  + data + " 이전까지의 자동차가 생성되었습니다.";
+        return NAME_LENGTH_ERROR_MESSAGE
+                .replaceFirst("length", String.valueOf(CAR_NAME_LENGTH))
+                .replaceFirst("name", data);
     }
 }
