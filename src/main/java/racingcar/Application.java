8aa00ac1dd906ea76f8accea91729b4889e9feef
bcc0830 <bcc0830@naver.com>
2021-12-05 21:12:25 +0900

    feat(Application) : Controller를 통한 전체 게임 진행 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import controller.GameController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		// TODO 구현 진행
+		GameController.runGame();
+	}
 }
