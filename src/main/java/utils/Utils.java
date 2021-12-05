a21b76f0926f4502aba2fa40ffd3588247997f0f
bcc0830 <bcc0830@naver.com>
2021-12-05 16:59:57 +0900

    feat(Utils) : 사용자의 입력을 특정 구분자를 기준으로 잘라주는 기능 추가

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -2,13 +2,20 @@ package utils;
 
 import java.lang.IllegalArgumentException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Utils {
 	private static final String specialCharMessage = "[ERROR] 자동차의 이름에는 특수문자 혹은 공백이 들어갈 수 없습니다.\n";
 	private static final String lengthMessage = "[ERROR] 자동차의 이름의 글자수는 5를 초과할 수 없습니다.\n";
 	private static final String duplicationMessage = "[ERROR] 자동차의 이름은 중복을 허용하지 않습니다.\n";
-	private static final String positiveIntMessage = "[ERROR] 전체 게임 횟수는 양수여야 합니다.\n";
+	private static final String someIntMessage = "[ERROR] 전체 게임 횟수는 양수여야 합니다.\n";
+
+	public static ArrayList<String> parseInput(String input, String delimiter) {
+		ArrayList<String> parsedName = new ArrayList<>();
+		parsedName.addAll(Arrays.asList(input.split(delimiter)));
+		return parsedName;
+	}
 
 	public static void checkSpecialChar(String inputToken, char start, char end) throws IllegalArgumentException {
 		for (int index = 0; index < inputToken.length(); index++) {
@@ -36,7 +43,7 @@ public class Utils {
 
 	public static void checkOverSomeInt(String inputToken, int someInt) throws IllegalArgumentException {
 		if (Integer.valueOf(inputToken) <= someInt) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(someIntMessage);
 		}
 	}
 }
