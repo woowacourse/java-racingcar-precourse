2fd0aab4e551c67ff7df85a3652e204d55e83990
iamsseaa <homingsea@gmail.com>
2021-12-05 16:10:05 +0900

    feat: 차 이름 잘못 입력 시 반복하여 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,10 +7,17 @@ public class Application {
 
         User user = new User();
 
-        System.out.println("경주할 자동차 이름은 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
-        user.enterCarName();
-        user.toNameArray();
+        while (true) {
+            try {
+                System.out.println("경주할 자동차 이름은 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                user.enterCarName();
+                user.toNameArray();
+                CarNameException.validateCarName(user.getCarName());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
         System.out.println("시도할 회수는 몇회인가요?");
 
