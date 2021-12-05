f2427ddb52b9614f5f9742da7ff16d020e56f13c
yezyvibe <yeji0501@gmail.com>
2021-12-05 11:08:03 +0900

    feat(Validator): validate car names and attempt number
    - 5자를 초과하는 자동차 이름을 입력한 경우 IllegalArgumentException 발생 후 재입력 받기
    - 시도 횟수로 숫자가 아닌 문자가 들어온 경우 IllegalArgumentException 발생 후 재입력 받기

--- src/main/java/racingcar/utils/Validator.java
+++ src/main/java/racingcar/utils/Validator.java
@@ -7,24 +7,47 @@ import racingcar.view.OutputView;
 public class Validator {
 	private static final int CAR_NAME_MAX_LENGTH = 5;
 
-	public static boolean checkCarNameLength(List<String> carNameList) {
-		for (String carName : carNameList) {
-			if (carName.length() > CAR_NAME_MAX_LENGTH) {
-				OutputView.printInvalidCarName();
+	public static boolean checkValid(List<String> carNameList) {
+		for (String name : carNameList) {
+			if (!checkCarNameLength(name)) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public static boolean checkAttemptNumber(String tempInput) {
-		for (int i = 0; i < tempInput.length(); i++) {
-			if (!Character.isDigit(tempInput.charAt(i))) {
-				OutputView.printInvalidAttemptNumber();
+	public static boolean checkAttemptNumberValid(String attemptNumber) {
+		for (int i = 0; i < attemptNumber.length(); i++) {
+			if (!checkAttemptNumber(attemptNumber.charAt(i))) {
 				return false;
 			}
 		}
 		return true;
+	}
+
+
+	public static boolean checkCarNameLength(String carName) {
+		try {
+			if ((carName.length() <= CAR_NAME_MAX_LENGTH)) {
+				return true;
+			}
+			throw new IllegalArgumentException();
+		} catch (Exception e) {
+			OutputView.printInvalidCarName();
+			return false;
+		}
+	}
+
+	public static boolean checkAttemptNumber(char tempInput) {
+		try	{
+			if (Character.isDigit(tempInput)) {
+				return true;
+			}
+			throw new IllegalArgumentException();
+		} catch (Exception e) {
+			OutputView.printInvalidAttemptNumber();
+		}
+		return false;
 	}
 
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,10 @@
 package racingcar.view;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.domain.Car;
+import racingcar.utils.Message;
 import racingcar.utils.Validator;
 
 public class InputView {
@@ -13,33 +12,29 @@ public class InputView {
 		System.out.println(Message.INPUT_CAR_NAME.getMessage());
 	}
 
-	public static void inputCarName() {
-		String carNames = Console.readLine();
-		List<String> carNameList = Arrays.asList(carNames.split(","));
-		if (Validator.checkCarNameLength(carNameList)) {
-			// System.out.println(carNames);
-			createCarInstance(carNameList);
-
+	public static List<String> inputCarName() {
+		boolean isValidCarNameList;
+		while (true) {
+			List<String> carNameList = Arrays.asList(Console.readLine().split(","));
+			isValidCarNameList = Validator.checkValid(carNameList);
+			if (isValidCarNameList) {
+				return carNameList;
+			}
 		}
-	}
-
-	public static Car[] createCarInstance(List<String> carNameList) {
-		Car[] carName = new Car[carNameList.size()];
-		for (int i = 0; i < carNameList.size(); i++) {
-			carName[i] = new Car(carNameList.get(i));
-		}
-		return carName;
 	}
 
 	public static void printAttemptInputMessage() {
 		System.out.println(Message.INPUT_ATTEMPT_NUMBER.getMessage());
 	}
 
-	public static void inputAttemptNumber() {
-		String tempInput = Console.readLine();
-		if (Validator.checkAttemptNumber(tempInput)) {
-			int attemptNumber = Integer.parseInt(tempInput);
-			System.out.println(attemptNumber);
+	public static int inputAttemptNumber() {
+		boolean isValidAttemptNumber;
+		while (true) {
+			String tempInput = Console.readLine();
+			isValidAttemptNumber = Validator.checkAttemptNumberValid(tempInput);
+			if (isValidAttemptNumber) {
+				return Integer.parseInt(tempInput);
+			}
 		}
 	}
 }
