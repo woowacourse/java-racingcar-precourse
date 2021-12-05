530ac02687d1aa814006035091fe9471717ea889
bcc0830 <bcc0830@naver.com>
2021-12-05 16:50:51 +0900

    feat(Utils) : 특정 문자열의 중복이 있는지 알려주는 기능 추가

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,10 +1,13 @@
 package utils;
 
 import java.lang.IllegalArgumentException;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Utils {
 	private static final String specialCharMessage = "[ERROR] 자동차의 이름에는 특수문자 혹은 공백이 들어갈 수 없습니다.\n";
-	private static final String lengthMessage = "[ERROR] 자동차의 이름의 글자는 5글자를 초과할 수 없습니다.\n";
+	private static final String lengthMessage = "[ERROR] 자동차의 이름의 글자수는 5를 초과할 수 없습니다.\n";
+	private static final String duplicationMessage = "[ERROR] 자동차의 이름은 중복을 허용하지 않습니다.\n";
 
 	public static void checkSpecialChar(String inputToken, char start, char end) throws IllegalArgumentException {
 		for (int index = 0; index < inputToken.length(); index++) {
@@ -17,6 +20,16 @@ public class Utils {
 	public static void checkLength(String inputToken, int size) throws IllegalArgumentException {
 		if (inputToken.length() > size) {
 			throw new IllegalArgumentException(lengthMessage);
+		}
+	}
+
+	public static void checkDuplication(ArrayList<String> input) throws IllegalArgumentException {
+		HashSet<String> nameSet = new HashSet<>();
+		for (String inputToken : input) {
+			if (nameSet.contains(inputToken)) {
+				throw new IllegalArgumentException(duplicationMessage);
+			}
+			nameSet.add(inputToken);
 		}
 	}
 }
