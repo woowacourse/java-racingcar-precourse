8378469cc255a660223bbf98b2389550fd42330f
OzRagwort <ozragwort@gmail.com>
2021-12-05 15:36:19 +0900

    test(View): validate car names exception test

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,6 +1,9 @@
 package racingcar.view;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +12,13 @@ import racingcar.utils.CustomNsTest;
 
 public class InputViewTest extends CustomNsTest {
 	private static final String CORRECT_CAR_NAMES = "ozr,rag,wort";
+	private static final String INVALID_DUPLICATE_CAR_NAMES = "ozr,rag,ozr";
+	private static final String INVALID_LONG_LENGTH_CAR_NAMES = "ozr,ragwort";
+	private static final String INVALID_EMPTY_LENGTH_CAR_NAMES = "ozr,,wort";
 
-	@DisplayName("차의 이름을 받아오는 기능 테스트")
+	private static final String ERROR_MESSAGE = "[ERROR]";
+
+	@DisplayName("차의 이름을 받아오는 테스트")
 	@Test
 	public void readCarNamesTest() {
 		command(CORRECT_CAR_NAMES);
@@ -19,6 +27,39 @@ public class InputViewTest extends CustomNsTest {
 		for (int i = 0; i < testValues.length; i++) {
 			assertEquals(inputs[i], testValues[i]);
 		}
+	}
+
+	@DisplayName("자동차 이름 중복 입력 예외 테스트")
+	@Test
+	public void readCarNamesDuplicateExceptionTest() {
+		command(INVALID_DUPLICATE_CAR_NAMES);
+		try {
+			InputView.readCarNames();
+		} catch (NoSuchElementException ignored) {
+		}
+		assertThat(output()).contains(ERROR_MESSAGE);
+	}
+
+	@DisplayName("자동차 이름 길이 초과 예외 테스트")
+	@Test
+	public void readCarNamesLongLengthExceptionTest() {
+		command(INVALID_LONG_LENGTH_CAR_NAMES);
+		try {
+			InputView.readCarNames();
+		} catch (NoSuchElementException ignored) {
+		}
+		assertThat(output()).contains(ERROR_MESSAGE);
+	}
+
+	@DisplayName("자동차 이름 빈칸 입력 예외 테스트")
+	@Test
+	public void readCarNamesExceptionTest() {
+		command(INVALID_EMPTY_LENGTH_CAR_NAMES);
+		try {
+			InputView.readCarNames();
+		} catch (NoSuchElementException ignored) {
+		}
+		assertThat(output()).contains(ERROR_MESSAGE);
 	}
 
 	@Override
