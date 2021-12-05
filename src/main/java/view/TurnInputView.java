8fd52fb61631233ad51215815b78ef98318f4211
bcc0830 <bcc0830@naver.com>
2021-12-05 17:55:20 +0900

    feat(TurnInputView) : 사용자의 전체 게임 횟수 입력을 가져오는 기능 추가

--- src/main/java/view/TurnInputView.java
+++ src/main/java/view/TurnInputView.java
@@ -1,5 +1,10 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import utils.MissionUtils;
+
 enum TurnStringEnums {
 	TURN_REGEX("^[0-9]+$");
 	private final String value;
@@ -26,6 +31,21 @@ enum TurnIntEnums {
 	}
 }
 
+public class TurnInputView implements InputView{
 
-public class TurnInputView {
+	public String getInput(String rawInput) {
+		String turnInput = "";
+		while (turnInput.isEmpty()) {
+			try {
+				String nowInput = camp.nextstep.edu.missionutils.Console.readLine();
+				ArrayList<String> nowInputArray = new ArrayList<>(Arrays.asList(nowInput));
+				MissionUtils.checkRegex(nowInputArray, TurnStringEnums.TURN_REGEX.getValue());
+				MissionUtils.checkOverSomeInt(nowInput, TurnIntEnums.TURN_MIN.getValue());
+				turnInput = nowInput;
+			} catch (IllegalArgumentException error) {
+				System.out.print(error.getMessage());
+			}
+		}
+		return turnInput;
+	}
 }
