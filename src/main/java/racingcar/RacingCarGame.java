8c8eff3e104e469dc5f36264bde74bca4f04643e
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-05 23:07:35 +0900

    feat(RacingCarGame.java): play()안에서 검사된 user의 입력대로 makeCarList()부분 구현

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarGame {
@@ -9,10 +10,19 @@ public class RacingCarGame {
 
 	RacingCarGame() {
 		user = new User();
+		carList = new ArrayList<>();
 	}
 
 	public void Play() {
 		user.askNamesUntilCorrect();
 		user.askNumberOfTimesUntilCorrect();
+		makeCarList();
+	}
+
+	private void makeCarList() {
+		for (String name : user.getNames()) {
+			Car car = new Car(name);
+			carList.add(car);
+		}
 	}
 }
