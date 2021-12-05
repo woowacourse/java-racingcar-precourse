589223cdcced83240637c0d027b7387ba9a8a92f
summerlunaa <rhgus1085@gmail.com>
2021-12-06 03:16:24 +0900

    feat(CarList): cars의 모든 자동차에 대해 전진 여부를 판단하여 전진시키는 moveCars 메소드 구현

--- src/main/java/racingcar/CarList.java
+++ src/main/java/racingcar/CarList.java
@@ -2,12 +2,21 @@ package racingcar;
 
 import java.util.ArrayList;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class CarList {
-	private ArrayList<Car> cars = new ArrayList<>();
+	private final ArrayList<Car> cars = new ArrayList<>();
 
 	public CarList(String [] carNames){
 		for (String carName : carNames) {
-			cars.add(new Car(carName));
+			this.cars.add(new Car(carName));
+		}
+	}
+
+	private void moveCars() {
+		for (Car car : this.cars) {
+			if (Randoms.pickNumberInRange(0, 9) >= 4)
+				car.moveForward();
 		}
 	}
 }
