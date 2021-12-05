0632a3201ae69fc5e655323c43b21e68fc6ed170
OzRagwort <ozragwort@gmail.com>
2021-12-05 17:40:18 +0900

    fix(OutputView): add line separator in printCarState

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,7 +9,7 @@ public class OutputView {
 	private static final String READ_CAR_NAMES_MASSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String READ_RACING_ROUND_MASSAGE = "시도할 회수는 몇회인가요?";
 	private static final String START_ROUND_MASSAGE = "실행 결과";
-	private static final String CAR_STATE = "%s : %s";
+	private static final String CAR_STATE = "%s : %s" + System.lineSeparator();
 	private static final String WINNERS_FORM = "최종 우승자 : %s";
 
 	public static void printReadCarNamesMessage() {
