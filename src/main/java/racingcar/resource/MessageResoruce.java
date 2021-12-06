4c4bd4b127c6f971821d914ea78e9f88f2a52b03
Kim Seunglae <obey1342@gmail.com>
2021-12-07 02:03:51 +0900

    refactor: private 생성자 추가

--- src/main/java/racingcar/resource/MessageResoruce.java
+++ src/main/java/racingcar/resource/MessageResoruce.java
@@ -1,6 +1,8 @@
 package racingcar.resource;
 
 public class MessageResoruce {
+	private MessageResoruce() {}
+	
 	public static final String INPUT_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
 
