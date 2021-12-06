a188aecc73aebcec32dbb828bbfcfee7dd647ae1
sejung-hong <sejungh@naver.com>
2021-12-07 00:04:58 +0900

    feat : 잘못된 값을 입력할 경우 에러메시지 출력 기능

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
-    //자동차 이름 입력
+    // 자동차 이름 입력
     public String[] InputCarName(){
         String carsString = Console.readLine();
         String[] cars = carsString.split(",");
@@ -23,6 +23,29 @@ public class User {
             if(car.length() > 5) {
                 throw new IllegalArgumentException("[ERROR] 이름은 5자 이하만 가능하다.");
             }
+        }
+    }
+
+    // 시도할 횟수 입력
+    public Integer InputPlayNum(){
+        String playNumString = Console.readLine();
+        try{
+            IsValidNum(playNumString);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return InputPlayNum();
+        }
+        return Integer.parseInt(playNumString);
+    }
+
+    private void IsValidNum(String num){
+        for(int i=0; i<num.length(); i++){
+            if(!Character.isDigit(num.charAt(i))){
+                throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 한다.");
+            }
+        }
+        if(Integer.parseInt(num) < 1){
+            throw new IllegalArgumentException("[ERROR] 숫자는 1보다 커야한다.");
         }
     }
 }
