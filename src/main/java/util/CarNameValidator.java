28cf13f3c68142beed820bff8d3bc6b753b02e68
SuyeonChoi <tndus970120@gmail.com>
2021-12-07 04:55:56 +0900

    feat(CarNameValidator): validation for duplicate car names

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CarNameValidator {
 	private static final int MIN_CAR_NAME_LENGTH = 1;
 	private static final int MAX_CAR_NAME_LENGTH = 5;
@@ -9,13 +13,23 @@ public class CarNameValidator {
 
 	public static void validate(String[] carNames) {
 		validateCarNameLength(carNames);
+		validateDuplicateCarName(carNames);
 	}
 
-	public static void validateCarNameLength(String[] carNames) {
+	private static void validateCarNameLength(String[] carNames) {
 		for (String car : carNames) {
 			if (car.length() < MIN_CAR_NAME_LENGTH || car.length() > MAX_CAR_NAME_LENGTH) {
 				throw new IllegalArgumentException();
 			}
+		}
+	}
+
+	private static void validateDuplicateCarName(String[] carNames) {
+		Set<String> uniqueCarNames = new HashSet<>();
+		Collections.addAll(uniqueCarNames, carNames);
+
+		if (uniqueCarNames.size() != carNames.length) {
+			throw new IllegalArgumentException();
 		}
 	}
 }
