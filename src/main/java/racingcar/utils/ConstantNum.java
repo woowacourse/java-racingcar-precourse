24de5b2e24f4ce5fb87c90e638181b9e0c739628
kanamycine <jinyeong5320@gmail.com>
2021-12-06 16:53:01 +0900

    feat(ConstatntNum) : 매직넘버 치환 상수 클래스 생성 및 구현

---
+++ src/main/java/racingcar/utils/ConstantNum.java
@@ -0,0 +1,7 @@
+package racingcar.utils;
+
+public class ConstantNum {
+	public static final int RANDOM_NUM_MIN = 0;
+	public static final int RANDOM_NUM_MAX = 9;
+	public static final int  PROCEED_NUM_MIN= 4;
+}
