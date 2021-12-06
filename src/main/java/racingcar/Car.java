2d5c6eb76ddd60008251a9778106326ebe10dd97
zzambbang <zzambbang@gmail.com>
2021-12-06 16:59:23 +0900

    feat(Car): add car domain

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,33 @@
 package racingcar;
 
+import racingcar.strategy.MoveStrategy;
+import racingcar.strategy.RandomNumberStrategy;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	MoveStrategy moveStrategy = new RandomNumberStrategy();
 
-    // 추가 기능 구현
+	public Car(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getPosition() {
+		return this.position;
+	}
+
+	public void move() {
+		this.position++;
+	}
+
+	public void playRound() {
+		if (moveStrategy.isMoveable()) {
+			this.move();
+		}
+	}
 }
