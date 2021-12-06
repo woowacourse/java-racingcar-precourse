3a798421ce61b1f93b445d54d8504ec8724c98a8
SuyeonChoi <tndus970120@gmail.com>
2021-12-07 04:49:27 +0900

    feat(CarNameValidator): handle exception when space is entered as name - fix incorrect operand

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -1,18 +1,19 @@
 package util;
 
 public class CarNameValidator {
+	private static final int MIN_CAR_NAME_LENGTH = 1;
 	private static final int MAX_CAR_NAME_LENGTH = 5;
 
 	private CarNameValidator() {
 	}
 
 	public static void validate(String[] carNames) {
-			validateCarNameLength(carNames);
+		validateCarNameLength(carNames);
 	}
 
 	public static void validateCarNameLength(String[] carNames) {
 		for (String car : carNames) {
-			if (car.length() >= MAX_CAR_NAME_LENGTH) {
+			if (car.length() < MIN_CAR_NAME_LENGTH || car.length() > MAX_CAR_NAME_LENGTH) {
 				throw new IllegalArgumentException();
 			}
 		}
