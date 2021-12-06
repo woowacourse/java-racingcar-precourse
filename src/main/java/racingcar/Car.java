b133f14df63ca6c9733ecffa64fa1294b0e78804
SuyeonChoi <tndus970120@gmail.com>
2021-12-06 18:56:27 +0900

    feat(Car): add random value generation function

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private static final int MIN = 0;
+	private static final int MAX = 9;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	private final String name;
+	private int position = 0;
 
-    // 추가 기능 구현
+	public Car(String name) {
+		this.name = name;
+	}
+
+	public int generateRandomValue() {
+		return Randoms.pickNumberInRange(MIN, MAX);
+	}
 }
