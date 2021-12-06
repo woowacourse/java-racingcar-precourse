2a88ffa37efeb206292d5abd6c4e422a66323c2c
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-07 03:48:13 +0900

    test: 라운드 수 검증 테스트 추가

---
+++ src/test/java/racingcar/domain/RacingCarGameTest.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RacingCarGameTest {
+	private static Cars cars;
+
+	@BeforeAll
+	static void setupCars() {
+		cars = Cars.generateCars("바흐,모차르트,쇼팽");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"0", "다섯", "04", "열3"})
+	void validateRoundNumberTest(String roundNumber) {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			RacingCarGame racingCarGame = RacingCarGame.generateGame(roundNumber, cars);
+		});
+	}
+
+}
