4854ddf423e5846aecf90872e60881cb913d2a3b
koo <gudonghee2000@gmail.com>
2021-12-06 13:55:56 +0900

    feat : 자동차 이름 입력 요구를 출력하는 기능 구현

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import view.Display;
+
+public class GameController {
+
+	private GameController() {
+	}
+
+	public static void runGame() {
+		Display.pleaseInputRacingCarNames();
+	}
+}
---
+++ src/main/java/view/Display.java
@@ -0,0 +1,11 @@
+package view;
+
+public class Display {
+
+	private Display() {
+	}
+
+	public static void pleaseInputRacingCarNames() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+	}
+}
