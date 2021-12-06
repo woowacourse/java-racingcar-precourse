50a8797c3fbdd29e86c940bc10917c8bb560d21d
hyunrrr <whgusrms96@gmail.com>
2021-12-06 22:09:51 +0900

    feat: 질문을 사용자에게 출력하고 답을 입력받는 기능 추가

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -5,4 +5,9 @@ public class Player {
 	protected void getMessage(String message) {
 		System.out.print(message);
 	}
+
+	protected String askQuestionReturnAnswer(String message) {
+		getMessage(message);
+		return camp.nextstep.edu.missionutils.Console.readLine();
+	}
 }
