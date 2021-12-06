5659b3e172abe3797f3c334bc979be6dc9d1186d
soominsohn <soomin.sohn.dev@gmail.com>
2021-12-06 20:19:00 +0900

    feat: add move cars function in Car and RacingGame class - Car add moveOnestep()
    - RacingGame
    add runGame()
    add generateRandomNumber()
    add moveCars()
    add isMovable()

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingGame {
 
 	public User user;
@@ -10,6 +12,7 @@ public class RacingGame {
 	public void startGame() {
 		makeCarList();
 		Integer playRound = user.getGameNumber();
+		runGame(playRound);
 	}
 
 	public void makeCarList() {
@@ -18,6 +21,33 @@ public class RacingGame {
 			cars.add(new Car(carName));
 		}
 
+	}
+
+	public void runGame(int playRound) {
+		for (int i = 0; i < playRound; i++) {
+			moveCars();
+		}
+	}
+
+	public void moveCars() {
+
+		for (Car car : cars) {
+			int randomNumber = generateRandomNumber();
+			if (isMovable(randomNumber)) {
+				car.moveOneStep();
+			}
+		}
+	}
+
+	public int generateRandomNumber() {
+		return Randoms.pickNumberInRange(1, 9);
+	}
+
+	public boolean isMovable(int randomNumber) {
+		if (randomNumber < 4) {
+			return false;
+		}
+		return true;
 	}
 
 }
