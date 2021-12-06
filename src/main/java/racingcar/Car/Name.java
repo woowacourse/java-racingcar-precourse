2299e32c1525318107b4e415cb75a673f4715317
KIM <km0192@naver.com>
2021-12-06 16:08:59 +0900

    feat(Name) : 초기화, 에러체크, getter생성 - toString 오버라이드

---
+++ src/main/java/racingcar/Car/Name.java
@@ -0,0 +1,25 @@
+package racingcar.Car;
+
+public class Name {
+	private final String name;
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	Name(String name) {
+		if (isLengthValid(name)) {
+			this.name = name;
+		}
+		throw new IllegalArgumentException();
+	}
+
+	private boolean isLengthValid(String string) {
+		final int MIN_LENGTH = 1;
+		final int MAX_LENGTH = 5;
+		int len = string.length();
+
+		return MIN_LENGTH <= len && len <= MAX_LENGTH;
+	}
+}
