4555ae83612a34bc2b738bb609aa3df8349b9f88
LEE EUN JIN <lej4545@naver.com>
2021-12-07 07:20:01 +0900

    feat(InputValidator): 에러 처리 기능 추가 구현
    - 공백인 이름을 입력할 경우
     - validateEmptyName(): 공백인 이름이 존재할 경우 True 반환
    - 이름을 중복해서 입력할 경우
     - validateDuplicateName: 중복된 이름이 있을 경우 True 반환

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -4,13 +4,20 @@ import java.util.Arrays;
 
 public class InputValidator {
     private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "[ERROR] 5자 이하만의 이름을 입력해주세요.";
+    private static final String EMPTY_NAME_EXCEPTION_MESSAGE = "[ERROR] 공백을 제외한 이름을 입력해주세요.";
+    private static final String DUPLICATE_NAME_EXCEPTION_MESSAGE = "[ERROR] 중복되지 않는 서로 다른 이름을 입력해주세요.";
     private static final String NUMBER_EXCEPTION_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
     private static final int MAX_LENGTH_CAR_NAME = 5;
+    private static final int MIN_LENGTH_CAR_NAME = 0;
 
     protected static final boolean validateCarName(String[] carNameArray) {
         try {
-            if (!validateNameSize(carNameArray)) {
+            if (validateNameSize(carNameArray)) {
                 throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
+            } else if (validateEmptyName(carNameArray)) {
+                throw new IllegalArgumentException(EMPTY_NAME_EXCEPTION_MESSAGE);
+            } else if (validateDuplicateName(carNameArray)) {
+                throw new IllegalArgumentException(DUPLICATE_NAME_EXCEPTION_MESSAGE);
             }
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -32,8 +39,19 @@ public class InputValidator {
     }
 
     private static final boolean validateNameSize(String[] carNameArray) {
-        return ! Arrays.stream(carNameArray)
-                .allMatch(carName -> carName.length() > MAX_LENGTH_CAR_NAME);
+        return Arrays.stream(carNameArray)
+                .anyMatch(carName -> carName.length() > MAX_LENGTH_CAR_NAME);
+    }
+
+    private static final boolean validateEmptyName(String[] carNameArray) {
+        return !Arrays.stream(carNameArray)
+                .allMatch(carName -> carName.length() > MIN_LENGTH_CAR_NAME);
+    }
+
+    private static final boolean validateDuplicateName(String[] carNameArray) {
+        return carNameArray.length != Arrays.stream(carNameArray)
+                .distinct()
+                .count();
     }
 
     private static final boolean validateNumber(String numberOfAttempts) {
