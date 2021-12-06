bf4d62ecf31f80f0ac453ab912bb73caadc65979
CptBluebear <cptbluebear@naver.com>
2021-12-06 20:17:15 +0900

    refactor: 예외처리 및 에러 메세지 출력 방식 변경

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,9 +1,9 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
+	import java.util.ArrayList;
+	import java.util.List;
 
-import camp.nextstep.edu.missionutils.Console;
+	import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
 
@@ -11,25 +11,20 @@ public class InputHandler {
 		String[] racingCarNames = parseRacingCarNames(Console.readLine());
 		List<String> results = new ArrayList<String>();
 		for (String racingCarName : racingCarNames) {
-			if (racingCarName.length() > GameData.CARNAME_LIMIT) {
-				throw new IllegalArgumentException(racingCarName);
-			}
 			results.add(racingCarName);
 		}
+		InputValidator.validateNameLength(results);
+		InputValidator.validateNameIsDuplicated(results);
+		InputValidator.validateNameContatainsEmptyString(results);
 		return results;
 	}
 
 	public int readCount() {
 		String input = Console.readLine();
-		try {
-			return parseCount(input);
-		} catch (NumberFormatException nfe) {
-			throw new IllegalArgumentException(input, nfe);
-		}
-	}
-
-	private int parseCount(String count) {
-		return Integer.parseInt(count);
+		InputValidator.validateTurnCountIsInteger(input);
+		int turnCount = Integer.parseInt(input);
+		InputValidator.validateTurnCountNotTooSmall(turnCount);
+		return turnCount;
 	}
 
 	private String[] parseRacingCarNames(String carNames) {
