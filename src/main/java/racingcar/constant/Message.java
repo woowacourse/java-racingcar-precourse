3517f375b68ef29e4b08f513d6b4fd5a8002b596
bomi <bonita9931@gmail.com>
2021-12-06 22:47:08 +0900

    feat : 사용할 상수 메세지 정의

---
+++ src/main/java/racingcar/constant/Message.java
@@ -0,0 +1,10 @@
+package racingcar.constant;
+
+public class Message {
+	public static final String CAR_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
+	public static final String EXECUTION_RESULT = "실행 결과";
+	public static final String FINAL_WINNER = "최종 우승자 : %s";
+	public static final String RESULT_OF_STEP = "%s : %s";
+	public static final String BLOCK = "-";
+}
