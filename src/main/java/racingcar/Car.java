6b6e1d8c883d47ced006b3c0f342bac70dcead69
kanamycine <jinyeong5320@gmail.com>
2021-12-06 16:08:18 +0900

    delete(All) : 두 번째 구현을 위해

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -10,27 +10,4 @@ public class Car {
 	public Car(String carName) {
 		this.carName = carName;
 	}
-
-	public void playTurn() {
-		RandomGenerator randomGenerator = new RandomGenerator();
-		if (randomGenerator.getRandomNum() >= Constant.MOVE_VALUE) {
-			this.position++;
-		}
-	}
-
-	public void printPosition() {
-		System.out.print(this.carName + " : ");
-		for (int i = 0; i < position; i++) {
-			System.out.print("-");
-		}
-		System.out.println();
-	}
-
-	public String getCarName() {
-		return this.carName;
-	}
-
-	public int getPosition() {
-		return this.position;
-	}
 }
