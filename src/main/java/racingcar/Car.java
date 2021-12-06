c38545421e5f27b6d5c8922846b735583b2885c9
limm-jk <junkyu.lim.dev@gmail.com>
2021-12-06 20:49:36 +0900

    feat : Car 멤버변수 getter 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,13 +3,19 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-
-
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     public void move() {
