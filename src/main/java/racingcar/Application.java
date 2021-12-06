09c7cdbe6c06db5f4758e9ac24138b7bd9f051a0
qbang <vhfhddl0101@naver.com>
2021-12-06 23:42:39 +0900

    refactor: Game 클래스 삭제 - Application 클래스와 같은 역할을 수행하여 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,11 +3,16 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         User user = new User();
-        Game game = new Game();
+        Cars cars = new Cars();
 
         String[] nameList = user.inputCarName();
         int roundNumber = user.inputRoundNumber();
 
-        game.startGame(nameList, roundNumber);
+        cars.createCar(nameList);
+        for (int i = 0; i < roundNumber; i++) {
+            cars.moveCar();
+            cars.printResult(i);
+        }
+        cars.getWinner();
     }
 }
