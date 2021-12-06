c0ca2a4ced72365be42b5db05a59268b6600532f
CptBluebear <cptbluebear@naver.com>
2021-12-06 19:41:01 +0900

    refactor: 오류 상황에 따른 오류 메세지 다각화

--- src/main/java/racingcar/GameData.java
+++ src/main/java/racingcar/GameData.java
@@ -11,7 +11,11 @@ public class GameData {
 	public static final String CARNAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String TRY_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 	public static final String INPUT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
-	public static final String CARNAME_LIMIT_ERROR_MESSAGE = "[ERROR] 자동차 이름은 %d 이하여야 한다.";
+	public static final String CARNAME_LIMIT_ERROR_MESSAGE = String.format("[ERROR] 자동차 이름은 %d 이하여야 한다.",
+		CARNAME_LIMIT);
+	public static final String CARNAME_DUPLICATED_ERROR_MESSAGE = "[ERROR] 자동차 이름은 중복되서는 안된다.";
+	public static final String CARNAME_EMPTY_ERROR_MESSAGE = "[ERROR] 자동차 이름엔 공백이 포함되서는 안된다.";
+	public static final String TURNCOUNT_TOO_SMALL_ERROR_MESSAGE = "[ERROR] 시도 횟수가 너무 작습니다.";
 	public static final String RESULT_MESSAGE = "\n실행결과";
 	public static final String RESULT_FORMAT = "%s : %s\n";
 
