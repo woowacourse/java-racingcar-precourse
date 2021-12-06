01fe0528656f7dbde80a531a4ed9e5ae2441cb8b
LEE EUN JIN <lej4545@naver.com>
2021-12-07 01:07:58 +0900

    feat(Car, RacingCar): 경주 실행 기능 구현
    - Car
     - generateRandomNum(): 0~9 사이의 랜덤 값 생성
     - moveCar(): 랜덤 값이 4이상일 경우 1보 전진
     - makeTrace(): "-"로 위치(흔적) 출력 기능
     - printCarPosition(): 각 자동차 별 이름과 위치 출력 기능
    - RacingCar
     - createCarArray(): Car의 클래스 인스턴스를 담은 CarArray 생성
     - gameStart(): 횟수 만큼 반복 실행

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,42 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
+
+    private static final int CONDITIONS_TO_MOVE_FORWARD = 4;
+    private static final Character CAR_TRACE = '-';
 
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    private int generateRandomNum() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        return randomNum;
+    }
+
+    protected void moveCar() {
+        if (generateRandomNum() >= CONDITIONS_TO_MOVE_FORWARD) {
+            position++;
+        }
+    }
+
+    protected void printCarPosition() {
+        String trace = makeTrace();
+        System.out.println(name + " : " + trace);
+    }
+
+    private String makeTrace() {
+        if (position == 0) {
+            return " ";
+        }
+        String trace = String.format("%" + Integer.toString(position) + "s", " ").replace(' ', CAR_TRACE);
+        return trace;
+    }
+
+
+
 }
