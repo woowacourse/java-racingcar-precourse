76fe04682c8baf7c4392a0f567aae2ca9ac940b8
usuyn <usuyn000@gmail.com>
2021-12-06 22:22:38 +0900

    feat(ErrorText): define error text
    Error messages when exception occurs.

---
+++ src/main/java/communicate/ErrorText.java
@@ -0,0 +1,10 @@
+package communicate;
+
+public class ErrorText {
+    public static final String ERROR_SEPARATOR_COMMA = "[ERROR] 각 자동차의 이름은 쉼표(,)로 구분되어야 합니다.";
+    public static final String ERROR_CAR_NUMBER = "[ERROR] 2개 이상의 자동차 이름이 입력되어야 합니다.";
+    public static final String ERROR_NAME_LENGTH = "[ERROR] 각 자동차 이름은 1자 이상, 5자 이하여야 합니다.";
+    public static final String ERROR_NAME_OVERLAP = "[ERROR] 입력된 자동차 이름 중 중복이 존재합니다.";
+    public static final String ERROR_ATTEMPT_NOT_NUMBER = "[ERROR] 시도 횟수는 숫자만 입력되어야 합니다.";
+    public static final String ERROR_ATTEMPT_NUMBER_RANGE = "[ERROR] 시도 횟수는 1 이상이어야 합니다.";
+}
