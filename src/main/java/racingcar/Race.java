dc0fd759ed3735a4a76cb21689270960faba666b
limm-jk <junkyu.lim.dev@gmail.com>
2021-12-06 22:00:00 +0900

    feat : input valid 체크 추가 및 valid하지 않은 input이 들어올 시 다시 입력을 받는 기능 추가
    - 테스트 통과 확인

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -5,34 +5,46 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 
 public class Race {
+    private final InputValidator inputValidator = new InputValidator();
+
     public void play() {
         List<Car> cars = new ArrayList<>();
 
-        for (String name : getRacePlayerNames()) {
+        for (String name : getCarNames()) {
             cars.add(new Car(name));
         }
 
-        int raceCount = getRaceCount();
+        int raceCount = getTryCount();
 
         Game game = new Game(cars, raceCount);
 
         game.play();
     }
 
-    private String[] getRacePlayerNames() {
+    private String[] getCarNames() {
         String input = Console.readLine();
         String[] names = input.split(",");
+
+        try {
+            inputValidator.validateCarNamesInput(names);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getCarNames();
+        }
 
         return names;
     }
 
-    private int getRaceCount() {
+    private int getTryCount() {
         String input = Console.readLine();
 
         try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            inputValidator.validateTryCount(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getTryCount();
         }
+
+        return Integer.parseInt(input);
     }
 }
