5398b6a7a22d8d1abf13282adce5fbaee1b36948
devHudi <devhudi@gmail.com>
2021-12-06 17:05:51 +0900

    refactor: 매직 넘버를 상수로 분리

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,6 +3,10 @@ package domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+	private static final int MINIMUM_RANDOM_NUMBER = 0;
+	private static final int MAXIMUM_RANDOM_NUMBER = 9;
+	private static final int MOVING_STANDARD_NUMBER = 4;
+
 	private final Name name;
 	private Position position = new Position();
 
@@ -29,7 +33,7 @@ public class Car {
 	}
 
 	private boolean willMove() {
-		int randomNumber = Randoms.pickNumberInRange(0, 9);
-		return randomNumber >= 4;
+		int randomNumber = Randoms.pickNumberInRange(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
+		return randomNumber >= MOVING_STANDARD_NUMBER;
 	}
 }
