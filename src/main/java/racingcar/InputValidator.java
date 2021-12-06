c0d9411fc017f3261114a53146beee74b37a3eb2
CptBluebear <cptbluebear@naver.com>
2021-12-06 19:48:10 +0900

    feat(InputValidator.java): 사용자 입력값 검증 수행 클래스 추가

---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,52 @@
+package racingcar;
+
+import java.util.Collections;
+import java.util.List;
+
+import racingcar.exception.NameContainsEmptyStringException;
+import racingcar.exception.NameDuplicatedException;
+import racingcar.exception.NameLengthException;
+import racingcar.exception.TurnCountNotIntegerException;
+import racingcar.exception.TurnCountTooSmallException;
+
+public class InputValidator {
+
+	public static void validateNameLength(List<String> nameList) {
+		for (String name : nameList) {
+			if (name.length() > GameData.CARNAME_LIMIT) {
+				throw new NameLengthException(GameData.CARNAME_LIMIT_ERROR_MESSAGE);
+			}
+		}
+	}
+
+	public static void validateNameIsDuplicated(List<String> nameList) {
+		for (String name : nameList) {
+			if (Collections.frequency(nameList, name) != 1) {
+				throw new NameDuplicatedException(GameData.CARNAME_DUPLICATED_ERROR_MESSAGE);
+			}
+		}
+	}
+
+	public static void validateNameContatainsEmptyString(List<String> nameList) {
+		String pattern = ".*\\s.*";
+		for (String name : nameList) {
+			if (name.isEmpty() || name.matches(pattern)) {
+				throw new NameContainsEmptyStringException(GameData.CARNAME_EMPTY_ERROR_MESSAGE);
+			}
+		}
+	}
+
+	public static void validateTurnCountIsInteger(String turnCount) {
+		try {
+			Integer.parseInt(turnCount);
+		} catch (NumberFormatException nfe) {
+			throw new TurnCountNotIntegerException(GameData.INPUT_ERROR_MESSAGE, nfe);
+		}
+	}
+
+	public static void validateTurnCountNotTooSmall(int turnCount) {
+		if (turnCount <= 0) {
+			throw new TurnCountTooSmallException(GameData.TURNCOUNT_TOO_SMALL_ERROR_MESSAGE);
+		}
+	}
+}
