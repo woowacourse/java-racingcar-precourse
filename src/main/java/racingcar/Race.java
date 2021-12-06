e9080ccc4a82abbec9f2c81dd780ab3b4eb07978
limm-jk <junkyu.lim.dev@gmail.com>
2021-12-06 19:47:55 +0900

    feat : 자동차 이름을 입력하고 파싱할 수  있는 기능 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Race {
     public void play() {
 
+    }
+
+    private String[] getRacePlayerNames() {
+        String input = Console.readLine();
+        String[] names = input.split(", ");
+
+        return names;
     }
 }
