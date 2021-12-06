8d45b24ad9a980dc8f1580666b43a4c5db829d98
monk <2012.monk@gmail.com>
2021-12-07 01:30:09 +0900

    refactor: 예외 던지는 방식 변경

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -2,13 +2,14 @@ package racingcar.utils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class InputValidator {
 
     private static final String ERROR_PREFIX = "[ERROR]";
-    private static final String ERR_INVALID_NAMES = ERROR_PREFIX + "잘못된 형식의 입력입니다!";
+    private static final String ERR_INVALID_FORMAT = ERROR_PREFIX + "잘못된 형식의 입력입니다!";
     private static final String ERR_INVALID_NUMBER = ERROR_PREFIX + "숫자만 입력해주세요.";
+    private static final String ERR_DUPLICATED_NAME = ERROR_PREFIX + "중복된 이름이 있습니다.";
+    private static final String ERR_INVALID_NAMES = ERROR_PREFIX + "이름은 1자 이상 5자 이하로 입력해주세요.";
     private static final int NAME_MAX_LENGTH = 5;
     private static final char DELIMITER_NAME_CHAR = ',';
     private static final String NUMERIC_REGEX = "[0-9]";
@@ -16,40 +17,36 @@ public class InputValidator {
     public static void validateCarNames(String input)
         throws IllegalArgumentException {
         List<String> names = Arrays.asList(input.split(String.valueOf(DELIMITER_NAME_CHAR)));
-        if (!isValidFormat(input, names)
-            || !isValidNames(names)
-            || !isNonDuplicatedNames(names)) {
-            throw new IllegalArgumentException(ERR_INVALID_NAMES);
-        }
+        validateFormat(input, names);
+        validateNames(names);
+        validateDuplicate(names);
     }
 
     public static void validatePlayCount(String playCount)
         throws IllegalArgumentException {
-        if (!isNumeric(playCount)) {
+        if (!playCount.matches(NUMERIC_REGEX)) {
             throw new IllegalArgumentException(ERR_INVALID_NUMBER);
         }
     }
 
-    private static boolean isNumeric(String number) {
-        Pattern pattern = Pattern.compile(NUMERIC_REGEX);
-        return pattern.matcher(number).matches();
+    private static void validateNames(List<String> names) {
+        if (names.stream().allMatch(name ->
+            !name.isEmpty() && name.length() <= NAME_MAX_LENGTH)) {
+            throw new IllegalArgumentException(ERR_INVALID_NAMES);
+        }
     }
 
-    private static boolean isValidNames(List<String> names) {
-        return names.stream().allMatch(InputValidator::isValidName);
-    }
-
-    private static boolean isValidFormat(String name, List<String> names) {
-        return name.chars()
+    private static void validateFormat(String name, List<String> names) {
+        if (name.chars()
             .filter(c -> c == DELIMITER_NAME_CHAR)
-            .count() + 1 == names.size();
+            .count() + 1 == names.size()) {
+            throw new IllegalArgumentException(ERR_INVALID_FORMAT);
+        }
     }
 
-    private static boolean isValidName(String name) {
-        return !name.isEmpty() && name.length() <= NAME_MAX_LENGTH;
-    }
-
-    private static boolean isNonDuplicatedNames(List<String> names) {
-        return names.size() == names.stream().distinct().count();
+    private static void validateDuplicate(List<String> names) {
+        if (names.size() != names.stream().distinct().count()) {
+            throw new IllegalArgumentException(ERR_DUPLICATED_NAME);
+        }
     }
 }
