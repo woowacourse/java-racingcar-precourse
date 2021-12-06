1ffec6bec5655b3b3fbfc3c4376fd01071305266
GYUTAEKIM <kkt219a@gmail.com>
2021-12-07 00:22:27 +0900

    feat: 자동차들의 생성 및 검증 로직 구현
    - 자동차들을 관리하는 그룹의 생성과 검증 로직을 구현

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,6 +1,9 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 
@@ -9,6 +12,30 @@ public class Cars {
 
 	private Cars(List<Car> cars) {
 		this.cars = cars;
+	}
+
+	public static Cars of(List<String> names){
+		validateSize(names);
+		validateDuplication(names);
+		List<Car> cars = new ArrayList<>();
+		for (String name : names) {
+			Car car = new Car(name);
+			cars.add(car);
+		}
+		return new Cars(cars);
+	}
+
+	private static void validateSize(List<String> names){
+		if(names==null||names.isEmpty()){
+			throw new IllegalArgumentException("[ERROR] 자동차의 이름을 입력해주세요.");
+		}
+	}
+
+	private static void validateDuplication(List<String> names){
+		Set<String> carSet = new HashSet<>(names);
+		if(carSet.size()!=names.size()){
+			throw new IllegalArgumentException("[ERROR] 자동차의 이름은 중복될 수 없습니다.");
+		}
 	}
 
 	@Override
