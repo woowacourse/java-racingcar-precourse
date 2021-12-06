98d25e0ad1085717eb4dd35961dab027dd5592a2
GYUTAEKIM <kkt219a@gmail.com>
2021-12-06 18:23:34 +0900

    feat: 자동차 생성 시 이름 검증 기능 구현
    - 자동차의 이름이 5자를 초과했는지 검증
    - 자동차의 이름이 비어있는지 검증

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,26 @@
 package racingcar;
 
 public class Car {
+    private static final int NAME_MAX_LENGTH = 5;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateNameEmpty(name);
+        validateNameLength(name);
         this.name = name;
     }
 
-    // 추가 기능 구현
+    private void validateNameLength(String name){
+        if(name.length()>NAME_MAX_LENGTH){
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름이 5자를 초과했습니다.");
+        }
+    }
+
+    private void validateNameEmpty(String name){
+        if(name==null||name.isEmpty()){
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름이 비어있습니다.");
+        }
+    }
 }
