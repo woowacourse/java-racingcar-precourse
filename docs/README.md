e01990f1ca59c9ae156f24a9c637cb889137675e
PanghoChao <Sobrightlf@naver.com>
2021-12-06 21:33:27 +0900

    feat : Car.java 객체에 필요한 5가지 메소드 구현

--- docs/README.md
+++ docs/README.md
@@ -6,7 +6,7 @@ racingcar
  ├─ Appliction.java
  ┃    └─ Car.java
  ┃	   └─ Randoms_method
- ┃    └─ input.java
+ ┃    └─ Input.java
  ┃           └─ console_method
  └─ ApplictionTest
 ```
@@ -14,10 +14,17 @@ racingcar
 ##✔️구현한 기능목록 
 
 ### 사용자 입력 
-#### input.java 
+#### Input.java 
  - 자동차 이름을 한줄로 입력 받을 것 -> carNameArray[]
      - 쉼표로 자동차 이름을 구별한다.
      - 띄어쓰기 제거해준다.trim
   
  - 시도할 횟수 int로 입력 받기
 
+### 자동차 객체 구현 
+####Car.java
+ - 0~9무작위값 하나 출력하는 함수
+ - 4이상이면 position을 +1 증가시킨다
+ - 현재 position을 visual로 보여주는 함수
+ - 현재 position의 값을 출력
+ - 인스턴스에서 차이름을 출력
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,42 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	static final int startNum = 0;
+	static final int endNum = 9;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	private final String name;
+	private int position = 0;
 
-    // 추가 기능 구현
+	public Car(String name) {
+		this.name = name;
+	}
+
+	int randomNumber() {
+		int rand = Randoms.pickNumberInRange(startNum, endNum);
+		return rand;
+	}
+
+	void positionMove(int n) {
+		if (n >= 4) {
+			this.position += 1;
+		}
+	}
+
+	void visual() {
+		System.out.print(this.name + " : ");
+		for (int i = 0; i < this.position; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+
+	int getPosition() {
+		return this.position;
+	}
+
+	String getName() {
+		return this.name;
+	}
 }
