86d7a817fb6fe40f4c1c61c27ea151f2a1032621
LEE EUN JIN <lej4545@naver.com>
2021-12-07 07:33:04 +0900

    feat(InputValidator): 예외 처리 기능 추가 구현
    README.md 수정
     - 자동차는 최소 2대 이상이어야 함
      - validateCarCountRule(): 입력한 자동차가 2대 미만일 경우 True 반환

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -7,6 +7,8 @@ public class InputValidator {
     private static final String EMPTY_NAME_EXCEPTION_MESSAGE = "[ERROR] 공백을 제외한 이름을 입력해주세요.";
     private static final String DUPLICATE_NAME_EXCEPTION_MESSAGE = "[ERROR] 중복되지 않는 서로 다른 이름을 입력해주세요.";
     private static final String NUMBER_EXCEPTION_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
+    private static final String MIN_CAR_COUNT_MESSAGE = "[ERROR] 차동차는 최소 2대 이상이어야 한다.";
+    private static final int MIN_CAR_COUNT = 2;
     private static final int MAX_LENGTH_CAR_NAME = 5;
     private static final int MIN_LENGTH_CAR_NAME = 0;
 
@@ -18,6 +20,8 @@ public class InputValidator {
                 throw new IllegalArgumentException(EMPTY_NAME_EXCEPTION_MESSAGE);
             } else if (validateDuplicateName(carNameArray)) {
                 throw new IllegalArgumentException(DUPLICATE_NAME_EXCEPTION_MESSAGE);
+            } else if (validateCarCountRule(carNameArray)) {
+                throw new IllegalArgumentException(MIN_CAR_COUNT_MESSAGE);
             }
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -52,6 +56,10 @@ public class InputValidator {
         return carNameArray.length != Arrays.stream(carNameArray)
                 .distinct()
                 .count();
+    }
+
+    private static final boolean validateCarCountRule(String[] carNameArray) {
+        return carNameArray.length < MIN_CAR_COUNT;
     }
 
     private static final boolean validateNumber(String numberOfAttempts) {
