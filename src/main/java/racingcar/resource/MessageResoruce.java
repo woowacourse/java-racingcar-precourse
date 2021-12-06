c3177fe7921cecb4f397fd0553464fbcf6612eed
Kim Seunglae <obey1342@gmail.com>
2021-12-06 16:04:21 +0900

    feat: Message Resource 추가

---
+++ src/main/java/racingcar/resource/MessageResoruce.java
@@ -0,0 +1,14 @@
+package racingcar.resource;
+
+public class MessageResoruce {
+	public static final String INPUT_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+
+	public static final String ERROR_CAR_NAME_SIZE_OVER = "[ERROR] 자동차 이름의 크기는 5자 이하여야 합니다.";
+	public static final String ERROR_CAR_NAME_SIZE_UNDER = "[ERROR] 자동차 이름의 크기는 1보다 커야 합니다.";
+	public static final String ERROR_ROUND_NUMBER_NOT_VALID = "[ERROR] 시도 횟수는 숫자여야 한다.";
+	public static final String ERROR_ROUND_NUMBER_GREATER_THAN_ZERO = "[ERROR] 시도 횟수는 0보다 큰 정수여야 합니다.";
+
+	public static final String OUTPUT_RESULT = "실행 결과";
+	public static final String OUTPUT_WINNER = "최종 우승자 : ";
+}
