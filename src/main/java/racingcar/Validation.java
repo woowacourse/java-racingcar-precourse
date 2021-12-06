69a424570a1777f850a716b329939ff53330af43
qbang <vhfhddl0101@naver.com>
2021-12-07 00:01:56 +0900

    feat: Validation 인터페이스 추가

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -1,12 +1,13 @@
 package racingcar;
 
-public class Validation {
+public class Validation implements IValidation {
     private final String INPUT_LENGTH_ERROR = "[ERROR] 5자 이하의 이름을 입력해주세요.";
     private final String INPUT_NUMBER_ERROR = "[ERROR] 숫자를 입력해주세요.";
     private final String INPUT_RANGE_ERROR = "[ERROR] 0 이상의 값을 입력해주세요.";
     private final String INPUT_BLANK_ERROR = "[ERROR] 이름에는 공백이 포함될 수 없습니다.";
     private final int NAME_MAX_LENGTH = 5;
 
+    @Override
     public void validateLength(String inputList) {
         for (String s : inputList.split(",")) {
             if (s.length() > NAME_MAX_LENGTH) {
@@ -15,6 +16,7 @@ public class Validation {
         }
     }
 
+    @Override
     public void validateNumber(String input) {
         try {
             Integer.parseInt(input);
@@ -23,12 +25,14 @@ public class Validation {
         }
     }
 
+    @Override
     public void validateRange(String input) {
         if (Integer.parseInt(input) < 0) {
             throw new IllegalArgumentException(INPUT_RANGE_ERROR);
         }
     }
 
+    @Override
     public void validateName(String input) {
         String noSpaces = input.replaceAll(" ", "");
         for (String s : noSpaces.split(",", -1)) {
