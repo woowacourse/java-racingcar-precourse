268ba12c0f4dffca04573534ffc8de63e2e5dcbb
summerlunaa <rhgus1085@gmail.com>
2021-12-07 03:12:57 +0900

    feat(GameHandler): 전체 게임의 흐름을 담당하는 stratRace 메서드 구현

---
+++ src/main/java/racingcar/GameHandler.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public class GameHandler {
+	public static void startRace() {
+		String [] carNames = Input.carName();
+		int tryNumber = Input.tryNumber();
+
+		CarList cars = new CarList(carNames);
+
+		for(int i=0; i<tryNumber; i++){
+			cars.moveAll();
+			Output.movementState(cars);
+		}
+		Output.winners(cars);
+	}
+}
