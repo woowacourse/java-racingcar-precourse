c1211924c6659e8803c0a8afb0167b4184919550
erie0210 <eriee0210@gmail.com>
2021-12-07 08:33:55 +0900

    feat: System Message 추가

--- src/main/java/constants/SystemMessage.java
+++ src/main/java/constants/SystemMessage.java
@@ -1,2 +1,9 @@
 package constants;public class SystemMessage {
+    public static String INPUT_CARNAME = "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)";
+    public static String INPUT_TURN_NUMBER = "시도할 회수";
+    public static String OUTPUT_ONE_WINNER = "최종 우승자";
+    public static String OUTPUT_MULTI_WINNER = "우승자";
+
+    public static String ERROR_CARNAME_UNDER_5 = "[ERROR] 이름은 5글자 이하여야 합니다.";
+    public static String ERROR_VALID_NUMBER = "[ERROR] 숫자를 입력해주세요";
 }
