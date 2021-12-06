5d16cbc03ae947d98be4c6daabe5274f1de08428
Kim Seunglae <obey1342@gmail.com>
2021-12-07 01:20:40 +0900

    refactor: InputView 정적메소드로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,12 +5,14 @@ import static racingcar.resource.MessageResoruce.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-	public String inputNames() {
+	private InputView() {}
+
+	public static String inputNames() {
 		System.out.println(INPUT_NAMES_MESSAGE);
 		return Console.readLine();
 	}
 
-	public String inputRound() {
+	public static String inputRound() {
 		System.out.println(INPUT_ROUND_MESSAGE);
 		return Console.readLine();
 	}
