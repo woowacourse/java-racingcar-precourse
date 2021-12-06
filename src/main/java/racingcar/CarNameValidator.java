4f73a1344c6b58806cf1eaf5a7a5cf2593d3c264
TaeHyeon Kim <kth990303@naver.com>
2021-12-06 18:49:31 +0900

    refactor: CarNameValidator 클래스 일부 매직넘버 상수화

--- src/main/java/racingcar/CarNameValidator.java
+++ src/main/java/racingcar/CarNameValidator.java
@@ -3,7 +3,8 @@ package racingcar;
 import java.util.List;
 
 public class CarNameValidator {
-    private static final int NAME_SIZE = 5;
+    private static final int NAME_MIN_SIZE = 1;
+    private static final int NAME_MAX_SIZE = 5;
     private static final String NAME_SIZE_ERROR_MESSAGE = "[ERROR] 이름은 1자 이상 5자 이하여야 한다.";
     private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 이름은 중복될 수 없다.";
 
@@ -18,7 +19,7 @@ public class CarNameValidator {
 
     public static boolean checkCarNameLength(List<String> carNameList) {
         return carNameList.stream()
-                .filter(carName -> carName.length() > 0 && carName.length() <= NAME_SIZE)
+                .filter(carName -> carName.length() >= NAME_MIN_SIZE && carName.length() <= NAME_MAX_SIZE)
                 .count() == carNameList.size();
     }
 
