2cdb74965ecd5a8c32ba8cef2847054d3828ff30
Byunghoon <nbalance__@kyonggi.ac.kr>
2021-12-06 12:26:39 +0900

    style(함수명 오기입 수정): getInputCar 함수명 오기입 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -21,7 +21,7 @@ public class Application {
         while (true) {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             try {
-                inputData = getInputData();
+                inputData = getInputCar();
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 차의 이름은 5자 이내이어야 한다.");
