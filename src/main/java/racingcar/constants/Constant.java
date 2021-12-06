8f1691c083aed7d5a3ff94706fe1c8996b530c41
xi_jjun <rlawowns97@naver.com>
2021-12-07 00:38:49 +0900

    feat: add colon, progress contant

--- src/main/java/racingcar/constants/Constant.java
+++ src/main/java/racingcar/constants/Constant.java
@@ -3,18 +3,19 @@ package racingcar.constants;
 public class Constant {
 	public static final int MIN_CONDITION = 0;
 	public static final int MAX_CONDITION = 9;
-	public static final int MOVING_FORWARD = 4;
-
 	public static final int MIN_NAME_LENGTH = 1;
 	public static final int MAX_NAME_LENGTH = 5;
-	public static final int IMPROPER_CHARACTER = ',';
-	public static final String SPLIT_STRING = ",";
 
-	public static final int MIN_STAGE_NUMBER = 1;
+	public static final int MOVING_FORWARD = 4;
+
+	public static final int IMPROPER_CHARACTER = ',';
 	public static final int INVALID_STAGE_NUMBER = -1;
 
+	public static final String SPLIT_STRING = ",";
 	public static final String EMPTY = "";
 	public static final String SPACE = " ";
+	public static final String COLON = " : ";
+	public static final String PROGRESS = "-";
 
 	private Constant() {
 	}
