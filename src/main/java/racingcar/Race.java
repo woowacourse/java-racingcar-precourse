669996bb52c1d5ab2096d116579831a46cc51b27
limm-jk <junkyu.lim.dev@gmail.com>
2021-12-06 19:48:32 +0900

    feat : 입력을 기반으로 입력한 이름을 가진 Car들을 생성할 수 있는 기능 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -2,9 +2,15 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.*;
+
 public class Race {
     public void play() {
+        List<Car> cars = new ArrayList<>();
 
+        for (String name : getRacePlayerNames()) {
+            cars.add(new Car(name));
+        }
     }
 
     private String[] getRacePlayerNames() {
