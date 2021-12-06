35e85d141dd8d1e208c8d8be384acc42cd0d1b76
bonkwon <rnjsqhsrnjs@naver.com>
2021-12-06 23:52:49 +0900

    feature : 6-4 findChampion 메소드 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 public class Game {
+	private static int numberOfStages;
 
 	private Car[] cars;
 	private int numberOfCars;
@@ -12,5 +13,78 @@ public class Game {
 		for(String name : carsName){
 			cars[numberOfCars++] = new Car(name);
 		}
+	}
+
+	public void stageProcess(){
+		for(Car car : cars){
+			car.goForward();
+		}
+
+		//GamePrinter.stagePrinter();
+	}
+
+	public void gameProcess(){
+		//GamePrinter.getNamePrint();
+		carMaker();
+		//GamePrinter.getNumberPrint();
+
+		String trialNumber = UserInputGetter.getUserInput();
+
+		if(UserInputValidator.checkInt(trialNumber)){
+			numberOfStages = trialNumber.charAt(0) - '0';
+		}
+
+		for(int i = 0; i < numberOfStages; i++){
+			stageProcess();
+		}
+
+		Car[] Champions = findChampion();
+
+	}
+
+	public Car[] findChampion(){
+
+		int maxPosition = findChampionsPosition();
+		int numberOfChampion = findNumberOfChampion(maxPosition);
+
+
+		Car[] champions = new Car[numberOfChampion];
+
+		int tempChampion = 0;
+
+		for(int i = 0; i <= numberOfCars; i++){
+			if(cars[i].getPosition() == maxPosition){
+				champions[tempChampion] = cars[i];
+				tempChampion++;
+			}
+		}
+
+		return champions;
+	}
+
+	public int findChampionsPosition(){
+
+		int maxPosition = 0;
+
+		for(Car car : cars){
+			if(car.getPosition() > maxPosition){
+				maxPosition = car.getPosition();
+			}
+		}
+
+		return maxPosition;
+	}
+
+	public int findNumberOfChampion(int maxPosition){
+
+		int numberOfChampion = 0;
+
+		for(Car car : cars){
+			if(car.getPosition() == maxPosition){
+				numberOfChampion++;
+			}
+		}
+
+		return numberOfChampion;
 	}
 }
