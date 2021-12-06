b3632e2f0acb5b9f92e23eaaa0f468a771606473
harinn1014 <gkfla7555@gmail.com>
2021-12-06 18:35:32 +0900

    feat(Constant): add strings and integer
    <description>
    Constant는 프로그램을 실행할 때 필요한 정적 변수들을 모아놓는 클래스
    따라서 필요한 정적 변수들을 입력
    - 자동차 이름 길이 제한
    - 랜덤값 생성에 필요한 상수
    - 게임을 실행할 때 필요한 문자열

--- src/main/java/racingcar/utils/Constant.java
+++ src/main/java/racingcar/utils/Constant.java
@@ -1,5 +1,18 @@
 package racingcar.utils;
 
 public class Constant {
+	// LIMIT ABOUT NUMBER
+	public static final int CAR_NAME_LENGTH = 5;
+	public static final int RANDOM_RANGE_MINIMUM = 0;
+	public static final int RANDOM_RANGE_MAXIMUM = 9;
+	public static final int RANDOM_RANGE_STANDARD = 4;
 
+	public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String INPUT_TRY_NUM = "시도할 회수는 몇회인가요?";
+	public static final String EXECUTION_RESULT = "실행 결과";
+	public static final String LAST_WINNER_IS = "최종 우승자 : ";
+
+	public static final String DELIMITER_car_NAME = ",";
+	public static final String BETWEEN_NAME_RESULT = " : ";
+	public static final String EXECUTION_RESULT_MARKER = "-";
 }
