817bea0caa9e69503400d186deb1d216b811a616
swcho <progress0407@gmail.com>
2021-12-06 22:30:58 +0900

    refactor: 콘솔입력부분 리펙터링

--- src/main/java/racingcar/view/ConsoleInputStrategy.java
+++ src/main/java/racingcar/view/ConsoleInputStrategy.java
@@ -9,9 +9,14 @@ import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
 public class ConsoleInputStrategy implements InputStrategy {
+
+	public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	public static final String INPUT_TRY_NUMBERS = "시도할 회수는 몇회인가요?";
+
 	@Override
 	public Cars getCars() {
 		while (true) {
+			System.out.println(INPUT_CAR_NAMES);
 			String carNamesInput = Console.readLine();
 			List<Car> cars = new LinkedList<>();
 			String[] names = carNamesInput.split(",");
@@ -26,6 +31,7 @@ public class ConsoleInputStrategy implements InputStrategy {
 
 	@Override
 	public int getTryNumber() {
+		System.out.println(INPUT_TRY_NUMBERS);
 		String inputTryNumber = Console.readLine();
 		validateTryNumber(inputTryNumber);
 		return Integer.parseInt(inputTryNumber);
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,7 @@ import racingcar.domain.Cars;
 
 public class InputView {
 
-	public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	public static final String INPUT_TRY_NUMBERS = "시도할 회수는 몇회인가요?";
+
 
 	private final InputStrategy inputStrategy;
 
@@ -14,9 +13,9 @@ public class InputView {
 	}
 
 	public ViewDto getCarsAndTryNumber() {
-		System.out.println(INPUT_CAR_NAMES);
+
 		Cars cars = inputStrategy.getCars();
-		System.out.println(INPUT_TRY_NUMBERS);
+
 		int tryNumber = inputStrategy.getTryNumber();
 		return new ViewDto(cars, tryNumber);
 	}
