bf5415380fb870d6aa7de96c11ab2f82b8eaa203
xfrnk2 <xfrnk2@gmail.com>
2021-12-06 11:15:05 +0900

    feat: 메시지 출력 함수 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,10 +3,29 @@ package racingcar.view;
 public class OutputView {
 	public static final String ERROR_MESSAGE = "[ERROR] %s";
 	public static final String WINNER_MESSAGE = "최종 우승자 : %s";
+	public static final String CAR_DISTANCE_MESSAGE =  "%s : %s";
 
-	public static void printfMessage (String message, Object ...args) {
+	public static void printfMessage(String message, Object... args) {
 		System.out.printf(message, args);
+		printNewLine();
+	}
+
+	public static void printNewLine() {
 		System.out.println();
 	}
+
+	public static void printError(String error) {
+		printfMessage(ERROR_MESSAGE, error);
+	}
+
+	public static void printWinner(StringBuffer winner) {
+		printfMessage(WINNER_MESSAGE, winner);
+	}
+
+	public static void printCarNameAndPosition(String name, StringBuilder position){
+		printfMessage(CAR_DISTANCE_MESSAGE, name, position);
+	}
+
 }
+
 
