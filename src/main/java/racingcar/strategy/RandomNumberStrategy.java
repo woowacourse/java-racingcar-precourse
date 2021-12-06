97a075d37520ebd61542a82d39afda2ec8af6bcb
zzambbang <zzambbang@gmail.com>
2021-12-06 18:18:48 +0900

    style: update formatting

--- src/main/java/racingcar/strategy/RandomNumberStrategy.java
+++ src/main/java/racingcar/strategy/RandomNumberStrategy.java
@@ -2,7 +2,7 @@ package racingcar.strategy;
 
 import static camp.nextstep.edu.missionutils.Randoms.*;
 
-public class RandomNumberStrategy implements MoveStrategy{
+public class RandomNumberStrategy implements MoveStrategy {
 	private static final int MIN_NUMBER = 0;
 	private static final int MAX_NUMBER = 9;
 	private static final int PASS_NUM = 4;
--- src/main/java/racingcar/view/UserView.java
+++ src/main/java/racingcar/view/UserView.java
@@ -3,14 +3,14 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 /*
-* 사용자로부터 입력받기
-* */
+ * 사용자로부터 입력받기
+ * */
 public class UserView {
 
 	public static String[] setCarNamesInput() {
 		System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
 
-		while(true) {
+		while (true) {
 			try {
 				String[] carNames = Console.readLine().split(",");
 				Validator.validateCarNames(carNames);
@@ -24,7 +24,7 @@ public class UserView {
 	public static int setRoundInput() {
 		System.out.println("시도할 횟수는 몇회인가요?");
 
-		while(true) {
+		while (true) {
 			try {
 				String trialNum = Console.readLine();
 				Validator.validateRoundNum(trialNum);
