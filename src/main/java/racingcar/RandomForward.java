4850a15805ed3a5aae9ab4cd6804dc18a72eca3d
Chan Kim <chankim@Chanui-MacBookPro.local>
2021-12-07 04:27:37 +0900

    feat : 자동차 전진 기능 구현

---
+++ src/main/java/racingcar/RandomForward.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomForward {
+	private static final int FORWARD_NUM = 4;
+	private static final int startInclusive = 0;
+	private static final int endInclusive = 9;
+
+	public boolean isForward() {
+		if (Randoms.pickNumberInRange(startInclusive, endInclusive) >= FORWARD_NUM) {
+			return true;
+		}
+		return false;
+	}
+}
