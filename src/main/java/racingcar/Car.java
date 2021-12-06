313dd892b07b7a1e3fafe57c67c2c8404010d6be
Byunghoon <nbalance__@kyonggi.ac.kr>
2021-12-06 13:46:06 +0900

    feat(자동차 이동 기능): 자동차 이동 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,20 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
 
 public class Car {
     private final String name;
     private int position = 0;
+    private static final int STANDARD = 4;
 
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void movement() {
+        int randomData = Randoms.pickNumberInRange(0, 9);
+        if (randomData >= STANDARD) {
+            position += 1;
+        }
+    }
 }
