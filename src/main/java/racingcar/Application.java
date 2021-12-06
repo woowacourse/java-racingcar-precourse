254a5dc5c075f7c711c4c6a6c05e5132b8cf0401
grace_goose <choieungi@gm.gist.ac.kr>
2021-12-06 22:24:31 +0900

    fix: test를 위해 controller 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        InputRole inputRole = new InputRole();
-        inputRole.getInputValue();
+        GameController gameController = new GameController();
+        gameController.gameStart();
 
     }
 }
