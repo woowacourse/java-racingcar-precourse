349b1bb6cacf8cb0bff7323cb1f990ec444ffbbd
kanamycine <jinyeong5320@gmail.com>
2021-12-06 16:53:54 +0900

    feat(Car,Game) : race 진행 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,29 @@
 package racingcar;
 
+import racingcar.utils.RandomGenerator;
+
+import static racingcar.utils.ConstantNum.*;
+
 public class Car {
 	private final String carName;
 	private int position = 0;
 
 	public Car(String carName) {
 		this.carName = carName;
+	}
+
+	public void proceedOrPause() {
+		RandomGenerator randomGenerator = new RandomGenerator();
+		if (randomGenerator.getRandomNum() >= PROCEED_NUM_MIN) {
+			this.position++;
+		}
+	}
+
+	public void printPosition(){
+		System.out.println(this.carName + " : ");
+		for (int i = 0; i < position; i++){
+			System.out.print("-");
+		}
+		System.out.println();
 	}
 }
--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -2,8 +2,12 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.Car;
+import racingcar.utils.RandomGenerator;
 import racingcar.view.InputView;
+
+import static racingcar.utils.ConstantNum.*;
 
 public class Game {
 
@@ -12,9 +16,25 @@ public class Game {
 		int gameCount = InputView.gameCount();
 
 		//랜덤 경기 진행
-
+		race(carsList, gameCount);
 		//우승자 선발
 
 		//위너 출력
 	}
+
+	private void race(ArrayList<Car> carsList, int gameCount) {
+		for (Car car : carsList){
+			playTurn(car, gameCount);
+		}
+	}
+
+	public void playTurn(Car car, int gameCount){
+		for(int i = 0; i < gameCount; i++){
+			car.proceedOrPause();
+			car.printPosition();
+		}
+		System.out.println();
+	}
+
+
 }
