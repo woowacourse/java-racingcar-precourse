4741cb4ac1bdd7a2974c5ebde0312e076afb7f15
dls4585 <dls4585@naver.com>
2021-12-06 11:16:32 +0900

    feat: 난수 생성 클래스 구현

---
+++ src/main/java/racingcar/OneDigitNumberGenerator.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class OneDigitNumberGenerator implements RandomNumberGenerator {
+
+    @Override
+    public int generateRandomNumberInRange(int startInclusive, int endInclusive) {
+        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+    }
+}
---
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -0,0 +1,5 @@
+package racingcar;
+
+public interface RandomNumberGenerator {
+    int generateRandomNumberInRange(int startInclusive, int endInclusive);
+}
