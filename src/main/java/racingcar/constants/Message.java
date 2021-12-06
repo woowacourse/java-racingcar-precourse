bf508529a40c6173a32bc0d6a209e539390ffc9d
xi_jjun <rlawowns97@naver.com>
2021-12-07 01:26:44 +0900

    fix: 실행결과 메세지 한줄 띄우고 출력되게 변경

--- src/main/java/racingcar/constants/Message.java
+++ src/main/java/racingcar/constants/Message.java
@@ -3,7 +3,7 @@ package racingcar.constants;
 public class Message {
 	public static final String START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	public static final String ASK_STAGES = "시도할 회수는 몇회인가요?";
-	public static final String EXECUTE_RESULT = "실행 결과";
+	public static final String EXECUTE_RESULT = "\n실행 결과";
 	public static final String WINNERS = "최종 우승자 : ";
 	public static final String ERROR_INVALID_NAME_FORMAT = "[ERROR] 자동차의 이름은 중복이 안되는 1~5글자 입니다";
 	public static final String ERROR_INVALID_STAGE_VALUE = "[ERROR] 시도할 횟수는 0이 아닌 숫자여야 합니다";
