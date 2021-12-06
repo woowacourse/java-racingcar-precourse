4102f7c6791fda081125008d25b68fa46852343e
TaeHyeon Kim <kth990303@naver.com>
2021-12-06 18:46:41 +0900

    refactor: 정규식 상수로 치환

--- src/main/java/racingcar/AttemptCountValidator.java
+++ src/main/java/racingcar/AttemptCountValidator.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class AttemptCountValidator {
     private static final String COUNTS_NUMERIC_ERROR_MESSAGE = "[ERROR] 시도 횟수는 자연수여야 한다.";
+    private static final String ATTEMPT_COUNTS_VALIDATE_REGEX = "^[1-9][0-9]*$";
 
     public static void checkException(String counts) {
         if (!checkNaturalNumber(counts)) {
@@ -10,6 +11,6 @@ public class AttemptCountValidator {
     }
 
     public static boolean checkNaturalNumber(String counts) {
-        return counts.matches("^[1-9][0-9]*$");
+        return counts.matches(ATTEMPT_COUNTS_VALIDATE_REGEX);
     }
 }
