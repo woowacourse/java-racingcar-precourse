72c038c8aa3da17db6ce7822d4bfca00ee102583
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-06 22:30:45 +0900

    refactor: INPUT_CAR_NAMES_REGEX Car name 입력 시 regex 매직 리터럴 > 상수 변환

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_CAR_NAMES_REGEX = ",";
+
     private static final String INPUT_ROUND = "시도할 회수는 몇회인가요?";
 
     private InputView() {
@@ -15,7 +17,7 @@ public class InputView {
 
     public static List<String> inputCarNames() {
         System.out.println(INPUT_CAR_NAMES);
-        return Arrays.stream(Console.readLine().split(","))
+        return Arrays.stream(Console.readLine().split(INPUT_CAR_NAMES_REGEX))
             .map(String::trim)
             .collect(Collectors.toList());
     }
