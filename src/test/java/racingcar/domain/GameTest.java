d9b2fa47b75cff6ac19290f8a340e2b18ee9fa67
0chil <0@chll.it>
2021-12-07 08:52:06 +0900

    test: reflect Game changes and add mock test
    use Mockito to test game run

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -1,87 +1,69 @@
 package racingcar.domain;
 
+import static org.mockito.ArgumentMatchers.*;
 import static racingcar.constant.GameConstants.GameStringConversion.*;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class GameTest {
 	List<String> carNameList;
-	Game game;
+	List<Car> carSpyList;
+	Game spyGame;
 
 	@BeforeEach
 	void setUp() {
+		MockitoAnnotations.openMocks(this);
 		carNameList = Arrays.asList("자1동차", "자2동차", "자3동차");
-		game = new Game(carNameList);
-	}
-
-	@Test
-	void 자동차_리스트_삽입() {
-		// given, when
-		List<String> carNameListFromGameClass = game.getCarList()
-			.stream()
-			.map(Car::getName)
-			.collect(Collectors.toList());
-		// then
-		Assertions.assertThat(carNameListFromGameClass.containsAll(carNameList)).isTrue();
+		carSpyList = carNameList.stream().map(name -> Mockito.spy(new Car(name))).collect(Collectors.toList());
+		spyGame = Mockito.spy(new Game(carSpyList));
 	}
 
 	@Test
 	void 게임_한판_진행() {
 		// given, when
-		game.runSingle();
-		List<Car> list = game.getCarList();
-		int farthestPosition = 0;
-		for (Car car : list)
-			farthestPosition = Math.max(farthestPosition, car.getPosition());
-		final int finalFarthestPosition = farthestPosition;
+		spyGame.runSingle();
 
-		//then
-		Assertions.assertThat(game.getCarList()
-				.stream()
-				.filter(car -> car.getPosition() == finalFarthestPosition)
-				.anyMatch(winnerCandidate -> game.getWinnerCandidateList().contains(winnerCandidate)))
-			.isTrue();
+		// then
+		carSpyList.forEach(carMock -> Mockito.verify(carMock, Mockito.atLeastOnce()).moveByNumber(anyInt()));
 	}
 
 	@Test
 	void 게임_여러판_진행() {
 		// given, when
-		game.runMultiple(5);
-		List<Car> list = game.getCarList();
-		int farthestPosition = 0;
-		for (Car car : list)
-			farthestPosition = Math.max(farthestPosition, car.getPosition());
-		final int finalFarthestPosition = farthestPosition;
+		spyGame.runMultiple(5);
 
-		//then
-		Assertions.assertThat(game.getCarList()
-				.stream()
-				.filter(car -> car.getPosition() == finalFarthestPosition)
-				.anyMatch(winnerCandidate -> game.getWinnerCandidateList().contains(winnerCandidate)))
-			.isTrue();
+		Mockito.verify(spyGame, Mockito.times(5)).runSingle();
 	}
 
 	@Test
 	void 게임_결과_문자열_변환() {
 		// given, when
-		game.runMultiple(5);
+		spyGame.runMultiple(5);
 		StringBuilder expectedWinnerStringBuilder = new StringBuilder();
-		game.getWinnerCandidateList()
+		Car farthestCar = Collections.max(spyGame.getCarList(), Comparator.comparingInt(Car::getPosition));
+		spyGame.getCarList()
+			.stream()
+			.filter(car -> car.getPosition() == farthestCar.getPosition())
+			.collect(Collectors.toList())
 			.forEach(winner -> expectedWinnerStringBuilder.append(winner.getName())
 				.append(GAME_WINNER_DELIMITER.getString()));
 		String expectedWinnerString = expectedWinnerStringBuilder.substring(0,
 			expectedWinnerStringBuilder.length() - GAME_WINNER_DELIMITER.getString().length());
 
 		//then
-		carNameList.forEach(car -> Assertions.assertThat(game.toString()).contains(car.toString()));
-		Assertions.assertThat(game.toString()).contains(GAME_RESULT_HINT.getString());
-		Assertions.assertThat(game.toString()).contains(expectedWinnerString);
+		carNameList.forEach(car -> Assertions.assertThat(spyGame.toString()).contains(car.toString()));
+		Assertions.assertThat(spyGame.toString()).contains(GAME_RESULT_HINT.getString());
+		Assertions.assertThat(spyGame.toString()).contains(expectedWinnerString);
 	}
 
 	@Test
--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -5,23 +5,30 @@ import static racingcar.constant.GameConstants.GameStringConversion.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import racingcar.domain.Car;
 import racingcar.domain.Game;
 
 class OutputViewTest {
-	List<String> carList;
+	List<String> carNameList;
+	List<Car> carList;
 	Game game;
 	ByteArrayOutputStream byteArrayOutputStream;
 	PrintStream printStream;
 
 	@BeforeEach
 	void setUp() {
-		carList = Arrays.asList("자1동차", "자2동차", "자3동차");
+		carNameList = Arrays.asList("자1동차", "자2동차", "자3동차");
+		carList = carNameList.stream().map(Car::new).collect(Collectors.toList());
 		game = new Game(carList);
 		byteArrayOutputStream = new ByteArrayOutputStream();
 		printStream = new PrintStream(byteArrayOutputStream);
@@ -34,15 +41,18 @@ class OutputViewTest {
 		game.runMultiple(5);
 
 		StringBuilder expectedWinnerStringBuilder = new StringBuilder();
-		game.getWinnerCandidateList()
-			.forEach(winner -> expectedWinnerStringBuilder.append(winner.getName())
-				.append(GAME_WINNER_DELIMITER.getString()));
+		Car farthestCar = Collections.max(game.getCarList(), Comparator.comparingInt(Car::getPosition));
+		game.getCarList()
+			.stream()
+			.filter(car -> car.getPosition() == farthestCar.getPosition())
+			.collect(Collectors.toList())
+			.forEach(winner -> expectedWinnerStringBuilder.append(winner.getName()).append(GAME_WINNER_DELIMITER.getString()));
 		String expectedWinnerString = expectedWinnerStringBuilder.substring(0,
 			expectedWinnerStringBuilder.length() - GAME_WINNER_DELIMITER.getString().length());
 
 		//then
 		OutputView.printGameResult(game);
-		carList.forEach(car -> Assertions.assertThat(byteArrayOutputStream.toString()).contains(car.toString()));
+		carNameList.forEach(car -> Assertions.assertThat(byteArrayOutputStream.toString()).contains(car.toString()));
 		Assertions.assertThat(byteArrayOutputStream.toString()).contains(GAME_RESULT_HINT.getString());
 		Assertions.assertThat(byteArrayOutputStream.toString()).contains(expectedWinnerString);
 	}
