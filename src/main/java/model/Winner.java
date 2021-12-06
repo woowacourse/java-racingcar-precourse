ef553930f9013f32f7f78e45a210cc7cc1e1a27a
koo <gudonghee2000@gmail.com>
2021-12-06 20:11:20 +0900

    refactor : Winner 클래스 함수 구조 변경

--- src/main/java/model/Winner.java
+++ src/main/java/model/Winner.java
@@ -4,22 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Winner {
-	private static final int FIRST_CAR = 0;
+	private static final int FIRST_CAR_LOCATION = 0;
 	private static final int MIN_CAR_POSITION = 0;
 	private List<String> winners = new ArrayList<>();
 
 	public Winner(String[] carNames, int[] carPositions) {
 		int maxPosition = maxCarPosition(carPositions);
-		for (int index = FIRST_CAR; index < carPositions.length; index++) {
-			if (carPositions[index] == maxPosition) {
-				winners.add(carNames[index]);
-			}
+		int lastCarPostion = carPositions.length - 1;
+		for (int index = FIRST_CAR_LOCATION; index <= lastCarPostion; index++) {
+			int candidateCarPosition = carPositions[index];
+			String candidateCarName = carNames[index];
+			compareCandidateWithWinner(candidateCarName, candidateCarPosition, maxPosition);
+		}
+	}
+
+	private void compareCandidateWithWinner(String candidateCarName, int candidateCarPostion, int maxPosition) {
+		if (candidateCarPostion == maxPosition) {
+			winners.add(candidateCarName);
 		}
 	}
 
 	private int maxCarPosition(int[] carPositions) {
 		int maxPosition = MIN_CAR_POSITION;
-		for (int index = FIRST_CAR; index < carPositions.length; index++) {
+		for (int index = FIRST_CAR_LOCATION; index < carPositions.length; index++) {
 			if (carPositions[index] > maxPosition) {
 				maxPosition = carPositions[index];
 			}
