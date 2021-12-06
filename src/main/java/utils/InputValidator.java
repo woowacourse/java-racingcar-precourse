898f66d8f878da8eb97cfacd3f710765e2983461
koo <gudonghee2000@gmail.com>
2021-12-06 15:46:06 +0900

    refactor : 에러문구에서 [ERROR]를 출력하도록 수정

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -23,19 +23,19 @@ public class InputValidator {
 
 	public static int isValideRaceCount(String raceCount) {
 		if (raceCount.length() < 1) {
-			throw new IllegalArgumentException("입력한 경주횟수가 없다");
+			throw new IllegalArgumentException("[ERROR] 입력한 경주횟수가 없다");
 		}
 
 		if (raceCount.contains(" ")) {
-			throw new IllegalArgumentException("입력한 경주횟수에 공백이 있다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 경주횟수에 공백이 있다.");
 		}
 
 		if (!Pattern.matches(NUMBER_PATTERN, raceCount)) {
-			throw new IllegalArgumentException("입력한 경주횟수가 숫자가 아니다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 경주횟수가 숫자가 아니다.");
 		}
 
 		if (raceCount.startsWith("0")) {
-			throw new IllegalArgumentException("입력한 경주횟수가 0으로 시작한다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 경주횟수가 0으로 시작한다.");
 		}
 
 		return Integer.parseInt(raceCount);
@@ -50,21 +50,21 @@ public class InputValidator {
 
 	private static void haveBlankInRacingCarName(String racingCarName) {
 		if (racingCarName.contains(" ")) {
-			throw new IllegalArgumentException("입력한 자동차 이름에 공백이 있습니다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 자동차 이름에 공백이 있습니다.");
 		}
 	}
 
 	private static boolean haveDuplicateNames(String[] racingCarNames) {
 		int racingCarNamesCount = (int)Arrays.stream(racingCarNames).distinct().count();
 		if (racingCarNames.length != racingCarNamesCount) {
-			throw new IllegalArgumentException("입력한 자동차 이름중 중복되는 이름이 있다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 자동차 이름중 중복되는 이름이 있다.");
 		}
 		return false;
 	}
 
 	private static boolean isMoreThanOneRacingCar(String[] racingCarNames) {
 		if (racingCarNames.length < 2) {
-			throw new IllegalArgumentException("입력한 자동차가 두대 미만 이다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 자동차가 두대 미만 이다.");
 		}
 		return true;
 	}
@@ -78,18 +78,18 @@ public class InputValidator {
 
 	private static void isValideRacingCarNameLength(String racingCarName) {
 		if (racingCarName.length() > 5) {
-			throw new IllegalArgumentException("입력한 자동차 이름중 이름이 5글자가 넘는 자동차가 있다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 자동차 이름중 이름이 5글자가 넘는 자동차가 있다.");
 		}
 
 		if (racingCarName.length() < 1) {
-			throw new IllegalArgumentException("입력한 자동차 이름중 이름이 없는 경우가 있다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 자동차 이름중 이름이 없는 경우가 있다.");
 		}
 
 	}
 
 	private static void isNoNameAfterComma(String racingCarNames) {
 		if (',' == racingCarNames.charAt(racingCarNames.length() - 1)) {
-			throw new IllegalArgumentException("입력한 자동차 이름 마지막 ',' 다음에 입력된 자동차 이름이 없다.");
+			throw new IllegalArgumentException("[ERROR] 입력한 자동차 이름 마지막 ',' 다음에 입력된 자동차 이름이 없다.");
 		}
 	}
 }
