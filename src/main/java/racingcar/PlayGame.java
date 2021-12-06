38fd39bbff6f6deedbbe7eba091568be66ec0759
hwihwi99 <chamgrace@naver.com>
2021-12-07 02:07:12 +0900

    게임 실행하기ㅣ

---
+++ src/main/java/racingcar/PlayGame.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class PlayGame {
+    private InputCarName inputCarName;
+    private InputCount inputCount;
+
+    private Car[] cars;
+    private int playCount;
+
+    public PlayGame() {
+        inputCarName = new InputCarName();
+        inputCount = new InputCount();
+    }
+
+    private void init() {
+        String[] carArray = inputCarName.inputCar();
+        cars = new Car[carArray.length];
+        for(int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(carArray[i]);
+        }
+        playCount = inputCount.stringToInteger();
+    }
+
+    public void playGame() {
+        init();
+        System.out.println();
+        System.out.println("실행 결과");
+        for(int i = 0; i<playCount;i++) {
+            perGame();
+            System.out.println();
+        }
+    }
+
+    // 한번 게임이 실행될 때마다 각 자동차에게 랜덤 수 생성해서 전진/멈춤 유무 판단
+    private void perGame() {
+        for(int i = 0; i<cars.length;i++){
+            int score = Randoms.pickNumberInRange(0,9);
+            if(score>=4) { // 4이상이면 직진 가능
+                cars[i].addPosition();
+            }
+            System.out.println(cars[i].toString()); // 메소드로 변경
+        }
+    }
+}
