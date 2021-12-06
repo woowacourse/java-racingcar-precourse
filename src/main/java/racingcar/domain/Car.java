1e4c506f859c02779b3cdde193df3d42fc71d0b0
Kim Seunglae <obey1342@gmail.com>
2021-12-07 01:51:51 +0900

    style: 포맷팅 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,31 +1,31 @@
 package racingcar.domain;
 
 public class Car {
-    private final CarName name;
-    private final Position position;
+	private final CarName name;
+	private final Position position;
 
-    public Car(final CarName name, final int position) {
-        this.name = name;
-        this.position = new Position(position);
-    }
+	public Car(final CarName name, final int position) {
+		this.name = name;
+		this.position = new Position(position);
+	}
 
-    public Car(final CarName name) {
-        this(name, 0);
-    }
+	public Car(final CarName name) {
+		this(name, 0);
+	}
 
-    public Car(final String name) {
-        this(new CarName(name));
-    }
+	public Car(final String name) {
+		this(new CarName(name));
+	}
 
-    public String getName() {
-        return name.getCarName();
-    }
+	public String getName() {
+		return name.getCarName();
+	}
 
-    public int getPosition() {
-        return position.getValue();
-    }
+	public int getPosition() {
+		return position.getValue();
+	}
 
-    public void move(RandomNumberGenerator generator) {
-        position.isMove(generator);
-    }
+	public void move(RandomNumberGenerator generator) {
+		position.isMove(generator);
+	}
 }
