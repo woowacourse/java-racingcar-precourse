ef3819b0f12327bfb2188716d6ec632d3da829f6
sejung-hong <sejungh@naver.com>
2021-12-06 23:46:58 +0900

    feat : 자동차 이름 입력 기능

--- src/main/java/racingcar/PlayGame.java
+++ src/main/java/racingcar/PlayGame.java
@@ -1,4 +1,27 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class PlayGame {
+    Computer computer;
+    User user;
+    String[] cars;
+
+    public void Play(){
+        initSetting();
+        CarName();
+    }
+
+    //초기 세팅 함수
+    private void initSetting(){
+        computer = new Computer();
+        user = new User();
+    }
+    
+    //자동차 이름 입력 함수
+    private void CarName(){
+        computer.MSGCarName();
+        cars = user.InputCarName();
+    }
+
 }
--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -1,4 +1,28 @@
 package racingcar;
 
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
+
+    //자동차 이름 입력
+    public String[] InputCarName(){
+        String carsString = Console.readLine();
+        String[] cars = carsString.split(",");
+        try {
+            IsValidName(cars);
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return InputCarName();
+        }
+        return cars;
+    }
+
+    private void IsValidName(String[] cars){
+        for(String car : cars){
+            if(car.length() > 5) {
+                throw new IllegalArgumentException("[ERROR] 이름은 5자 이하만 가능하다.");
+            }
+        }
+    }
 }
