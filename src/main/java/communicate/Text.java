bad2c172ae38e364afe8d3579b617d5a26fc6dc6
usuyn <usuyn000@gmail.com>
2021-12-06 22:16:09 +0900

    feat(Text): define output text for race

---
+++ src/main/java/communicate/Text.java
@@ -0,0 +1,12 @@
+package communicate;
+
+public class Text {
+    public static final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String REQUEST_ATTEMPT_NUMBER = "시도할 횟수는 몇 회인가요?";
+    public static final String RACE_RESULT = "실행 결과";
+    public static final String FINAL_WINNER = "최종 우승자";
+    public static final String MOVE_MARK = "-";
+    public static final String SEPARATOR_COMMA = ",";
+    public static final String SEPARATOR_COMMA_WITH_SPACE = ", ";
+    public static final String COLON = " : ";
+}
