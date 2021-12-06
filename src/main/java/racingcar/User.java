2aeeb485d3ad0abd1f35ea8987f9c66bfcbe14af
soominsohn <soomin.sohn.dev@gmail.com>
2021-12-06 20:25:15 +0900

    feat: add get game number function in User class - User add getGameNumber() add isNumeric()

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -32,6 +32,23 @@ public class User {
 		return true;
 	}
 
+	public Integer getGameNumber() {
+		while (true) {
+			String gameNumber = Console.readLine();
+			if (!isNumeric(gameNumber)) {
+				throw new IllegalArgumentException();
+			}
+			return Integer.parseInt(gameNumber);
+		}
+	}
 
+	public boolean isNumeric(String gameNumber) {
 
+		for (int i = 0; i < gameNumber.length(); i++) {
+			if (!Character.isDigit(gameNumber.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
