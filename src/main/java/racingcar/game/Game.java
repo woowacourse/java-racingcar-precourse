bfbfa08c9f72d00f2af56fbfab176597e3764c7f
Mo0nl19ht <mool2480@gmail.com>
2021-12-07 12:46:32 +0900

    차 리스트 생성 기능 추가

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -2,12 +2,19 @@ package racingcar.game;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.car.Car;
+
 public class Game {
 	public int num;
+	public List<Car> carList;
 
 	public Game() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String[] names = readLine().split(",");
+		carList = makeCarList(names);
 
 		System.out.println("시도할 회수는 몇회인가요?");
 		num = inputNum();
@@ -22,7 +29,15 @@ public class Game {
 			System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.\n");
 			num = inputNum();
 		}
+		
 		return num;
 	}
 
+	private List<Car> makeCarList(String[] names) {
+		List<Car> carList = new ArrayList<>();
+		for (String name : names) {
+			carList.add(new Car(name));
+		}
+		return carList;
+	}
 }
