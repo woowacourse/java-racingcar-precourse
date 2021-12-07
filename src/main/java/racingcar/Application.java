d3e15c82879e41f11446627558432808d2ca3a5b
hyunrrr <whgusrms96@gmail.com>
2021-12-07 17:16:57 +0900

    feat: main함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,8 @@ package racingcar;
 public class Application {
 	public static void main(String[] args) {
 		// TODO 구현 진행
+		Player player = new Player();
+		RacingGame racingGame = new RacingGame();
+		racingGame.playRacingGame(player);
 	}
 }
