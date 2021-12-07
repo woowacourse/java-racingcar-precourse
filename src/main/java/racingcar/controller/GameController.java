1274db8042c35ec1096bda49066f2fa377800f83
harinn1014 <gkfla7555@gmail.com>
2021-12-07 18:54:13 +0900

    feat(GameController): add code in play() method
    <description>
    play() 함수의 내용을 추가함.
    1. InputController를 사용하여 자동차와 시도횟수를 입력받는다.
    2. OutputView를 활용하여 경주 시작 메시지를 출력
    3. 라운드 시작(roundPlay())

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -6,7 +6,11 @@ import racingcar.view.OutputView;
 public class GameController {
 
 	public void play() {
-
+		InputController game = new InputController();
+		Cars carList = game.getInput_Car();
+		int tryNum = game.getInput_try();
+		OutputView.printRacingStart();
+		roundPlay(carList, tryNum);
 	}
 
 	public void roundPlay(Cars carList, int tryNum) {
