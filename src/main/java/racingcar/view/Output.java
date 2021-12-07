8bdffac788a39f1fe676a31f2b21644de37bd646
summerlunaa <rhgus1085@gmail.com>
2021-12-07 10:52:30 +0900

    feat(Output): "실행 결과" 메시지를 출력하는 raceStartPhrase 메서드 구현

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -3,6 +3,10 @@ package racingcar.view;
 import racingcar.domain.CarList;
 
 public class Output {
+	public static void raceStartPhrase() {
+		System.out.println("\n실행 결과");
+	}
+
 	public static void movementState(CarList cars) {
 		for (String state : cars.getMovementState()) {
 			System.out.println(state);
