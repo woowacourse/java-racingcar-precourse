03ab67a2cc8c5b90d2da33ffefb30894dba2c422
iamsseaa <homingsea@gmail.com>
2021-12-07 10:19:26 +0900

    chore: 에러 문구 수정

--- src/main/java/racingcar/Constant.java
+++ src/main/java/racingcar/Constant.java
@@ -5,12 +5,13 @@ public class Constant {
     public static final String INPUT_MESSAGE_TRY_TIMES = "시도할 회수는 몇회인가요?";
     public static final String RACE_PROCESS = "실행 결과";
     public static final String FINAL_WINNER = "최종 우승자 : ";
+    public static final String FINAL_WINNER_JOIN_DELIMITER = ", ";
 
     public static final String ERROR_PREFIX = "[ERROR]";
-    public static final String ERROR_EMPTY_NAME = "이름에 공백이 있을 수 없습니다.";
-    public static final String ERROR_OVERLAP_NAME = "이름은 중복될 수 없습니다.";
-    public static final String ERROR_RIGHT_LENGTH_NAME = "1~5자 사이의 이름만 가능합니다.";
+    public static final String ERROR_EMPTY_NAME = ERROR_PREFIX + "이름에 공백이 있을 수 없습니다.";
+    public static final String ERROR_OVERLAP_NAME = ERROR_PREFIX + "이름은 중복될 수 없습니다.";
+    public static final String ERROR_RIGHT_LENGTH_NAME = ERROR_PREFIX + "1~5자 사이의 이름만 가능합니다.";
 
-    public static final String ERROR_NOT_NUMBER_TIMES = "시도 횟수는 정수여야 합니다.";
-    public static final String ERROR_POSITIVE_INTEGER = "시도 횟수는 양의 정수여야 합니다.";
+    public static final String ERROR_NOT_NUMBER_TIMES = ERROR_PREFIX + "시도 횟수는 정수여야 합니다.";
+    public static final String ERROR_POSITIVE_INTEGER = ERROR_PREFIX + "시도 횟수는 양의 정수여야 합니다.";
 }
