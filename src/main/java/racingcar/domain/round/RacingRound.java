4d3256bc40f502d8e4e980f56330a1bb1f006c65
cjlee38 <gptpem38@gmail.com>
2021-12-07 19:11:26 +0900

    feat: 전진시도 횟수 객체(RacingRound) 추가
    정수형의 양수를 받아 전진 시도 횟수를 초기화 한 뒤,
    게임이 끝났는지 여부를 판단하는 객체인 RacingRound 추가

---
+++ src/main/java/racingcar/domain/round/RacingRound.java
@@ -0,0 +1,31 @@
+package racingcar.domain.round;
+
+public class RacingRound {
+	private static final String ERR_NON_POSITIVE = "입력값이 양수가 아닙니다.";
+
+	private int count = 0;
+
+	public RacingRound(int count) {
+		validateInteger(count);
+		this.count = count;
+	}
+
+	private static void validateInteger(int count) {
+		validateZeroOrLess(count);
+	}
+
+	private static void validateZeroOrLess(int count) {
+		if (count <= 0) {
+			throw new IllegalArgumentException(ERR_NON_POSITIVE);
+		}
+	}
+
+	public boolean isOver() {
+		return this.count <= 0;
+	}
+
+	public boolean decrease() {
+		this.count--;
+		return true;
+	}
+}
