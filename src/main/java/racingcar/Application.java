42873c166c6032adcee1f78f3e0c05a51d12e810
bonkwon <rnjsqhsrnjs@naver.com>
2021-12-07 21:48:06 +0900

    feature : Application Class 에서 전체 게임 진행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,8 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        Game newGame = new Game();
+
+        newGame.gameProcess();
     }
 }
