1e95720fedc553be9e2d54445077961f249f3aa3
Huicheol Moon <huicheol7@gmail.com>
2021-12-07 23:24:50 +0900

    feat(Application): 자동차 경주 게임 어플리케이션 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import controller.RacingGame;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		RacingGame racingGame = new RacingGame();
+		racingGame.start();
+		racingGame.end();
+	}
 }
