22a8d8643725581543cf08d9ea221b61b72ebb83
harinn1014 <gkfla7555@gmail.com>
2021-12-07 17:25:13 +0900

    feat(InputView): add input method
    <description>
    자동차 이름 입력받는 함수 추가
    시도 횟수 입력받는 함수 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,18 @@
 package racingcar.view;
 
+import racingcar.utils.Constant;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+	public static String[] getCarNames() {
+		System.out.println(Constant.INPUT_CAR_NAME);
+		return Console.readLine().split(Constant.DELIMITER_CAR_NAME);
+	}
+
+	public static String getTryNum() {
+		System.out.println(Constant.INPUT_TRY_NUM);
+		return Console.readLine();
+	}
 
 }
