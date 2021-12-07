fc4847651a0c1d5ba292d65d441adbd43b582ffa
ayoung0073 <ayong0310@naver.com>
2021-12-07 21:51:29 +0900

    test: InputValidator 테스트 코드 수정

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -9,20 +9,18 @@ class InputValidatorTest {
     @DisplayName("입력값 길이가 5보다 긴 이름이 있으면 예외를 던진다.")
     @Test
     void validateLengthTest() {
-        InputValidator validator = new InputValidator();
         String[] inputNames = {"apple", "banana", "mango"};
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-                validator.validateCarNameListInput(inputNames)
+                InputValidator.validateCarNamesInput(inputNames)
         );
     }
 
     @DisplayName("입력값 중 중복된 이름이 있으면 예외를 던진다.")
     @Test
     void validateExistDuplicateNameTest() {
-        InputValidator validator = new InputValidator();
         String[] inputNames = {"apple", "mango", "apple"};
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-                validator.validateCarNameListInput(inputNames)
+                InputValidator.validateCarNamesInput(inputNames)
         );
     }
 }
