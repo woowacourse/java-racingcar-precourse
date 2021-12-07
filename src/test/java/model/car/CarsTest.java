c8a21611271086d55b2563e7b862dfec7e24af23
Byeongju-Kong <bjuuuu98@gmail.com>
2021-12-07 13:04:04 +0900

    test(CarsTest): 가독성 개선
    비교적 가독성이 좋지 않은 여러 개의 isEqualTo 문들
    extracting, containsOnly를 통해 개선

--- src/test/java/model/car/CarsTest.java
+++ src/test/java/model/car/CarsTest.java
@@ -1,20 +1,19 @@
 package model.car;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-import java.util.Arrays;
-import java.util.List;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import model.movement.Movement;
 import model.movement.RandomMovement;
 
 class CarsTest {
-    private final boolean[] movement = new boolean[] {true, false, true};
+    private final boolean[] movement = new boolean[]{true, false, true};
     private int movementIndex = 0;
     private final Movement randomMovement = new RandomMovement() {
         @Override
@@ -34,25 +33,19 @@ class CarsTest {
     @DisplayName("자동차들을 전진시키고 결과를 반환한다.")
     void race_getRacingResult() {
         List<Car> carsAfterRacing = cars.getAttemptResult();
-        assertAll(
-                () -> assertThat(carsAfterRacing.get(0).getName()).isEqualTo("Chris"),
-                () -> assertThat(carsAfterRacing.get(1).getName()).isEqualTo("Henry"),
-                () -> assertThat(carsAfterRacing.get(2).getName()).isEqualTo("Paul"),
-                () -> assertThat(carsAfterRacing.get(0).getPosition()).isEqualTo(1),
-                () -> assertThat(carsAfterRacing.get(1).getPosition()).isEqualTo(0),
-                () -> assertThat(carsAfterRacing.get(2).getPosition()).isEqualTo(1)
-        );
+        assertThat(carsAfterRacing)
+                .extracting(Car::getName, Car::getPosition)
+                .containsOnly(tuple("Chris", 1),
+                        tuple("Henry", 0),
+                        tuple("Paul", 1)
+                );
     }
 
     @Test
     @DisplayName("위치 값이 가장 큰 자동차(들)을 반환한다.")
     void getWinners() {
         List<Car> winners = cars.getWinners();
-        assertAll(
-                () -> assertThat(winners.size()).isEqualTo(2),
-                () -> assertThat(winners.get(0).getName()).isEqualTo("Chris"),
-                () -> assertThat(winners.get(1).getName()).isEqualTo("Paul")
-        );
+        assertThat(winners).extracting("name").containsOnly("Chris", "Paul");
     }
 
     @Test
