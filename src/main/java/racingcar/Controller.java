078bb71d5f2622e179f487f2d46bd0c59f5f0707
YJ <soltwood6862@naver.com>
2021-12-07 23:20:36 +0900

    feat(Controller.java): 게임 컨트롤러 클래스 구현

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import racingcar.Cars;
+
+import java.util.List;
+
+public class Controller {
+
+    private final Cars cars;
+
+    private Controller(Cars carGroups) {
+        this.cars = carGroups;
+    }
+
+    public static Controller of(List<String> carNames) {
+        return new Controller(Cars.of(carNames));
+    }
+
+    public void start(int tryNumber) {
+        System.out.println("\n실행 결과");
+        tryGame(tryNumber);
+        printWinner();
+    }
+
+    private void tryGame(int tryCount) {
+        for (int i = 0; i < tryCount; i++) {
+            cars.playRound();
+            System.out.println();
+        }
+    }
+
+    private void printWinner() {
+        System.out.print("최종 우승자 : " + cars.getWinner());
+    }
+}
