343dbb1c314d07011292e9494ba3424433bdab2b
maprk <zldndlzl@naver.com>
2021-12-07 17:30:55 +0900

    feat: main 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.game.manager.GameManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        GameManager.run();
     }
+
 }
