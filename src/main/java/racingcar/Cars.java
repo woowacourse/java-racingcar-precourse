10b291231eee5d4a4e231b259c2d20c30cc75c1d
YJ <soltwood6862@naver.com>
2021-12-07 23:18:53 +0900

    feat(Cars.java): 자동차목록 클래스 구현

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars of(List<String> carNames) {
+        List<Car> cars = carNames.stream()
+                .map(Car::new)
+                .collect(toList());
+        return new Cars(cars);
+    }
+
+    public void playRound() {
+        for (Car car : cars) {
+            car.moveOrStay(Randoms.pickNumberInRange(0, 9));
+        }
+    }
+
+    public String getWinner() {
+        int winnerPosition = findMaxPosition();
+        return cars.stream()
+                .filter(car -> car.isMaxPosition(winnerPosition))
+                .map(Car::getName)
+                .collect(joining(", "));
+    }
+
+    private int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+}
