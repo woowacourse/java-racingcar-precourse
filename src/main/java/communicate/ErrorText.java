511191e716585c44e2a435ff8c989ee246d4d611
usuyn <usuyn000@gmail.com>
2021-12-07 21:32:58 +0900

    modify: change construction of text and error messages

--- src/main/java/communicate/ErrorText.java
+++ src/main/java/communicate/ErrorText.java
@@ -1,11 +1,12 @@
 package communicate;
 
 public class ErrorText {
-    public static final String ERROR_CAR_NUMBER = "[ERROR] 쉼표(,)로 구분된 2개 이상의 자동차 이름이 입력되어야 합니다.";
-    public static final String ERROR_NAME_ONLY_SPACE = "[ERROR] 공백으로만 이루어진 이름이 존재합니다.";
-    public static final String ERROR_NAME_SPACE_POSITION = "[ERROR] 각 자동차 이름 맨 앞, 뒤에는 공백이 존재할 수 없습니다.";
-    public static final String ERROR_NAME_LENGTH = "[ERROR] 각 자동차 이름은 1자 이상, 5자 이하여야 합니다.";
-    public static final String ERROR_NAME_OVERLAP = "[ERROR] 입력된 자동차 이름 중 중복이 존재합니다.";
-    public static final String ERROR_ATTEMPT_NOT_NUMBER = "[ERROR] 시도 횟수는 자연수 1개만 입력되어야 합니다.";
-    public static final String ERROR_ATTEMPT_NUMBER_RANGE = "[ERROR] 시도 횟수는 1 이상이어야 합니다.";
+    public static final String ERROR = "[ERROR] ";
+    public static final String ERROR_CAR_NUMBER = ERROR + "쉼표(,)로 구분된 2개 이상의 자동차 이름이 입력되어야 합니다.";
+    public static final String ERROR_NAME_ONLY_SPACE = ERROR + "공백으로만 이루어진 이름이 존재합니다.";
+    public static final String ERROR_NAME_SPACE_POSITION = ERROR + "각 자동차 이름 맨 앞, 뒤에는 공백이 존재할 수 없습니다.";
+    public static final String ERROR_NAME_LENGTH = ERROR + "각 자동차 이름은 1자 이상, 5자 이하여야 합니다.";
+    public static final String ERROR_NAME_OVERLAP = ERROR + "입력된 자동차 이름 중 중복이 존재합니다.";
+    public static final String ERROR_ATTEMPT_NOT_NUMBER = ERROR + "시도 횟수는 자연수 1개만 입력되어야 합니다.";
+    public static final String ERROR_ATTEMPT_NUMBER_RANGE = ERROR + "시도 횟수는 1 이상이어야 합니다.";
 }
