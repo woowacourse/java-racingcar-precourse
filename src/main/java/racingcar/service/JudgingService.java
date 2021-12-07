58cc1179b0885893d951338e753480ca8606dc80
jojogreen91 <iawbg13@gmail.com>
2021-12-07 17:11:29 +0900

    refactor(JudgingService): add getMaxDriveLength method
    - 자동차들 중 가장 멀리 이동한 자동차의 거리를 구하는 기능을 refactoring 하여 method 를 분리하였다.

--- src/main/java/racingcar/service/JudgingService.java
+++ src/main/java/racingcar/service/JudgingService.java
@@ -9,20 +9,26 @@ public class JudgingService {
 	public ArrayList<String> judge(ArrayList<Car> carList) {
 		ArrayList<String> winnerList = new ArrayList<>();
 
+		int maxDriveLength = getMaxDriveLength(carList);
+
+		for (int i = 0; i < carList.size(); i++) {
+			Car car = carList.get(i);
+
+			if (car.showPosition().length() == maxDriveLength) {
+				winnerList.add(car.getName());
+			}
+		}
+
+		return winnerList;
+	}
+
+	private int getMaxDriveLength(ArrayList<Car> carList) {
 		int max = carList.stream()
 			.map(car -> car.showPosition().length())
 			.mapToInt(length -> Integer.valueOf(length))
 			.max()
 			.getAsInt();
 
-		for (int i = 0; i < carList.size(); i++) {
-			Car car = carList.get(i);
-
-			if (car.showPosition().length() == max) {
-				winnerList.add(car.getName());
-			}
-		}
-
-		return winnerList;
+		return max;
 	}
 }
