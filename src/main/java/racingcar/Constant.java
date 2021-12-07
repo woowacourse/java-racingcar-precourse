42bf563b94733e23ea30e4a9534e4801d50a2950
iamsseaa <homingsea@gmail.com>
2021-12-07 10:03:18 +0900

    feat: 경기 진행 관련 문구 및 에러 문구 상수 클래스에 선언

--- src/main/java/racingcar/Constant.java
+++ src/main/java/racingcar/Constant.java
@@ -1,4 +1,16 @@
 package racingcar;
 
 public class Constant {
+    public static final String INPUT_MESSAGE_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_MESSAGE_TRY_TIMES = "시도할 회수는 몇회인가요?";
+    public static final String RACE_PROCESS = "실행 결과";
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+
+    public static final String ERROR_PREFIX = "[ERROR]";
+    public static final String ERROR_EMPTY_NAME = "이름에 공백이 있을 수 없습니다.";
+    public static final String ERROR_OVERLAP_NAME = "이름은 중복될 수 없습니다.";
+    public static final String ERROR_RIGHT_LENGTH_NAME = "1~5자 사이의 이름만 가능합니다.";
+
+    public static final String ERROR_NOT_NUMBER_TIMES = "시도 횟수는 정수여야 합니다.";
+    public static final String ERROR_POSITIVE_INTEGER = "시도 횟수는 양의 정수여야 합니다.";
 }
