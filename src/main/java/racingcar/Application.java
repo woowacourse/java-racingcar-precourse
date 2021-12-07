aea266eb23c548ed9156ba255045db2d829c098f
Your Name Here <learningssik@gmail.com>
2021-12-07 17:55:58 +0900

    feat: RunGame의 run을 통해 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.rungame.RunGame;
+
 public class Application {
     public static void main(String[] args) {
+        RunGame.run();
         // TODO 구현 진행
     }
 }
