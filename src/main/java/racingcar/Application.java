50a859bc7264cc89fe7a84d119ee42f95e964d98
iamsseaa <homingsea@gmail.com>
2021-12-07 13:27:50 +0900

    feat: Application 클래스 메서드 호출 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,49 +4,11 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
         User user = new User();
 
-        while (true) {
-            try {
-                System.out.println(Constant.INPUT_MESSAGE_CAR_NAME);
-                user.enterCarName();
-                user.toNameArray();
-                CarNameException.validateCarName(user.getCarName());
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-        while (true) {
-            try {
-                System.out.println(Constant.INPUT_MESSAGE_TRY_TIMES);
-                user.enterTimes();
-                RoundTimesException.validateRoundTimes(user.getInputTimes());
-                user.toIntegerTimes();
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        System.out.println();
-        System.out.println(Constant.RACE_PROCESS);
-
-        Game game = new Game();
-
-        List<Car> cars = game.makeCarObject(user.getCarName());
-
-        for (int i = 0; i < user.getTimes(); i++) {
-            game.runCarRacingOneRound(cars);
-            System.out.println();
-        }
-
-        int frontPosition = game.getFrontPosition(cars);
-
-        List<String> winner = game.getWinner(cars, frontPosition);
-
-        System.out.print(Constant.FINAL_WINNER + String.join(Constant.FINAL_WINNER_JOIN_DELIMITER, winner));
-
+        Race.generateCarName(user);
+        Race.generateTryTimes(user);
+        Race.printRaceProcess(user);
+        Race.printFinalWinner(user);
     }
 }
