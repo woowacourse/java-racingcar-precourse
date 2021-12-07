e04618eb08dd4564ceae7023184cfc13d051aafa
harinn1014 <gkfla7555@gmail.com>
2021-12-07 18:26:07 +0900

    feat(InputController): add validation method about tryNum
    <description>
    tryNum 시도 횟수에 대해 유효성 검사를 하는 validation 함수 추가
    - validateType_tryNum(): 정수가 아닌 경우 exception 발생
    - validateRange_tryNum(): 값이 음수인 경우 exception 발생

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
+import racingcar.utils.Constant;
+import racingcar.utils.ExceptionMessage;
 import racingcar.view.InputView;
 
 public class InputController {
@@ -17,5 +19,20 @@ public class InputController {
 			}
 		}
 		return carList;
+	}
+
+	public int validateType_tryNum() {
+		try {
+			int tryNum = Integer.parseInt(InputView.getTryNum());
+			return tryNum;
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(ExceptionMessage.TRY_TYPE_FAIL);
+		}
+	}
+
+	public void validateRange_tryNum(int tryNum) {
+		if (tryNum < Constant.TRY_NUM_RANGE_LIMIT) {
+			throw new IllegalArgumentException(ExceptionMessage.TRY_RANGE_FAIL);
+		}
 	}
 }
