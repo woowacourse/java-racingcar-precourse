48a7100ac6328b35f6624b6a7bd7f2080be22194
harinn1014 <gkfla7555@gmail.com>
2021-12-07 17:50:34 +0900

    feat(Cars): add raceCars() method and add validation code to
    <description>
    라운드 하나동안의 car list의 자동차들이 경주하는 동작을 담은 함수
    
    라운드 하나동안 자동차는
    1) car.move: 경주한다.
    2) car.printState(): 경주를 끝내고 해당 자동차의 현재 상태를 출력한다.
    리스트의 자동차들이 움직이고나면 "\n"을 출력한다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -7,11 +7,20 @@ public class Cars {
 	private final List<Car> carList = new ArrayList<>();
 
 	public Cars(String[] array_carNames) {
+		ValidatorName.duplicate(array_carNames);
 		for (String name : array_carNames) {
 			ValidatorName.isNull(name);
 			ValidatorName.outOfLength(name);
 			this.carList.add(new Car(name));
 		}
+	}
+
+	public void raceCars() {
+		carList.forEach(car -> {
+			car.move();
+			car.printState();
+		});
+		System.out.println();
 	}
 
 }
