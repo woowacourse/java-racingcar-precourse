6a5dc18128d190e65942cda0ef8a7cba61dfa918
YJ <soltwood6862@naver.com>
2021-12-07 23:24:56 +0900

    feat(InputView.java): input 클래스 구현 - 자동차 이름 목록, 게임횟수

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static List<String> requestCarInput() {
+        List<String> result;
+        String input;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        input = Console.readLine();
+        result = input;
+        
+        return result;
+    }
+
+    public static int requestTryCount() {
+        String input;
+        System.out.println("시도할 회수는 몇회인가요?");
+        input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+}
