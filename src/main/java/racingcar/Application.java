38ae74f60f79e37443b43e112b2f9a88979b12e6
Hoesung Jung <jrps2212@gmail.com>
2021-12-07 20:41:01 +0900

    getNames 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,42 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 public class Application {
+
+    public static void checkNames(String[] names){
+        for (String name: names){
+            if (name.length() > 5){
+                throw new IllegalArgumentException("[ERROR] 자동차 이름은 5 이내여야 한다.");
+            }
+        }
+    }
+
+    public static String[] getNames(){
+
+        while (true){
+            try{
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                String inputNames = Console.readLine();
+
+                String[] names = inputNames.split(",");
+
+                checkNames(names);
+
+                return names;
+
+            }catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 자동차 이름은 5 이내여야 한다.");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // TODO 구현 진행
+        String[] names = getNames();
+
     }
 }
