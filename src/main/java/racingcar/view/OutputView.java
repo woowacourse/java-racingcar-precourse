c753bdfd565ad89032eb8587ba7fcabba77ca6ca
harinn1014 <gkfla7555@gmail.com>
2021-12-07 18:34:51 +0900

    feat(OutputView): add print method(startMessage, winners)
    <description>
    - printRacingStart(): 경주가 시작되고 결과를 출력함을 알리는 메시지를 print하는 함수
    - printWinner(): 우승자 리스트를 입력받아서 출력하는 함수

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,26 @@
 package racingcar.view;
 
+import java.util.List;
+
+import racingcar.domain.Car;
+import racingcar.utils.Constant;
+
 public class OutputView {
+
+	public static void printRacingStart() {
+		System.out.println(Constant.EXECUTION_RESULT);
+	}
+
+	public static void printWinner(List<Car> winnerList) {
+		StringBuilder sb= new StringBuilder();
+		sb.append(Constant.LAST_WINNER_IS);
+		for(int i=0; i<winnerList.size(); i++) {
+			if(i!=0) {
+				sb.append(Constant.DELIMITER_CAR_NAME);
+			}
+			sb.append(winnerList.get(i).getName());
+		}
+		System.out.println(sb.toString());
+	}
 
 }
