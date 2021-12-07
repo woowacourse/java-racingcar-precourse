6cbdf212cc871013030c30ee801dbbbc6e830f13
summerlunaa <rhgus1085@gmail.com>
2021-12-07 10:16:28 +0900

    feat(Input): 자동차 이름의 공백을 제거하는 trimCarNames 메서드 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -12,7 +12,7 @@ public class Input {
 		do {
 			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 			carNames = Console.readLine().split(",");
-			// trimCarNames(carNames);
+			trimCarNames(carNames);
 		} while (!checkCarNames(carNames));
 
 		return carNames;
@@ -22,10 +22,16 @@ public class Input {
 		String number;
 		do {
 			System.out.println("시도할 회수는 몇회인가요?");
-			number = Console.readLine();
+			number = Console.readLine().trim();
 		} while (!checkTryNumber(number));
 
 		return Integer.parseInt(number);
+	}
+
+	private static void trimCarNames(String[] carNames) {
+		for (int i = 0; i < carNames.length; i++) {
+			carNames[i] = carNames[i].trim();
+		}
 	}
 
 	private static boolean checkCarNames(String[] carNames) {
