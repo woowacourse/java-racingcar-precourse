8e694b2b64c73308015327904f123efb79260be1
kanamycine <jinyeong5320@gmail.com>
2021-12-07 20:17:45 +0900

    refactor(RandomGenerator) : RandomGenerator 삭제

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,19 +1,21 @@
 package racingcar.domain;
 
-import racingcar.utils.RandomGenerator;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 	private final String carName;
 	private int position = 0;
 
 	private final int PROCEED_NUM_MIN = 4;
+	private static final int RANDOM_NUM_MIN = 0;
+	private static final int RANDOM_NUM_MAX = 9;
 
 	public Car(String carName) {
 		this.carName = carName;
 	}
 
 	public void proceedOrPause() {
-		if (RandomGenerator.getRandomNum() >= PROCEED_NUM_MIN) {
+		if (Randoms.pickNumberInRange(RANDOM_NUM_MIN, RANDOM_NUM_MAX) >= PROCEED_NUM_MIN) {
 			this.position++;
 		}
 	}
