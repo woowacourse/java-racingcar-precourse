40f8fabc92c0f51a1b097a61fe4bd8c5c12ed536
cjlee38 <gptpem38@gmail.com>
2021-12-07 17:44:09 +0900

    test: 자동차의 순위를 비교하는 테스트 추가
    여러대의 자동차가 전진한 후, 이를 비교하는 테스트 추가

--- src/test/java/racingcar/domain/car/CarSetTest.java
+++ src/test/java/racingcar/domain/car/CarSetTest.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import racingcar.domain.movement.InputMovementStrategy;
+
 public class CarSetTest {
 
 	@Test
@@ -32,4 +34,24 @@ public class CarSetTest {
 		// then
 		assertThrows(IllegalArgumentException.class, () -> new CarSet(cars));
 	}
+
+	@Test
+	@DisplayName("자동차의 위치를 기반으로 비교할 수 있다.")
+	public void testCarPositionComparable() throws Exception {
+		// given
+		Car carA = new Car("A");
+		Car carB = new Car("B");
+		Car carC = new Car("C");
+		carA.move(new InputMovementStrategy(1));
+		carB.move(new InputMovementStrategy(2));
+		carC.move(new InputMovementStrategy(3));
+		List<Car> cars = new ArrayList<>(Arrays.asList(carA, carB, carC));
+		// when
+		CarSet carSet = new CarSet(cars);
+		// then
+		List<Car> winners = carSet.getWinners();
+		assertEquals(1, winners.size());
+		assertSame(winners.get(0), carC);
+	}
+
 }
