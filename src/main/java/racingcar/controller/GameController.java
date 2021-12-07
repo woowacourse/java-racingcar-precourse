f7ad611276ad4dbda094302ea7bd50245da6e733
harinn1014 <gkfla7555@gmail.com>
2021-12-07 18:48:58 +0900

    feat(GameController): add roundPlay method
    <description>
    - create play() method
    : 생성만 함
    
    - play()에 필요한 roundPlay method 추가
    : tryNum만큼 반복하여 경주를 하며, 우승자를 출력한다.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,19 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
+import racingcar.view.OutputView;
 
 public class GameController {
+
+	public void play() {
+
+	}
+
+	public void roundPlay(Cars carList, int tryNum) {
+		for (int i = 0; i < tryNum; i++) {
+			carList.raceCars();
+		}
+		OutputView.printWinner(carList.findWinners());
+	}
 
 }
