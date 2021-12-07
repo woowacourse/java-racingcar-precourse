0e8162d56553fa0c5ec72cc031087e33837f2a49
iamsseaa <homingsea@gmail.com>
2021-12-07 13:14:27 +0900

    feat: 차 이름을 입력받는 메서드 생성

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,4 +1,18 @@
 package racingcar;
 
 public class Race {
+
+    public static void generateCarName(User user) {
+        while (true) {
+            try {
+                System.out.println(Constant.INPUT_MESSAGE_CAR_NAME);
+                user.enterCarName();
+                user.toNameArray();
+                CarNameException.validateCarName(user.getCarName());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
