e9ce84f0f0f50a5a88a164b69956d2a52957f518
iamsseaa <homingsea@gmail.com>
2021-12-07 10:23:03 +0900

    chore: 상수 클래스로 코드 대체

--- src/main/java/racingcar/RoundTimesException.java
+++ src/main/java/racingcar/RoundTimesException.java
@@ -8,13 +8,13 @@ public class RoundTimesException {
 
     public static void isNumber(String times) {
         if (!times.matches("-?\\d+")) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 정수여야 합니다.");
+            throw new IllegalArgumentException(Constant.ERROR_NOT_NUMBER_TIMES);
         }
     }
 
     public static void isPositiveInteger(String times) {
         if (Integer.parseInt(times) <= 0) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 양의 정수여야 합니다.");
+            throw new IllegalArgumentException(Constant.ERROR_POSITIVE_INTEGER);
         }
     }
 }
