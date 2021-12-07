6e98045d7f0fb646a006dbec397ee898749b39c0
Hoesung Jung <jrps2212@gmail.com>
2021-12-07 20:41:57 +0900

    getTimes 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -34,9 +34,45 @@ public class Application {
         }
     }
 
+    public static int checkTimes(String inputTimes){
+        String patternNumberOnly = "^[0-9]*$";
+        boolean regex = Pattern.matches(patternNumberOnly, inputTimes);
+        int times = Integer.parseInt(inputTimes);
+
+        if (!regex || (inputTimes.isEmpty())) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 한다.");
+        }
+
+        return times;
+    }
+
+    public static int getTimes(){
+
+        int times;
+
+        while (true) {
+
+            try{
+
+                System.out.println("시도할 횟수는 몇회인가요?");
+                String inputTimes = Console.readLine();
+
+                times = checkTimes(inputTimes);
+
+                return times;
+
+            }catch(IllegalArgumentException e){
+                System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.");
+            }
+
+        }
+
+    }
+
     public static void main(String[] args) {
         // TODO 구현 진행
         String[] names = getNames();
 
+        int times = getTimes();
     }
 }
