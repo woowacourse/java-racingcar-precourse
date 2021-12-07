1166e305a6c2495899ca220b8151486526035866
cjlee38 <gptpem38@gmail.com>
2021-12-07 17:33:47 +0900

    test: 자동차가 움직이는 기능 테스트
    자동차가 strategy에 따라 움직이는 기능 테스트 추가

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,5 +1,24 @@
 package racingcar.domain.car;
 
-class CarTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import racingcar.domain.movement.InputMovementStrategy;
+import racingcar.domain.movement.MovementStrategy;
+
+class CarTest {
+	@Test
+	@DisplayName("자동차의 위치가 움직일 수 있다.")
+	public void testCarMoveForward() throws Exception {
+		// given
+		int distance = 1;
+		Car car = new Car("myCar");
+		MovementStrategy strategy = new InputMovementStrategy(distance);
+		// when
+		car.move(strategy);
+		// then
+		assertEquals(car.toString(), "myCar : -");
+	}
 }
