91a82fc7dfba11bcb3f9e3f37aed18201ae3a897
harinn1014 <gkfla7555@gmail.com>
2021-12-07 16:53:41 +0900

    feat(ValidatorName): add duplicate method
    <description>
    입력받은 자동차 이름 리스트에서 중복값이 있는지 확인하는 함수 추가
    중복값이 있을 경우 메시지와 함께 exception을 날린다.

--- src/main/java/racingcar/domain/ValidatorName.java
+++ src/main/java/racingcar/domain/ValidatorName.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Arrays;
+
 import racingcar.utils.Constant;
 import racingcar.utils.ExceptionMessage;
 
@@ -14,6 +16,13 @@ public class ValidatorName {
 	public static void outOfLength(String name) {
 		if (name.length() > Constant.CAR_NAME_LENGTH) {
 			throw new IllegalArgumentException(ExceptionMessage.CAR_OUTOF_RANGE);
+		}
+	}
+
+	public static void duplicate(String[] array_carNames) {
+		long distinctSize = Arrays.stream(array_carNames).distinct().count();
+		if (distinctSize < array_carNames.length) {
+			throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_DUPLICATE);
 		}
 	}
 
