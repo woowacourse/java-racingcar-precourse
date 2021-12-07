7c27d2eabfb63499442903a6600cfef21becad25
dls4585 <dls4585@naver.com>
2021-12-07 17:20:28 +0900

    fix: ERROR_MESSAGE 추가

--- src/main/java/service/IntInputValidatorImpl.java
+++ src/main/java/service/IntInputValidatorImpl.java
@@ -3,11 +3,13 @@ package service;
 public class IntInputValidatorImpl implements IntInputValidator {
     private static final String ERROR_NOT_IN_RANGE = "시도 회수는 자연수여야 합니다.";
     private static final String ERROR_NOT_A_NUMBER = "시도 회수는 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     @Override
     public void isInRange(int number) {
 
         if (number <= 0) {
-            throw new IllegalArgumentException(ERROR_NOT_IN_RANGE);
+            throw new IllegalArgumentException(ERROR_MESSAGE + " " + ERROR_NOT_IN_RANGE);
         }
     }
 
@@ -16,7 +18,7 @@ public class IntInputValidatorImpl implements IntInputValidator {
         int length = toCheck.length();
         for (int i = 0; i < length; i++) {
             if (!Character.isDigit(toCheck.charAt(i))) {
-                throw new IllegalArgumentException(ERROR_NOT_A_NUMBER);
+                throw new IllegalArgumentException(ERROR_MESSAGE + " " + ERROR_NOT_A_NUMBER);
             }
         }
     }
--- src/main/java/service/StringInputValidatorImpl.java
+++ src/main/java/service/StringInputValidatorImpl.java
@@ -2,10 +2,12 @@ package service;
 
 public class StringInputValidatorImpl implements StringInputValidator {
     private final static String ERROR_NAME_LENGTH = "자동차 이름은 5자 이하여야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     @Override
     public void checkLength(String string) {
         if (string.length() > 5) {
-            throw new IllegalArgumentException(string + " - " + ERROR_NAME_LENGTH);
+            throw new IllegalArgumentException(ERROR_MESSAGE + " " + string + " - " + ERROR_NAME_LENGTH);
         }
     }
 }
