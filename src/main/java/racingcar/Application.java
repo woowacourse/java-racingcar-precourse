bd8aacca209ab6ce79dc2de7b5062dafeb88b4cb
wooseok <diddmlvkf@gmail.com>
2021-12-07 15:36:37 +0900

    feat(InputController) : 자동차 이름을 입력받아 참가할 자동차 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import java.util.List;
+
+import racingcar.controller.InputController;
+import racingcar.model.Car;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		// TODO 구현 진행
+		List<Car> cars = InputController.createCars();
+	}
 }
--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -1,8 +1,38 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+import racingcar.view.InputView;
+
 public class InputController {
+
+	private static final String NAME_DELIMITER = ",";
+
+	public static List<Car> createCars() {
+		try {
+			InputView.carNameUI();
+			String input = Console.readLine();
+			validateContainDelimiter(input);
+			String[] names = Console.readLine().split(NAME_DELIMITER);
+			return toCars(names);
+		} catch (IllegalArgumentException e) {
+			return createCars();
+		}
+	}
+
+	public static List<Car> toCars(String[] names) throws IllegalArgumentException {
+		List<Car> cars = new ArrayList<>();
+		for (String name : names) {
+			cars.add(new Car(name));
+		}
+		return cars;
+	}
+
 	public static void validateContainDelimiter(String input) throws IllegalArgumentException {
-		if (!input.contains(",")) {
+		if (!input.contains(NAME_DELIMITER)) {
 			throw new IllegalArgumentException();
 		}
 	}
