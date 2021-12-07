73153a2b8c314772708720345c92b68578aedfc0
Your Name Here <learningssik@gmail.com>
2021-12-07 18:56:29 +0900

    feat: 사용자가 입력 하는 부분, 예외처리

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,40 @@
+package racingcar.view;
+
+import racingcar.util.Constants;
+
+public class InputView {
+    public static String[] splitString(String input) {
+        String[] result = input.split(",");
+        isValidLength(result);
+        return result;
+    }
+
+    private static void isValidLength(String[] carNames) {
+        for (String name : carNames) {
+            if (name.length() > Constants.CAR_NAME_MAX_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static int parseInt(String input) {
+        isInteger(input);
+        int result = Integer.parseInt(input);
+        isPositive(result);
+        return result;
+    }
+
+    private static void isPositive(int input) {
+        if (input < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
