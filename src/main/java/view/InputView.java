763ddc16fc03a5578914a3580a74ff8af8d19d64
Huicheol Moon <huicheol7@gmail.com>
2021-12-07 23:14:06 +0900

    feat: 이동을 시도할 횟수를 묻는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	private static final String INPUT_CAR_NAMES_PHRASE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+	private static final String INPUT_TRIAL_NUMBER_PHRASE = "시도할 회수는 몇회인가요?\n";
 
 	private String getInput() {
 		String carNamesInput = Console.readLine();
