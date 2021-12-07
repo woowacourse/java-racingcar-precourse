a9e693c16dc6e55c9202a5e944305bd06c20e844
YJ <soltwood6862@naver.com>
2021-12-07 23:25:56 +0900

    feat(Constant.java): constant 클래스 구현

---
+++ src/main/java/racingcar/Constant.java
@@ -0,0 +1,20 @@
+package racingcar.utils;
+
+public class Constant {
+
+    // String Constant
+    public static final String BAR = "-";
+    public static final String JOIN_WITH = ", ";
+    public static final String SPLIT_BY = ",";
+    public static final String REQUEST_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String MINIMUM_TRY_NUMBER_ERROR_MESSAGE = "[ERROR] 시도횟수는 0회 이상 입력하세요.";
+    public static final String DUPLICATE_NAME_ERROR_MESSAGE = "[ERROR] 이름이 중복됐습니다.";
+    public static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 이름은 1자 이상 5자 이하로 입력하세요.";
+
+    // int Constant
+    public static final int ZERO_NUMBER = 0;
+    public static final int MOVE_JUDGE = 4;
+    public static final int MAXIMUM_NAME_LENGTH = 5;
+    public static final int MAXIMUM_NUMBER = 9;
+}
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         input = Console.readLine();
         result = input;
-        
+
         return result;
     }
 
