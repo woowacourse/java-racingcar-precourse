3d334607cdcf8cc32f78cfaedb0a167c305563f7
harinn1014 <gkfla7555@gmail.com>
2021-12-07 18:19:59 +0900

    feat(InputController): add getInput_Car() method
    <description>
    자동차를 입력받아 Cars carList를 만든다.
    잘못된 입력시에 예외처리를 해주고 재입력 받는다.
    Cars(자동차 리스트)를 반환

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -1,5 +1,21 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
+import racingcar.view.InputView;
+
 public class InputController {
 
+	public Cars getInput_Car() {
+		Cars carList;
+		while (true) {
+			String[] carName = InputView.getCarNames();
+			try {
+				carList = new Cars(carName);
+				break;
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return carList;
+	}
 }
