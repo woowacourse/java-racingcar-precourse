e64cf066c3aba627759af05ee3be5dab0a578319
iamsseaa <homingsea@gmail.com>
2021-12-07 10:29:05 +0900

    chore: 에러 조건 상수 선언 및 코드 수정

--- src/main/java/racingcar/CarNameException.java
+++ src/main/java/racingcar/CarNameException.java
@@ -3,6 +3,10 @@ package racingcar;
 import java.util.*;
 
 public class CarNameException {
+    public static final String NAME_ONE_SPACE = " ";
+    public static final int NAME_LENGTH_LOWEST = 1;
+    public static final int NAME_LENGTH_HIGHEST = 5;
+
     public static void validateCarName(String[] allCarName) {
         isEmptyName(allCarName);
         isOverlap(allCarName);
@@ -12,7 +16,7 @@ public class CarNameException {
 
     public static void isEmptyName(String[] allCarName) {
         for (String name : allCarName) {
-            if (name.contains(" ")) {
+            if (name.contains(NAME_ONE_SPACE)) {
                 throw new IllegalArgumentException(Constant.ERROR_EMPTY_NAME);
             }
         }
@@ -29,7 +33,7 @@ public class CarNameException {
 
     public static void isRightLength(String[] allCarName) {
         for (String name : allCarName) {
-            if (name.length() < 1 || name.length() > 5) {
+            if (name.length() < NAME_LENGTH_LOWEST || name.length() > NAME_LENGTH_HIGHEST) {
                 throw new IllegalArgumentException(Constant.ERROR_RIGHT_LENGTH_NAME);
             }
         }
