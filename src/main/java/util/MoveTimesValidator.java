6c765ce62b4cb95ccc27b37ac91186b8acb001ea
SuyeonChoi <tndus970120@gmail.com>
2021-12-07 22:27:18 +0900

    feat(MoveTimesValidator): validate if input move times is positive

--- src/main/java/util/MoveTimesValidator.java
+++ src/main/java/util/MoveTimesValidator.java
@@ -6,12 +6,19 @@ public class MoveTimesValidator {
 
 	public static void validate(String moveTimes) {
 		validateMoveTimes(moveTimes);
+		validatePositiveNumber(moveTimes);
 	}
 
 	private static void validateMoveTimes(String moveTimes) {
 		try {
 			Integer.parseInt(moveTimes);
 		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static void validatePositiveNumber(String moveTimes) {
+		if (Integer.parseInt(moveTimes) < 0) {
 			throw new IllegalArgumentException();
 		}
 	}
