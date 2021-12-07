48a7f377f5c2236bc839a7f3026700f6888c0ea4
johrae99 <70756680+johrae99@users.noreply.github.com>
2021-12-07 15:54:53 +0900

    refactor : 입력 검증을 위한 InputValidator 클래스 분리

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -18,13 +18,13 @@ public class InputManager {
 
 	public void scanCarsList() {
 		boolean carsScan = true;
-		System.out.println(CAR_NAME_INPUT);
+		System.out.println(Constant.CAR_NAME_INPUT);
 		while (carsScan) {
 			try {
 				String carsInput = Console.readLine();
-				isValidCarsInput(carsInput);
+				InputValidator.isValidCarsInput(carsInput);
 				carsList = carsInput.split(",");
-				isValidCarsList();
+				InputValidator.isValidCarsList(carsList);
 				carsScan = false;
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
@@ -38,63 +38,13 @@ public class InputManager {
 		while (numberScan) {
 			try {
 				String numberOfMove = Console.readLine();
-				isValidNumber(numberOfMove);
+				InputValidator.isValidNumber(numberOfMove);
 				this.numberOfMove = toInt(numberOfMove);
 				numberScan = false;
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
 			}
 		}
-	}
-
-	public void isValidCarsInput(String carsInput) throws IllegalArgumentException {
-		if (carsInput.length() == 0) {
-			throw new IllegalArgumentException(CAR_NAME_ERROR);
-		}
-	}
-
-	public void isValidCarsList() throws IllegalArgumentException {
-		if (!checkEmptyList(carsList) || !checkCarNameLength(carsList) || !checkCarsListLength(carsList)) {
-			throw new IllegalArgumentException(CAR_NAME_ERROR);
-		}
-	}
-
-	public boolean checkEmptyList(String[] carsList) {
-		if (carsList.length == 0) {
-			return false;
-		}
-		return true;
-	}
-
-	public boolean checkCarNameLength(String[] carsList) {
-		for (String car : carsList) {
-			if (car.length() > MAXIMUM_NAME_LENGTH) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public boolean checkCarsListLength(String[] carsList) {
-		if (carsList.length == 1) {
-			return false;
-		}
-		return true;
-	}
-
-	public void isValidNumber(String number) throws IllegalArgumentException {
-		if (number.length() == 0 || !isNumber(number) || number.equals(TRY_NUMBER_ZERO)) {
-			throw new IllegalArgumentException(TRY_NUMBER_ERROR);
-		}
-	}
-
-	public boolean isNumber(String number) {
-		for (int i = 0; i < number.length(); i++) {
-			if (!Character.isDigit((number.charAt(i)))) {
-				return false;
-			}
-		}
-		return true;
 	}
 
 	public static int toInt(String number) {
---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,55 @@
+package racingcar;
+
+import static racingcar.Constant.*;
+
+public class InputValidator {
+	public static void isValidCarsInput(String carsInput) throws IllegalArgumentException {
+		if (carsInput.length() == 0) {
+			throw new IllegalArgumentException(CAR_NAME_ERROR);
+		}
+	}
+
+	public static void isValidCarsList(String[] carsList) throws IllegalArgumentException {
+		if (!checkEmptyList(carsList) || !checkCarNameLength(carsList) || !checkCarsListLength(carsList)) {
+			throw new IllegalArgumentException(CAR_NAME_ERROR);
+		}
+	}
+
+	public static boolean checkEmptyList(String[] carsList) {
+		if (carsList.length == 0) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean checkCarNameLength(String[] carsList) {
+		for (String car : carsList) {
+			if (car.length() > MAXIMUM_NAME_LENGTH) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean checkCarsListLength(String[] carsList) {
+		if (carsList.length == 1) {
+			return false;
+		}
+		return true;
+	}
+
+	public static void isValidNumber(String number) throws IllegalArgumentException {
+		if (number.length() == 0 || !isNumber(number) || number.equals(TRY_NUMBER_ZERO)) {
+			throw new IllegalArgumentException(TRY_NUMBER_ERROR);
+		}
+	}
+
+	public static boolean isNumber(String number) {
+		for (int i = 0; i < number.length(); i++) {
+			if (!Character.isDigit((number.charAt(i)))) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
