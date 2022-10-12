ff4d7022f16328bda0f292004b67798d8b76d871
swonny <wonny921@gmail.com>
2022-10-12 23:00:21 +0900

    feat: Game 클래스 구현
    1. 게임 초기 세팅 구현:
    게임이 시작되면 게임 클래스의 인스턴스가 생성되고, 게임 진행을 위한 정보가 초기화된다.
    
    2. 게임이 초기화되면 자동으로 Game 시작:
    인스턴스 생성과 동시에 시작되어야 하므로 초기화 블럭에서 playGame() 메서드를 호출한다.
    
    3. 사용자가 입력한 횟수만큼 Game 반복:
    - 한 게임이 진행될 때마다 난수를 생성하고, 조건에 맞게 앞으로 이동시킨다.
    - 한 게임이 진행될 때마다 진행 상황을 출력한다.
    
    4. 게임이 종료되면 최종 우승자 출력

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,31 +2,37 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+
 public class Game {
     { playGame(); }
     Player player;
     Car[] cars;
     int rounds;
+    ArrayList<String> winners;
 
     Game() { // Game의 생성자, 게임 시작을 위한 초기 세팅
         player = new Player();
         rounds = player.rounds;
         cars = new Car[player.cars.length]; // 자동차 n개의 배열을 위한 메모리 할당
-        for (String car : player.cars) { // n개의 car 인스턴스 생성
-            cars[i] = new Car(car);
+        for (int i = 0; i < player.cars.length; i++) { // n개의 car 인스턴스 생성
+            cars[i] = new Car(player.cars[i]);
         }
     }
 
     public void playGame() { // 입력 횟수만큼 반복
-        for (int i = 0; i < player.round; i++) {
+        for (int i = 0; i < rounds; i++) {
             startRound();
+            printProgress();
         }
+        getWinners();
     }
 
     public void startRound() {
-        for (int j = 0; j < player.cars.length; j++) {
-            boolean move = moveForward(Randoms.pickNumberInRange(0, 9));
-
+        for (int i = 0; i < cars.length; i++) {
+            if (moveForward(Randoms.pickNumberInRange(0, 9))) { // 난수 생성 및 4보다 큰지 판단
+                cars[i].moveForward();
+            }
         }
     }
 
@@ -38,15 +44,27 @@ public class Game {
         return false;
     }
 
-    public int getRandomNumbers() {
-
-    }
-
     public void printProgress() {
-
+        for (int i = 0; i < cars.length; i++) {
+            String name = cars[i].getName();
+            int position = cars[i].getPosition();
+            System.out.print(name + " : ");
+            for (int j = 0; j < position; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
     }
 
-    public void printResult() {
-
+    public void getWinners() {
+        int firstPositoin = 0;
+        for (int i = 0; i < cars.length; i++) {
+            int tmp = cars[i].getPosition();
+            if (tmp < firstPositoin) {
+                continue;
+            }
+            winners.add(cars[i].getName());
+            firstPositoin = tmp;
+        }
     }
 }
