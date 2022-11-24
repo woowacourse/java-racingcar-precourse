d674ed600020a8703ab992143e95a60623329e56
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-24 16:51:36 +0900

    <feat>(InputView.java): 로직 호출시 메시지 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,10 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String sendNamesOfParticipatingCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
     public String sendCountOfMoves() {
+        System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
     }
 }
