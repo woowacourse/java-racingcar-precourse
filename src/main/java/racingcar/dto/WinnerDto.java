59135aaeef5e68aaf83aed8f28001b1853d954ce
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-26 13:48:26 +0900

    <feat>(WinnerDto.java): 우승자의 이름을 전달할 DTO

---
+++ src/main/java/racingcar/dto/WinnerDto.java
@@ -0,0 +1,14 @@
+package racingcar.dto;
+
+public class WinnerDto {
+
+    private final String name;
+
+    public WinnerDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
