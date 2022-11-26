70ff2f7cfbcdb9a5bfcc0de1a101d40697531618
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-26 16:06:51 +0900

    <feat>(RacingGame.java): 우승자를 구하는 기능

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,10 +1,27 @@
 package racingcar.domain;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.RandomNumberGenerator;
+import racingcar.dto.WinnerDto;
 
 public class RacingGame {
 
     public void play(Cars cars) {
         cars.play(new RandomNumberGenerator());
+    }
+
+    public List<WinnerDto> getWinners(Cars cars) {
+        int maximum = cars.getCars()
+                .stream()
+                .map(Car::getPosition)
+                .max(Integer::compareTo)
+                .get();
+        return cars.getCars()
+                .stream()
+                .filter(car -> car.getPosition() == maximum)
+                .map(car -> new WinnerDto(car.getName()))
+                .collect(Collectors.toList());
     }
 }
