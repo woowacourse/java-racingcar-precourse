322e7d4161b3412d1bd98a61fa56f0ee736e8f21
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-26 12:45:23 +0900

    <feat>(InputView.java): string을 파싱해서 cars를 리턴하는 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,21 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
 
 public class InputView {
 
-    public String sendNamesOfParticipatingCars() {
+    public Cars sendNamesOfParticipatingCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String namesOfParticipatingCars = Console.readLine();
+        List<Car> cars = Arrays.stream(namesOfParticipatingCars.split(","))
+                .map(Car::new)
+                .collect(Collectors.toList());
+        return new Cars(cars);
     }
 
     public String sendCountOfMoves() {
