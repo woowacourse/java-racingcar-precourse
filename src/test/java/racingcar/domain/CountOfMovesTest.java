62c8d7a95801895403328edf22cd053fde0df765
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-27 15:22:03 +0900

    <test>(CountOfMovesTest.java): 테스트 추가

---
+++ src/test/java/racingcar/domain/CountOfMovesTest.java
@@ -0,0 +1,44 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CountOfMovesTest {
+
+    @DisplayName("CountOfMoves 생성시 인자값이 0이하면 예외를 반환한다.")
+    @Test
+    void test1() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new CountOfMoves(0));
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new CountOfMoves(-1));
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new CountOfMoves(-999));
+    }
+
+    @DisplayName("CountOfMoves 생성시 인자값이 1이상이면 정상 생성한다.")
+    @Test
+    void test2() {
+        assertThatNoException().isThrownBy(
+                () -> new CountOfMoves(1));
+    }
+
+    @DisplayName("목표 이동횟수와 현재이동 횟수가 같으면 true를 반환한다.")
+    @Test
+    void test3() {
+        CountOfMoves countOfMoves = new CountOfMoves(1);
+        countOfMoves.upperCurrentRound();
+        assertThat(countOfMoves.getGoalCount()).isTrue();
+    }
+
+    @DisplayName("목표 이동횟수와 현재이동 횟수가 다르면 false를 반환한다.")
+    @Test
+    void test4() {
+        CountOfMoves countOfMoves = new CountOfMoves(1);
+        assertThat(countOfMoves.getGoalCount()).isFalse();
+    }
+}
