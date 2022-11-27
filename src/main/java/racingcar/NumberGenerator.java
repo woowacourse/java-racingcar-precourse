3a27db6572ae85465efca138462e2f1398facd94
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-27 15:05:09 +0900

    <refactor>(NumberGenerator.java): 인터페이스화

---
+++ src/main/java/racingcar/NumberGenerator.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+@FunctionalInterface
+public interface NumberGenerator {
+
+    int generate();
+}
--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator{
 
     public int generate() {
         return Randoms.pickNumberInRange(1, 9);
