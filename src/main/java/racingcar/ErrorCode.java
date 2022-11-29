05d44dc3537ac0e6dfba99c551034d339ca013b6
yoon <beatus96@naver.com>
2022-11-30 07:32:57 +0900

    feat : 예외 발생 enum클래스 추가

---
+++ src/main/java/racingcar/ErrorCode.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+public enum ErrorCode {
+    NOT_VALID_CAR_COUNT("레이스 할 차의 개수가 2개 이상이어야 합니다."),
+    NOT_VALID_CAR_NAME("자동차의 이름은 5자 이하여야 합니다.");
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    private static final String ERROR_BEGIN = "[ERROR] ";
+    private final String errorMessage;
+
+    ErrorCode(String errorMessage) {
+        this.errorMessage = ERROR_BEGIN + errorMessage;
+    }
+
+    public void throwError() {
+        throw new IllegalArgumentException(this.getErrorMessage());
+    }
+
+}
