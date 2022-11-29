c57aceebff4ba660e7f7c234b2221e8fd61348e2
yoon <beatus96@naver.com>
2022-11-30 07:33:32 +0900

    feat : 자동차 이름 유효성 검사 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,21 @@
 package racingcar;
 
+import static racingcar.ErrorCode.NOT_VALID_CAR_NAME;
+
 public class Car {
+    private static final int nameLength = 5;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateCarName(name);
         this.name = name;
     }
 
+    public void validateCarName(String name) {
+        if(name.length()> nameLength) {
+            NOT_VALID_CAR_NAME.throwError();
+        }
+    }
     // 추가 기능 구현
 }
