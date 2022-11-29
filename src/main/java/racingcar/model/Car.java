8216e04a5eda8aa162fe3e0d6ba9f44590417b1b
SEUNHGHEE96 <dnffktmdgml@gmail.com>
2022-11-29 18:54:34 +0900

    feat: Car 객체 기능 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
