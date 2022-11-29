115b878c40030b0d54c114648877d185fc2b0134
yoon <beatus96@naver.com>
2022-11-30 07:32:36 +0900

    feat : 입력한 자동차 이름 자동차 객체로 변환 기능 추가

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,12 +1,24 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static racingcar.ErrorCode.NOT_VALID_CAR_COUNT;
 
 public class RacingCarGame {
+    private List<Car> cars;
+
+    public RacingCarGame(List<String> cars) {
+        validateCarCount(cars);
+        setCars(cars);
+    }
+
+    //차 객체들로 변환
+    public void setCars(List<String> carNames) {
+        cars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
 
 
     public void validateCarCount(List<String> cars) {
