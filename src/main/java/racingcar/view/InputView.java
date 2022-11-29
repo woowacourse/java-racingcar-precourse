45a918411d47432c584e341fab307943fad7133e
yoon <beatus96@naver.com>
2022-11-30 07:14:11 +0900

    feat : 자동차 이름 입력 받기 기능 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final String MESSAGE_INPUT_CARS = "경주할 자동차 이름을 입력하세요.";
+    private static final String MESSAGE_INPUT_COUNT = "시도할 회수는 몇회인가요?";
+//    (이름은 쉼표(,) 기준으로 구분)
+
+    public List<String> inputCars() {
+        System.out.println(MESSAGE_INPUT_CARS);
+
+        String input = Console.readLine();
+        //빈값 혹은 null인지 유효성 체크
+        return createCarLists(input);
+    }
+
+    public List<String> createCarLists(String input) {
+        return Arrays.stream(input.split(","))
+                .collect(Collectors.toList());
+    }
+
+    public int inputMovingCount() {
+        System.out.println(MESSAGE_INPUT_COUNT);
+
+        String input = Console.readLine();
+
+        return 0;
+    }
+}
