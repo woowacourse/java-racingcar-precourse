42780193fbab2de82937041e37c8472420c273ed
SEUNHGHEE96 <dnffktmdgml@gmail.com>
2022-11-29 20:51:44 +0900

    feat: 우승자 결과 출력 기능 구현

--- src/main/java/racingcar/view/outputView.java
+++ src/main/java/racingcar/view/outputView.java
@@ -9,12 +9,25 @@ public class outputView {
 
     public void round(List<Car> cars) {
         for(Car car : cars) {
-            //out(outputMessage.GAME_ROUND);
             System.out.print(car.getName() + " : ");
             System.out.print("-".repeat(car.getPosition()));
             System.out.println();
         }
         System.out.println();
+    }
+
+    public void result(List<Car> cars, int firstPrize) {
+        System.out.print("최종 우승자 : ");
+        int cnt = 0;
+        for(int n=0; n<cars.size(); n++){
+            if(cars.get(n).getPosition() == firstPrize) {
+                if(cnt==0){
+                    System.out.print(cars.get(n).getName());
+                } else {
+                    System.out.print(", "+cars.get(n).getName());
+                }
+            }
+        }
     }
 
 }
