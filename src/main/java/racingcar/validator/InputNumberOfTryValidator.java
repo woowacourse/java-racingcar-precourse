40131d9b21e666e5e975e2a9431fad3f3911eb08
JunH <tjdtls690@gmail.com>
2022-11-30 12:49:07 +0900

    :white_check_mark: test(Validator : InputNumberOfTryValidator) : 차 이름 입력 시, null 또는 empty 입력 시 예외처리 기능 테스트 완료

--- src/main/java/racingcar/validator/InputNumberOfTryValidator.java
+++ src/main/java/racingcar/validator/InputNumberOfTryValidator.java
@@ -2,6 +2,10 @@ package racingcar.validator;
 
 public class InputNumberOfTryValidator {
     public static void validate(final String numberOfTry) {
+        validateNullOrEmpty(numberOfTry);
+    }
     
+    private static void validateNullOrEmpty(final String numberOfTry) {
+        InputCommonValidator.validate(numberOfTry);
     }
 }
--- src/test/java/racingcar/validator/InputNumberOfTryValidatorTest.java
+++ src/test/java/racingcar/validator/InputNumberOfTryValidatorTest.java
@@ -3,7 +3,10 @@ package racingcar.validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import racingcar.constants.ErrorMessageConstant;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class InputNumberOfTryValidatorTest {
@@ -13,5 +16,14 @@ class InputNumberOfTryValidatorTest {
     void normalInput(final String numberOfTry) {
         assertThatNoException()
                 .isThrownBy(() -> InputNumberOfTryValidator.validate(numberOfTry));
+    }
+    
+    @DisplayName("예외 처리 : null or empty 입력 시")
+    @ParameterizedTest(name = "{displayName} : carsName => {0}")
+    @NullAndEmptySource
+    void nullOrEmptyInput(final String carsName) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputNumberOfTryValidator.validate(carsName))
+                .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
     }
 }
