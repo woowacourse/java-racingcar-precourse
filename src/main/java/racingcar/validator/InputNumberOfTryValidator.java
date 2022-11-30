38d40dd62a7d3224589df9848bdf0b66fd3e1f84
JunH <tjdtls690@gmail.com>
2022-11-30 16:40:39 +0900

    :white_check_mark: test(Validator : InputNumberOfTryValidator) : 차 이름 입력 시, 공백 입력 시 예외처리 기능 테스트 완료

--- src/main/java/racingcar/validator/InputNumberOfTryValidator.java
+++ src/main/java/racingcar/validator/InputNumberOfTryValidator.java
@@ -5,11 +5,14 @@ import java.util.regex.Pattern;
 
 public class InputNumberOfTryValidator {
     private static final String EXIST_ZERO_AT_FIRST_PLACE_EXCEPTION_MESSAGE = "[ERROR] 1번째 자리에는 0이 올 수 없습니다.";
+    private static final String NON_NUMERIC_CHARACTERS_EXCEPTION_MESSAGE = "[ERROR] '숫자'만 입력 가능합니다.";
     private static final String EXIST_ZERO_AT_FIRST_PLACE_FORM = "0\\d+";
+    private static final String INPUT_NON_NUMBER_FORM = "\\D";
     
     public static void validate(final String numberOfTry) {
         validateNullOrEmpty(numberOfTry);
         validateZeroAtFirstPlaceExist(numberOfTry);
+        validateInputNumberFormatMatching(numberOfTry);
     }
     
     private static void validateNullOrEmpty(final String numberOfTry) {
@@ -24,6 +27,16 @@ public class InputNumberOfTryValidator {
     
     private static boolean isExistZeroAtFirstPlace(final String inputNumber) {
         return matcher(inputNumber, EXIST_ZERO_AT_FIRST_PLACE_FORM).matches();
+    }
+    
+    private static void validateInputNumberFormatMatching(final String inputNumber) {
+        if (isNonNumericCharactersExist(inputNumber)) {
+            throw new IllegalArgumentException(NON_NUMERIC_CHARACTERS_EXCEPTION_MESSAGE);
+        }
+    }
+    
+    private static boolean isNonNumericCharactersExist(final String inputNumber) {
+        return matcher(inputNumber, INPUT_NON_NUMBER_FORM).find();
     }
     
     private static Matcher matcher(final String inputNumber, final String correctInputForm) {
--- src/test/java/racingcar/validator/InputNumberOfTryValidatorTest.java
+++ src/test/java/racingcar/validator/InputNumberOfTryValidatorTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class InputNumberOfTryValidatorTest {
-    @DisplayName("자동차 이름 정상 입력")
+    @DisplayName("이동 횟수 정상 입력")
     @ParameterizedTest(name = "{displayName} : numberOfTry => {0}")
     @CsvSource(value = {"1", "9", "19", "20"})
     void normalInput(final String numberOfTry) {
@@ -32,6 +32,15 @@ class InputNumberOfTryValidatorTest {
     @ParameterizedTest(name = "{displayName} => {0}")
     @ValueSource(strings = {"014000", "0014000", "00014000", "01", "001", "045", "0045", "00045"})
     void zeroAtFirstPlaceExistException(String carsName) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputNumberOfTryValidator.validate(carsName))
+                .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
+    }
+    
+    @DisplayName("예외 처리 : 공백 입력 시")
+    @ParameterizedTest(name = "{displayName} => {0}")
+    @ValueSource(strings = {"14 000", " 14000", "14000 "})
+    void spaceInputException(String carsName) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> InputNumberOfTryValidator.validate(carsName))
                 .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
