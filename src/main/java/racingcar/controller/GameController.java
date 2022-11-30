3bbd941932da2cbb8416bdf11c3b52d1cc63d38f
Ridealist <cjsn92@gmail.com>
2022-11-30 23:00:07 +0900

    feat(GameController): controller 클래스 구현
    view에서 입력값 받아 옴, 잘못된 입력값에 error 메세지 출력 후 재요청
    domain 객체들 생성 및 연결
    view에서 게임 상황 및 결과 출력 메소드 호출

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,4 +1,51 @@
 package racingcar.controller;
 
+import java.util.List;
+import java.util.function.Supplier;
+import racingcar.domain.GameResult;
+import racingcar.domain.Player;
+import racingcar.domain.RacingGame;
+import racingcar.domain.car.NumberGenerator;
+import racingcar.domain.car.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class GameController {
+
+    private RacingGame racingGame;
+    private GameResult gameResult;
+
+    public GameController() {
+        setUp();
+    }
+
+    private void setUp() {
+        InputView inputView = new InputView();
+        List<String> playerNames = repeat(inputView::readPlayerName);
+        Player player = new Player(playerNames);
+        this.gameResult = new GameResult(player);
+        int gameRound = repeat(inputView::readGameRound);
+        this.racingGame = new RacingGame(player, gameRound);
+    }
+
+    public void start() {
+        OutputView outputView = new OutputView();
+        outputView.printResultMessage();
+        do {
+            NumberGenerator numberGenerator = new RandomNumberGenerator();
+            racingGame.play(numberGenerator);
+            outputView.printRoundResult(gameResult);
+        } while (racingGame.isGameContinue());
+        outputView.printFinalWinner(gameResult);
+    }
+
+    private <T> T repeat(Supplier<T> inputReader) {
+        try {
+            return inputReader.get();
+        } catch (IllegalArgumentException e) {
+            OutputView outputView = new OutputView();
+            outputView.printErrorMessage(e.getMessage());
+            return repeat(inputReader);
+        }
+    }
 }
