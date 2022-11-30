493ccd07ce1b60521618cbcda3205e65f3e2d0bb
Ridealist <cjsn92@gmail.com>
2022-11-30 19:20:42 +0900

    feat(NumberGenerator): 랜덤 숫자 생성 인터페이스 및 구현 클래스 구현

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    private static final int LOWER_BOUND = 0;
+    private static final int UPPER_BOUND = 9;
+
+    @Override
+    public int generate() {
+        return Randoms.pickNumberInRange(LOWER_BOUND, UPPER_BOUND);
+    }
+}
