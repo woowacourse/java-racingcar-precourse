2b0a986a665b71eae59ffd3cb92a68c7ed05a3fd
SEUNHGHEE96 <dnffktmdgml@gmail.com>
2022-11-30 13:23:26 +0900

    feat: 자동차 이름 예외 처리

--- src/main/java/racingcar/view/inputView.java
+++ src/main/java/racingcar/view/inputView.java
@@ -9,7 +9,21 @@ public class inputView {
         out(inputMessage.START_GAME);
         String inputname = Console.readLine();
         String[] names = inputname.split(",");
+        try{
+            nameRange(names);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            getNames();
+        }
         return names;
+    }
+
+    private void nameRange(String[] names) {
+        for(String name : names) {
+            if(name.length()>5){
+                throw new IllegalArgumentException("[ERROR] 이름은 다섯 글자를 넘길 수 없다.");
+            }
+        }
     }
 
     public int getRound() {
