95808b3019f2d33383c2adf67b042a161316b3a1
Ridealist <cjsn92@gmail.com>
2022-11-30 23:02:05 +0900

    feat(Application): 전체 기능 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        GameController gameController = new GameController();
+        gameController.start();
     }
 }
