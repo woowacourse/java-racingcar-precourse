7396c5cfea43de4a6527b2958ef51bbd18a92780
yoon <beatus96@naver.com>
2022-11-30 11:06:04 +0900

    feat : 자동차 경주게임 생성 기능 추가

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class Controller {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public RacingCarGame createRacingGame() {
+        try {
+            List<String> cars = inputView.inputCars();
+            return new RacingCarGame(cars);
+
+        }catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+        }
+        return createRacingGame();
+    }
+
+}
