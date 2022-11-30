b641c7a5da08314fb58c4e6fc17a4fd8a2188796
Ridealist <cjsn92@gmail.com>
2022-11-30 19:00:13 +0900

    feat: 전체 클래스 구조 밑그림 및 패키지 분리

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+/*
+Car 기본 생성자를 추가할 수 없다.
+name, position 변수의 접근 제어자인 private을 변경할 수 없다.
+가능하면 setPosition(int position) 메소드를 추가하지 않고 구현한다.
+ */
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // 추가 기능 구현
+}
