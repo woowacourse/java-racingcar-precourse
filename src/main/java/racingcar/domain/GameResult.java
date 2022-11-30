3ef0908dd45a4225a6f9ef1670117ddb385d6582
Ridealist <cjsn92@gmail.com>
2022-11-30 22:40:06 +0900

    feat(GameResult): 게임 상황 및 최종 결과 반환 클래스 구현

--- src/main/java/racingcar/domain/GameResult.java
+++ src/main/java/racingcar/domain/GameResult.java
@@ -1,14 +1,40 @@
 package racingcar.domain;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import racingcar.domain.car.Car;
 
 public class GameResult {
 
-    private HashMap<String, Integer> playerPosition;
+    private final Player player;
+    private Map<String, Integer> playerPosition = new LinkedHashMap<>();
 
-    public List<Object> getFinalWinner() {
-        return Arrays.asList(0);
+    public GameResult(Player player) {
+        this.player = player;
+    }
+
+    public void update() {
+        List<Car> racingCars = player.getRacingCars();
+        for (Car car : racingCars) {
+            playerPosition.put(car.getName(), car.getPosition());
+        }
+    }
+
+    public Map<String, Integer> getPlayerPosition() {
+        return playerPosition;
+    }
+
+    public List<String> getFinalWinner() {
+        List<String> finalWinner = new ArrayList<>();
+        int maxValue = Collections.max(playerPosition.values());
+        for (Map.Entry<String, Integer> m : playerPosition.entrySet()) {
+            if (m.getValue() == maxValue) {
+                finalWinner.add(m.getKey());
+            }
+        }
+        return finalWinner;
     }
 }
