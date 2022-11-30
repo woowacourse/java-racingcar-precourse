cf377a78fdda7dbc9b36dae0e8abb3a9e922019c
yoon <beatus96@naver.com>
2022-11-30 11:07:59 +0900

    feat : 어플리케이션 완성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,7 +2,6 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
