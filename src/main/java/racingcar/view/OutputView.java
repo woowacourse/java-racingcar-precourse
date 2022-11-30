a6e1dad9c3c39169f014a181ba8f1b9bca016f28
yoon <beatus96@naver.com>
2022-11-30 11:04:20 +0900

    feat : 자동차 경주 최종 결과 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,12 +7,17 @@ import java.util.List;
 public class OutputView {
     private static final String MESSAGE_WINNING_CARS = "최종 우승자 : ";
 
-
     public void printMovingResult(RacingCarGame racingCarGame) {
         List<String> movingResult = racingCarGame.getMovingResult();
         movingResult.stream()
                 .forEach(System.out::println);
         System.out.println();
+    }
+
+    public void printRacingResult(RacingCarGame racingCarGame) {
+        String racingResult = String.join(",", racingCarGame.getRacingResult());
+
+        System.out.println(MESSAGE_WINNING_CARS + racingResult);
     }
 
     }
