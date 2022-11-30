8cc7bb2613286fc5c7b27363e13629f415d45573
JunH <tjdtls690@gmail.com>
2022-11-30 19:12:15 +0900

    :art: refactor : 상수 접근제한자 개선

--- src/main/java/racingcar/domain/movestrategy/RandomBasedMoveStrategy.java
+++ src/main/java/racingcar/domain/movestrategy/RandomBasedMoveStrategy.java
@@ -3,9 +3,9 @@ package racingcar.domain.movestrategy;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomBasedMoveStrategy implements MoveStrategy {
-    public static final int MIN_RANDOM_NUMBER = 0;
-    public static final int MAX_RANDOM_NUMBER = 9;
-    public static final int MIN_MOVABLE_NUMBER = 4;
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int MIN_MOVABLE_NUMBER = 4;
     
     @Override
     public boolean isMovable() {
