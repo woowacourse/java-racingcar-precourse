ae36b03adaa9161849458f10b7d2e2a9de352d91
ksh-beginner <ohk9134@naver.com>
2022-12-01 17:40:46 +0900

    test(RacingCarsTest) : 우승자 판단 기능 테스트

--- src/test/java/racingcar/model/RacingCarsTest.java
+++ src/test/java/racingcar/model/RacingCarsTest.java
@@ -21,4 +21,37 @@ class RacingCarsTest {
 
         assertThat(racingCars.getRacingCars()).isEqualTo(cars);
     }
+
+    @Test
+    void 우승자_판단_기능_단독_우승() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("testA"));
+        cars.add(new Car("testB"));
+        RacingCars racingCars = new RacingCars(cars);
+        Car carA = racingCars.getRacingCars().get(0);
+        Car carB = racingCars.getRacingCars().get(1);
+        carA.forward();
+        carA.forward();
+        List<String> winners = racingCars.decideWinners();
+
+        assertThat(winners.size()).isEqualTo(1);
+        assertThat(winners).contains("testA");
+    }
+
+    @Test
+    void 우승자_판단_기능_공동_우승() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("testA"));
+        cars.add(new Car("testB"));
+        RacingCars racingCars = new RacingCars(cars);
+        Car carA = racingCars.getRacingCars().get(0);
+        Car carB = racingCars.getRacingCars().get(1);
+        carA.forward();
+        carB.forward();
+        List<String> winners = racingCars.decideWinners();
+
+        assertThat(winners.size()).isEqualTo(2);
+        assertThat(winners).contains("testA");
+        assertThat(winners).contains("testB");
+    }
 }
