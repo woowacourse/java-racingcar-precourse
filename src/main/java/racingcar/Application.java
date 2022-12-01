bb54b206d679ba47594291246e4e2ef2b9f4cad2
ksh-beginner <ohk9134@naver.com>
2022-12-01 17:14:34 +0900

    feat : 게임 실행 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        RacingCarGameController racingCarGameController = new RacingCarGameController();
+        racingCarGameController.gamePlay();
     }
 }
--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,4 +1,77 @@
 package racingcar.controller;
 
+import racingcar.service.RacingCarGameService;
+import racingcar.service.RacingCarGameValidateService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
 public class RacingCarGameController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final RacingCarGameService racingCarGameService = new RacingCarGameService();
+    private final RacingCarGameValidateService racingCarGameValidateService = new RacingCarGameValidateService();
+
+    private String carNames;
+    private String tryNumber;
+
+    public void gamePlay() {
+        inputCarName();
+        generateRacingCars();
+        inputTryNumber();
+        forwardTry();
+        printForwardTryResult();
+        printFinalWinners();
+    }
+
+    public void inputCarName() {
+        outputView.printInputCarNameMessage();
+        while (true) {
+            try {
+                carNames = inputView.inputCarName();
+                racingCarGameValidateService.validateInputCarName(carNames);
+                break;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    public void generateRacingCars() {
+        racingCarGameService.generateRacingCars(carNames);
+    }
+
+    public void inputTryNumber() {
+        outputView.printInputTryNumberMessage();
+        while (true) {
+            try {
+                tryNumber = inputView.inputTryNumber();
+                racingCarGameValidateService.validateInputTryNumber(tryNumber);
+                break;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    public void forwardTry() {
+        outputView.printGameResultMessage();
+        for (int tryIndex = 0; tryIndex < Integer.parseInt(tryNumber); tryIndex++) {
+            racingCarGameService.forwardTry();
+            printForwardTryResult();
+        }
+    }
+
+    public void printForwardTryResult() {
+        List<Map<String, String>> forwardTryResult = racingCarGameService.getForwardTryResult();
+        outputView.printForwardTryResult(forwardTryResult);
+    }
+
+    public void printFinalWinners() {
+        List<String> finalWinners = racingCarGameService.getFinalWinner();
+        outputView.printFinalWinners(finalWinners);
+    }
 }
