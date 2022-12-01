0822c57fe8c4e9e6aea4f5254d6656289f7158fc
ksh-beginner <ohk9134@naver.com>
2022-12-01 17:48:35 +0900

    feat(InputCarNameValidator) : 입력한 자동차 이름 검증 로직 구현

--- src/main/java/racingcar/validator/InputCarNameValidator.java
+++ src/main/java/racingcar/validator/InputCarNameValidator.java
@@ -1,4 +1,20 @@
 package racingcar.validator;
 
+import racingcar.ErrorConstants;
+
+import java.util.regex.Pattern;
+
 public class InputCarNameValidator {
+
+    private static final String CORRECT_CAR_NAME_REGEX = "^[가-힣\\w]{1,5}+[가-힣\\w,]{1,5}$";
+
+    public void validate(String inputCarName) {
+        if (isWrongType(inputCarName)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "자동차 이름은 5자 이하이고, 쉼표로 구분된 문자여야합니다.");
+        }
+    }
+
+    private boolean isWrongType(String inputCarName) {
+        return !Pattern.compile(CORRECT_CAR_NAME_REGEX).matcher(inputCarName).matches();
+    }
 }
