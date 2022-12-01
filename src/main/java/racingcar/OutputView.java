40e9197955935f3515d3b0a80e402447c65c8a93
This2sho <zx00018@naver.com>
2022-12-01 16:19:19 +0900

    feat(OutputView): 우승자 출력 기능 구현

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
     public static final String CAR_FORMAT = "%s : %s";
     public static final String MOVE_UNIT = "-";
+    public static final String DELIMITER = ", ";
 
     public void printCars(List<Car> cars) {
         for (Car car : cars) {
@@ -29,5 +31,11 @@ public class OutputView {
             stringBuilder.append(MOVE_UNIT);
         }
         return stringBuilder.toString();
+    }
+
+    public void printWinners(List<String> winners) {
+        String winnersName = winners.stream()
+            .collect(Collectors.joining(DELIMITER));
+        System.out.printf("최종 우승자 : %s", winnersName);
     }
 }
