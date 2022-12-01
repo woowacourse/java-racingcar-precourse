551ce817fe2e017e8992ed3111496c1b3191b473
kim0914 <kosa0914@naver.com>
2022-12-01 17:27:15 +0900

    style(RacingGameTest): Assertion static import 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,10 +1,11 @@
 package racingcar.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
 
 class RacingGameTest {
 
@@ -22,7 +23,7 @@ class RacingGameTest {
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
         car1.move();
 
-        Assertions.assertThat(racingGame.getWinners().size()).isEqualTo(1);
+        assertThat(racingGame.getWinners().size()).isEqualTo(1);
     }
 
     @Test
@@ -31,21 +32,21 @@ class RacingGameTest {
         car1.move();
         car2.move();
 
-        Assertions.assertThat(racingGame.getWinners().size()).isEqualTo(2);
+        assertThat(racingGame.getWinners().size()).isEqualTo(2);
     }
 
     @Test
     void 입력한_횟수보다_게임을_적게_진행하면_종료하지_않는다() {
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
 
-        Assertions.assertThat(racingGame.canContinue(2)).isEqualTo(true);
+        assertThat(racingGame.canContinue(2)).isEqualTo(true);
     }
 
     @Test
     void 입력한_횟수만큼_게임을_진행하면_종료한다() {
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
 
-        Assertions.assertThat(racingGame.canContinue(4)).isEqualTo(false);
+        assertThat(racingGame.canContinue(4)).isEqualTo(false);
     }
 
     @Test
@@ -55,6 +56,6 @@ class RacingGameTest {
         car2.move();
         car2.move();
 
-        Assertions.assertThat(racingGame.getMaxPosition()).isEqualTo(2);
+        assertThat(racingGame.getMaxPosition()).isEqualTo(2);
     }
 }
