3003563737854b390d7d13bba0d7512375e097a2
ksh-beginner <ohk9134@naver.com>
2022-12-01 13:19:03 +0900

    feat(OutputView) : 실행 결과 문구 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
     public void printInputTryNumberMessage() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
+
+    public void printGameResultMessage() {
+        System.out.println("실행 결과");
+    }
 }
