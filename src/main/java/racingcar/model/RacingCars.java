a87c67893f6d5b7e949b19508fb548137ccdbf78
ksh-beginner <ohk9134@naver.com>
2022-12-01 14:32:41 +0900

    feat(RacingCars) : 경주할 자동차 생성 기능 구현

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,4 +1,12 @@
 package racingcar.model;
 
+import java.util.List;
+
 public class RacingCars {
+
+    private final List<Car> racingCars;
+
+    public RacingCars(List<Car> racingCars) {
+        this.racingCars = racingCars;
+    }
 }
