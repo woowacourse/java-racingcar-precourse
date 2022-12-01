a57298e43cbc04bc1974a2c3209a677eb7a184d8
ksh-beginner <ohk9134@naver.com>
2022-12-01 14:25:40 +0900

    feat(RacingCarGameService) : 경주할 자동차 생성 기능 구현

--- src/main/java/racingcar/service/RacingCarGameService.java
+++ src/main/java/racingcar/service/RacingCarGameService.java
@@ -1,4 +1,20 @@
 package racingcar.service;
 
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCarGameService {
+
+    private static final String CAR_NAME_SPLIT_REGEX = ",";
+
+    private List<Car> racingCars = new ArrayList<>();
+
+    public void generateRacingCars(String inputCarName) {
+        for (String carName : inputCarName.split(CAR_NAME_SPLIT_REGEX)) {
+            Car generatedCar = new Car(carName);
+            racingCars.add(generatedCar);
+        }
+    }
 }
