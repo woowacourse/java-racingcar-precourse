f67470e3702d9a11ca9651dcfb5eca1d350c3b9b
ksh-beginner <ohk9134@naver.com>
2022-12-01 13:21:43 +0900

    feat(InputView) : 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,10 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String inputCarName() {
+        return Console.readLine();
+    }
 }
