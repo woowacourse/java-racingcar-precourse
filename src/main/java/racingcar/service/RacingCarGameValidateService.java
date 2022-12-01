5e54b05d6dd41a9a5913e5bddb39b194658307b1
ksh-beginner <ohk9134@naver.com>
2022-12-01 17:15:30 +0900

    feat(RacingCarGameValidateService) : 자동차 경주 게임 검증 서비스 로직 구현

--- src/main/java/racingcar/service/RacingCarGameValidateService.java
+++ src/main/java/racingcar/service/RacingCarGameValidateService.java
@@ -1,5 +1,18 @@
 package racingcar.service;
 
+import racingcar.validator.InputCarNameValidator;
+import racingcar.validator.InputTryNumberValidator;
 
 public class RacingCarGameValidateService {
+
+    private final InputCarNameValidator inputCarNameValidator = new InputCarNameValidator();
+    private final InputTryNumberValidator inputTryNumberValidator = new InputTryNumberValidator();
+
+    public void validateInputCarName(String inputCarName) {
+        inputCarNameValidator.validate(inputCarName);
+    }
+
+    public void validateInputTryNumber(String inputTryNumber) {
+        inputTryNumberValidator.validate(inputTryNumber);
+    }
 }
