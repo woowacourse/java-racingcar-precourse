cf0e06923d1d17fed1c7718e958a93aa08bd3a7c
drunkenhw <drunkenhw@gmail.com>
2022-12-01 16:14:38 +0900

    refactor(RacingGameTest): 반복된 구문 제거

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -3,19 +3,27 @@ package racingcar.domain;
 import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RacingGameTest {
+    private Car han;
+    private Car kim;
+    private RacingGame racingGame;
+
+    @BeforeEach
+    void before() {
+        han = new Car("han");
+        kim = new Car("kim");
+        racingGame = new RacingGame(Arrays.asList(han, kim));
+    }
 
     @DisplayName("우승자를 반환한다.")
     @Test
     void getWinner() {
-        Car han = new Car("han");
-        Car kim = new Car("kim");
-        List<Car> list = Arrays.asList(han, kim);
-        RacingGame racingGame = new RacingGame(list);
         han.addPosition();
+
         RacingResult winner = racingGame.getWinner();
 
         Assertions.assertThat(winner.getCarNames()).containsExactly("han");
@@ -24,12 +32,9 @@ class RacingGameTest {
     @DisplayName("우승자가 2명이면 모두 반환한다.")
     @Test
     void getTwoWinner() {
-        Car han = new Car("han");
-        Car kim = new Car("kim");
-        List<Car> list = Arrays.asList(han, kim);
-        RacingGame racingGame = new RacingGame(list);
         han.addPosition();
         kim.addPosition();
+
         RacingResult winner = racingGame.getWinner();
 
         Assertions.assertThat(winner.getCarNames()).containsOnly("han", "kim");
