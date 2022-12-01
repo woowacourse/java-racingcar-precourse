f7e5a72f05f3fcc65dda07dfac0ad7000d2b4f6d
drunkenhw <drunkenhw@gmail.com>
2022-12-01 13:20:52 +0900

    feat(Car): 이름을 검증하는 기능 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름의 길이는 5자 이하만 가능합니다.");
+        }
+    }
+
+}
---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CarTest {
+
+    @DisplayName("이름의 길이가 5자를 초과하면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @CsvSource(value = {"abcdef", "1234567"})
+    void nameValidateTest(String name) {
+        assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
