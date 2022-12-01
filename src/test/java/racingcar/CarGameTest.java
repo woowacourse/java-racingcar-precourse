b9be6d027b37aa7e8620a52b90fefaa4196107ae
This2sho <zx00018@naver.com>
2022-12-01 14:40:44 +0900

    refactor(CarGameTest): 초기화 init 메소드로 추출

--- src/test/java/racingcar/CarGameTest.java
+++ src/test/java/racingcar/CarGameTest.java
@@ -7,16 +7,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarGameTest {
+    private CarGame carGame;
+
+    @BeforeEach
+    void init() {
+        carGame = new CarGame();
+    }
 
     @Test
     @DisplayName("자동차 경주 자동차 등록 성공 테스트")
     void 자동차_등록_성공_테스트() {
-        //given
-        CarGame carGame = new CarGame();
         //when
         List<String> cars = Arrays.asList("pobi", "woni", "jun");
         carGame.registerCar(cars);
@@ -33,8 +38,6 @@ class CarGameTest {
     @Test
     @DisplayName("자동차 경주 자동차 등록 실패 테스트")
     void 자동차_등록_실패_테스트() {
-        //given
-        CarGame carGame = new CarGame();
         //when
         List<String> cars = new ArrayList<>();
         //then
