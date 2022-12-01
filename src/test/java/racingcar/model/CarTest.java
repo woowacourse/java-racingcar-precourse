4e5c94713077f0f76d8ae2a2263ee00772ebf8e8
ksh-beginner <ohk9134@naver.com>
2022-12-01 17:31:21 +0900

    test(CarTest) : 출력용 자동차 정보 반환 기능 테스트

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,6 +3,8 @@ package racingcar.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +19,18 @@ class CarTest {
         String convertedPosition = car.positionConvertToDash();
 
         assertThat(convertedPosition).isEqualTo("---");
+    }
+
+    @Test
+    void 출력용_자동차_정보_반환_기능() {
+        Car car = new Car("test");
+        car.forward();
+        car.forward();
+        Map<String, String> carInfos = car.getCarInfos();
+        String carName = carInfos.get("name");
+        String position = carInfos.get("position");
+
+        assertThat(carName).isEqualTo("test");
+        assertThat(position).isEqualTo("--");
     }
 }
