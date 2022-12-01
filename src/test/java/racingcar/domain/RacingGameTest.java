02627f604c0f3296d6a47cf24a88c2f65539c5c6
kim0914 <kosa0914@naver.com>
2022-12-01 17:20:37 +0900

    test(RacingGameTest): 게임 종료 조건 테스트 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-
 class RacingGameTest {
 
     @Test
@@ -29,6 +28,26 @@ class RacingGameTest {
         car2.move();
 
         Assertions.assertThat(racingGame.getWinners().size()).isEqualTo(2);
+    }
+
+    @Test
+    void 입력한_횟수보다_게임을_적게_진행하면_종료하지_않는다() {
+        Car car1 = new Car("AAA");
+        Car car2 = new Car("BBB");
+
+        RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
+
+        Assertions.assertThat(racingGame.canContinue(2)).isEqualTo(true);
+    }
+
+    @Test
+    void 입력한_횟수만큼_게임을_진행하면_종료한다() {
+        Car car1 = new Car("AAA");
+        Car car2 = new Car("BBB");
+
+        RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
+
+        Assertions.assertThat(racingGame.canContinue(4)).isEqualTo(false);
     }
 
 }
