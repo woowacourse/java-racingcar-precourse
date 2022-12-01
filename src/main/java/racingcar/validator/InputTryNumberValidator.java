8f0d867f19eceab99465a2c06dfc091b20fc02e6
ksh-beginner <ohk9134@naver.com>
2022-12-01 13:58:47 +0900

    feat(InputTryNumberValidator) : 시도할 횟수가 1 이상의 숫자가 아닌 경우 예외 처리 기능 구현

--- src/main/java/racingcar/validator/InputTryNumberValidator.java
+++ src/main/java/racingcar/validator/InputTryNumberValidator.java
@@ -1,4 +1,20 @@
 package racingcar.validator;
 
+import racingcar.ErrorConstants;
+
+import java.util.regex.Pattern;
+
 public class InputTryNumberValidator {
+
+    private static final String CORRECT_TRY_NUMBER_REGEX = "[1-9]+";
+
+    public void validate(String inputTryNumber) {
+        if (isNotDigit(inputTryNumber)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "시도 횟수는 1이상의 숫자여야합니다.");
+        }
+    }
+
+    private boolean isNotDigit(String inputTryNumber) {
+        return !Pattern.compile(CORRECT_TRY_NUMBER_REGEX).matcher(inputTryNumber).matches();
+    }
 }
