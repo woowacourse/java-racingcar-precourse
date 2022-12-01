9c721efbcbbeacca9708b1cb926aaaf789266e7a
kim0914 <kosa0914@naver.com>
2022-12-01 17:26:41 +0900

    refactor(RacingGameTest): Car 객체 생성 중복 코드 제거

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,17 +1,24 @@
 package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 class RacingGameTest {
 
+    Car car1;
+    Car car2;
+
+    @BeforeEach
+    void init() {
+        car1 = new Car("AAA");
+        car2 = new Car("BBB");
+    }
+
     @Test
     void 우승자_한명를_반환한다() {
-        Car car1 = new Car("AAA");
-        Car car2 = new Car("BBB");
-
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
         car1.move();
 
@@ -20,9 +27,6 @@ class RacingGameTest {
 
     @Test
     void 우승자_두명를_반환한다() {
-        Car car1 = new Car("AAA");
-        Car car2 = new Car("BBB");
-
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
         car1.move();
         car2.move();
@@ -32,9 +36,6 @@ class RacingGameTest {
 
     @Test
     void 입력한_횟수보다_게임을_적게_진행하면_종료하지_않는다() {
-        Car car1 = new Car("AAA");
-        Car car2 = new Car("BBB");
-
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
 
         Assertions.assertThat(racingGame.canContinue(2)).isEqualTo(true);
@@ -42,9 +43,6 @@ class RacingGameTest {
 
     @Test
     void 입력한_횟수만큼_게임을_진행하면_종료한다() {
-        Car car1 = new Car("AAA");
-        Car car2 = new Car("BBB");
-
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
 
         Assertions.assertThat(racingGame.canContinue(4)).isEqualTo(false);
@@ -52,9 +50,6 @@ class RacingGameTest {
 
     @Test
     void 자동차들_중에서_선두_위치를_반환한다() {
-        Car car1 = new Car("AAA");
-        Car car2 = new Car("BBB");
-
         RacingGame racingGame = new RacingGame(Arrays.asList(car1, car2), 3);
         car1.move();
         car2.move();
