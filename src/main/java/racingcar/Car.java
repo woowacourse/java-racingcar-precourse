5c37b2f83d103704c6051f42e98dc19bc7c3767c
This2sho <zx00018@naver.com>
2022-12-01 13:52:59 +0900

    feat(Car): 자동차 연료에 따른 전진 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -20,4 +20,26 @@ public class Car {
             throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
         }
     }
+
+
+    public void moveOrStop(int fuel) {
+        validateFuel(fuel);
+        if (fuel >= MIN_FUEL) {
+            move();
+        }
+    }
+
+    private void validateFuel(int fuel) {
+        if (fuel < 0 || fuel > MAX_FUEL) {
+            throw new IllegalArgumentException("연료는 0 이상 9 이하의 값이어야 합니다.");
+        }
+    }
+
+    private void move() {
+        position++;
+    }
+
+    public int getCurrentPosition() {
+        return position;
+    }
 }
