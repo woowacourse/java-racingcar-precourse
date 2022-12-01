7ea7596cfedfc7cb43ae2f8d3da98fcd7a436910
ksh-beginner <ohk9134@naver.com>
2022-12-01 15:41:54 +0900

    feat(Car) : 출력용 자동차 정보 생성 기능 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,8 +1,12 @@
 package racingcar.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Car {
     private final String name;
     private int position = 0;
+    private final Map<String, String> printCarInfos = new HashMap<>();
 
     public Car(String name) {
         this.name = name;
@@ -10,5 +14,20 @@ public class Car {
 
     public void forward() {
         this.position++;
+    }
+
+    public String positionConvertToDash() {
+        StringBuilder convertPosition = new StringBuilder();
+        for (int repeatIndex = 0; repeatIndex < this.position; repeatIndex++) {
+            convertPosition.append("-");
+        }
+        return convertPosition.toString();
+    }
+
+    public Map<String, String> getCarInfos() {
+        printCarInfos.put("name", this.name);
+        String convertedPosition = positionConvertToDash();
+        printCarInfos.put("position", convertedPosition);
+        return printCarInfos;
     }
 }
