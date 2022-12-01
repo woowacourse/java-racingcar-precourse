d3b17d40a8c9459b5e49b2a450d2e2268f00ebe5
This2sho <zx00018@naver.com>
2022-12-01 13:50:34 +0900

    feat(Car): 자동차 이름 유효성 검사 로직 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,23 @@
 package racingcar;
 
 public class Car {
+
+    public static final int MIN_FUEL = 4;
+    public static final int MAX_FUEL = 9;
+    public static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
     }
 
     // 추가 기능 구현
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+        }
+    }
 }
