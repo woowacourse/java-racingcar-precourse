772cbc043bc00bb34654488dc6074363d48c487d
ksh-beginner <ohk9134@naver.com>
2022-12-01 14:29:54 +0900

    refactor(RacingCarGameService) : 경주할 자동차 생성 기능 리팩토링

--- src/main/java/racingcar/service/RacingCarGameService.java
+++ src/main/java/racingcar/service/RacingCarGameService.java
@@ -1,6 +1,7 @@
 package racingcar.service;
 
 import racingcar.model.Car;
+import racingcar.model.RacingCars;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +10,15 @@ public class RacingCarGameService {
 
     private static final String CAR_NAME_SPLIT_REGEX = ",";
 
-    private List<Car> racingCars = new ArrayList<>();
+    private RacingCars racingCars;
+
+    private List<Car> cars = new ArrayList<>();
 
     public void generateRacingCars(String inputCarName) {
         for (String carName : inputCarName.split(CAR_NAME_SPLIT_REGEX)) {
             Car generatedCar = new Car(carName);
-            racingCars.add(generatedCar);
+            cars.add(generatedCar);
         }
+        racingCars = new RacingCars(cars);
     }
 }
