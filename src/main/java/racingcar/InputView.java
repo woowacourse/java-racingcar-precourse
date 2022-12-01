0d8b21e75b7948e8c7ddc246108aa830570f727a
This2sho <zx00018@naver.com>
2022-12-01 16:29:37 +0900

    feat(InputView): 입력 메시지 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -11,13 +11,18 @@ public class InputView {
     public static final String CAR_REGEX = ",";
     private static final String ONLY_NUMBER_REGEX = "^[0-9]+$";
 
+    public static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_MOVE_COUNTS_MESSAGE = "시도할 회수는 몇회인가요?";
+
     public List<String> inputCars() {
+        System.out.println(INPUT_CAR_MESSAGE);
         String input = Console.readLine();
         String[] inputCars = input.split(CAR_REGEX);
         return Arrays.stream(inputCars).collect(Collectors.toList());
     }
 
     public int inputMoveCounts() {
+        System.out.println(INPUT_MOVE_COUNTS_MESSAGE);
         String input = Console.readLine();
         validateNumber(input);
         return Integer.parseInt(input);
