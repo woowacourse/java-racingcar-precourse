803af743a81b2abd6560aa29a6b670d85426f8f5
kim0914 <kosa0914@naver.com>
2022-12-01 15:42:03 +0900

    feat(InputView): 자동차 이름을 입력받는 메서드 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,36 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Log;
+import racingcar.util.Parser;
+import racingcar.validation.InputValidator;
+
+import java.util.List;
+
+public class InputView {
+
+    public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRIAL_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public List<String> readCarNames() {
+        printMessage(CAR_NAME_INPUT_MESSAGE);
+        String carNames = Console.readLine();
+
+        return checkCarNames(carNames);
+    }
+
+    private List<String> checkCarNames(String carNames) {
+        try {
+            List<String> names = Parser.getNames(carNames);
+            InputValidator.checkNames(names);
+            return names;
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+            return readCarNames();
+        }
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+}
