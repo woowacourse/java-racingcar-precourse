fd6cd4e4c578da842a2d548bf9c6b8f3594db587
kim0914 <kosa0914@naver.com>
2022-12-01 15:45:31 +0900

    feat(Car): 자동차가 움직일 수 있는지 판단하는 메서드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,17 @@
-package racingcar;
+package racingcar.domain;
 
 public class Car {
+
     private final String name;
     private int position = 0;
+    private static final int MOVE_MIN_NUMBER = 4;
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    public boolean canMove(int number) {
+        return number >= MOVE_MIN_NUMBER;
+    }
 }
