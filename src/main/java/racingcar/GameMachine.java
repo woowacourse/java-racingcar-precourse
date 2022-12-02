fe4230d1e8911acd3f622fcaaa23795d836f8dc4
SeokyungKim <kskyung0624@gmail.com>
2022-12-02 16:37:11 +0900

    feat(GameMachine) : 게임이 일정 횟수 반복되면 악성 공격 시도로 판단되어 게임을 종료하도록 한다

--- src/main/java/racingcar/GameMachine.java
+++ src/main/java/racingcar/GameMachine.java
@@ -7,10 +7,13 @@ public class GameMachine {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
 
+    private static final int LIMIT = 1_000_000;
+    private int tolerance = 0;
+
     public void play() {
         Cars cars = createCars(
-                inputView.readCarNames(),
-                inputView.readCoins());
+                readCarNames(),
+                readCoins());
     }
 
     private Cars createCars(CarNames carNames, Coin coin) {
@@ -20,5 +23,33 @@ public class GameMachine {
                         .collect(Collectors.toList()),
                 coin
         );
+    }
+
+    // todo: read 부분 추상화하기
+    private CarNames readCarNames() {
+        try {
+            return inputView.readCarNames();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            plusGameTimes();
+            return inputView.readCarNames();
+        }
+    }
+
+    private Coin readCoins() {
+        try {
+            return inputView.readCoins();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            plusGameTimes();
+            return inputView.readCoins();
+        }
+    }
+
+    private void plusGameTimes() {
+        tolerance += 1;
+        if (tolerance > LIMIT) {
+            throw new IllegalArgumentException("[ERROR] 게임 진행 횟수 한도가 초과되었습니다. 악성 접근으로 판단되어 게임이 종료됩니다.");
+        }
     }
 }
