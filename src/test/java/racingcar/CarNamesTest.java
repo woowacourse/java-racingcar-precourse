b300069b1d76cab773772cbb025f98b700c2da86
SeokyungKim <kskyung0624@gmail.com>
2022-12-02 14:53:54 +0900

    test(CarNames) : 변경된 요구사항에 맞춰 CarNamesTest 수정

--- src/test/java/racingcar/CarNamesTest.java
+++ src/test/java/racingcar/CarNamesTest.java
@@ -2,28 +2,25 @@ package racingcar;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarNamesTest {
-    final static String ERROR = "[ERROR]";
 
-    @Test
+    @ParameterizedTest
     @CsvSource({"'pobi,woni,jun'", "sk12"})
     void CarNames_자동차_이름을_입력받는다(String input) {
-        assertThat(carNames(input)).isEqualTo(input.split(","));
+        System.out.println(input);
+        assertThat(CarNames.from(input.split(",")))
+                .hasNoNullFieldsOrProperties();
     }
 
     @Test
-    @CsvSource({
-            "'pobi,,jun'",
-            "", ", , , ,",
-            "'chooooovy, pobi'",
-    })
-    void CarNames_이름_길이가_해당_범위가_아니라면_에러를_발생시킨다(String input) {
-        Assertions.assertThatThrownBy(() -> carNames(input))
+    void CarNames_이름_길이가_해당_범위가_아니라면_에러를_발생시킨다() {
+        Assertions.assertThatThrownBy(() -> CarNames.from(new String[] {"chooooovy", "pobi"}))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ERROR);
+                .hasMessage("[ERROR] 자동차 이름은 1글자 이상 5글자 이하여야 합니다");
     }
 }
