2917488d4983823cbf7d5c3ccbe0d78c4dac94e9
SeokyungKim <kskyung0624@gmail.com>
2022-12-02 17:25:08 +0900

    test(CarNames) : limit 검증 메서드의 자동차 이름 생성 부분을 메서드로 분리한다

--- src/test/java/racingcar/CarNamesTest.java
+++ src/test/java/racingcar/CarNamesTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarNamesTest {
@@ -19,21 +21,31 @@ public class CarNamesTest {
 
     @Test
     void CarNames_이름_길이가_해당_범위가_아니라면_에러를_발생시킨다() {
-        Assertions.assertThatThrownBy(() -> new CarNames(new String[] {"chooooovy", "pobi"}))
+        Assertions.assertThatThrownBy(() -> new CarNames(new String[]{"chooooovy", "pobi"}))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 자동차 이름은 1글자 이상 5글자 이하여야 합니다");
     }
 
     @Test
     void CarNames_자동차_수가_LIMIT_이상이라면_에러를_발생시킨다() {
-        Assertions.assertThatThrownBy(() -> new CarNames(new String[] {
-                "pobi", "pobi1", "pobi2", "pobi3", "pobi4", "pobi5"
-        })).hasMessage("[ERROR] 자동차 수는 5대를 초과해 입력받을 수 없습니다");
+        Assertions.assertThatThrownBy(() -> new CarNames(
+                createCarNamesOverLimit()
+        )).hasMessage("[ERROR] 자동차 수는 5대를 초과해 입력받을 수 없습니다");
     }
 
     @Test
     void CarNames_자동차_이름이_중복되면_에러를_발생시킨다() {
-        Assertions.assertThatThrownBy(() -> new CarNames(new String[] {"pobi", "pobi"}))
+        Assertions.assertThatThrownBy(() -> new CarNames(new String[]{"pobi", "pobi"}))
                 .hasMessage("[ERROR] 자동차 이름은 중복되어선 안됩니다");
+    }
+
+
+    private String[] createCarNamesOverLimit() {
+        final int LIMIT = 5;
+        final String name = "pobi";
+
+        return IntStream.rangeClosed(0, LIMIT)
+                .mapToObj(i -> name + i)
+                .toArray(String[]::new);
     }
 }
