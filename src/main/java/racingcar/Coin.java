def9e8f557c7902445ad73781ec59c7d166c45a6
SeokyungKim <kskyung0624@gmail.com>
2022-12-02 16:45:03 +0900

    feat(Coin) : 게임 수행의 최대 수를 제한하도록 검증하는 로직을 추가한다

--- src/main/java/racingcar/Coin.java
+++ src/main/java/racingcar/Coin.java
@@ -1,9 +1,17 @@
 package racingcar;
 
 public class Coin {
+    private static final int LIMIT = 10_000;
     private int amount;
 
     public Coin(int amount) {
+        validate(amount);
         this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount > LIMIT) {
+            throw new IllegalArgumentException("[ERROR] 게임 실행 횟수는 " + LIMIT + "번 이상 수행될 수 없습니다");
+        }
     }
 }
