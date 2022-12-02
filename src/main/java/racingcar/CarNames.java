b0e98faf2fe6b80c3f149aee1101240e1573f56a
SeokyungKim <kskyung0624@gmail.com>
2022-12-02 17:06:59 +0900

    feat(CarNames) : 자동차 수와 이름 중복 검사 검증 로직을 추가한다

--- src/main/java/racingcar/CarNames.java
+++ src/main/java/racingcar/CarNames.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class CarNames {
+    private static final int LIMIT = 5;
     private String[] names;
     private Iterator<String> iterator;
 
     public CarNames(String[] input) {
-        validateRange(input);
+        validate(input);
         this.names = input;
         iterator = Arrays.stream(names).iterator();
     }
@@ -25,6 +26,27 @@ public class CarNames {
         throw new IllegalArgumentException("[ERROR] CarName이 더이상 존재하지 않습니다");
     }
 
+    private void validate(String[] input) {
+        validateRange(input);
+        validateSize(input);
+        validateDuplicate(input);
+    }
+
+    private void validateDuplicate(String[] input) {
+        if (isDuplicated(input)) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 중복되어선 안됩니다");
+        }
+    }
+
+    private boolean isDuplicated(String[] input) {
+        return input.length != Arrays.stream(input).distinct().count();
+    }
+
+    private void validateSize(String[] input) {
+        if (input.length > LIMIT) {
+            throw new IllegalArgumentException("[ERROR] 자동차 수는 " + LIMIT + "대를 초과해 입력받을 수 없습니다");
+        }
+    }
 
     private void validateRange(String[] input) {
         boolean isOver = Arrays.stream(input)
--- src/test/java/racingcar/CarNamesTest.java
+++ src/test/java/racingcar/CarNamesTest.java
@@ -23,4 +23,17 @@ public class CarNamesTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 자동차 이름은 1글자 이상 5글자 이하여야 합니다");
     }
+
+    @Test
+    void CarNames_자동차_수가_LIMIT_이상이라면_에러를_발생시킨다() {
+        Assertions.assertThatThrownBy(() -> new CarNames(new String[] {
+                "pobi", "pobi1", "pobi2", "pobi3", "pobi4", "pobi5"
+        })).hasMessage("[ERROR] 자동차 수는 5대를 초과해 입력받을 수 없습니다");
+    }
+
+    @Test
+    void CarNames_자동차_이름이_중복되면_에러를_발생시킨다() {
+        Assertions.assertThatThrownBy(() -> new CarNames(new String[] {"pobi", "pobi"}))
+                .hasMessage("[ERROR] 자동차 이름은 중복되어선 안됩니다");
+    }
 }
