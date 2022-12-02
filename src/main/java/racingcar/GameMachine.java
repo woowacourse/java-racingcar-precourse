c408494abe617ed3c6ad4c4003d57b199a0f7fb7
SeokyungKim <kskyung0624@gmail.com>
2022-12-02 16:20:58 +0900

    feat(Cars) : Car들을 한 번에 관리하는 Cars를 구현한다

---
+++ src/main/java/racingcar/GameMachine.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class GameMachine {
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+
+    public void play() {
+        Cars cars = createCars(inputView.readCarNames());
+        Coin coin = inputView.readCoins();
+    }
+
+    private Cars createCars(CarNames carNames) {
+        return new Cars(
+                IntStream.rangeClosed(1, carNames.amount())
+                        .mapToObj(i -> new Car(carNames.next()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
