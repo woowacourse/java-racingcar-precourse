1f078400a178ce5511f031e98019c1687c58d91a
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 08:05:52 +0900

    refactor: 수정 outputView
    상수화가 안된 메시지가 있어서 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -11,9 +11,9 @@ public class OutputView {
 
     private static final String MESSAGE_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MESSAGE_INPUT_TRY = "시도할 회수는 몇회인가요?";
+    private static final String MESSAGE_RESULT = "실행 결과";
     private static final String MOVE_MARK = "-";
     private static final String JOIN_NAME_AND_POSITION = " : ";
-
     private static final String WINNER_DELIMITER = ",";
     private static final String MESSAGE_WINNER_PREFIX = "최종 우승자 : ";
     private static final String MESSAGE_WINNER_SUFFIX = "";
@@ -35,7 +35,7 @@ public class OutputView {
     }
 
     public void printResult(Cars cars) {
-        System.out.println("실행 결과");
+        System.out.println(MESSAGE_RESULT);
         for(Car car : cars.getCars()){
             System.out.println(car.getName() + JOIN_NAME_AND_POSITION + convertMoveMark(car.getPosition()));
         }
