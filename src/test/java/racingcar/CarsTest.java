c3f9689b3950673ab567ac9c97cf44ff304e5f2d
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 03:41:04 +0900

    test: 추가 주어진 횟수 동안 n대의 자동차는 전진 또는 멈춤 테스트기능

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,11 +1,17 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.Car;
+import racingcar.domain.CarMoveNumberGenerator;
 import racingcar.domain.Cars;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.util.Lists.newArrayList;
 
 public class CarsTest {
 
@@ -16,9 +22,9 @@ public class CarsTest {
     @ValueSource(strings = {"pobii,pop,p", "p", "pob,pb,bb,dcd,wqwqg,xvd,aw,be", "pb,,pob,,,bo"})
     void createCarsSuccess(String input) {
         boolean result = true;
-        try{
+        try {
             Cars.createCarNameByWord(input);
-        }catch (IllegalArgumentException exception) {
+        } catch (IllegalArgumentException exception) {
             result = false;
         }
         assertThat(result).isEqualTo(true);
@@ -27,9 +33,24 @@ public class CarsTest {
     @DisplayName("Cars 객체 생성시 중복되는 자동차 이름이있다")
     @ParameterizedTest
     @ValueSource(strings = {"d,gme,d", "wwwww,ef,weah,wwwww,wwwww", "d,d,d,d,d,d,d,d,d,d"})
-    void createCarsByDuplicatedName (String input) {
+    void createCarsByDuplicatedName(String input) {
         assertThatThrownBy(() -> Cars.createCarNameByWord(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(EXCEPTION_MESSAGE_CARS_DUPLICATED_NAME);
+    }
+
+    @DisplayName("모든 자동차가 정상적으로 움직인다.")
+    @Test
+    void moveCarsSuccess() {
+        Cars cars = Cars.createCarNameByWord("pobi,randi,rudy");
+        CarMoveNumberGenerator carMoveNumberGenerator
+                = new CarTest.TestNumberGenerator(newArrayList(4, 2, 6, 9, 5, 6, 4, 0, 1));
+        cars.move(carMoveNumberGenerator);
+        cars.move(carMoveNumberGenerator);
+        cars.move(carMoveNumberGenerator);
+
+        assertThat(cars.getCars().get(0)).extracting("position").isEqualTo(3);
+        assertThat(cars.getCars().get(1)).extracting("position").isEqualTo(1);
+        assertThat(cars.getCars().get(2)).extracting("position").isEqualTo(2);
     }
 }
