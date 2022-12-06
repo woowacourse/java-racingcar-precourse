9f8abc4c52280cff05f19f312a6d8ca8ded5dc9d
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 02:41:46 +0900

    test: 추가 자동차는 전진 또는 멈춤 기능 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,12 +1,17 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
+import racingcar.domain.CarMoveNumberGenerator;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.util.Lists.newArrayList;
 
 public class CarTest {
 
@@ -32,5 +37,49 @@ public class CarTest {
         assertThatThrownBy(() -> new Car(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(EXCEPTION_MESSAGE_CAR_NAME_LENGTH);
+    }
+
+    @DisplayName("자동차가 4 이상의 숫자가 들어올시 움직인다.")
+    @Test
+    void moveCarSuccess() {
+        Car car = new Car("pobi");
+        CarMoveNumberGenerator carMoveNumberGenerator
+                = new TestNumberGenerator(newArrayList(4, 5, 6, 7, 8, 9));
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+
+        assertThat(car).extracting("position").isEqualTo(6);
+    }
+
+    @DisplayName("자동차가 4 이하의 숫자가 들어올시 움직이지 않는다.")
+    @Test
+    void moveCarFail() {
+        Car car = new Car("pobi");
+        CarMoveNumberGenerator carMoveNumberGenerator
+                = new TestNumberGenerator(newArrayList(0, 1, 2, 3));
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+        car.move(carMoveNumberGenerator);
+
+        assertThat(car).extracting("position").isEqualTo(0);
+    }
+
+    static class TestNumberGenerator implements CarMoveNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
     }
 }
