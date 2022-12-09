e645cb2c605c0453611f56690206b33b0bb54d14
Daeeun Ko <7273322@naver.com>
2022-12-09 16:26:35 +0900

    feat: validate if car name is duplicate

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,12 +1,16 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
+
+    private static final String DUPLICATE_CAR_NAME_MESSAGE = "[ERROR] 자동차 이름이 중복되었습니다.";
 
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
+        validate(cars);
         this.cars = cars;
     }
 
@@ -18,5 +22,18 @@ public class Cars {
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    private void validate(List<Car> cars) {
+        if (isDuplicate(cars)) {
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME_MESSAGE);
+        }
+    }
+
+    private boolean isDuplicate(List<Car> cars) {
+        return cars.stream()
+                .map(Car::getName)
+                .collect(Collectors.toSet())
+                .size() != cars.size();
     }
 }
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -8,8 +8,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
+
+    private final String ERROR = "[ERROR]";
 
     @DisplayName("숫자가 4 이상이라면 자동차들은 전진한다.")
     @Test
@@ -33,5 +36,15 @@ class CarsTest {
 
         assertThat(cars.getCars().stream().map(Car::getPosition))
                 .containsExactly(0, 0);
+    }
+
+    @DisplayName("자동차 이름이 중복되면 예외가 발생한다.")
+    @Test
+    void duplicateCarName() {
+        List<Car> car = Stream.of("test", "test").map(Car::new).collect(Collectors.toList());
+
+        assertThatThrownBy(() -> new Cars(car))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
     }
 }
