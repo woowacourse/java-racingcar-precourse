2140df27517d9f724f73e34d3dd1d15c07723afc
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-10 13:26:09 +0900

    feat: create Car move method

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import racingcar.util.Number;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -10,6 +12,6 @@ public class Car {
 
     // 추가 기능 구현
     private void move(int moveInput){
-
+        if(moveInput >= Number.STANDARD_NUMBER.getNumber())position++;
     }
 }
--- src/main/java/racingcar/util/Number.java
+++ src/main/java/racingcar/util/Number.java
@@ -2,7 +2,8 @@ package racingcar.util;
 
 public enum Number {
     START_NUMBER(0),
-    LAST_NUMBER(9);
+    LAST_NUMBER(9),
+    STANDARD_NUMBER(4);
     private int number;
 
     Number(int number) {
