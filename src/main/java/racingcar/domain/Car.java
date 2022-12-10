44737bfdf2bd73220d9cc49033594f1382e82496
Choi-JJunho <junho5336@gmail.com>
2022-12-10 13:51:57 +0900

    test(Car): add CarTest

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class CarTest {
+
+    Car car = new Car("붕붕이");
+
+    @Test
+    void Car_객체의_이름은_생성자의_인자로_결정된다() {
+        Assertions.assertThat(car.getName()).isEqualTo("붕붕이");
+    }
+
+    @Test
+    void move_메소드는_자동차가_이동하고난_뒤의_위치를_반환한다() {
+        //given
+        int position = car.getPosition();
+        //when
+        car.move();
+        //then
+        Assertions.assertThat(car.getPosition())
+                .isEqualTo(position + 1);
+    }
+}
