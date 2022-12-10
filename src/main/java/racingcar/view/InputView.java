0ebbb85b75e0483f8cc58b7114ee7fbeee886ce0
Choi-JJunho <junho5336@gmail.com>
2022-12-10 17:38:19 +0900

    feat(InputView): add validateNumber for InputTryCount

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static racingcar.utils.ErrorMessage.TRY_COUNT_MUST_BE_NUMBER;
+
 public class InputView {
 
     private final OutputView outputView;
@@ -24,12 +26,22 @@ public class InputView {
         List<Car> cars = Arrays.stream(split)
                 .map(Car::new)
                 .collect(Collectors.toList());
+
         return new CarGroup(cars);
     }
 
     public int inputTryCount() {
         outputView.printInputTryCount();
         String input = Console.readLine();
+        validateNumber(input);
         return Integer.parseInt(input);
+    }
+
+    private void validateNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(TRY_COUNT_MUST_BE_NUMBER.getMessage());
+        }
     }
 }
