15215a99dde9c800d4a5b8a0cb851f7ba66473c1
juno-junho <ssmm0205@naver.com>
2022-12-12 15:50:34 +0900

    feat(Util): int보다 큰 값을 입력했을 경우 체크

--- src/main/java/racingcar/util/Util.java
+++ src/main/java/racingcar/util/Util.java
@@ -3,10 +3,11 @@ package racingcar.util;
 public class Util {
     public static int convertStringToInt(String input) {
         try {
-            return Integer.parseInt(input);
+            long longValue = Long.parseLong(input);
+            Validator.validateNumberValue(longValue);
+            return (int) longValue;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력하셔야 합니다");
         }
     }
-
 }
