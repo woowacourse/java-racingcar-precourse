a685a17578bb497ea11e9ba8e93251967d003202
juno-junho <ssmm0205@naver.com>
2022-12-12 17:14:52 +0900

    feat(InputView): 예외발생시 다시 입력받는 기능 구현 완료

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,16 +7,26 @@ import racingcar.util.Validator;
 public class InputView {
 
     public String readCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        Validator.validateCarName(carNames);
-        return carNames;
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carNames = Console.readLine();
+            Validator.validateCarName(carNames);
+            return carNames;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return readCarName();
     }
 
     public int readGameCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        int gameCount = Util.convertStringToInt(Console.readLine());
-        Validator.validateGameCount(gameCount);
-        return gameCount;
+        try {
+            System.out.println("시도할 회수는 몇회인가요?");
+            int gameCount = Util.convertStringToInt(Console.readLine());
+            Validator.validateGameCount(gameCount);
+            return gameCount;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return readGameCount();
     }
 }
