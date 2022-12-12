9c1b09855aef25c7e1296dd241781115ff648e48
Choi-JJunho <junho5336@gmail.com>
2022-12-12 22:55:50 +0900

    refactor(TryCount): remove unused Constructor

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -5,10 +5,6 @@ import static racingcar.message.ErrorMessage.TRY_COUNT_MUST_BE_NUMBER;
 public class TryCount {
     private final int tryCount;
 
-    public TryCount(int tryCount) {
-        this.tryCount = tryCount;
-    }
-
     public TryCount(String tryCount) {
         validateNumber(tryCount);
         this.tryCount = Integer.parseInt(tryCount);
