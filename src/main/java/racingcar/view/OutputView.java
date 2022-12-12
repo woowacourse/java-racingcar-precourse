cf2979f782a803c0487a8cdeaced012f40618c5b
juno-junho <ssmm0205@naver.com>
2022-12-12 16:23:35 +0900

    feat(OutputView): 결과를 출력하는 메서드 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
 public class OutputView {
+
+    public void printResult(String result) {
+        System.out.println(result);
+    }
+
+    public void printResultMessage() {
+        System.out.println("실행 결과");
+    }
 }
