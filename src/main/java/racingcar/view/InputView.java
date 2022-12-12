44acfb93ed4804d37db28777c2690e439f26fce9
juno-junho <ssmm0205@naver.com>
2022-12-12 15:41:54 +0900

    feat(InputView): 시도 횟수를 입력받는 메서드 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,20 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.util.Util;
 import racingcar.util.Validator;
 
 public class InputView {
 
-    public void readCarName() {
+    public String readCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
         String carNames = Console.readLine();
         Validator.validateCarName(carNames);
+        return carNames;
     }
 
-
+    public int readGameCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Util.convertStringToInt(Console.readLine());
+    }
 }
