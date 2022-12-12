4e9050be9c8f401cf8aec697f253ba50d83f4ed0
juno-junho <ssmm0205@naver.com>
2022-12-12 16:47:18 +0900

    feat(OutputView): 최종 우승자를 출력하는 메서드 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ import racingcar.controller.RacingGameController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
         RacingGameController controller = new RacingGameController();
         controller.run();
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
     public void printResultMessage() {
         System.out.println("실행 결과");
     }
+
+    public void printWinner(String winner) {
+        System.out.println("최종 우승자 : " + winner);
+    }
 }
