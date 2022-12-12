26cb4bf366f47fa487dad6b2db7298d3e2c661b7
juno-junho <ssmm0205@naver.com>
2022-12-12 16:25:08 +0900

    feat(Car): getter와 게임 진행시 position 증가 메서드 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +11,17 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public void playGame(){
+        if(Randoms.pickNumberInRange(0, 9) >= 4){
+            position += 1;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
