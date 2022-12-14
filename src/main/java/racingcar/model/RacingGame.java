7c43478566e048bc76acdb17753705b94bd375fa
yoondgu <doyou2ing@naver.com>
2022-12-14 21:22:55 +0900

    fix: 자동차 이름 저장 기능에 중복 검증 로직 추가

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.dto.CarDTO;
@@ -17,13 +18,16 @@ public class RacingGame {
     }
 
     public void enrollCars(List<String> carNames) {
-        validateCarCount(carNames.size());
+        validateCarNames(carNames);
         carNames.forEach(name -> racingCars.add(new Car(name)));
     }
 
-    private void validateCarCount(int count) {
-        if (count < GameRule.CARS_COUNT_LOWER_LIMIT) {
+    private void validateCarNames(List<String> carNames) {
+        if (carNames.size() < GameRule.CARS_COUNT_LOWER_LIMIT) {
             throw new IllegalArgumentException(ErrorMessage.CARS_LACK_OF_COUNT);
+        }
+        if (carNames.size() != new HashSet<>(carNames).size()) {
+            throw new IllegalArgumentException(ErrorMessage.CARS_DUPLICATED_NAME);
         }
     }
 
