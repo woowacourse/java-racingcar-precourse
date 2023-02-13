ac2841e587c67667c9276aa47c93c0fafdeecbbf
LEEDAESEUNG-Developer <eotmd1016@gmail.com>
2023-02-13 12:58:34 +0900

    테스트 입니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        Scanner scanner = new Scanner(System.in);
+        String carName;
+        int num;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carName = scanner.next();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        try{
+            num = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("타입 오류");
+        }
     }
 }
