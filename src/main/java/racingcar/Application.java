cfa16f25bda14804f0f75cd94f86da1ff3bc9479
junseoplee <junseoplee@soongsil.ac.kr>
2024-01-02 18:45:33 +0900

    유효성 검사 메서드를 Car 클래스 안으로 이동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -25,7 +25,7 @@ public class Application {
             String input = Console.readLine();
             carNames = Arrays.asList(input.split(",", -1));
             try { // 예외가 발생한다면? -> catch
-                validateCarNames(carNames); // 유효성 검사
+                Car.validateCarNames(carNames); // 유효성 검사
                 break;
             }
             catch (IllegalArgumentException e) {
@@ -33,19 +33,6 @@ public class Application {
             }
         }
         return carNames;
-    }
-
-    private static void validateCarNames(List<String> carNames) {
-        for (String inputName : carNames) {
-            int MAX_NAME_LENGTH = 5;
-            if (inputName.isEmpty() || inputName.length() > MAX_NAME_LENGTH) {
-                throw new IllegalArgumentException();
-            }
-        }
-        Set<String> uniqueNames = new HashSet<>(carNames); // 중복을 허용하지 않는 자료구조
-        if (uniqueNames.size() < carNames.size()) { // 작다면 중복된 이름이 있다는 것을 의미함
-            throw new IllegalArgumentException();
-        }
     }
 
     private static int receiveTryCount() {
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,6 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Car {
     private final String name;
     private int position;
@@ -24,5 +28,18 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public static void validateCarNames(List<String> carNames) {
+        for (String inputName : carNames) {
+            int MAX_NAME_LENGTH = 5;
+            if (inputName.isEmpty() || inputName.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
+        Set<String> uniqueNames = new HashSet<>(carNames); // 중복을 허용하지 않는 자료구조
+        if (uniqueNames.size() < carNames.size()) { // 작다면 중복된 이름이 있다는 것을 의미함
+            throw new IllegalArgumentException();
+        }
     }
 }
