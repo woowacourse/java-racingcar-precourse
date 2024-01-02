06548c066d2c18ab00f793f2a997bccb8dc97b5a
junseoplee <junseoplee@soongsil.ac.kr>
2024-01-02 17:01:21 +0900

    멤버 변수는 되도록 생성자에서 초기화 하도록 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,10 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private final String name;
-    private int position = 0;
+    private int position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
     public void moveForward() {
