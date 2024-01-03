b696c3d8dc735c9037839a3c075264a90c21e306
junseoplee <junseoplee@soongsil.ac.kr>
2024-01-03 18:14:24 +0900

    fix: 와일드 카드 사용하지 않기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
